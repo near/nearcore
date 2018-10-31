5261e5de493046dea6fa0fd787cd5a6625d51ec1
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2018-10-31 14:55:36 -0700

    Add scripts to init and build wasm

---
+++ scripts/common.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ROOT=`dirname "$0"`
+
+# A list of directories which contain wasm projects.
+SRCS=(
+	"core/executor/wasm"
+	"node/runtime/wasm"
+)
+
+# Make pushd/popd silent.
+
+pushd () {
+	command pushd "$@" > /dev/null
+}
+
+popd () {
+	command popd "$@" > /dev/null
+}
