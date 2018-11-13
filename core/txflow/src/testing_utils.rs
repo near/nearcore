8ed29ea368e5020572101a44fc6a098223d8aaa7
Maksym Zavershynskyi <max@near.ai>
2018-11-13 14:20:21 -0800

    Add more tests for TxFlow DAG

--- core/txflow/src/testing_utils.rs
+++ core/txflow/src/testing_utils.rs
@@ -2,7 +2,7 @@ use primitives::traits::{Payload, WitnessSelector};
 use primitives::types::UID;
 use std::hash::{Hash, Hasher};
 
-#[derive(Hash, Clone)]
+#[derive(Hash, Clone, Debug)]
 pub struct FakePayload {}
 
 impl Payload for FakePayload {
@@ -65,79 +65,88 @@ pub fn simple_message<'a, W>(owner_uid: UID, epoch: u64,
 /// Create two messages with `owner_uid=0`, `epoch=2` and `owner_uid=1`, `epoch=3`.
 ///
 /// ```
-/// simple_bare_messages!(arena [0, 2; 1, 3;]);
+/// simple_bare_messages!(arena, all_messages [0, 2; 1, 3;]);
 /// ```
 ///
 /// Create two messages and save a reference to the first message in `a`.
 ///
 /// ```
 /// let a;
-/// simple_bare_messages!(arena [0, 2 => a; 1, 3;]);
+/// simple_bare_messages!(arena, all_messages [0, 2 => a; 1, 3;]);
 /// ```
 ///
 /// Create two messages and link them to the third message as parents.
 ///
 /// ```
-/// simple_bare_messages!(arena [[0, 2; 1, 3;] => 0, 4;]);
+/// simple_bare_messages!(arena, all_messages [[0, 2; 1, 3;] => 0, 4;]);
 /// ```
 ///
 /// Reuse some message instead of creating a new one.
 ///
 /// ```
 /// let a;
-/// simple_bare_messages!(arena [[0, 0 => a; 1, 2;] => 2, 3;]);
-/// simple_bare_messages!(arena [[=> a; 3, 4;] => 4, 5;]);
+/// simple_bare_messages!(arena, all_messages [[0, 0 => a; 1, 2;] => 2, 3;]);
+/// simple_bare_messages!(arena, all_messages [[=> a; 3, 4;] => 4, 5;]);
 /// ```
 ///
 /// Create several forests with different structure.
 ///
 /// ```
-/// simple_bare_messages!(arena [[0, 1;] => 0, 5; [0, 2; 1, 3;] => 0, 4;]);
-/// simple_bare_messages!(arena [0, 1; [0, 2; 1, 3;] => 0, 4;]);
+/// simple_bare_messages!(arena, all_messages [[0, 1;] => 0, 5; [0, 2; 1, 3;] => 0, 4;]);
+/// simple_bare_messages!(arena, all_messages [0, 1; [0, 2; 1, 3;] => 0, 4;]);
 /// ```
 ///
 macro_rules! simple_bare_messages {
-    ($arena:ident, $messages:ident [  ]) => (());
+    ($arena:ident, $all_messages:ident, $messages:ident [  ]) => (());
 
-    ($arena:ident, $messages:ident [ [ $($parents:tt)* ] => $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
-        let ps = simple_bare_messages!($arena [ $($parents)* ]);
-        $messages.push(&*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, ps)));
-        simple_bare_messages!($arena, $messages [$($rest)*]);
+    ($arena:ident, $all_messages:ident, $messages:ident [ [ $($parents:tt)* ] => $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
+        let ps = simple_bare_messages!($arena, $all_messages [ $($parents)* ]);
+        let r = &*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, ps));
+        $all_messages.push(r);
+        $messages.push(r);
+        simple_bare_messages!($arena, $all_messages, $messages [$($rest)*]);
     }};
 
-    ($arena:ident, $messages:ident [ [ $($parents:tt)* ] => $owner:expr, $epoch:expr => $name:ident; $($rest:tt)* ]) => {{
-        let ps = simple_bare_messages!($arena [ $($parents)* ]);
+    ($arena:ident, $all_messages:ident, $messages:ident [ [ $($parents:tt)* ] => $owner:expr, $epoch:expr => $name:ident; $($rest:tt)* ]) => {{
+        let ps = simple_bare_messages!($arena, $all_messages [ $($parents)* ]);
         $name = &*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, ps));
+        $all_messages.push($name);
         $messages.push($name);
-        simple_bare_messages!($arena, $messages [$($rest)*]);
+        simple_bare_messages!($arena, $all_messages, $messages [$($rest)*]);
     }};
 
-    ($arena:ident, $messages:ident [ => $element:expr; $($rest:tt)* ]) => {{
+    ($arena:ident, $all_messages:ident, $messages:ident [ => $element:expr; $($rest:tt)* ]) => {{
+        $all_messages.push($element);
         $messages.push($element);
-        simple_bare_messages!($arena, $messages [$($rest)*]);
+        simple_bare_messages!($arena, $all_messages, $messages [$($rest)*]);
     }};
 
-    ($arena:ident, $messages:ident [ $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
-        $messages.push(&*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, vec![])));
-        simple_bare_messages!($arena, $messages [ $($rest)* ]);
+    ($arena:ident, $all_messages:ident, $messages:ident [ $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
+        let r = &*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, vec![]));
+        $all_messages.push(r);
+        $messages.push(r);
+        simple_bare_messages!($arena, $all_messages, $messages [ $($rest)* ]);
     }};
 
-    ($arena:ident, $messages:ident [ $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
-        $messages.push(&*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, vec![])));
-        simple_bare_messages!($arena, $messages [ $($rest)* ]);
+    ($arena:ident, $all_messages:ident, $messages:ident [ $owner:expr, $epoch:expr; $($rest:tt)* ]) => {{
+        let r = &*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, vec![]));
+        $all_messages.push(r);
+        $messages.push(r);
+        simple_bare_messages!($arena, $all_messages, $messages [ $($rest)* ]);
     }};
 
-    ($arena:ident, $messages:ident [ $owner:expr, $epoch:expr => $name:ident; $($rest:tt)* ]) => {{
+    ($arena:ident, $all_messages:ident, $messages:ident [ $owner:expr, $epoch:expr => $name:ident; $($rest:tt)* ]) => {{
         $name = &*$arena.alloc(::testing_utils::simple_bare_message($owner, $epoch, vec![]));
+        $all_messages.push($name);
         $messages.push($name);
-        simple_bare_messages!($arena, $messages [ $($rest)* ]);
+        simple_bare_messages!($arena, $all_messages, $messages [ $($rest)* ]);
     }};
 
-    ($arena:ident [ $($rest:tt)* ]) => {{
+    ($arena:ident, $all_messages:ident [ $($rest:tt)* ]) => {{
         let mut v = vec![];
         {
           let p = &mut v;
-          simple_bare_messages!($arena, p [ $($rest)* ]);
+          simple_bare_messages!($arena, $all_messages, p [ $($rest)* ]);
         }
         v
     }};
@@ -212,8 +221,9 @@ mod tests {
     #[test]
     fn flat_bare_messages() {
         let arena = Arena::new();
+        let mut all = vec![];
         let a;
-        let v = simple_bare_messages!(arena [0, 0 => a; 1, 2;]);
+        let v = simple_bare_messages!(arena, all [0, 0 => a; 1, 2;]);
         assert_eq!(v.len(), 2);
         assert_eq!((&a.body).epoch, 0);
     }
@@ -221,8 +231,9 @@ mod tests {
     #[test]
     fn link_bare_messages() {
         let arena = Arena::new();
+        let mut all = vec![];
         let a;
-        let v = simple_bare_messages!(arena [[0, 0 => a; 1, 2;] => 2, 3; [0, 1; 2, 3;] => 3, 4;]);
+        let v = simple_bare_messages!(arena, all [[0, 0 => a; 1, 2;] => 2, 3; [0, 1; 2, 3;] => 3, 4;]);
         assert_eq!(v.len(), 2);
         assert_eq!(v[0].body.parents.len(), 2);
     }
@@ -230,16 +241,18 @@ mod tests {
     #[test]
     fn reuse_bare_messages() {
         let arena = Arena::new();
+        let mut all = vec![];
         let a;
-        simple_bare_messages!(arena [[0, 0 => a; 1, 2;] => 2, 3;]);
-        simple_bare_messages!(arena [[=> a; 3, 4;] => 4, 5;]);
+        simple_bare_messages!(arena, all [[0, 0 => a; 1, 2;] => 2, 3;]);
+        simple_bare_messages!(arena, all [[=> a; 3, 4;] => 4, 5;]);
     }
 
     #[test]
     fn bare_several_trees() {
         let arena = Arena::new();
-        simple_bare_messages!(arena [[0, 1;] => 0, 5; [0, 2; 1, 3;] => 0, 4;]);
-        simple_bare_messages!(arena [0, 1; [0, 2; 1, 3;] => 0, 4;]);
+        let mut all = vec![];
+        simple_bare_messages!(arena, all [[0, 1;] => 0, 5; [0, 2; 1, 3;] => 0, 4;]);
+        simple_bare_messages!(arena, all [0, 1; [0, 2; 1, 3;] => 0, 4;]);
     }
 
   struct FakeWitnessSelector {
