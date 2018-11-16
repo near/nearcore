0b12e2f66c368b86d19a67eed1b1575e25c520e6
haydn dufrene <haydn.dufrene@gmail.com>
2018-11-16 12:07:31 -0800

    rpc: remove clone,copy from RpcImpl

--- node/service/src/rpc/api.rs
+++ node/service/src/rpc/api.rs
@@ -1,4 +1,4 @@
-use jsonrpc_core::Result as JsonRpcResult;
+use jsonrpc_core::{IoHandler, Result as JsonRpcResult};
 
 use client::Client;
 use primitives::types::TransactionBody;
@@ -11,7 +11,7 @@ build_rpc_trait! {
     }
 }
 
-#[derive(Default, Copy, Clone)]
+#[derive(Default)]
 pub struct RpcImpl {
     client: Client,
 }
@@ -19,5 +19,35 @@ pub struct RpcImpl {
 impl TransactionApi for RpcImpl {
     fn rpc_receive_transaction(&self, t: TransactionBody) -> JsonRpcResult<()> {
         Ok(self.client.receive_transaction(&t))
+    }
+}
+
+pub fn get_handler() -> IoHandler {
+    let mut io = IoHandler::new();
+    io.extend_with(RpcImpl::default().to_delegate());
+    io
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    extern crate jsonrpc_test;
+    extern crate primitives;
+
+    use self::jsonrpc_test::Rpc;
+    use primitives::types::TransactionBody;
+
+    #[test]
+    fn test_call() {
+        let handler = get_handler();
+        let rpc = Rpc::from(handler);
+        let t = TransactionBody {
+            nonce: 0,
+            sender: 1,
+            receiver: 0,
+            amount: 0,
+        };
+        assert_eq!(rpc.request("receive_transaction", &[t]), "null");
     }
 }
