a967f1771d8741bee471fc2d0c0aa8fbb3f8d042
Maksym Zavershynskyi <max@near.ai>
2018-11-15 17:24:38 -0800

    Allow self-referential TxFlowTask

--- core/txflow/src/txflow_task.rs
+++ core/txflow/src/txflow_task.rs
@@ -1,9 +1,10 @@
 use futures::sync::mpsc;
-use futures::{Stream, Async, Poll};
+use futures::{Stream, Poll};
 
 use primitives::types::{UID, Gossip};
 use primitives::traits::{Payload, WitnessSelector};
 use dag::DAG;
+
 
 pub struct TxFlowTask<'a, P: Payload, W: WitnessSelector> {
     owner_uid: UID,
@@ -11,23 +12,26 @@ pub struct TxFlowTask<'a, P: Payload, W: WitnessSelector> {
     messages_receiver: mpsc::Receiver<Gossip<P>>,
     payload_receiver: mpsc::Receiver<P>,
     messages_sender: mpsc::Sender<Gossip<P>>,
-    dag: Option<DAG<'a, P, W>>,
+    witness_selector: Box<W>,
+    dag: Option<Box<DAG<'a, P, W>>>,
 }
 
-impl<'a, P: Payload, W: WitnessSelector> TxFlowTask<'a, P, W>{
-    pub fn new(owner_uid: UID,
-               starting_epoch: u64,
-               messages_receiver: mpsc::Receiver<Gossip<P>>,
-               payload_receiver: mpsc::Receiver<P>,
-               messages_sender: mpsc::Sender<Gossip<P>>) -> Self {
-        let result = Self {
+impl<'a, P: Payload, W: WitnessSelector> TxFlowTask<'a, P, W> {
+    fn new(owner_uid: UID,
+           starting_epoch: u64,
+           messages_receiver: mpsc::Receiver<Gossip<P>>,
+           payload_receiver: mpsc::Receiver<P>,
+           messages_sender: mpsc::Sender<Gossip<P>>,
+           witness_selector: Box<W>) -> Self {
+        Self {
             owner_uid,
             starting_epoch,
             messages_receiver,
             payload_receiver,
             messages_sender,
-            dag: None};
-        result
+            witness_selector,
+            dag: None,
+        }
     }
 }
 
@@ -36,6 +40,12 @@ impl<'a, P: Payload, W: WitnessSelector> Stream for TxFlowTask<'a, P, W> {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        // Check if DAG needs to be created.
+        if self.dag.is_none() {
+            let witness_ptr = self.witness_selector.as_ref() as *const W;
+            self.dag = Some(Box::new(
+                DAG::new(self.owner_uid, self.starting_epoch, unsafe {&*witness_ptr})));
+        }
         unimplemented!()
     }
 }
