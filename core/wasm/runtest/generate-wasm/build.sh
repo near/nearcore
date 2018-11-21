67e3261c09f59ab340d5e6adf232601040747d4a
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2018-11-21 15:32:50 -0800

    Adding code that generates .wasm for a test

---
+++ core/wasm/runtest/generate-wasm/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd $(dirname "$0")/to-wasm
+cargo +nightly build --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/to_wasm.wasm ../
+wasm-gc ../to_wasm.wasm
+popd
+
+pushd $(dirname "$0")/import-memory
+cargo +nightly run --release
+wasm-gc ../../res/wasm_with_mem.wasm
+popd
