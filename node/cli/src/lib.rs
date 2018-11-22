557b54efff24836676c6c041836779029c548d5a
haydn dufrene <haydn.dufrene@gmail.com>
2018-11-21 16:23:56 -0800

    cli: add clap, take in base_path

--- node/cli/src/lib.rs
+++ node/cli/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate beacon;
+extern crate clap;
 extern crate client;
 extern crate network;
 extern crate primitives;
@@ -11,20 +12,30 @@ extern crate service;
 extern crate storage;
 
 use beacon::types::BeaconBlockHeader;
+use clap::{App, Arg};
 use client::Client;
 use network::protocol::ProtocolConfig;
 use network::service::NetworkConfiguration;
 use network::service::Service as NetworkService;
 use service::network_handler::NetworkHandler;
 use service::run_service;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use storage::{DiskStorage, Storage};
 
 mod chain_spec;
 
-pub fn run() {
-    // TODO: add argument parsing into service/config.rs.
-    let storage: Arc<Storage> = Arc::new(DiskStorage::new("storage/db/"));
+fn get_storage_path(base_path: &Path) -> PathBuf {
+    let mut path = base_path.to_owned();
+    path.push("storage/db");
+    path
+}
+
+fn start_service(base_path: &Path) {
+    let storage_path = get_storage_path(base_path);
+    let storage: Arc<Storage> = Arc::new(
+        DiskStorage::new(&storage_path.to_string_lossy())
+    );
     let client = Arc::new(Client::new(storage));
     let network_handler = NetworkHandler {
         client: client.clone(),
@@ -36,4 +47,23 @@ pub fn run() {
         client.clone(),
     ).unwrap();
     run_service::<_, _, BeaconBlockHeader>(client.clone(), &network);
+}
+
+pub fn run() {
+    let matches = App::new("near")
+        .arg(
+            Arg::with_name("base_path")
+                .short("b")
+                .long("base-path")
+                .value_name("PATH")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        ).get_matches();
+
+    let base_path = matches
+        .value_of("base_path")
+        .map(|x| Path::new(x))
+        .unwrap_or_else(|| Path::new("."));
+
+    start_service(base_path);
 }
