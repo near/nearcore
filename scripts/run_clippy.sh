7379ec58f54b0f5ff3308ad6836b5f5399cb5e09
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2018-11-23 13:04:01 -0800

    Disable clippy::useless-let-if-seq
    It does not understand when return, break, continue are used. Relevant: https://github.com/rust-lang/rust-clippy/issues/2918

--- scripts/run_clippy.sh
+++ scripts/run_clippy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo clippy --all  -- -A clippy::unit_arg -A clippy::if_same_then_else -A clippy::collapsible_if -D warnings
+cargo clippy --all  -- -A clippy::unit_arg -A clippy::if_same_then_else -A clippy::collapsible_if -A clippy::useless-let-if-seq -D warnings
