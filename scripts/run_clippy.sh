e436895e984cc1c4de63b27897a2091dbe295060
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2018-11-25 19:25:38 -0800

    This should not be a blocker on CI

--- scripts/run_clippy.sh
+++ scripts/run_clippy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo clippy --all  -- -A clippy::unit_arg -A clippy::if_same_then_else -A clippy::collapsible_if -A clippy::useless-let-if-seq -A clippy::map-entry -D warnings
+cargo clippy --all  -- -A clippy::too-many-arguments -A clippy::unit_arg -A clippy::if_same_then_else -A clippy::collapsible_if -A clippy::useless-let-if-seq -A clippy::map-entry -D warnings
