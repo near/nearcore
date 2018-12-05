101422f01384cc167512dfe845e4d7ba296df73f
Marcelo Fornet <mfornet94@gmail.com>
2018-12-05 15:41:38 -0500

    Update violations.
    Update violation lists and adapt BadEpoch check

--- core/txflow/src/dag/reporter.rs
+++ core/txflow/src/dag/reporter.rs
@@ -37,18 +37,19 @@ impl MisbehaviorReporter for NoopMisbehaviorReporter{
     }
 }
 
-/// TODO: Enumerate all violations and implement evidence to check that the
-/// misbehavior really took place.
+/// Enumeration of all TxFlow protocol violations.
+/// Discussion at: https://github.com/nearprotocol/nearcore/issues/131
 #[derive(Debug)]
 pub enum ViolationType {
-    BadEpoch {
-        message: TxFlowHash,
-    },
-
-    InvalidSignature,
-
-    ForkAttempt {
-        message_0: TxFlowHash,
-        message_1: TxFlowHash,
-    },
+    /// Message with incorrect epoch
+    BadEpoch(TxFlowHash),
+    /// There is no BLS signature for representative when there must be one
+    MissingEndorsement(TxFlowHash),
+    /// Invalid part of the BLS signature
+    InvalidEndorsement(TxFlowHash),
+    /// Message contains invalid signature from participant.
+    /// Someone pretending being another participant maybe.
+    InvalidSignature(TxFlowHash),
+    /// Two messages from the same participant that are not approved by each other.
+    ForkAttempt(TxFlowHash, TxFlowHash),
 }
