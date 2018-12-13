aa6c04b673f388a43618bc85d1efe081dbef5178
haydn dufrene <haydn.dufrene@gmail.com>
2018-12-12 17:39:37 -0800

    separate service and cli

--- test-utils/devnet/src/lib.rs
+++ test-utils/devnet/src/lib.rs
@@ -3,14 +3,14 @@ extern crate node_cli;
 use std::path::PathBuf;
 
 pub fn start_devnet() {
-    let config = node_cli::ServiceConfig {
+    let config = node_cli::service::ServiceConfig {
         base_path: PathBuf::from("."),
         chain_spec_path: None,
         p2p_port: None,
         rpc_port: None,
         test_node_index: None,
     };
-    node_cli::start_service(
+    node_cli::service::start_service(
         config,
         node_cli::test_utils::spawn_pasthrough_consensus,
     );
