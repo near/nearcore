95b6220f00db584248e3c4828fdd76012d0f5f2b
azban <me@azban.net>
2018-12-13 11:49:40 -0800

    remove antiquated comment

--- node/network/src/protocol.rs
+++ node/network/src/protocol.rs
@@ -107,7 +107,6 @@ impl<B: SignedBlock, Header: BlockHeader, P: Payload> Protocol<B, Header, P> {
 
     pub fn on_peer_connected(&self, peer: NodeIndex) {
         self.handshaking_peers.write().insert(peer, time::Instant::now());
-        // use this placeholder for now. Change this when block storage is ready
         let best_block_header = self.chain.best_block().header();
         let status = message::Status {
             version: CURRENT_VERSION,
