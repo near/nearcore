825189af00bde1cf12124b20e3235e6f13fa9d19
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2018-12-13 09:38:15 -0800

    Externalize memory section for WASM on every contract (#223)

--- core/wasm/runtest/generate-wasm/build.sh
+++ core/wasm/runtest/generate-wasm/build.sh
@@ -4,11 +4,12 @@ pushd $(dirname "$0")/to-wasm
 cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/to_wasm.wasm ../
 wasm-gc ../to_wasm.wasm
+cp ../to_wasm.wasm ../../res/wasm_with_mem.wasm
 popd
 
-pushd $(dirname "$0")/import-memory
-cargo run --release ../to_wasm.wasm ../../res/wasm_with_mem.wasm
-wasm-gc ../../res/wasm_with_mem.wasm
-popd
+# pushd $(dirname "$0")/import-memory
+# cargo run --release ../to_wasm.wasm ../../res/wasm_with_mem.wasm
+# wasm-gc ../../res/wasm_with_mem.wasm
+# popd
 
 rm $(dirname "$0")/to_wasm.wasm
