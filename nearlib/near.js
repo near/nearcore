9418e55b48e747d1616b0cc936e9db724e5a1a3d
janedegtiareva <jane.degtiareva@gmail.com>
2018-12-27 10:21:02 -0800

    [nearlib] placeholder for nearlib (#313)

---
+++ nearlib/near.js
@@ -0,0 +1,8 @@
+/*
+ * This is the javascript library for using near blockchain.
+ * This is a placeholder for now.
+ */
+
+exports.sampleFunction = function() {
+    console.log("This is a message from nearlib");
+}
