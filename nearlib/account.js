09f1df2295647ebf97fe1547b62490d35b5aa505
Evguenia degtiareva <jane.degtiareva@gmail.com>
2018-12-27 15:55:11 -0800

    [nearlib] Added issues to TODOs or resolved them

--- nearlib/account.js
+++ nearlib/account.js
@@ -3,7 +3,7 @@
  * Creates a new account with a random new key.
  */
 exports.createRandomAccount = async function() {
-    // TODO: implement
+    // Not implemented yet
 };
 
 /**
@@ -13,15 +13,9 @@ exports.viewAccount = async account_id => {
     return await viewAccount(account_id);
 };
 
-// TODO: this is to sanity check that tests are working. Remove soon.
-exports.returnOne = function() {
-    return 1;
-}
 
-// TODO: move impl to a diff file.
-
+// Implementation
 const superagent = require('superagent');
-
 
 const viewAccount = async account_id => {
     const viewAccountResponse = await request('view_account', {
@@ -32,7 +26,7 @@ const viewAccount = async account_id => {
 
 const request = async (methodName, params) => {
     try {
-        // TODO: make this configurable and testable
+        // TODO: (issue 320) make this configurable and testable
         const response = await superagent
             .post(`http://localhost:3030/${methodName}`)
             .use(require('superagent-logger'))
--- nearlib/test/integration.test.js
+++ nearlib/test/integration.test.js
@@ -8,11 +8,6 @@ const aliceAccount = {
     code_hash: '2Kkfab2QQ3GK7DSiuzcRF2H4vNSPKFfE9gkCDkPGumtV'
 };
 
-test('returns 1', () => {
-    // Sanity check
-    expect(account.returnOne()).toBe(1);
-});
-
 test('view account', async () => {
     const viewAccountResponse = await account.viewAccount("alice.near");
     expect(viewAccountResponse).toEqual(aliceAccount);
