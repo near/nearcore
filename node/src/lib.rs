8edf022cb3bb6237ae4dcf5b5d79d364aafdf0b6
Vladimir Grichina <vgrichina@gmail.com>
2018-12-28 01:33:33 -0800

    Fix lint errors

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -13,7 +13,7 @@ use primitives::types::ChainPayload;
 
 
 #[no_mangle]
-pub extern fn run_with_config(
+pub unsafe extern fn run_with_config(
     base_path_ptr: *const c_char,
     account_id_ptr: *const c_char,
     public_key_ptr: *const c_char,
@@ -24,57 +24,55 @@ pub extern fn run_with_config(
     boot_nodes_ptr: *const c_char,
     test_network_key_seed_val: u32) {
 
-    unsafe {
-        let base_path = CStr::from_ptr(base_path_ptr)
-            .to_str()
-            .map(PathBuf::from)
-            .unwrap();
+    let base_path = CStr::from_ptr(base_path_ptr)
+        .to_str()
+        .map(PathBuf::from)
+        .unwrap();
 
-        let account_id = CStr::from_ptr(account_id_ptr)
-            .to_str()
-            .map(String::from)
-            .unwrap();
+    let account_id = CStr::from_ptr(account_id_ptr)
+        .to_str()
+        .map(String::from)
+        .unwrap();
 
-        let public_key = CStr::from_ptr(public_key_ptr)
-            .to_str()
-            .map(String::from)
-            .ok();
+    let public_key = CStr::from_ptr(public_key_ptr)
+        .to_str()
+        .map(String::from)
+        .ok();
 
-        let chain_spec_path = CStr::from_ptr(chain_spec_path_ptr)
-            .to_str()
-            .map(PathBuf::from)
-            .ok();
+    let chain_spec_path = CStr::from_ptr(chain_spec_path_ptr)
+        .to_str()
+        .map(PathBuf::from)
+        .ok();
 
-        let log_level = CStr::from_ptr(log_level_ptr)
-            .to_str()
-            .map(log::LevelFilter::from_str)
-            .unwrap()
-            .unwrap();
+    let log_level = CStr::from_ptr(log_level_ptr)
+        .to_str()
+        .map(log::LevelFilter::from_str)
+        .unwrap()
+        .unwrap();
 
-        let boot_nodes = CStr::from_ptr(boot_nodes_ptr)
-            .to_str()
-            .unwrap()
-            .split(",")
-            .map(String::from)
-            .collect();
+    let boot_nodes = CStr::from_ptr(boot_nodes_ptr)
+        .to_str()
+        .unwrap()
+        .split(',')
+        .map(String::from)
+        .collect();
 
-        let test_network_key_seed = Some(test_network_key_seed_val); 
+    let test_network_key_seed = Some(test_network_key_seed_val);
 
-        let config = service::ServiceConfig {
-            base_path,
-            account_id,
-            public_key,
-            chain_spec_path,
-            log_level,
-            p2p_port,
-            rpc_port,
-            boot_nodes,
-            test_network_key_seed,
-        };
+    let config = service::ServiceConfig {
+        base_path,
+        account_id,
+        public_key,
+        chain_spec_path,
+        log_level,
+        p2p_port,
+        rpc_port,
+        boot_nodes,
+        test_network_key_seed,
+    };
 
-        service::start_service(
-            config,
-            txflow::txflow_task::spawn_task::<ChainPayload, BeaconWitnessSelector>
-        );
-    }
+    service::start_service(
+        config,
+        txflow::txflow_task::spawn_task::<ChainPayload, BeaconWitnessSelector>
+    );
 }
