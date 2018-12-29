6ad4ea60019c15dbc25e029993b8a474068a0c1e
Evguenia degtiareva <jane.degtiareva@gmail.com>
2018-12-28 21:27:53 -0800

    [nearlib] fix typo in test

--- nearlib/test/integration.test.js
+++ nearlib/test/integration.test.js
@@ -2,7 +2,7 @@ const account = require('../account');
 
 const aliceAccount = {
     account_id: 'alice.near',
-    amount: 1000,
+    amount: 100,
     stake: 0,
     nonce: 0,
     code_hash: '2Kkfab2QQ3GK7DSiuzcRF2H4vNSPKFfE9gkCDkPGumtV'
