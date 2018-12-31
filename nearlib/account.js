cf79c8b81461924f8ed4b5b3281739f79f3f9c41
Evguenia degtiareva <jane.degtiareva@gmail.com>
2018-12-31 15:18:38 -0800

    [nearlib] add create account functionality (only from alice account for now)
    + integration test

--- nearlib/account.js
+++ nearlib/account.js
@@ -1,9 +1,22 @@
+const superagent = require('superagent');
+const ed25519 = require('ed25519');
+const bs58 = require('bs58');
+
+const MAX_RETRIES = 3;
 
 /**
- * Creates a new account with a random new key.
+ * Creates a new account with a given name and key,
  */
-exports.createRandomAccount = async function() {
-    // Not implemented yet
+exports.createAccount = async (newAccountId, publicKey, amount, originatorAccountId) => {
+    const createAccountParams = {
+        originator: originatorAccountId,
+        new_account_id: newAccountId,
+        amount: amount,
+        public_key: publicKey,
+    };
+
+    const transactionResponse = await submitTransaction("create_account", createAccountParams);
+    return transactionResponse;
 };
 
 /**
@@ -13,9 +26,24 @@ exports.viewAccount = async account_id => {
     return await viewAccount(account_id);
 };
 
+const submitTransaction = async (method, args) => {
+    // TODO: Make sender param names consistent
+    // TODO: https://github.com/nearprotocol/nearcore/issues/287
+    const senderKeys = ['sender_account_id', 'originator_account_id', 'originator_id', 'sender', 'originator'];
+    const sender = senderKeys.map(key => args[key]).find(it => !!it)
+    const nonce = await getNonce(sender);
+    for (let i = 0; i < MAX_RETRIES; i++) {
+        const response = await request(method, Object.assign({}, args, { nonce }));
+        const transaction = response.body;
+        const signedTransaction = await signTransaction(transaction);
+        const submitResponse = await request('submit_transaction', signedTransaction);
+    }
+    return { nonce: nonce };
+}
 
-// Implementation
-const superagent = require('superagent');
+const getNonce = async account_id => {
+    return (await viewAccount(account_id)).nonce + 1;
+}
 
 const viewAccount = async account_id => {
     const viewAccountResponse = await request('view_account', {
@@ -23,6 +51,18 @@ const viewAccount = async account_id => {
     });
     return viewAccountResponse;
 }
+
+const signTransaction = async (transaction) => {
+    const stringifiedTxn = JSON.stringify(transaction);
+    // How do we want to pass in real signatures?
+    const hardcodedKey = bs58.decode("2hoLMP9X2Vsvib2t4F1fkZHpFd6fHLr5q7eqGroRoNqdBKcPja2jCrmxW9uGBLXdTnbtZYibWe4NoFtB4Bk7LWg6");
+    const signature = [...ed25519.Sign(new Buffer(stringifiedTxn, 'utf8'), hardcodedKey)];
+    const response = { 
+        body: transaction,
+        signature: signature
+    };
+    return response;
+};
 
 const request = async (methodName, params) => {
     try {
--- nearlib/test/integration.test.js
+++ nearlib/test/integration.test.js
@@ -1,14 +1,42 @@
 const account = require('../account');
+const uuidV4 = require('uuid/v4');
 
-const aliceAccount = {
-    account_id: 'alice.near',
-    amount: 100,
-    stake: 0,
-    nonce: 0,
-    code_hash: '2Kkfab2QQ3GK7DSiuzcRF2H4vNSPKFfE9gkCDkPGumtV'
-};
+const aliceAccountName = 'alice.near';
+const MAX_RETRIES = 3;
 
-test('view account', async () => {
-    const viewAccountResponse = await account.viewAccount("alice.near");
-    expect(viewAccountResponse).toEqual(aliceAccount);
+test('view pre-defined account works and returns correct name', async () => {
+    // We do not want to check the other properties of this account since we create other accounts
+    // using this account as the originator
+    const viewAccountResponse = await account.viewAccount(aliceAccountName);
+    expect(viewAccountResponse.account_id).toEqual(aliceAccountName);
 });
+
+test('create account and then view account returns the created account', async () => {
+    const newAccountName = await generateUniqueAccountId("create.account.test");
+    const newAccountPublicKey = '9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE';
+    const createAccountResponse = await account.createAccount(newAccountName, newAccountPublicKey, 1, aliceAccountName);
+
+    // try to read the account a few times to wait for the transaction to go through
+    for (var viewAttempt = 0; viewAttempt < 1; viewAttempt++) {
+        try {
+            const viewAccountResponse = await account.viewAccount(newAccountName);
+            const expctedAccount = {
+                nonce: 0,
+                account_id: newAccountName,
+                amount: 1,
+                code_hash: 'GKot5hBsd81kMupNCXHaqbhv3huEbxAFMLnpcX2hniwn',
+                stake: 0,
+            };
+
+            expect(viewAccountResponse).toEqual(expctedAccount);
+            return; // success!
+        } catch (_) {}
+        // exceeded retries. fail.
+        fail('exceeded number of retries for viewing account');
+    }
+});
+
+const generateUniqueAccountId = async (prefix) => {
+    const viewAccountResponse = await account.viewAccount(aliceAccountName);
+    return prefix + viewAccountResponse.nonce;
+}
