24559f1d3ca2564cab4b875805ad5e97df1f011c
Mikhail <mikhail@nearprotocol.com>
2019-01-03 15:00:55 -0800

    Unit tests for two substrate trie bugs.
    Unclear if bugs or features, but substrate trie breaks in several ways
    if keys are not cryptographic hashes.

--- core/storage/src/substrate_storage.rs
+++ core/storage/src/substrate_storage.rs
@@ -34,6 +34,8 @@ mod tests {
     use std::ops::Deref;
     use super::*;
     use super::super::{COL_STATE, StateDb};
+    use kvdb::KeyValueDB;
+    use kvdb::DBTransaction;
 
     fn state_transition(
         backend: &TrieBackend,
@@ -113,5 +115,143 @@ mod tests {
         let puppy =
             backend.storage(&b"dog3".to_vec()).unwrap().expect("the key should be in the backend");
         assert_eq!(b"puppy3".to_vec(), puppy);
+    }
+
+
+    // Bug#1: trie node codec produces equivalent output if two nodes have equivalent subtrees.
+    // build_db_transaction should treat the node storage as a multiset of key/value pairs.
+    // The bug is hard to hit if trie keys are hashes.
+    #[test]
+    #[should_panic]
+    fn substrate_bug_kvdb_multiset() {
+        let kvdb = kvdb_memorydb::create(1 /*numColumns*/);
+        let mut root = CryptoHash::default();
+        let storage_db = Arc::new(StateDb::new(Arc::new(kvdb)));
+
+        let mut prefix1 = String::from("dog");
+        let mut prefix2 = String::from("cat");
+        let value = String::from("puppypuppypuppypuppypuppypuppypuppypuppypuppypuppypuppypuppypuppy");
+
+        for _block in 0..4 {
+            let storage_db_copy = Arc::clone(&storage_db);
+            let backend: TrieBackend = TrieBackend::new(storage_db_copy, root);
+            let mut overlay = OverlayedChanges::default();
+
+            {
+                let mut ext = StateExt::new(&mut overlay, &backend, None);
+                match _block {
+                    0 => {
+                        // Write dogaa and dogbb
+                        //
+                        ext.place_storage((prefix1.clone() + "aa").into_bytes(), Some(value.clone().into_bytes()));
+                        ext.place_storage((prefix1.clone() + "bb").into_bytes(), Some(value.clone().into_bytes()));
+                    }
+                    1 => {
+                        // Write cataa and catbb
+                        //
+                        ext.place_storage((prefix2.clone() + "aa").into_bytes(), Some(value.clone().into_bytes()));
+                        ext.place_storage((prefix2.clone() + "bb").into_bytes(), Some(value.clone().into_bytes()));
+                    }
+                    2 => {
+                        // Write dogac which deletes the old branching node.
+                        // The same node had another identical copy which also gets deleted (bug).
+                        //
+                        ext.place_storage((prefix1.clone() + "ac").into_bytes(), Some(value.clone().into_bytes()));
+                        ext.place_storage((prefix1.clone() + "bc").into_bytes(), Some(value.clone().into_bytes()));
+                    }
+                    3 => {
+                        // Crash trying to read the incorrectly deleted node.
+                        //
+                        ext.storage(&(prefix2.clone() + "aa").into_bytes()[..]);
+                    }
+                    _ => {}
+                }
+            }
+            overlay.commit_prospective();
+
+
+            let mdb = storage_db.deref().storage.deref();
+
+            let (root_after, transaction) = {
+                let mut ext = StateExt::new(&mut overlay, &backend, None);
+                let root_after = ext.storage_root();
+                let (mut storage_transaction, _changes_trie_transaction) = ext.transaction();
+                let transaction = build_db_transaction(&mut storage_transaction, mdb);
+                (root_after, transaction)
+            };
+
+            mdb.write(transaction).expect("write to db");
+            root = root_after;
+            println!("{}: root after changes is {:?}", _block, root_after);
+        }
+    }
+
+    // Bug#2: trie node codec does not work when keys can have long common prefixes (~190 bytes).
+    // The bug is hard to hit if trie keys are hashes.
+    #[test]
+    #[should_panic]
+    fn substrate_bug_long_extension() {
+        let kvdb = kvdb_memorydb::create(1 /*numColumns*/);
+        let mut root = CryptoHash::default();
+        let storage_db = Arc::new(StateDb::new(Arc::new(kvdb)));
+
+        let mut prefix1 = String::new();
+        for _i in 0..190 {
+            prefix1 += "x";
+        }
+        let value = String::from("puppy");
+
+        for _block in 0..2 {
+            let storage_db_copy = Arc::clone(&storage_db);
+            let backend: TrieBackend = TrieBackend::new(storage_db_copy, root);
+            let mut overlay = OverlayedChanges::default();
+
+            {
+                let mut ext = StateExt::new(&mut overlay, &backend, None);
+                match _block {
+                    0 => {
+                        // Trie root is an extension node with ~380 nibbles.
+                        // Substrate node codec breaks on extension nodes with more than ~(256+128).
+                        //
+                        ext.place_storage((prefix1.clone() + "aa").into_bytes(), Some(value.clone().into_bytes()));
+                        ext.place_storage((prefix1.clone() + "bb").into_bytes(), Some(value.clone().into_bytes()));
+                    }
+                    1 => {
+                        // Crash attempting to decode the root node.
+                        //
+                        ext.storage(&(prefix1.clone() + "aa").into_bytes()[..]);
+                    }
+                    _ => {}
+                }
+            }
+            overlay.commit_prospective();
+
+
+            let mdb = storage_db.deref().storage.deref();
+
+            let (root_after, transaction) = {
+                let mut ext = StateExt::new(&mut overlay, &backend, None);
+                let root_after = ext.storage_root();
+                let (mut storage_transaction, _changes_trie_transaction) = ext.transaction();
+                let transaction = build_db_transaction(&mut storage_transaction, mdb);
+                (root_after, transaction)
+            };
+
+            mdb.write(transaction).expect("write to db");
+            root = root_after;
+            println!("{}: root after changes is {:?}", _block, root_after);
+        }
+    }
+
+    fn build_db_transaction(storage_transaction: &mut MemoryDB, mdb: &KeyValueDB) -> DBTransaction {
+        let mut transaction = mdb.transaction();
+        for (k, (v, rc)) in storage_transaction.drain() {
+            if rc > 0 {
+                transaction.put(COL_STATE, k.as_ref(), &v.to_vec());
+            } else if rc < 0 {
+                transaction.delete(COL_STATE, k.as_ref());
+            }
+        }
+        transaction
     }
 }
