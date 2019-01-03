236d04a7c7cfe1d014e0f411ae9b802d5ad22d3f
janedegtiareva <jane.degtiareva@gmail.com>
2019-01-03 13:10:37 -0800

    [nearlib] add create account functionality + integration test (#326)
    * [nearlib] add create account functionality (only from alice account for now)
    
    + integration test
    
    * [nearlib] clean up dependencies
    
    * [nearlib] add in memory keystore to testtools and use from the test
    
    * [nearlib] make account into an  es6 class.
    
    * [nearlib] address code review comments
    
    * [nearlib] nit in integration test

--- nearlib/account.js
+++ nearlib/account.js
@@ -1,39 +1,76 @@
-
-/**
- * Creates a new account with a random new key.
- */
-exports.createRandomAccount = async function() {
-    // Not implemented yet
-};
-
-/**
- * Retrieves account data by plain-text account id. 
- */
-exports.viewAccount = async account_id => {
-    return await viewAccount(account_id);
-};
-
-
-// Implementation
 const superagent = require('superagent');
+const ed25519 = require('ed25519');
+const bs58 = require('bs58');
+const MAX_RETRIES = 3;
 
-const viewAccount = async account_id => {
-    const viewAccountResponse = await request('view_account', {
-        account_id: account_id,
-    });
-    return viewAccountResponse;
-}
+class Account {
+    constructor(keyStore) {
+        this.keyStore = keyStore;
+    }
 
-const request = async (methodName, params) => {
-    try {
+    /**
+     * Creates a new account with a given name and key,
+     */
+    async createAccount (newAccountId, publicKey, amount, originatorAccountId) {
+        const createAccountParams = {
+            originator: originatorAccountId,
+            new_account_id: newAccountId,
+            amount: amount,
+            public_key: publicKey,
+        };
+
+        const transactionResponse = await this.submitTransaction("create_account", createAccountParams);
+        return transactionResponse;
+    };
+
+    /**
+     * Retrieves account data by plain-text account id. 
+     */
+    async viewAccount (account_id) {
+        return await viewAccount(account_id);
+    };
+
+    async viewAccount (account_id) {
+        const viewAccountResponse = await this.request('view_account', {
+            account_id: account_id,
+        });
+        return viewAccountResponse;
+    };
+
+    async signTransaction (transaction, sender) {
+        const stringifiedTxn = JSON.stringify(transaction);
+        const encodedKey = await this.keyStore.getKey(sender);
+        const key = bs58.decode(encodedKey.secret_key);
+        const signature = [...ed25519.Sign(new Buffer(stringifiedTxn, 'utf8'), key)];
+        const response = {
+            body: transaction,
+            signature: signature
+        };
+        return response;
+    };
+
+    async submitTransaction (method, args) {
+        const senderKey = 'originator';
+        const sender = args[senderKey];
+        const nonce = await this.getNonce(sender);
+        const response = await this.request(method, Object.assign({}, args, { nonce }));
+        const transaction = response.body;
+        const signedTransaction = await this.signTransaction(transaction, sender);
+        const submitResponse = await this.request('submit_transaction', signedTransaction);
+        return { nonce: nonce };
+    };
+
+    async getNonce (account_id) {
+        return (await this.viewAccount(account_id)).nonce + 1;
+    };
+
+    async request (methodName, params) {
         // TODO: (issue 320) make this configurable and testable
         const response = await superagent
             .post(`http://localhost:3030/${methodName}`)
             .use(require('superagent-logger'))
             .send(params);
         return JSON.parse(response.text);
-    } catch(e) {
-        console.error("error calling rpc ", e);
-        throw e;
-    }
+    };
 };
+module.exports = Account;
--- nearlib/test/integration.test.js
+++ nearlib/test/integration.test.js
@@ -1,14 +1,49 @@
-const account = require('../account');
-
-const aliceAccount = {
-    account_id: 'alice.near',
-    amount: 100,
-    stake: 0,
-    nonce: 0,
-    code_hash: '2Kkfab2QQ3GK7DSiuzcRF2H4vNSPKFfE9gkCDkPGumtV'
+const InMemoryKeyStore = require('../test-tools/in_memory_key_store.js');
+const Account = require('../account');
+const aliceAccountName = 'alice.near';
+const aliceKey = {
+    public_key: "9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE",
+    secret_key: "2hoLMP9X2Vsvib2t4F1fkZHpFd6fHLr5q7eqGroRoNqdBKcPja2jCrmxW9uGBLXdTnbtZYibWe4NoFtB4Bk7LWg6"
 };
+const test_key_store = new InMemoryKeyStore();
+test_key_store.setKey(aliceAccountName, aliceKey);
+const account = new Account(test_key_store);
+const TEST_MAX_RETRIES = 5;
 
-test('view account', async () => {
-    const viewAccountResponse = await account.viewAccount("alice.near");
-    expect(viewAccountResponse).toEqual(aliceAccount);
+
+test('view pre-defined account works and returns correct name', async () => {
+    // We do not want to check the other properties of this account since we create other accounts
+    // using this account as the originator
+    const viewAccountResponse = await account.viewAccount(aliceAccountName);
+    expect(viewAccountResponse.account_id).toEqual(aliceAccountName);
 });
+
+test('create account and then view account returns the created account', async () => {
+    const newAccountName = await generateUniqueAccountId("create.account.test");
+    const newAccountPublicKey = '9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE';
+    const createAccountResponse = await account.createAccount(newAccountName, newAccountPublicKey, 1, aliceAccountName);
+
+    // try to read the account a few times to wait for the transaction to go through
+    for (var viewAttempt = 0; viewAttempt < TEST_MAX_RETRIES; viewAttempt++) {
+        try {
+            const viewAccountResponse = await account.viewAccount(newAccountName);
+            const expctedAccount = {
+                nonce: 0,
+                account_id: newAccountName,
+                amount: 1,
+                code_hash: 'GKot5hBsd81kMupNCXHaqbhv3huEbxAFMLnpcX2hniwn',
+                stake: 0,
+            };
+
+            expect(viewAccountResponse).toEqual(expctedAccount);
+            return; // success!
+        } catch (_) {}
+    }
+    // exceeded retries. fail. 
+    fail('exceeded number of retries for viewing account');
+});
+
+const generateUniqueAccountId = async (prefix) => {
+    const viewAccountResponse = await account.viewAccount(aliceAccountName);
+    return prefix + viewAccountResponse.nonce;
+}
