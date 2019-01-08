145e4eee5f91b9e2e2ec85c25041e205a450c25f
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-01-08 11:23:07 -0800

    [nearlib] a few test infrastructure fixes+ pass base url to connection

--- nearlib/local_node_connection.js
+++ nearlib/local_node_connection.js
@@ -1,9 +1,13 @@
 const superagent = require('superagent');
 
 class LocalNodeConnection {
+    constructor (baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
     async request(methodName, params) {
         const response = await superagent
-            .post(`http://localhost:3030/${methodName}`)
+            .post(`${this.baseUrl}/${methodName}`)
             .use(require('superagent-logger'))
             .send(params);
         return JSON.parse(response.text);
