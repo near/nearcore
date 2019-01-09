ca6d037c7ed2fc27497dd61604e29d2cd6205448
Illia Polosukhin <ilblackdragon@gmail.com>
2019-01-09 12:20:56 -0800

    Speeding up CI (#327)
    * Adding builder docker and switching gitlab ci to it
    
    * Try without setting cargo home
    
    * Return back cache
    
    * [CI] configure GitLab CI to match the configured on-premise gitlab-runner (buidl-2) where /tmp/cache/nearcore is mounted from the host system
    
    * [CI] resolved the issue of overriden before_script
    
    * [CI] simplified the setup for cargo cache
    
    * [CI] fixed the CARGO_HOME variable not being able to resolve CACHE_ROOT
    
    * [CI] GNU make didn't like colon in the path name used by the cache folder
    
    * [CI] fixed the issue of too aggressive cache cleanup leading to missing some sources in cargo registry
    
    * Fixed a test
    
    * [CI] avoid endless waiting when server is broken
    
    * [CI] dropped builder.Dockerfile as it is currently useless

--- scripts/waitonserver.sh
+++ scripts/waitonserver.sh
@@ -1,2 +1,13 @@
-echo 'waiting on healthcheck'
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:3030/healthz)" != "200" ]]; do sleep 5; done'
+#!/bin/bash
+
+echo 'waiting on healthcheck' >&2
+
+for _ in {1..6}; do
+    if [[ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:3030/healthz)" == "200" ]]; then
+        exit 0
+    fi
+    sleep 5
+done
+
+echo 'ERROR: waiting timeout' >&2
+exit 1
