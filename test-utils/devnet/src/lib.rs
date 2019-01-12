f501f0630fe3b7372af9fba1ee6e6d58d1033b66
Maksym Zavershynskyi <max@near.ai>
2019-01-11 18:27:20 -0800

    Extract state shared objects into client

--- test-utils/devnet/src/lib.rs
+++ test-utils/devnet/src/lib.rs
@@ -1,15 +1,18 @@
 extern crate node_cli;
 extern crate consensus;
+extern crate client;
 
-pub use node_cli::service::ServiceConfig;
+pub use node_cli::service::NetworkConfig;
+pub use client::ClientConfig;
 use consensus::passthrough;
 
-pub fn start_devnet(service_config: Option<ServiceConfig>) {
-    let service_config = service_config.unwrap_or_else(|| {
-        ServiceConfig::default()
-    });
+pub fn start_devnet(network_cfg: Option<NetworkConfig>,
+client_cfg: Option<ClientConfig>, ) {
+    let network_cfg = network_cfg.unwrap_or_else(NetworkConfig::default);
+    let client_cfg = client_cfg.unwrap_or_else(ClientConfig::default);
     node_cli::service::start_service(
-        service_config,
+        network_cfg,
+        client_cfg,
         passthrough::spawn_consensus,
     );
 }
