6d6ac9833c217a2774fd8e9b7911c8466280328f
Maksym Zavershynskyi <max@near.ai>
2019-01-15 16:28:05 -0800

    Minor test fix

--- core/beacon/src/authority.rs
+++ core/beacon/src/authority.rs
@@ -395,7 +395,7 @@ mod test {
         );
         assert_eq!(
             authority.get_authorities(2).unwrap(),
-            vec![initial_authorities[2].clone(), initial_authorities[1].clone()]
+            vec![initial_authorities[1].clone(), initial_authorities[2].clone()]
         );
         assert_eq!(
             authority.get_authorities(3).unwrap(),
@@ -403,7 +403,7 @@ mod test {
         );
         assert_eq!(
             authority.get_authorities(4).unwrap(),
-            vec![initial_authorities[2].clone(), initial_authorities[1].clone()]
+            vec![initial_authorities[1].clone(), initial_authorities[2].clone()]
         );
         assert!(authority.get_authorities(5).is_err());
         let block1 = SignedBeaconBlock::new(1, bc.genesis_hash, vec![], CryptoHash::default());
@@ -417,11 +417,11 @@ mod test {
         authority.process_block_header(&header2);
         assert_eq!(
             authority.get_authorities(5).unwrap(),
-            vec![initial_authorities[2].clone(), initial_authorities[0].clone()]
+            vec![initial_authorities[0].clone(), initial_authorities[2].clone()]
         );
         assert_eq!(
             authority.get_authorities(6).unwrap(),
-            vec![initial_authorities[0].clone(), initial_authorities[1].clone()]
+            vec![initial_authorities[2].clone(), initial_authorities[1].clone()]
         );
     }
 
