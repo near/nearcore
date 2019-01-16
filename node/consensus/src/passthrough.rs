b38ed53fe11496cb2ee4abccc10c26743cee4d29
Bowen Wang <bowenwang1996@uchicago.edu>
2019-01-16 09:31:13 -0800

    fix small bugs

--- node/consensus/src/passthrough.rs
+++ node/consensus/src/passthrough.rs
@@ -16,36 +16,40 @@ pub fn spawn_consensus(
     _inc_gossip_rx: Receiver<Gossip<ChainPayload>>,
     payload_rx: Receiver<ChainPayload>,
     _out_gossip_tx: Sender<Gossip<ChainPayload>>,
-    _control_rx: Receiver<Control<BeaconWitnessSelector>>,
+    control_rx: Receiver<Control<BeaconWitnessSelector>>,
     consensus_tx: Sender<ChainConsensusBlockBody>,
 ) {
     let interval_stream = Interval::new_interval(BLOCK_PERIOD)
         .map(|_| None as Option<ChainPayload>)
         .map_err(|_| ());
     let payload_stream = payload_rx.map(Some);
-    let task = payload_stream.select(interval_stream).fold(vec![], move |mut acc, p| {
-        if let Some(payload) = p {
-            let message: SignedMessageData<ChainPayload> = SignedMessageData {
-                owner_sig: DEFAULT_SIGNATURE, // TODO: Sign it.
-                hash: 0,                      // Compute real hash
-                body: MessageDataBody {
-                    owner_uid: 0,
-                    parents: HashSet::new(),
-                    epoch: 0,
-                    payload,
-                    endorsements: vec![],
-                },
-            };
-            acc.push(message);
-            future::ok(acc)
-        } else {
-            let c = ChainConsensusBlockBody { messages: acc };
-            tokio::spawn(consensus_tx.clone().send(c).map(|_| ()).map_err(|e| {
-                error!("Failure sending pass-through consensus {:?}", e);
-            }));
-            future::ok(vec![])
-        }
-    }).map(|_| ());
+    let task = payload_stream
+        .select(interval_stream)
+        .fold((control_rx, vec![]), move |(control_rx, mut acc), p| {
+            if let Some(payload) = p {
+                let message: SignedMessageData<ChainPayload> = SignedMessageData {
+                    owner_sig: DEFAULT_SIGNATURE, // TODO: Sign it.
+                    hash: 0,                      // Compute real hash
+                    body: MessageDataBody {
+                        owner_uid: 0,
+                        parents: HashSet::new(),
+                        epoch: 0,
+                        payload,
+                        endorsements: vec![],
+                    },
+                };
+                acc.push(message);
+                future::ok((control_rx, acc))
+            } else {
+                if !acc.is_empty() {
+                    let c = ChainConsensusBlockBody { messages: acc };
+                    tokio::spawn(consensus_tx.clone().send(c).map(|_| ()).map_err(|e| {
+                        error!("Failure sending pass-through consensus {:?}", e);
+                    }));
+                }
+                future::ok((control_rx, vec![]))
+            }
+        }).map(|_| ());
 
     tokio::spawn(task);
 }
