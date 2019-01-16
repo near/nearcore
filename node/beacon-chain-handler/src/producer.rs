55f835ce0274740eeb1b88e1216826d67b63492b
Maksym Zavershynskyi <max@near.ai>
2019-01-16 13:30:40 -0800

    Minor fix to block producer

--- node/beacon-chain-handler/src/producer.rs
+++ node/beacon-chain-handler/src/producer.rs
@@ -23,65 +23,62 @@ pub fn spawn_block_producer(
     control_tx: Sender<Control<BeaconWitnessSelector>>,
 ) {
     let task = receiver
-        .fold(
-            (client, block_announce_tx, new_receipts_tx, authority_tx, control_tx),
-            |(client, block_announce_tx, new_receipts_tx, authority_tx, control_tx), body| {
-                let (new_block, new_shard_block) = client.produce_block(body);
-                // send beacon block to network
-                tokio::spawn({
-                    block_announce_tx
-                        .clone()
-                        .send(new_block.clone())
-                        .map(|_| ())
-                        .map_err(|e| error!("Error sending block: {}", e))
-                });
+        .fold(client, move |client, body| {
+            let (new_block, new_shard_block) = client.produce_block(body);
+            // send beacon block to network
+            tokio::spawn({
+                block_announce_tx
+                    .clone()
+                    .send(new_block.clone())
+                    .map(|_| ())
+                    .map_err(|e| error!("Error sending block: {}", e))
+            });
 
-                // Redirect the receipts from the previous block for processing in the next one.
-                tokio::spawn({
-                    new_receipts_tx
-                        .clone()
-                        .send_all(stream::iter_ok(new_shard_block.body.new_receipts.to_vec()))
-                        .map(|_| ())
-                        .map_err(|e| error!("Error sending receipts: {}", e))
-                });
+            // Redirect the receipts from the previous block for processing in the next one.
+            tokio::spawn({
+                new_receipts_tx
+                    .clone()
+                    .send_all(stream::iter_ok(new_shard_block.body.new_receipts.to_vec()))
+                    .map(|_| ())
+                    .map_err(|e| error!("Error sending receipts: {}", e))
+            });
 
-                // Take care of changed authorities.
-                // Notify the network about the new set of UID -> AccountId.
-                let (owner_uid, uid_to_authority_map) =
-                    client.get_uid_to_authority_map(new_block.header().index());
-                tokio::spawn({
-                    authority_tx
-                        .clone()
-                        .send(uid_to_authority_map.clone())
-                        .map(|_| ())
-                        .map_err(|e| error!("Error sending authorities to the network: {}", e))
-                });
-                // Send control to TxFlow.
-                let control = match owner_uid {
-                    None => Control::Stop,
-                    Some(owner_uid) => {
-                        let witness_selector = Box::new(BeaconWitnessSelector::new(
-                            uid_to_authority_map.keys().cloned().collect(),
-                            owner_uid,
-                        ));
-                        Control::Reset(State {
-                            owner_uid,
-                            starting_epoch: 0,
-                            gossip_size: 1, // TODO: Use adaptive gossip size.
-                            witness_selector,
-                        })
-                    }
-                };
-                tokio::spawn({
-                    control_tx
-                        .clone()
-                        .send(control)
-                        .map(|_| ())
-                        .map_err(|e| error!("Error sending control to TxFlow: {}", e))
-                });
-                future::ok((client, block_announce_tx, new_receipts_tx, authority_tx, control_tx))
-            },
-        )
+            // Take care of changed authorities.
+            // Notify the network about the new set of UID -> AccountId.
+            let (owner_uid, uid_to_authority_map) =
+                client.get_uid_to_authority_map(new_block.header().index());
+            tokio::spawn({
+                authority_tx
+                    .clone()
+                    .send(uid_to_authority_map.clone())
+                    .map(|_| ())
+                    .map_err(|e| error!("Error sending authorities to the network: {}", e))
+            });
+            // Send control to TxFlow.
+            let control = match owner_uid {
+                None => Control::Stop,
+                Some(owner_uid) => {
+                    let witness_selector = Box::new(BeaconWitnessSelector::new(
+                        uid_to_authority_map.keys().cloned().collect(),
+                        owner_uid,
+                    ));
+                    Control::Reset(State {
+                        owner_uid,
+                        starting_epoch: 0,
+                        gossip_size: 1, // TODO: Use adaptive gossip size.
+                        witness_selector,
+                    })
+                }
+            };
+            tokio::spawn({
+                control_tx
+                    .clone()
+                    .send(control)
+                    .map(|_| ())
+                    .map_err(|e| error!("Error sending control to TxFlow: {}", e))
+            });
+            future::ok(client)
+        })
         .and_then(|_| Ok(()));
     tokio::spawn(task);
 }
