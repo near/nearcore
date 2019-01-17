7701301302153822f6a2f3050957a8fe56469b46
Maksym Zavershynskyi <max@near.ai>
2019-01-17 12:33:10 -0800

    Nit

--- node/client/src/test_utils.rs
+++ node/client/src/test_utils.rs
@@ -16,7 +16,7 @@ use storage::StateDb;
 use node_runtime::test_utils::generate_test_chain_spec;
 use node_runtime::state_viewer::StateDbViewer;
 
-/// Implements dummy clien for testing. The differences with the real client:
+/// Implements dummy client for testing. The differences with the real client:
 /// * It does not do the correct signing;
 /// * It has in-memory storage.
 pub fn get_client_from_cfg(chain_spec: &ChainSpec, signer: InMemorySigner) -> Client {
