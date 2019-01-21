9896565f69ce5bc8070e52315e31d63238f65597
Bowen Wang <bowenwang1996@uchicago.edu>
2019-01-20 17:40:15 -0800

    rewrite network using tasks instead of implementing stream

--- core/near-network/src/codec.rs
+++ core/near-network/src/codec.rs
@@ -1,6 +1,8 @@
 use ::tokio_codec::{Encoder, Decoder};
 use ::bytes::{BytesMut, BufMut, Bytes};
 use std::io::{Error, ErrorKind};
+use ::primitives::traits::{Encode, Decode};
+use super::ServiceEvent;
 
 pub struct Codec {
     max_length: u32
@@ -15,30 +17,33 @@ impl Codec {
 }
 
 impl Encoder for Codec {
-    type Item = Bytes;
+    type Item = ServiceEvent;
     type Error = Error;
 
-    fn encode(&mut self, item: Bytes, buf: &mut BytesMut) -> Result<(), Error> {
+    fn encode(&mut self, item: ServiceEvent, buf: &mut BytesMut) -> Result<(), Error> {
+        let bytes = Bytes::from(
+            item.encode().ok_or_else(|| Error::new(ErrorKind::InvalidInput, "cannot encode event"))?
+        );
         // first four bytes is the length of the buffer
-        buf.reserve(item.len() + 4);
-        if item.len() > self.max_length as usize {
+        buf.reserve(bytes.len() + 4);
+        if bytes.len() > self.max_length as usize {
             Err(Error::new(
                 ErrorKind::InvalidInput,
                 "Input is too long"
             ))
         } else {
-            buf.put_u32_le(item.len() as u32);
-            buf.put(item);
+            buf.put_u32_le(bytes.len() as u32);
+            buf.put(bytes);
             Ok(())
         }
     }
 }
 
 impl Decoder for Codec {
-    type Item = Bytes;
+    type Item = ServiceEvent;
     type Error = Error;
 
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Bytes>, Error> {
+    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<ServiceEvent>, Error> {
         if buf.len() < 4 {
             // not enough bytes to start decoding
             return Ok(None)
@@ -52,9 +57,10 @@ impl Decoder for Codec {
             // not enough bytes, keep waiting
             Ok(None)
         } else {
-            let res = Bytes::from(&buf[4..4 + len as usize]);
+            let event = Decode::decode(&buf[4..4 + len as usize])
+                .ok_or_else(|| Error::new(ErrorKind::InvalidInput, "cannot decode event"))?;
             buf.advance(4 + len as usize);
-            Ok(Some(res))
+            Ok(Some(event))
         }
     }
 }
@@ -62,14 +68,18 @@ impl Decoder for Codec {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ::primitives::hash::hash_struct;
 
     #[test]
     fn test_codec_simple() {
         let mut codec = Codec::new();
-        let msg = b"hello, world!".to_vec();
+        let event = ServiceEvent::Message {
+            peer_id: hash_struct(&0),
+            data: b"hello, world".to_vec()
+        };
         let mut buffer = BytesMut::new();
-        codec.encode(msg.clone().into(), &mut buffer).unwrap();
+        codec.encode(event.clone(), &mut buffer).unwrap();
         let decoded = codec.decode(&mut buffer).unwrap().unwrap();
-        assert_eq!(decoded.to_vec(), msg);
+        assert_eq!(decoded, event);
     }
 }
