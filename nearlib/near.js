46e813a395ea39f528f0855fc1d6271ee16b582a
janedegtiareva <jane.degtiareva@gmail.com>
2019-01-23 15:31:48 -0800

    [nearlib] imrove code comments so that jsdoc produced good quality output (#457)

--- nearlib/near.js
+++ nearlib/near.js
@@ -2,18 +2,23 @@ const NearClient = require('./nearclient');
 const BrowserLocalStorageKeystore = require('./signing/browser_local_storage_keystore');
 const SimpleKeyStoreSigner = require('./signing/simple_key_store_signer');
 const LocalNodeConnection = require('./local_node_connection');
-const KeyPair = require("./signing/key_pair");
 
-/*
- * This is javascript library for interacting with blockchain.
+/**
+ * Javascript library for interacting with near. 
  */
 class Near {
+    /**
+     * Constructs near with an instance of nearclient.
+     * @constructor
+     * @param {NearClient} nearClient 
+     */
     constructor(nearClient) {
         this.nearClient = nearClient;
     }
 
     /**
-     * Default setup for browser
+     * Generate a default configuration for nearlib
+     * @param {string} nodeUrl url of the near node to connect to
      */
     static createDefaultConfig(nodeUrl = "http://localhost:3030") {
         return new Near(new NearClient(
@@ -24,6 +29,10 @@ class Near {
 
     /**
      * Calls a view function. Returns the same value that the function returns.
+     * @param {string} sender account id of the sender
+     * @param {string} contractAccountId account id of the contract
+     * @param {string} methodName method to call
+     * @param {object} args arguments to pass to the method
      */
     async callViewFunction(sender, contractAccountId, methodName, args) {
         if (!args) {
@@ -41,7 +50,13 @@ class Near {
     };
 
     /**
-     * Schedules an asynchronous function call.
+     * Schedules an asynchronous function call. Returns a hash which can be used to
+     * check the status of the transaction later.
+     * @param {number} amount amount of tokens to transfer as part of the operation
+     * @param {string} sender account id of the sender 
+     * @param {string} contractAccountId account id of the contract
+     * @param {string} methodName method to call
+     * @param {object} args arguments to pass to the method
      */
     async scheduleFunctionCall(amount, sender, contractAccountId, methodName, args) {
         if (!args) {
@@ -58,14 +73,17 @@ class Near {
     };
 
     /**
-     * Deploys a contract.
+     *  Deploys a smart contract to the block chain
+     * @param {string} sender account id of the sender
+     * @param {string} contractAccountId account id of the contract
+     * @param {Uint8Array} wasmArray wasm binary
      */
-    async deployContract(senderAccountId, contractAccountId, wasmArray, publicKey) {
+    async deployContract(sender, contractAccountId, wasmArray) {
         return await this.nearClient.submitTransaction('deploy_contract', {
-            originator: senderAccountId,
+            originator: sender,
             contract_account_id: contractAccountId,
             wasm_byte_array: wasmArray,
-            public_key: publicKey
+            public_key: "9AhWenZ3JddamBoyMqnTbp7yVbRuvqAv3zwfrWgfVRJE" // This parameter is not working properly yet. Use some fake value
         });
     }
 
