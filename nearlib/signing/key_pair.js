c66f2ca5589b1fafd3c90759907057a3e0c101b6
janedegtiareva <jane.degtiareva@gmail.com>
2019-01-23 18:01:59 -0800

    [documentation] improve code comments for nearlib+ add readme for generating docs (#461)
    * [nearlib] imrove code comments so that jsdoc produced good quality output
    
    * [docs] pretty up docs for key pair + add instructions for docs
    
    * [nearlib] code style fix merge undoing code style improvements
    
    * [docs] address code review comments
    
    * [nearlib] update wasm file location for integration test

--- nearlib/signing/key_pair.js
+++ nearlib/signing/key_pair.js
@@ -1,23 +1,39 @@
-/**
- * Key pair.
- */
+
 const bs58 = require('bs58');
 const nacl = require('tweetnacl');
 
+/**
+ * This class provides key pair functionality (generating key pairs, encoding key pairs).
+ */
 class KeyPair {
+    /**
+     * Construct an instance of key pair given a public key and secret key. It's generally assumed that these
+     * are encoded in bs58.
+     * @param {string} publicKey 
+     * @param {string} secretKey 
+     */
     constructor(publicKey, secretKey) {
         this.publicKey = publicKey;
         this.secretKey = secretKey;
     }
 
+    /**
+     * Get the public key.
+     */
     getPublicKey() {
         return this.publicKey;
     }
 
+    /**
+     * Get the secret key.
+     */
     getSecretKey() {
         return this.secretKey;
     }
 
+    /**
+     * Generate a new keypair from a random seed
+     */
     static async fromRandomSeed() {
         var newKeypair = nacl.sign.keyPair();
         const result = new KeyPair(
@@ -26,6 +42,10 @@ class KeyPair {
         return result;
     }
 
+    /**
+     * Encode a buffer as string using bs58
+     * @param {Buffer} buffer 
+     */
     static encodeBufferInBs58(buffer) {
         const bytes = Buffer.from(buffer);
         const encodedValue = bs58.encode(bytes);
