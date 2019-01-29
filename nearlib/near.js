83f8d3ff4f3bade3d949bbad56056bf43b5d756e
Vladimir Grichina <vgrichina@gmail.com>
2019-01-29 15:37:19 -0800

    Increase loadContract methods poll period to 750 ms

--- nearlib/near.js
+++ nearlib/near.js
@@ -4,7 +4,7 @@ const SimpleKeyStoreSigner = require('./signing/simple_key_store_signer');
 const LocalNodeConnection = require('./local_node_connection');
 
 const MAX_STATUS_POLL_ATTEMPTS = 3;
-const STATUS_POLL_PERIOD_MS = 250;
+const STATUS_POLL_PERIOD_MS = 750;
 
 /**
  * Javascript library for interacting with near.
