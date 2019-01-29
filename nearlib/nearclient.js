8146a707b547268fc737ed5fb3469b9d32fe266d
Evguenia degtiareva <jane.degtiareva@gmail.com>
2019-01-29 14:18:51 -0800

    [nearlib] remove usage of window to fix contract helper

--- nearlib/nearclient.js
+++ nearlib/nearclient.js
@@ -5,13 +5,7 @@ const { SignedTransaction } = require('./protos');
  */
 
 function _arrayBufferToBase64( buffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
-    }
-    return window.btoa( binary );
+    return Buffer.from(b64Encoded, 'base64').toString();
 }
 
 class NearClient {
