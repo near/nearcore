9abcb03657f959a29599226ec818bdfa0f959a09
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2019-02-11 10:20:33 -0800

    Removing dead code. Importing memory is done by runtime now

--- core/wasm/runtest/generate-wasm/build.sh
+++ core/wasm/runtest/generate-wasm/build.sh
@@ -7,9 +7,4 @@ wasm-gc ../to_wasm.wasm
 cp ../to_wasm.wasm ../../res/wasm_with_mem.wasm
 popd
 
-# pushd $(dirname "$0")/import-memory
-# cargo run --release ../to_wasm.wasm ../../res/wasm_with_mem.wasm
-# wasm-gc ../../res/wasm_with_mem.wasm
-# popd
-
 rm $(dirname "$0")/to_wasm.wasm
