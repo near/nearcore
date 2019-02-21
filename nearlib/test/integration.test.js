1392d51e236a4c25f2362fb011a01fa696e7a967
janedegtiareva <jane.degtiareva@gmail.com>
2019-02-20 17:29:42 -0800

    [nearlib] reduce amount of boilerplate for connecting (#562)
    * [nearlib] reduce amount of boilerplate for connecting
    
    this will be used from command line tools too
    
    * [nearlib] reduce boilerplate p2
    
    * [nearlib] reduce boiler place v3
    
    * [nits] addressing nits
    
    * Few nit changes, eslint runs, a bit of comments for setupConnection
    
    * A bit more update in dev.js
    
    * [nearlib] a few minor code cleanup changes
    :w
    
    * [nearlib] address code review comments for connect method

--- nearlib/test/integration.test.js
+++ nearlib/test/integration.test.js
@@ -1,18 +1,20 @@
-const { Account, SimpleKeyStoreSigner, InMemoryKeyStore, KeyPair, LocalNodeConnection, NearClient, Near } = require('../');
+const { Account, KeyPair, Near, InMemoryKeyStore } = require('../');
+const dev = require('../dev');
 const fs = require('fs');
-
 const aliceAccountName = 'alice.near';
-const aliceKey = new KeyPair(
-    '22skMptHjFWNyuEWY22ftn2AbLPSYpmYwGJRGwpNHbTV',
-    '2wyRcSwSuHtRVmkMCGjPwnzZmQLeXLzLLyED1NDMt4BjnKgQL6tF85yBx6Jr26D2dUNeC716RBoTxntVHsegogYw'
-);
-const test_key_store = new InMemoryKeyStore();
-const simple_key_store_signer = new SimpleKeyStoreSigner(test_key_store);
-test_key_store.setKey(aliceAccountName, aliceKey);
-const localNodeConnection = new LocalNodeConnection('http://localhost:3030');
-const nearClient = new NearClient(simple_key_store_signer, localNodeConnection);
-const account = new Account(nearClient);
-const nearjs = new Near(nearClient);
+let nearjs;
+let account;
+let keyStore;
+
+beforeAll(async () => {
+    keyStore = new InMemoryKeyStore();
+    nearjs = await dev.connect({
+        nodeUrl: 'http://localhost:3030',
+        useDevAccount: true,
+        deps: { keyStore },
+    });
+    account = new Account(nearjs.nearClient);
+});
 
 test('test creating default config', async () => {
     // Make sure createDefaultConfig doesn't crash.
@@ -80,7 +82,7 @@ describe('with deployed contract', () => {
             10,
             aliceAccountName);
         await nearjs.waitForTransactionResult(createAccountResponse);
-        test_key_store.setKey(contractName, keyWithRandomSeed);
+        keyStore.setKey(contractName, keyWithRandomSeed);
         const data = [...fs.readFileSync('../tests/hello.wasm')];
         await nearjs.waitForTransactionResult(
             await nearjs.deployContract(contractName, data));
@@ -132,12 +134,11 @@ describe('with deployed contract', () => {
         expect(getValueResult).toEqual(setCallValue);
 
         // test that load contract works and we can make calls afterwards
-        const options = {
+        const contract = await nearjs.loadContract(contractName, {
             viewMethods: ['hello', 'getValue'],
             changeMethods: ['setValue'],
-            sender: aliceAccountName
-        };
-        const contract = await nearjs.loadContract(contractName, options);
+            sender: aliceAccountName,
+        });
         const helloResult = await contract.hello(args);
         expect(helloResult).toEqual('hello trex');
         var setCallValue2 = await generateUniqueString('setCallPrefix');
