a66e0c65804cd3eab502d94d5270e90f54c9057c
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2019-02-20 19:33:25 -0800

    Minor mempool fixes (#589)

--- core/mempool/src/lib.rs
+++ core/mempool/src/lib.rs
@@ -1,14 +1,17 @@
-use std::collections::HashSet;
-use std::sync::Arc;
-use parking_lot::RwLock;
-use tokio::sync::mpsc::{Sender, Receiver};
 use futures::{Future, Stream};
 use log::error;
-use primitives::transaction::{SignedTransaction, verify_transaction_signature};
-use primitives::chain::{ReceiptBlock, ChainPayload, SignedShardBlock};
-use primitives::merkle::verify_path;
+use primitives::chain::{ChainPayload, ReceiptBlock, SignedShardBlock};
 use primitives::hash::hash_struct;
+use primitives::merkle::verify_path;
+use primitives::transaction::{verify_transaction_signature, SignedTransaction};
 use shard::ShardBlockChain;
+use std::collections::HashSet;
+use std::sync::Arc;
+use std::sync::RwLock;
+use tokio::sync::mpsc::{Receiver, Sender};
+
+const POISONED_LOCK_ERR: &str = "The lock was poisoned";
+const TOKIO_RECV_ERR: &str = "Implementation of tokio Receiver should not return an error";
 
 /// mempool that stores transactions and receipts for a chain
 struct Pool {
@@ -20,11 +23,7 @@ struct Pool {
 
 impl Pool {
     fn new(chain: Arc<ShardBlockChain>) -> Self {
-        Pool {
-            transactions: HashSet::new(),
-            receipts: HashSet::new(),
-            chain,
-        }
+        Pool { transactions: HashSet::new(), receipts: HashSet::new(), chain }
     }
 
     fn add_transaction(&mut self, transaction: SignedTransaction) -> Result<(), String> {
@@ -33,10 +32,8 @@ impl Pool {
         }
         let mut state_update = self.chain.get_state_update();
         let originator = transaction.body.get_originator();
-        let public_keys = self.chain.trie_viewer.get_public_keys_for_account(
-            &mut state_update,
-            &originator
-        )?;
+        let public_keys =
+            self.chain.trie_viewer.get_public_keys_for_account(&mut state_update, &originator)?;
         if !verify_transaction_signature(&transaction, &public_keys) {
             return Err(format!(
                 "transaction not signed with a public key of originator {:?}",
@@ -53,7 +50,7 @@ impl Pool {
             return Ok(());
         }
         if !verify_path(receipt.header.body.receipt_merkle_root, &receipt.path, &receipt.receipts) {
-            return Err("Invalid receipt block".to_string())
+            return Err("Invalid receipt block".to_string());
         }
         self.receipts.insert(receipt);
         Ok(())
@@ -86,51 +83,47 @@ pub fn spawn_mempool(
     chain: Arc<ShardBlockChain>,
 ) {
     let pool = Arc::new(RwLock::new(Pool::new(chain)));
-    let block_task = block_rx.for_each({
-        let pool = pool.clone();
-        move |b| {
-            pool.write().import_block(&b);
-            Ok(())
-        }
-    }).map_err(|e| error!("Error in receiving block: {:?}", e));
+    let block_task = block_rx
+        .for_each({
+            let pool = pool.clone();
+            move |b| {
+                pool.write().expect(POISONED_LOCK_ERR).import_block(&b);
+                Ok(())
+            }
+        })
+        .map_err(|_| error!(target: "memorypool", "{}", TOKIO_RECV_ERR));
     tokio::spawn(block_task);
-    let transaction_task = transaction_rx.for_each({
-        let pool = pool.clone();
-        move |t| {
-            // TODO: report malicious behavior
-            match pool.write().add_transaction(t) {
-                Ok(_) => Ok(()),
-                Err(e) => {
-                    error!("{}", e);
-                    Ok(())
-                }
+    let transaction_task = transaction_rx
+        .for_each({
+            let pool = pool.clone();
+            move |t| {
+                // TODO: report malicious behavior
+                pool.write().expect(POISONED_LOCK_ERR).add_transaction(t)
+                    .or_else(|e| Ok(error!(target: "memorypool", "Failed to write transaction into a memory pool {}", e)))
             }
-        }
-    }).map_err(|e| error!("Error in receiving transaction {:?}", e));
+        })
+        .map_err(|_| error!(target: "memorypool", "{}", TOKIO_RECV_ERR));
     tokio::spawn(transaction_task);
-    let receipt_task = receipt_rx.for_each({
-        let pool = pool.clone();
-        move |r| {
-            // TODO: report malicious behavior
-            match pool.write().add_receipt(r) {
-                Ok(_) => Ok(()),
-                Err(e) => {
-                    error!("{}", e);
-                    Ok(())
-                }
+    let receipt_task = receipt_rx
+        .for_each({
+            let pool = pool.clone();
+            move |r| {
+                // TODO: report malicious behavior
+                pool.write().expect(POISONED_LOCK_ERR).add_receipt(r)
+                    .or_else(|e| Ok(error!(target: "memorypool", "Failed to write receipt into a memory pool {}", e)))
             }
-        }
-    }).map_err(|e| error!("Error in receiving receipt {:?}", e));
+        })
+        .map_err(|_| error!(target: "memorypool", "{}", TOKIO_RECV_ERR));
     tokio::spawn(receipt_task);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use primitives::hash::CryptoHash;
-    use primitives::signature::{SecretKey, sign};
-    use primitives::transaction::{TransactionBody, SendMoneyTransaction};
     use node_runtime::test_utils::generate_test_chain_spec;
+    use primitives::hash::CryptoHash;
+    use primitives::signature::{sign, SecretKey};
+    use primitives::transaction::{SendMoneyTransaction, TransactionBody};
     use storage::test_utils::create_beacon_shard_storages;
 
     fn get_test_chain() -> (ShardBlockChain, SecretKey) {
@@ -153,10 +146,7 @@ mod tests {
         });
         let hash = tx_body.get_hash();
         let signature = sign(hash.as_ref(), &secret_key);
-        let transaction = SignedTransaction::new(
-            signature,
-            tx_body
-        );
+        let transaction = SignedTransaction::new(signature, tx_body);
         pool.add_transaction(transaction.clone()).unwrap();
         assert_eq!(pool.transactions.len(), 1);
         let block = SignedShardBlock::new(
@@ -166,7 +156,7 @@ mod tests {
             CryptoHash::default(),
             vec![transaction],
             vec![],
-            CryptoHash::default()
+            CryptoHash::default(),
         );
         pool.import_block(&block);
         assert_eq!(pool.transactions.len(), 0);
