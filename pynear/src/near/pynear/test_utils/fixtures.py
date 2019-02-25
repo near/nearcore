2179c5924352cfda71541e850546c920711a9bdd
azban <me@azban.net>
2019-02-25 13:20:21 -0800

    pynear: use separate devnet port for each test run to avoid collision

--- pynear/src/near/pynear/test_utils/fixtures.py
+++ pynear/src/near/pynear/test_utils/fixtures.py
@@ -6,25 +6,40 @@ from retrying import retry
 
 from near.pynear.lib import NearLib
 
-nearlib = NearLib('http://localhost:3030/')
-
 
 @retry(stop_max_attempt_number=5, wait_fixed=1000)
-def check_devnet_health(process):
+def check_devnet_health(process, nearlib):
     if not process.is_alive:
         return False
 
     return nearlib.check_health()
 
 
+@pytest.fixture(scope='session')
+def get_incrementing_number():
+    # jank because py2 does not support nonlocal keyword
+    # https://stackoverflow.com/a/3190783
+    d = {'latest': -1}
+
+    def _get_incrementing_number():
+        d['latest'] += 1
+        return d['latest']
+
+    return _get_incrementing_number
+
+
 @pytest.fixture
-def make_devnet(request):
+def make_devnet(request, get_incrementing_number):
     def _make_devnet(base_dir):
+        port = 3030 + get_incrementing_number()
         devnet_exe = os.environ['NEAR_DEVNET_EXE']
-        command = "{devnet_exe} -d {base_dir} --test-block-period 5" \
-            .format(devnet_exe=devnet_exe, base_dir=base_dir)
+        command = "{devnet_exe} -d {base_dir} --rpc_port {port} " \
+                  "--test-block-period 5" \
+            .format(devnet_exe=devnet_exe, base_dir=base_dir, port=port)
         process = delegator.run(command, block=False)
         request.addfinalizer(process.kill)
-        return check_devnet_health(process)
+        nearlib = NearLib("http://localhost:{}/".format(port))
+        assert check_devnet_health(process, nearlib)
+        return port
 
     return _make_devnet
--- pynear/tests/integration/test_cli.py
+++ pynear/tests/integration/test_cli.py
@@ -1,6 +1,6 @@
 import json
 
-from near.pynear import test_utils
+from near.pynear.test_utils.cli import CliHelpers
 from near.pynear.test_utils.fixtures import *
 
 
@@ -15,79 +15,79 @@ def hello_wasm_path():
 
 
 def test_view_latest_beacon_block(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    test_utils.cli.get_latest_beacon_block()
+    port = make_devnet(tmpdir)
+    CliHelpers(port).get_latest_beacon_block()
 
 
 def test_get_beacon_block_by_hash(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    latest_block = test_utils.cli.get_latest_beacon_block()
+    port = make_devnet(tmpdir)
+    latest_block = CliHelpers(port).get_latest_beacon_block()
     hash_ = latest_block['hash']
     command = "get_beacon_block_by_hash {}".format(hash_)
-    out = test_utils.cli.run_command(command)
+    out = CliHelpers(port).run_command(command)
     assert latest_block == json.loads(out)
 
 
 def test_view_latest_shard_block(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    test_utils.cli.get_latest_shard_block()
+    port = make_devnet(tmpdir)
+    CliHelpers(port).get_latest_shard_block()
 
 
 def test_get_shard_block_by_hash(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    latest_block = test_utils.cli.get_latest_shard_block()
+    port = make_devnet(tmpdir)
+    latest_block = CliHelpers(port).get_latest_shard_block()
     hash_ = latest_block['hash']
     command = "get_shard_block_by_hash {}".format(hash_)
-    out = test_utils.cli.run_command(command)
+    out = CliHelpers(port).run_command(command)
     assert latest_block == json.loads(out)
 
 
 def test_view_account(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    test_utils.cli.view_account()
+    port = make_devnet(tmpdir)
+    CliHelpers(port).view_account()
 
 
 def test_create_account(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
+    port = make_devnet(tmpdir)
     account_id = 'eve.near'
-    test_utils.cli.create_account(account_id)
+    CliHelpers(port).create_account(account_id)
 
 
 def test_deploy_contract(make_devnet, tmpdir, hello_wasm_path):
-    assert make_devnet(tmpdir)
-    test_utils.cli.deploy_contract(hello_wasm_path)
+    port = make_devnet(tmpdir)
+    CliHelpers(port).deploy_contract(hello_wasm_path)
 
 
 def test_send_money(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
+    port = make_devnet(tmpdir)
     receiver = 'send_money_test.near'
-    test_utils.cli.create_account(receiver)
+    CliHelpers(port).create_account(receiver)
     command = "send_money --receiver {} --amount 1".format(receiver)
-    test_utils.cli.run_command(command)
+    CliHelpers(port).run_command(command)
 
     @retry(stop_max_attempt_number=5, wait_fixed=1000)
     def _wait_for_balance_change():
-        account = test_utils.cli.view_account(receiver)
+        account = CliHelpers(port).view_account(receiver)
         assert account['amount'] == 11
 
     _wait_for_balance_change()
 
 
 def test_set_get_values(make_devnet, tmpdir, hello_wasm_path):
-    assert make_devnet(tmpdir)
-    contract = test_utils.cli.deploy_contract(hello_wasm_path)
+    port = make_devnet(tmpdir)
+    contract = CliHelpers(port).deploy_contract(hello_wasm_path)
     contract_name = contract['account_id']
     value = 'test'
     args = {'value': value}
     command = "schedule_function_call {} setValue --args '{}'" \
         .format(contract_name, json.dumps(args))
-    test_utils.cli.run_command(command)
+    CliHelpers(port).run_command(command)
 
     @retry(stop_max_attempt_number=5, wait_fixed=1000)
     def _wait_for_state_change():
         command_ = "call_view_function {} getValue --args {{}}" \
             .format(contract_name)
-        out = test_utils.cli.run_command(command_)
+        out = CliHelpers(port).run_command(command_)
         data = json.loads(out)
         assert data['result'] == value
 
@@ -95,17 +95,17 @@ def test_set_get_values(make_devnet, tmpdir, hello_wasm_path):
 
 
 def test_view_state(make_devnet, tmpdir, hello_wasm_path):
-    assert make_devnet(tmpdir)
-    contract = test_utils.cli.deploy_contract(hello_wasm_path)
+    port = make_devnet(tmpdir)
+    contract = CliHelpers(port).deploy_contract(hello_wasm_path)
     contract_name = contract['account_id']
     command = "view_state {}".format(contract_name)
-    out = test_utils.cli.run_command(command)
+    out = CliHelpers(port).run_command(command)
     data = json.loads(out)
     assert data['values'] == {}
 
 
 def test_swap_key(make_devnet, tmpdir):
-    assert make_devnet(tmpdir)
-    public_key = nearlib.keystore.create_key_pair('alice.near')
+    port = make_devnet(tmpdir)
+    public_key = NearLib().keystore.create_key_pair('alice.near')
     command = "swap_key {} {}".format(public_key, public_key)
-    test_utils.cli.run_command(command)
+    CliHelpers(port).run_command(command)
