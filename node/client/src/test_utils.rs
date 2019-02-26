c8e5898ea3016a46b1c28ebfe2d6e5fea6eb487f
Bowen Wang <bowenwang1996@users.noreply.github.com>
2019-02-26 18:34:36 +0800

    Rename BeaconBlockChain to BeaconClient (#622)

--- node/client/src/test_utils.rs
+++ node/client/src/test_utils.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use crate::Client;
-use beacon::beacon_chain::BeaconBlockChain;
+use beacon::beacon_chain::BeaconClient;
 use configs::ChainSpec;
 use primitives::beacon::SignedBeaconBlock;
 use primitives::signer::InMemorySigner;
@@ -16,7 +16,7 @@ pub fn get_client_from_cfg(chain_spec: &ChainSpec, signer: InMemorySigner) -> Cl
     let (beacon_storage, shard_storage) = create_beacon_shard_storages();
     let shard_client = ShardClient::new(chain_spec, shard_storage);
     let genesis = SignedBeaconBlock::genesis(shard_client.genesis_hash());
-    let beacon_chain = BeaconBlockChain::new(genesis, chain_spec, beacon_storage);
+    let beacon_chain = BeaconClient::new(genesis, chain_spec, beacon_storage);
     Client {
         account_id: signer.account_id.clone(),
         signer,
