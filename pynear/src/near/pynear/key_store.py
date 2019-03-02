0d105119019dd06a47094b687ebc5dec25a1e908
azban <714931+azban@users.noreply.github.com>
2019-03-02 10:36:10 -0800

    pynear: ed25519 -> pyca/cryptography (#633)

--- pynear/src/near/pynear/key_store.py
+++ pynear/src/near/pynear/key_store.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-import ed25519
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
 
 from near.pynear import b58
 
@@ -25,12 +26,20 @@ class KeyStore(object):
                 raise Exception('max seed length is 32')
 
             seed = seed.encode('utf-8')
+            seed = seed.ljust(32)
+            key = ed25519.Ed25519PrivateKey.from_private_bytes(seed)
+        else:
+            key = ed25519.Ed25519PrivateKey.generate()
 
-        kwargs = {}
-        if seed is not None:
-            kwargs['entropy'] = lambda x: seed.ljust(32)
+        return key, key.public_key()
 
-        return ed25519.create_keypair(**kwargs)
+    @staticmethod
+    def _get_b58_encoded_public_key(public_key):
+        public_key = public_key.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw,
+        )
+        return b58.b58encode(public_key).decode('utf-8')
 
     def create_key_pair(self, seed=None):
         raise NotImplementedError
@@ -48,7 +57,7 @@ class InMemoryKeyStore(KeyStore):
 
     def create_key_pair(self, seed=None):
         (secret_key, public_key) = self._create_key_pair(seed)
-        encoded = b58.b58encode(public_key.to_bytes()).decode('utf-8')
+        encoded = self._get_b58_encoded_public_key(public_key)
         self._key_pairs[encoded] = secret_key
         return encoded
 
@@ -77,8 +86,14 @@ class FileKeyStore(KeyStore):
             os.makedirs(self._path)
 
         (secret_key, public_key) = self._create_key_pair(seed)
-        encoded_pub = b58.b58encode(public_key.to_bytes()).decode('utf-8')
-        encoded_secret = b58.b58encode(public_key.to_bytes()).decode('utf-8')
+        encoded_pub = self._get_b58_encoded_public_key(public_key)
+
+        secret_key = secret_key.private_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PrivateFormat.Raw,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+        encoded_secret = b58.b58encode(secret_key).decode('utf-8')
 
         with open(os.path.join(self._path, encoded_pub), 'w') as f:
             key_file = {
@@ -98,7 +113,7 @@ class FileKeyStore(KeyStore):
             encoded_secret = key_file['secret_key']
 
         secret_key = b58.b58decode(encoded_secret)
-        secret_key = ed25519.SigningKey(secret_key)
+        secret_key = ed25519.Ed25519PrivateKey.from_private_bytes(secret_key)
         return secret_key.sign(data)
 
     def get_only_public_key(self):
