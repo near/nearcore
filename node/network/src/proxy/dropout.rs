bb6deb061aeafa77be3e7d277201ee7142bfe941
Marcelo Fornet <mfornet94@gmail.com>
2019-03-15 21:38:48 -0400

    Comment proxy

--- node/network/src/proxy/dropout.rs
+++ node/network/src/proxy/dropout.rs
@@ -1,13 +1,15 @@
-use crate::proxy::ProxyHandler;
-use crate::message::Message;
 use rand::Rng;
+
+use crate::message::Message;
+use crate::proxy::ProxyHandler;
 
 const DROPOUT_RATE: f64 = 0.5;
 
 pub struct Dropout {}
 
+/// Messages will be dropped with probability `1/2`
 impl ProxyHandler for Dropout {
-    fn pipe_once(&self, message: &Message) -> Vec<&Message> {
+    fn pipe_one(&self, message: &Message) -> Vec<&Message> {
         let mut rng = rand::thread_rng();
 
         if rng.gen::<f64>() < DROPOUT_RATE {
--- node/network/src/proxy/mod.rs
+++ node/network/src/proxy/mod.rs
@@ -1,6 +1,6 @@
+use std::sync::Arc;
 
 use super::message::Message;
-use std::sync::Arc;
 
 pub mod dropout;
 
@@ -8,6 +8,14 @@ pub struct Proxy {
     handlers: Vec<Arc<ProxyHandler>>
 }
 
+/// All messages before being sent to other participants will be processed by several handlers
+/// chained sequentially in a pipeline.
+///
+/// Each handler will receive an iterator of messages and will return (not necessarily the same)
+/// iterator of messages. This method allows easy manipulation of messages before being emitted
+/// to other authorities through the network.
+///
+/// The order in which handlers are grouped can affect the behavior of the proxy.
 impl Proxy {
     pub fn new() -> Self {
         Self {
@@ -26,16 +34,21 @@ impl Proxy {
     }
 }
 
+/// ProxyHandler interface.
 pub trait ProxyHandler where Self: Send + Sync {
     fn pipe(&self, mut messages: Vec<&Message>) -> Vec<&Message> {
         messages
             .drain(..)
-            .map(|message| self.pipe_once(message))
+            .map(|message| self.pipe_one(message))
             .fold(vec![], |mut cur_messages, new_messages| {
                 cur_messages.extend(new_messages);
                 cur_messages
             })
     }
 
-    fn pipe_once(&self, message: &Message) -> Vec<&Message>;
+    /// Logic of the handler. This function will be called once for every message that get
+    /// to this point in the same order messages arrive to the proxy.
+    ///
+    /// The input of each handler is output of the previous handler in the `pipeline`.
+    fn pipe_one(&self, message: &Message) -> Vec<&Message>;
 }
