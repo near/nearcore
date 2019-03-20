39b33a41310c1a46edf29f3548f45224636486d9
Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
2019-03-20 14:35:22 -0700

    Refactor Wasm APIs (breaking change for old contracts) (#746)
    Improving performance and APIs.
    
    Removing prefix hack. Where first 4 bytes contained the length of the buffer. It quite often resulted in a copy that was needed before providing data to runtime. Switched to provide length and ptr as 2 variables instead.
    Updating read API to require only one function call instead of 2 (in most cases). It will try to write into the given buffer if the length allowed or just return a new length. New near.ts https://github.com/nearprotocol/near-runtime-ts/tree/wasm-new-read-api
    Modified AssemblyScript compiler to use new return_value API. Also removing "near_func_" prefix, since we don't need it anymore. To use it, I had to push a near-shell branch which points to the new AS branch: https://github.com/nearprotocol/assemblyscript/tree/wasm-new-read-api

--- tests/hello/assembly/main.near.ts
+++ tests/hello/assembly/main.near.ts
@@ -1,18 +1,14 @@
 
-      import { near } from "./near";
+      import { storage, near } from "./near";
       import { JSONEncoder} from "./json/encoder"
       import { JSONDecoder, ThrowingJSONHandler, DecoderState  } from "./json/decoder"
       import {hello as wrapped_hello, setKeyValue as wrapped_setKeyValue, getValueByKey as wrapped_getValueByKey, setValue as wrapped_setValue, getValue as wrapped_getValue, getAllKeys as wrapped_getAllKeys, benchmark as wrapped_benchmark, benchmark_storage as wrapped_benchmark_storage, benchmark_sum_n as wrapped_benchmark_sum_n, generateLogs as wrapped_generateLogs, returnHiWithLogs as wrapped_returnHiWithLogs, triggerAssert as wrapped_triggerAssert, testSetRemove as wrapped_testSetRemove, callPromise as wrapped_callPromise, callbackWithName as wrapped_callbackWithName, getLastResult as wrapped_getLastResult} from "./main";
 
       // Runtime functions
       @external("env", "return_value")
-      declare function return_value(value_ptr: u32): void;
-      @external("env", "input_read_len")
-      declare function input_read_len(): u32;
-      @external("env", "input_read_into")
-      declare function input_read_into(ptr: usize): void;
+      declare function return_value(value_len: usize, value_ptr: usize): void;
     
-import {contractContext as contractContext,globalStorage as globalStorage,ContractPromise as ContractPromise,ContractPromiseResult as ContractPromiseResult,near as near} from "./near";
+import {context as context,storage as storage,ContractPromise as ContractPromise,ContractPromiseResult as ContractPromiseResult,near as near} from "./near";
 import {PromiseArgs as PromiseArgs,InputPromiseArgs as InputPromiseArgs,MyCallbackResult as MyCallbackResult,MyContractPromiseResult as MyContractPromiseResult,ResultWrappedMyCallbackResult as ResultWrappedMyCallbackResult} from "./model.near";
 export class __near_ArgsParser_hello extends ThrowingJSONHandler {
         buffer: Uint8Array;
@@ -54,9 +50,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_hello(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function hello(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_hello();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_hello>(handler);
@@ -75,7 +71,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_setKeyValue extends ThrowingJSONHandler {
@@ -127,9 +124,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_setKeyValue(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function setKeyValue(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_setKeyValue();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_setKeyValue>(handler);
@@ -178,9 +175,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_getValueByKey(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function getValueByKey(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_getValueByKey();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_getValueByKey>(handler);
@@ -199,7 +196,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_setValue extends ThrowingJSONHandler {
@@ -242,9 +240,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_setValue(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function setValue(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_setValue();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_setValue>(handler);
@@ -263,7 +261,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_getValue extends ThrowingJSONHandler {
@@ -293,9 +292,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_getValue(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function getValue(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_getValue();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_getValue>(handler);
@@ -314,7 +313,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_getAllKeys extends ThrowingJSONHandler {
@@ -355,9 +355,9 @@ if (value[i] != null) {
           }
 }
 }
-export function near_func_getAllKeys(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function getAllKeys(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_getAllKeys();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_getAllKeys>(handler);
@@ -378,7 +378,8 @@ if (result != null) {
         }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_benchmark extends ThrowingJSONHandler {
@@ -408,9 +409,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_benchmark(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function benchmark(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_benchmark();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_benchmark>(handler);
@@ -431,7 +432,8 @@ if (result != null) {
         }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_benchmark_storage extends ThrowingJSONHandler {
@@ -474,9 +476,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_benchmark_storage(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function benchmark_storage(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_benchmark_storage();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_benchmark_storage>(handler);
@@ -495,7 +497,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_benchmark_sum_n extends ThrowingJSONHandler {
@@ -538,9 +541,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_benchmark_sum_n(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function benchmark_sum_n(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_benchmark_sum_n();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_benchmark_sum_n>(handler);
@@ -559,7 +562,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_generateLogs extends ThrowingJSONHandler {
@@ -589,9 +593,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_generateLogs(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function generateLogs(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_generateLogs();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_generateLogs>(handler);
@@ -627,9 +631,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_returnHiWithLogs(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function returnHiWithLogs(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_returnHiWithLogs();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_returnHiWithLogs>(handler);
@@ -648,7 +652,8 @@ if (result != null) {
           }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_triggerAssert extends ThrowingJSONHandler {
@@ -678,9 +683,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_triggerAssert(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function triggerAssert(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_triggerAssert();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_triggerAssert>(handler);
@@ -729,9 +734,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_testSetRemove(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function testSetRemove(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_testSetRemove();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_testSetRemove>(handler);
@@ -777,9 +782,9 @@ if (name == "args") {
         return super.pushArray(name);
       }
 }
-export function near_func_callPromise(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function callPromise(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_callPromise();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_callPromise>(handler);
@@ -825,9 +830,9 @@ if (name == "args") {
       }
 }
 import { __near_encode_MyCallbackResult } from "./model.near";
-export function near_func_callbackWithName(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function callbackWithName(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_callbackWithName();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_callbackWithName>(handler);
@@ -848,7 +853,8 @@ if (result != null) {
         }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
 export class __near_ArgsParser_getLastResult extends ThrowingJSONHandler {
@@ -878,9 +884,9 @@ if (!this.handledRoot) {
         return super.pushArray(name);
       }
 }
-export function near_func_getLastResult(): void {
-      let json = new Uint8Array(input_read_len());
-      input_read_into(json.buffer.data);
+export function getLastResult(): void {
+      // Reading input bytes.
+      let json = storage._internalReadBytes(4, 0, 0);
       let handler = new __near_ArgsParser_getLastResult();
       handler.buffer = json;
       handler.decoder = new JSONDecoder<__near_ArgsParser_getLastResult>(handler);
@@ -901,6 +907,7 @@ if (result != null) {
         }
 
         encoder.popObject();
-        return_value(near.bufferWithSize(encoder.serialize()).buffer.data);
+        let val = encoder.serialize();
+        return_value(val.byteLength, val.buffer.data);
       
 }
