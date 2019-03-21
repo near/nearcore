42a67313f859ce129747245b66451bdbbd7d4bfa
Illia Polosukhin <ilblackdragon@gmail.com>
2019-03-21 14:05:30 -0700

    Allow to specify prefix and image when launch GCloud

--- ops/deploy_alphanet.sh
+++ ops/deploy_alphanet.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-gcloud compute instances create-with-container alphanet-0 \
+IMAGE=${1:-throwawaydude/alphanet:0.0.1}
+PREFIX=${2:-alphanet}
+
+echo "Starting 4 nodes prefixed $PREFIX of $IMAGE on GCloud..."
+
+gcloud compute instances create-with-container $PREFIX-0 \
     --container-env BOOT_NODE_IP=127.0.0.1 \
     --container-env NODE_NUM=0 \
     --container-env TOTAL_NODES=4 \
-    --container-image throwawaydude/alphanet:0.0.1 \
+    --container-image $IMAGE \
     --zone us-west2-a
 
 BOOT_NODE_IP=$(
@@ -14,21 +19,21 @@ gcloud compute instances describe alphanet-0 \
     awk '{print $2}'
 )
 
-gcloud compute instances create-with-container alphanet-1 \
+gcloud compute instances create-with-container $PREFIX-1 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=1 \
     --container-env TOTAL_NODES=4 \
-    --container-image throwawaydude/alphanet:0.0.1 \
+    --container-image $IMAGE \
     --zone us-west2-a
-gcloud compute instances create-with-container alphanet-2 \
+gcloud compute instances create-with-container $PREFIX-2 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=2 \
     --container-env TOTAL_NODES=4 \
-    --container-image throwawaydude/alphanet:0.0.1 \
+    --container-image $IMAGE \
     --zone us-west2-a
-gcloud compute instances create-with-container alphanet-3 \
+gcloud compute instances create-with-container $PREFIX-3 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=3 \
     --container-env TOTAL_NODES=4 \
-    --container-image throwawaydude/alphanet:0.0.1 \
+    --container-image $IMAGE \
     --zone us-west2-a
