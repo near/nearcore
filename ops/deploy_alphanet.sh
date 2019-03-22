40521f875da1a360973e38e20a8469080bfc5562
azban <me@azban.net>
2019-03-21 17:36:45 -0700

    ops: fix up deploy_alphanet script

--- ops/deploy_alphanet.sh
+++ ops/deploy_alphanet.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-IMAGE=${1:-throwawaydude/alphanet:0.0.1}
+IMAGE=${1:-throwawaydude/alphanet:0.0.2}
 PREFIX=${2:-alphanet}
 STUDIO_IMAGE=${3:-throwawaydude/studio:0.0.0}
 
-echo "Starting 4 nodes prefixed $PREFIX of $IMAGE on GCloud..."
+echo "Starting 4 nodes prefixed ${PREFIX} of ${IMAGE} on GCloud..."
 
-gcloud compute instances create-with-container $PREFIX-0 \
+gcloud compute instances create-with-container ${PREFIX}-0 \
     --container-env BOOT_NODE_IP=127.0.0.1 \
     --container-env NODE_NUM=0 \
     --container-env TOTAL_NODES=4 \
-    --container-image $IMAGE \
+    --container-image ${IMAGE} \
     --zone us-west2-a
 
 BOOT_NODE_IP=$(
@@ -20,27 +20,28 @@ gcloud compute instances describe ${PREFIX}-0 \
     awk '{print $2}'
 )
 
-gcloud compute instances create-with-container $PREFIX-1 \
+gcloud compute instances create-with-container ${PREFIX}-1 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=1 \
     --container-env TOTAL_NODES=4 \
-    --container-image $IMAGE \
+    --container-image ${IMAGE} \
     --zone us-west2-a
-gcloud compute instances create-with-container $PREFIX-2 \
+gcloud compute instances create-with-container ${PREFIX}-2 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=2 \
     --container-env TOTAL_NODES=4 \
-    --container-image $IMAGE \
+    --container-image ${IMAGE} \
     --zone us-west2-a
-gcloud compute instances create-with-container $PREFIX-3 \
+gcloud compute instances create-with-container ${PREFIX}-3 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=3 \
     --container-env TOTAL_NODES=4 \
-    --container-image $IMAGE \
+    --container-image ${IMAGE} \
     --zone us-west2-a
-gcloud compute instances create-with-container $PREFIX-studio \
+gcloud compute instances create-with-container ${PREFIX}-studio \
     --container-env DEVNET_HOST=http://${BOOT_NODE_IP} \
-    --container-image $IMAGE \
+    --container-env PLATFORM=GCP \
+    --container-image ${STUDIO_IMAGE} \
     --zone us-west2-a
 
 sleep 30
