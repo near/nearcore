4645c08732bae4533b6eac94fcb85591de3e7700
azban <me@azban.net>
2019-03-22 19:58:56 -0700

    ops: add firewall rule creation to alphanet deploy script

--- ops/deploy_alphanet.sh
+++ ops/deploy_alphanet.sh
@@ -8,12 +8,17 @@ ZONE=${4:-us-west2-a}
 
 echo "Starting 4 nodes prefixed ${PREFIX} of ${IMAGE} on GCloud ${ZONE} zone..."
 
+gcloud compute firewall-rules create alphanet-instance \
+    --allow tcp:3000,tcp:3030 \
+    --target-tags=alphanet-instance
+
 gcloud compute instances create-with-container ${PREFIX}-0 \
     --container-env BOOT_NODE_IP=127.0.0.1 \
     --container-env NODE_NUM=0 \
     --container-env TOTAL_NODES=4 \
     --container-image ${IMAGE} \
-    --zone ${ZONE}
+    --zone ${ZONE} \
+    --tags=alphanet-instance
 
 BOOT_NODE_IP=$(
 gcloud compute instances describe ${PREFIX}-0 \
@@ -27,27 +32,35 @@ gcloud compute instances create-with-container ${PREFIX}-1 \
     --container-env NODE_NUM=1 \
     --container-env TOTAL_NODES=4 \
     --container-image ${IMAGE} \
-    --zone ${ZONE}
+    --zone ${ZONE} \
+    --tags=alphanet-instance
 
 gcloud compute instances create-with-container ${PREFIX}-2 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=2 \
     --container-env TOTAL_NODES=4 \
     --container-image ${IMAGE} \
-    --zone ${ZONE}
+    --zone ${ZONE} \
+    --tags=alphanet-instance
 
 gcloud compute instances create-with-container ${PREFIX}-3 \
     --container-env BOOT_NODE_IP=${BOOT_NODE_IP} \
     --container-env NODE_NUM=3 \
     --container-env TOTAL_NODES=4 \
     --container-image ${IMAGE} \
-    --zone ${ZONE}
+    --zone ${ZONE} \
+    --tags=alphanet-instance
+
+gcloud compute firewall-rules create alphanet-studio \
+    --allow tcp:80 \
+    --target-tags=alphanet-studio
 
 gcloud compute instances create-with-container ${PREFIX}-studio \
     --container-env DEVNET_HOST=http://${BOOT_NODE_IP} \
     --container-env PLATFORM=GCP \
     --container-image ${STUDIO_IMAGE} \
-    --zone ${ZONE}
+    --zone ${ZONE} \
+    --tags=alphanet-studio
 
 # borrowed from https://stackoverflow.com/a/20369590
 spinner()
