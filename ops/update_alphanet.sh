03fd89dbe91abc2a46878940b8e134407d812d15
azban <me@azban.net>
2019-03-27 16:37:49 -0700

    ops: add update_alphanet script

---
+++ ops/update_alphanet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+IMAGE=${1:-nearprotocol/alphanet:0.1.1}
+PREFIX=${2:-alphanet}
+ZONE=${3:-us-west2-a}
+
+gcloud beta compute instances update-container ${PREFIX}-0 \
+    --zone ${ZONE} \
+    --container-image ${IMAGE}
+
+gcloud beta compute instances update-container ${PREFIX}-1 \
+    --zone ${ZONE} \
+    --container-image ${IMAGE}
+
+gcloud beta compute instances update-container ${PREFIX}-2 \
+    --zone ${ZONE} \
+    --container-image ${IMAGE}
+
+gcloud beta compute instances update-container ${PREFIX}-3 \
+    --zone ${ZONE} \
+    --container-image ${IMAGE}
