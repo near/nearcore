84a8630a06d742f008d410dfe64257879c4ea3e7
Illia Polosukhin <ilblackdragon@gmail.com>
2019-04-04 22:23:59 -0700

    Fix coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,8 +11,11 @@ make install DESTDIR=../../kcov-build
 cd ../..
 rm -rf kcov-master
 
+# Delete binaries
+rm target/debug/nearcore*
+rm target/debug/keystore*
+
 for file in target/debug/*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done
 
-bash <(curl -s https://codecov.io/bash)
+curl -s https://codecov.io/bash | bash
 echo "Uploaded code coverage"
-
