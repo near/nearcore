a491d151d3cbfb156fa76c95096a066b83f52d81
Illia Polosukhin <ilblackdragon@gmail.com>
2019-04-05 22:39:20 -0700

    Instead of building -> check. Split coverage into separate step

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,11 +11,14 @@ make install DESTDIR=../../kcov-build
 cd ../..
 rm -rf kcov-master
 
-# Delete binaries
-rm target/debug/nearcore*
-rm target/debug/keystore*
-
-for file in target/debug/*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done
+for file in target/debug/*
+do
+  if [ -f $file ] && [ -x $file ]; then
+    mkdir -p "target/cov/$(basename $file)"
+    ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$fi
+le"
+  fi
+done
 
 curl -s https://codecov.io/bash | bash
 echo "Uploaded code coverage"
