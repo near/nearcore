4e9f6606b00ddd1e10831bbd6e9030a041d606c6
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2019-04-09 12:28:45 -0700

    Studio version {0.1.6, 0.1.7} -> 0.1.8 (#844)

--- ops/deploy_alphanet.sh
+++ ops/deploy_alphanet.sh
@@ -3,7 +3,7 @@ set -e
 
 IMAGE=${1:-nearprotocol/alphanet:0.1.4}
 PREFIX=${2:-alphanet}
-STUDIO_IMAGE=${3:-nearprotocol/studio:0.1.6}
+STUDIO_IMAGE=${3:-nearprotocol/studio:0.1.8}
 ZONE=${4:-us-west2-a}
 REGION=${5:-us-west2}
 
