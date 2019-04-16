f4e031a79006441c1735f71db3980b93105079e0
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2019-04-16 16:20:42 -0700

    Tee should collect stdout and stderr (#872)

--- ops/deploy_alphanet.sh
+++ ops/deploy_alphanet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-IMAGE=${1:-nearprotocol/alphanet:0.1.5}
-PREFIX=${2:-loadtesting4}
+IMAGE=${1:-nearprotocol/alphanet:0.1.6}
+PREFIX=${2:-loadtesting-4-16-3}
 STUDIO_IMAGE=${3:-nearprotocol/studio:0.1.8}
 ZONE=${4:-us-west2-a}
 REGION=${5:-us-west2}
