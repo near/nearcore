90405992431c8d5c4c44cd2921fdc4b5e8e3b348
Maksym Zavershynskyi <max@near.ai>
2019-04-24 15:51:44 -0700

    Fix local deploy script

--- ops/deploy_local.sh
+++ ops/deploy_local.sh
@@ -2,30 +2,30 @@
 set -e
 
 IMAGE=${1:-nearprotocol/nearcore:0.1.1}
-STUDIO_IMAGE=${2:-nearprotocol/studio:0.1.8}
+STUDIO_IMAGE=${2:-nearprotocol/studio:0.2.4}
 TOTAL_NODES=2
 
-sudo docker run -d --name testnet-0 -p 3030:3030 -p 26656:26656 \
+sudo docker run -d --name testnet-0 -p 3030:3030 -p 26656:26656 --rm \
 	-e "BOOT_NODES=127.0.0.1:26658" \
 	-e "NODE_ID=0" \
 	-e "TOTAL_NODES=${TOTAL_NODES}" \
 	-e "NODE_KEY=53Mr7IhcJXu3019FX+Ra+VbxSQ5y2q+pknmM463jzoFzldWZb16dSYRxrhYrLRXe/UA0wR2zFy4c3fY5yDHjlA==" \
 	${IMAGE}
 
-sudo docker run -d --name testnet-1 -p 3031:3030 -p 26670:26656 \
+sudo docker run -d --name testnet-1 -p 3031:3030 -p 26670:26656 --add-host=testnet-0:172.17.0.2 --rm \
 	-e "BOOT_NODES=6f99d7b49a10fff319cd8bbbd13c3a964dcd0248@172.17.0.2:26656" \
 	-e "NODE_ID=1" \
 	-e "TOTAL_NODES=${TOTAL_NODES}" \
 	${IMAGE}
 
-sudo docker run -d --name studio -p 80:80 \
+sudo docker run -d --name studio -p 80:80 --add-host=testnet-0:172.17.0.2 --rm \
     -e "DEVNET_HOST=http://172.17.0.2" \
-    -e "NEARLIB_COMMIT=\"348509b526cf4ca0495d86cb211d1013d84629a2\"" \
-    -e "NEARLIB_VERSION=\"0.5.2\"" \
+    -e "NEARLIB_COMMIT=348509b526cf4ca0495d86cb211d1013d84629a2" \
+    -e "NEARLIB_VERSION=0.5.2" \
     -e "EXTERNAL_HOST_NAME=http://localhost" \
     ${STUDIO_IMAGE}
 
-STUDIO_IP=127.0.0.1
+STUDIO_IP=localhost
 
 spinner()
 {
