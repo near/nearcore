282925a59aaa1c733f97919da38246bf8a29ca98
janedegtiareva <jane.degtiareva@gmail.com>
2019-04-30 11:39:33 -0700

    [nearlib] update index.js to include account info (#917)
    * [nearlib] update index.js to include account info
    
    * [nearlib] fix linter warning in index.js

--- nearlib/index.js
+++ nearlib/index.js
@@ -9,7 +9,8 @@ const KeyPair = require('./signing/key_pair');
 const WalletAccount = require('./wallet-account');
 const UnencryptedFileSystemKeyStore = require('./signing/unencrypted_file_system_keystore');
 const dev = require('./dev');
+const AccountInfo = require('./signing/account_info');
 
-module.exports = { Near, NearClient, Account, SimpleKeyStoreSigner, InMemoryKeyStore, BrowserLocalStorageKeystore, LocalNodeConnection, KeyPair, WalletAccount, UnencryptedFileSystemKeyStore, dev };
+module.exports = { Near, NearClient, Account, SimpleKeyStoreSigner, InMemoryKeyStore, BrowserLocalStorageKeystore, LocalNodeConnection, KeyPair, WalletAccount, UnencryptedFileSystemKeyStore, dev, AccountInfo };
 
 
