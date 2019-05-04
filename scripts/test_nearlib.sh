bd5222dd4ad8921751d1c4c8d8a898bf2db554e9
Vladimir Grichina <vgrichina@gmail.com>
2019-05-04 01:22:16 -0700

    Simplify near-shell testing

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -17,16 +17,10 @@ npm run doc
 cd ..
 
 # Try creating and building new project using NEAR CLI tools
-rm -rf new_project
-mkdir new_project
-cd new_project
-npm install git+https://git@github.com/nearprotocol/near-shell.git
-$(npm bin)/near new_project
-# Disabled running create_account / test, as it's currently deploys to general devnet instead of local.
-# $(npm bin)/near create_account --account_id=near-hello-devnet
+git clone https://git@github.com/nearprotocol/near-shell.git near-shell
+cd near-shell
 npm install
-npm run build
-# npm test
+npm test
 cd ..
 
 ./scripts/kill_devnet.sh
