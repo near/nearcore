e35cf2ed818a380164e1e8c4f6664385d741ac83
janedegtiareva <jane.degtiareva@gmail.com>
2019-05-08 14:04:37 -0700

    Pull steps to start tendermint node into a shell script for easier lo… (#932)
    * Pull steps to start tendermint node into a shell script for easier local dev.
    
    * Tweaks to nearlib ci scripting
    
    * Increase timeout for waitonserver.sh used in integration tests
    
    * Increase timeout waiting on nearmint to start for nearlib integration tests
    
    * Increase timeout on waiting for nearmint to start
    
    * Start nearmint script: rename the script to be more consistent with conventions

--- scripts/waitonserver.sh
+++ scripts/waitonserver.sh
@@ -2,7 +2,7 @@
 
 echo 'waiting on healthcheck' >&2
 
-for _ in {1..6}; do
+for _ in {1..100}; do
     if [[ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:3030/status)" == "200" ]]; then
         exit 0
     fi
