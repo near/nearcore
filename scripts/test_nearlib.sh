00f6fa653cfe3bf52d0c140fecc5d968e0a58277
Vladimir Grichina <vgrichina@gmail.com>
2019-06-11 15:48:23 -0700

    Fix nearlib branch selection

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -7,8 +7,7 @@ set -ex
 
 # Run nearlib tests
 rm -rf nearlib
-git clone https://github.com/nearprotocol/nearlib.git nearlib
-git checkout origin/nightshade
+git clone --single-branch --branch nightshade https://github.com/nearprotocol/nearlib.git nearlib
 export NEAR_PROTOS_DIR="../nearcore/core/protos/protos"
 export HELLO_WASM_PATH="../nearcore/tests/hello.wasm"
 npm install
