a3da574128dd07f34edc9e40b139426f98e6bd10
Vladimir Grichina <vgrichina@gmail.com>
2019-06-12 17:33:42 -0700

    Use yarn for faster installs

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -14,15 +14,15 @@ git clone --single-branch --branch nightshade https://github.com/nearprotocol/ne
 cd nearlib
 export NEAR_PROTOS_DIR="../core/protos/protos"
 export HELLO_WASM_PATH="../tests/hello.wasm"
-npm install
-npm run build
-npm test
-npm run doc
+yarn
+yarn build
+yarn test
+yarn doc
 cd ..
 
 # Try creating and building new project using NEAR CLI tools
 git clone --single-branch --branch nightshade https://git@github.com/nearprotocol/near-shell.git near-shell
 cd near-shell
-npm install
-#npm test
+yarn
+#yarn test
 cd ..
