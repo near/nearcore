a0d6bb0b47f766f4d19e27007a5ad63ddc4363b0
Bowen Wang <bowenwang1996@uchicago.edu>
2019-08-13 10:17:31 -0700

    use heavy_test

--- near/tests/sync_nodes.rs
+++ near/tests/sync_nodes.rs
@@ -16,7 +16,7 @@ use near_network::test_utils::{convert_boot_nodes, open_port, WaitOrTimeout};
 use near_network::{NetworkClientMessages, PeerInfo};
 use near_primitives::crypto::signer::InMemorySigner;
 use near_primitives::serialize::BaseEncode;
-use near_primitives::test_utils::{init_integration_logger, init_test_logger};
+use near_primitives::test_utils::{heavy_test, init_integration_logger, init_test_logger};
 use near_primitives::transaction::{StakeTransaction, TransactionBody};
 use near_primitives::types::{BlockIndex, EpochId};
 use near_store::test_utils::create_test_store;
@@ -85,192 +85,198 @@ fn add_blocks(
 /// One client is in front, another must sync to it before they can produce blocks.
 #[test]
 fn sync_nodes() {
-    init_test_logger();
+    heavy_test(|| {
+        init_test_logger();
 
-    let mut genesis_config = GenesisConfig::test(vec!["other"]);
-    genesis_config.epoch_length = 5;
-    let genesis_block = genesis_block(genesis_config.clone());
+        let mut genesis_config = GenesisConfig::test(vec!["other"]);
+        genesis_config.epoch_length = 5;
+        let genesis_block = genesis_block(genesis_config.clone());
 
-    let (port1, port2) = (open_port(), open_port());
-    let mut near1 = load_test_config("test1", port1, &genesis_config);
-    near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
-    near1.client_config.min_num_peers = 1;
-    let mut near2 = load_test_config("test2", port2, &genesis_config);
-    near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
-    near2.client_config.min_num_peers = 1;
+        let (port1, port2) = (open_port(), open_port());
+        let mut near1 = load_test_config("test1", port1, &genesis_config);
+        near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
+        near1.client_config.min_num_peers = 1;
+        let mut near2 = load_test_config("test2", port2, &genesis_config);
+        near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
+        near2.client_config.min_num_peers = 1;
 
-    let system = System::new("NEAR");
+        let system = System::new("NEAR");
 
-    let dir1 = TempDir::new("sync_nodes_1").unwrap();
-    let (client1, _) = start_with_config(dir1.path(), near1);
+        let dir1 = TempDir::new("sync_nodes_1").unwrap();
+        let (client1, _) = start_with_config(dir1.path(), near1);
 
-    let signer = Arc::new(InMemorySigner::from_seed("other", "other"));
-    let _ = add_blocks(vec![genesis_block], client1, 12, genesis_config.epoch_length, signer);
+        let signer = Arc::new(InMemorySigner::from_seed("other", "other"));
+        let _ = add_blocks(vec![genesis_block], client1, 12, genesis_config.epoch_length, signer);
 
-    let dir2 = TempDir::new("sync_nodes_2").unwrap();
-    let (_, view_client2) = start_with_config(dir2.path(), near2);
+        let dir2 = TempDir::new("sync_nodes_2").unwrap();
+        let (_, view_client2) = start_with_config(dir2.path(), near2);
 
-    WaitOrTimeout::new(
-        Box::new(move |_ctx| {
-            actix::spawn(view_client2.send(GetBlock::Best).then(|res| {
-                match &res {
-                    Ok(Ok(b)) if b.header.height == 12 => System::current().stop(),
-                    Err(_) => return futures::future::err(()),
-                    _ => {}
-                };
-                futures::future::ok(())
-            }));
-        }),
-        100,
-        60000,
-    )
-    .start();
+        WaitOrTimeout::new(
+            Box::new(move |_ctx| {
+                actix::spawn(view_client2.send(GetBlock::Best).then(|res| {
+                    match &res {
+                        Ok(Ok(b)) if b.header.height == 12 => System::current().stop(),
+                        Err(_) => return futures::future::err(()),
+                        _ => {}
+                    };
+                    futures::future::ok(())
+                }));
+            }),
+            100,
+            60000,
+        )
+        .start();
 
-    system.run().unwrap();
+        system.run().unwrap();
+    });
 }
 
 /// Clients connect and then one of them becomes in front. The other one must then sync to it.
 #[test]
 fn sync_after_sync_nodes() {
-    init_test_logger();
+    heavy_test(|| {
+        init_test_logger();
 
-    let mut genesis_config = GenesisConfig::test(vec!["other"]);
-    genesis_config.epoch_length = 5;
-    let genesis_block = genesis_block(genesis_config.clone());
+        let mut genesis_config = GenesisConfig::test(vec!["other"]);
+        genesis_config.epoch_length = 5;
+        let genesis_block = genesis_block(genesis_config.clone());
 
-    let (port1, port2) = (open_port(), open_port());
-    let mut near1 = load_test_config("test1", port1, &genesis_config);
-    near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
-    near1.client_config.min_num_peers = 1;
-    let mut near2 = load_test_config("test2", port2, &genesis_config);
-    near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
-    near2.client_config.min_num_peers = 1;
+        let (port1, port2) = (open_port(), open_port());
+        let mut near1 = load_test_config("test1", port1, &genesis_config);
+        near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
+        near1.client_config.min_num_peers = 1;
+        let mut near2 = load_test_config("test2", port2, &genesis_config);
+        near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
+        near2.client_config.min_num_peers = 1;
 
-    let system = System::new("NEAR");
+        let system = System::new("NEAR");
 
-    let dir1 = TempDir::new("sync_nodes_1").unwrap();
-    let (client1, _) = start_with_config(dir1.path(), near1);
+        let dir1 = TempDir::new("sync_nodes_1").unwrap();
+        let (client1, _) = start_with_config(dir1.path(), near1);
 
-    let dir2 = TempDir::new("sync_nodes_2").unwrap();
-    let (_, view_client2) = start_with_config(dir2.path(), near2);
+        let dir2 = TempDir::new("sync_nodes_2").unwrap();
+        let (_, view_client2) = start_with_config(dir2.path(), near2);
 
-    let signer = Arc::new(InMemorySigner::from_seed("other", "other"));
-    let blocks = add_blocks(
-        vec![genesis_block],
-        client1.clone(),
-        12,
-        genesis_config.epoch_length,
-        signer.clone(),
-    );
+        let signer = Arc::new(InMemorySigner::from_seed("other", "other"));
+        let blocks = add_blocks(
+            vec![genesis_block],
+            client1.clone(),
+            12,
+            genesis_config.epoch_length,
+            signer.clone(),
+        );
 
-    let next_step = Arc::new(AtomicBool::new(false));
-    let epoch_length = genesis_config.epoch_length;
-    WaitOrTimeout::new(
-        Box::new(move |_ctx| {
-            let blocks1 = blocks.clone();
-            let client11 = client1.clone();
-            let signer1 = signer.clone();
-            let next_step1 = next_step.clone();
-            actix::spawn(view_client2.send(GetBlock::Best).then(move |res| {
-                match &res {
-                    Ok(Ok(b)) if b.header.height == 12 => {
-                        if !next_step1.load(Ordering::Relaxed) {
-                            let _ = add_blocks(blocks1, client11, 11, epoch_length, signer1);
-                            next_step1.store(true, Ordering::Relaxed);
+        let next_step = Arc::new(AtomicBool::new(false));
+        let epoch_length = genesis_config.epoch_length;
+        WaitOrTimeout::new(
+            Box::new(move |_ctx| {
+                let blocks1 = blocks.clone();
+                let client11 = client1.clone();
+                let signer1 = signer.clone();
+                let next_step1 = next_step.clone();
+                actix::spawn(view_client2.send(GetBlock::Best).then(move |res| {
+                    match &res {
+                        Ok(Ok(b)) if b.header.height == 12 => {
+                            if !next_step1.load(Ordering::Relaxed) {
+                                let _ = add_blocks(blocks1, client11, 11, epoch_length, signer1);
+                                next_step1.store(true, Ordering::Relaxed);
+                            }
                         }
-                    }
-                    Ok(Ok(b)) if b.header.height > 20 => System::current().stop(),
-                    Err(_) => return futures::future::err(()),
-                    _ => {}
-                };
-                futures::future::ok(())
-            }));
-        }),
-        100,
-        60000,
-    )
-    .start();
+                        Ok(Ok(b)) if b.header.height > 20 => System::current().stop(),
+                        Err(_) => return futures::future::err(()),
+                        _ => {}
+                    };
+                    futures::future::ok(())
+                }));
+            }),
+            100,
+            60000,
+        )
+        .start();
 
-    system.run().unwrap();
+        system.run().unwrap();
+    });
 }
 
 /// Starts one validation node, it reduces it's stake to 1/2 of the stake.
 /// Second node starts after 1s, needs to catchup & state sync and then make sure it's
 #[test]
 fn sync_state_stake_change() {
-    init_integration_logger();
+    heavy_test(|| {
+        init_integration_logger();
 
-    let mut genesis_config = GenesisConfig::test(vec!["test1"]);
-    genesis_config.epoch_length = 5;
+        let mut genesis_config = GenesisConfig::test(vec!["test1"]);
+        genesis_config.epoch_length = 5;
 
-    let (port1, port2) = (open_port(), open_port());
-    let mut near1 = load_test_config("test1", port1, &genesis_config);
-    near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
-    near1.client_config.min_num_peers = 0;
-    near1.client_config.min_block_production_delay = Duration::from_millis(200);
-    let mut near2 = load_test_config("test2", port2, &genesis_config);
-    near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
-    near2.client_config.min_block_production_delay = Duration::from_millis(200);
-    near2.client_config.min_num_peers = 1;
-    near2.client_config.skip_sync_wait = false;
+        let (port1, port2) = (open_port(), open_port());
+        let mut near1 = load_test_config("test1", port1, &genesis_config);
+        near1.network_config.boot_nodes = convert_boot_nodes(vec![("test2", port2)]);
+        near1.client_config.min_num_peers = 0;
+        near1.client_config.min_block_production_delay = Duration::from_millis(200);
+        let mut near2 = load_test_config("test2", port2, &genesis_config);
+        near2.network_config.boot_nodes = convert_boot_nodes(vec![("test1", port1)]);
+        near2.client_config.min_block_production_delay = Duration::from_millis(200);
+        near2.client_config.min_num_peers = 1;
+        near2.client_config.skip_sync_wait = false;
 
-    let system = System::new("NEAR");
+        let system = System::new("NEAR");
 
-    let unstake_transaction = TransactionBody::Stake(StakeTransaction {
-        nonce: 1,
-        originator: "test1".to_string(),
-        amount: TESTING_INIT_STAKE / 2,
-        public_key: near1.block_producer.as_ref().unwrap().signer.public_key().to_base(),
-    })
-    .sign(&*near1.block_producer.as_ref().unwrap().signer);
+        let unstake_transaction = TransactionBody::Stake(StakeTransaction {
+            nonce: 1,
+            originator: "test1".to_string(),
+            amount: TESTING_INIT_STAKE / 2,
+            public_key: near1.block_producer.as_ref().unwrap().signer.public_key().to_base(),
+        })
+        .sign(&*near1.block_producer.as_ref().unwrap().signer);
 
-    let dir1 = TempDir::new("sync_state_stake_change_1").unwrap();
-    let dir2 = TempDir::new("sync_state_stake_change_2").unwrap();
-    let (client1, view_client1) = start_with_config(dir1.path(), near1);
+        let dir1 = TempDir::new("sync_state_stake_change_1").unwrap();
+        let dir2 = TempDir::new("sync_state_stake_change_2").unwrap();
+        let (client1, view_client1) = start_with_config(dir1.path(), near1);
 
-    actix::spawn(
-        client1
-            .send(NetworkClientMessages::Transaction(unstake_transaction))
-            .map(|_| ())
-            .map_err(|_| ()),
-    );
+        actix::spawn(
+            client1
+                .send(NetworkClientMessages::Transaction(unstake_transaction))
+                .map(|_| ())
+                .map_err(|_| ()),
+        );
 
-    let started = Arc::new(AtomicBool::new(false));
-    let dir2_path = dir2.path().to_path_buf();
-    WaitOrTimeout::new(
-        Box::new(move |_ctx| {
-            let started_copy = started.clone();
-            let near2_copy = near2.clone();
-            let dir2_path_copy = dir2_path.clone();
-            actix::spawn(view_client1.send(GetBlock::Best).then(move |res| {
-                let latest_block_height = res.unwrap().unwrap().header.height;
-                if !started_copy.load(Ordering::SeqCst) && latest_block_height > 10 {
-                    started_copy.store(true, Ordering::SeqCst);
-                    let (_, view_client2) = start_with_config(&dir2_path_copy, near2_copy);
+        let started = Arc::new(AtomicBool::new(false));
+        let dir2_path = dir2.path().to_path_buf();
+        WaitOrTimeout::new(
+            Box::new(move |_ctx| {
+                let started_copy = started.clone();
+                let near2_copy = near2.clone();
+                let dir2_path_copy = dir2_path.clone();
+                actix::spawn(view_client1.send(GetBlock::Best).then(move |res| {
+                    let latest_block_height = res.unwrap().unwrap().header.height;
+                    if !started_copy.load(Ordering::SeqCst) && latest_block_height > 10 {
+                        started_copy.store(true, Ordering::SeqCst);
+                        let (_, view_client2) = start_with_config(&dir2_path_copy, near2_copy);
 
-                    WaitOrTimeout::new(
-                        Box::new(move |_ctx| {
-                            actix::spawn(view_client2.send(GetBlock::Best).then(move |res| {
-                                if let Ok(block) = res.unwrap() {
-                                    if block.header.height > latest_block_height + 1 {
-                                        System::current().stop()
+                        WaitOrTimeout::new(
+                            Box::new(move |_ctx| {
+                                actix::spawn(view_client2.send(GetBlock::Best).then(move |res| {
+                                    if let Ok(block) = res.unwrap() {
+                                        if block.header.height > latest_block_height + 1 {
+                                            System::current().stop()
+                                        }
                                     }
-                                }
-                                future::result::<_, ()>(Ok(()))
-                            }));
-                        }),
-                        100,
-                        30000,
-                    )
-                    .start();
-                }
-                future::result(Ok(()))
-            }));
-        }),
-        100,
-        35000,
-    )
-    .start();
+                                    future::result::<_, ()>(Ok(()))
+                                }));
+                            }),
+                            100,
+                            30000,
+                        )
+                        .start();
+                    }
+                    future::result(Ok(()))
+                }));
+            }),
+            100,
+            35000,
+        )
+        .start();
 
-    system.run().unwrap();
+        system.run().unwrap();
+    });
 }
--- near/tests/sync_state_nodes.rs
+++ near/tests/sync_state_nodes.rs
@@ -7,78 +7,82 @@ use tempdir::TempDir;
 use near::{load_test_config, start_with_config, GenesisConfig};
 use near_client::GetBlock;
 use near_network::test_utils::{convert_boot_nodes, open_port, WaitOrTimeout};
-use near_primitives::test_utils::init_test_logger;
+use near_primitives::test_utils::{heavy_test, init_test_logger};
 
 /// One client is in front, another must sync to it using state (fast) sync.
 #[test]
 fn sync_state_nodes() {
-    init_test_logger();
+    heavy_test(|| {
+        init_test_logger();
 
-    let genesis_config = GenesisConfig::test(vec!["test1"]);
+        let genesis_config = GenesisConfig::test(vec!["test1"]);
 
-    let (port1, port2) = (open_port(), open_port());
-    let mut near1 = load_test_config("test1", port1, &genesis_config);
-    near1.network_config.boot_nodes = convert_boot_nodes(vec![]);
-    near1.client_config.min_num_peers = 0;
-    let system = System::new("NEAR");
+        let (port1, port2) = (open_port(), open_port());
+        let mut near1 = load_test_config("test1", port1, &genesis_config);
+        near1.network_config.boot_nodes = convert_boot_nodes(vec![]);
+        near1.client_config.min_num_peers = 0;
+        let system = System::new("NEAR");
 
-    let dir1 = TempDir::new("sync_nodes_1").unwrap();
-    let (_, view_client1) = start_with_config(dir1.path(), near1);
+        let dir1 = TempDir::new("sync_nodes_1").unwrap();
+        let (_, view_client1) = start_with_config(dir1.path(), near1);
 
-    let view_client2_holder = Arc::new(RwLock::new(None));
+        let view_client2_holder = Arc::new(RwLock::new(None));
 
-    WaitOrTimeout::new(
-        Box::new(move |_ctx| {
-            if view_client2_holder.read().unwrap().is_none() {
-                let view_client2_holder2 = view_client2_holder.clone();
-                let genesis_config2 = genesis_config.clone();
+        WaitOrTimeout::new(
+            Box::new(move |_ctx| {
+                if view_client2_holder.read().unwrap().is_none() {
+                    let view_client2_holder2 = view_client2_holder.clone();
+                    let genesis_config2 = genesis_config.clone();
 
-                actix::spawn(view_client1.send(GetBlock::Best).then(move |res| {
-                    match &res {
-                        Ok(Ok(b)) if b.header.height >= 101 => {
-                            let mut view_client2_holder2 = view_client2_holder2.write().unwrap();
+                    actix::spawn(view_client1.send(GetBlock::Best).then(move |res| {
+                        match &res {
+                            Ok(Ok(b)) if b.header.height >= 101 => {
+                                let mut view_client2_holder2 =
+                                    view_client2_holder2.write().unwrap();
 
-                            if view_client2_holder2.is_none() {
-                                let mut near2 = load_test_config("test2", port2, &genesis_config2);
-                                near2.client_config.skip_sync_wait = false;
-                                near2.client_config.min_num_peers = 1;
-                                near2.network_config.boot_nodes =
-                                    convert_boot_nodes(vec![("test1", port1)]);
+                                if view_client2_holder2.is_none() {
+                                    let mut near2 =
+                                        load_test_config("test2", port2, &genesis_config2);
+                                    near2.client_config.skip_sync_wait = false;
+                                    near2.client_config.min_num_peers = 1;
+                                    near2.network_config.boot_nodes =
+                                        convert_boot_nodes(vec![("test1", port1)]);
 
-                                let dir2 = TempDir::new("sync_nodes_2").unwrap();
-                                let (_, view_client2) = start_with_config(dir2.path(), near2);
-                                *view_client2_holder2 = Some(view_client2);
+                                    let dir2 = TempDir::new("sync_nodes_2").unwrap();
+                                    let (_, view_client2) = start_with_config(dir2.path(), near2);
+                                    *view_client2_holder2 = Some(view_client2);
+                                }
                             }
-                        }
-                        Ok(Ok(b)) if b.header.height < 101 => {
-                            println!("FIRST STAGE {}", b.header.height)
-                        }
-                        Err(_) => return futures::future::err(()),
-                        _ => {}
-                    };
-                    futures::future::ok(())
-                }));
-            }
+                            Ok(Ok(b)) if b.header.height < 101 => {
+                                println!("FIRST STAGE {}", b.header.height)
+                            }
+                            Err(_) => return futures::future::err(()),
+                            _ => {}
+                        };
+                        futures::future::ok(())
+                    }));
+                }
 
-            if let Some(view_client2) = &*view_client2_holder.write().unwrap() {
-                actix::spawn(view_client2.send(GetBlock::Best).then(|res| {
-                    match &res {
-                        Ok(Ok(b)) if b.header.height >= 101 => System::current().stop(),
-                        Ok(Ok(b)) if b.header.height < 101 => {
-                            println!("SECOND STAGE {}", b.header.height)
-                        }
-                        Err(_) => return futures::future::err(()),
-                        _ => {}
-                    };
-                    futures::future::ok(())
-                }));
-            } else {
-            }
-        }),
-        100,
-        60000,
-    )
-    .start();
+                if let Some(view_client2) = &*view_client2_holder.write().unwrap() {
+                    actix::spawn(view_client2.send(GetBlock::Best).then(|res| {
+                        match &res {
+                            Ok(Ok(b)) if b.header.height >= 101 => System::current().stop(),
+                            Ok(Ok(b)) if b.header.height < 101 => {
+                                println!("SECOND STAGE {}", b.header.height)
+                            }
+                            Err(_) => return futures::future::err(()),
+                            _ => {}
+                        };
+                        futures::future::ok(())
+                    }));
+                } else {
+                }
+            }),
+            100,
+            60000,
+        )
+        .start();
 
-    system.run().unwrap();
+        system.run().unwrap();
+    });
 }
