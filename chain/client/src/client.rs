2b0d8e8a844f9d16a84e7b25c4749dffc2c8faf5
Bowen Wang <bowenwang1996@users.noreply.github.com>
2019-08-25 16:11:29 -0700

    Fix incorrect epoch hash computation (#1214)
    * Fix incorrect epoch hash computation
    
    * fix another bug
    
    * bump version to 0.2.8
    
    * use bs58 properly

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -41,7 +41,7 @@ use crate::types::{
     BlockProducer, ClientConfig, Error, ShardSyncStatus, Status, StatusSyncInfo, SyncStatus,
 };
 use crate::{sync, StatusResponse};
-use std::cmp::max;
+use std::cmp::min;
 
 pub struct ClientActor {
     config: ClientConfig,
@@ -629,7 +629,12 @@ impl ClientActor {
             return Ok(());
         }
         // Check that we are were called at the block that we are producer for.
-        let next_block_proposer = self.get_block_proposer(&head.epoch_hash, next_height)?;
+        let (epoch_hash, _) = self
+            .runtime_adapter
+            .get_epoch_offset(head.last_block_hash, next_height)
+            .map_err(|e| Error::from(ErrorKind::Other(e.to_string())))?;
+
+        let next_block_proposer = self.get_block_proposer(&epoch_hash, next_height)?;
         if block_producer.account_id != next_block_proposer {
             info!(target: "client", "Produce block: chain at {}, not block producer for next block.", next_height);
             return Ok(());
@@ -651,7 +656,7 @@ impl ClientActor {
             == block_producer.account_id.clone();
         // If epoch changed, and before there was 2 validators and now there is 1 - prev_same_bp is false, but total validators right now is 1.
         let total_approvals =
-            total_validators - max(if prev_same_bp { 1 } else { 2 }, total_validators);
+            total_validators - min(if prev_same_bp { 1 } else { 2 }, total_validators);
         if self.approvals.len() < total_approvals
             && self.last_block_processed.elapsed() < self.config.max_block_production_delay
         {
