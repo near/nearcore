b02499948d85ae14ea27e95c2dc1c34e677eb309
Bowen Wang <bowenwang1996@uchicago.edu>
2019-08-29 19:41:20 -0700

    change wasm path

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -12,7 +12,7 @@ rm -rf nearlib
 git clone --single-branch --branch staging https://github.com/nearprotocol/nearlib.git nearlib
 cd nearlib
 export NEAR_PROTOS_DIR="../core/protos/protos"
-export HELLO_WASM_PATH="../runtime/near-vm-runner/tests/res/test_contract_rs.wasm"
+export HELLO_WASM_PATH="../tests/hello.wasm"
 yarn
 yarn build
 ../scripts/waitonserver.sh
