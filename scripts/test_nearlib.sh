da98dba95be3bc79153e532411f074a831d0b8b5
Vladimir Grichina <vgrichina@gmail.com>
2019-10-01 01:46:10 -0700

    Remove unneeded env overrides from test_nearlib.sh

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -11,8 +11,6 @@ trap 'pkill -15 -P $NEAR_PID' 0
 rm -rf nearlib
 git clone --single-branch --branch staging https://github.com/nearprotocol/nearlib.git nearlib
 cd nearlib
-export NEAR_PROTOS_DIR="../core/protos/protos"
-export HELLO_WASM_PATH="../tests/hello.wasm"
 yarn
 yarn build
 ../scripts/waitonserver.sh
