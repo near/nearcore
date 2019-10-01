1e7d087b82d9c1e4085348ff419a0fbc9f81b1a6
Evgeny Kuzyakov <ek@nearprotocol.com>
2019-10-01 13:46:28 -0700

    Keep near-shell staging until it's fixed

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -21,7 +21,7 @@ yarn doc
 cd ..
 
 # Try creating and building new project using NEAR CLI tools
-git clone --single-branch https://git@github.com/nearprotocol/near-shell.git near-shell
+git clone --single-branch --branch staging https://git@github.com/nearprotocol/near-shell.git near-shell
 cd near-shell
 yarn
 #yarn test
