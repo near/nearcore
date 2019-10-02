6316be31ddcfe34a83a15fff6333667c8563f637
Vladimir Grichina <vgrichina@gmail.com>
2019-10-01 17:20:20 -0700

    Remove hello.wasm source (use hello-wasm from npm instead) (#1389)
    * Remove hello.wasm source (use hello-wasm from npm instead)
    
    * Remove unneeded env overrides from test_nearlib.sh
    
    * Use `master` nearlib and near-shell
    
    * Try kicking CI
    
    * Try parallelizing nearlib tests with cargo tests
    
    * Remove legacy stuff from GitLab config
    
    * Try installing Node using n-install
    
    * Fix typo in n-install command
    
    * Change the way Node is installed

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -9,10 +9,8 @@ trap 'pkill -15 -P $NEAR_PID' 0
 
 # Run nearlib tests
 rm -rf nearlib
-git clone --single-branch --branch staging https://github.com/nearprotocol/nearlib.git nearlib
+git clone --single-branch --branch master https://github.com/nearprotocol/nearlib.git nearlib
 cd nearlib
-export NEAR_PROTOS_DIR="../core/protos/protos"
-export HELLO_WASM_PATH="../tests/hello.wasm"
 yarn
 yarn build
 ../scripts/waitonserver.sh
@@ -21,7 +19,7 @@ yarn doc
 cd ..
 
 # Try creating and building new project using NEAR CLI tools
-git clone --single-branch --branch staging https://git@github.com/nearprotocol/near-shell.git near-shell
+git clone --single-branch --branch master https://git@github.com/nearprotocol/near-shell.git near-shell
 cd near-shell
 yarn
 #yarn test
