789e0e8479d66df0fdc6e9e6f3e79c1a18b7de48
Evgeny Kuzyakov <ek@nearprotocol.com>
2019-10-07 14:40:01 -0700

    Add panic message to GuestPanic

--- runtime/near-vm-runner/tests/test-contract-rs/build.sh
+++ runtime/near-vm-runner/tests/test-contract-rs/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+pushd $( dirname "${BASH_SOURCE[0]}" )
+
 RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/test_contract_rs.wasm ../res/
 rm -rf target
+
+popd
