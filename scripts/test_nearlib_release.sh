cc5da8ad7761cdd303009a58b436a20904e7120e
Bo Yao <bo@nearprotocol.com>
2019-10-08 14:31:57 -0700

    correct pathname for some tests

--- scripts/test_nearlib_release.sh
+++ scripts/test_nearlib_release.sh
@@ -8,25 +8,24 @@ trap 'pkill -15 -P $NEAR_PID' 0
 #./scripts/build_wasm.sh
 
 # Run nearlib tests
-rm -rf nearlib_release_test
+rm -rf nearlib_release_test near-shell nearlib
 mkdir nearlib_release_test
 cd nearlib_release_test
 
-yarn add nearlib
-rm package.json yarn.lock
-mv node_modules/nearlib .
-cd nearlib
+yarn add nearlib near-shell
+
+mv node_modules/nearlib ..
+mv node_modules/near-shell ..
+
+cd ../nearlib
+
 yarn
 yarn build
-../../scripts/waitonserver.sh
+../scripts/waitonserver.sh
 yarn test
 yarn doc
-cd ..
 
 # Try creating and building new project using NEAR CLI tools
-yarn add near-shell
-rm package.json yarn.lock
-mv node_modules/near-shell .
-ce near-shell
+cd ../near-shell
 yarn
-cd ../..
+cd ..
