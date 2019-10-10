bade57e20036a497be6df74c634aecb2fd2fa19c
Bo Yao <bo@nearprotocol.com>
2019-10-10 11:41:54 -0700

    fix with new nearlib version and refactor

--- scripts/test_nearlib.sh
+++ scripts/test_nearlib.sh
@@ -7,9 +7,32 @@ trap 'pkill -15 -P $NEAR_PID' 0
 
 #./scripts/build_wasm.sh
 
+function get_nearlib_nearshell_release () {
+    rm -rf nearlib_release_test near-shell nearlib
+    mkdir nearlib_release_test
+    cd nearlib_release_test
+
+    yarn add nearlib near-shell
+
+    mv node_modules/nearlib ..
+    mv node_modules/near-shell ..
+    cd ..
+}
+
+function get_nearlib_nearshell_git () {
+    rm -rf nearlib
+    git clone --single-branch --branch master https://github.com/nearprotocol/nearlib.git nearlib
+    rm -rf near-shell
+    git clone --single-branch --branch master https://git@github.com/nearprotocol/near-shell.git near-shell
+}
+
+if [ -z "${NEARLIB_RELEASE}" ]; then
+    get_nearlib_nearshell_git
+else
+    get_nearlib_nearshell_release
+fi
+
 # Run nearlib tests
-rm -rf nearlib
-git clone --single-branch --branch master https://github.com/nearprotocol/nearlib.git nearlib
 cd nearlib
 yarn
 yarn build
@@ -19,7 +42,6 @@ yarn doc
 cd ..
 
 # Try creating and building new project using NEAR CLI tools
-git clone --single-branch --branch master https://git@github.com/nearprotocol/near-shell.git near-shell
 cd near-shell
 yarn
 #yarn test
