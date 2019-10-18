ec4f9d9145eadb63738a3d7a343f89c72a16a7de
Alexey <alexey@fckt.dev>
2019-10-18 18:53:57 +0300

    Registers as input values for externs (#1476)
    * initial read from registers API
    
    * read_memory costs
    
    * get_utf8_string and get_utf16_string don't read from registers
    
    * Test registers API + fixes
    
    * fix tests
    
    * fix testnet.json

--- runtime/near-vm-logic/tests/test_iterators.rs
+++ runtime/near-vm-logic/tests/test_iterators.rs
@@ -15,10 +15,15 @@ fn add_key_vals(logic: &mut VMLogic, key_vals: &[KeyVal]) {
     }
 }
 
-fn iter_prefix_check(logic: &mut VMLogic, prefix: &[u8], key_vals: &[KeyVal]) -> u64 {
-    let iter_id = logic
-        .storage_iter_prefix(prefix.len() as _, prefix.as_ptr() as _)
-        .expect("create iterator ok");
+fn iter_prefix_check(logic: &mut VMLogic, prefix: &[u8], key_vals: &[KeyVal], use_register: bool) -> u64 {
+    let iter_id = if use_register {
+        logic.write_register(3, prefix).unwrap();
+        logic.storage_iter_prefix(std::u64::MAX, 3)
+    } else {
+        logic
+            .storage_iter_prefix(prefix.len() as _, prefix.as_ptr() as _)
+    }.expect("create iterator ok");
+
     for KeyVal(key, val) in key_vals {
         assert_eq!(
             logic.storage_iter_next(iter_id, 0, 1).unwrap(),
@@ -37,15 +42,20 @@ fn iter_prefix_check(logic: &mut VMLogic, prefix: &[u8], key_vals: &[KeyVal]) ->
     iter_id
 }
 
-fn iter_range_check(logic: &mut VMLogic, start: &[u8], end: &[u8], key_vals: &[KeyVal]) -> u64 {
-    let iter_id = logic
-        .storage_iter_range(
-            start.len() as _,
-            start.as_ptr() as _,
-            end.len() as _,
-            end.as_ptr() as _,
-        )
-        .expect("create iterator ok");
+fn iter_range_check(logic: &mut VMLogic, start: &[u8], end: &[u8], key_vals: &[KeyVal], use_register: bool) -> u64 {
+    let iter_id = if use_register {
+        logic.write_register(3, start).unwrap();
+        logic.write_register(4, end).unwrap();
+        logic.storage_iter_range(std::u64::MAX, 0, std::u64::MAX, 1)
+    } else {
+        logic.storage_iter_range(
+                start.len() as _,
+                start.as_ptr() as _,
+                end.len() as _,
+                end.as_ptr() as _,
+            )
+    }.expect("create iterator ok");
+
     for KeyVal(key, val) in key_vals {
         assert_eq!(
             logic.storage_iter_next(iter_id, 0, 1).unwrap(),
@@ -64,8 +74,7 @@ fn iter_range_check(logic: &mut VMLogic, start: &[u8], end: &[u8], key_vals: &[K
     iter_id
 }
 
-#[test]
-fn test_iterator() {
+fn test_iterator(use_register: bool) {
     let mut ext = MockedExternal::default();
     let context = get_context(vec![], false);
     let config = Config::default();
@@ -78,7 +87,26 @@ fn test_iterator() {
         &mut logic,
         b"foo",
         &[KeyVal(b"foo1", b"bar1"), KeyVal(b"foo2", b"bar2")],
+        use_register
     );
+    // iterator exhausted
+    assert_eq!(logic.storage_iter_next(iter_id, 0, 1).unwrap(), 0);
+}
+
+fn test_iterator_from_second(use_register: bool) {
+    let mut ext = MockedExternal::default();
+    let context = get_context(vec![], false);
+    let config = Config::default();
+    let promise_results = vec![];
+    let mut memory = MockedMemory::default();
+    let mut logic = VMLogic::new(&mut ext, context, &config, &promise_results, &mut memory);
+
+    add_key_vals(
+        &mut logic,
+        &[KeyVal(b"aaa", b"bar1"), KeyVal(b"aaa", b"bar2"), KeyVal(b"aaaa", b"bar3")],
+    );
+    let iter_id =
+        iter_prefix_check(&mut logic, b"aaa", &[KeyVal(b"aaa", b"bar2"), KeyVal(b"aaaa", b"bar3")], use_register);
     // iterator exhausted
     assert_eq!(logic.storage_iter_next(iter_id, 0, 1).unwrap(), 0);
 }
@@ -95,25 +123,6 @@ fn test_iterator_invalidation() {
     let iter_id = logic.storage_iter_prefix(1, b"a".as_ptr() as _).unwrap();
     add_key_vals(&mut logic, &[KeyVal(b"f3", b"a")]);
     logic.storage_iter_next(iter_id, 0, 1).expect_err("storage has changed, iterator is invalid");
-}
-
-#[test]
-fn test_iterator_from_second() {
-    let mut ext = MockedExternal::default();
-    let context = get_context(vec![], false);
-    let config = Config::default();
-    let promise_results = vec![];
-    let mut memory = MockedMemory::default();
-    let mut logic = VMLogic::new(&mut ext, context, &config, &promise_results, &mut memory);
-
-    add_key_vals(
-        &mut logic,
-        &[KeyVal(b"aaa", b"bar1"), KeyVal(b"aaa", b"bar2"), KeyVal(b"aaaa", b"bar3")],
-    );
-    let iter_id =
-        iter_prefix_check(&mut logic, b"aaa", &[KeyVal(b"aaa", b"bar2"), KeyVal(b"aaaa", b"bar3")]);
-    // iterator exhausted
-    assert_eq!(logic.storage_iter_next(iter_id, 0, 1).unwrap(), 0);
 }
 
 #[test]
@@ -139,14 +148,14 @@ fn test_iterator_range() {
         b"aaa",
         b"abb",
         &[KeyVal(b"aaa", b"bar2"), KeyVal(b"ab", b"bar2")],
-    );
+    false);
     assert_eq!(id0, 0);
     let id1 = iter_range_check(
         &mut logic,
         b"aaa",
         b"abb",
         &[KeyVal(b"aaa", b"bar2"), KeyVal(b"ab", b"bar2")],
-    );
+        false);
     assert_eq!(id1, 1);
 }
 
@@ -169,5 +178,25 @@ fn test_iterator_range_intersect() {
             KeyVal(b"abb", b"bar3"),
         ],
     );
-    iter_range_check(&mut logic, b"ab", b"a", &[]);
+    iter_range_check(&mut logic, b"ab", b"a", &[], false);
+}
+
+#[test]
+fn test_iterator_from_register() {
+    test_iterator(true);
+}
+
+#[test]
+fn test_iterator_memory() {
+    test_iterator(false);
+}
+
+#[test]
+fn test_iterator_from_second_register() {
+    test_iterator_from_second(true);
+}
+
+#[test]
+fn test_iterator_from_second_guest_mem() {
+    test_iterator_from_second(false);
 }
