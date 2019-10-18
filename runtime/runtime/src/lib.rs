a767c45f3f2dc7edd5864b23277d8bb71528ace5
Evgeny Kuzyakov <ek@nearprotocol.com>
2019-10-18 12:30:46 -0700

    Merge bug

--- runtime/runtime/src/lib.rs
+++ runtime/runtime/src/lib.rs
@@ -851,7 +851,6 @@ impl Runtime {
         prev_receipts: &[Receipt],
         transactions: &[SignedTransaction],
     ) -> Result<ApplyResult, RuntimeError> {
-        let initial_state = TrieUpdate::new(state_update.trie.clone(), state_update.get_root());
         // TODO(#1481): Remove clone after Runtime bug is fixed.
         let initial_state = state_update.clone();
 
