6aeaa588ff6caa463e639268441d59210e3af2e6
Bo Yao <bo@nearprotocol.com>
2019-10-31 14:26:39 -0700

    coverage debugging

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -13,8 +13,11 @@ do
   if [ -f $file ] && [ -x $file ]; then
     mkdir -p "target/cov/$(basename $file)"
     kcov --include-pattern=nearcore --verify "target/cov/$(basename $file)" "$file"
+    break
   fi
 done
 
+pwd
+ls -l
 curl -s https://codecov.io/bash | bash
 echo "Uploaded code coverage"
