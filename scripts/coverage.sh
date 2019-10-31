91df10bfbcfa8ae174bd81f7ec9e1a7cfad9188b
Bo Yao <bo@nearprotocol.com>
2019-10-31 15:47:27 -0700

    work around to fix codecov symlink

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [[ -z "${GITLAB_CI}" ]]; then
+  echo "This script only works in CI"
+  exit 1
+fi
+
 for file in `find target/debug/deps/ \
   ! -name 'test*' \
   ! -name 'near' \
@@ -12,7 +17,6 @@ do
       # codecov script cannot follow symlinks, so place here and mv it to target
     mkdir -p "target2/cov/$(basename $file)"
     kcov --include-pattern=nearcore --verify "target2/cov/$(basename $file)" "$file"
-    break
   fi
 done
 
