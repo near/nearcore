9e30d8cbd30cbb79ce84380642e4066308960e82
Illia Polosukhin <ilblackdragon@gmail.com>
2019-11-10 12:36:09 -0800

    Reduce wait for validators who are offline, remove mem leak (#1647)
    * Initial implementation to reduce time to wait for validators who are offline. Adding cache instead of unbounded hashmap to EpochManager
    
    * some refactoring
    
    * add test
    
    * Calculate produced/expected blocks in epoch manager faster
    
    * Fix overflow in duration computation
    
    * Fix test_block_tracker test
    
    * Fix comments
    
    * Replace panic with print to handle when rpc didn't boot up yet in tx_propagation

--- chain/client/src/types.rs
+++ chain/client/src/types.rs
@@ -87,6 +87,8 @@ pub struct ClientConfig {
     pub max_block_production_delay: Duration,
     /// Maximum duration before skipping given height.
     pub max_block_wait_delay: Duration,
+    /// Duration to reduce the wait for each missed block by validator.
+    pub reduce_wait_for_missing_block: Duration,
     /// Expected block weight (num of tx, gas, etc).
     pub block_expected_weight: u32,
     /// Skip waiting for sync (for testing or single node testnet).
@@ -148,6 +150,7 @@ impl ClientConfig {
             min_block_production_delay: Duration::from_millis(block_prod_time),
             max_block_production_delay: Duration::from_millis(2 * block_prod_time),
             max_block_wait_delay: Duration::from_millis(3 * block_prod_time),
+            reduce_wait_for_missing_block: Duration::from_millis(0),
             block_expected_weight: 1000,
             skip_sync_wait,
             sync_check_period: Duration::from_millis(100),
