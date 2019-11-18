d777c0340515d1cb1129fc81a1004b3e2a6df069
Maksym Zavershynskyi <max@near.ai>
2019-11-17 19:11:21 -0800

    Lockup and vesting contract

--- genesis-tools/lockup-contract/build.sh
+++ genesis-tools/lockup-contract/build.sh
@@ -2,5 +2,7 @@
 
 RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/lockup_contract.wasm ./
+RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release --features vesting
+cp target/wasm32-unknown-unknown/release/lockup_contract.wasm ./lockup_vesting_contract.wasm
 #rm -rf target
-md5sum ./res/lockup_contract.wasm > md5.txt
+md5sum ./*.wasm > md5.txt
--- genesis-tools/lockup-contract/src/lib.rs
+++ genesis-tools/lockup-contract/src/lib.rs
@@ -17,18 +17,83 @@ pub struct LockupContract {
     keys: HashMap<KeyType, HashSet<PublicKey>>,
     /// Whether this account is disallowed to stake anymore.
     permanently_unstaked: bool,
+    #[cfg(feature = "vesting")]
+    vesting_start_timestamp: u64,
+    #[cfg(feature = "vesting")]
+    vesting_cliff_timestamp: u64,
+    #[cfg(feature = "vesting")]
+    vesting_end_timestamp: u64,
 }
 
 #[near_bindgen(init => new)]
 impl LockupContract {
+    /// Check that all timestamps are strictly in the future.
+    fn check_timestamps_future(timestamps: &[u64]) {
+        let block_timestamp = env::block_timestamp();
+        for stamp in timestamps {
+            assert!(*stamp > block_timestamp, "All timestamps should be strictly in the future.");
+        }
+    }
+
+    /// Check that the timestamps are monotonically non-decreasing.
+    #[cfg(feature = "vesting")]
+    fn check_timestamp_ordering(timestamps: &[u64]) {
+        for i in 1..timestamps.len() {
+            assert!(
+                timestamps[i - 1] <= timestamps[i],
+                "Timestamps should be monotonically non-decreasing."
+            );
+        }
+    }
+
     /// Initializes an account with the given lockup amount, lockup timestamp (when it
     /// expires), and the keys that it needs to add
+    #[cfg(not(feature = "vesting"))]
     pub fn new(
         lockup_amount: u128,
         lockup_timestamp: u64,
         initial_keys: Vec<(KeyType, PublicKey)>,
     ) -> Self {
         let mut res = Self { lockup_amount, lockup_timestamp, ..Default::default() };
+        Self::check_timestamps_future(&[lockup_timestamp]);
+        for (key_type, key) in initial_keys {
+            res.add_key_no_check(key_type, key);
+        }
+        res
+    }
+
+    /// Same initialization method as above, but with vesting functionality.
+    #[cfg(feature = "vesting")]
+    pub fn new(
+        lockup_amount: u128,
+        lockup_timestamp: u64,
+        vesting_start_timestamp: u64,
+        vesting_cliff_timestamp: u64,
+        vesting_end_timestamp: u64,
+        initial_keys: Vec<(KeyType, PublicKey)>,
+    ) -> Self {
+        let mut res = Self {
+            lockup_amount,
+            lockup_timestamp,
+            vesting_start_timestamp,
+            vesting_cliff_timestamp,
+            vesting_end_timestamp,
+            ..Default::default()
+        };
+        Self::check_timestamps_future(&[
+            lockup_timestamp,
+            vesting_start_timestamp,
+            vesting_cliff_timestamp,
+            vesting_end_timestamp,
+        ]);
+        // The cliff should be between start and end of the vesting, potentially inclusive.
+        Self::check_timestamp_ordering(&[
+            vesting_start_timestamp,
+            vesting_cliff_timestamp,
+            vesting_end_timestamp,
+        ]);
+        // The lockup should be after start of the vesting, potentially inclusive.
+        Self::check_timestamp_ordering(&[vesting_start_timestamp, lockup_timestamp]);
         for (key_type, key) in initial_keys {
             res.add_key_no_check(key_type, key);
         }
@@ -95,9 +160,35 @@ impl LockupContract {
     }
 
     /// Get the amount of liquid tokens that this account has.
-    fn get_liquid(&self) -> u128 {
-        if env::block_timestamp() < self.lockup_timestamp {
+    #[cfg(not(feature = "vesting"))]
+    pub fn get_liquid(&self) -> u128 {
+        if env::block_timestamp() >= self.lockup_timestamp {
             env::account_balance()
+        } else {
+            env::account_balance() - self.lockup_amount
+        }
+    }
+
+    /// Get the amount of tokens that were vested.
+    #[cfg(feature = "vesting")]
+    pub fn get_vested(&self) -> u128 {
+        let block_timestamp = env::block_timestamp();
+        if block_timestamp <= self.vesting_cliff_timestamp {
+            0
+        } else if block_timestamp >= self.vesting_end_timestamp {
+            self.lockup_amount
+        } else {
+            (block_timestamp - self.vesting_start_timestamp) as u128 * self.lockup_amount
+                / (self.vesting_end_timestamp - self.vesting_start_timestamp) as u128
+        }
+    }
+
+    /// Get the amount of liquid tokens that this account has. Takes vesting into account.
+    #[cfg(feature = "vesting")]
+    pub fn get_liquid(&self) -> u128 {
+        let vested_tokens = self.get_vested();
+        if env::block_timestamp() >= self.lockup_timestamp {
+            env::account_balance() - self.lockup_amount + vested_tokens
         } else {
             env::account_balance() - self.lockup_amount
         }
@@ -115,5 +206,15 @@ impl LockupContract {
     pub fn permanently_unstake(&mut self, key: PublicKey) {
         Promise::new(env::current_account_id()).stake(0, key);
         self.permanently_unstaked = true;
+    }
+
+    /// Stop vesting and transfer all unvested tokens to a beneficiary.
+    #[cfg(feature = "vesting")]
+    pub fn terminate(&mut self, beneficiary_id: String) {
+        let vested = self.get_vested();
+        let unvested = self.lockup_amount - vested;
+        self.lockup_amount = vested;
+        self.vesting_end_timestamp = env::block_timestamp();
+        Promise::new(beneficiary_id).transfer(unvested);
     }
 }
