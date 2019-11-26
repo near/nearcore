6ea15240c63172728b86ebcb19ed5899805394da
Bo Yao <bo@nearprotocol.com>
2019-11-25 16:11:33 -0800

    both debug and release build

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -7,5 +7,7 @@ fi
 
 # file name example: near-staging-a449f839907f060e75f4448e8f30c7bf7f7a951b
 near_binary_file=near-$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)-$(git rev-parse HEAD)
-cp target/release/near ${near_binary_file}
-gsutil cp ${near_binary_file} gs://nearprotocol_nearcore_release
+cp target/release/near ${near_binary_file}-release
+cp target/debug/near ${near_binary_file}-debug
+/snap/bin/gsutil cp ${near_binary_file}-release gs://nearprotocol_nearcore_release
+/snap/bin/gsutil cp ${near_binary_file}-debug gs://nearprotocol_nearcore_release
