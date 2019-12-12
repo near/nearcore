5007890837b264d362b75837ae76b31cff3464c7
Bo Yao <bo@nearprotocol.com>
2019-12-11 20:24:41 -0500

    Parallel coverage (#1823)
    * Isolated parallel test
    * update gitlab-ci
    * backward compatible with python3.6
    * Merge branch 'staging' into really-isolated-parallel-test
    * fix tests
    * Merge branch 'staging' into really-isolated-parallel-test
    * blocking print
    * Merge branch 'really-isolated-parallel-test' of github.com:nearprotocol/nearcore into really-isolated-parallel-test
    * update using os.path
    * update using os.path
    * fix
    * Merge branch 'staging' into really-isolated-parallel-test
    * reduce one more cpu to avoid timeout
    * Merge branch 'really-isolated-parallel-test' of github.com:nearprotocol/nearcore into really-isolated-parallel-test
    * WIP parallel coverage
    * contunue run all tests after some test fail
    * log wording
    * increase timeout for circle
    * Merge branch 'staging' into really-isolated-parallel-test
    * optimize coverage to finish in 6 minutes (locally)
    * Merge branch 'really-isolated-parallel-test' of github.com:nearprotocol/nearcore into really-isolated-parallel-test
    * increase circle timeout more as it timeout again
    * uploading coverage
    * use gitlab-ci.yml to upload coverage
    * add more time
    * Merge branch 'staging' into really-isolated-parallel-test
    * revert test, only keep parallel coverage
    * revert time
    * Merge branch 'staging' into really-isolated-parallel-test
    * Merge refs/heads/staging into really-isolated-parallel-test

---
+++ scripts/parallel_run_tests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import os
+from testlib import clean_binary_tests, build_tests, test_binaries, workers
+from concurrent.futures import as_completed, ThreadPoolExecutor
+import subprocess
+
+
+def run_test(test_binary):
+    """ Run a single test by copying to docker, save exitcode, stdout and stderr """
+    cmd = ['docker', 'run', '--rm',
+    '-v', f'{test_binary}:{test_binary}', 
+    'ailisp/near-test-runtime',
+    'bash', '-c', f'chmod +x {test_binary} && RUST_BACKTRACE=1 {test_binary}']
+    p = subprocess.Popen(cmd, 
+    stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate()
+    return (p.returncode, stdout, stderr)
+
+
+if __name__ == "__main__":
+    clean_binary_tests()
+    build_tests()
+    binaries = test_binaries(exclude=[r'test_regression-.*'])
+    print(f'========= collected {len(binaries)} test binaries:')
+    print('\n'.join(binaries))
+
+    completed = 0
+    fails = []
+    with ThreadPoolExecutor(max_workers=workers()) as executor:
+        future_to_binary = {executor.submit(run_test, binary): binary for binary in binaries}
+        for future in as_completed(future_to_binary):
+            completed += 1
+            binary = os.path.basename(future_to_binary[future])
+            result = future.result()
+            print(f'========= test binary {binary}')
+            print(f'========= stdout of {binary}:')
+            print(result[1])
+            print(f'========= stderr of {binary}:')
+            print(result[2])
+            if result[0] != 0:
+                fails.append(f'========= test binary {binary} failed, exit code {result[0]}')
+
+    print(f"========= finished run {completed} test binaries")
+    if fails:
+        for f in fails:
+            print(f)
+        exit(1)
+    else:
+        print("========= all tests passed")
