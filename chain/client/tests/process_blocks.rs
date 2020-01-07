8e65578ebba02c4474d7d2e00f5d74386046fbeb
Bo Yao <bo@nearprotocol.com>
2020-01-07 00:08:54 -0800

    all tests passed

--- chain/client/tests/process_blocks.rs
+++ chain/client/tests/process_blocks.rs
@@ -59,6 +59,7 @@ fn produce_two_blocks() {
 // TODO: figure out how to re-enable it correctly
 #[ignore]
 fn produce_blocks_with_tx() {
+    use reed_solomon_erasure::ReedSolomon;
     let mut encoded_chunks: Vec<EncodedShardChunk> = vec![];
     init_test_logger();
     System::run(|| {
@@ -88,10 +89,18 @@ fn produce_blocks_with_tx() {
                             Some(part.part.clone());
                     }
 
+                    let parity_parts = total_parts - data_parts;
+                    let rs = ReedSolomon::<reed_solomon_erasure::galois_8::Field>::new(
+                        data_parts,
+                        parity_parts,
+                    )
+                    .unwrap();
+
                     if let ChunkStatus::Complete(_) = ShardsManager::check_chunk_complete(
                         data_parts,
                         total_parts,
                         &mut encoded_chunks[height - 2],
+                        &rs,
                     ) {
                         let chunk = encoded_chunks[height - 2].decode_chunk(data_parts).unwrap();
                         if chunk.transactions.len() > 0 {
