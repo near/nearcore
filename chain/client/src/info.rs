004ead2c04a518b5fafd797c27bb85b350eae568
Vlad Frolov <frolvlad@gmail.com>
2020-01-13 20:33:06 +0100

    Restructured Telemetry data and added agent (near-rs) name and version (resolves #1750)

--- chain/client/src/info.rs
+++ chain/client/src/info.rs
@@ -9,28 +9,31 @@ use sysinfo::{get_current_pid, Pid, ProcessExt, System, SystemExt};
 use near_chain::Tip;
 use near_network::types::{NetworkInfo, PeerId};
 use near_primitives::serialize::to_base;
+use near_primitives::types::Version;
 use near_telemetry::{telemetry, TelemetryActor};
 
-use crate::types::{BlockProducer, ShardSyncStatus, SyncStatus};
+use crate::types::{BlockProducer, ClientConfig, ShardSyncStatus, SyncStatus};
 use near_primitives::types::Gas;
 use std::cmp::min;
 
 /// A helper that prints information about current chain and reports to telemetry.
 pub struct InfoHelper {
+    /// Nearcore agent (executable) version
+    nearcore_version: Version,
+    /// System reference.
+    sys: System,
+    /// Process id to query resources.
+    pid: Option<Pid>,
     /// Timestamp when client was started.
     started: Instant,
+    /// Sign telemetry with block producer key if available.
+    block_producer: Option<BlockProducer>,
     /// Total number of blocks processed.
     num_blocks_processed: u64,
     /// Total gas used during period.
     gas_used: u64,
     /// Total gas limit during period.
     gas_limit: u64,
-    /// Process id to query resources.
-    pid: Option<Pid>,
-    /// System reference.
-    sys: System,
-    /// Sign telemetry with block producer key if available.
-    block_producer: Option<BlockProducer>,
     /// Telemetry actor.
     telemetry_actor: Addr<TelemetryActor>,
 }
@@ -38,15 +41,17 @@ pub struct InfoHelper {
 impl InfoHelper {
     pub fn new(
         telemetry_actor: Addr<TelemetryActor>,
+        client_config: &ClientConfig,
         block_producer: Option<BlockProducer>,
     ) -> Self {
         InfoHelper {
+            nearcore_version: client_config.version.clone(),
+            sys: System::new(),
+            pid: get_current_pid().ok(),
             started: Instant::now(),
             num_blocks_processed: 0,
             gas_used: 0,
             gas_limit: 0,
-            pid: get_current_pid().ok(),
-            sys: System::new(),
             telemetry_actor,
             block_producer,
         }
@@ -68,7 +73,7 @@ impl InfoHelper {
         is_fisherman: bool,
         num_validators: usize,
     ) {
-        let (cpu_usage, memory) = if let Some(pid) = self.pid {
+        let (cpu_usage, memory_usage) = if let Some(pid) = self.pid {
             if self.sys.refresh_process(pid) {
                 let proc = self
                     .sys
@@ -94,7 +99,7 @@ impl InfoHelper {
               Cyan.bold().paint(format!("{:2}/{:?}/{:2} peers", network_info.num_active_peers, network_info.most_weight_peers.len(), network_info.peer_max_count)),
               Cyan.bold().paint(format!("⬇ {} ⬆ {}", pretty_bytes_per_sec(network_info.received_bytes_per_sec), pretty_bytes_per_sec(network_info.sent_bytes_per_sec))),
               Green.bold().paint(format!("{:.2} bps {}", avg_bls, gas_used_per_sec(avg_gas_used))),
-              Blue.bold().paint(format!("CPU: {:.0}%, Mem: {}", cpu_usage, pretty_bytes(memory * 1024)))
+              Blue.bold().paint(format!("CPU: {:.0}%, Mem: {}", cpu_usage, pretty_bytes(memory_usage * 1024)))
         );
 
         self.started = Instant::now();
@@ -106,17 +111,26 @@ impl InfoHelper {
             &self.telemetry_actor,
             try_sign_json(
                 json!({
-                    "account_id": self.block_producer.clone().map(|bp| bp.account_id).unwrap_or("".to_string()),
-                    "is_validator": is_validator,
-                    "node_id": format!("{}", node_id),
-                    "status": display_sync_status(&sync_status, &head),
-                    "latest_block_hash": to_base(&head.last_block_hash),
-                    "latest_block_height": head.height,
-                    "num_peers":  network_info.num_active_peers,
-                    "bandwidth_download": network_info.received_bytes_per_sec,
-                    "bandwidth_upload": network_info.sent_bytes_per_sec,
-                    "cpu": cpu_usage,
-                    "memory": memory,
+                    "agent": {
+                        "name": "near-rs",
+                        "version": self.nearcore_version.version,
+                        "build": self.nearcore_version.build,
+                    },
+                    "system": {
+                        "bandwidth_download": network_info.received_bytes_per_sec,
+                        "bandwidth_upload": network_info.sent_bytes_per_sec,
+                        "cpu_usage": cpu_usage,
+                        "memory_usage": memory_usage,
+                    },
+                    "chain": {
+                        "node_id": node_id.to_string(),
+                        "account_id": self.block_producer.clone().map(|bp| bp.account_id).unwrap_or("".to_string()),
+                        "is_validator": is_validator,
+                        "status": sync_status.as_variant_name(),
+                        "latest_block_hash": to_base(&head.last_block_hash),
+                        "latest_block_height": head.height,
+                        "num_peers":  network_info.num_active_peers,
+                    }
                 }),
                 &self.block_producer,
             ),
