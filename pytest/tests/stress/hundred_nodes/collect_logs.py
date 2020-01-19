6e17f1cbd234398c8638be52f7c695463768f1f1
Bo Yao <bo@nearprotocol.com>
2020-01-18 20:02:50 -0800

    collect log from name node

--- pytest/tests/stress/hundred_nodes/collect_logs.py
+++ pytest/tests/stress/hundred_nodes/collect_logs.py
@@ -3,8 +3,11 @@ from distutils.util import strtobool
 import sys
 import datetime
 
+sys.path.append('lib')
+from utils import user_name
+
 machines = gcloud.list()
-node_prefix = sys.argv[1] if len(sys.argv) >= 2 else "pytest-node"
+node_prefix = sys.argv[1] if len(sys.argv) >= 2 else f"pytest-node-{user_name()}"
 nodes = list(filter(lambda m: m.name.startswith(node_prefix), machines))
 
 log_file = sys.argv[2] if len(sys.argv) >= 3 else "produce_record.txt"
