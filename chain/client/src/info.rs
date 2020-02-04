7c7ad6e7eb8b01ff9a324652bf0b3769bec9bd7a
Bo Yao <bo@nearprotocol.com>
2020-02-04 14:45:17 -0800

    Move sysinfo fix from staging

--- chain/client/src/info.rs
+++ chain/client/src/info.rs
@@ -4,7 +4,7 @@ use actix::Addr;
 use ansi_term::Color::{Blue, Cyan, Green, White, Yellow};
 use log::info;
 use serde_json::json;
-use sysinfo::{get_current_pid, Pid, ProcessExt, System, SystemExt};
+use sysinfo::{get_current_pid, set_open_files_limit, Pid, ProcessExt, System, SystemExt};
 
 use near_chain::Tip;
 use near_network::types::{NetworkInfo, PeerId};
@@ -40,6 +40,7 @@ impl InfoHelper {
         telemetry_actor: Addr<TelemetryActor>,
         block_producer: Option<BlockProducer>,
     ) -> Self {
+        set_open_files_limit(0);
         InfoHelper {
             started: Instant::now(),
             num_blocks_processed: 0,
