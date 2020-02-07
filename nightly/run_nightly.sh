6459923a229b69d8d151583da550a935ccd066e3
Bo Yao <bo@nearprotocol.com>
2020-02-07 15:07:10 -0800

    save bigger than 1m log to cloud storage

--- nightly/run_nightly.sh
+++ nightly/run_nightly.sh
@@ -5,6 +5,15 @@ do
         timestamp=$(date +%y%m%d_%H%M%S)
         output_path=~/testruns/$(git rev-parse HEAD)_${timestamp}
         python nightly.py run nightly.txt ${output_path}
-        gsutil cp -h -r ${output_path} gs://log.nightly.neartest.com
-        find output_path -type f ! -name log -exec rm {} \;
+
+        # Upload file bigger than 1M to gcloud storage
+        for f in `find ${output_path} -type f -size +1M`
+        do
+                path=$(echo ${f} | sed "s|.*testruns/||");
+                cd ~/testruns
+                gsutil cp -r ${path} gs://log.nightly.neartest.com/${path}
+                # Original log save the log url
+                echo "https://storage.cloud.google.com/log.nightly.neartest.com/${path}" > ${f}
+                cd -
+        done
 done
