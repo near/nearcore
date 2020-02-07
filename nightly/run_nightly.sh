f84f7a5653e710bc9b4b7b9ff45eeb4c2b2be2dd
Bo Yao <bo@nearprotocol.com>
2020-02-06 18:13:53 -0800

    Nightly log in gcloud storage

--- nightly/run_nightly.sh
+++ nightly/run_nightly.sh
@@ -2,5 +2,9 @@ while true
 do
         git pull --ff-only origin staging
         cd .. && cargo build --all --tests && cd nightly
-        python nightly.py run nightly.txt ~/testruns/$(git rev-parse HEAD)_$(date +%y%m%d_%H%M%S)
+        timestamp=$(date +%y%m%d_%H%M%S)
+        output_path=~/testruns/$(git rev-parse HEAD)_${timestamp}
+        python nightly.py run nightly.txt ${output_path}
+        gsutil cp -h -r ${output_path} gs://log.nightly.neartest.com
+        find output_path -type f ! -name log -exec rm {} \;
 done
