631e0fa920b0717ccb0c2b740e422310d5d05ac5
Bo Yao <bo@nearprotocol.com>
2020-02-14 16:46:02 -0800

    remove split records, make split-genesis.py output identical to state-viewer dump

--- scripts/split-state.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-## Split near/res/state into several at most 40M files to fit github
-split -b 40000000 near/res/testnet_records.json near/res/testnet_records.json.
--- scripts/state/split-genesis.py
+++ scripts/state/split-genesis.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
 # Splits testnet.json into 2 files:
-# - testnet.json.config - that contains all fields except for the records
-# - testnet.json.records - that contains all records from the testnet.json
-# It also sorts keys in the genesis config file.
-# NOTE: Python 3.7 required for sort_keys
+# - testnet_config.json - that contains all fields except for the records
+# - testnet_records.json - that contains all records from the testnet.json
+# It keeps key order in the genesis config file.
 
 import json
 import os
+from collections import OrderedDict
 
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../near/res/testnet.json')
-q = json.loads(open(filename).read())
+q = json.loads(open(filename).read(), object_pairs_hook=OrderedDict)
 
 records = q['records']
 q['records'] = []
 
-open(filename + '.config', 'w').write(json.dumps(q, indent=2, sort_keys=True))
-open(filename + '.records', 'w').write(json.dumps(records, indent=2))
+open(filename.replace('.json', '_config.json'), 'w').write(json.dumps(q, indent=2))
+open(filename.replace('.json', '_records.json'), 'w').write(json.dumps(records, indent=2))
