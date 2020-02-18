00c5af09672fc70467bbc5bb654d5e2e09575be0
Bo Yao <bo@nearprotocol.com>
2020-02-18 14:59:23 -0800

    script to dump genesis records, config from genesis and upload to s3

--- scripts/state/split-genesis.py
+++ scripts/state/split-genesis.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 # Splits testnet.json into 2 files:
-# - testnet_config.json - that contains all fields except for the records
-# - testnet_records.json - that contains all records from the testnet.json
+# - testnet_genesis_config.json - that contains all fields except for the records
+# - testnet_genesis_records.json - that contains all records from the testnet.json
 # It keeps key order in the genesis config file.
 
 import json
@@ -15,5 +15,5 @@ q = json.loads(open(filename).read(), object_pairs_hook=OrderedDict)
 records = q['records']
 q['records'] = []
 
-open(filename.replace('.json', '_config.json'), 'w').write(json.dumps(q, indent=2))
-open(filename.replace('.json', '_records.json'), 'w').write(json.dumps(records, indent=2))
+open(filename.replace('.json', '_genesis_config.json'), 'w').write(json.dumps(q, indent=2))
+open(filename.replace('.json', '_genesis_records.json'), 'w').write(json.dumps(records, indent=2))
