d53bd32b09fa200b4c6b1fedfb8a9bf7d86e4773
Bo Yao <bo@nearprotocol.com>
2020-03-06 12:19:35 -0800

    linux subdir

--- scripts/binary-release.sh
+++ scripts/binary-release.sh
@@ -8,9 +8,9 @@ cargo build -p near --release
 cargo build -p keypair-generator --release
 cargo build -p genesis-csv-to-json --release
 
-aws s3 cp --acl public-read target/release/near s3://build.nearprotocol.com/nearcore/${branch}/near
-aws s3 cp --acl public-read target/release/near s3://build.nearprotocol.com/nearcore/${branch}/${commit}/near
-aws s3 cp --acl public-read target/release/keypair-generator s3://build.nearprotocol.com/nearcore/${branch}/keypair-generator
-aws s3 cp --acl public-read target/release/keypair-generator s3://build.nearprotocol.com/nearcore/${branch}/${commit}/keypair-generator
-aws s3 cp --acl public-read target/release/genesis-csv-to-json s3://build.nearprotocol.com/nearcore/${branch}/genesis-csv-to-json
-aws s3 cp --acl public-read target/release/genesis-csv-to-json s3://build.nearprotocol.com/nearcore/${branch}/${commit}/genesis-csv-to-json
+aws s3 cp --acl public-read target/release/near s3://build.nearprotocol.com/nearcore/linux/${branch}/near
+aws s3 cp --acl public-read target/release/near s3://build.nearprotocol.com/nearcore/linux/${branch}/${commit}/near
+aws s3 cp --acl public-read target/release/keypair-generator s3://build.nearprotocol.com/nearcore/linux/${branch}/keypair-generator
+aws s3 cp --acl public-read target/release/keypair-generator s3://build.nearprotocol.com/nearcore/linux/${branch}/${commit}/keypair-generator
+aws s3 cp --acl public-read target/release/genesis-csv-to-json s3://build.nearprotocol.com/nearcore/linux/${branch}/genesis-csv-to-json
+aws s3 cp --acl public-read target/release/genesis-csv-to-json s3://build.nearprotocol.com/nearcore/linux/${branch}/${commit}/genesis-csv-to-json
