b5642d1f228b8bd77229e4e63d82e42043f9c564
Marcelo Fornet <mfornet94@gmail.com>
2020-03-17 12:55:57 -0400

    feat(network): Network metric recorder (#2239)
    * feat: Record aggregated information from newtork.
    
    Extract useful metadata from each message passing through the network
    to fully characterize the behavior of it regarding number and size
    of each message.
    
    * feat(recorder): Report network information using json.
    
    * feat(recorder): Use ping to detect latency.
    
    Nodes sent frequently (every 5 seconds) a ping to every other
    reachable node and waits for the pong to arrive and record
    the time that was needed for the full round.
    
    * feat(recorder): Send ping to all peers every 20s.
    
    Don't make report frequently but instead expose it
    via RPC. Don't implemented yet.
    
    * feat(recorder): Expose metrics via rpc call.
    
    * Put metric-recorder behind a flag.
    
    * Use sized_cache for ping/pong to avoid memory leak
    
    * Run ping_all_peers only if compiled with metric_recorder
    
    * Impl from trait instead of using custom function.
    
    * Address comments
    
    * Use `num_different_hashes`
    
    Co-Authored-By: Bowen Wang <bowenwang1996@users.noreply.github.com>
    
    Co-authored-by: Bowen Wang <bowenwang1996@users.noreply.github.com>

--- chain/client/src/types.rs
+++ chain/client/src/types.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "metric_recorder")]
+use near_network::recorder::MetricRecorder;
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -222,6 +224,8 @@ pub struct NetworkInfoResponse {
     pub received_bytes_per_sec: u64,
     /// Accounts of known block and chunk producers from routing table.
     pub known_producers: Vec<KnownProducer>,
+    #[cfg(feature = "metric_recorder")]
+    pub metric_recorder: MetricRecorder,
 }
 
 /// Status of given transaction including all the subsequent receipts.
