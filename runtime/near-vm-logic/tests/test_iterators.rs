6b85c73548fc5e3c8a314e81fd456ad7f644591e
nearmax <max@nearprotocol.com>
2020-03-17 00:07:49 +0000

    Nit

--- runtime/near-vm-logic/tests/test_iterators.rs
+++ runtime/near-vm-logic/tests/test_iterators.rs
@@ -4,7 +4,7 @@ use crate::fixtures::get_context;
 use near_runtime_fees::RuntimeFeesConfig;
 use near_vm_logic::mocks::mock_external::MockedExternal;
 use near_vm_logic::mocks::mock_memory::MockedMemory;
-use near_vm_logic::{VMConfig, VMLogic, VMLogicError, HostError};
+use near_vm_logic::{HostError, VMConfig, VMLogic, VMLogicError};
 
 #[test]
 fn test_iterator_deprecated() {
@@ -16,7 +16,22 @@ fn test_iterator_deprecated() {
     let mut memory = MockedMemory::default();
     let mut logic = VMLogic::new(&mut ext, context, &config, &fees, &promise_results, &mut memory);
 
-    assert_eq!(Err(VMLogicError::HostError(HostError::Deprecated {method_name: "storage_iter_prefix".to_string()})), logic.storage_iter_prefix(1, b"a".as_ptr() as _));
-    assert_eq!(Err(VMLogicError::HostError(HostError::Deprecated {method_name: "storage_iter_range".to_string()})), logic.storage_iter_range(1, b"a".as_ptr() as _, 1, b"b".as_ptr() as _));
-    assert_eq!(Err(VMLogicError::HostError(HostError::Deprecated {method_name: "storage_iter_next".to_string()})), logic.storage_iter_next(0, 0, 1));
+    assert_eq!(
+        Err(VMLogicError::HostError(HostError::Deprecated {
+            method_name: "storage_iter_prefix".to_string()
+        })),
+        logic.storage_iter_prefix(1, b"a".as_ptr() as _)
+    );
+    assert_eq!(
+        Err(VMLogicError::HostError(HostError::Deprecated {
+            method_name: "storage_iter_range".to_string()
+        })),
+        logic.storage_iter_range(1, b"a".as_ptr() as _, 1, b"b".as_ptr() as _)
+    );
+    assert_eq!(
+        Err(VMLogicError::HostError(HostError::Deprecated {
+            method_name: "storage_iter_next".to_string()
+        })),
+        logic.storage_iter_next(0, 0, 1)
+    );
 }
