b49ea144af0be1f20e1c3e4abc8e255d7ba788a6
Mikhail <mikhail@nearprotocol.com>
2020-04-01 13:23:38 -0700

    near binary in script

--- scripts/testlib.py
+++ scripts/testlib.py
@@ -47,7 +47,8 @@ def test_binaries(exclude=None):
     for f in glob.glob(f'{target_debug}/deps/*'):
         fname = os.path.basename(f)
         ext = os.path.splitext(fname)[1]
-        if os.path.isfile(f) and fname != 'near' and ext == '':
+        is_near_binary = re.match('near-[0-9a-f]{16}$', fname) or fname == 'near'
+        if os.path.isfile(f) and not is_near_binary and ext == '':
             if not exclude:
                 binaries.append(f)
             elif not any(map(lambda e: re.match(e, fname), exclude)):
