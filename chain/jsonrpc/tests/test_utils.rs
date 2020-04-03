a6c66fcd5198742fb4476f01f797c97ccc18d636
Bo Yao <bo@nearprotocol.com>
2020-04-03 14:23:18 -0700

    resolve conflict

--- chain/jsonrpc/tests/test_utils.rs
+++ chain/jsonrpc/tests/test_utils.rs
@@ -13,13 +13,7 @@ use near_primitives::views::AccountView;
 
 lazy_static::lazy_static! {
     pub static ref TEST_GENESIS_CONFIG: GenesisConfig =
-<<<<<<< HEAD
         GenesisConfig::from_json(include_str!("../../../near/res/genesis_config.json"));
-||||||| merged common ancestors
-        GenesisConfig::from_json(include_str!("../../../near/res/testnet_genesis_config.json"));
-=======
-        GenesisConfig::from_json(include_str!("../../../neard/res/testnet_genesis_config.json"));
->>>>>>> master
 }
 
 pub fn start_all(validator: bool) -> (Addr<ViewClientActor>, String) {
