ae7dcdf26a421e73b9943f6c2f4e631e67851a4b
Bowen Wang <bowenwang1996@uchicago.edu>
2020-04-08 10:14:38 -0700

    genesis update

--- scripts/migrations/8-fraction.py
+++ scripts/migrations/8-fraction.py
@@ -20,22 +20,10 @@ config = json.load(open(os.path.join(home, 'output.json')), object_pairs_hook=Or
 assert config['protocol_version'] == 7
 
 config['protocol_version'] = 8
-config['gas_price_adjustment_rate'] = {
-    'numerator': 1,
-    'denominator': 100,
-}
-config['protocol_reward_percentage'] = {
-    'numerator': 1,
-    'denominator': 10,
-}
-config['developer_reward_percentage'] = {
-    'numerator': 3,
-    'denominator': 10,
-}
-config['max_inflation_rate'] = {
-    'numerator': 5,
-    'denominator': 100,
-}
-
+config['gas_price_adjustment_rate'] = [1, 100]
+config['protocol_reward_percentage'] = [1, 10]
+config['developer_reward_percentage'] = [3, 10]
+config['max_inflation_rate'] = [5, 100]
+config['runtime_config']['transaction_costs']['burnt_gas_reward'] = [3, 10]
 
 json.dump(config, open(os.path.join(output_home, 'output.json'), 'w'), indent=2)
