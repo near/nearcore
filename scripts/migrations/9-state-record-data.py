847c481d059a39804d2d007db913141b61320a49
Bo Yao <bo@nearprotocol.com>
2020-04-15 12:24:28 -0700

    fix(script): migration for betanet and testnet

--- scripts/migrations/9-state-record-data.py
+++ scripts/migrations/9-state-record-data.py
@@ -27,13 +27,25 @@ for record in config['records']:
         record["Account"]["account"].pop("storage_paid_at", None)
     if "Data" in record:
         # Removing old joined key
-        key = base64.b64decode(["Data"].pop("key"))
+        key = base64.b64decode(record["Data"].pop("key"))
         # Splitting key
         separator_pos = key.find(b',')
         assert(separator_pos > 0)
-        account_id = key[:separator_pos]
+        account_id = key[1:separator_pos]
         data_key = key[separator_pos + 1:]
         record["Data"]["account_id"] = account_id.decode('utf-8')
-        record["Data"]["data_key"] = base64.b64encode(data_key)
+        record["Data"]["data_key"] = base64.b64encode(data_key).decode('utf-8')
+
+validators = []
+for v in config['validators']:
+    validators.append(account_id)
+
+for record in config['records']:
+    if "Account" in record:
+        if record["Account"]["account"]["locked"] != "0" and record["Account"]["account_id"] not in validators:
+            a = int(record["Account"]["account"]["amount"])
+            l = int(record["Account"]["account"]["locked"])
+            record["Account"]["account"]["locked"] = str(0)
+            record["Account"]["account"]["amount"] = str(a+l)
 
 json.dump(config, open(os.path.join(output_home, 'output.json'), 'w'), indent=2)
