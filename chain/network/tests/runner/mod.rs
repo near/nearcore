80f09368382abd049e608ce1922c3e75153ae749
Illia Polosukhin <ilblackdragon@gmail.com>
2020-04-20 01:51:24 -0700

    fix(rpc): Fix returning missing tx error (#2494)
    * Fixing #2488: Return if tx status is missing on the node that tracks this shard
    
    * Keep tx_polling for polling vs tx_fetch to fetch immediate status
    
    * Fix comment
    
    * Made tx_polling to use tx_status_fetch on repeat or timeout
    
    Co-authored-by: Bowen Wang <bowenwang1996@users.noreply.github.com>

--- chain/network/tests/runner/mod.rs
+++ chain/network/tests/runner/mod.rs
@@ -89,6 +89,7 @@ pub fn setup_network_node(
         .unwrap()
         .start();
         let view_client_actor = ViewClientActor::new(
+            config.account_id.clone(),
             &chain_genesis,
             runtime.clone(),
             network_adapter.clone(),
