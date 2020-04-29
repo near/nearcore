b951c71bf5445eaabb2815a807904d4b94a4f76f
Alexey <alexey@fckt.dev>
2020-04-29 14:23:19 +0300

    fix test_consensus_with_epoch_switches test

--- chain/client/tests/consensus.rs
+++ chain/client/tests/consensus.rs
@@ -5,7 +5,7 @@ mod tests {
     use near_chain::Block;
     use near_client::test_utils::setup_mock_all_validators;
     use near_client::{ClientActor, ViewClientActor};
-    use near_logger_utils::init_test_logger;
+    use near_logger_utils::init_integration_logger;
     use near_network::{NetworkClientMessages, NetworkRequests, NetworkResponses, PeerInfo};
     use near_primitives::block::{Approval, ApprovalInner};
     use near_primitives::types::BlockHeight;
