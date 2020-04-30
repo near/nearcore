57377a2ff650149af7b00ab302305b50dce4b629
Bo Yao <bo@nearprotocol.com>
2020-04-30 11:02:59 -0700

    assert return code 0

--- scripts/binary-release-windows.py
+++ scripts/binary-release-windows.py
@@ -5,6 +5,9 @@ uname = 'Windows'
 branch = os.environ.get('GITHUB_REF').split('/')[-1]
 commit = os.environ.get('GITHUB_SHA')
 
-subprocess.run('cargo build -p near-vm-runner-standalone --release', shell=True)
-subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/', shell=True)
-subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/{commit}/', shell=True)
+p = subprocess.run('cargo build -p near-vm-runner-standalone --release', shell=True)
+assert p.returncode == 0
+p = subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/', shell=True)
+assert p.returncode == 0
+p = subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/{commit}/', shell=True)
+assert p.returncode == 0
