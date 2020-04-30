118288ebaa41ee2f9346bd0553193fcd0e3e5be0
Bo Yao <bo@nearprotocol.com>
2020-04-30 09:56:49 -0700

    Feat(script): windows release build (vm-standalone only)

---
+++ scripts/binary-release-windows.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+uname = os.uname()[0]
+branch = os.environ.get('GITHUB_REF').split('/')[-1]
+commit = os.environ.get('GITHUB_SHA')
+
+subprocess.run('cargo build -p near-vm-runner-standalone --release', shell=True)
+subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/', shell=True)
+subprocess.run(f'aws s3 cp --acl public-read target/release/near-vm-runner-standalone s3://build.nearprotocol.com/nearcore/{uname}/{branch}/{commit}/', shell=True)
