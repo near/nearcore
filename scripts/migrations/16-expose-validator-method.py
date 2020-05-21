94974db22877bb8d0db3aca25daa818e928049a2
Bowen Wang <bowenwang1996@uchicago.edu>
2020-05-21 15:20:30 -0700

    bump protocol version

--- scripts/migrations/16-expose-validator-method.py
+++ scripts/migrations/16-expose-validator-method.py
@@ -15,9 +15,9 @@ output_home = sys.argv[2]
 
 config = json.load(open(os.path.join(home, 'output.json')), object_pairs_hook=OrderedDict)
 
-assert config['protocol_version'] == 14
+assert config['protocol_version'] == 15
 
-config['protocol_version'] = 15
+config['protocol_version'] = 16
 
 config['runtime_config']['wasm_config']['ext_costs']['validator_stake_base'] = 303944908800
 config['runtime_config']['wasm_config']['ext_costs']['validator_total_stake_base'] = 303944908800
