d7f095d38792a21fce19f21b1584048171bcf434
Marcelo Fornet <mfornet94@gmail.com>
2020-05-22 19:43:24 -0400

    test(sync): Test syncing nodes with massive data

---
+++ pytest/lib/populate.py
@@ -0,0 +1,29 @@
+from subprocess import Popen
+from os.path import join
+from shutil import copy2, rmtree
+
+
+def genesis_populate(near_root, additional_accounts, node_dir):
+    proc = Popen([
+        join(near_root, 'genesis-populate'),
+        '--additional-accounts-num',
+        str(additional_accounts),
+        '--home',
+        node_dir
+    ])
+    proc.wait()
+    rmtree(join(node_dir, 'data'), ignore_errors=True)
+
+
+def copy_genesis(node_dir_source, node_dir_target):
+    for file_name in ['genesis.json', 'genesis_roots', 'state_dump']:
+        source_file = join(node_dir_source, file_name)
+        target_file = join(node_dir_target, file_name)
+        copy2(source_file, target_file)
+
+
+def genesis_populate_all(near_root, additional_accounts, node_dirs):
+    genesis_populate(near_root, additional_accounts, node_dirs[0])
+    for node_dir in node_dirs[1:]:
+        copy_genesis(node_dirs[0], node_dir)
+        rmtree(join(node_dir, 'data'), ignore_errors=True)
---
+++ pytest/tests/sanity/state_sync_massive.py
@@ -0,0 +1,120 @@
+# Survive massive state sync
+#
+# Create 3 nodes, 1 validator and 2 observers tracking shard the single shard 0.
+# Generate a large state using genesis-populate. [*]
+#
+# Spawn validator and first observer and wait for them to make some progress.
+# Spawn second observer and watch how it is able to sync state
+# without degrading blocks per second.
+#
+# To run this test is important to compile genesis-populate tool first.
+# In nearcore folder run:
+#
+# ```
+# cargo build -p genesis-populate
+# ```
+#
+# [*] This test might take a very large time generating the state.
+# To speed up this between multiple executions, large state can be generated once
+# saved, and reused on multiple executions. Steps to do this.
+#
+# 1. Run test for first time:
+#
+# ```
+# python3 python3 tests/sanity/state_sync_massive.py
+# ```
+#
+# Stop at any point after seeing the message: "Genesis generated"
+#
+# 2. Save generated data:
+#
+# ```
+# cp ~/.near/test0_finished ~/.near/backup_genesis
+# ```
+#
+# 3. Run test passing path to backup_genesis
+#
+# ```
+# python3 tests/sanity/state_sync_massive.py ~/.near/backup_genesis
+# ```
+#
+
+import sys, time, requests
+from queue import Queue
+
+sys.path.append('lib')
+
+from cluster import init_cluster, spin_up_node, load_config
+from utils import TxContext, LogTracker
+from populate import genesis_populate_all, copy_genesis
+
+if len(sys.argv) >= 2:
+    genesis_data = sys.argv[1]
+else:
+    genesis_data = None
+    additional_accounts = 200000
+
+config = load_config()
+near_root, node_dirs = init_cluster(
+    1, 2, 1, config,
+    [["min_gas_price", 0], ["max_inflation_rate", [0, 1]], ["epoch_length", 20],
+     ["block_producer_kickout_threshold", 80]], {1: {
+         "tracked_shards": [0]
+     }, 2: {
+         "tracked_shards": [0]
+     }})
+
+print("Populating genesis")
+
+if genesis_data is None:
+    genesis_populate_all(near_root=near_root, additional_accounts=additional_accounts, node_dirs=node_dirs)
+else:
+    for node_dir in node_dirs:
+        copy_genesis(genesis_data, node_dir)
+
+print("Genesis generated")
+
+boot_node = spin_up_node(config, near_root, node_dirs[0], 0, None, None)
+observer = spin_up_node(config, near_root, node_dirs[1], 1, boot_node.node_key.pk, boot_node.addr())
+
+def wait_for_height(target_height, rpc_node, sleep_time=2):
+    queue = []
+    latest_height = 0
+
+    while latest_height < target_height:
+        # Check current height
+        try:
+            status = rpc_node.get_status()
+            new_height = status['sync_info']['latest_block_height']
+            print(f"Height: {latest_height} => {new_height}")
+            latest_height = new_height
+        except requests.ReadTimeout:
+            print("Timeout Error")
+
+        # Computing bps
+        cur_time = time.time()
+        queue.append((cur_time, latest_height))
+
+        while len(queue) > 2 and queue[0][0] <= cur_time - 7:
+            queue.pop(0)
+
+        if len(queue) <= 1:
+            bps = 0
+        else:
+            head = queue[-1]
+            tail = queue[0]
+            bps = (head[1] - tail[1]) / (head[0] - tail[0])
+
+        print(f"bps: {bps} queue length: {len(queue)}")
+
+        time.sleep(sleep_time)
+
+wait_for_height(40, boot_node)
+
+observer = spin_up_node(config, near_root, node_dirs[2], 2, boot_node.node_key.pk, boot_node.addr())
+
+# TODO: Check that bps is not degraded
+wait_for_height(100, boot_node)
+
+# TODO: Make sure this node is able to sync
+wait_for_height(40, observer)
