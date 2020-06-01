e2c5b91ecb063e33740f86e4d643af3aede152d8
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-06-01 12:16:13 -0700

    Refund allowance

--- scripts/migrations/20-refund-allowance.py
+++ scripts/migrations/20-refund-allowance.py
@@ -12,8 +12,8 @@ output_home = sys.argv[2]
 
 config = json.load(open(os.path.join(home, 'output.json')), object_pairs_hook=OrderedDict)
 
-assert config['protocol_version'] == 17
+assert config['protocol_version'] == 19
 
-config['protocol_version'] = 18
+config['protocol_version'] = 20
 
 json.dump(config, open(os.path.join(output_home, 'output.json'), 'w'), indent=2)
