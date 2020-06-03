b8cbaeae3d39adf9c0a9ccb4d6f4b64ac424a806
Alexander Skidanov <skidanov.alexander@gmail.com>
2020-06-02 17:25:14 -0700

    test: Test infra and two tests for network (#2744)
    * Adding Borsh deserialization to python tests infra
    * Partial schema for peer messages (only handshakes)
    * A basic tests that performs a handshake with the node, and verifies that it adheres to the spec
    * A test that opens multiple connections with the node and starts sending very large messages

---
+++ pytest/tests/spec/network/handshake.py
@@ -0,0 +1,50 @@
+import sys, time
+import socket, base58
+import nacl.signing
+
+sys.path.append('lib')
+
+from cluster import start_cluster
+from peer import *
+from utils import obj_to_string
+from messages.crypto import *
+from messages.network import *
+
+schema = dict(crypto_schema + network_schema)
+my_key_pair_nacl = nacl.signing.SigningKey.generate()
+
+nodes = start_cluster(1, 0, 4, None, [], {})
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(nodes[0].addr())
+    handshake = create_handshake(my_key_pair_nacl, nodes[0].node_key.pk, 12345)
+    sign_handshake(my_key_pair_nacl, handshake.Handshake)
+
+    send_obj(s, schema, handshake)
+
+    response = recv_obj(s, schema, PeerMessage)
+    assert response.enum == 'HandshakeFailure'
+    assert response.HandshakeFailure[1].enum == 'GenesisMismatch'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(nodes[0].addr())
+    gm = response.HandshakeFailure[1].GenesisMismatch
+    handshake.Handshake.chain_info.genesis_id.chain_id = gm.chain_id
+    handshake.Handshake.chain_info.genesis_id.hash = gm.hash
+    sign_handshake(my_key_pair_nacl, handshake.Handshake)
+
+    send_obj(s, schema, handshake)
+    response = recv_obj(s, schema, PeerMessage)
+
+    assert response.enum == 'Handshake'
+    assert response.Handshake.chain_info.genesis_id.chain_id == handshake.Handshake.chain_info.genesis_id.chain_id
+    assert response.Handshake.chain_info.genesis_id.hash == handshake.Handshake.chain_info.genesis_id.hash
+    assert response.Handshake.edge_info.nonce == 1
+    assert response.Handshake.peer_id.keyType == 0
+    assert response.Handshake.peer_id.data == base58.b58decode(
+        nodes[0].node_key.pk[len(ED_PREFIX):])
+    assert response.Handshake.target_peer_id.keyType == 0
+    assert response.Handshake.target_peer_id.data == bytes(
+        my_key_pair_nacl.verify_key)
+    assert response.Handshake.version == handshake.Handshake.version
+    assert response.Handshake.listen_port == nodes[0].addr()[1]
