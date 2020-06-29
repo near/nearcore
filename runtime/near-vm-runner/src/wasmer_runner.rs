010a8fb7411b0447e6bb1c91904bd202155aaeaa
Nikolay Igotti <igotti@gmail.com>
2020-06-29 15:17:23 +0300

    Formatting.

--- runtime/near-vm-runner/src/wasmer_runner.rs
+++ runtime/near-vm-runner/src/wasmer_runner.rs
@@ -110,8 +110,9 @@ impl IntoVMError for wasmer_runtime::error::RuntimeError {
                 }
                 // A trap that Wasmer knows about occurred.
                 // As of 0.17.1, can be thrown on C signals caught, for example OOM.
-                InvokeError::TrapCode { code, srcloc } =>
-                    VMError::FunctionCallError(WasmUnknownError),
+                InvokeError::TrapCode { code, srcloc } => {
+                    VMError::FunctionCallError(WasmUnknownError)
+                }
                 // A trap occurred that Wasmer knows about but it had a trap code that
                 // we weren't expecting or that we do not handle.
                 // As of 0.17.0, thrown only from Cranelift BE.
