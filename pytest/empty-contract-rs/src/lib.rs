5c07c21f0057a0cf9a1c63810fcc1006daa1f8ac
Michael Birch <michael@near.org>
2020-07-15 17:19:41 +0000

    Address comments from @evgenykuzyakov

--- pytest/empty-contract-rs/src/lib.rs
+++ pytest/empty-contract-rs/src/lib.rs
@@ -2,4 +2,4 @@ use near_sdk::{env, metadata, near_bindgen};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: near_sdk::wee_alloc::WeeAlloc = near_sdk::wee_alloc::WeeAlloc::INIT;
