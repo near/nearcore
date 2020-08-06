d58af5b9fdf859e56b16ae95fef30f73cec07098
Willem Wyndham <willem@cs.umd.edu>
2020-08-06 19:01:30 -0400

    refactor(runtime-params-estimator>: use match in main

--- runtime/runtime-params-estimator/src/main.rs
+++ runtime/runtime-params-estimator/src/main.rs
@@ -62,23 +62,24 @@ fn main() {
     let warmup_iters_per_block = matches.value_of("warmup-iters").unwrap().parse().unwrap();
     let iter_per_block = matches.value_of("iters").unwrap().parse().unwrap();
     let active_accounts = matches.value_of("accounts-num").unwrap().parse().unwrap();
-    let metric = matches.value_of("metric").unwrap().to_string();
+    let metric = match matches.value_of("metric").unwrap() {
+      "icount" => GasMetric::ICount,
+      "time" => GasMetric::Time,
+      other => panic!("Unknown metric {}", other)
+    };
     let vm_kind = match matches.value_of("vm") {
         Some("wasmer") => VMKind::Wasmer,
         Some("wasmtime") => VMKind::Wasmtime,
         _ => VMKind::Wasmer,
     };
-    let known_metrics = vec!["icount".to_string(), "time".to_string()];
-    if !known_metrics.contains(&metric) {
-        panic!("Unknown metric {}", metric);
-    }
+
     let runtime_config = run(Config {
         warmup_iters_per_block,
         iter_per_block,
         active_accounts,
         block_sizes: vec![],
         state_dump_path: state_dump_path.clone(),
-        metric: if metric == "icount" { GasMetric::ICount } else { GasMetric::Time },
+        metric,
         vm_kind,
     });
 
