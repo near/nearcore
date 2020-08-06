20e65093f731ed8004ae5258ef574990a107fee5
pmnoxx <you@example.com>
2020-08-06 12:00:19 -0700

    test

--- chain/client/tests/cross_shard_tx.rs
+++ chain/client/tests/cross_shard_tx.rs
@@ -4,6 +4,7 @@ use std::sync::{Arc, RwLock};
 use actix::{Addr, System};
 use futures::{future, FutureExt};
 
+
 use near_client::test_utils::setup_mock_all_validators;
 use near_client::{ClientActor, Query, ViewClientActor};
 use near_logger_utils::init_test_logger;
