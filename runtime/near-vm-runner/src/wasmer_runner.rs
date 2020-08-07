3799da41447e249ffee6da78f7860a71bb808a1c
Nikolay Igotti <igotti@gmail.com>
2020-08-07 17:22:11 +0300

    Perform AVX checks only if Wasmer runner used as a part of the runtime.

--- runtime/near-vm-runner/src/wasmer_runner.rs
+++ runtime/near-vm-runner/src/wasmer_runner.rs
@@ -189,6 +189,7 @@ pub fn run_wasmer<'a>(
             "Execution of smart contracts is only supported for x86 and x86_64 CPU architectures."
         );
     }
+    #[cfg(feature = "compatibility_checks")]
     if !is_x86_feature_detected!("avx") {
         panic!("AVX support is required in order to run Wasmer VM Singlepass backend.");
     }
