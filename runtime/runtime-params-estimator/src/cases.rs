6d9b74a5e0932d964ff3c6168e570e0cd5a3b3a4
Bo Yao <bo@nearprotocol.com>
2020-08-07 12:08:20 -0700

    remove debug print

--- runtime/runtime-params-estimator/src/cases.rs
+++ runtime/runtime-params-estimator/src/cases.rs
@@ -150,7 +150,6 @@ pub fn run(mut config: Config) -> RuntimeConfig {
     config.block_sizes = vec![100];
     // Measure the speed of processing empty receipts.
     measure_actions(Metric::Receipt, &mut m, &config, None, vec![], false, false);
-    println!("fully finish one measure actions");
     // Measure the speed of processing simple transfers.
     measure_actions(
         Metric::ActionTransfer,
