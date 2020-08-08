a24eb1dd15aaf7c8a759a1f0bdb32f92988c6ccd
Bo Yao <bo@nearprotocol.com>
2020-08-07 17:33:19 -0700

    debug on ci

--- runtime/runtime-params-estimator/emu-cost/ci.sh
+++ runtime/runtime-params-estimator/emu-cost/ci.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 ./build.sh
 export HOST_DIR=${DIR}/../../../..
+ls $HOST_DIR
 docker run \
      --rm --mount type=bind,source=$HOST_DIR,target=/host \
      --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
