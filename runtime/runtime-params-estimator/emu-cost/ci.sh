c792bebfdc2659c7a5fbfa7a4b0f39166da74827
Bo Yao <bo@nearprotocol.com>
2020-08-07 17:49:49 -0700

    fix ci

--- runtime/runtime-params-estimator/emu-cost/ci.sh
+++ runtime/runtime-params-estimator/emu-cost/ci.sh
@@ -3,9 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 ./build.sh
 export HOST_DIR=${DIR}/../../../..
-if [[ -z "${BUILDKITE}" ]]; then
-    srcdir=nearcore
-else
+srcdir=nearcore
+if [[ -n "${BUILDKITE}" ]]; then
     srcdir=runtime-params-estimator-qemu
 fi
 docker run \
