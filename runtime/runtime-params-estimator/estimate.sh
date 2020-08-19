e5d4e93afb8d5b742009e836655d6a99e4b76218
Willem Wyndham <willem@cs.umd.edu>
2020-08-19 17:00:39 -0400

    fix: remove exit from script and rename feature to remove confusion

--- runtime/runtime-params-estimator/estimate.sh
+++ runtime/runtime-params-estimator/estimate.sh
@@ -19,8 +19,6 @@ rm -rf /tmp/data
 
 set -ex
 
-exit 0
-
 cargo run --release --package neard --bin neard -- --home /tmp/data init --chain-id= --test-seed=alice.near --account-id=test.near --fast
 cargo run --release --package genesis-populate --bin genesis-populate -- --additional-accounts-num=200000 --home /tmp/data
 cargo build --release --package runtime-params-estimator $features
