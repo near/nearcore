423de2f42e9b482d76914da20e0da73c424c973c
Bowen Wang <bowen@near.org>
2020-08-21 10:10:16 -0700

    add check chunk

--- test-utils/state-viewer/src/main.rs
+++ test-utils/state-viewer/src/main.rs
@@ -328,12 +328,22 @@ fn view_chain(
     }
 }
 
-fn check_block_existence(store: Arc<Store>, near_config: &NearConfig) {
+fn check_block_chunk_existence(store: Arc<Store>, near_config: &NearConfig) {
     let genesis_height = near_config.genesis.config.genesis_height;
     let mut chain_store = ChainStore::new(store.clone(), genesis_height);
     let head = chain_store.head().unwrap();
     let mut cur_block = chain_store.get_block(&head.last_block_hash).unwrap().clone();
     while cur_block.header().height() > genesis_height {
+        for chunk_header in cur_block.chunks().iter() {
+            if chunk_header.height_included == cur_block.header().height() {
+                if let Err(_) = chain_store.get_chunk(&chunk_header.hash) {
+                    panic!(
+                        "chunk {:?} cannot be found in storage, last block {:?}",
+                        chunk_header, cur_block
+                    );
+                }
+            }
+        }
         cur_block = match chain_store.get_block(cur_block.header().prev_hash()) {
             Ok(b) => b.clone(),
             Err(_) => {
@@ -521,7 +531,7 @@ fn main() {
             view_chain(store, &near_config, height, view_block, view_chunks);
         }
         ("check_block", Some(_)) => {
-            check_block_existence(store, &near_config);
+            check_block_chunk_existence(store, &near_config);
         }
         (_, _) => unreachable!(),
     }
