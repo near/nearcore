6cfde1f14a451f8084be620031440aa6eb300a41
Willem Wyndham <willem@cs.umd.edu>
2020-08-25 11:52:59 -0400

    fix (runtime): Finishing adding wasmtime support (#3074)
    * feat: add CLI arg for wasmtime
    * fix(params): add wasmtime_default feature for vm logic.
    * feat: add compile-only flag
    * feat: add script to run compiler measurement tests
    * fix: actually use lightbeam
    * feat: make lightbeam a feature
    * fix: allow files to fail
    * fix: remove exit from script and rename feature to remove confusion
    * fix: remove floats and use ratio instead
    * feat: Use glob to find all wasm files and improve setup.sh
    * fix: Update runtime version and CHANGELOG
    * fix: actually pass vm-kind and add directory for test files
    
    Co-authored-by: willem <willem@near.org>
    Co-authored-by: Nikolay Igotti <olonho@users.noreply.github.com>

---
+++ runtime/runtime-params-estimator/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd test-binaries || exit 1;
+
+ensure_repo () {
+  if [[ -e $1 ]]; then
+    cd $1;
+    git pull
+  else
+    git clone --depth=1 https://github.com/near/$1;
+  fi
+}
+
+ensure_repo near-sdk-rs;
+ensure_repo core-contracts;
