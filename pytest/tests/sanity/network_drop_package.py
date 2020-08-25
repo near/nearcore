5950aa2483cf5cd65c2c317700d550019265e89d
Marcelo Fornet <mfornet94@gmail.com>
2020-08-25 18:42:35 -0400

    Update filtered messages

--- pytest/tests/sanity/network_drop_package.py
+++ pytest/tests/sanity/network_drop_package.py
@@ -17,8 +17,6 @@ height = Value('i', 0)
 # Ratio of message that are dropped to simulate bad network performance
 DROP_RATIO = 0.05
 
-DONT_FILTER = ['Handshake', 'HandshakeFailure', 'LastEdge', 'Sync', 'RequestUpdateNonce', 'ResponseUpdateNonce']
-
 class Handler(ProxyHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,7 +38,7 @@ class Handler(ProxyHandler):
                     logging.info(f'SUCCESS DROP={self.dropped} TOTAL={self.total}')
                     success.value = 1
 
-        drop = random.random() < DROP_RATIO and msg.enum not in DONT_FILTER
+        drop = random.random() < DROP_RATIO and 'Handshake' not in msg.enum
 
         if drop:
             self.dropped += 1
