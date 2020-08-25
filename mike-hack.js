09f8f1d4b69bc989e3bebe71fb0c9ec2626f3d9b
Mike Purvis <mikedotexe@gmail.com>
2020-08-24 21:59:19 -0700

    increase max gas genesis values

--- mike-hack.js
+++ mike-hack.js
@@ -7,9 +7,9 @@ let genesis = JSON.parse(fs.readFileSync(genesisPath));
 
 config.consensus.min_block_production_delay.nanos = 6660000;
 
-genesis.runtime_config.wasm_config.limit_config.max_total_prepaid_gas = 1000000000000000;
-genesis.runtime_config.wasm_config.limit_config.max_gas_burnt = 1000000000000000;
-genesis.runtime_config.wasm_config.limit_config.max_gas_burnt_view = 1000000000000000;
+genesis.runtime_config.wasm_config.limit_config.max_total_prepaid_gas = 10000000000000000000;
+genesis.runtime_config.wasm_config.limit_config.max_gas_burnt = 10000000000000000000;
+genesis.runtime_config.wasm_config.limit_config.max_gas_burnt_view = 10000000000000000000;
 
 fs.writeFileSync(configPath, JSON.stringify(config));
 fs.writeFileSync(genesisPath, JSON.stringify(genesis));
