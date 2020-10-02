3d26a9ff692080384031855ad0e68e9f86e6ccd7
Bohdan Khorolets <bogdan@khorolets.com>
2020-10-02 14:52:26 +0300

    Add warning about skipping the adding Receipt in ExecutionOutcome

--- chain/indexer/src/streamer/streamer.rs
+++ chain/indexer/src/streamer/streamer.rs
@@ -9,7 +9,7 @@ use futures::stream::StreamExt;
 use rocksdb::DB;
 use tokio::sync::mpsc;
 use tokio::time;
-use tracing::{debug, info};
+use tracing::{debug, info, warn};
 
 use near_client;
 pub use near_primitives::hash::CryptoHash;
@@ -278,7 +278,15 @@ async fn fetch_outcomes(
     for outcome in outcomes {
         let receipt = match fetch_receipt_by_id(&client, outcome.id).await {
             Ok(res) => res,
-            Err(_) => None,
+            Err(e) => {
+                warn!(
+                    target: INDEXER,
+                    "Unable to fetch Receipt with id {}. Skipping it in ExecutionOutcome \n {:#?}",
+                    outcome.id,
+                    e,
+                );
+                None
+            }
         };
         outcomes_with_receipts.insert(
             outcome.id,
