b78683a554e80f087b4abcce2249441a25847021
Bowen Wang <bowen@near.org>
2020-10-23 15:54:31 -0700

    address comment

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -69,7 +69,7 @@ impl ProtocolVersionRange {
 /// #[cfg(feature = "evm")]
 /// EVM
 /// ```
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 pub enum ProtocolFeature {}
 
 /// Current latest stable version of the protocol.
@@ -80,19 +80,30 @@ pub const PROTOCOL_VERSION: ProtocolVersion = 40;
 #[cfg(feature = "nightly_protocol")]
 pub const PROTOCOL_VERSION: ProtocolVersion = 40;
 
-/// Map of feature to the minimal protocol version that introduces the feature. We can determine
-/// whether to apply the new feature by comparing the current protocol version of the network to
-/// `PROTOCOL_FEATURES_TO_VERSION_MAPPING[feature]`.
-#[cfg(not(feature = "nightly_protocol"))]
 lazy_static! {
-    static ref PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> = vec![
+    static ref STABLE_PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> = vec![
         /* add mapping here */
     ].into_iter().collect();
 }
 
+#[cfg(not(feature = "nightly_protocol"))]
+lazy_static! {
+    /// Map of feature to the minimal protocol version that introduces the feature. We can determine
+    /// whether to apply the new feature by comparing the current protocol version of the network to
+    /// `PROTOCOL_FEATURES_TO_VERSION_MAPPING[feature]`.
+    static ref PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> =
+        STABLE_PROTOCOL_FEATURES_TO_VERSION_MAPPING.clone();
+}
+
 #[cfg(feature = "nightly_protocol")]
 lazy_static! {
-    static ref PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> = vec![
-        /* add mapping here */
-    ].into_iter().collect();
+    static ref PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> = {
+        let nightly_protocol_features_to_version_mapping: HashMap<ProtocolFeature, ProtocolVersion> = vec![
+            /* add mapping here */
+        ].into_iter().collect();
+        for (stable_protocol_feature, stable_protocol_version) in STABLE_PROTOCOL_FEATURES_TO_VERSION_MAPPING.iter() {
+            assert!(nightly_protocol_features_to_version_mapping[stable_protocol_feature] >= *stable_protocol_version);
+        }
+        nightly_protocol_features_to_version_mapping
+    };
 }
