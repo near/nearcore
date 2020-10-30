091ce4f6cf20fd2a6357727d211069420c7cdd5d
Alex Kouprin <kpr@nearprotocol.com>
2020-10-30 12:52:44 +0000

    init eth contracts

--- pytest/tests/bridge/simple.py
+++ pytest/tests/bridge/simple.py
@@ -5,9 +5,8 @@ sys.path.append('lib')
 from cluster import start_cluster
 from bridge import start_ganache, start_bridge
 
-#ganache = start_ganache()
-bridge = start_bridge()
 nodes = start_cluster(2, 0, 4, None, [], {})
+ganache = start_ganache()
 
 time.sleep(3)
 status = nodes[0].get_status()
@@ -15,7 +14,22 @@ print(status)
 status = nodes[1].get_status()
 print(status)
 
-bridge.init_near_contracts()
+bridge = start_bridge()
 
-while True:
-    pass
+print('=== BRIDGE IS STARTED')
+status = nodes[0].get_status()
+print(status)
+status = nodes[1].get_status()
+print(status)
+
+print('=== SENDING ETH TO NEAR')
+bridge.transfer_eth2near('0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200',
+                         'rainbow_bridge_eth_on_near_prover',
+                         'rainbow_bridge_eth_on_near_prover',
+                         1000)
+print('=== SENDING NEAR TO ETH')
+bridge.transfer_near2eth('rainbow_bridge_eth_on_near_prover',
+                         '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200',
+                         1)
+# TODO check balance
+print('EPIC')
