5815a64b28e83a13d4eb8d99d8a9a55f9e42e748
Alex Kouprin <kpr@nearprotocol.com>
2020-11-09 12:24:42 +0000

    use js adapter + upds

--- pytest/tests/bridge/simple.py
+++ pytest/tests/bridge/simple.py
@@ -8,7 +8,7 @@ from bridge import start_ganache, start_bridge
 nodes = start_cluster(2, 0, 4, None, [], {})
 ganache = start_ganache()
 
-time.sleep(3)
+time.sleep(2)
 status = nodes[0].get_status()
 print(status)
 status = nodes[1].get_status()
@@ -17,21 +17,22 @@ print(status)
 bridge = start_bridge()
 
 print('=== BRIDGE IS STARTED')
-status = nodes[0].get_status()
-print(status)
-status = nodes[1].get_status()
-print(status)
+balance_before = bridge.get_eth_balance('0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200')
 
-print('=== SENDING ETH TO NEAR')
+print('=== BALANCE BEFORE', balance_before)
+print('=== SENDING 1000 ETH TO NEAR')
 bridge.transfer_eth2near('0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200',
                          'rainbow_bridge_eth_on_near_prover',
                          'rainbow_bridge_eth_on_near_prover',
                          1000)
-print('=== SENDING NEAR TO ETH')
+
+balance_after = bridge.get_eth_balance('0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200')
+print('=== BALANCE AFTER', balance_after)
+assert balance_after + 1000 == balance_before
+
+# TODO check NEAR balance
+# TODO send NEAR to ETH
+#print('=== SENDING NEAR TO ETH')
 #bridge.transfer_near2eth('rainbow_bridge_eth_on_near_prover',
 #                         '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200',
 #                         1)
-# TODO check balance
-print('EPIC')
-while True:
-    pass
