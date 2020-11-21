8a47367065f9741235807ff2d520a6531e8411f2
Alex Kouprin <kpr@nearprotocol.com>
2020-11-21 13:15:27 +0000

    feat(testing): Bridge Nayduck basic integration (#3535)

---
+++ pytest/lib/bridge_helpers/write_config.js
@@ -0,0 +1,17 @@
+const { RainbowConfig } = require('rainbow-bridge-utils')
+
+RainbowConfig.setParam('near-node-url', 'http://0.0.0.0:3040')
+RainbowConfig.setParam('near-network-id', 'local')
+RainbowConfig.setParam('near-master-account', 'test0')
+RainbowConfig.setParam(
+  'near-master-sk',
+  'ed25519:3KyUucjyGk1L58AJBB6Rf6EZFqmpTSSKG7KKsptMvpJLDBiZmAkU4dR1HzNS6531yZ2cR5PxnTM7NLVvSfJjZPh7'
+//'ed25519:3D4YudUQRE39Lc4JHghuB5WM8kbgDDa34mnrEP5DdTApVH81af7e2dWgNPEaiQfdJnZq1CNPp5im4Rg5b733oiMP'
+)
+RainbowConfig.setParam('eth-node-url', 'ws://localhost:9545')
+RainbowConfig.setParam(
+  'eth-master-sk',
+  '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
+)
+RainbowConfig.setParam('near-client-validate-ethash', 'false')
+RainbowConfig.saveConfig()
