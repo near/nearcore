c9aebec83929ebe987633634f5406c0a20e7b0bd
Bowen Wang <bowen@near.org>
2020-12-10 19:51:42 -0800

    fix(test): fix test_inflation

--- chain/network/src/peer_manager.rs
+++ chain/network/src/peer_manager.rs
@@ -1121,7 +1121,6 @@ impl Actor for PeerManagerActor {
         if let Some(server_addr) = self.config.addr {
             // TODO: for now crashes if server didn't start.
 
-            println!("peer manager actor started");
             let pending_incoming_connections_counter =
                 self.pending_incoming_connections_counter.clone();
             let peer_counter = self.peer_counter.clone();
