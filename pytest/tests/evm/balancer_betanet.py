cda6fe38041646a4cb7e939ffb96490bb85bee2c
Bo Yao <bo@nearprotocol.com>
2020-12-18 15:16:56 -0800

    keys, test on betanet

---
+++ pytest/tests/evm/balancer_betanet.py
@@ -0,0 +1,21 @@
+from rc import bash
+import sys
+
+p = bash('''
+git clone https://github.com/near/balancer-core.git
+cd balancer-core
+npm i
+npm i -g near-cli
+
+# test account exists, so do not create them again
+# env NEAR_ENV=betanet near evm-dev-init balancer-core-test.betanet 10
+
+mkdir -p ~/.near-credentials/betanet
+cp ../tests/evm/keys/*.json ~/.near-credentials/betanet/
+
+env NEAR_MASTER_ACCOUNT=balancer-core-test.betanet truffle migrate --network near_betanet --reset
+env NEAR_MASTER_ACCOUNT=balancer-core-test.betanet truffle test --network near_betanet
+''')
+
+print(p.stdout)
+print(p.stderr, file=sys.stderr)
--- pytest/tests/evm/balancer_localnode.py
+++ pytest/tests/evm/balancer_localnode.py
@@ -1,14 +1,17 @@
-print('test')
-
 from rc import bash
+import sys
+
 p = bash('''
-set -x
-pwd
-ls -al
-cd
-ls -al
-cat ~/.bashrc
-echo $PATH
+git clone https://github.com/near/balancer-core.git
+cd balancer-core
+npm i
+npm i -g near-cli
+
+# env NEAR_ENV=localnet near evm-dev-init balancer-core-test.near 10
+
+# env NEAR_MASTER_ACCOUNT=balancer-core-test.near truffle migrate --network near_localnet --reset
+# env NEAR_MASTER_ACCOUNT=balancer-core-test.near truffle test --network near_localnet
 ''')
+
 print(p.stdout)
-print(p.stderr)
+print(p.stderr, file=sys.stderr)
