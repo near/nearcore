b1643c0c0c91a947aeb78e6f19e03f1df0302b87
Nikolay Igotti <igotti@gmail.com>
2020-12-22 17:43:00 +0300

    Removed loop in script

--- runtime/runtime-params-estimator/emu-cost/io_cost.sh
+++ runtime/runtime-params-estimator/emu-cost/io_cost.sh
@@ -9,20 +9,17 @@ echo $mark | tee -a $log
 # cargo build --release --package neard
 # cargo build --release --package genesis-populate
 
-#for acc in 10
-for acc in 1000000
-do
-  echo "Using $acc accounts..." | tee -a $log
-  dir=/tmp/data$acc
-  rm -rf $dir
-  $base/../../target/release/neard  --home $dir init --chain-id= \
+
+acc=1000000
+echo "Using $acc accounts..." | tee -a $log
+dir=/tmp/data$acc
+rm -rf $dir
+$base/../../target/release/neard  --home $dir init --chain-id= \
     --test-seed=alice.near --account-id=test.near --fast
-  $base/emu-cost/counter_plugin/qemu-x86_64  -d plugin -cpu Westmere-v1 -R 8G \
+$base/emu-cost/counter_plugin/qemu-x86_64  -d plugin -cpu Westmere-v1 -R 8G \
       -plugin file=$base/emu-cost/counter_plugin/libcounter.so,arg="started",arg="on_every_close"  \
       $base/../../target/release/genesis-populate --home $dir --additional-accounts-num $acc 2>&1 | tee -a $log
-  rm -rf $dir
-  # tail -1
-done
+rm -rf $dir
 
 # brew install feedgnuplot
 # seq 20 | awk '{print $1, A, $1*$1}' | feedgnuplot --domain --dataid --lines --points  --title "Instructions to IO"  --unset grid --terminal 'dumb 80,40' --exit
