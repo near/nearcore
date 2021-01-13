da56c3024c8f8c954a1c73460280570b9169c3fb
Illia Polosukhin <illia@nearprotocol.com>
2021-01-13 20:39:54 +0800

    Fix a crash inside meta-tx processing. (#3801)

--- runtime/near-evm-runner/src/meta_parsing.rs
+++ runtime/near-evm-runner/src/meta_parsing.rs
@@ -450,7 +450,10 @@ pub fn prepare_meta_call_args(
     args: &[u8],
 ) -> Result<(RawU256, Vec<u8>)> {
     let mut bytes = Vec::new();
-    let method_arg_start = method_def.find('(').unwrap();
+    let method_arg_start = match method_def.find('(') {
+        Some(index) => index,
+        None => return Err(VMLogicError::EvmError(EvmError::InvalidMetaTransactionMethodName)),
+    };
     let arguments = "Arguments".to_string() + &method_def[method_arg_start..];
     // Note: method_def is like "adopt(uint256 petId,PetObj petObj)PetObj(string name,address owner)",
     // MUST have no space after `,`. EIP-712 requires hashStruct start by packing the typeHash,
