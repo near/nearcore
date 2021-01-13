6a3110fe9cd1d7ed8295f1d90402c7f7c57ec178
Willem Wyndham <willem@cs.umd.edu>
2021-01-13 13:05:42 -0500

    fix: move cost_counting to required feature set

--- runtime/runtime-params-estimator/src/testbed.rs
+++ runtime/runtime-params-estimator/src/testbed.rs
@@ -94,6 +94,7 @@ impl RuntimeTestbed {
             cache: Some(Arc::new(StoreCompiledContractCache { store: tries.get_store() })),
             #[cfg(feature = "protocol_feature_evm")]
             evm_chain_id: near_chain_configs::TEST_EVM_CHAIN_ID,
+            #[cfg(feature = "costs_counting")]
             profile: None,
         };
         Self {
