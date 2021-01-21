35b96c017b64b5076160732d429b175afa84f60f
Willem Wyndham <willem@cs.umd.edu>
2021-01-21 14:25:42 -0500

    fix: fix warnings with vm-logic

--- runtime/near-vm-logic/src/gas_counter.rs
+++ runtime/near-vm-logic/src/gas_counter.rs
@@ -42,6 +42,7 @@ pub struct GasCounter {
     is_view: bool,
     ext_costs_config: ExtCostsConfig,
     /// Where to store profile data, if needed.
+    #[allow(dead_code)]
     profile: Option<ProfileData>,
 }
 
@@ -133,7 +134,7 @@ impl GasCounter {
 
     #[cfg(not(feature = "costs_counting"))]
     #[inline]
-    fn update_profile_host(&mut self, cost: ExtCosts, _value: u64) {}
+    fn update_profile_host(&mut self, _cost: ExtCosts, _value: u64) {}
 
     #[cfg(feature = "costs_counting")]
     #[inline]
@@ -146,7 +147,7 @@ impl GasCounter {
 
     #[cfg(not(feature = "costs_counting"))]
     #[inline]
-    fn update_profile_action(&mut self, action: ActionCosts, _value: u64) {}
+    fn update_profile_action(&mut self, _action: ActionCosts, _value: u64) {}
 
     pub fn pay_wasm_gas(&mut self, value: u64) -> Result<()> {
         self.deduct_gas(value, value)
