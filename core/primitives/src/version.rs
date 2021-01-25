fc25f286e0b59fd19f59595324cd2ae05e961f34
EgorKulikov <egor@egork.net>
2021-01-25 11:47:45 +0100

    Update core/primitives/src/version.rs
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -99,7 +99,7 @@ pub const PROTOCOL_VERSION: ProtocolVersion = 42;
 
 /// Current latest nightly version of the protocol.
 #[cfg(feature = "nightly_protocol")]
-pub const PROTOCOL_VERSION: ProtocolVersion = 48;
+pub const PROTOCOL_VERSION: ProtocolVersion = 47;
 
 lazy_static! {
     static ref STABLE_PROTOCOL_FEATURES_TO_VERSION_MAPPING: HashMap<ProtocolFeature, ProtocolVersion> = vec![
