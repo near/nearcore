6ac5a2cc3464efde45e2e4bd3a0e67f329399efe
EgorKulikov <egor@egork.net>
2021-02-03 17:18:29 +0100

    Change receipt id to use current block hash (#3821)
    * Change receipt id to use current block hash
    
    * Fixed confusion between last_block_hash and prev_block_hash
    Changed the way data ids and action hashes are calculated
    
    * Formatting
    Version in json
    
    * Reverting changes to data id as it was calculated properly before
    
    * Correct stable version
    
    * Update core/primitives/src/version.rs
    
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>
    
    * Update core/primitives/src/version.rs
    
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>
    
    * Test with fork
    
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>
    Co-authored-by: Evgeny Kuzyakov <ek@nearprotocol.com>

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -52,6 +52,9 @@ pub const DELETE_KEY_STORAGE_USAGE_PROTOCOL_VERSION: ProtocolVersion = 40;
 
 pub const SHARD_CHUNK_HEADER_UPGRADE_VERSION: ProtocolVersion = 41;
 
+/// Updates the way receipt ID is constructed to use current block hash instead of last block hash
+pub const CREATE_RECEIPT_ID_SWITCH_TO_CURRENT_BLOCK_VERSION: ProtocolVersion = 42;
+
 pub struct ProtocolVersionRange {
     lower: ProtocolVersion,
     upper: Option<ProtocolVersion>,
@@ -88,7 +91,7 @@ pub enum ProtocolFeature {
 
 /// Current latest stable version of the protocol.
 #[cfg(not(feature = "nightly_protocol"))]
-pub const PROTOCOL_VERSION: ProtocolVersion = 41;
+pub const PROTOCOL_VERSION: ProtocolVersion = 42;
 
 /// Current latest nightly version of the protocol.
 #[cfg(feature = "nightly_protocol")]
