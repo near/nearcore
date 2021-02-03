68db29ee96c1fec6e096c9e232b4d477414eadb5
Frank Braun <frank@cryptogroup.net>
2021-02-03 10:02:56 +0000

    just measure actual ERC-721 tranfer in benchmark

--- runtime/near-evm-runner/benches/benches/cryptozombies.rs
+++ runtime/near-evm-runner/benches/benches/cryptozombies.rs
@@ -53,19 +53,18 @@ pub fn transfer_erc721(c: &mut Criterion) {
     let outcome = evm_call(&mut ext, "alice", "deploy_code", bytes, false).0.unwrap();
     let contract_id = address_from_arr(&outcome.return_data.as_value().unwrap());
 
+    // create zombie
     let (input, _decoder) = cryptozombies::functions::create_random_zombie::call("test");
     let args_create = encode_call_function_args(contract_id, input);
+    evm_call(&mut ext, "alice", "call", args_create.clone(), false).0.unwrap();
 
     let alice_address = near_evm_runner::utils::near_account_id_to_evm_address(&alice_account());
     let bob_address = near_evm_runner::utils::near_account_id_to_evm_address(&bob_account());
 
     c.bench_function("transfer_erc721", |b| {
         b.iter(|| {
-            // create zombie
-            let mut ext_copy = ext.clone();
-            evm_call(&mut ext_copy, "alice", "call", args_create.clone(), false).0.unwrap();
-
             // transfer ERC-721 zombie token
+            let mut ext_copy = ext.clone();
             let (input, _decoder) = cryptozombies::functions::transfer_from::call(
                 alice_address,
                 bob_address,
