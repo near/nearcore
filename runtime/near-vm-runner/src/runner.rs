5e8e23385b6926c3d64049ba2f543797b9fbb0e7
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-02-18 13:51:53 +0300

    refactor: reduce duplication in the vm running code

--- runtime/near-vm-runner/src/runner.rs
+++ runtime/near-vm-runner/src/runner.rs
@@ -32,24 +32,9 @@ pub fn run<'a>(
     cache: Option<&'a dyn CompiledContractCache>,
     #[cfg(feature = "costs_counting")] profile: Option<&ProfileData>,
 ) -> (Option<VMOutcome>, Option<VMError>) {
-    #[cfg(feature = "costs_counting")]
-    if let Some(profile) = profile {
-        return run_vm_profiled(
-            code_hash,
-            code,
-            method_name,
-            ext,
-            context,
-            wasm_config,
-            fees_config,
-            promise_results,
-            VMKind::default(),
-            profile.clone(),
-            current_protocol_version,
-            cache,
-        );
-    }
-    run_vm(
+    #[cfg(not(feature = "costs_counting"))]
+    let profile = None;
+    run_vm_impl(
         code_hash,
         code,
         method_name,
@@ -59,6 +44,7 @@ pub fn run<'a>(
         fees_config,
         promise_results,
         VMKind::default(),
+        profile,
         current_protocol_version,
         cache,
     )
@@ -76,67 +62,20 @@ pub fn run_vm<'a>(
     current_protocol_version: ProtocolVersion,
     cache: Option<&'a dyn CompiledContractCache>,
 ) -> (Option<VMOutcome>, Option<VMError>) {
-    #[cfg(feature = "wasmer0_vm")]
-    use crate::wasmer_runner::run_wasmer;
-
-    #[cfg(feature = "wasmtime_vm")]
-    use crate::wasmtime_runner::wasmtime_runner::run_wasmtime;
-
-    #[cfg(feature = "wasmer1_vm")]
-    use crate::wasmer1_runner::run_wasmer1;
-
-    match vm_kind {
-        #[cfg(feature = "wasmer0_vm")]
-        VMKind::Wasmer0 => run_wasmer(
-            code_hash,
-            code,
-            method_name,
-            ext,
-            context,
-            wasm_config,
-            fees_config,
-            promise_results,
-            None,
-            current_protocol_version,
-            cache,
-        ),
-        #[cfg(not(feature = "wasmer0_vm"))]
-        VMKind::Wasmer0 => panic!("Wasmer0 is not supported, compile with '--features wasmer0_vm'"),
-        #[cfg(feature = "wasmtime_vm")]
-        VMKind::Wasmtime => run_wasmtime(
-            code_hash,
-            code,
-            method_name,
-            ext,
-            context,
-            wasm_config,
-            fees_config,
-            promise_results,
-            None,
-            current_protocol_version,
-            cache,
-        ),
-        #[cfg(not(feature = "wasmtime_vm"))]
-        VMKind::Wasmtime => {
-            panic!("Wasmtime is not supported, compile with '--features wasmtime_vm'")
-        }
-        #[cfg(feature = "wasmer1_vm")]
-        VMKind::Wasmer1 => run_wasmer1(
-            code_hash,
-            code,
-            method_name,
-            ext,
-            context,
-            wasm_config,
-            fees_config,
-            promise_results,
-            None,
-            current_protocol_version,
-            cache,
-        ),
-        #[cfg(not(feature = "wasmer1_vm"))]
-        VMKind::Wasmer1 => panic!("Wasmer1 is not supported, compile with '--features wasmer1_vm'"),
-    }
+    run_vm_impl(
+        code_hash,
+        code,
+        method_name,
+        ext,
+        context,
+        wasm_config,
+        fees_config,
+        promise_results,
+        vm_kind,
+        None,
+        current_protocol_version,
+        cache,
+    )
 }
 
 pub fn run_vm_profiled<'a>(
@@ -153,6 +92,36 @@ pub fn run_vm_profiled<'a>(
     current_protocol_version: ProtocolVersion,
     cache: Option<&'a dyn CompiledContractCache>,
 ) -> (Option<VMOutcome>, Option<VMError>) {
+    run_vm_impl(
+        code_hash,
+        code,
+        method_name,
+        ext,
+        context,
+        wasm_config,
+        fees_config,
+        promise_results,
+        vm_kind,
+        Some(&profile),
+        current_protocol_version,
+        cache,
+    )
+}
+
+fn run_vm_impl(
+    code_hash: Vec<u8>,
+    code: &[u8],
+    method_name: &[u8],
+    ext: &mut dyn External,
+    context: VMContext,
+    wasm_config: &VMConfig,
+    fees_config: &RuntimeFeesConfig,
+    promise_results: &[PromiseResult],
+    vm_kind: VMKind,
+    profile: Option<&ProfileData>,
+    current_protocol_version: ProtocolVersion,
+    cache: Option<&dyn CompiledContractCache>,
+) -> (Option<VMOutcome>, Option<VMError>) {
     #[cfg(feature = "wasmer0_vm")]
     use crate::wasmer_runner::run_wasmer;
 
@@ -161,6 +130,7 @@ pub fn run_vm_profiled<'a>(
 
     #[cfg(feature = "wasmer1_vm")]
     use crate::wasmer1_runner::run_wasmer1;
+
     let (outcome, error) = match vm_kind {
         #[cfg(feature = "wasmer0_vm")]
         VMKind::Wasmer0 => run_wasmer(
@@ -172,7 +142,7 @@ pub fn run_vm_profiled<'a>(
             wasm_config,
             fees_config,
             promise_results,
-            Some(profile.clone()),
+            profile.cloned(),
             current_protocol_version,
             cache,
         ),
@@ -188,7 +158,7 @@ pub fn run_vm_profiled<'a>(
             wasm_config,
             fees_config,
             promise_results,
-            Some(profile.clone()),
+            profile.cloned(),
             current_protocol_version,
             cache,
         ),
@@ -206,17 +176,18 @@ pub fn run_vm_profiled<'a>(
             wasm_config,
             fees_config,
             promise_results,
-            Some(profile.clone()),
+            profile.cloned(),
             current_protocol_version,
             cache,
         ),
         #[cfg(not(feature = "wasmer1_vm"))]
         VMKind::Wasmer1 => panic!("Wasmer1 is not supported, compile with '--features wasmer1_vm'"),
     };
-    match &outcome {
-        Some(VMOutcome { burnt_gas, .. }) => profile.set_burnt_gas(*burnt_gas),
-        _ => (),
-    };
+    if let Some(profile) = profile {
+        if let Some(VMOutcome { burnt_gas, .. }) = &outcome {
+            profile.set_burnt_gas(*burnt_gas)
+        }
+    }
     (outcome, error)
 }
 /// `precompile` compiles WASM contract to a VM specific format and stores result into the `cache`.
