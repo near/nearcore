bef3d4f41c5a8db78094c885c18cec53cf87999e
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-02-23 17:07:20 -0800

    Switch to cross-platform filesize

--- runtime/runtime-params-estimator/test-contract/build.sh
+++ runtime/runtime-params-estimator/test-contract/build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 set -ex
 
+function filesize
+{
+  local file=$1
+  size=`stat -c%s $file 2>/dev/null` # linux
+  if [ $? -eq 0 ]; then
+    echo $size
+    return 0
+  fi
+
+  size=`stat -f%z $file 2>/dev/null` # macos
+  if [ $? -eq 0 ]; then
+    echo $size
+    return 0
+  fi
+
+  exit 1
+}
+
 # First, measure the size of the file without payload.
 rm -rf target
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-# bare_wasm=$(stat -c%s target/wasm32-unknown-unknown/release/test_contract.wasm)
-bare_wasm=$(stat -f%z target/wasm32-unknown-unknown/release/test_contract.wasm)
+bare_wasm=$(filesize target/wasm32-unknown-unknown/release/test_contract.wasm)
 echo ${bare_wasm}
 
 # Generate several files of various sizes. We will compile these files into the Wasm binary to
@@ -32,8 +49,7 @@ rm ./res/payload
 
 rm -rf target
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release --features nightly_protocol_features
-# bare_wasm=$(stat -c%s target/wasm32-unknown-unknown/release/test_contract.wasm)
-bare_wasm=$(stat -f%z target/wasm32-unknown-unknown/release/test_contract.wasm)
+bare_wasm=$(filesize target/wasm32-unknown-unknown/release/test_contract.wasm)
 echo ${bare_wasm}
 
 # Generate several files of various sizes. We will compile these files into the Wasm binary to
