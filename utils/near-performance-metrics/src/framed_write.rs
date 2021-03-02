ea359dd01f696bbcc441ef36d9fc0756a78ac877
Piotr Mikulski <piotr@near.org>
2021-03-02 15:09:30 -0800

    cargo fmt

--- utils/near-performance-metrics/src/framed_write.rs
+++ utils/near-performance-metrics/src/framed_write.rs
@@ -201,8 +201,6 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-
-
 use std::cell::RefCell;
 use std::marker::PhantomData;
 use std::ops::DerefMut;
