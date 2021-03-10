b05da04f9b5aa1b4c32ca1e8af10be2cd240d84d
Nikolay Igotti <olonho@users.noreply.github.com>
2021-03-10 22:44:54 +0300

    Concurrent preloader for contracts (#3985)
    To minimize deserialization delays we introduce mechanism of pipelined constructing
    WASM modules before execution. We concurrent deserialize multiple contracts,
    and use those deserialized contracts for execution in single thread. So no concurrency
    issues should happen, and execution is fully deterministic.

---
+++ runtime/near-vm-runner/src/tests/contract_preload.rs
@@ -0,0 +1,197 @@
+use crate::{run_vm, ContractCallPrepareRequest, ContractCaller, VMError};
+use near_primitives::hash::hash;
+use near_primitives::runtime::fees::RuntimeFeesConfig;
+use near_vm_logic::{ProtocolVersion, VMConfig, VMContext, VMKind, VMOutcome};
+
+use near_primitives::borsh::BorshSerialize;
+use near_primitives::types::CompiledContractCache;
+use near_vm_errors::VMError::FunctionCallError;
+use near_vm_logic::mocks::mock_external::MockedExternal;
+use near_vm_logic::profile::ProfileData;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
+
+const TEST_CONTRACT_1: &'static [u8] = include_bytes!("../../tests/res/test_contract_rs.wasm");
+const TEST_CONTRACT_2: &'static [u8] = include_bytes!("../../tests/res/test_contract_ts.wasm");
+
+fn default_vm_context() -> VMContext {
+    return VMContext {
+        current_account_id: "alice".to_string(),
+        signer_account_id: "bob".to_string(),
+        signer_account_pk: vec![0, 1, 2],
+        predecessor_account_id: "carol".to_string(),
+        input: vec![],
+        block_index: 1,
+        block_timestamp: 1586796191203000000,
+        account_balance: 10u128.pow(25),
+        account_locked_balance: 0,
+        storage_usage: 100,
+        attached_deposit: 0,
+        prepaid_gas: 10u64.pow(18),
+        random_seed: vec![0, 1, 2],
+        is_view: false,
+        output_data_receivers: vec![],
+        epoch_height: 1,
+    };
+}
+
+#[derive(Default, Clone)]
+pub struct MockCompiledContractCache {
+    store: Arc<Mutex<HashMap<Vec<u8>, Vec<u8>>>>,
+    delay: Duration,
+}
+
+impl MockCompiledContractCache {
+    pub fn new(delay: i32) -> Self {
+        Self {
+            store: Arc::new(Mutex::new(HashMap::new())),
+            delay: Duration::from_millis(delay as u64),
+        }
+    }
+}
+
+impl CompiledContractCache for MockCompiledContractCache {
+    fn put(&self, key: &[u8], value: &[u8]) -> Result<(), std::io::Error> {
+        sleep(self.delay);
+        self.store.lock().unwrap().insert(key.to_vec(), value.to_vec());
+        Ok(())
+    }
+
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, std::io::Error> {
+        sleep(self.delay);
+        let res = self.store.lock().unwrap().get(key).cloned();
+        Ok(res)
+    }
+}
+
+fn test_result(result: (Option<VMOutcome>, Option<VMError>), check_gas: bool) -> (i32, i32) {
+    let mut oks = 0;
+    let mut errs = 0;
+    match result.0 {
+        Some(outcome) => {
+            if check_gas {
+                assert_eq!(outcome.burnt_gas, 11088051921);
+            }
+            oks += 1;
+        }
+        None => {}
+    };
+    match result.1 {
+        Some(err) => match err {
+            FunctionCallError(_) => {
+                errs += 1;
+            }
+            _ => assert!(false, "Unexpected error: {:?}", err),
+        },
+        None => {}
+    }
+    (oks, errs)
+}
+
+fn test_vm_runner(preloaded: bool, vm_kind: VMKind, repeat: i32) {
+    let code1 = TEST_CONTRACT_1;
+    let code2 = TEST_CONTRACT_2;
+    let method_name1 = "log_something";
+    let code1_hash = hash(code1);
+    let code2_hash = hash(code2);
+
+    let mut fake_external = MockedExternal::new();
+
+    let context = default_vm_context();
+    let vm_config = VMConfig::default();
+    let cache: Option<Arc<dyn CompiledContractCache>> =
+        Some(Arc::new(MockCompiledContractCache::new(0)));
+    let fees = RuntimeFeesConfig::default();
+    let promise_results = vec![];
+    let profile_data = ProfileData::new_disabled();
+    let mut oks = 0;
+    let mut errs = 0;
+
+    if preloaded {
+        let mut requests = Vec::new();
+        let mut caller = ContractCaller::new(4);
+        for _ in 0..repeat {
+            requests.push(ContractCallPrepareRequest {
+                code_hash: code1_hash,
+                code: code1.to_vec(),
+                vm_config: vm_config.clone(),
+                cache: cache.clone(),
+            });
+            requests.push(ContractCallPrepareRequest {
+                code_hash: code2_hash,
+                code: code2.to_vec(),
+                vm_config: vm_config.clone(),
+                cache: cache.clone(),
+            });
+        }
+        let calls = caller.preload(requests, vm_kind);
+        for prepared in &calls {
+            let result = caller.run_preloaded(
+                prepared,
+                method_name1,
+                &mut fake_external,
+                context.clone(),
+                &vm_config,
+                &fees,
+                &promise_results,
+                ProtocolVersion::MAX,
+                profile_data.clone(),
+            );
+            let (ok, err) = test_result(result, true);
+            oks += ok;
+            errs += err;
+        }
+    } else {
+        for _ in 0..repeat {
+            let result1 = run_vm(
+                code1_hash.try_to_vec().unwrap(),
+                code1,
+                method_name1,
+                &mut fake_external,
+                context.clone(),
+                &vm_config,
+                &fees,
+                &promise_results,
+                vm_kind,
+                ProtocolVersion::MAX,
+                cache.as_deref(),
+                profile_data.clone(),
+            );
+            let (ok, err) = test_result(result1, false);
+            oks += ok;
+            errs += err;
+            let result2 = run_vm(
+                code2_hash.try_to_vec().unwrap(),
+                code2,
+                method_name1,
+                &mut fake_external,
+                context.clone(),
+                &vm_config,
+                &fees,
+                &promise_results,
+                vm_kind,
+                ProtocolVersion::MAX,
+                cache.as_deref(),
+                profile_data.clone(),
+            );
+            let (ok, err) = test_result(result2, false);
+            oks += ok;
+            errs += err;
+        }
+    }
+
+    assert_eq!(oks, repeat);
+    assert_eq!(errs, repeat);
+}
+
+#[test]
+pub fn test_vm_run_sequential() {
+    test_vm_runner(false, VMKind::Wasmer0, 100);
+}
+
+#[test]
+pub fn test_vm_run_preloaded() {
+    test_vm_runner(true, VMKind::Wasmer0, 100);
+}
