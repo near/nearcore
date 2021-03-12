5a01db5b5d8e3299dc68296db76329c356421cec
Bohdan Khorolets <b@khorolets.com>
2021-03-12 21:22:52 +0200

    refactor(jsonrpc): Refactor view_client structured errors according to removal of BlockMissing for GetBlockError, GetChunkError, QueryError (#4079)
    According to recent removal of `BlockMissing` we had to refactor some structures to be up to date

--- chain/jsonrpc-primitives/src/types/query.rs
+++ chain/jsonrpc-primitives/src/types/query.rs
@@ -52,6 +52,8 @@ pub enum RpcQueryError {
         block_height: near_primitives::types::BlockHeight,
         block_hash: near_primitives::hash::CryptoHash,
     },
+    #[error("Block either has never been observed on the node or has been garbage collected: {block_reference:?}")]
+    UnknownBlock { block_reference: near_primitives::types::BlockReference },
     #[error("The node reached its limits. Try again later. More details: {error_message}")]
     InternalError { error_message: String },
     // NOTE: Currently, the underlying errors are too broad, and while we tried to handle
@@ -164,6 +166,9 @@ impl From<near_client_primitives::types::QueryError> for RpcQueryError {
             near_client_primitives::types::QueryError::NoSyncedBlocks => Self::NoSyncedBlocks,
             near_client_primitives::types::QueryError::UnavailableShard { requested_shard_id } => {
                 Self::UnavailableShard { requested_shard_id }
+            }
+            near_client_primitives::types::QueryError::UnknownBlock { block_reference } => {
+                Self::UnknownBlock { block_reference }
             }
             near_client_primitives::types::QueryError::InvalidAccount {
                 requested_account_id,
