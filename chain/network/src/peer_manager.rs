8f5f70be58c180e106a9956a70edf280f0a01d77
Piotr Mikulski <piotr@near.org>
2021-03-13 17:18:24 -0800

    undo changes

--- chain/network/src/peer_manager.rs
+++ chain/network/src/peer_manager.rs
@@ -1485,7 +1485,14 @@ impl Handler<NetworkRequests> for PeerManagerActor {
             }
             NetworkRequests::Sync { peer_id, sync_data } => {
                 // Process edges and add new edges to the routing table. Also broadcast new edges.
-                let SyncData { edges, accounts } = sync_data;
+                let SyncData { mut edges, accounts } = sync_data;
+                edges.retain(|edge| {
+                    match self.routing_table.get_edge(edge.peer0.clone(), edge.peer1.clone()) {
+                        // only consider edges with bigger nonce
+                        Some(cur_edge) => cur_edge.nonce < edge.nonce,
+                        None => true,
+                    }
+                });
 
                 self.edge_verifier_pool.send(EdgeList(edges, self.routing_table.get_edges_info_shared(), self.routing_table.edges_to_add_sender.clone()))
                     .into_actor(self)
