c4ca3886dc0c82923d35a4802519408dfee9994b
Piotr Mikulski <piotr@near.org>
2021-03-15 02:32:26 -0700

    add test

--- chain/network/src/ibf_set.rs
+++ chain/network/src/ibf_set.rs
@@ -95,172 +95,42 @@ where
         }
         true
     }
-
-    /*
-    fn add_edges(&mut self, items: &HashSet<T>) {
-        for e in items.iter() {
-            self.add_edge(e);
-        }
-    }
-
-    fn add_edges2(&mut self, items: &Vec<T>) {
-        for e in items.iter() {
-            self.add_edge(e);
-        }
-    }
-
-    fn split(&mut self, items: &Vec<u64>) -> (Vec<SlotMapId>, Vec<u64>) {
-        let mut local_id = Vec::new();
-        let mut remote_hash = Vec::new();
-        for e in items {
-            if let Some(e) = self.h2e.get(e) {
-                local_id.push(e.clone());
-            } else {
-                remote_hash.push(*e);
-            }
-        }
-        (local_id, remote_hash)
-    }
-
-    fn get_edges(&mut self, items: &Vec<u64>) -> Vec<SlotMapId> {
-        let mut local_id = Vec::new();
-        for e in items {
-            if let Some(e) = self.h2e.get(e) {
-                local_id.push(e.clone());
-            }
-        }
-        local_id
-    }
-    */
 }
 
 #[cfg(test)]
 mod test {
-    /*
-        use crate::blt_graph::BltGraph;
-        use crate::graph::Graph;
-        use std::cmp::max;
-        use std::iter::FromIterator;
-        use std::time::Instant;
+    use crate::ibf_peer_set::SlotMapId;
+    use crate::ibf_set::IbfSet;
 
-        #[test]
-        fn test_999000_550_small_diff() {
-            test_general(999000, 500);
+    #[test]
+    fn test_ibf_set() {
+        let mut a = IbfSet::<u64>::new();
+        let mut b = IbfSet::<u64>::new();
+
+        for i in 0..10000 {
+            a.add_edge(&(i as u64), (i + 1000000) as SlotMapId);
         }
-
-        #[test]
-        fn test_0_500000_full_sync() {
-            test_general(0, 500000);
+        for i in 0..10 {
+            a.remove_edge(&(i as u64));
         }
+        for i in 0..10000 {
+            b.add_edge(&(i + 100 as u64), (i + 2000000) as SlotMapId);
+        }
+        for i in 10..=17 {
+            let mut ibf1 = a.get_ibf(i);
+            let ibf2 = a.get_ibf(i);
+            ibf1.merge(&ibf2);
+            let (mut res, diff) = ibf1.try_recover();
+            assert_eq!(0, diff);
+            assert_eq!(100 - 10, res.len());
 
-        fn test_general(common: usize, one_side: usize) {
-            let mut g = Graph::new(1000000);
-            let mut rng = rand::thread_rng();
-            g.add_random_edges(common, &mut rng);
-
-            let mut a = g.clone();
-            a.add_random_edges(one_side, &mut rng);
-            let mut b = g.clone();
-            b.add_random_edges(one_side, &mut rng);
-
-            let mut blt_a = BltGraph::new();
-            let mut blt_b = BltGraph::new();
-
-            blt_a.add_edges(&a.edges);
-            blt_b.add_edges(&b.edges);
-
-            let start = Instant::now();
-
-            // We have created Ibf for each power of 2.
-            // We are going to exchange Ibf starting from 2^10, because it takes less than 1ms to compute
-            // and the size of meta data is just 8kb.
-
-            // Let's say Alice and Bob each have their own graph.
-            // They graphs differ, Alice may have some edges Bob doesn't, and vice versa.
-            // Both of them want to have the same graph with added edges from the other party.
-            for i in 10..20 {
-                // Each item is one round trip
-                if blt_b.blts[i].capacity > max(a.edges.len(), b.edges.len()) / 10 {
-                    // 2 round trips
-
-                    // Alice send list of all hashes to Bob
-                    let hashes_from_a: Vec<u64> = Vec::from_iter(blt_a.h2e.keys().cloned().into_iter());
-
-                    // Bob responds with edges for Alice and with that he needs
-                    let (_, edges_b_needs) = blt_b.split(&hashes_from_a);
-                    // and edges he knows about
-                    let hashes_from_b: Vec<u64> = Vec::from_iter(blt_b.h2e.keys().cloned().into_iter());
-
-                    // Alice knows which edges she is missing.
-                    let (_, edges_a_needs) = blt_a.split(&hashes_from_b);
-
-                    //  Alice sends edges which Bob is missing
-                    let edges_for_b = blt_a.get_edges(&edges_b_needs);
-
-                    // Bob applies edges
-                    b.add_edges(&edges_for_b);
-                    blt_b.add_edges2(&edges_for_b);
-
-                    // Bob returns edges Alice needs
-                    let edges_for_a = blt_b.get_edges(&edges_a_needs);
-
-                    // Alice adds edges from Bob
-                    a.add_edges(&edges_for_a);
-                    blt_a.add_edges2(&edges_for_a);
-
-                    println!(
-                        "FULL SYNC {} capacity: {} total: {}ms a.edges: {} b.edges: {} added_a: {} added_b: {}",
-                        i,
-                        blt_b.blts[i].capacity,
-                        start.elapsed().as_millis(),
-                        a.edges.len(),
-                        b.edges.len(),
-                        edges_for_a.len(),
-                        edges_for_b.len()
-                    );
-                    break;
-                }
-                // 1 round trip
-
-                // Alice asks BOB to give her Bob's Ibf[i] and gets response.
-                let mut bob_response = blt_b.blts[i].clone();
-
-                // Alice merges the result
-                bob_response.merge(&blt_a.blts[i]);
-
-                // Alice tries to recover the result, she sees list of edges which are in the difference
-                let (res, success) = bob_response.try_recover();
-
-                let (edges_for_b, edges_a_needs) = blt_a.split(&res);
-
-                // Alice sends edges Bob doesn't have and he adds them
-                b.add_edges(&edges_for_b);
-                blt_b.add_edges2(&edges_for_b);
-
-                // Bob sends list to Alice list of edges she asked for and she adds them
-                let edges_for_a = blt_b.get_edges(&edges_a_needs);
-                a.add_edges(&edges_for_a);
-                blt_a.add_edges2(&edges_for_a);
-
-                println!(
-                    "{} capacity: {} {} {} total: {}ms a.edges: {} b.edges: {} added_a: {} added_b: {}",
-                    i,
-                    blt_b.blts[i].capacity,
-                    res.len(),
-                    success,
-                    start.elapsed().as_millis(),
-                    a.edges.len(),
-                    b.edges.len(),
-                    edges_for_a.len(),
-                    edges_for_b.len()
-                );
-                if success {
-                    // All edges were recovered
-                    break;
-                }
+            for x in 0..333 {
+                res.push(x + 33333333);
             }
-            assert_eq!(a.edges.len(), b.edges.len());
-            assert!(a.edges.len() as f64 >= common as f64 + 1.9 * (one_side as f64));
+            assert_eq!(100 - 10, a.get_edges_by_hashes(&res).len());
+
+            assert_eq!(100 - 10, a.get_edges_by_hashes2(&res).0.len());
+            assert_eq!(333 + 10, a.get_edges_by_hashes2(&res).1.len());
         }
-    */
+    }
 }
