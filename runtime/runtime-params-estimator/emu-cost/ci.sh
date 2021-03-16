40f46930d77a859489db7e1d5774a41c8c6f8921
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-03-16 17:03:45 +0300

    fix: unbreak params estimator CI (#4118)

--- runtime/runtime-params-estimator/emu-cost/ci.sh
+++ runtime/runtime-params-estimator/emu-cost/ci.sh
@@ -12,7 +12,9 @@ docker run \
      --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
      -i -t rust-emu \
      /bin/bash -c "
+set -ex
 cd /host/${srcdir}/runtime/runtime-params-estimator
+$(cd test-contract && ./build.sh)
 cargo run --release --package neard --bin neard -- --home /tmp/data init --chain-id= --test-seed=alice.near --account-id=test.near --fast
 cargo run --release --package genesis-populate --bin genesis-populate -- --additional-accounts-num=200000 --home /tmp/data
 cargo build --release --package runtime-params-estimator --features required
