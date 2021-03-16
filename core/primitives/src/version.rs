178a75b3c2e0488e1f9019757b34d56b0cf2e2f0
Michael Birch <michael@near.org>
2021-03-16 10:26:08 -0400

    Update PROTOCOL_VERSION

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -101,7 +101,7 @@ pub enum ProtocolFeature {
 
 /// Current latest stable version of the protocol.
 #[cfg(not(feature = "nightly_protocol"))]
-pub const PROTOCOL_VERSION: ProtocolVersion = 42;
+pub const PROTOCOL_VERSION: ProtocolVersion = 43;
 
 /// Current latest nightly version of the protocol.
 #[cfg(feature = "nightly_protocol")]
