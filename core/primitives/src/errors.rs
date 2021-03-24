4088a98a5a6ba46b0580a5649779c6d9f31c2e8e
Aleksandr Logunov <the.aleksandr.logunov@gmail.com>
2021-03-24 15:20:29 +0300

    Set transaction size limit (#4107)
    * Implement first version of validating summary tx size
    
    * Add feature flag for limiting tx size
    
    * Fix formatting
    
    * Add test checking exceeding tx size limit
    
    * Add new test
    
    * fix
    
    * Improve computation of transaction size
    
    * Add check for tx with valid size
    
    * Run cargo fmt
    
    * Add TODO
    
    * Introduce size field for SignedTransaction
    
    * Fix error in test
    
    * Minor fix
    
    Co-authored-by: Bowen Wang <bowen@near.org>

--- core/primitives/src/errors.rs
+++ core/primitives/src/errors.rs
@@ -143,6 +143,9 @@ pub enum InvalidTxError {
     Expired,
     /// An error occurred while validating actions of a Transaction.
     ActionsValidation(ActionsValidationError),
+    /// The size of serialized transaction exceeded the limit.
+    #[cfg(feature = "protocol_feature_tx_size_limit")]
+    TransactionSizeExceeded { size: u64, limit: u64 },
 }
 
 #[derive(
@@ -441,6 +444,12 @@ impl Display for InvalidTxError {
                 write!(f, "Transaction actions validation error: {}", error)
             }
             InvalidTxError::NonceTooLarge { tx_nonce, upper_bound } => { write!(f, "Transaction nonce {} must be smaller than the access key nonce upper bound {}", tx_nonce, upper_bound) }
+            #[cfg(feature = "protocol_feature_tx_size_limit")]
+            InvalidTxError::TransactionSizeExceeded { size, limit } => write!(
+                f,
+                "Size of serialized transaction {} exceeded the limit {}",
+                size, limit
+            ),
         }
     }
 }
