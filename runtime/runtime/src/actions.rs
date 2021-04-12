6829eb33fbda16205c6bb3503d164c51b67be22b
Nikolay Igotti <igotti@gmail.com>
2021-04-12 09:54:52 +0300

    Do not actually precompile contract on chain.

--- runtime/runtime/src/actions.rs
+++ runtime/runtime/src/actions.rs
@@ -440,7 +440,7 @@ pub(crate) fn action_deploy_contract(
     set_code(state_update, account_id.clone(), &code);
     // Precompile the contract and store result (compiled code or error) in the database.
     #[allow(unused_must_use)]
-    {
+    if false {
         precompile_contract(&code, &apply_state.config.wasm_config, apply_state.cache.as_deref());
     }
     Ok(())
