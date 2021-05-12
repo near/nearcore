5afd65b576add0109957bb44d2e6edb63182c531
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-05-12 14:45:13 +0300

    Profile by default (#4185)
    Having costs breakdown (how much gas was spend for each operation type)
    is helpful for both increasing reliability (as we can eyeball if the
    costs "make sense") and developer friendliness (as devs can understand
    where all the gas is going).
    
    Currently this is hidden behind a feature flag, but I would love to
    argue that we should just always enable it.
    
    Performance cost seems to be negligible: we heap-allocate an array once
    per apply state, and actual cost increasse are just counter increments,
    they should be dwarfed by the host functions themselves.
    
    Correctness-wise, the code is semi-sensitive. It is executed by runtime,
    but doesn't affect the results. So it's enough if it doesn't panic.
    
    # Test Plan
    
    All of existing tests now exercise costs councting code. An additional unit-test is added to smoke-test that costs profiling can't panic due to overflow.

--- core/primitives-core/src/profile.rs
+++ core/primitives-core/src/profile.rs
@@ -1,33 +1,21 @@
+use std::rc::Rc;
 use std::{cell::Cell, fmt};
 
 use crate::config::{ActionCosts, ExtCosts};
 use crate::types::Gas;
 
 /// Data for total gas burnt (index 0), and then each external cost and action
-#[allow(dead_code)]
 type DataArray = [Cell<u64>; ProfileData::LEN];
 
 /// Profile of gas consumption.
 #[derive(Clone)]
 pub struct ProfileData {
-    repr: Repr,
+    data: Rc<DataArray>,
 }
-
-#[derive(Clone)]
-enum Repr {
-    #[cfg(feature = "costs_counting")]
-    Enabled {
-        data: std::rc::Rc<DataArray>,
-    },
-    Disabled,
-}
-
-#[cfg(not(feature = "costs_counting"))]
-const _ASSERT_NO_OP_IF_COUNTING_DISABLED: [(); 0] = [(); std::mem::size_of::<ProfileData>()];
 
 impl Default for ProfileData {
     fn default() -> ProfileData {
-        ProfileData::new_disabled()
+        ProfileData::new()
     }
 }
 
@@ -37,26 +25,12 @@ impl ProfileData {
     const LEN: usize = 1 + ActionCosts::count() + ExtCosts::count();
 
     #[inline]
-    #[cfg(feature = "costs_counting")]
-    pub fn new_enabled() -> Self {
+    pub fn new() -> Self {
         // We must manually promote to a constant for array literal to work.
         const ZERO: Cell<u64> = Cell::new(0);
 
-        let data = std::rc::Rc::new([ZERO; ProfileData::LEN]);
-        let repr = Repr::Enabled { data };
-        ProfileData { repr }
-    }
-
-    #[inline]
-    #[cfg(not(feature = "costs_counting"))]
-    pub fn new_enabled() -> Self {
-        Self::new_disabled()
-    }
-
-    #[inline]
-    pub fn new_disabled() -> Self {
-        let repr = Repr::Disabled;
-        ProfileData { repr }
+        let data = Rc::new([ZERO; ProfileData::LEN]);
+        ProfileData { data }
     }
 
     #[inline]
@@ -69,24 +43,14 @@ impl ProfileData {
     }
     #[inline]
     fn add_val(&self, index: usize, value: u64) {
-        self.with_slot(index, |slot| {
-            let old = slot.get();
-            slot.set(old + value);
-        })
+        let slot = &self.data[index];
+        let old = slot.get();
+        slot.set(old.saturating_add(value));
     }
 
-    #[inline]
-    fn with_slot(&self, index: usize, f: impl FnOnce(&Cell<u64>)) {
-        match &self.repr {
-            #[cfg(feature = "costs_counting")]
-            Repr::Enabled { data } => f(&data[index]),
-            Repr::Disabled => drop((index, f)),
-        }
-    }
     fn read(&self, index: usize) -> u64 {
-        let mut res = 0;
-        self.with_slot(index, |slot| res = slot.get());
-        res
+        let slot = &self.data[index];
+        slot.get()
     }
 
     pub fn all_gas(&self) -> Gas {
@@ -120,23 +84,13 @@ impl ProfileData {
         self.all_gas() - self.host_gas() - self.action_gas()
     }
 
-    pub fn unaccounted_gas(&self) -> u64 {
-        self.all_gas() - self.wasm_gas() - self.action_gas() - self.host_gas()
-    }
-
     pub fn set_burnt_gas(&self, burnt_gas: u64) {
-        self.with_slot(0, |slot| slot.set(burnt_gas));
+        let slot = &self.data[0];
+        slot.set(burnt_gas)
     }
 }
 
 impl fmt::Debug for ProfileData {
-    #[cfg(not(feature = "costs_counting"))]
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "ERROR: cost_counting feature is not enabled in near-primitives-core, cannot print profile data")?;
-        Ok(())
-    }
-
-    #[cfg(feature = "costs_counting")]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use num_rational::Ratio;
         let all_gas = self.all_gas();
@@ -168,15 +122,6 @@ impl fmt::Debug for ProfileData {
             wasm_gas,
             Ratio::new(wasm_gas * 100, all_gas).to_integer()
         )?;
-        let unaccounted_gas = self.unaccounted_gas();
-        if unaccounted_gas > 0 {
-            writeln!(
-                f,
-                "Unaccounted: {} [{}% total]",
-                unaccounted_gas,
-                Ratio::new(unaccounted_gas * 100, all_gas).to_integer()
-            )?;
-        }
         writeln!(f, "------ Host functions --------")?;
         for e in 0..ExtCosts::count() {
             let d = self.get_ext_cost(e);
@@ -214,24 +159,31 @@ mod test {
     use super::*;
     #[test]
     fn test_profile_all_gas() {
-        let profile_data = ProfileData::new_enabled();
+        let profile_data = ProfileData::new();
         profile_data.set_burnt_gas(42);
-        #[cfg(not(feature = "costs_counting"))]
-        assert_eq!(profile_data.all_gas(), 0);
-        #[cfg(feature = "costs_counting")]
         assert_eq!(profile_data.all_gas(), 42);
     }
 
     #[test]
     fn test_profile_data_debug() {
-        let profile_data = ProfileData::new_enabled();
+        let profile_data = ProfileData::new();
         profile_data.set_burnt_gas(42);
         println!("{:#?}", &profile_data);
     }
 
     #[test]
     fn test_profile_data_debug_no_data() {
-        let profile_data = ProfileData::new_enabled();
+        let profile_data = ProfileData::new();
         println!("{:#?}", &profile_data);
+    }
+
+    #[test]
+    fn test_no_panic_on_overflow() {
+        let profile_data = ProfileData::new();
+        profile_data.add_action_cost(ActionCosts::function_call, u64::MAX);
+        profile_data.add_action_cost(ActionCosts::function_call, u64::MAX);
+
+        let res = profile_data.get_action_cost(ActionCosts::function_call as usize);
+        assert_eq!(res, u64::MAX);
     }
 }
