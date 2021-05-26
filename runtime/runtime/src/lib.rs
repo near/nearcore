ae5a7094bd4370ec4ad0d42fd1dd2be2f4641282
Longarithm <the.aleksandr.logunov@gmail.com>
2021-05-26 16:40:51 +0300

    add comment

--- runtime/runtime/src/lib.rs
+++ runtime/runtime/src/lib.rs
@@ -1116,6 +1116,7 @@ impl Runtime {
             == protocol_version
             && migration_flags.is_first_block_with_chunk_of_version
         {
+            // We restore receipts only on mainnet so result will be None on other chains.
             migration_data.restored_receipts.get(&0u64).cloned().unwrap_or_default()
         } else {
             vec![]
