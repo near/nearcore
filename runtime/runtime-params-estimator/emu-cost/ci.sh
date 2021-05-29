79bef3d93b7d762b76335cc49adbfdd9b0bd8424
Miraculous Owonubi <omiraculous@gmail.com>
2021-05-29 19:42:04 +0100

    first attempt at fixing runtime-params-estimator

--- runtime/runtime-params-estimator/emu-cost/ci.sh
+++ runtime/runtime-params-estimator/emu-cost/ci.sh
@@ -14,7 +14,7 @@ docker run \
      /usr/bin/env bash -c "
 set -ex
 cd /host/${srcdir}/runtime/runtime-params-estimator
-$(cd test-contract && ./build.sh)
+: "'$(cd test-contract && ./build.sh)'"
 cargo run --release --package neard --bin neard -- --home /tmp/data init --chain-id= --test-seed=alice.near --account-id=test.near --fast
 cargo run --release --package genesis-populate --bin genesis-populate -- --additional-accounts-num=200000 --home /tmp/data
 cargo build --release --package runtime-params-estimator --features required
