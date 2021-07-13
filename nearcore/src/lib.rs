2e4fb7ced4319c3e9eb13ab1c572f8012d38c43e
Aleksandr Logunov <alex.logunov@near.org>
2021-07-13 18:26:58 +0300

    Store restored receipts in the storage (#4444)
    **Idea**
    ~~Return `restored_receipts` from `apply_transactions` to indicate that some receipts were added in runtime due to migration, so they will not be lost in chain and could be accessed by Indexer.~~
    
    Create new database migration to add receipts missing in store directly into it.
    
    **Testing**
    ~~Extend existing `protocol_feature_restore_receipts_after_fix_tests` by running `get_receipt` method.~~
    
    - setup archival mainnet node
    - run `./target/release/neard --home ~/.near init`, observe migration:
    ```
    $ ./target/debug/neard --home ~/.near run
    Jul 09 12:57:59.635  INFO near: Version: 1.19.1, Build: c8e49bb5, Latest Protocol: 45
    Jul 09 12:57:59.760  INFO near: Opening store database at "/home/Aleksandr1/.near/data"
    apply_store_migrations
    Jul 09 12:57:59.781  INFO near: Migrate DB from version 22 to 23
    ```
    - stop node, access store using separate script and ensure that receipts are saved in it
    ```
    for receipt in receipts {
        eprintln!("{}", receipt.get_hash());
        chain_store.get_receipt(&receipt.get_hash()).unwrap().unwrap();
    }
    ```

--- nearcore/src/lib.rs
+++ nearcore/src/lib.rs
@@ -19,7 +19,9 @@ use near_store::{create_store, Store};
 use near_telemetry::TelemetryActor;
 
 pub use crate::config::{init_configs, load_config, load_test_config, NearConfig, NEAR_BASE};
-use crate::migrations::{migrate_12_to_13, migrate_18_to_19, migrate_19_to_20, migrate_22_to_23};
+use crate::migrations::{
+    migrate_12_to_13, migrate_18_to_19, migrate_19_to_20, migrate_22_to_23, migrate_23_to_24,
+};
 pub use crate::runtime::NightshadeRuntime;
 use near_store::migrations::{
     fill_col_outcomes_by_hash, fill_col_transaction_refcount, get_store_version, migrate_10_to_11,
@@ -214,6 +216,10 @@ pub fn apply_store_migrations(path: &String, near_config: &NearConfig) {
     if db_version <= 22 {
         info!(target: "near", "Migrate DB from version 22 to 23");
         migrate_22_to_23(&path, &near_config);
+    }
+    if db_version <= 23 {
+        info!(target: "near", "Migrate DB from version 23 to 24");
+        migrate_23_to_24(&path, &near_config);
     }
     #[cfg(feature = "nightly_protocol")]
     {
