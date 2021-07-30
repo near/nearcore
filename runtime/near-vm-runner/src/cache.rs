43683f3f61841a02575255e506ada51901f7e317
Longarithm <the.aleksandr.logunov@gmail.com>
2021-07-31 00:43:10 +0300

    fix

--- runtime/near-vm-runner/src/cache.rs
+++ runtime/near-vm-runner/src/cache.rs
@@ -141,10 +141,10 @@ pub mod wasmer0_cache {
         Ok(module)
     }
 
-    fn _deserialize_get_artifact() -> Artifact {
+    fn _deserialize_get_artifact() -> Result<Artifact, CacheError> {
         let _span = tracing::debug_span!(target: "vm", "deserialize_wasmer").entered();
         Artifact::deserialize(serialized_artifact.as_slice())
-            .map_err(|_e| CacheError::DeserializationError)?
+            .map_err(|_e| CacheError::DeserializationError)
     }
 
     fn _deserialize_compiler_for_backend() -> Box<dyn Compiler> {
@@ -173,7 +173,7 @@ pub mod wasmer0_cache {
             CacheRecord::Error(err) => return Ok(Err(err)),
             CacheRecord::Code(code) => code,
         };
-        let artifact = _deserialize_get_artifact();
+        let artifact = _deserialize_get_artifact()?;
         unsafe {
             let compiler = _deserialize_compiler_for_backend();
             _deserialize_load_cache_with(artifact, compiler.as_ref())
