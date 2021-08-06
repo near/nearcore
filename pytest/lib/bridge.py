b24e7a44323bf36e01bb6ba6deba33bc624cae9f
Michal Nazarewicz <mina86@mina86.com>
2021-08-06 20:02:39 +0200

    pytest: bridge: make sure rainbow-bridge repository is up to date (#4648)
    Issue: https://github.com/near/nearcore/issues/4618

--- pytest/lib/bridge.py
+++ pytest/lib/bridge.py
@@ -305,8 +305,7 @@ class RainbowBridge:
         self.bridge_dir = self.config['bridge_dir']
         self.config_dir = self.config['config_dir']
         self.cli_dir = os.path.join(self.bridge_dir, 'cli')
-        if not os.path.exists(self.bridge_dir):
-            self.git_clone_install()
+        self.git_clone_install()
         # TODO(aurora-is-near/rainbow-bridge#150): Rainbow Bridge has some tools
         # written in Go [yuck] and the tests assemble and run all the things
         # related to the bridge which means that it requires Go compiler.  See
@@ -369,10 +368,17 @@ class RainbowBridge:
         assert_success(created['result']['status']['SuccessValue'])
 
     def git_clone_install(self):
-        logger.info('No rainbow-bridge repo found, cloning...')
-        args = ('git', 'clone', '--recurse-submodules',
-                str(self.config['bridge_repo']), str(self.bridge_dir))
-        subprocess.check_call(args)
+        if os.path.exists(self.bridge_dir):
+            logger.info('No rainbow-bridge repo found, cloning...')
+            subprocess.check_call(
+                ('git', 'clone', '--recurse-submodules',
+                 str(self.config['bridge_repo']), str(self.bridge_dir)))
+        else:
+            logger.info('rainbow-bridge repo found, updating...')
+            subprocess.check_call(('git', 'remote', 'update', '-p', 'origin'),
+                                  cwd=self.bridge_dir)
+            subprocess.check_call(('git', 'reset', '--hard', 'origin/master'),
+                                  cwd=self.bridge_dir)
         assert_success(subprocess.check_output(['yarn'], cwd=self.bridge_dir))
         ethash_dir = os.path.join(self.bridge_dir, 'eth2near/ethashproof')
         assert_success(subprocess.check_output(['/bin/sh', 'build.sh'], cwd=ethash_dir))
