12d9a80b1f402e85ef625c2df971d54490756a50
Longarithm <the.aleksandr.logunov@gmail.com>
2021-08-17 17:59:36 +0300

    remove accidentially added files

--- runner.sh
+++
@@ -1,1 +0,0 @@
-/host/runtime/runtime-params-estimator/emu-cost/counter_plugin/qemu-x86_64 -cpu Westmere-v1 -plugin file=/host/runtime/runtime-params-estimator/emu-cost/counter_plugin/libcounter.so $@
