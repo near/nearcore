b9652cba54fe56097b265fbd58b68f649724f65b
posvyatokum <posvyatokum@edu.hse.ru>
2021-08-26 20:53:54 +0300

    re scenario out of output

--- pytest/tests/runtime/fuzz.py
+++ pytest/tests/runtime/fuzz.py
@@ -1,12 +1,24 @@
 import subprocess
 import os
+import re
+import sys
 
 def run_fuzz():
     env = os.environ.copy()
     env["RUSTC_BOOTSTRAP"] = "1"
-    subprocess.check_call(['cargo', 'fuzz', 'run', 'runtime-fuzzer',
-                           '--', '-len_control=0' '-prefer_small=0'],
-                           env=env, cwd='../test-utils/runtime-tester/fuzz')
+    fuzzing = subprocess.Popen(('cargo', 'fuzz', 'run', 'runtime-fuzzer',
+                                '--', '-len_control=0' '-prefer_small=0', '-max_len=4000000'),
+                               env=env, cwd='../test-utils/runtime-tester/fuzz',
+                               stderr=subprocess.PIPE)
+    out, err = fuzzing.communicate()
+    print(err, file=sys.stderr)
+
+    re_expr = re.compile(r'Output of `std::fmt::Debug`:((.|\n)*)Reproduce', re.MULTILINE)
+    res = re_expr.search(err.decode('utf-8'))
+
+    if res:
+        print(res.group(1))
+
 
 def main():
     run_fuzz()
