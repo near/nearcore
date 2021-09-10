0e911e85e141a0655b9117efc5fd0a2439148406
posvyatokum <posvyatokum@edu.hse.ru>
2021-09-10 15:47:16 +0300

    adding commet to lib.rs

--- test-utils/runtime-tester/src/lib.rs
+++ test-utils/runtime-tester/src/lib.rs
@@ -2,6 +2,7 @@
 //! You can create Scenario in rust code or have it in a JSON file.
 //! Scenario::run executes scenario, keeping track of different metrics.
 //! So far, the only metric is how much time block production takes.
+//! To create scenario in rust code you can use ScenarioBuilder which covers basic scenarios.
 //! fuzzing provides Arbitrary trait for Scenario, thus enabling creating random scenarios.
 pub mod fuzzing;
 pub mod run_test;
