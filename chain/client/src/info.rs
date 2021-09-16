3080cf7d305c8e1d25d09d378c7f0ab1f841ec27
EgorKulikov <egor@egork.net>
2021-09-17 00:20:10 +0200

    Apply state parts in separate actor (#4714)
    Instead of waiting for state parts to be applied we now do this in separate thread

--- chain/client/src/info.rs
+++ chain/client/src/info.rs
@@ -247,7 +247,8 @@ fn display_sync_status(
                         match shard_status.status {
                             ShardSyncStatus::StateDownloadHeader => format!("header"),
                             ShardSyncStatus::StateDownloadParts => format!("parts"),
-                            ShardSyncStatus::StateDownloadFinalize => format!("finalization"),
+                            ShardSyncStatus::StateDownloadScheduling => format!("scheduling"),
+                            ShardSyncStatus::StateDownloadApplying => format!("applying"),
                             ShardSyncStatus::StateDownloadComplete => format!("download complete"),
                             ShardSyncStatus::StateSplit => format!("split"),
                         }
--- integration-tests/tests/client/challenges.rs
+++ integration-tests/tests/client/challenges.rs
@@ -683,7 +683,8 @@ fn test_challenge_in_different_epoch() {
     for block in fork_blocks {
         let height = block.header().height();
         let (_, result) = env.clients[0].process_block(block, Provenance::NONE);
-        match env.clients[0].run_catchup(&vec![]) {
+        let f = |_| {};
+        match env.clients[0].run_catchup(&vec![], &f) {
             Ok(accepted_blocks) => {
                 for accepted_block in accepted_blocks {
                     env.clients[0].on_block_accepted(
