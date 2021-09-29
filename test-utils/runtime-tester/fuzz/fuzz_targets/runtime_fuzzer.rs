0cc31675c099e514f03a01860749817d8dc4b100
Michal Nazarewicz <mina86@mina86.com>
2021-09-29 02:10:15 +0200

    runtime-tester: fix runtime_fuzzer test
    This fixes the following error:
    
    error[E0308]: mismatched types
      --> fuzz_targets/runtime_fuzzer.rs:10:12
    10 |     if let Err(e) = runtime_stats {
       |            ^^^^^^   ------------- this expression has type `ScenarioResult<RuntimeStats, near_client_primitives::types::Error>`
       |            |
       |            expected struct `ScenarioResult`, found enum `Result`
    error[E0599]: no method named `unwrap` found for struct `ScenarioResult` in the current scope
      --> fuzz_targets/runtime_fuzzer.rs:14:35
    14 |         let stats = runtime_stats.unwrap();
       |                                   ^^^^^^ method not found in `ScenarioResult<RuntimeStats, near_client_primitives::types::Error>`
    
    It can be reproduced by running runtime/fuzz.py pytest.
    
    Issue: https://github.com/near/nearcore/issues/4835

--- test-utils/runtime-tester/fuzz/fuzz_targets/runtime_fuzzer.rs
+++ test-utils/runtime-tester/fuzz/fuzz_targets/runtime_fuzzer.rs
@@ -6,20 +6,20 @@ use std::time::Duration;
 
 fuzz_target!(|scenario: Scenario| {
     let filename = "failed_scenario.json";
-    let runtime_stats = scenario.run();
-    if let Err(e) = runtime_stats {
-        serde_json::to_writer(&File::create(filename).unwrap(), &scenario).unwrap();
-        panic!("Bad scenario: {}, Error: {}", filename, e);
-    } else {
-        let stats = runtime_stats.unwrap();
-        for block_stats in stats.blocks_stats {
-            if block_stats.block_production_time > Duration::from_secs(1) {
-                serde_json::to_writer(&File::create(filename).unwrap(), &scenario).unwrap();
-                panic!(
-                    "Bad scenario: {}, block at height {} was produced in {:?}",
-                    filename, block_stats.height, block_stats.block_production_time
-                );
-            }
+    let stats = match scenario.run().result {
+        Err(e) => {
+            serde_json::to_writer(&File::create(filename).unwrap(), &scenario).unwrap();
+            panic!("Bad scenario: {}, Error: {}", filename, e);
+        },
+        Ok(stats) => stats
+    };
+    for block_stats in stats.blocks_stats {
+        if block_stats.block_production_time > Duration::from_secs(1) {
+            serde_json::to_writer(&File::create(filename).unwrap(), &scenario).unwrap();
+            panic!(
+                "Bad scenario: {}, block at height {} was produced in {:?}",
+                filename, block_stats.height, block_stats.block_production_time
+            );
         }
     }
 });
