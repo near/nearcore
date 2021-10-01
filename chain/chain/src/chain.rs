9ae3d5658f9131843fc28ab135ce51c28e028b0b
Nikolay Kurtov <nikolay.kurtov@near.org>
2021-10-01 12:26:57 +0200

    Include block height in the logging message.

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -3451,8 +3451,10 @@ impl<'a> ChainUpdate<'a> {
     where
         F: FnMut(ChallengeBody) -> (),
     {
-        let _span = tracing::debug_span!(target: "chain", "Process block", "{} at {}", block.hash(), block.header().height()).entered();
-        debug!(target: "chain", "Approvals: {}, me: {:?}", block.header().num_approvals(), me);
+        let _span =
+            tracing::debug_span!(target: "chain", "Process block", "#{}", block.header().height())
+                .entered();
+        debug!(target: "chain", "Block {}, approvals: {}, me: {:?}", block.hash(), block.header().num_approvals(), me);
 
         // Check that we know the epoch of the block before we try to get the header
         // (so that a block from unknown epoch doesn't get marked as an orphan)
