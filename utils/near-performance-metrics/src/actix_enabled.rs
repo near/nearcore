0aa7a4f92a5768773c087d561b8c8e073bc0657b
marcelo-gonzalez <37846884+marcelo-gonzalez@users.noreply.github.com>
2021-10-07 16:44:31 -0700

    refactor: Don't explicitly pass file/line to actix::run_later()/spawn() (#4950)
    Getting this info from std::panic::Location::caller() removes
    a bunch of boilerplate in the callers.
    
    Test plan
    ---------
    No behavior change, so manually checking that log output looks the same.

--- utils/near-performance-metrics/src/actix_enabled.rs
+++ utils/near-performance-metrics/src/actix_enabled.rs
@@ -1,4 +1,5 @@
 use log::warn;
+use std::panic::Location;
 use std::time::Duration;
 use std::time::Instant;
 
@@ -6,27 +7,26 @@ use crate::stats_enabled::{get_thread_stats_logger, MyFuture, REF_COUNTER, SLOW_
 
 use near_rust_allocator_proxy::allocator::get_tid;
 
-pub fn spawn<F>(class_name: &'static str, file: &'static str, line: u32, f: F)
+#[track_caller]
+pub fn spawn<F>(class_name: &'static str, f: F)
 where
     F: futures::Future<Output = ()> + 'static,
 {
-    *REF_COUNTER.lock().unwrap().entry((file, line)).or_insert_with(|| 0) += 1;
-    actix::spawn(MyFuture { f, class_name, file, line });
+    let loc = Location::caller();
+    *REF_COUNTER.lock().unwrap().entry((loc.file(), loc.line())).or_insert_with(|| 0) += 1;
+    actix::spawn(MyFuture { f, class_name, file: loc.file(), line: loc.line() });
 }
 
-pub fn run_later<F, A, B>(
-    ctx: &mut B,
-    file: &'static str,
-    line: u32,
-    dur: Duration,
-    f: F,
-) -> actix::SpawnHandle
+#[track_caller]
+pub fn run_later<F, A, B>(ctx: &mut B, dur: Duration, f: F) -> actix::SpawnHandle
 where
     B: actix::AsyncContext<A>,
     A: actix::Actor<Context = B>,
     F: FnOnce(&mut A, &mut A::Context) + 'static,
 {
-    *REF_COUNTER.lock().unwrap().entry((file, line)).or_insert_with(|| 0) += 1;
+    let loc = Location::caller();
+    *REF_COUNTER.lock().unwrap().entry((loc.file(), loc.line())).or_insert_with(|| 0) += 1;
+
     let f2 = move |a: &mut A, b: &mut A::Context| {
         let stat = get_thread_stats_logger();
         let now = Instant::now();
@@ -36,18 +36,18 @@ where
 
         let ended = Instant::now();
         let took = ended - now;
-        stat.lock().unwrap().log("run_later", file, line, took, ended, "");
+        stat.lock().unwrap().log("run_later", loc.file(), loc.line(), took, ended, "");
         if took > SLOW_CALL_THRESHOLD {
             warn!(
                 "Slow function call {}:{} {}:{} took: {}ms",
                 "run_later",
                 get_tid(),
-                file,
-                line,
+                loc.file(),
+                loc.line(),
                 took.as_millis()
             );
         }
-        *REF_COUNTER.lock().unwrap().entry((file, line)).or_insert_with(|| 0) -= 1;
+        *REF_COUNTER.lock().unwrap().entry((loc.file(), loc.line())).or_insert_with(|| 0) -= 1;
     };
     ctx.run_later(dur, f2)
 }
