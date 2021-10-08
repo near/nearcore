923599d79a7f8d88db5ffbab1f9429ca16650b95
Longarithm <the.aleksandr.logunov@gmail.com>
2021-10-08 16:59:45 +0300

    fix protocol version

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -129,7 +129,7 @@ pub enum ProtocolFeature {
 /// Some features (e. g. FixStorageUsage) require that there is at least one epoch with exactly
 /// the corresponding version
 #[cfg(not(feature = "nightly_protocol"))]
-pub const PROTOCOL_VERSION: ProtocolVersion = 49;
+pub const PROTOCOL_VERSION: ProtocolVersion = 48;
 
 /// Current latest nightly version of the protocol.
 #[cfg(feature = "nightly_protocol")]
@@ -153,7 +153,7 @@ impl ProtocolFeature {
             ProtocolFeature::MathExtension => 46,
             ProtocolFeature::RestoreReceiptsAfterFix => 47,
             ProtocolFeature::Wasmer2 => 48,
-            ProtocolFeature::LowerDataReceiptAndEcrecoverBaseCost => 49,
+            ProtocolFeature::LowerDataReceiptAndEcrecoverBaseCost => 48,
 
             // Nightly features
             #[cfg(feature = "protocol_feature_alt_bn128")]
