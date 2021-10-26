60d63a82b9b9e843117171d09bb7f974be8afbda
Bo Yao <bo@nearprotocol.com>
2021-10-26 17:37:49 +0800

    use none for gas call

--- runtime/near-vm-runner/src/imports.rs
+++ runtime/near-vm-runner/src/imports.rs
@@ -47,10 +47,10 @@ macro_rules! wrapped_imports {
                     $(#[cfg(feature = $feature_name)])*
                     pub fn $func( ctx: &mut Ctx, $( $arg_name: $arg_type ),* ) -> VMResult<($( $returns ),*)> {
                         let _span = if stringify!($func) == "gas" {
-                            tracing::trace_span!(target: "host-function", "gas")
+                            None
                         } else {
-                            tracing::debug_span!(target: "host-function", stringify!($func))
-                        }.entered();
+                            Some(tracing::debug_span!(target: "host-function", stringify!($func)).entered())
+                        };
                         let logic: &mut VMLogic<'_> = unsafe { &mut *(ctx.data as *mut VMLogic<'_>) };
                         logic.$func( $( $arg_name, )* )
                     }
@@ -68,10 +68,10 @@ macro_rules! wrapped_imports {
                 $(#[cfg(feature = $feature_name)])*
                 pub fn $func(env: &NearWasmerEnv, $( $arg_name: $arg_type ),* ) -> VMResult<($( $returns ),*)> {
                     let _span = if stringify!($func) == "gas" {
-                        tracing::trace_span!(target: "host-function", "gas")
+                        None
                     } else {
-                        tracing::debug_span!(target: "host-function", stringify!($func))
-                    }.entered();
+                        Some(tracing::debug_span!(target: "host-function", stringify!($func)).entered())
+                    };
                     let logic: &mut VMLogic = unsafe { &mut *(env.logic.0 as *mut VMLogic<'_>) };
                     logic.$func( $( $arg_name, )* )
                 }
@@ -96,10 +96,10 @@ macro_rules! wrapped_imports {
                     #[cfg(all(feature = "wasmtime_vm" $(, feature = $feature_name)*))]
                     pub fn $func( $( $arg_name: rust2wasm!($arg_type) ),* ) -> VMResult<($( rust2wasm!($returns)),*)> {
                         let _span = if stringify!($func) == "gas" {
-                            tracing::trace_span!(target: "host-function", "gas")
+                            None
                         } else {
-                            tracing::debug_span!(target: "host-function", stringify!($func))
-                        }.entered();
+                            Some(tracing::debug_span!(target: "host-function", stringify!($func)).entered())
+                        };
                         let data = CALLER_CONTEXT.with(|caller_context| {
                             unsafe {
                                 *caller_context.get()
