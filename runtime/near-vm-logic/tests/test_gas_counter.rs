6acfea0d21a454f8c67ba58f278ecefc64c189eb
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-11-05 14:05:05 +0300

    test: add more cases for gas overflow behavior

--- runtime/near-vm-logic/tests/test_gas_counter.rs
+++ runtime/near-vm-logic/tests/test_gas_counter.rs
@@ -20,7 +20,7 @@ fn test_dont_burn_gas_when_exceeding_attached_gas_limit() {
 
     // Just avoid hard-coding super-precise amount of gas burnt.
     assert!(outcome.burnt_gas < limit / 2);
-    assert!(outcome.used_gas == limit);
+    assert_eq!(outcome.used_gas, limit);
 }
 
 #[test]
@@ -29,7 +29,7 @@ fn test_limit_wasm_gas_after_attaching_gas() {
     let context = get_context(vec![], false);
     let regular_op_cost = logic_builder.config.regular_op_cost;
     let limit = context.prepaid_gas;
-    let op_limit = limit / (regular_op_cost as u64);
+    let op_limit = (limit / (regular_op_cost as u64)) as u32;
     let mut logic = logic_builder.build(context);
 
     let index = promise_create(&mut logic, b"rick.test", 0, 0).expect("should create a promise");
@@ -38,6 +38,39 @@ fn test_limit_wasm_gas_after_attaching_gas() {
     logic.gas((op_limit / 2) as u32).expect_err("should fail with gas limit");
     let outcome = logic.outcome();
 
-    assert!(outcome.used_gas == limit);
-    assert!(limit / 2 < outcome.burnt_gas && outcome.burnt_gas < limit);
+    assert_eq!(outcome.used_gas, limit);
+    assert!(limit / 2 < outcome.burnt_gas);
+    assert!(outcome.burnt_gas < limit);
+}
+
+#[test]
+fn test_cant_burn_more_than_max_gas_burnt_gas() {
+    let mut logic_builder = VMLogicBuilder::default();
+    let regular_op_cost = logic_builder.config.regular_op_cost;
+    let limit = logic_builder.config.limit_config.max_gas_burnt;
+    let op_limit = (limit / (regular_op_cost as u64)) as u32;
+    let mut context = get_context(vec![], false);
+    context.prepaid_gas = limit * 2;
+    let mut logic = logic_builder.build(context);
+
+    logic.gas(op_limit * 3).expect_err("should fail with gas limit");
+    let outcome = logic.outcome();
+
+    assert_eq!(outcome.burnt_gas, limit);
+}
+
+#[test]
+fn test_cant_burn_more_than_prepaid_gas() {
+    let mut logic_builder = VMLogicBuilder::default();
+    let regular_op_cost = logic_builder.config.regular_op_cost;
+    let limit = logic_builder.config.limit_config.max_gas_burnt;
+    let op_limit = (limit / (regular_op_cost as u64)) as u32;
+    let mut context = get_context(vec![], false);
+    context.prepaid_gas = limit / 2;
+    let mut logic = logic_builder.build(context);
+
+    logic.gas(op_limit).expect_err("should fail with gas limit");
+    let outcome = logic.outcome();
+
+    assert_eq!(outcome.burnt_gas, limit / 2);
 }
