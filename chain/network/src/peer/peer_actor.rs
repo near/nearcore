61da3956ad119c62f2722fc3c44baa762535ecc7
pmnoxx <piotr.mikulski@gmail.com>
2021-11-12 14:12:05 -0800

    A prototype of socket read limiter (#4927)
     `ThrottleRateLimiter` will allow us to stop/resume reading from TcpSocket from given peer under specific conditions.
    
    Current implementation:
    - [x] stop reading after we detect that we have more than 20 unprocessed messages from peer
    - [x] stop reading after we detect that the total size of all messages for peer is more than 512mb.
    - [x] add a unit test for `RateLimiterHelper`
    
    In the future we can extend this implementation to have more features. Please refer to design doc:  (https://docs.google.com/document/d/1FV3oyMRo52EQAgYroVd6Ws-5-ylkLSTdMOstY-IdL-M/edit?usp=sharing)
    
    Related to https://github.com/near/nearcore/issues/5155

--- chain/network/src/peer/peer_actor.rs
+++ chain/network/src/peer/peer_actor.rs
@@ -34,6 +34,7 @@ use near_primitives::utils::DisplayOption;
 use near_primitives::version::{
     ProtocolVersion, OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION, PROTOCOL_VERSION,
 };
+use near_rate_limiter::ThrottleController;
 use near_rust_allocator_proxy::allocator::get_tid;
 
 use crate::routing::codec::{self, bytes_to_peer_message, peer_message_to_bytes, Codec};
@@ -202,6 +203,9 @@ pub struct PeerActor {
     last_time_received_epoch_sync_request: Instant,
     /// Cache of recently routed messages, this allows us to drop duplicates
     routed_message_cache: SizedCache<(PeerId, PeerIdOrHash, Signature), Instant>,
+    /// A helper data structure for limiting reading
+    #[allow(unused)]
+    throttle_controller: ThrottleController,
 }
 
 impl Debug for PeerActor {
@@ -225,6 +229,7 @@ impl PeerActor {
         network_metrics: NetworkMetrics,
         txns_since_last_block: Arc<AtomicUsize>,
         peer_counter: Arc<AtomicUsize>,
+        throttle_controller: ThrottleController,
     ) -> Self {
         PeerActor {
             node_info,
@@ -249,6 +254,7 @@ impl PeerActor {
             last_time_received_epoch_sync_request: Clock::instant()
                 - Duration::from_millis(EPOCH_SYNC_PEER_TIMEOUT_MS),
             routed_message_cache: SizedCache::with_size(ROUTED_MESSAGE_CACHE_SIZE),
+            throttle_controller,
         }
     }
 
@@ -706,6 +712,8 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for PeerActor {
                 return;
             }
         };
+        // TODO(#5155) We should change our code to track size of messages received from Peer
+        // as long as it travels to PeerManager, etc.
 
         near_metrics::inc_counter_by(&metrics::PEER_DATA_RECEIVED_BYTES, msg.len() as u64);
         near_metrics::inc_counter(&metrics::PEER_MESSAGE_RECEIVED_TOTAL);
@@ -1026,6 +1034,11 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for PeerActor {
             }
             #[cfg(feature = "protocol_feature_routing_exchange_algorithm")]
             (_, _state, PeerMessage::RoutingTableSyncV2(ibf_message)) => {
+                // TODO(#5155) Add wrapper to be something like this for all messages.
+                // self.peer_manager_addr.do_send(ActixMessageWrapper<NetworkRequests>::new(
+                //        self.rate_limiter.clone, NetworkRequests::IbfMessage {
+                //         ...
+
                 self.peer_manager_addr.do_send(PeerManagerMessageRequest::NetworkRequests(
                     NetworkRequests::IbfMessage {
                         peer_id: self.peer_id().unwrap(),
