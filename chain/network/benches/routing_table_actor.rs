310a91b8aaa2f517919b273ac85b5d0313f3bca0
Piotr Mikulski <piotr@near.org>
2021-11-12 15:56:29 -0800

    Add more benches

--- chain/network/benches/routing_table_actor.rs
+++ chain/network/benches/routing_table_actor.rs
@@ -75,7 +75,38 @@ fn get_all_edges_bench_new2(bench: &mut Bencher) {
     });
 }
 
-benchmark_group!(benches, get_all_edges_bench_old, get_all_edges_bench_new2);
+#[allow(dead_code)]
+fn get_all_edges_bench_new3(bench: &mut Bencher) {
+    // this is how we efficient we could make get_all_edges by using Arc
+
+    // 1000 nodes, 10m edges
+    let routing_table_actor = build_graph(10, 100);
+    let all_edges = routing_table_actor.get_all_edges();
+    let mut new_edges_info = HashMap::new();
+    for edge in all_edges {
+        let edge = EdgeNew2 {
+            key: (Arc::new(edge.key.0.clone()), Arc::new(edge.key.1.clone())),
+            nonce: edge.nonce,
+            signature0: edge.signature0.clone(),
+            signature1: edge.signature1.clone(),
+            removal_info: edge.removal_info.clone(),
+        };
+
+        new_edges_info.insert(edge.key.clone(), Arc::new(edge));
+    }
+
+    bench.iter(|| {
+        let result: Vec<Arc<EdgeNew2>> = new_edges_info.iter().map(|x| x.1.clone()).collect();
+        black_box(result);
+    });
+}
+
+benchmark_group!(
+    benches,
+    get_all_edges_bench_old,
+    get_all_edges_bench_new2,
+    get_all_edges_bench_new3
+);
 
 benchmark_main!(benches);
 
@@ -88,6 +119,24 @@ benchmark_main!(benches);
 pub struct EdgeNew {
     /// Since edges are not directed `peer0 < peer1` should hold.
     pub key: Arc<(PeerId, PeerId)>,
+    /// Nonce to keep tracking of the last update on this edge.
+    /// It must be even
+    pub nonce: u64,
+    /// Signature from parties validating the edge. These are signature of the added edge.
+    #[allow(unused)]
+    signature0: Signature,
+    #[allow(unused)]
+    signature1: Signature,
+    /// Info necessary to declare an edge as removed.
+    /// The bool says which party is removing the edge: false for Peer0, true for Peer1
+    /// The signature from the party removing the edge.
+    #[allow(unused)]
+    removal_info: Option<(bool, Signature)>,
+}
+
+pub struct EdgeNew2 {
+    /// Since edges are not directed `peer0 < peer1` should hold.
+    pub key: (Arc<PeerId>, Arc<PeerId>),
     /// Nonce to keep tracking of the last update on this edge.
     /// It must be even
     pub nonce: u64,
