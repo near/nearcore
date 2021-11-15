050db149bfeb17157aca8ba3ee53eb0a95e5c8c6
Nikolay Kurtov <nikolay.kurtov@near.org>
2021-11-15 12:06:19 +0100

    Migrate `state-viewer`'s command line interface to `clap-derive` aka `clap3-beta`. Manually tested that every command still works. Note that names of some parameter names changed: `--start-index` instead of `--start_index`.

--- test-utils/state-viewer/src/lib.rs
+++ test-utils/state-viewer/src/lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 
 mod apply_chain_range;
-pub use crate::apply_chain_range::apply_chain_range;
-
 mod state_dump;
-pub use crate::state_dump::state_dump;
+
+pub mod cli;
+pub use cli::StateViewerSubCommand;
