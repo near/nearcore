ead41772bdb4842ff3ef509cf312f8d3b8707993
Longarithm <the.aleksandr.logunov@gmail.com>
2021-11-16 21:10:22 +0300

    fix default value

--- core/primitives-core/src/config.rs
+++ core/primitives-core/src/config.rs
@@ -19,6 +19,10 @@ pub struct VMConfig {
     pub limit_config: VMLimitConfig,
 }
 
+fn default_max_transaction_size() -> u64 {
+    4 * 2u64.pow(20) // 4 MiB
+}
+
 /// Describes limits for VM and Runtime.
 /// TODO #4139: consider switching to strongly-typed wrappers instead of raw quantities
 #[derive(Debug, Serialize, Deserialize, Clone, Hash, PartialEq, Eq)]
@@ -71,7 +75,7 @@ pub struct VMLimitConfig {
     /// Max contract size
     pub max_contract_size: u64,
     /// Max transaction size
-    #[serde(default = "4_194_304")] // 4 Mib
+    #[serde(default = "default_max_transaction_size")]
     pub max_transaction_size: u64,
     /// Max storage key size
     pub max_length_storage_key: u64,
