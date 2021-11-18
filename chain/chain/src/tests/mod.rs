3b08d564fed323bf2c11d6bfd712d24b6ceef476
Min Zhang <min@near.org>
2021-11-18 13:23:34 -0500

    fix CI

--- chain/chain/src/tests/mod.rs
+++ chain/chain/src/tests/mod.rs
@@ -1,18 +1,20 @@
-use crate::types::Tip;
-use crate::{Block, Chain, Error, Provenance};
-use near_primitives::account::id::AccountId;
-
 mod challenges;
 mod simple_chain;
 
-impl Chain {
-    /// A wrapper function around process_block that doesn't trigger all the callbacks
-    /// Only used in tests
-    pub(crate) fn process_block_test(
-        &mut self,
-        me: &Option<AccountId>,
-        block: Block,
-    ) -> Result<Option<Tip>, Error> {
-        self.process_block(me, block, Provenance::PRODUCED, |_| {}, |_| {}, |_| {})
+#[test]
+mod tests {
+    use crate::types::Tip;
+    use crate::{Block, Chain, Error, Provenance};
+    use near_primitives::account::id::AccountId;
+    impl Chain {
+        /// A wrapper function around process_block that doesn't trigger all the callbacks
+        /// Only used in tests
+        pub(crate) fn process_block_test(
+            &mut self,
+            me: &Option<AccountId>,
+            block: Block,
+        ) -> Result<Option<Tip>, Error> {
+            self.process_block(me, block, Provenance::PRODUCED, |_| {}, |_| {}, |_| {})
+        }
     }
 }
