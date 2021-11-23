f7a4850833633cce6ff66514df85179dd6269d50
Egor Kulikov <egor@egork.net>
2021-11-23 10:31:05 +0100

    Revert version

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -142,7 +142,7 @@ pub enum ProtocolFeature {
 /// Some features (e. g. FixStorageUsage) require that there is at least one epoch with exactly
 /// the corresponding version
 #[cfg(not(feature = "nightly_protocol"))]
-pub const PROTOCOL_VERSION: ProtocolVersion = 50;
+pub const PROTOCOL_VERSION: ProtocolVersion = 49;
 
 /// Current latest nightly version of the protocol.
 #[cfg(feature = "nightly_protocol")]
@@ -170,10 +170,9 @@ impl ProtocolFeature {
             | ProtocolFeature::LowerRegularOpCost
             | ProtocolFeature::SimpleNightshade => 48,
             ProtocolFeature::LowerRegularOpCost2
-            | ProtocolFeature::LimitContractFunctionsNumber => 49,
-            ProtocolFeature::BlockHeaderV3 | ProtocolFeature::AliasValidatorSelectionAlgorithm => {
-                50
-            }
+            | ProtocolFeature::LimitContractFunctionsNumber
+            | ProtocolFeature::BlockHeaderV3
+            | ProtocolFeature::AliasValidatorSelectionAlgorithm => 49,
 
             // Nightly features
             #[cfg(feature = "protocol_feature_alt_bn128")]
