e64704f8a6f7095d742a11ee8f25d771fffd4f0d
Michal Nazarewicz <mina86@mina86.com>
2021-11-25 17:25:35 +0100

    nearcore: support dling compressed `genesis.json` and `config.json`
    Introduce `AutoXzDecoder` struct (which automatically detects
    xz-compressed files and transparently decompresses them) and use it in
    `download_file` to support compressed `genesis.json` and `config.json`
    files.
    
    Especially the former file may get quite large resulting in long
    downloads and wasted bandwidth.  Being able to download compressed
    JSON files will reduce the time it takes for `neard init` to finish.
    For example, testnet’s `genesis.json` file is 5.5 GB and shrinks to 45
    MB when compressed.
    
    The default download URLs still point to uncompressed files (since
    compressed ones are not yet available).
    
    Issue: https://github.com/near/nearcore/issues/4951

--- nearcore/src/config.rs
+++ nearcore/src/config.rs
@@ -10,7 +10,7 @@ use near_primitives::time::Clock;
 use num_rational::Rational;
 use serde::{Deserialize, Serialize};
 use tokio::io::AsyncWriteExt;
-use tracing::info;
+use tracing::{error, info};
 
 use near_chain_configs::{
     get_initial_supply, ClientConfig, Genesis, GenesisConfig, LogSummaryStyle,
@@ -1100,6 +1100,10 @@ pub enum FileDownloadError {
     OpenError(std::io::Error),
     #[error("Failed to write to file: {0}")]
     WriteError(std::io::Error),
+    #[error("Failed to decompress XZ stream: {0}")]
+    XZDecodeError(#[from] xz2::stream::Error),
+    #[error("Failed to decompress XZ stream: internal error: unexpected status {0:?}")]
+    XZStatusError(String),
     #[error("Failed to rename file: {0}")]
     RenameError(std::io::Error),
     #[error("Invalid URI: {0}")]
@@ -1108,20 +1112,210 @@ pub enum FileDownloadError {
     RemoveTemporaryFileError(std::io::Error, Box<FileDownloadError>),
 }
 
+/// Object which allows transparent XZ decoding when saving data to a file.
+/// It automatically detects whether the data being read is compressed by
+/// looking at the magic at the beginning of the file.
+struct AutoXzDecoder {
+    file: tokio::fs::File,
+    state: AutoXzState,
+}
+
+/// State in which of the AutoXzDecoder
+enum AutoXzState {
+    /// Given number of bytes have been read so far and all of them match bytes
+    /// in [`XZ_HEADER_MAGIC`].  The object starts in `Probing(0)` state and the
+    /// number never reaches the length of the [`XZ_HEADER_MAGIC`] buffer.
+    Probing(usize),
+
+    /// The header did not match XZ stream header and thus the data is passed
+    /// through.
+    PlainText,
+
+    /// The header did match XZ stream header and thus the data is being
+    /// decompressed.
+    Compressed(xz2::stream::Stream, Box<[u8]>),
+}
+
+static XZ_HEADER_MAGIC: [u8; 6] = [0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00];
+
+impl AutoXzDecoder {
+    fn new(file: std::fs::File) -> Self {
+        Self { file: tokio::fs::File::from_std(file), state: AutoXzState::Probing(0) }
+    }
+
+    /// Writes data from the chunk to the output file automatically
+    /// decompressing it if the stream is XZ-compressed.  Note that once all the
+    /// data has been written [`finish`] function must be called to flush
+    /// internal buffers.
+    async fn write_all(&mut self, chunk: &[u8]) -> Result<(), FileDownloadError> {
+        if let Some(len) = self.probe(chunk) {
+            if len != 0 {
+                self.write_all_impl(&XZ_HEADER_MAGIC[..len]).await?;
+            }
+            self.write_all_impl(&chunk).await?;
+        }
+        Ok(())
+    }
+
+    /// Flushes all internal buffers and closes the output file.
+    async fn finish(mut self) -> Result<(), FileDownloadError> {
+        match self.state {
+            AutoXzState::Probing(pos) => self
+                .file
+                .write_all(&XZ_HEADER_MAGIC[..pos])
+                .await
+                .map_err(FileDownloadError::WriteError),
+            AutoXzState::PlainText => Ok(()),
+            AutoXzState::Compressed(ref mut stream, ref mut buffer) => {
+                Self::decompress(&mut self.file, stream, buffer, b"").await
+            }
+        }
+    }
+
+    /// If object is still in `Probing` state, read more data from the input to
+    /// determine whether it’s XZ stream or not.  Updates `state` accordingly.
+    /// If probing succeeded, returns number of bites from XZ header magic that
+    /// need to be processed before `chunk` is processed.  If the entire data
+    /// from `chunk` has been processed and it should be discarded by the
+    /// caller, returns `None`.
+    fn probe(&mut self, chunk: &[u8]) -> Option<usize> {
+        if chunk.is_empty() {
+            None
+        } else if let AutoXzState::Probing(pos) = self.state {
+            let len = std::cmp::min(XZ_HEADER_MAGIC.len() - pos, chunk.len());
+            if XZ_HEADER_MAGIC[pos..(pos + len)] != chunk[..len] {
+                self.state = AutoXzState::PlainText;
+                Some(pos)
+            } else if pos + len == XZ_HEADER_MAGIC.len() {
+                let stream = xz2::stream::Stream::new_stream_decoder(u64::max_value(), 0).unwrap();
+                // TODO(mina86): Once ‘new_uninit’ feature gets stabilised
+                // replaced buffer initialisation by:
+                //     let buffer = Box::new_uninit_slice(64 << 10);
+                //     let buffer = unsafe { buffer.assume_init() };
+                let buffer = vec![0u8; 64 << 10].into_boxed_slice();
+                self.state = AutoXzState::Compressed(stream, buffer);
+                Some(pos)
+            } else {
+                self.state = AutoXzState::Probing(pos + len);
+                None
+            }
+        } else {
+            Some(0)
+        }
+    }
+
+    /// Writes data to the output file.  Panics if the object is still in
+    /// probing stage.
+    async fn write_all_impl(&mut self, chunk: &[u8]) -> Result<(), FileDownloadError> {
+        match self.state {
+            AutoXzState::Probing(_) => unreachable!(),
+            AutoXzState::PlainText => {
+                self.file.write_all(chunk).await.map_err(FileDownloadError::WriteError)
+            }
+            AutoXzState::Compressed(ref mut stream, ref mut buffer) => {
+                Self::decompress(&mut self.file, stream, buffer, chunk).await
+            }
+        }
+    }
+
+    /// Internal implementation for [`write_all`] and [`finish`] methods used
+    /// when performing decompression.  Calling it with an empty `chunk`
+    /// indicates the end of the compressed data.
+    async fn decompress(
+        file: &mut tokio::fs::File,
+        stream: &mut xz2::stream::Stream,
+        buffer: &mut [u8],
+        mut chunk: &[u8],
+    ) -> Result<(), FileDownloadError> {
+        let action =
+            if chunk.is_empty() { xz2::stream::Action::Finish } else { xz2::stream::Action::Run };
+        loop {
+            let total_in = stream.total_in();
+            let total_out = stream.total_out();
+            let status = stream.process(chunk, buffer, action)?;
+            match status {
+                xz2::stream::Status::Ok => (),
+                xz2::stream::Status::StreamEnd => (),
+                status => {
+                    let status = format!("{:?}", status);
+                    error!(target: "near", "Got unexpected status ‘{}’ when decompressing downloaded file.", status);
+                    return Err(FileDownloadError::XZStatusError(status));
+                }
+            };
+            let read = (stream.total_in() - total_in).try_into().unwrap();
+            chunk = &chunk[read..];
+            let out = (stream.total_out() - total_out).try_into().unwrap();
+            file.write_all(&buffer[..out]).await.map_err(FileDownloadError::WriteError)?;
+            if chunk.is_empty() {
+                break Ok(());
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+fn auto_xz_test_write_file(buffer: &[u8], chunk_size: usize) -> Vec<u8> {
+    let (file, path) = tempfile::NamedTempFile::new().unwrap().into_parts();
+    {
+        let mut out = AutoXzDecoder::new(file);
+        tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap().block_on(
+            async move {
+                for chunk in buffer.chunks(chunk_size) {
+                    out.write_all(chunk).await.unwrap();
+                }
+                out.finish().await.unwrap();
+            },
+        );
+    }
+    std::fs::read(path).unwrap()
+}
+
+#[test]
+fn test_auto_xz_decode_plain() {
+    let buffer = "Zażółć gęślą jaźń".as_bytes();
+    for chunk_size in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] {
+        let got = auto_xz_test_write_file(buffer, chunk_size);
+        assert_eq!(got, buffer);
+    }
+}
+
+#[test]
+fn test_auto_xz_decode_plain_with_partial_prefix() {
+    let buffer = b"\xfd\x37\x7aA quick brown fox";
+    for chunk_size in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] {
+        let got = auto_xz_test_write_file(buffer, chunk_size);
+        assert_eq!(got, buffer);
+    }
+}
+
+#[test]
+fn test_auto_xz_decode_compressed() {
+    let buffer = b"\xfd\x37\x7a\x58\x5a\x00\x00\x04\xe6\xd6\xb4\x46\
+                   \x02\x00\x21\x01\x1c\x00\x00\x00\x10\xcf\x58\xcc\
+                   \x01\x00\x19\x5a\x61\xc5\xbc\xc3\xb3\xc5\x82\xc4\
+                   \x87\x20\x67\xc4\x99\xc5\x9b\x6c\xc4\x85\x20\x6a\
+                   \x61\xc5\xba\xc5\x84\x00\x00\x00\x89\x4e\xdf\x72\
+                   \x66\xbe\xa9\x51\x00\x01\x32\x1a\x20\x18\x94\x30\
+                   \x1f\xb6\xf3\x7d\x01\x00\x00\x00\x00\x04\x59\x5a";
+    for chunk_size in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] {
+        let got = auto_xz_test_write_file(buffer, chunk_size);
+        assert_eq!(got, "Zażółć gęślą jaźń".as_bytes());
+    }
+}
+
 /// Downloads resource at given `uri` and saves it to `file`.  On failure,
 /// `file` may be left in inconsistent state (i.e. may contain partial data).
-async fn download_file_impl(
-    uri: hyper::Uri,
-    mut file: tokio::fs::File,
-) -> Result<(), FileDownloadError> {
+/// If the path in the URI ends in ‘.xz’, transparently decompress the file as
+/// it’s being downloaded.
+async fn download_file_impl(uri: hyper::Uri, file: std::fs::File) -> Result<(), FileDownloadError> {
+    let mut out = AutoXzDecoder::new(file);
     let https_connector = hyper_tls::HttpsConnector::new();
     let client = hyper::Client::builder().build::<_, hyper::Body>(https_connector);
     let mut resp = client.get(uri).await?;
     while let Some(next_chunk_result) = resp.data().await {
-        let next_chunk = next_chunk_result?;
-        file.write_all(next_chunk.as_ref()).await.map_err(FileDownloadError::WriteError)?;
+        out.write_all(next_chunk_result?.as_ref()).await?;
     }
-    Ok(())
+    out.finish().await
 }
 
 /// Downloads a resource at given `url` and saves it to `path`.  On success, if
@@ -1133,14 +1327,11 @@ pub fn download_file(url: &str, path: &Path) -> Result<(), FileDownloadError> {
         let tmp_dir = path.parent().unwrap_or(&Path::new("."));
         tempfile::NamedTempFile::new_in(tmp_dir).map_err(FileDownloadError::OpenError)?.into_parts()
     };
-
-    let result =
-        tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap().block_on(
-            async move {
-                let tmp_file = tokio::fs::File::from_std(tmp_file);
-                download_file_impl(uri, tmp_file).await
-            },
-        );
+    let result = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async move { download_file_impl(uri, tmp_file).await });
 
     let result = match result {
         Err(err) => Err((tmp_path, err)),
