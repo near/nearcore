eef617dbdd4ef0342400e9e265754cc56f735991
pmnoxx <piotr.mikulski@gmail.com>
2021-11-29 14:31:00 -0800

    feat: Allow running any test in `/pytest` from any folder (#5496)
    * Make `routing_table_sync` work from any folder
    
    * yapf
    
    Co-authored-by: Piotr Mikulski <piotr@near.org>

--- pytest/tests/sanity/proxy_simple.py
+++ pytest/tests/sanity/proxy_simple.py
@@ -3,8 +3,9 @@
 # and wait until block at height >= 10 pass through the proxy.
 import sys, time
 import multiprocessing
+import pathlib
 
-sys.path.append('lib')
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / 'lib'))
 
 from cluster import start_cluster
 from configured_logger import logger
