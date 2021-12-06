a0c93b6129f8fa4414a79258da84420f6b85ada2
mzhangmzz <34969888+mzhangmzz@users.noreply.github.com>
2021-12-06 13:28:28 -0500

    doc: add comments for chunk_cache.rs (#5616)
    * add comments for chunk_cache.rs
    
    * Update chain/chunks/src/chunk_cache.rs
    
    Co-authored-by: Bowen Wang <bowenwang1996@users.noreply.github.com>
    
    * change to //
    
    Co-authored-by: Bowen Wang <bowenwang1996@users.noreply.github.com>
    Co-authored-by: near-bulldozer[bot] <73298989+near-bulldozer[bot]@users.noreply.github.com>

--- chain/chunks/src/chunk_cache.rs
+++ chain/chunks/src/chunk_cache.rs
@@ -8,11 +8,31 @@ use near_primitives::sharding::{
 };
 use near_primitives::types::{BlockHeight, BlockHeightDelta, ShardId};
 
+// This file implements EncodedChunksCache, which provides two main functionalities:
+// 1) It stores a map from a chunk hash to all the parts and receipts received so far for the chunk.
+//    This map is used to aggregate chunk parts and receipts before the full chunk can be reconstructed
+//    or the necessary parts and receipts are received.
+//    When a PartialEncodedChunk is received, the parts and receipts it contains are merged to the
+//    corresponding chunk entry in the map.
+//    Entries in the map are removed if the chunk is found to be invalid or the chunk goes out of
+//    horizon [chain_head_height - HEIGHT_HORIZON, chain_head_height + MAX_HEIGHTS_AHEAD]
+// 2) It stores a map from block hash to chunk headers that are ready to be included in a block.
+//    This functionality is meant for block producers. When producing a block, the block producer
+//    will only include chunks in the block for which it has received the part it owns.
+//    Users of the data structure are responsible for adding chunk to this map at the right time.
+
+/// A chunk is out of horizon if its height + HEIGHT_HORIZON < largest_seen_height
 const HEIGHT_HORIZON: BlockHeightDelta = 1024;
+/// A chunk is out of horizon if its height > HEIGHT_HORIZON + largest_seen_height
 const MAX_HEIGHTS_AHEAD: BlockHeightDelta = 5;
+/// A chunk header is out of horizon if its height + CHUNK_HEADER_HORIZON < largest_seen_height
 const CHUNK_HEADER_HEIGHT_HORIZON: BlockHeightDelta = 10;
+/// Max number of entries stored in `block_hash_to_chunk_headers`
 const NUM_BLOCK_HASH_TO_CHUNK_HEADER: usize = 30;
 
+/// EncodedChunksCacheEntry stores the consolidated parts and receipts received for a chunk
+/// When a PartialEncodedChunk is received, it can be merged to the existing EncodedChunksCacheEntry
+/// for the chunk
 pub struct EncodedChunksCacheEntry {
     pub header: ShardChunkHeader,
     pub parts: HashMap<u64, PartialEncodedChunkPart>,
@@ -23,8 +43,15 @@ pub struct EncodedChunksCache {
     /// Largest seen height from the head of the chain
     largest_seen_height: BlockHeight,
 
+    /// A map from a chunk hash to the corresponding EncodedChunksCacheEntry of the chunk
+    /// Entries in this map have height in
+    /// [chain_head_height - HEIGHT_HORIZON, chain_head_height + MAX_HEIGHTS_AHEAD]
     encoded_chunks: HashMap<ChunkHash, EncodedChunksCacheEntry>,
+    /// A map from a block height to chunk hashes at this height for all chunk stored in the cache
+    /// This is used to gc chunks that are out of horizon
     height_map: HashMap<BlockHeight, HashSet<ChunkHash>>,
+    /// A sized cache mapping a block hash to the chunk headers that are ready
+    /// to be included when producing the next block after the block
     block_hash_to_chunk_headers: SizedCache<CryptoHash, HashMap<ShardId, ShardChunkHeader>>,
 }
 
@@ -98,6 +125,7 @@ impl EncodedChunksCache {
         self.height_within_front_horizon(height) || self.height_within_rear_horizon(height)
     }
 
+    /// add parts and receipts stored in a partial encoded chunk to the corresponding chunk entry
     pub fn merge_in_partial_encoded_chunk(
         &mut self,
         partial_encoded_chunk: &PartialEncodedChunkV2,
@@ -110,6 +138,7 @@ impl EncodedChunksCache {
         self.height_map.entry(height).or_insert_with(|| HashSet::default()).insert(chunk_hash);
     }
 
+    /// Remove a chunk from the cache if it is outside of horizon
     pub fn remove_from_cache_if_outside_horizon(&mut self, chunk_hash: &ChunkHash) {
         if let Some(entry) = self.encoded_chunks.get(chunk_hash) {
             let height = entry.header.height_created();
@@ -119,6 +148,7 @@ impl EncodedChunksCache {
         }
     }
 
+    /// Update largest seen height and removes chunks from the cache that are outside of horizon
     pub fn update_largest_seen_height<T>(
         &mut self,
         new_height: BlockHeight,
@@ -139,6 +169,7 @@ impl EncodedChunksCache {
         }
     }
 
+    /// Insert a chunk header to indicate the chunk header is ready to be included in a block
     pub fn insert_chunk_header(&mut self, shard_id: ShardId, header: ShardChunkHeader) {
         let height = header.height_created();
         if height >= self.largest_seen_height.saturating_sub(CHUNK_HEADER_HEIGHT_HORIZON)
@@ -155,6 +186,8 @@ impl EncodedChunksCache {
         }
     }
 
+    /// Returns all chunk headers to be included in the next block after `prev_block_hash`
+    /// Also removes these chunk headers from the map
     pub fn get_chunk_headers_for_block(
         &mut self,
         prev_block_hash: &CryptoHash,
@@ -164,6 +197,7 @@ impl EncodedChunksCache {
             .unwrap_or_else(|| HashMap::new())
     }
 
+    /// Returns number of chunks that are ready to be included in the next block
     pub fn num_chunks_for_block(&mut self, prev_block_hash: &CryptoHash) -> ShardId {
         self.block_hash_to_chunk_headers
             .cache_get(prev_block_hash)
