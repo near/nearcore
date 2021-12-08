7c989c2899dd53cbfc8a9ad941fa4850fa6cce69
Nikolay Kurtov <nikolay.kurtov@near.org>
2021-12-08 12:28:50 +0100

    Remove the `single_file` argument from the `dump_state` function.

--- test-utils/state-viewer/src/cli.rs
+++ test-utils/state-viewer/src/cli.rs
@@ -107,13 +107,8 @@ pub struct DumpStateCmd {
     /// Dumps state records and genesis config into separate files.
     /// Has reasonable RAM requirements.
     /// Use for chains with large state, such as mainnet and testnet.
-    #[clap(long, conflicts_with("single_file_output"))]
+    #[clap(long)]
     stream: bool,
-    /// Produces a single json file containing the state.
-    /// Needs the whole state to fit in RAM.
-    /// Use for chains with small state, such as betanet, guildnet, localnet.
-    #[clap(long, conflicts_with("stream"))]
-    single_file_output: bool,
     /// Location of the dumped state.
     #[clap(long, parse(from_os_str))]
     file: Option<PathBuf>,
