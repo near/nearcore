ec3fe1ef616d17d22d6e676e523a568a13775ece
mm-near <91919554+mm-near@users.noreply.github.com>
2021-12-09 23:05:13 +0200

    documentation: 3 crates in chain (mostly to chunk handling) (#5687)
    Added documentation to 3 crates in chain - but most of it is about chunk handling.

--- chain/chunks/src/lib.rs
+++ chain/chunks/src/lib.rs
@@ -1542,6 +1542,7 @@ impl ShardsManager {
         )
     }
 
+    /// Returns true if we need this part to sign the block.
     fn need_part(&self, prev_block_hash: &CryptoHash, part_ord: u64) -> Result<bool, Error> {
         Ok(Some(self.runtime_adapter.get_part_owner(prev_block_hash, part_ord)?) == self.me)
     }
@@ -1563,6 +1564,9 @@ impl ShardsManager {
         Ok(true)
     }
 
+    /// Returns true if we have all the parts that are needed to validate the block.
+    /// NOTE: this doesn't mean that we got *all* the parts (as given verifier only needs the ones
+    /// for which it is the 'owner').
     fn has_all_parts(
         &self,
         prev_block_hash: &CryptoHash,
