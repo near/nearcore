b4b2d987c028443bdbee89c9a0d203b5f646c12b
Piotr Mikulski <piotr@near.org>
2021-12-12 03:55:01 -0800

    Refactor `impl TryFrom<&[u8]> for CryptoHash`

--- core/primitives-core/src/hash.rs
+++ core/primitives-core/src/hash.rs
@@ -88,12 +88,7 @@ impl TryFrom<&[u8]> for CryptoHash {
     type Error = Box<dyn std::error::Error>;
 
     fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
-        if bytes.len() != 32 {
-            return Err("incorrect length for hash".into());
-        }
-        let mut buf = [0; 32];
-        buf.copy_from_slice(bytes);
-        Ok(CryptoHash(buf))
+        bytes.try_into().map(|v| CryptoHash(v)).map_err(|_| "incorrect length for hash".into())
     }
 }
 
