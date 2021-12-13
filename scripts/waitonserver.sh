3709d8d41552ca3d62f3048327b4fb3634aaad2f
Piotr Mikulski <piotr@near.org>
2021-12-12 19:31:02 -0800

    Improve handling of errors in `scripts/waitonserver.sh`

--- scripts/waitonserver.sh
+++ scripts/waitonserver.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
-echo 'waiting on healthcheck' >&2
+if [[ -z "${NEAR_PID}" ]]; then
+  echo "NEAR_PID is undefined"
+  exit 1
+fi
+
+echo "waiting on healthcheck NEAR_PID = ${NEAR_PID}" >&2
 
 for _ in {1..500}; do
     echo -n '.' >&2
-    kill -0 $NEAR_PID > /dev/null 2>&1 || exit 1
+    kill -0 "$NEAR_PID" > /dev/null 2>&1 || exit 1
     if [[ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:3030/status)" == "200" ]]; then
         exit 0
     fi
