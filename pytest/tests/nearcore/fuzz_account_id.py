b355351bf960f92ab65a9c6174b27e60452c6f68
Miraculous Owonubi <omiraculous@gmail.com>
2021-12-13 15:36:26 +0100

    add fuzz testing infra for account-id + jsonrpc

---
+++ pytest/tests/nearcore/fuzz_account_id.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import sys
+
+sys.path.append('lib')
+
+import cargo_fuzz
+
+if __name__ == '__main__':
+    sys.exit(cargo_fuzz.run('core/account-id/fuzz', 'serde'))
+    sys.exit(cargo_fuzz.run('core/account-id/fuzz', 'borsh'))
