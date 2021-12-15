f1fc109a789c4860d95e7887da3ecf57c1de052f
Piotr Mikulski <piotr@near.org>
2021-12-15 02:42:53 -0800

    Simplify code

--- chain/network/src/peer_manager/peer_manager_actor.rs
+++ chain/network/src/peer_manager/peer_manager_actor.rs
@@ -920,8 +920,7 @@ impl PeerManagerActor {
 
     /// Get a random peer we are not connected to from the known list.
     fn sample_random_peer(&self, ignore_fn: impl Fn(&KnownPeerState) -> bool) -> Option<PeerInfo> {
-        let res = self.peer_store.unconnected_peers(ignore_fn, 1);
-        res.get(0).cloned()
+        self.peer_store.unconnected_peers(ignore_fn, 1).get(0).cloned()
     }
 
     /// Query current peers for more peers.
