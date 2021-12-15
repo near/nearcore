682d8d27aec9532f2a8b2af199e9efb38e61c345
Piotr Mikulski <piotr@near.org>
2021-12-15 10:51:07 -0800

    Fix Clion error in `near-client`

--- chain/client/src/client_actor.rs
+++ chain/client/src/client_actor.rs
@@ -1091,7 +1091,7 @@ impl ClientActor {
                 // we don't need to do anything here
                 near_chain::ErrorKind::ChunksMissing(_) => {}
                 _ => {
-                    debug!(target: "client", "Process block: block {} refused by chain: {}", hash, e.kind());
+                    debug!(target: "client", "Process block: block {} refused by chain: {:?}", hash, e.kind());
                 }
             },
         }
