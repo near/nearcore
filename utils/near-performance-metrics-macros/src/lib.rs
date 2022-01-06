0861e983f5ed36dec5e83ad8d6a3a56e80a6c346
Piotr Mikulski <piotr@near.org>
2022-01-05 16:38:56 -0800

    Simplify code in `near-performance-metrics-macros`

--- utils/near-performance-metrics-macros/src/lib.rs
+++ utils/near-performance-metrics-macros/src/lib.rs
@@ -79,30 +79,24 @@ fn perf_internal(_attr: TokenStream, item: TokenStream, debug: bool) -> TokenStr
     let item: syn::Item = syn::parse(item).expect("failed to parse input");
 
     if let syn::Item::Fn(mut func) = item {
-        let block = func.clone().block;
-
-        let function_body = quote! { #block };
+        let function_body = func.block;
 
         let new_body: TokenStream = if debug {
-            let b: TokenStream = quote! {
+            quote! (
                 fn xxx() {
-                    use near_performance_metrics::stats::measure_performance_with_debug;
                     near_performance_metrics::stats::measure_performance_with_debug(std::any::type_name::<Self>(), msg, move |msg| {
                         #function_body
                     })
                 }
-            }.into();
-            b
+            ).into()
         } else {
-            let b: TokenStream = quote! {
+            quote! (
                 fn xxx() {
-                    use near_performance_metrics::stats::measure_performance;
                     near_performance_metrics::stats::measure_performance(std::any::type_name::<Self>(), msg, move |msg| {
                         #function_body
                     })
                 }
-             }.into();
-            b
+             ).into()
         };
 
         if let syn::Item::Fn(func2) = syn::parse(new_body).expect("failed to parse input") {
@@ -111,9 +105,7 @@ fn perf_internal(_attr: TokenStream, item: TokenStream, debug: bool) -> TokenStr
             panic!("failed to parse example function");
         }
 
-        let result_item = syn::Item::Fn(func);
-        let output = quote! { #result_item };
-        output.into()
+        quote! ( #func ).into()
     } else {
         panic!("not a function");
     }
