f6d8ad0402c6a07692fc2235c457e919b4f9464a
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-01-06 10:31:52 +0100

    .

--- pytest/tests/mocknet/helpers/load_test_spoon_helper.py
+++ pytest/tests/mocknet/helpers/load_test_spoon_helper.py
@@ -227,6 +227,13 @@ def get_test_accounts_from_args(argv):
                         base_block_hash,
                         rpc_infos=rpc_infos)
     ]
+    accounts = [
+        account.Account(key,
+                        get_nonce_for_pk(key.account_id, key.pk),
+                        base_block_hash,
+                        rpc_infos=rpc_infos)
+        for key, rpc_node in test_account_keys
+    ]
     max_tps_per_node = max_tps / num_nodes
     return node_account, accounts, max_tps_per_node
 
