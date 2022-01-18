efd248c2015a324b0d1e87c88e3f223cb5241cfa
Longarithm <the.aleksandr.logunov@gmail.com>
2022-01-18 16:13:30 +0300

    compilation fix

--- nearcore/benches/store.rs
+++ nearcore/benches/store.rs
@@ -4,6 +4,7 @@ extern crate bencher;
 use bencher::Bencher;
 use near_chain::{ChainStore, ChainStoreAccess, RuntimeAdapter};
 use near_chain_configs::GenesisValidationMode;
+use near_logger_utils::init_integration_logger;
 use near_primitives::types::StateRoot;
 use near_store::{create_store, TrieIterator};
 use nearcore::{get_default_home, get_store_path, load_config, NightshadeRuntime};
@@ -11,6 +12,7 @@ use std::time::{Duration, Instant};
 
 /// Read `TrieItem`s - nodes containing values - using Trie iterator, stop when `num_trie_items` items were read.
 fn read_trie_items(bench: &mut Bencher, num_trie_items: usize, shard_id: usize) {
+    init_integration_logger();
     let home_dir = get_default_home();
     let near_config = load_config(&home_dir, GenesisValidationMode::UnsafeFast);
 
