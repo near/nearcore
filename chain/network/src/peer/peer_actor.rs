a6efeea0684aae352e6b63757160c01e6fd7c02d
Piotr Mikulski <piotr@near.org>
2022-01-25 14:50:57 -0800

    Apply feedback

--- chain/network/src/peer/peer_actor.rs
+++ chain/network/src/peer/peer_actor.rs
@@ -680,15 +680,10 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for PeerActor {
             self.routed_message_cache.put(key, now);
         }
         if let PeerMessage::Routed(routed) = &peer_msg {
-            match routed.as_ref() {
-                RoutedMessage { body: RoutedMessageBody::ForwardTx(_), .. } => {
-                    self.txns_since_last_block.fetch_add(1, Ordering::AcqRel);
-                }
-                _ => {
-                    self.txns_since_last_block.store(0, Ordering::Release);
-                }
+            if let RoutedMessage { body: RoutedMessageBody::ForwardTx(_), .. } = routed.as_ref() {
+                self.txns_since_last_block.fetch_add(1, Ordering::AcqRel);
             }
-        } else {
+        } else if let PeerMessage::Block(_) = &peer_msg {
             self.txns_since_last_block.store(0, Ordering::Release);
         }
 
