38bcc1b5d8eb74f656ad516c5ff7bcf517681846
Michal Nazarewicz <mina86@mina86.com>
2022-01-25 16:19:32 +0100

    neard: generate NEARD_VERSION et al via `build.rs` script
    On it’s own this change doesn’t really offer us a lot.  There is
    a tiny optimisation where values don’t need to be constructed lazily
    at run time but are embedded into the executable but that’s really
    not an important win.
    
    The actual reason for this change is to later extend the `build.rs`
    script with other information.  Namely, rustc version is one such
    piece of information that we’ll like to include.
    
    Issue: https://github.com/near/nearcore/issues/5979

--- neard/src/main.rs
+++ neard/src/main.rs
@@ -3,9 +3,6 @@ mod cli;
 use std::env;
 
 use self::cli::NeardCmd;
-use clap::crate_version;
-use git_version::git_version;
-use near_primitives::version::{Version, DB_VERSION, PROTOCOL_VERSION};
 #[cfg(feature = "memory_stats")]
 use near_rust_allocator_proxy::allocator::MyAllocator;
 use nearcore::get_default_home;
@@ -13,23 +10,9 @@ use once_cell::sync::Lazy;
 use std::path::PathBuf;
 use std::time::Duration;
 
-pub fn get_version() -> String {
-    match crate_version!() {
-        "0.0.0" => "trunk".to_string(),
-        version => version.to_string(),
-    }
-}
+// Defines NEARD_VERSION and NEARD_VERSION_STRING variables.
+include!(concat!(env!("OUT_DIR"), "/version.rs"));
 
-static NEARD_VERSION: Lazy<Version> = Lazy::new(|| Version {
-    version: get_version(),
-    build: git_version!(fallback = "unknown").to_string(),
-});
-static NEARD_VERSION_STRING: Lazy<String> = Lazy::new(|| {
-    format!(
-        "(release {}) (build {}) (protocol {}) (db {})",
-        NEARD_VERSION.version, NEARD_VERSION.build, PROTOCOL_VERSION, DB_VERSION
-    )
-});
 static DEFAULT_HOME: Lazy<PathBuf> = Lazy::new(get_default_home);
 
 #[cfg(feature = "memory_stats")]
