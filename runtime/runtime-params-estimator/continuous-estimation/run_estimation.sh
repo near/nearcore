b21dacf97ac3bb54cd7fedb553cc3e5bc401426d
Jakob Meier <mail@jakobmeier.ch>
2022-01-28 14:22:24 +0100

    Use correct estimator settings in cont. estimation

--- runtime/runtime-params-estimator/continuous-estimation/run_estimation.sh
+++ runtime/runtime-params-estimator/continuous-estimation/run_estimation.sh
@@ -5,16 +5,26 @@ if [[ -z "${SQLI_DB}" ]]; then
   exit
 fi
 
+if [[ -z "${ESTIMATOR_NEAR_HOME}" ]]; then
+  echo '$ESTIMATOR_NEAR_HOME is not defined, will use /tmp/estimator_home as near home'
+  ESTIMATOR_NEAR_HOME="/tmp/estimator_home"
+  echo 'Initialize near home'
+  cargo run --release --package neard --bin neard -- --home ${ESTIMATOR_NEAR_HOME} init --test-seed=alice.near --account-id=test.near --fast
+  cargo run --release --package genesis-populate --bin genesis-populate -- --additional-accounts-num=10000000 --home ${ESTIMATOR_NEAR_HOME}
+fi
+
 GIT_HASH=`git rev-parse HEAD`
+
+COMMON_ARGS="--vm-kind wasmer2 --home ${ESTIMATOR_NEAR_HOME} --accounts-num 1000000 --iters 1 --warmup-iters 1"
 
 # Run icount measurments inside docker container. Output will be in stdout.
 cargo run --release -p runtime-params-estimator --features required -- \
-    --vm-kind wasmer2 \
+    ${COMMON_ARGS} \
     --metric icount --docker --full \
     | tee >(while read LINE; do echo $LINE | tr -d _ | awk -v git_hash="${GIT_HASH}" -f insert_icount.awk | sqlite3 ${SQLI_DB}; done);
     
 # Run time measurements normally (outside docker). Output is in stderr.
 cargo run --release -p runtime-params-estimator --features required -- \
-    --vm-kind wasmer2 \
+    ${COMMON_ARGS} \
     --metric time \
     2> >(tee /dev/stderr | while read LINE; do echo $LINE | tr -d _ | awk -v git_hash="${GIT_HASH}" -f insert_time.awk | sqlite3 ${SQLI_DB}; done);
