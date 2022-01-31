a01b752be743f95e0c59ddda759726e7ee6dcbf1
Longarithm <the.aleksandr.logunov@gmail.com>
2022-01-31 15:26:56 +0300

    introduce protocol feature

--- core/store/src/trie/mod.rs
+++ core/store/src/trie/mod.rs
@@ -617,6 +617,8 @@ impl Trie {
             return storage.chargeable_retrieve_raw_bytes(hash).map(|(value, need_charge)| {
                 if need_charge {
                     self.counter.increment();
+                } else {
+                    tracing::debug!(target: "trie", "saved on hash", hash = %hash);
                 }
                 value
             });
@@ -650,7 +652,7 @@ impl Trie {
     ) -> Result<Option<(u32, CryptoHash)>, StorageError> {
         let mut hash = *root;
         let result = key.until_offset(true);
-        tracing::debug!(target: "trie", "key in lookup: {:?}", key = result);
+        tracing::debug!(target: "trie", "key in lookup", key = ?result, hash = %hash);
 
         loop {
             if hash == Trie::empty_root() {
@@ -658,7 +660,7 @@ impl Trie {
             }
             let bytes = self.retrieve_raw_bytes(&hash)?;
             let result = key.until_offset(false);
-            tracing::debug!(target: "trie", "key in lookup: {:?}", key = result);
+            tracing::debug!(target: "trie", "key in lookup", key = ?result, hash = %hash);
 
             let node = RawTrieNodeWithSize::decode(&bytes).map_err(|_| {
                 StorageError::StorageInconsistentState("RawTrieNode decode failed".to_string())
