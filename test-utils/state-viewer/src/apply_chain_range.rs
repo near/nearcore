5f3fd29b7707288505921fb6dc73be9520f8175b
Longarithm <the.aleksandr.logunov@gmail.com>
2022-02-03 00:57:16 +0300

    compilation fix

--- test-utils/state-viewer/src/apply_chain_range.rs
+++ test-utils/state-viewer/src/apply_chain_range.rs
@@ -252,8 +252,8 @@ pub fn apply_chain_range(
                 apply_result.total_balance_burnt,
             );
 
-            let state_update = runtime_adapter.get_tries().new_trie_update(shard_uid, *chunk_extra.state_root());
-            let delayed_indices = get::<DelayedReceiptIndices>(&state_update, &TrieKey::DelayedReceiptIndices).unwrap();
+            // let state_update = runtime_adapter.get_tries().new_trie_update(shard_uid, *chunk_extra.state_root());
+            let delayed_indices = None; //get::<DelayedReceiptIndices>(&state_update, &TrieKey::DelayedReceiptIndices).unwrap();
 
         match existing_chunk_extra {
             Some(existing_chunk_extra) => {
