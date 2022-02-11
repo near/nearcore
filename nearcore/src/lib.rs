25fc76cfef08af9395d500e707f7a13e9643e17c
Edvard Davtyan <davtyaneg@gmail.com>
2022-02-11 12:38:26 +0000

    Tiny change in the error messages

--- nearcore/src/lib.rs
+++ nearcore/src/lib.rs
@@ -76,7 +76,7 @@ pub fn get_default_home() -> PathBuf {
 
 mod r#impl {
     const MIGRATION_IS_IN_PROGRESS_STORE_KEY: &'static [u8] = b"migration_is_in_progress";
-    const STORE_IS_PROBABLY_BROKEN_STORE_KEY: &'static [u8] = b"store_is_probably_broken";
+    const STORE_IS_PROBABLY_CORRUPTED_STORE_KEY: &'static [u8] = b"store_is_probably_corrupted";
     use near_store::Store;
     use near_store::db::DBCol::ColBlockMisc;
     use tracing::error;
@@ -105,15 +105,15 @@ mod r#impl {
     }
 
     pub fn check_if_migration_is_in_progress(store: &Store, fail_if_migration_is_in_progress: bool) {
-        let store_is_probably_broken =
-            match store.get_ser::<bool>(ColBlockMisc, &STORE_IS_PROBABLY_BROKEN_STORE_KEY) {
+        let store_is_probably_corrupted =
+            match store.get_ser::<bool>(ColBlockMisc, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY) {
                 Ok(Some(x)) => x,
                 Ok(None) => false,
                 Err(e) => panic!("Can't read DB, {:?}", e),
             };
-        if store_is_probably_broken {
+        if store_is_probably_corrupted {
             error!(concat!("The state was earlier migrated while another migration was interrupted",
-                           " so the current state can be broken and lead to undefined failures."));
+                           " so the current state can be corrupted and lead to undefined failures."));
         }
 
         let migration_is_in_progress =
@@ -132,7 +132,7 @@ mod r#impl {
                        "the flag fail_if_migration_is_in_progress is enabled"));
             } else {
                 let mut store_update = store.store_update();
-                store_update.set_ser(ColBlockMisc, &STORE_IS_PROBABLY_BROKEN_STORE_KEY, &true).unwrap();
+                store_update.set_ser(ColBlockMisc, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY, &true).unwrap();
                 store_update.commit().unwrap();
             }
         }
