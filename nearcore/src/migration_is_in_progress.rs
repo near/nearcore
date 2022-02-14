2cb90e9c714e63d5d45647f5f4226085032261e7
Edvard Davtyan <davtyaneg@gmail.com>
2022-02-14 08:30:40 +0000

    Refactor code in migration_is_in_progress

--- nearcore/src/migration_is_in_progress.rs
+++ nearcore/src/migration_is_in_progress.rs
@@ -5,6 +5,20 @@ use tracing::{error, info};
 const MIGRATION_IS_IN_PROGRESS_STORE_KEY: &'static [u8] = b"migration_is_in_progress";
 const STORE_IS_PROBABLY_CORRUPTED_STORE_KEY: &'static [u8] = b"store_is_probably_corrupted";
 
+fn get_bool_flag_from_db(store: &Store, key: &[u8]) -> bool {
+    match store.get_ser::<bool>(ColBlockMisc, &key) {
+        Ok(Some(x)) => x,
+        Ok(None) => false,
+        Err(e) => panic!("Can't read DB, {:?}", e),
+    }
+}
+
+fn set_bool_flag_in_db(store: &Store, key: &[u8], value: bool) {
+    let mut store_update = store.store_update();
+    store_update.set_ser(ColBlockMisc, &key, &value).unwrap();
+    store_update.commit().unwrap();
+}
+
 /// This is a RAII class to mark in the state an ongoing DB migration.
 /// It's needed to notify the user about DB probably being corrupted.
 /// Possible scenarios: the user interruted node startup during the migration or the migration
@@ -16,9 +30,7 @@ pub struct MigrationIsInProgress<'a> {
 
 impl<'a> MigrationIsInProgress<'a> {
     pub fn new(store: &'a Store, current_version: u32) -> MigrationIsInProgress {
-        let mut store_update = store.store_update();
-        store_update.set_ser(ColBlockMisc, &MIGRATION_IS_IN_PROGRESS_STORE_KEY, &true).unwrap();
-        store_update.commit().unwrap();
+        set_bool_flag_in_db(&store, &MIGRATION_IS_IN_PROGRESS_STORE_KEY, true);
         info!(target: "near", concat!("DB migration (v{} to v{}) is in progress please don't interrupt ",
                       " the node otherwise DB can become corrupted."),
                       current_version, current_version + 1);
@@ -29,11 +41,7 @@ impl<'a> MigrationIsInProgress<'a> {
 impl<'a> Drop for MigrationIsInProgress<'a> {
     fn drop(&mut self) {
         if !std::thread::panicking() {
-            let mut store_update = self.store.store_update();
-            store_update
-                .set_ser(ColBlockMisc, &MIGRATION_IS_IN_PROGRESS_STORE_KEY, &false)
-                .unwrap();
-            store_update.commit().unwrap();
+            set_bool_flag_in_db(&self.store, &MIGRATION_IS_IN_PROGRESS_STORE_KEY, false);
             info!(target: "near", "DB migration (v{} to v{}) succeeded.", self.current_version, self.current_version + 1);
         }
     }
@@ -43,11 +51,7 @@ impl<'a> Drop for MigrationIsInProgress<'a> {
 /// The behaviour (panic or ignore) can be controlled with the config flag fail_if_migration_is_in_progress.
 pub fn check_if_migration_is_in_progress(store: &Store, fail_if_migration_is_in_progress: bool) {
     let store_is_probably_corrupted =
-        match store.get_ser::<bool>(ColBlockMisc, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY) {
-            Ok(Some(x)) => x,
-            Ok(None) => false,
-            Err(e) => panic!("Can't read DB, {:?}", e),
-        };
+        get_bool_flag_from_db(&store, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY);
     if store_is_probably_corrupted {
         error!(target: "near", concat!("DB was earlier migrated while another migration was interrupted",
                        " so the current state can be corrupted and lead to undefined failures, ",
@@ -55,12 +59,7 @@ pub fn check_if_migration_is_in_progress(store: &Store, fail_if_migration_is_in_
     }
 
     let migration_is_in_progress =
-        match store.get_ser::<bool>(ColBlockMisc, &MIGRATION_IS_IN_PROGRESS_STORE_KEY) {
-            Ok(Some(x)) => x,
-            Ok(None) => false,
-            Err(e) => panic!("Can't read DB, {:?}", e),
-        };
-
+        get_bool_flag_from_db(&store, &MIGRATION_IS_IN_PROGRESS_STORE_KEY);
     if migration_is_in_progress {
         error!(target: "near", concat!("DB migration was earlier interrupted so the current state can ",
                "be corrupted, please consider recovering the state from backup."));
@@ -70,11 +69,7 @@ pub fn check_if_migration_is_in_progress(store: &Store, fail_if_migration_is_in_
                 "rerun with --fail_if_migration_is_in_progress=false."
             ));
         } else {
-            let mut store_update = store.store_update();
-            store_update
-                .set_ser(ColBlockMisc, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY, &true)
-                .unwrap();
-            store_update.commit().unwrap();
+            set_bool_flag_in_db(&store, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY, true);
         }
     }
 }
@@ -82,19 +77,10 @@ pub fn check_if_migration_is_in_progress(store: &Store, fail_if_migration_is_in_
 #[cfg(test)]
 mod tests {
     use crate::create_store;
-    use crate::migration_is_in_progress::MIGRATION_IS_IN_PROGRESS_STORE_KEY;
-    use crate::migration_is_in_progress::STORE_IS_PROBABLY_CORRUPTED_STORE_KEY;
-    use crate::{check_if_migration_is_in_progress, MigrationIsInProgress};
-    use near_store::ColBlockMisc;
-    use near_store::Store;
-
-    fn get_bool_flag_from_db(store: &Store, key: &[u8]) -> bool {
-        match store.get_ser::<bool>(ColBlockMisc, &key) {
-            Ok(Some(x)) => x,
-            Ok(None) => false,
-            Err(e) => panic!("Can't read DB, {:?}", e),
-        }
-    }
+    use crate::migration_is_in_progress::{
+        check_if_migration_is_in_progress, get_bool_flag_from_db, MigrationIsInProgress,
+        MIGRATION_IS_IN_PROGRESS_STORE_KEY, STORE_IS_PROBABLY_CORRUPTED_STORE_KEY,
+    };
 
     #[test]
     fn test_migration_is_in_progress_set() {
@@ -117,7 +103,7 @@ mod tests {
     }
 
     #[test]
-    fn test_check_if_migration_is_in_progress_no_panic() {
+    fn test_migration_is_in_progress_check_if_migration_is_in_progress_no_panic() {
         let tmp_dir =
             tempfile::Builder::new().prefix("_test_migration_is_in_progress").tempdir().unwrap();
         let store = create_store(tmp_dir.path());
@@ -127,7 +113,7 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_check_if_migration_is_in_progress_panic() {
+    fn test_migration_is_in_progress_check_if_migration_is_in_progress_panic() {
         let tmp_dir =
             tempfile::Builder::new().prefix("_test_migration_is_in_progress").tempdir().unwrap();
         let store = create_store(tmp_dir.path());
@@ -136,12 +122,13 @@ mod tests {
     }
 
     #[test]
-    fn test_store_is_probably_corrupted_set() {
+    fn test_migration_is_in_progress_store_is_probably_corrupted_set_and_no_panic() {
         let tmp_dir =
             tempfile::Builder::new().prefix("_test_migration_is_in_progress").tempdir().unwrap();
         let store = create_store(tmp_dir.path());
         let _migration_is_in_progress = MigrationIsInProgress::new(&store, 1);
         check_if_migration_is_in_progress(&store, false);
         assert_eq!(get_bool_flag_from_db(&store, &STORE_IS_PROBABLY_CORRUPTED_STORE_KEY), true);
+        check_if_migration_is_in_progress(&store, false);
     }
 }
