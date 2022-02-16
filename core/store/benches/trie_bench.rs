fef9bf061026a9b25785c7283efd5713b888bb4d
Longarithm <the.aleksandr.logunov@gmail.com>
2022-02-16 22:15:14 +0300

    revert bench updates

--- core/store/benches/trie_bench.rs
+++ core/store/benches/trie_bench.rs
@@ -2,8 +2,7 @@
 extern crate bencher;
 
 use bencher::Bencher;
-use rand::{random, Rng};
-use std::time::Instant;
+use rand::random;
 
 use near_primitives::shard_layout::ShardUId;
 use near_store::test_utils::create_tries;
@@ -13,39 +12,25 @@ fn rand_bytes() -> Vec<u8> {
     (0..10).map(|_| random::<u8>()).collect()
 }
 
-fn rand_nibbles() -> Vec<u8> {
-    (0..10)
-        .map(|_| rand::thread_rng().gen_range(0, 2) + 16 * rand::thread_rng().gen_range(0, 2))
-        .collect()
-}
-
-fn long_rand_bytes() -> Vec<u8> {
-    (0..1000).map(|_| random::<u8>()).collect()
-}
-
 fn trie_lookup(bench: &mut Bencher) {
     let tries = create_tries();
     let trie = tries.get_trie_for_shard(ShardUId::single_shard());
     let root = Trie::empty_root();
     let mut changes = vec![];
-    for _ in 0..3000 {
-        changes.push((rand_nibbles(), Some(rand_bytes())));
+    for _ in 0..100 {
+        changes.push((rand_bytes(), Some(rand_bytes())));
     }
     let other_changes = changes.clone();
     let trie_changes = trie.update(&root, changes.drain(..)).unwrap();
     let (state_update, root) = tries.apply_all(&trie_changes, ShardUId::single_shard()).unwrap();
     state_update.commit().expect("Failed to commit");
 
-    let f = || {
-        for (key, _) in other_changes.iter() {
-            trie.get(&root, key).unwrap();
+    bench.iter(|| {
+        for _ in 0..1 {
+            for (key, _) in other_changes.iter() {
+                trie.get(&root, key).unwrap();
+            }
         }
-    };
-    bench.iter(move || {
-        let start = Instant::now();
-        f();
-        let took = start.elapsed();
-        println!("took {:?}", took);
     });
 }
 
