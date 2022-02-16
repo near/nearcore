c6f8cac704198874dea9872c1310630e776b2ce1
Longarithm <the.aleksandr.logunov@gmail.com>
2022-02-16 21:27:55 +0300

    bench update

--- core/store/benches/trie_bench.rs
+++ core/store/benches/trie_bench.rs
@@ -29,13 +29,14 @@ fn trie_lookup(bench: &mut Bencher) {
     let (state_update, root) = tries.apply_all(&trie_changes, ShardUId::single_shard()).unwrap();
     state_update.commit().expect("Failed to commit");
 
-    bench.bench_n(2, |bench| {
-        bench.iter(|| {
-            for (key, _) in other_changes.iter() {
-                trie.get(&root, key).unwrap();
-            }
-        })
-    });
+    let f = || {
+        for (key, _) in other_changes.iter() {
+            trie.get(&root, key).unwrap();
+        }
+    };
+    for _ in 0..2 {
+        bench.iter(f);
+    }
 }
 
 fn trie_update(bench: &mut Bencher) {
