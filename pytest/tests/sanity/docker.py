a1086c8f3a448a8b98160dfb9aa96d453bdf4328
Michal Nazarewicz <mina86@mina86.com>
2022-02-21 14:13:13 +0100

    Test `docker run` without `sh -c`

--- pytest/tests/sanity/docker.py
+++ pytest/tests/sanity/docker.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 """Verifies that node can be started inside of a Docker image.
 
-The script builds Docker image using ‘make docker’ command and then starts
-a cluster with all nodes running inside of containers.  To see if the cluster
-works correctly, the test simply waits for a several blocks to be generated and
-then interrogates each node about that block expecting all of them to return the
-same data.
+The script builds Docker image using ‘make docker-nearcore’ command and then
+starts a cluster with all nodes running inside of containers.  As sanity check
+to see if the cluster works correctly, the test waits for a several blocks and
+then interrogates each node about block with specified hash expecting all of
+them to return the same data.
+
+The purpose of the test is to verify that:
+- `make docker-nearcore` builds a working Docker image,
+- `docker run ... nearcore` (i.e. the `run_docker.sh` script) works,
+- `docker run -eBOOT_NODE=... ... nearcore` (i.e. passing boot nodes via
+  BOOT_NODE environment variable) works and
+- `docker run ... nearcore sh -c 'neard ...'` works.
+
+Note that
 """
 
 import os
@@ -22,8 +31,8 @@ import cluster
 from configured_logger import logger
 import utils
 
-NUM_NODES = 2
-BLOCKS = 42
+BLOCKS = 10
+NUM_NODES = 42
 
 _REPO_DIR = pathlib.Path(__file__).resolve().parents[3]
 _CD_PRINTED = False
@@ -53,17 +62,21 @@ def run(cmd: _Command, *, capture_output: bool = False) -> typing.Optional[str]:
     return None
 
 
-def docker_run(shell_cmd: str,
+def docker_run(shell_cmd: typing.Optional[str] = None,
                *,
                detach: bool = False,
                network: bool = False,
-               home: pathlib.Path) -> typing.Optional[str]:
+               home: pathlib.Path,
+               container_home: str = '/home/near',
+               env: typing.Dict[str, str] = {}) -> typing.Optional[str]:
     """Runs a `docker run` command.
 
     Args:
-        shell_cmd: A shell command to execute inside of the container.  It’s
-            going to be run using `sh -c` and it’s caller’s responsibility to
-            make sure all data inside is properly sanitised.
+        shell_cmd: Optionally a shell command to execute inside of the
+            container.  It’s going to be run using `sh -c` and it’s caller’s
+            responsibility to make sure all data inside is properly sanitised.
+            If not given, command configured via CMD when building the container
+            will be executed as typical for Docker images.
         detach: Whether the `docker run` command should detach or not.  If
             False, standard output and standard error will be passed through to
             test outputs.  If True, the container will be detached and the
@@ -71,7 +84,10 @@ def docker_run(shell_cmd: str,
         network: Whether to enable network.  If True, the container will be
             configured to use host’s network.  This allows processes in
             different containers to connect with each other easily.
-        home: A directory to mount as /home/near volume inside of the container.
+        home: A directory to mount as `container_home` inside of the container.
+        container_home: Path within the container where which `home` directory
+            is mounted.
+        env: *Additional* environment variables set inside of the container.
 
     Returns:
         Command’s stripped standard output if `detach` is true, None otherwise.
@@ -90,28 +106,32 @@ def docker_run(shell_cmd: str,
         # inside of the containers to communicate with each other.
         cmd.append('--network=host')
     else:
+        # The command does not need networking so disable it.
         cmd.append('--network=none')
 
-    # Map home to /home/near and since the command will not modify anything else
-    # run in read only mode.
-    cmd.extend(('--read-only', '-v', f'{home}:/home/near'))
+    # Map home to /home/near or /srv/near and since the command will not modify
+    # anything else run in read only mode.
+    cmd.extend(('--read-only', f'-v{home}:{container_home}'))
 
     # Use current user to run the code inside the container so that data saved
     # in home will be readable by us outside of the container (otherwise, the
     # command would be run as root and data would be owned by root).
     cmd.extend(('-u', f'{os.getuid()}:{os.getgid()}', '--userns=host'))
 
-    # Configure logging and debugging via environment variables.
+    # Set environment variables.
     rust_log = ('actix_web=warn,mio=warn,tokio_util=warn,'
                 'actix_server=warn,actix_http=warn,' +
                 os.environ.get('RUST_LOG', 'debug'))
-    cmd.extend(('-e', 'RUST_BACKTRACE=1', '-e', f'RUST_LOG={rust_log}'))
+    cmd.extend(('-eRUST_BACKTRACE=1', f'-eRUST_LOG={rust_log}'))
+    for key, value in env.items():
+        cmd.append((f'-e{key}={value}'))
 
     # Specify the image to run.
     cmd.append('nearcore')
 
     # And finally, specify the command.
-    cmd.extend(('sh', '-c', shell_cmd))
+    if shell_cmd:
+        cmd.extend(('sh', '-c', shell_cmd))
 
     return run(cmd, capture_output=detach)
 
@@ -127,16 +147,21 @@ class DockerNode(cluster.LocalNode):
                          blacklist=[])
         self._container_id = None
 
-    def start(self, boot_node: cluster.BootNode = None) -> None:
+    def start(self, *, boot_node: cluster.BootNode = None) -> None:
+        """Starts a node inside of a Docker container.
+
+        Args:
+            boot_node: Optional boot node to pass to the node.
+        """
         assert self._container_id is None
         assert not self.cleaned
 
-        script = 'neard --home /home/near run '
-        script += ' '.join(
-            shlex.quote(arg) for arg in cluster.make_boot_nodes_arg(boot_node))
-        script += '>/home/near/stdout 2>/home/near/stderr'
+        env = {}
+        if boot_node:
+            env['BOOT_NODES'] = cluster.make_boot_nodes_arg(boot_node)[1]
 
-        cid = docker_run(script, detach=True, network=True, home=self.node_dir)
+        cid = docker_run(detach=True, network=True, home=self.node_dir,
+                         container_home='/srv/near', env=env)
         self._container_id = cid
         logger.info(f'Node started in Docker container {cid}')
 
@@ -147,10 +172,24 @@ class DockerNode(cluster.LocalNode):
             logger.info(f'Stopping container {cid}')
             run(('docker', 'stop', cid))
 
+    __WARN_LOGS = True
+
+    def output_logs(self):
+        # Unfortunately because we’re running the containers as detached
+        # anything neard writes to stdout and stderr is lost.  We could start
+        # nodes using `docker run -d ... sh -c 'neard ... >stdout 2>stderr'` but
+        # that would mean that we’re not testing the `run_docker.sh` script
+        # which we do want to read.
+        if self.__WARN_LOGS:
+            logger.info(
+                'Due to technical libation logs from node is not available')
+            type(self).__WARN_LOGS = False
+        pass
+
 
 def main():
     # Build the container
-    run(('make', 'docker-nearcore'))
+    # run(('make', 'docker-nearcore'))
 
     dot_near = pathlib.Path.home() / '.near'
 
@@ -158,38 +197,50 @@ def main():
     cmd = f'neard --home /home/near localnet --v {NUM_NODES} --prefix test'
     docker_run(cmd, home=dot_near)
 
-    # Start all the nodes
     nodes = []
-    for ordinal in range(NUM_NODES):
-        logger.info(f'Starting node {ordinal}')
-        node = DockerNode(ordinal, dot_near / f'test{ordinal}')
-        node.start(boot_node=nodes)
-        nodes.append(node)
+    try:
+        # Start all the nodes
+        for ordinal in range(NUM_NODES):
+            logger.info(f'Starting node {ordinal}')
+            node = DockerNode(ordinal, dot_near / f'test{ordinal}')
+            node.start(boot_node=nodes)
+            nodes.append(node)
 
-    # Wait for them to initialise
-    for ordinal, node in enumerate(nodes):
-        logger.info(f'Waiting for node {ordinal} to respond')
-        node.wait_for_rpc(10)
+        # Wait for them to initialise
+        for ordinal, node in enumerate(nodes):
+            logger.info(f'Waiting for node {ordinal} to respond')
+            node.wait_for_rpc(10)
 
-    # Wait for BLOCKS blocks to be generated
-    latest = utils.wait_for_blocks(nodes[0], target=BLOCKS)
+        # Wait for BLOCKS blocks to be generated
+        latest = utils.wait_for_blocks(nodes[0], target=BLOCKS)
 
-    # Fetch latest block from all the nodess
-    blocks = []
-    for ordinal, node in enumerate(nodes):
-        utils.wait_for_blocks(node, target=latest.height)
-        response = node.get_block(latest.hash)
-        assert 'result' in response, (ordinal, block)
-        block = response['result']
-        blocks.append(block)
-        bid = cluster.BlockId.from_header(block['header'])
-        logger.info(f'Node {ordinal} sees block: {bid}')
+        # Fetch latest block from all the nodess
+        blocks = []
+        for ordinal, node in enumerate(nodes):
+            utils.wait_for_blocks(node, target=latest.height)
+            response = node.get_block(latest.hash)
+            assert 'result' in response, (ordinal, block)
+            block = response['result']
+            blocks.append(block)
+            bid = cluster.BlockId.from_header(block['header'])
+            logger.info(f'Node {ordinal} sees block: {bid}')
 
-    # All blocks should be equal
-    for ordinal in range(1, NUM_NODES):
-        assert blocks[0] == blocks[1], (ordinal, blocks)
+        # All blocks should be equal
+        for ordinal in range(1, NUM_NODES):
+            assert blocks[0] == blocks[1], (ordinal, blocks)
 
-    logger.info(f'All good')
+        logger.info(f'All good')
+
+    finally:
+        # `docker stop` takes a few seconds so stop all containers in parallel.
+        # atexit we’ll call DockerNode.cleanup method for each node as well and
+        # it’ll handle all the other cleanups.
+        cids = tuple(filter(None, (node._container_id for node in nodes)))
+        if cids:
+            logger.info('Stopping containers')
+            run(('docker', 'stop') + cids)
+        for node in nodes:
+            node._container_id = None
 
 
 if __name__ == '__main__':
