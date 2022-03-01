af49f2c76d25bd93d46c0cdbf27a0741bc4225d0
Marcin Michalski <michalski@near.org>
2022-03-01 14:18:53 +0100

    adding command line flags

--- pytest/tests/loadtest/README.md
+++ pytest/tests/loadtest/README.md
@@ -2,6 +2,10 @@
 
 This test consists of two parts:
 
+./target/debug/neard --home ~/.near_tmp/7 init --chain-id localnet --num-shards=5
+
+
+
 ./setup.sh / setup.py -- is creating the test (compiling the contract, creating accounts and deploying it).
 
 ./loadtest.py is the test itself, that tries to schedule as many transactions  as possible.
--- pytest/tests/loadtest/setup.py
+++ pytest/tests/loadtest/setup.py
@@ -1,29 +1,51 @@
+import subprocess
 import mocknet_helpers
 import account
 import key
+import argparse
+from os.path import join
 
-validator_key = key.Key.from_json_file(
-    "/Users/michalski/.near_tmp/6/validator_key.json")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Setup loadtest')
 
-base_block_hash = mocknet_helpers.get_latest_block_hash()
-nonce = mocknet_helpers.get_nonce_for_key(validator_key)
+    parser.add_argument('--home', type=str, required=True)
+    parser.add_argument('--num_accounts', type=int, default=5)
+    parser.add_argument('--contract_dir',
+                        type=str,
+                        default='pytest/tests/loadtest/contract')
+    args = parser.parse_args()
 
-my_account = account.Account(validator_key,
-                             init_nonce=nonce,
-                             base_block_hash=base_block_hash,
-                             rpc_infos=[("localhost", "3030")])
+    print("Compiling contract")
+    subprocess.check_call(args=[
+        "cargo", "build", "--target", "wasm32-unknown-unknown", "--release"
+    ],
+                          cwd=args.contract_dir)
 
-account_name = "shard12.test.near"
-tx = my_account.send_create_account_tx(account_name)
-print(tx)
-account_key = key.Key(account_name, validator_key.pk, validator_key.sk)
-base_block_hash = mocknet_helpers.get_latest_block_hash()
+    validator_key = key.Key.from_json_file(join(args.home,
+                                                "validator_key.json"))
 
-new_account = account.Account(account_key,
-                              mocknet_helpers.get_nonce_for_key(account_key),
-                              base_block_hash=base_block_hash,
-                              rpc_infos=[("localhost", "3030")])
+    base_block_hash = mocknet_helpers.get_latest_block_hash()
+    nonce = mocknet_helpers.get_nonce_for_key(validator_key)
 
-new_account.send_deploy_contract_tx(
-    "pytest/tests/loadtest/contract/target/wasm32-unknown-unknown/release/loadtest_contract.wasm"
-)
+    my_account = account.Account(validator_key,
+                                 init_nonce=nonce,
+                                 base_block_hash=base_block_hash,
+                                 rpc_infos=[("localhost", "3030")])
+
+    print(f"Creating {args.num_accounts} accounts.")
+    for i in range(args.num_accounts):
+        account_name = f"shard{i}.test.near"
+        tx = my_account.send_create_account_tx(account_name)
+        print(f"Created account {tx}")
+        account_key = key.Key(account_name, validator_key.pk, validator_key.sk)
+        base_block_hash = mocknet_helpers.get_latest_block_hash()
+        new_account = account.Account(
+            account_key,
+            mocknet_helpers.get_nonce_for_key(account_key),
+            base_block_hash=base_block_hash,
+            rpc_infos=[("localhost", "3030")])
+
+        new_account.send_deploy_contract_tx(
+            join(
+                args.contract_dir,
+                "target/wasm32-unknown-unknown/release/loadtest_contract.wasm"))
