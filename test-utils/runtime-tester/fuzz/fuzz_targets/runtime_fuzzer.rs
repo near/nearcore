4eae9c7b268abd1327e05f15b660aec61c5300a5
Léo Gaspard <leo@gaspard.io>
2022-03-02 13:40:40 +0100

    fuzz: avoid runtime-tester false positives
    System time is a high-variability metric. CPU time consumed by the
    process should be much less volatile, and so hopefully will lead to both
    fewer false positives, but also to a lesser need for safety margins,
    thus allowing the lowering of 3 seconds to 2 seconds when fuzzing.

--- test-utils/runtime-tester/fuzz/fuzz_targets/runtime_fuzzer.rs
+++ test-utils/runtime-tester/fuzz/fuzz_targets/runtime_fuzzer.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 fn do_fuzz(scenario: &Scenario) -> Result<(), String> {
     let stats = scenario.run().result.map_err(|e| format!("{}", e))?;
     for block_stats in stats.blocks_stats {
-        if block_stats.block_production_time > Duration::from_secs(3) {
+        if block_stats.block_production_time > Duration::from_secs(2) {
             return Err(format!(
                 "block at height {} was produced in {:?}",
                 block_stats.height, block_stats.block_production_time
