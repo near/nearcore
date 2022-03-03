b19c0c9b9db4b6d84755c2fa3e3d0c62f84d107f
mzhangmzz <34969888+mzhangmzz@users.noreply.github.com>
2022-03-03 14:54:04 -0500

    mock network first iteration (#6340)
    mock network first iteration

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -3016,6 +3016,37 @@ impl Chain {
             .ok_or_else(|| ErrorKind::DBNotFoundErr(format!("EXECUTION OUTCOME: {}", id)).into())
     }
 
+    /// Retrieve the up to `max_headers_returned` headers on the main chain
+    /// `hashes`: a list of block "locators". `hashes` should be ordered from older blocks to
+    ///           more recent blocks. This function will find the first block in `hashes`
+    ///           that is on the main chain and returns the blocks after this block. If none of the
+    ///           blocks in `hashes` are on the main chain, the function returns an empty vector.
+    pub fn retrieve_headers(
+        &mut self,
+        hashes: Vec<CryptoHash>,
+        max_headers_returned: u64,
+        max_height: Option<BlockHeight>,
+    ) -> Result<Vec<BlockHeader>, Error> {
+        let header = match self.find_common_header(&hashes) {
+            Some(header) => header,
+            None => return Ok(vec![]),
+        };
+
+        let mut headers = vec![];
+        let header_head_height = self.header_head()?.height;
+        let max_height = max_height.unwrap_or(header_head_height);
+        // TODO: this may be inefficient if there are a lot of skipped blocks.
+        for h in header.height() + 1..=max_height {
+            if let Ok(header) = self.get_header_by_height(h) {
+                headers.push(header.clone());
+                if headers.len() >= max_headers_returned as usize {
+                    break;
+                }
+            }
+        }
+        Ok(headers)
+    }
+
     /// Returns a vector of chunk headers, each of which corresponds to the previous chunk of
     /// a chunk in the block after `prev_block`
     /// This function is important when the block after `prev_block` has different number of chunks
