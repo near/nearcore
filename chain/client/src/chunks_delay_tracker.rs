892200a6ead737e3b22c0ec84e3676a3a64c6845
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-03-07 13:30:45 +0100

    Rename to `blocks_in_progress`.

--- chain/client/src/chunks_delay_tracker.rs
+++ chain/client/src/chunks_delay_tracker.rs
@@ -5,39 +5,29 @@ use near_primitives::types::{BlockHeight, ShardId};
 
 use crate::metrics;
 
-/// Provides monitoring information about the delays of receiving blocks and their corresponding chunks.
-/// Keeps timestamps of a limited number of blocks before the current head.
-/// Keeps timestamps of all blocks and chunks from the future.
-/// Doesn't make any assumptions about shard layout or the number of shards. If the head was moved forward, we assume that all requirements were satisfied.
-/// This object provides two metrics:
-/// 1) max delay between discovering a block and receiving the last chunk from that block.
-/// 2) the difference between the latest known block and the current head.
-/// If a chunk or a block is received multiple times, only the first time is recorded.
+/// Provides monitoring information about the delays of receiving and requesting blocks and their corresponding chunks.
+/// Keeps timestamps for all future blocks and chunks that were requested or received.
+/// Updates the metrics when the head gets updated. Assumes that if the head moves forward, all needed chunks were received.
+/// If a chunk or a block is received or requested multiple times, only the first time is recorded.
 #[derive(Debug, Default)]
 pub(crate) struct ChunksDelayTracker {
-    heights: BTreeMap<BlockHeight, HeightInfo>,
+    blocks_in_progress: BTreeMap<BlockHeight, BlockInProgress>,
 }
 
 #[derive(Debug, Default)]
-struct HeightInfo {
+struct BlockInProgress {
+    /// Timestamp of trying to process a block for the first time.
     block_received: Option<Instant>,
+    /// Timestamps of receiving each of the shards in a block.
     chunks_received: HashMap<ShardId, Instant>,
+    /// Timestamps of requesting missing chunks.
+    /// Chunks are usually requested when a client falls out-of-sync with the head of the chain.
     chunks_requested: HashMap<ShardId, Instant>,
 }
 
 impl ChunksDelayTracker {
-    fn remove_old_entries(&mut self, head_height: BlockHeight) {
-        let heights_to_drop: Vec<BlockHeight> =
-            self.heights.range(..head_height).map(|(&k, _v)| k).collect();
-        for height in heights_to_drop {
-            self.update_block_chunks_metric(height);
-            self.update_chunks_metric(height);
-            self.heights.remove(&height);
-        }
-    }
-
     fn update_block_chunks_metric(&mut self, height: BlockHeight) {
-        if let Some(entry) = self.heights.get(&height) {
+        if let Some(entry) = self.blocks_in_progress.get(&height) {
             if let Some(block_received) = entry.block_received {
                 for (shard_id, received) in &entry.chunks_received {
                     metrics::BLOCK_CHUNKS_DELAY
@@ -49,7 +39,7 @@ impl ChunksDelayTracker {
     }
 
     fn update_chunks_metric(&mut self, height: BlockHeight) {
-        if let Some(entry) = self.heights.get(&height) {
+        if let Some(entry) = self.blocks_in_progress.get(&height) {
             for (shard_id, requested) in &entry.chunks_requested {
                 if let Some(received) = entry.chunks_received.get(&shard_id) {
                     metrics::CHUNK_DELAY
@@ -61,7 +51,7 @@ impl ChunksDelayTracker {
     }
 
     pub fn add_block_timestamp(&mut self, height: BlockHeight, timestamp: Instant) {
-        self.heights.entry(height).or_default().block_received.get_or_insert(timestamp);
+        self.blocks_in_progress.entry(height).or_default().block_received.get_or_insert(timestamp);
     }
 
     pub fn add_chunk_timestamp(
@@ -70,7 +60,7 @@ impl ChunksDelayTracker {
         shard_id: ShardId,
         timestamp: Instant,
     ) {
-        self.heights
+        self.blocks_in_progress
             .entry(height)
             .or_default()
             .chunks_received
@@ -78,12 +68,18 @@ impl ChunksDelayTracker {
             .or_insert(timestamp);
     }
 
-    pub fn processed_block(&mut self, height: BlockHeight) {
-        self.remove_old_entries(height);
+    pub fn processed_block(&mut self, processed_block_height: BlockHeight) {
+        let heights_to_drop: Vec<BlockHeight> =
+            self.blocks_in_progress.range(..=processed_block_height).map(|(&k, _v)| k).collect();
+        for height in heights_to_drop {
+            self.update_block_chunks_metric(height);
+            self.update_chunks_metric(height);
+            self.blocks_in_progress.remove(&height);
+        }
     }
 
     pub fn requested_chunk(&mut self, height: BlockHeight, shard_id: ShardId, timestamp: Instant) {
-        self.heights
+        self.blocks_in_progress
             .entry(height)
             .or_default()
             .chunks_requested
