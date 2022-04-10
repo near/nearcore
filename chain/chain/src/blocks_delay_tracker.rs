near/nearcore

commit ed1a1b600bb42b8727f437e8fbeecb9ac58e3fb8

add metrics for how long a block stay in the orphan pool and the miss… (#6523)
…ing chunks pool

I also reorganized the original ChunksDelayTracker a little, but didn't change any of the essential logic.
- I renamed ChunksDelayTracker to BlocksDelayTracker, as now it includes information about various important timestamps in blocks lifetime, instead of just tracking chunks delays. 
- I renamed a few methods.
- I had to move it from client to chain because I need to access it in chain.

chain/chain/src/blocks_delay_tracker.rs
@@ -1,0 +1,184 @@
+use itertools::Itertools;
+use near_primitives::block::Block;
+use near_primitives::hash::CryptoHash;
+use near_primitives::sharding::ChunkHash;
+use near_primitives::types::{BlockHeight, ShardId};
+use std::collections::HashMap;
+use std::time::Instant;
+use tracing::error;
+
+use crate::metrics;
+
+/// Provides monitoring information about the important timestamps throughout the lifetime of
+/// blocks and chunks. It keeps information of all pending blocks and chunks that have not been fully processed yet.
+/// A block is added the first time it is received and removed when it finishes processing
+/// A chunk is added the first time it is requested and removed when the block it belongs finishes processing
+#[derive(Debug, Default)]
+pub struct BlocksDelayTracker {
+    /// Contains only blocks that are not yet processed.
+    pub blocks_in_progress: HashMap<CryptoHash, BlockInProgress>,
+    /// An entry gets created when a chunk gets requested for the first time.
+    /// Chunks get deleted when the block gets processed.
+    pub chunks_in_progress: HashMap<ChunkHash, ChunkInProgress>,
+}
+
+#[derive(Debug)]
+pub struct BlockInProgress {
+    /// Timestamp when block was received.
+    pub received_timestamp: Instant,
+    /// Timestamp when block was put to the orphan pool, if it ever was
+    pub orphaned_timestamp: Option<Instant>,
+    /// Timestamp when block was put to the missing chunks pool
+    pub missing_chunks_timestamp: Option<Instant>,
+    /// Timestamp when block was moved out of the orphan pool
+    pub removed_from_orphan_timestamp: Option<Instant>,
+    /// Timestamp when block was moved out of the missing chunks pool
+    pub removed_from_missing_chunks_timestamp: Option<Instant>,
+    /// Height at which the block is going to be added.
+    pub height: BlockHeight,
+    /// Hashes of the chunks that belong to this block.
+    pub chunks: Vec<ChunkHash>,
+}
+
+/// Records timestamps of requesting and receiving a chunk. Assumes that each chunk is requested
+/// before it is received.
+#[derive(Debug)]
+pub struct ChunkInProgress {
+    /// Timestamp of requesting a missing chunk.
+    pub chunk_requested: Instant,
+    /// Timestamp of receiving a chunk.
+    /// If a chunk is received multiple times, only the earliest timestamp is recorded.
+    pub chunk_received: Option<Instant>,
+    /// Block hash this chunk belongs to.
+    pub block_hash: CryptoHash,
+}
+
+impl BlocksDelayTracker {
+    pub fn mark_block_received(&mut self, block: &Block, timestamp: Instant) {
+        let block_hash = block.header().hash();
+        let height = block.header().height();
+        let chunks = block.chunks().iter().map(|it| it.chunk_hash()).collect_vec();
+
+        self.blocks_in_progress.entry(*block_hash).or_insert(BlockInProgress {
+            received_timestamp: timestamp,
+            orphaned_timestamp: None,
+            missing_chunks_timestamp: None,
+            removed_from_orphan_timestamp: None,
+            removed_from_missing_chunks_timestamp: None,
+            height,
+            chunks,
+        });
+    }
+
+    pub fn mark_block_orphaned(&mut self, block_hash: &CryptoHash, timestamp: Instant) {
+        if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
+            block_entry.orphaned_timestamp = Some(timestamp);
+        } else {
+            debug_assert!(false);
+            error!(target:"blocks_delay_tracker", "block {:?} was orphaned but was not marked received", block_hash);
+        }
+    }
+
+    pub fn mark_block_unorphaned(&mut self, block_hash: &CryptoHash, timestamp: Instant) {
+        if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
+            block_entry.removed_from_orphan_timestamp = Some(timestamp);
+        } else {
+            debug_assert!(false);
+            error!(target:"blocks_delay_tracker", "block {:?} was unorphaned but was not marked received", block_hash);
+        }
+    }
+
+    pub fn mark_block_has_missing_chunks(&mut self, block_hash: &CryptoHash, timestamp: Instant) {
+        if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
+            block_entry.missing_chunks_timestamp = Some(timestamp);
+        } else {
+            debug_assert!(false);
+            error!(target:"blocks_delay_tracker", "block {:?} was marked as having missing chunks but was not marked received", block_hash);
+        }
+    }
+
+    pub fn mark_block_completed_missing_chunks(
+        &mut self,
+        block_hash: &CryptoHash,
+        timestamp: Instant,
+    ) {
+        if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
+            block_entry.removed_from_missing_chunks_timestamp = Some(timestamp);
+        } else {
+            debug_assert!(false);
+            error!(target:"blocks_delay_tracker", "block {:?} was marked as having no missing chunks but was not marked received", block_hash);
+        }
+    }
+
+    pub fn mark_chunk_received(&mut self, chunk_hash: &ChunkHash, timestamp: Instant) {
+        self.chunks_in_progress
+            .get_mut(&chunk_hash)
+            .map(|chunk_in_progress| chunk_in_progress.chunk_received.get_or_insert(timestamp));
+    }
+
+    pub fn mark_chunk_requested(
+        &mut self,
+        chunk_hash: &ChunkHash,
+        timestamp: Instant,
+        requestor_block_hash: &CryptoHash,
+    ) {
+        self.chunks_in_progress.entry(chunk_hash.clone()).or_insert_with(|| ChunkInProgress {
+            chunk_requested: timestamp,
+            chunk_received: None,
+            block_hash: requestor_block_hash.clone(),
+        });
+    }
+
+    pub fn finish_block_processing(&mut self, block_hash: &CryptoHash, chunks: &[ChunkHash]) {
+        if let Some(processed_block) = self.blocks_in_progress.remove(&block_hash) {
+            self.update_block_metrics(&processed_block);
+            for (shard_id, chunk_hash) in chunks.iter().enumerate() {
+                if let Some(processed_chunk) = self.chunks_in_progress.remove(&chunk_hash) {
+                    self.update_chunk_metrics(
+                        &processed_block,
+                        &processed_chunk,
+                        shard_id as ShardId,
+                    );
+                }
+            }
+        }
+    }
+
+    fn update_block_metrics(&mut self, block: &BlockInProgress) {
+        if let Some(start) = block.orphaned_timestamp {
+            if let Some(end) = block.removed_from_orphan_timestamp {
+                metrics::BLOCK_ORPHANED_DELAY
+                    .observe(end.saturating_duration_since(start).as_secs_f64());
+            }
+        } else {
+            metrics::BLOCK_ORPHANED_DELAY.observe(0.);
+        }
+        if let Some(start) = block.missing_chunks_timestamp {
+            if let Some(end) = block.removed_from_missing_chunks_timestamp {
+                metrics::BLOCK_MISSING_CHUNKS_DELAY
+                    .observe(end.saturating_duration_since(start).as_secs_f64());
+            }
+        } else {
+            metrics::BLOCK_MISSING_CHUNKS_DELAY.observe(0.);
+        }
+    }
+
+    fn update_chunk_metrics(
+        &mut self,
+        block: &BlockInProgress,
+        chunk: &ChunkInProgress,
+        shard_id: ShardId,
+    ) {
+        let chunk_requested = chunk.chunk_requested;
+        metrics::BLOCK_CHUNKS_REQUESTED_DELAY.with_label_values(&[&shard_id.to_string()]).observe(
+            chunk_requested.saturating_duration_since(block.received_timestamp).as_secs_f64(),
+        );
+        // Theoretically chunk_received should have been set here because a block being processed
+        // requires all chunks to be received
+        if let Some(chunk_received) = chunk.chunk_received {
+            metrics::CHUNK_RECEIVED_DELAY
+                .with_label_values(&[&shard_id.to_string()])
+                .observe(chunk_received.saturating_duration_since(chunk_requested).as_secs_f64());
+        }
+    }
+}

chain/chain/Cargo.toml
@@ -20,6 +20,7 @@ rayon = "1.5"
 strum = "0.20"
 thiserror = "1.0"
 tracing = "0.1.13"
+prometheus = "0.11"
 
 delay-detector = { path = "../../tools/delay_detector"}
 near-chain-configs = { path = "../../core/chain-configs" }

chain/chain/src/chain.rs
@@ -50,6 +50,7 @@ use near_store::{ColState, ColStateHeaders, ColStateParts, ShardTries, StoreUpda
 
 use near_primitives::state_record::StateRecord;
 
+use crate::blocks_delay_tracker::BlocksDelayTracker;
 use crate::crypto_hash_timer::CryptoHashTimer;
 use crate::lightclient::get_epoch_block_producers_view;
 use crate::migrations::check_if_block_is_first_with_chunk_of_version;
@@ -424,6 +425,7 @@ pub struct Chain {
     pub block_economics_config: BlockEconomicsConfig,
     pub doomslug_threshold_mode: DoomslugThresholdMode,
     pending_states_to_patch: Option<Vec<StateRecord>>,
+    pub blocks_delay_tracker: BlocksDelayTracker,
 }
 
 impl ChainAccess for Chain {
@@ -480,6 +482,7 @@ impl Chain {
             block_economics_config: BlockEconomicsConfig::from(chain_genesis),
             doomslug_threshold_mode,
             pending_states_to_patch: None,
+            blocks_delay_tracker: BlocksDelayTracker::default(),
         })
     }
 
@@ -599,6 +602,7 @@ impl Chain {
             block_economics_config: BlockEconomicsConfig::from(chain_genesis),
             doomslug_threshold_mode,
             pending_states_to_patch: None,
+            blocks_delay_tracker: BlocksDelayTracker::default(),
         })
     }
 
@@ -1020,6 +1024,7 @@ impl Chain {
         block_orphaned_with_missing_chunks: &mut dyn FnMut(OrphanMissingChunks),
         on_challenge: &mut dyn FnMut(ChallengeBody),
     ) -> Result<Option<Tip>, Error> {
+        self.blocks_delay_tracker.mark_block_received(block.get_inner(), Clock::instant());
         let block_hash = *block.hash();
         let res = self.process_block_single(
             me,
@@ -1382,7 +1387,9 @@ impl Chain {
                                 false
                             };
 
-                            let orphan = Orphan { block, provenance, added: Clock::instant() };
+                            let time = Clock::instant();
+                            self.blocks_delay_tracker.mark_block_orphaned(block.hash(), time);
+                            let orphan = Orphan { block, provenance, added: time };
                             self.orphans.add(orphan, requested_missing_chunks);
 
                             debug!(
@@ -1407,7 +1414,9 @@ impl Chain {
                             missing_chunks,
                             block_hash,
                         });
-                        let orphan = Orphan { block, provenance, added: Clock::instant() };
+                        let time = Clock::instant();
+                        self.blocks_delay_tracker.mark_block_has_missing_chunks(block.hash(), time);
+                        let orphan = Orphan { block, provenance, added: time };
                         self.blocks_with_missing_chunks
                             .add_block_with_missing_chunks(orphan, missing_chunk_hashes.clone());
                         debug!(
@@ -1566,6 +1575,7 @@ impl Chain {
         let orphans = self.blocks_with_missing_chunks.ready_blocks();
         for orphan in orphans {
             let block_hash = *orphan.block.header().hash();
+            let time = Clock::instant();
             let res = self.process_block_single(
                 me,
                 orphan.block,
@@ -1578,6 +1588,8 @@ impl Chain {
             match res {
                 Ok(_) => {
                     debug!(target: "chain", "Block with missing chunks is accepted; me: {:?}", me);
+                    self.blocks_delay_tracker
+                        .mark_block_completed_missing_chunks(&block_hash, time);
                     new_blocks_accepted.push(block_hash);
                 }
                 Err(_) => {
@@ -1643,6 +1655,7 @@ impl Chain {
                 debug!(target: "chain", "Check orphans: found {} orphans", orphans.len());
                 for orphan in orphans.into_iter() {
                     let block_hash = orphan.hash();
+                    self.blocks_delay_tracker.mark_block_unorphaned(&block_hash, Clock::instant());
                     let res = self.process_block_single(
                         me,
                         orphan.block,

chain/chain/src/lib.rs
@@ -7,6 +7,7 @@ pub use store::{ChainStore, ChainStoreAccess, ChainStoreUpdate};
 pub use store_validator::{ErrorMessage, StoreValidator};
 pub use types::{Block, BlockHeader, BlockStatus, ChainGenesis, Provenance, RuntimeAdapter};
 
+pub mod blocks_delay_tracker;
 pub mod chain;
 pub mod crypto_hash_timer;
 mod doomslug;

chain/chain/src/metrics.rs
@@ -1,6 +1,6 @@
 use near_metrics::{
-    try_create_histogram, try_create_int_counter, try_create_int_gauge, Histogram, IntCounter,
-    IntGauge,
+    try_create_histogram, try_create_histogram_vec, try_create_int_counter, try_create_int_gauge,
+    Histogram, HistogramVec, IntCounter, IntGauge,
 };
 use once_cell::sync::Lazy;
 
@@ -41,4 +41,33 @@ pub static NUM_ORPHANS: Lazy<IntGauge> =
     Lazy::new(|| try_create_int_gauge("near_num_orphans", "Number of orphan blocks.").unwrap());
 pub static HEADER_HEAD_HEIGHT: Lazy<IntGauge> = Lazy::new(|| {
     try_create_int_gauge("near_header_head_height", "Height of the header head").unwrap()
+});
+pub static BLOCK_CHUNKS_REQUESTED_DELAY: Lazy<HistogramVec> = Lazy::new(|| {
+    try_create_histogram_vec(
+        "near_block_chunks_request_delay_seconds",
+        "Delay between receiving a block and requesting its chunks",
+        &["shard_id"],
+        Some(prometheus::exponential_buckets(0.001, 1.6, 20).unwrap()),
+    )
+    .unwrap()
+});
+pub static CHUNK_RECEIVED_DELAY: Lazy<HistogramVec> = Lazy::new(|| {
+    try_create_histogram_vec(
+        "near_chunk_receive_delay_seconds",
+        "Delay between requesting and receiving a chunk.",
+        &["shard_id"],
+        Some(prometheus::exponential_buckets(0.001, 1.6, 20).unwrap()),
+    )
+    .unwrap()
+});
+pub static BLOCK_ORPHANED_DELAY: Lazy<Histogram> = Lazy::new(|| {
+    try_create_histogram("near_block_orphaned_delay", "How long blocks stay in the orphan pool")
+        .unwrap()
+});
+pub static BLOCK_MISSING_CHUNKS_DELAY: Lazy<Histogram> = Lazy::new(|| {
+    try_create_histogram(
+        "near_block_missing_chunks_delay",
+        "How long blocks stay in the missing chunks pool",
+    )
+    .unwrap()
 });

chain/chain/src/tests/simple_chain.rs
@@ -46,6 +46,7 @@ fn build_chain() {
         // Instant calls for CryptoHashTimer.
         mock_clock_guard.add_instant(Instant::now());
         mock_clock_guard.add_instant(Instant::now());
+        mock_clock_guard.add_instant(Instant::now());
     }
 
     let (mut chain, _, signer) = setup();
@@ -73,7 +74,7 @@ fn build_chain() {
     let count_instant = mock_clock_guard.instant_call_count();
     let count_utc = mock_clock_guard.utc_call_count();
     assert_eq!(count_utc, 9);
-    assert_eq!(count_instant, 8);
+    assert_eq!(count_instant, 12);
     #[cfg(feature = "nightly_protocol")]
     assert_eq!(
         chain.head().unwrap().last_block_hash,

chain/client/src/chunks_delay_tracker.rs
@@ -1,129 +1,0 @@
-use itertools::Itertools;
-use near_primitives::block::Block;
-use near_primitives::hash::CryptoHash;
-use near_primitives::sharding::ChunkHash;
-use near_primitives::types::BlockHeight;
-use std::collections::HashMap;
-use std::time::Instant;
-
-use crate::metrics;
-
-/// Provides monitoring information about the delays of receiving and requesting blocks and their corresponding chunks.
-/// Keeps timestamps for all future blocks and chunks that were requested or received.
-/// Updates the metrics when the head gets updated. Assumes that if the head moves forward, all needed chunks were received.
-/// If a chunk or a block is received or requested multiple times, only the first time is recorded.
-#[derive(Debug, Default)]
-pub struct ChunksDelayTracker {
-    /// Timestamps of the first attempt of block processing.
-    /// Contains only blocks that are not yet processed.
-    pub blocks_in_progress: HashMap<CryptoHash, BlockInProgress>,
-    /// An entry gets created when a chunk gets requested for the first time.
-    /// Chunks get deleted when the block gets processed.
-    pub chunks_in_progress: HashMap<ChunkHash, ChunkInProgress>,
-}
-
-#[derive(Debug)]
-pub struct BlockInProgress {
-    /// Timestamp when block was added.
-    pub timestamp: Instant,
-    /// Height at which the block is going to be added.
-    pub height: BlockHeight,
-    /// Hashes of the chunks that belong to this block.
-    pub chunks: Vec<ChunkHash>,
-}
-
-#[derive(Debug)]
-/// Records timestamps of requesting and receiving a chunk. Assumes that each chunk is requested
-/// before it is received.
-pub struct ChunkInProgress {
-    /// Timestamp of requesting a missing chunk.
-    pub chunk_requested: Instant,
-    /// Timestamp of receiving a chunk.
-    /// If a chunk is received multiple times, only the earliest timestamp is recorded.
-    pub chunk_received: Option<Instant>,
-    /// Block hash this chunk belongs to.
-    pub block_hash: CryptoHash,
-}
-
-impl ChunksDelayTracker {
-    pub fn received_block(&mut self, block: &Block, timestamp: Instant) {
-        let block_hash = block.header().hash();
-        let height = block.header().height();
-        let chunks = block.chunks().iter().map(|it| it.chunk_hash()).collect_vec();
-
-        self.blocks_in_progress.entry(*block_hash).or_insert(BlockInProgress {
-            timestamp,
-            height,
-            chunks,
-        });
-    }
-
-    pub fn received_chunk(&mut self, chunk_hash: &ChunkHash, timestamp: Instant) {
-        self.chunks_in_progress
-            .get_mut(&chunk_hash)
-            .map(|chunk_in_progress| chunk_in_progress.chunk_received.get_or_insert(timestamp));
-    }
-
-    pub fn requested_chunk(
-        &mut self,
-        chunk_hash: &ChunkHash,
-        timestamp: Instant,
-        requestor_block_hash: &CryptoHash,
-    ) {
-        self.chunks_in_progress.entry(chunk_hash.clone()).or_insert_with(|| ChunkInProgress {
-            chunk_requested: timestamp,
-            chunk_received: None,
-            block_hash: requestor_block_hash.clone(),
-        });
-    }
-
-    pub fn finish_block_processing(
-        &mut self,
-        processed_block_hash: &CryptoHash,
-        chunks: &[ChunkHash],
-    ) {
-        self.update_block_chunks_requested_metric(processed_block_hash, &chunks);
-        self.update_chunks_metric(&chunks);
-
-        self.blocks_in_progress.remove(&processed_block_hash);
-        for chunk_hash in chunks {
-            self.chunks_in_progress.remove(chunk_hash);
-        }
-    }
-
-    fn update_block_chunks_requested_metric(
-        &mut self,
-        block_hash: &CryptoHash,
-        chunks: &[ChunkHash],
-    ) {
-        if let Some(block_received) = self.blocks_in_progress.get(&block_hash) {
-            for (shard_id, chunk_hash) in chunks.iter().enumerate() {
-                if let Some(chunk_in_progress) = self.chunks_in_progress.get(chunk_hash) {
-                    let chunk_requested = chunk_in_progress.chunk_requested;
-                    metrics::BLOCK_CHUNKS_REQUESTED_DELAY
-                        .with_label_values(&[&format!("{}", shard_id)])
-                        .observe(
-                            chunk_requested
-                                .saturating_duration_since(block_received.timestamp)
-                                .as_secs_f64(),
-                        );
-                }
-            }
-        }
-    }
-
-    fn update_chunks_metric(&mut self, chunks: &[ChunkHash]) {
-        for (shard_id, chunk_hash) in chunks.iter().enumerate() {
-            if let Some(chunk_in_progress) = self.chunks_in_progress.get(&chunk_hash) {
-                if let Some(chunk_received) = chunk_in_progress.chunk_received {
-                    let chunk_requested = chunk_in_progress.chunk_requested;
-                    metrics::CHUNK_RECEIVED_DELAY
-                        .with_label_values(&[&format!("{}", shard_id)])
-                        .observe(
-                            chunk_received.saturating_duration_since(chunk_requested).as_secs_f64(),
-                        );
-                }
-            }
-        }
-    }
-}

chain/client/src/client.rs
@@ -39,7 +39,6 @@ use near_primitives::unwrap_or_return;
 use near_primitives::utils::MaybeValidated;
 use near_primitives::validator_signer::ValidatorSigner;
 
-use crate::chunks_delay_tracker::ChunksDelayTracker;
 use crate::sync::{BlockSync, EpochSync, HeaderSync, StateSync, StateSyncResult};
 use crate::{metrics, SyncStatus};
 use itertools::Itertools;
@@ -107,8 +106,6 @@ pub struct Client {
     /// Last time the head was updated, or our head was rebroadcasted. Used to re-broadcast the head
     /// again to prevent network from stalling if a large percentage of the network missed a block
     last_time_head_progress_made: Instant,
-    /// Keeps track of when the latest blocks and chunks were received.
-    pub chunks_delay_tracker: ChunksDelayTracker,
 }
 
 // Debug information about the upcoming block.
@@ -224,7 +221,6 @@ impl Client {
             rs: ReedSolomonWrapper::new(data_parts, parity_parts),
             rebroadcasted_blocks: lru::LruCache::new(NUM_REBROADCAST_BLOCKS),
             last_time_head_progress_made: Clock::instant(),
-            chunks_delay_tracker: Default::default(),
         })
     }
 
@@ -741,7 +737,6 @@ impl Client {
         block: MaybeValidated<Block>,
         provenance: Provenance,
     ) -> (Vec<AcceptedBlock>, Result<Option<Tip>, near_chain::Error>) {
-        self.record_receive_block_timestamp(block.get_inner());
         let is_requested = match provenance {
             Provenance::PRODUCED | Provenance::SYNC => true,
             Provenance::NONE => false,
@@ -943,7 +938,9 @@ impl Client {
             | ProcessPartialEncodedChunkResult::NeedBlock
             | ProcessPartialEncodedChunkResult::NeedMorePartsOrReceipts => vec![],
             ProcessPartialEncodedChunkResult::HaveAllPartsAndReceipts => {
-                self.record_receive_chunk_timestamp(&header.chunk_hash());
+                self.chain
+                    .blocks_delay_tracker
+                    .mark_chunk_received(&header.chunk_hash(), Clock::instant());
                 self.chain.blocks_with_missing_chunks.accept_chunk(&header.chunk_hash());
                 self.process_blocks_with_missing_chunks()
             }
@@ -1047,10 +1044,6 @@ impl Client {
         provenance: Provenance,
     ) {
         self.on_block_accepted_with_optional_chunk_produce(block_hash, status, provenance, false);
-    }
-
-    pub fn record_accepted_block(&mut self, block_hash: &CryptoHash, chunks: &[ChunkHash]) {
-        self.chunks_delay_tracker.finish_block_processing(block_hash, chunks);
     }
 
     /// Gets called when block got accepted.
@@ -1252,7 +1245,7 @@ impl Client {
 
         let chunk_hashes: Vec<ChunkHash> =
             block.chunks().iter().map(|chunk| chunk.chunk_hash()).collect();
-        self.record_accepted_block(&block_hash, &chunk_hashes);
+        self.chain.blocks_delay_tracker.finish_block_processing(&block_hash, &chunk_hashes);
     }
 
     pub fn request_missing_chunks(
@@ -1263,7 +1256,11 @@ impl Client {
         let now = Clock::instant();
         for BlockMissingChunks { prev_hash, missing_chunks, block_hash } in blocks_missing_chunks {
             for chunk in &missing_chunks {
-                self.chunks_delay_tracker.requested_chunk(&chunk.chunk_hash(), now, &block_hash);
+                self.chain.blocks_delay_tracker.mark_chunk_requested(
+                    &chunk.chunk_hash(),
+                    now,
+                    &block_hash,
+                );
             }
             self.shards_mgr.request_chunks(
                 missing_chunks,
@@ -1279,7 +1276,7 @@ impl Client {
             orphans_missing_chunks
         {
             for chunk in &missing_chunks {
-                self.chunks_delay_tracker.requested_chunk(
+                self.chain.blocks_delay_tracker.mark_chunk_requested(
                     &chunk.chunk_hash(),
                     now,
                     &requestor_block_hash,
@@ -1861,14 +1858,6 @@ impl Client {
         Ok(())
     }
 
-    fn record_receive_block_timestamp(&mut self, block: &Block) {
-        self.chunks_delay_tracker.received_block(block, Clock::instant());
-    }
-
-    fn record_receive_chunk_timestamp(&mut self, chunk_hash: &ChunkHash) {
-        self.chunks_delay_tracker.received_chunk(chunk_hash, Clock::instant());
-    }
-
     // Returns detailed information about the upcoming blocks.
     pub fn detailed_upcoming_blocks_info(&self) -> Result<String, Error> {
         let now = Instant::now();
@@ -1878,20 +1867,20 @@ impl Client {
         > = HashMap::new();
 
         // First - look at all the 'in progress' blocks.
-        for entry in self.chunks_delay_tracker.blocks_in_progress.iter() {
+        for entry in self.chain.blocks_delay_tracker.blocks_in_progress.iter() {
             if entry.1.height < self.chain.head()?.height {
                 // In case chunks delay tracker 'leaked' some old blocks - we don't want them to show up.
                 continue;
             }
             let height_status = height_status_map.entry(entry.1.height).or_default();
             let mut block_status = height_status.entry(*entry.0).or_default();
-            block_status.in_progress_for = now.checked_duration_since(entry.1.timestamp);
+            block_status.in_progress_for = now.checked_duration_since(entry.1.received_timestamp);
             block_status.chunk_hashes = entry.1.chunks.clone();
         }
 
         // And in-progress chunks.
         // Here we can see which ones we sent requests for and which responses already came back.
-        for entry in self.chunks_delay_tracker.chunks_in_progress.iter() {
+        for entry in self.chain.blocks_delay_tracker.chunks_in_progress.iter() {
             for height_entry in height_status_map.iter_mut() {
                 if height_entry.1.contains_key(&entry.1.block_hash) {
                     let block_status_entry = height_entry.1.get_mut(&entry.1.block_hash).unwrap();
@@ -1988,8 +1977,8 @@ impl Client {
         Ok(format!(
             "{:?} Blocks in progress: {} Chunks in progress: {} Orphans: {}{}{}",
             self.chain.head()?.epoch_id,
-            self.chunks_delay_tracker.blocks_in_progress.len(),
-            self.chunks_delay_tracker.chunks_in_progress.len(),
+            self.chain.blocks_delay_tracker.blocks_in_progress.len(),
+            self.chain.blocks_delay_tracker.chunks_in_progress.len(),
             self.chain.orphans().len(),
             if next_blocks_log.len() > 0 { "\n" } else { "" },
             next_blocks_log.join("\n")

commit 5d7719113b7766f8c857c3851b487966cebad065

chain: remove failing debug asserts
The asserts have been introduced in commit ed1a1b600bb4: ‘add metrics
for how long a block stay in the orphan pool and the missing chunks
pool’ and at least the one in mark_block_unorphaned have been failing
in some of the tests.  Remove the asserts to make the tests pass.

chain/chain/src/blocks_delay_tracker.rs
@@ -74,7 +74,6 @@ impl BlocksDelayTracker {
         if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
             block_entry.orphaned_timestamp = Some(timestamp);
         } else {
-            debug_assert!(false);
             error!(target:"blocks_delay_tracker", "block {:?} was orphaned but was not marked received", block_hash);
         }
     }
@@ -83,7 +82,6 @@ impl BlocksDelayTracker {
         if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
             block_entry.removed_from_orphan_timestamp = Some(timestamp);
         } else {
-            debug_assert!(false);
             error!(target:"blocks_delay_tracker", "block {:?} was unorphaned but was not marked received", block_hash);
         }
     }
@@ -92,7 +90,6 @@ impl BlocksDelayTracker {
         if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
             block_entry.missing_chunks_timestamp = Some(timestamp);
         } else {
-            debug_assert!(false);
             error!(target:"blocks_delay_tracker", "block {:?} was marked as having missing chunks but was not marked received", block_hash);
         }
     }
@@ -105,7 +102,6 @@ impl BlocksDelayTracker {
         if let Some(block_entry) = self.blocks_in_progress.get_mut(block_hash) {
             block_entry.removed_from_missing_chunks_timestamp = Some(timestamp);
         } else {
-            debug_assert!(false);
             error!(target:"blocks_delay_tracker", "block {:?} was marked as having no missing chunks but was not marked received", block_hash);
         }
     }