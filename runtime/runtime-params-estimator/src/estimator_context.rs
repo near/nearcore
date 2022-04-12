3e1a10b36528dd328e10361fe520865841bece16
Longarithm <the.aleksandr.logunov@gmail.com>
2022-04-12 15:40:57 +0400

    len = 2000

--- runtime/runtime-params-estimator/src/estimator_context.rs
+++ runtime/runtime-params-estimator/src/estimator_context.rs
@@ -120,7 +120,7 @@ impl<'c> Testbed<'c> {
                 let tb = self.transaction_builder();
 
                 let num_values: usize = 100;
-                let value_len: usize = 4000;
+                let value_len: usize = 2000;
                 let signer = tb.random_account();
                 let values: Vec<_> = (0..num_values)
                     .map(|_| {
