383b3db8a8883bbd513877e7f64a8f66b0d2a58b
Simonas Kazlauskas <git@kazlauskas.me>
2022-04-12 19:31:02 +0300

    Increase the secondary stack cleck limit
    200*1024 is still just under 2MiB, which we can definitely afford.

--- core/primitives/src/runtime/config_store.rs
+++ core/primitives/src/runtime/config_store.rs
@@ -22,6 +22,7 @@ static CONFIGS: &[(ProtocolVersion, &[u8])] = &[
     (50, include_config!("50.json")),
     // max_gas_burnt increased to 300 TGas
     (52, include_config!("52.json")),
+    // Increased deployment costs, increased wasmer2 stack_limit
     (53, include_config!("53.json")),
 ];
 
@@ -123,7 +124,7 @@ mod tests {
             "2cuq2HvuHT7Z27LUbgEtMxP2ejqrHK34J2V1GL1joiMn",
             "HFetcNKaC5s8Mj7bQz7jGMF7Rsvtuc3kjZRevWQ334n4",
             "EP9bv2znwbuBuimUgrSQm48ymHqwbHyUArZcWavSbPce",
-            "Gs3KXwHmXYGghRZvcVNGXVpbJxVF5SDRNMB3f32DiXUF",
+            "3i1T4Gg7JR8SgBDBQxeRUQ4FCgtjKvthC9LQ685zk7Qa",
         ];
         let actual_hashes = CONFIGS
             .iter()
