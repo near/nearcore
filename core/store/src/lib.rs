7c684086b2c9a4fe7efa332437925869564db0dc
Edvard Davtyan <davtyaneg@gmail.com>
2022-04-13 06:30:12 +0200

    Add some comments

--- core/store/src/lib.rs
+++ core/store/src/lib.rs
@@ -307,7 +307,11 @@ impl Default for StoreConfig {
         StoreConfig {
             read_only: false,
             enable_statistics: false,
+            // 512 is a value that we used since 3 Dec 2019.
             max_open_files: 512,
+            // We used to have the same cache size for all columns 32MB. When some RocksDB
+            // inefficiencies were found ColState cache size was increased up to 512MB.
+            // This was done Nov 13 2021.
             col_state_cache_size: 512 * 1024 * 1024,
         }
     }
