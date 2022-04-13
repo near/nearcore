be59a526e9afb4ca45d1d412515afa8b146de445
Aleksandr Logunov <alex.logunov@near.org>
2022-04-13 20:02:42 +0400

    feat: add read_cached_trie_node cost (#6588)
    Introduce `read_cached_trie_node` cost for reading trie node from memory. Use new trie nodes counter to charge this cost.
    
    ## Notes
    
    * This is a no-op change, because chunk nodes cache is not enabled. It will be actually used in https://github.com/near/nearcore/pull/6450 after merge
    * Default cost `760_000_000 * 3` is taken from our theoretical and practical understanding on how costly is to take and decode 4KB node from the hashmap. We want to proceed with this upper bound for the upcoming release and add the estimation later. cc @jakmeier 
    * This cost is tested on my test sample of 104 receipts on Aurora shard where an average receipt burns 66.7 Tgas.
    If we compute gas savings as `(tgas_new - tgas_old) / tgas_old`, average savings without `read_cached_trie_node` taken into account are 32%, and average savings with `read_cached_trie_node` are 27%. So we lose only 5% here, and `storage_read_base` still will be a new bottleneck (~30% of the new cost)
    * `add_trie_fees` is a method of `GasCounter` because on `storage_read` we immutably borrow `self.ext` to read the pointer, so we can't use a mutable `VMLogic` method.
    
    ## Testing
    
    Existing tests.

--- core/primitives-core/src/profile.rs
+++ core/primitives-core/src/profile.rs
@@ -239,6 +239,8 @@ impl Cost {
         Cost::ExtCost { ext_cost_kind: ExtCosts::storage_iter_next_key_byte },
         Cost::ExtCost { ext_cost_kind: ExtCosts::storage_iter_next_value_byte },
         Cost::ExtCost { ext_cost_kind: ExtCosts::touching_trie_node },
+        #[cfg(feature = "protocol_feature_chunk_nodes_cache")]
+        Cost::ExtCost { ext_cost_kind: ExtCosts::read_cached_trie_node },
         Cost::ExtCost { ext_cost_kind: ExtCosts::promise_and_base },
         Cost::ExtCost { ext_cost_kind: ExtCosts::promise_and_per_promise },
         Cost::ExtCost { ext_cost_kind: ExtCosts::promise_return },
@@ -326,20 +328,22 @@ impl Cost {
             Cost::ExtCost { ext_cost_kind: ExtCosts::validator_stake_base } => 60,
             Cost::ExtCost { ext_cost_kind: ExtCosts::validator_total_stake_base } => 61,
             Cost::WasmInstruction => 62,
+            #[cfg(feature = "protocol_feature_chunk_nodes_cache")]
+            Cost::ExtCost { ext_cost_kind: ExtCosts::read_cached_trie_node } => 63,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_base } => 63,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_base } => 64,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_byte } => 64,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_byte } => 65,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_sublinear } => 65,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_multiexp_sublinear } => 66,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_pairing_check_base } => 66,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_pairing_check_base } => 67,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_pairing_check_byte } => 67,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_pairing_check_byte } => 68,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_sum_base } => 68,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_sum_base } => 69,
             #[cfg(feature = "protocol_feature_alt_bn128")]
-            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_sum_byte } => 69,
+            Cost::ExtCost { ext_cost_kind: ExtCosts::alt_bn128_g1_sum_byte } => 70,
         }
     }
 }
