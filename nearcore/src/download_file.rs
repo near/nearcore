60ebb055e35b8b4f047e05a852c57c65426874d0
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-04-18 21:46:57 +0100

    test: drop dependency and fix a race
    We don't need portpicker, we can ask OS to pick the port. That way,
    we'll also avoid the race between us checking that the port is unused
    and actually using that.
    
    As a bonus, this also allows us to remove barrier sychronization.

--- nearcore/src/download_file.rs
+++ nearcore/src/download_file.rs
@@ -257,29 +257,21 @@ mod tests {
     use hyper::service::{make_service_fn, service_fn};
     use hyper::{Body, Request, Response, Server};
     use std::convert::Infallible;
-    use std::net::SocketAddr;
-    use std::sync::Arc;
-    use tokio::sync::Barrier;
 
     #[tokio::test]
     async fn test_file_download() {
-        let port = portpicker::pick_unused_port().expect("No ports free");
+        let listener = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
+        let port = listener.local_addr().unwrap().port();
 
         async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
             let data: [u8; 1024] = [42; 1024];
             Ok(Response::new(Body::from(data.to_vec())))
         }
 
-        let barrier = Arc::new(Barrier::new(2));
-        let server_barrier = barrier.clone();
-
         tokio::task::spawn(async move {
-            let addr = SocketAddr::from(([127, 0, 0, 1], port));
             let make_svc =
                 make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
-            let server = Server::bind(&addr).serve(make_svc);
-
-            server_barrier.wait().await;
+            let server = Server::from_tcp(listener).unwrap().serve(make_svc);
             if let Err(e) = server.await {
                 eprintln!("server error: {}", e);
             }
@@ -287,8 +279,6 @@ mod tests {
 
         let tmp_downloaded_file = tempfile::NamedTempFile::new().unwrap();
         let tmp_downloaded_file_path = tmp_downloaded_file.path();
-
-        barrier.wait().await;
 
         download_file(&format!("http://localhost:{}", port), tmp_downloaded_file_path)
             .await
