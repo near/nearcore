40b3c5d185878732c00140da3554b13523a68ff7
Edvard Davtyan <davtyaneg@gmail.com>
2022-04-25 12:01:13 +0200

    Print the current height while synching

--- chain/client/src/info.rs
+++ chain/client/src/info.rs
@@ -258,10 +258,11 @@ pub fn display_sync_status(
                     / ((highest_height - genesis_height) as f64)
             };
             format!(
-                "#{:>8} Downloading headers {:.2}% ({})",
+                "#{:>8} Downloading headers {:.2}% ({}, {})",
                 head.height,
                 percent,
-                highest_height - current_height
+                highest_height - current_height,
+                current_height
             )
         }
         SyncStatus::BodySync { current_height, highest_height } => {
