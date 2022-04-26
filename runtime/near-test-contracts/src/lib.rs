near/nearcore

commit 7f1672f89f1700eb3601f742759ac2742f7007aa

Merge pull request from GHSA-5vx4-7hvq-wr66
* Move function count validation to wasmparser

* Limit the number of locals in the contract

The large number of locals in WASM can be encoded in a constant amount
of space. It is straightforward for a piece of code operate with these
locals in a poor way – in fact we do so ourselves in multiple places,
expanding the constant-length encoding a linear vector containing the
local types.

While this new representation gives a `O(1)` access to any local's type,
constructing the vector like this in the first place is a linear-time
ordeal, one that the attacker can control by changing the value of a
couple of bytes.

Today we only have a limit (and a pretty generous one) per-function
conetained within the contract. Given a typical function limit (`10000`)
and number of locals per function imposed by `wasmparser` (`50000`), we
would end up up constructing vectors for up-to 500M local types,
possibly multiple times over, and most of the time with some
reallocations in between to resize the vectors. That's a lot of work!

In practice our contracts are also limited in size to 4MiB. Each
instruction to access (`local.get`, `local.set` or `local.tee`) is at
least 2 bytes and must specify a constant local index. This means that
an useful number of locals is largely limited by the size of the
contract. Even a contract which only ever reads and writes locals will
need at least 2 instructions for each “useful” local, so 1M of locals
seems like a very reasonable contract-wide upper limit on the number of
locals to impose.

runtime/near-test-contracts/src/lib.rs
@@ -1,7 +1,6 @@
 #![doc = include_str!("../README.md")]
 
 use once_cell::sync::OnceCell;
-use std::fmt::Write;
 use std::path::Path;
 
 /// Trivial contact with a do-nothing main function.
@@ -78,25 +77,41 @@ fn smoke_test() {
     assert!(!base_rs_contract().is_empty());
 }
 
-pub fn many_functions_contract(function_count: u32) -> Vec<u8> {
-    let mut functions = String::new();
-    for i in 0..function_count {
-        writeln!(
-            &mut functions,
-            "(func
-                i32.const {}
-                drop
-                return)",
-            i
-        )
-        .unwrap();
-    }
+/// Construct a contract with many entitites.
+///
+/// Currently supports constructing contracts that contain a specified number of functions with the
+/// specified number of locals each.
+///
+/// Exports a function called `main` that does nothing.
+pub fn large_contract(functions: u32, locals: u32) -> Vec<u8> {
+    use wasm_encoder::{
+        CodeSection, Export, ExportSection, Function, FunctionSection, Instruction, Module,
+        TypeSection, ValType,
+    };
+    // Won't generate a valid WASM without functions.
+    assert!(functions >= 1, "must specify at least 1 function to be generated");
+    let mut module = Module::new();
+    let mut type_section = TypeSection::new();
+    type_section.function([], []);
+    module.section(&type_section);
 
-    let code = format!(
-        r#"(module
-            (export "main" (func 0))
-            {})"#,
-        functions
-    );
-    wat::parse_str(code).unwrap()
+    let mut functions_section = FunctionSection::new();
+    for _ in 0..functions {
+        functions_section.function(0);
+    }
+    module.section(&functions_section);
+
+    let mut exports_section = ExportSection::new();
+    exports_section.export("main", Export::Function(0));
+    module.section(&exports_section);
+
+    let mut code_section = CodeSection::new();
+    for _ in 0..functions {
+        let mut f = Function::new([(locals, ValType::I64)]);
+        f.instruction(&Instruction::End);
+        code_section.function(&f);
+    }
+    module.section(&code_section);
+
+    module.finish()
 }

runtime/near-vm-runner/src/prepare.rs
@@ -8,7 +8,7 @@ use pwasm_utils::parity_wasm::elements::{self, External, MemorySection};
 
 pub(crate) const WASM_FEATURES: wasmparser::WasmFeatures = wasmparser::WasmFeatures {
     reference_types: false,
-    // wasmer singlepass compiler most likely requires multi_value return values to be disabled.
+    // wasmer singlepass compiler requires multi_value return values to be disabled.
     multi_value: false,
     bulk_memory: false,
     module_linking: false,
@@ -20,6 +20,60 @@ pub(crate) const WASM_FEATURES: wasmparser::WasmFeatures = wasmparser::WasmFeatu
     exceptions: false,
     memory64: false,
 };
+
+/// Decode and validate the provided WebAssembly code with the `wasmparser` crate.
+///
+/// This function will return the number of functions defined globally in the provided WebAssembly
+/// module as well as the number of locals declared by all functions. If either counter overflows,
+/// `None` is returned in its place.
+fn wasmparser_decode(
+    code: &[u8],
+) -> Result<(Option<u64>, Option<u64>), wasmparser::BinaryReaderError> {
+    use wasmparser::ValidPayload;
+    let mut validator = wasmparser::Validator::new();
+    validator.wasm_features(WASM_FEATURES);
+    let mut function_count = Some(0u64);
+    let mut local_count = Some(0u64);
+    for payload in wasmparser::Parser::new(0).parse_all(code) {
+        match validator.payload(&payload?)? {
+            ValidPayload::Ok => (),
+            ValidPayload::Submodule(_) => panic!("submodules are not reachable (not enabled)"),
+            ValidPayload::Func(mut validator, body) => {
+                validator.validate(&body)?;
+                function_count = function_count.and_then(|f| f.checked_add(1));
+                // Count the global number of local variables.
+                let mut local_reader = body.get_locals_reader()?;
+                for _ in 0..local_reader.get_count() {
+                    let (count, _type) = local_reader.read()?;
+                    local_count = local_count.and_then(|l| l.checked_add(count.into()));
+                }
+            }
+        }
+    }
+    Ok((function_count, local_count))
+}
+
+fn validate_contract(code: &[u8], config: &VMConfig) -> Result<(), PrepareError> {
+    let (function_count, local_count) =
+        wasmparser_decode(code).map_err(|_| PrepareError::Deserialization)?;
+    // Verify the number of functions does not exceed the limit we imposed. Note that the ordering
+    // of this check is important. In the past we first validated the entire module and only then
+    // verified that the limit is not exceeded. While it would be more efficient to check for this
+    // before validating the function bodies, it would change the results for malformed WebAssembly
+    // modules.
+    if let Some(max_functions) = config.limit_config.max_functions_number_per_contract {
+        if function_count.ok_or(PrepareError::TooManyFunctions)? > max_functions {
+            return Err(PrepareError::TooManyFunctions);
+        }
+    }
+    // Similarly, do the same for the number of locals.
+    if let Some(max_locals) = config.limit_config.max_locals_per_contract {
+        if local_count.ok_or(PrepareError::TooManyLocals)? > max_locals {
+            return Err(PrepareError::TooManyLocals);
+        }
+    }
+    Ok(())
+}
 
 /// Loads the given module given in `original_code`, performs some checks on it and
 /// does some preprocessing.
@@ -33,11 +87,7 @@ pub(crate) const WASM_FEATURES: wasmparser::WasmFeatures = wasmparser::WasmFeatu
 ///
 /// The preprocessing includes injecting code for gas metering and metering the height of stack.
 pub fn prepare_contract(original_code: &[u8], config: &VMConfig) -> Result<Vec<u8>, PrepareError> {
-    wasmparser::Validator::new()
-        .wasm_features(WASM_FEATURES)
-        .validate_all(original_code)
-        .map_err(|_| PrepareError::Deserialization)?;
-
+    validate_contract(original_code, config)?;
     match config.limit_config.stack_limiter_version {
         // Support for old protocol versions, where we incorrectly didn't
         // account for many locals of the same type.
@@ -45,7 +95,6 @@ pub fn prepare_contract(original_code: &[u8], config: &VMConfig) -> Result<Vec<u
         // See `test_stack_instrumentation_protocol_upgrade` test.
         near_vm_logic::StackLimiterVersion::V0 => pwasm_12::prepare_contract(original_code, config),
         near_vm_logic::StackLimiterVersion::V1 => ContractModule::init(original_code, config)?
-            .validate_functions_number()?
             .standardize_mem()
             .ensure_no_internal_memory()?
             .inject_gas_metering()?
@@ -184,18 +233,6 @@ impl<'a> ContractModule<'a> {
         Ok(Self { module, config })
     }
 
-    fn validate_functions_number(self) -> Result<Self, PrepareError> {
-        if let Some(max_functions_number) =
-            self.config.limit_config.max_functions_number_per_contract
-        {
-            let functions_number = self.module.functions_space() as u64;
-            if functions_number > max_functions_number {
-                return Err(PrepareError::TooManyFunctions);
-            }
-        }
-        Ok(self)
-    }
-
     fn into_wasm_code(self) -> Result<Vec<u8>, PrepareError> {
         elements::serialize(self.module).map_err(|_| PrepareError::Serialization)
     }
@@ -214,7 +251,6 @@ mod pwasm_12 {
         config: &VMConfig,
     ) -> Result<Vec<u8>, PrepareError> {
         ContractModule::init(original_code, config)?
-            .validate_functions_number()?
             .standardize_mem()
             .ensure_no_internal_memory()?
             .inject_gas_metering()?
@@ -352,18 +388,6 @@ mod pwasm_12 {
                 return Err(PrepareError::Memory);
             };
             Ok(Self { module, config })
-        }
-
-        fn validate_functions_number(self) -> Result<Self, PrepareError> {
-            if let Some(max_functions_number) =
-                self.config.limit_config.max_functions_number_per_contract
-            {
-                let functions_number = self.module.functions_space() as u64;
-                if functions_number > max_functions_number {
-                    return Err(PrepareError::TooManyFunctions);
-                }
-            }
-            Ok(self)
         }
 
         fn into_wasm_code(self) -> Result<Vec<u8>, PrepareError> {

integration-tests/src/tests/client/process_blocks.rs
@@ -34,8 +34,8 @@ use near_network_primitives::types::{PeerChainInfoV2, PeerInfo, ReasonForBan};
 use near_primitives::block::{Approval, ApprovalInner};
 use near_primitives::block_header::BlockHeader;
 use near_primitives::epoch_manager::RngSeed;
-use near_primitives::errors::InvalidTxError;
 use near_primitives::errors::TxExecutionError;
+use near_primitives::errors::{ActionErrorKind, InvalidTxError};
 use near_primitives::hash::{hash, CryptoHash};
 use near_primitives::merkle::verify_hash;
 use near_primitives::receipt::DelayedReceiptIndices;
@@ -65,6 +65,7 @@ use near_primitives::views::{
 use near_store::get;
 use near_store::test_utils::create_test_store;
 use near_store::DBCol::ColStateParts;
+use near_vm_errors::{CompilationError, FunctionCallErrorSer, PrepareError};
 use nearcore::config::{GenesisExt, TESTING_INIT_BALANCE, TESTING_INIT_STAKE};
 use nearcore::{TrackedConfig, NEAR_BASE};
 use rand::prelude::StdRng;
@@ -3332,13 +3333,14 @@ fn test_validator_stake_host_function() {
     }
 }
 
-// Check that we can't call a contract exceeding functions number limit after upgrade.
-#[test]
-fn test_limit_contract_functions_number_upgrade() {
-    let functions_number_limit: u32 = 10_000;
-
-    let old_protocol_version = ProtocolFeature::LimitContractFunctionsNumber.protocol_version() - 1;
-    let new_protocol_version = old_protocol_version + 1;
+fn verify_contract_limits_upgrade(
+    feature: ProtocolFeature,
+    function_limit: u32,
+    local_limit: u32,
+    expected_prepare_err: PrepareError,
+) {
+    let old_protocol_version = feature.protocol_version() - 1;
+    let new_protocol_version = feature.protocol_version();
 
     // Prepare TestEnv with a contract at the old protocol version.
     let mut env = {
@@ -3361,7 +3363,7 @@ fn test_limit_contract_functions_number_upgrade() {
         deploy_test_contract(
             &mut env,
             "test0".parse().unwrap(),
-            &near_test_contracts::many_functions_contract(functions_number_limit + 10),
+            &near_test_contracts::large_contract(function_limit + 1, local_limit + 1),
             epoch_length,
             1,
         );
@@ -3426,10 +3428,38 @@ fn test_limit_contract_functions_number_upgrade() {
     };
 
     assert!(matches!(old_outcome.status, FinalExecutionStatus::SuccessValue(_)));
-    assert!(matches!(
-        new_outcome.status,
-        FinalExecutionStatus::Failure(TxExecutionError::ActionError(_))
-    ));
+    let e = match new_outcome.status {
+        FinalExecutionStatus::Failure(TxExecutionError::ActionError(e)) => e,
+        status => panic!("expected transaction to fail, got {:?}", status),
+    };
+    match e.kind {
+        ActionErrorKind::FunctionCallError(FunctionCallErrorSer::CompilationError(
+            CompilationError::PrepareError(e),
+        )) if e == expected_prepare_err => (),
+        kind => panic!("got unexpected action error kind: {:?}", kind),
+    }
+}
+
+// Check that we can't call a contract exceeding functions number limit after upgrade.
+#[test]
+fn test_function_limit_change() {
+    verify_contract_limits_upgrade(
+        ProtocolFeature::LimitContractFunctionsNumber,
+        100_000,
+        0,
+        PrepareError::TooManyFunctions,
+    );
+}
+
+// Check that we can't call a contract exceeding functions number limit after upgrade.
+#[test]
+fn test_local_limit_change() {
+    verify_contract_limits_upgrade(
+        ProtocolFeature::LimitContractLocals,
+        64,
+        15625,
+        PrepareError::TooManyLocals,
+    );
 }
 
 #[test]

runtime/near-vm-runner/src/tests/compile_errors.rs
@@ -159,7 +159,7 @@ fn test_limit_contract_functions_number() {
         let functions_number_limit: u32 = 10_000;
         let method_name = "main";
 
-        let code = near_test_contracts::many_functions_contract(functions_number_limit + 1);
+        let code = near_test_contracts::large_contract(functions_number_limit + 1, 0);
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -168,7 +168,7 @@ fn test_limit_contract_functions_number() {
         );
         assert_eq!(err, None);
 
-        let code = near_test_contracts::many_functions_contract(functions_number_limit);
+        let code = near_test_contracts::large_contract(functions_number_limit, 0);
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -177,7 +177,7 @@ fn test_limit_contract_functions_number() {
         );
         assert_eq!(err, None);
 
-        let code = near_test_contracts::many_functions_contract(functions_number_limit + 1);
+        let code = near_test_contracts::large_contract(functions_number_limit + 1, 0);
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -193,19 +193,6 @@ fn test_limit_contract_functions_number() {
     });
 }
 
-fn many_locals(n_locals: usize) -> Vec<u8> {
-    wat::parse_str(&format!(
-        r#"
-            (module
-              (func $main (export "main")
-                (local {})
-                (call $main))
-            )"#,
-        "i32 ".repeat(n_locals)
-    ))
-    .unwrap()
-}
-
 #[test]
 fn test_limit_locals() {
     with_vm_variants(|vm_kind| {
@@ -216,7 +203,7 @@ fn test_limit_locals() {
             VMKind::Wasmtime => return,
         }
 
-        let wasm_err = many_locals(50_001);
+        let wasm_err = near_test_contracts::large_contract(1, 50_001);
         let res = make_simple_contract_call_vm(&wasm_err, "main", vm_kind);
         gas_and_error_match(
             res,
@@ -226,13 +213,57 @@ fn test_limit_locals() {
             ))),
         );
 
-        let wasm_ok = many_locals(50_000);
+        let wasm_ok = near_test_contracts::large_contract(1, 50_000);
         let res = make_simple_contract_call_vm(&wasm_ok, "main", vm_kind);
         gas_and_error_match(
             res,
-            Some(47583963),
+            Some(43682463),
             Some(VMError::FunctionCallError(FunctionCallError::WasmTrap(WasmTrap::Unreachable))),
         );
+    })
+}
+
+#[test]
+fn test_limit_locals_global() {
+    with_vm_variants(|vm_kind| {
+        let new_protocol_version = ProtocolFeature::LimitContractLocals.protocol_version();
+        let old_protocol_version = new_protocol_version - 1;
+        let code_1000001_locals = near_test_contracts::large_contract(101, 9901);
+        match vm_kind {
+            VMKind::Wasmer0 | VMKind::Wasmer2 => {}
+            // All contracts leading to hardware traps can not run concurrently on Wasmtime and Wasmer,
+            // Restore, once get rid of Wasmer 0.x.
+            VMKind::Wasmtime => return,
+        }
+
+        let (_, err) = make_simple_contract_call_with_protocol_version_vm(
+            &code_1000001_locals,
+            "main",
+            new_protocol_version,
+            vm_kind,
+        );
+        assert_eq!(
+            err,
+            Some(VMError::FunctionCallError(FunctionCallError::CompilationError(
+                CompilationError::PrepareError(PrepareError::TooManyLocals),
+            ))),
+        );
+
+        let (_, err) = make_simple_contract_call_with_protocol_version_vm(
+            &code_1000001_locals,
+            "main",
+            old_protocol_version,
+            vm_kind,
+        );
+        assert_eq!(err, None);
+
+        let (_, err) = make_simple_contract_call_with_protocol_version_vm(
+            &near_test_contracts::large_contract(64, 15625),
+            "main",
+            new_protocol_version,
+            vm_kind,
+        );
+        assert_eq!(err, None);
     })
 }
 

commit cf26100ac55268c4c6a9e95f99f2a89b8325123a

Count function imports towards the function limit
Fixes a regression introduced in
7f1672f89f1700eb3601f742759ac2742f7007aa

runtime/near-test-contracts/src/lib.rs
@@ -86,43 +86,66 @@ fn smoke_test() {
     assert!(!base_rs_contract().is_empty());
 }
 
-/// Construct a contract with many entitites.
-///
-/// Currently supports constructing contracts that contain a specified number of functions with the
-/// specified number of locals each.
-///
-/// Exports a function called `main` that does nothing.
-pub fn large_contract(functions: u32, locals: u32) -> Vec<u8> {
-    use wasm_encoder::{
-        CodeSection, Export, ExportSection, Function, FunctionSection, Instruction, Module,
-        TypeSection, ValType,
-    };
-    // Won't generate a valid WASM without functions.
-    assert!(functions >= 1, "must specify at least 1 function to be generated");
-    let mut module = Module::new();
-    let mut type_section = TypeSection::new();
-    type_section.function([], []);
-    module.section(&type_section);
+pub struct LargeContract {
+    pub functions: u32,
+    pub locals_per_function: u32,
+    pub panic_imports: u32, // How many times to import `env.panic`
+}
 
-    let mut functions_section = FunctionSection::new();
-    for _ in 0..functions {
-        functions_section.function(0);
+impl Default for LargeContract {
+    fn default() -> Self {
+        Self { functions: 1, locals_per_function: 0, panic_imports: 0 }
     }
-    module.section(&functions_section);
+}
 
-    let mut exports_section = ExportSection::new();
-    exports_section.export("main", Export::Function(0));
-    module.section(&exports_section);
+impl LargeContract {
+    /// Construct a contract with many entitites.
+    ///
+    /// Currently supports constructing contracts that contain a specified number of functions with the
+    /// specified number of locals each.
+    ///
+    /// Exports a function called `main` that does nothing.
+    pub fn make(&self) -> Vec<u8> {
+        use wasm_encoder::{
+            CodeSection, EntityType, Export, ExportSection, Function, FunctionSection,
+            ImportSection, Instruction, Module, TypeSection, ValType,
+        };
 
-    let mut code_section = CodeSection::new();
-    for _ in 0..functions {
-        let mut f = Function::new([(locals, ValType::I64)]);
-        f.instruction(&Instruction::End);
-        code_section.function(&f);
+        // Won't generate a valid WASM without functions.
+        assert!(self.functions >= 1, "must specify at least 1 function to be generated");
+        let mut module = Module::new();
+        let mut type_section = TypeSection::new();
+        type_section.function([], []);
+        module.section(&type_section);
+
+        if self.panic_imports != 0 {
+            let mut import_section = ImportSection::new();
+            for _ in 0..self.panic_imports {
+                import_section.import("env", "panic", EntityType::Function(0));
+            }
+            module.section(&import_section);
+        }
+
+        let mut functions_section = FunctionSection::new();
+        for _ in 0..self.functions {
+            functions_section.function(0);
+        }
+        module.section(&functions_section);
+
+        let mut exports_section = ExportSection::new();
+        exports_section.export("main", Export::Function(0));
+        module.section(&exports_section);
+
+        let mut code_section = CodeSection::new();
+        for _ in 0..self.functions {
+            let mut f = Function::new([(self.locals_per_function, ValType::I64)]);
+            f.instruction(&Instruction::End);
+            code_section.function(&f);
+        }
+        module.section(&code_section);
+
+        module.finish()
     }
-    module.section(&code_section);
-
-    module.finish()
 }
 
 /// Generate an arbitrary valid contract.

runtime/near-vm-runner/src/prepare.rs
@@ -29,13 +29,33 @@ pub(crate) const WASM_FEATURES: wasmparser::WasmFeatures = wasmparser::WasmFeatu
 fn wasmparser_decode(
     code: &[u8],
 ) -> Result<(Option<u64>, Option<u64>), wasmparser::BinaryReaderError> {
-    use wasmparser::ValidPayload;
+    use wasmparser::{ImportSectionEntryType, ValidPayload};
     let mut validator = wasmparser::Validator::new();
     validator.wasm_features(WASM_FEATURES);
     let mut function_count = Some(0u64);
     let mut local_count = Some(0u64);
     for payload in wasmparser::Parser::new(0).parse_all(code) {
-        match validator.payload(&payload?)? {
+        let payload = payload?;
+
+        // The validator does not output `ValidPayload::Func` for imported functions.
+        if let wasmparser::Payload::ImportSection(ref import_section_reader) = payload {
+            let mut import_section_reader = import_section_reader.clone();
+            for _ in 0..import_section_reader.get_count() {
+                match import_section_reader.read()?.ty {
+                    ImportSectionEntryType::Function(_) => {
+                        function_count = function_count.and_then(|f| f.checked_add(1))
+                    }
+                    ImportSectionEntryType::Table(_)
+                    | ImportSectionEntryType::Memory(_)
+                    | ImportSectionEntryType::Event(_)
+                    | ImportSectionEntryType::Global(_)
+                    | ImportSectionEntryType::Module(_)
+                    | ImportSectionEntryType::Instance(_) => {}
+                }
+            }
+        }
+
+        match validator.payload(&payload)? {
             ValidPayload::Ok => (),
             ValidPayload::Submodule(_) => panic!("submodules are not reachable (not enabled)"),
             ValidPayload::Func(mut validator, body) => {

integration-tests/src/tests/client/process_blocks.rs
@@ -3371,7 +3371,12 @@ fn verify_contract_limits_upgrade(
         deploy_test_contract(
             &mut env,
             "test0".parse().unwrap(),
-            &near_test_contracts::large_contract(function_limit + 1, local_limit + 1),
+            &near_test_contracts::LargeContract {
+                functions: function_limit + 1,
+                locals_per_function: local_limit + 1,
+                ..Default::default()
+            }
+            .make(),
             epoch_length,
             1,
         );

runtime/near-vm-runner/src/tests/compile_errors.rs
@@ -144,7 +144,11 @@ fn test_limit_contract_functions_number() {
         let functions_number_limit: u32 = 10_000;
         let method_name = "main";
 
-        let code = near_test_contracts::large_contract(functions_number_limit + 1, 0);
+        let code = near_test_contracts::LargeContract {
+            functions: functions_number_limit + 1,
+            ..Default::default()
+        }
+        .make();
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -153,7 +157,11 @@ fn test_limit_contract_functions_number() {
         );
         assert_eq!(err, None);
 
-        let code = near_test_contracts::large_contract(functions_number_limit, 0);
+        let code = near_test_contracts::LargeContract {
+            functions: functions_number_limit,
+            ..Default::default()
+        }
+        .make();
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -162,7 +170,49 @@ fn test_limit_contract_functions_number() {
         );
         assert_eq!(err, None);
 
-        let code = near_test_contracts::large_contract(functions_number_limit + 1, 0);
+        let code = near_test_contracts::LargeContract {
+            functions: functions_number_limit + 1,
+            ..Default::default()
+        }
+        .make();
+        let (_, err) = make_simple_contract_call_with_protocol_version_vm(
+            &code,
+            method_name,
+            new_protocol_version,
+            vm_kind,
+        );
+        assert_matches!(
+            err,
+            Some(VMError::FunctionCallError(FunctionCallError::CompilationError(
+                CompilationError::PrepareError(PrepareError::TooManyFunctions)
+            )))
+        );
+
+        let code = near_test_contracts::LargeContract {
+            functions: functions_number_limit / 2,
+            panic_imports: functions_number_limit / 2 + 1,
+            ..Default::default()
+        }
+        .make();
+        let (_, err) = make_simple_contract_call_with_protocol_version_vm(
+            &code,
+            method_name,
+            new_protocol_version,
+            vm_kind,
+        );
+        assert_matches!(
+            err,
+            Some(VMError::FunctionCallError(FunctionCallError::CompilationError(
+                CompilationError::PrepareError(PrepareError::TooManyFunctions)
+            )))
+        );
+
+        let code = near_test_contracts::LargeContract {
+            functions: functions_number_limit,
+            panic_imports: 1,
+            ..Default::default()
+        }
+        .make();
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
             &code,
             method_name,
@@ -188,7 +238,12 @@ fn test_limit_locals() {
             VMKind::Wasmtime => return,
         }
 
-        let wasm_err = near_test_contracts::large_contract(1, 50_001);
+        let wasm_err = near_test_contracts::LargeContract {
+            functions: 1,
+            locals_per_function: 50_001,
+            ..Default::default()
+        }
+        .make();
         let res = make_simple_contract_call_vm(&wasm_err, "main", vm_kind);
         gas_and_error_match(
             res,
@@ -198,7 +253,12 @@ fn test_limit_locals() {
             ))),
         );
 
-        let wasm_ok = near_test_contracts::large_contract(1, 50_000);
+        let wasm_ok = near_test_contracts::LargeContract {
+            functions: 1,
+            locals_per_function: 50_000,
+            ..Default::default()
+        }
+        .make();
         let res = make_simple_contract_call_vm(&wasm_ok, "main", vm_kind);
         gas_and_error_match(
             res,
@@ -213,7 +273,12 @@ fn test_limit_locals_global() {
     with_vm_variants(|vm_kind| {
         let new_protocol_version = ProtocolFeature::LimitContractLocals.protocol_version();
         let old_protocol_version = new_protocol_version - 1;
-        let code_1000001_locals = near_test_contracts::large_contract(101, 9901);
+        let code_1000001_locals = near_test_contracts::LargeContract {
+            functions: 101,
+            locals_per_function: 9901,
+            ..Default::default()
+        }
+        .make();
         match vm_kind {
             VMKind::Wasmer0 | VMKind::Wasmer2 => {}
             // All contracts leading to hardware traps can not run concurrently on Wasmtime and Wasmer,
@@ -243,7 +308,12 @@ fn test_limit_locals_global() {
         assert_eq!(err, None);
 
         let (_, err) = make_simple_contract_call_with_protocol_version_vm(
-            &near_test_contracts::large_contract(64, 15625),
+            &near_test_contracts::LargeContract {
+                functions: 64,
+                locals_per_function: 15625,
+                ..Default::default()
+            }
+            .make(),
             "main",
             new_protocol_version,
             vm_kind,