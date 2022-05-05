4d89986e11cc7e97d704f71ec229b875adc1a3cf
mzhangmzz <34969888+mzhangmzz@users.noreply.github.com>
2022-05-05 16:00:02 -0400

    move the saving of block_extra to after apply_chunks (#6744)
    helps #6745

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -3657,13 +3657,6 @@ impl<'a> ChainUpdate<'a> {
     {
         let mut result: Vec<Box<dyn FnOnce() -> Result<ApplyChunkResult, Error> + Send + 'static>> =
             Vec::new();
-        let challenges_result = self.verify_challenges(
-            block.challenges(),
-            block.header().epoch_id(),
-            block.header().prev_hash(),
-            Some(block.hash()),
-        )?;
-        self.chain_store_update.save_block_extra(block.hash(), BlockExtra { challenges_result });
         #[cfg(not(feature = "mock_node"))]
         let protocol_version =
             self.runtime_adapter.get_epoch_protocol_version(block.header().epoch_id())?;
@@ -4411,6 +4404,13 @@ impl<'a> ChainUpdate<'a> {
             return Err(ErrorKind::InvalidGasPrice.into());
         }
 
+        let challenges_result = self.verify_challenges(
+            block.challenges(),
+            block.header().epoch_id(),
+            block.header().prev_hash(),
+            Some(block.hash()),
+        )?;
+
         let prev_block = self.chain_store_update.get_block(&prev_hash)?.clone();
 
         self.validate_chunk_headers(&block, &prev_block)?;
@@ -4446,6 +4446,8 @@ impl<'a> ChainUpdate<'a> {
         for (shard_id, receipt_proofs) in receipts_by_shard {
             self.chain_store_update.save_incoming_receipt(block.hash(), shard_id, receipt_proofs);
         }
+
+        self.chain_store_update.save_block_extra(block.hash(), BlockExtra { challenges_result });
 
         self.chain_store_update.save_block_header(block.header().clone())?;
         self.update_header_head_if_not_challenged(block.header())?;
