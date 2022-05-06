af4317d3c845dd947e3d803361ead157be94ef08
Longarithm <the.aleksandr.logunov@gmail.com>
2022-05-06 20:46:14 +0400

    plain comment

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -4682,8 +4682,8 @@ impl<'a> ChainUpdate<'a> {
                 return Err(ErrorKind::InvalidBlockMerkleRoot.into());
             }
 
-            /// Check that challenges root is empty to ensure later that block doesn't contain challenges.
-            /// TODO (#2445): Enable challenges when they are working correctly.
+            // Check that challenges root is empty to ensure later that block doesn't contain challenges.
+            // TODO (#2445): Enable challenges when they are working correctly.
             if header.challenges_root() != &MerkleHash::default() {
                 return Err(ErrorKind::InvalidChallengeRoot.into());
             }
