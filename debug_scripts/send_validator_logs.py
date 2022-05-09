f47b87d809684c96e29754c9efa183c26ee70880
Anshal savla <anshalsavla@Anshals-MacBook-Pro.local>
2022-05-09 16:52:30 -0700

    formatting fix

--- debug_scripts/send_validator_logs.py
+++ debug_scripts/send_validator_logs.py
@@ -8,7 +8,8 @@ import sys
 import urllib.parse
 
 
-def filter_log_file(log_file: str, start_time: datetime.datetime, end_time: datetime.datetime) -> list:
+def filter_log_file(log_file: str, start_time: datetime.datetime,
+                    end_time: datetime.datetime) -> list:
     """
     Filter log file for a time range.
     start_time: datetime.datetime
@@ -27,7 +28,9 @@ def filter_log_file(log_file: str, start_time: datetime.datetime, end_time: date
         for line in f:
             # [0m and [2m are ANSI shell color codes. Removing them to parse dates.
             split_lines = line.split("[0m", 1)[0].replace("\x1b[2m", "")
-            dt = datetime.datetime.strptime(split_lines[:-5], "%b %d %H:%M:%S").replace(year=datetime.datetime.now().year)
+            dt = datetime.datetime.strptime(
+                split_lines[:-5],
+                "%b %d %H:%M:%S").replace(year=datetime.datetime.now().year)
             if start_time <= dt <= end_time:
                 filtered_logs.append(line)
     return filtered_logs
@@ -49,10 +52,13 @@ def upload_to_s3(file_lines: list, account: str) -> str:
 
     file_obj = io.BytesIO(file_string.getvalue().encode())
     gzipped_content = gzip.compress(file_obj.read())
-    print(f"uploading compressed file. File size is: {sys.getsizeof(gzipped_content)} Bytes")
-    
+    print(
+        f"uploading compressed file. File size is: {sys.getsizeof(gzipped_content)} Bytes"
+    )
+
     s3 = boto3.resource('s3')
-    s3.Bucket(BUCKET).upload_fileobj(io.BytesIO(gzipped_content), f"logs/{s3_destination}")
+    s3.Bucket(BUCKET).upload_fileobj(io.BytesIO(gzipped_content),
+                                     f"logs/{s3_destination}")
     s3_link = f"https://{BUCKET}.s3.amazonaws.com/logs/{urllib.parse.quote(s3_destination)}"
     print(f"Log File was uploaded to S3: {s3_link}")
     file_obj.close()
@@ -61,14 +67,26 @@ def upload_to_s3(file_lines: list, account: str) -> str:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send logs to near.')
-    parser.add_argument('--log_file', type=str, help='Absolute path to log file.', required=True)
-    parser.add_argument('--account', type=str, help='Near account id.', required=True)
-    parser.add_argument('--last_seconds', type=int, help='Filter logs for last x seconds.', required=True)
+    parser.add_argument('--log_file',
+                        type=str,
+                        help='Absolute path to log file.',
+                        required=True)
+    parser.add_argument('--account',
+                        type=str,
+                        help='Near account id.',
+                        required=True)
+    parser.add_argument('--last_seconds',
+                        type=int,
+                        help='Filter logs for last x seconds.',
+                        required=True)
     args = parser.parse_args()
-    
+
     log_file_path = args.log_file
     end_timestamp = datetime.datetime.utcnow()
-    start_timestamp = end_timestamp - datetime.timedelta(seconds=args.last_seconds)
+    start_timestamp = end_timestamp - datetime.timedelta(
+        seconds=args.last_seconds)
 
-    filtered_log_lines = filter_log_file(log_file=args.log_file, start_time=start_timestamp, end_time=end_timestamp)
+    filtered_log_lines = filter_log_file(log_file=args.log_file,
+                                         start_time=start_timestamp,
+                                         end_time=end_timestamp)
     upload_to_s3(file_lines=filtered_log_lines, account=args.account)
