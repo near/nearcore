3c3e5c5685547da0a34c8c0eec2b4f4dee6332ae
Grzegorz Prusak <pompon.pompon@gmail.com>
2022-05-09 09:23:28 +0200

    added missing protocol mismatch guard reimplemented handshake test in rust removed partial handshake parsing for version detection

--- chain/network/src/tests/peer_communication.rs
+++ chain/network/src/tests/peer_communication.rs
@@ -1,45 +1,18 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
 use crate::network_protocol::Encoding;
-use crate::peer::codec::Codec;
-use crate::peer::peer_actor::PeerActor;
-use crate::private_actix::{PeerRequestResult, RegisterPeer, RegisterPeerResponse, SendMessage};
-use crate::stats::metrics::NetworkMetrics;
 use crate::tests::data;
 use crate::tests::peer_actor::{PeerConfig, PeerHandle, Response};
+use crate::tests::stream::Stream;
 use crate::tests::util::{make_rng, FakeClock};
-use crate::types::{
-    NetworkClientMessages, NetworkClientResponses, NetworkRequests, NetworkResponses,
-    PeerManagerMessageRequest, PeerManagerMessageResponse, PeerMessage, RoutingTableUpdate,
-};
-use actix::{Actor, Addr, Context, Handler, StreamHandler as _};
-use anyhow::{anyhow, bail, Context as _};
-use near_crypto::{InMemorySigner, KeyType, PublicKey, SecretKey};
+use crate::types::{Handshake, HandshakeFailureReason, PeerMessage};
+use anyhow::Context as _;
+use near_logger_utils::init_test_logger;
 use near_network_primitives::types::{
-    Edge, NetworkViewClientMessages, NetworkViewClientResponses, PartialEdgeInfo,
-    PartialEncodedChunkRequestMsg, PartialEncodedChunkResponseMsg, PeerInfo, PeerType,
-    RoutedMessageBody,
+    PartialEncodedChunkRequestMsg, PartialEncodedChunkResponseMsg, RoutedMessageBody,
 };
-use near_performance_metrics::framed_write::FramedWrite;
-use near_primitives::block::{genesis_chunks, Block, BlockHeader, GenesisId};
-use near_primitives::hash::CryptoHash;
-use near_primitives::network::{AnnounceAccount, PeerId};
-use near_primitives::num_rational::Rational;
-use near_primitives::sharding::ShardChunk;
 use near_primitives::syncing::EpochSyncResponse;
-use near_primitives::types::{AccountId, BlockHeight, EpochId, StateRoot};
-use near_primitives::validator_signer::{InMemoryValidatorSigner, ValidatorSigner};
-use near_primitives::version::PROTOCOL_VERSION;
-use near_rate_limiter::{
-    ActixMessageResponse, ActixMessageWrapper, ThrottleController, ThrottleFramedRead,
-    ThrottleToken,
-};
-use rand::Rng;
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
-use std::sync::atomic::AtomicUsize;
+use near_primitives::types::EpochId;
+use near_primitives::version::PEER_MIN_ALLOWED_PROTOCOL_VERSION;
 use std::sync::Arc;
-use std::time;
-use tokio_stream::StreamExt;
 
 async fn test_peer_communication(
     outbound_encoding: Option<Encoding>,
@@ -49,21 +22,24 @@ async fn test_peer_communication(
     let mut clock = FakeClock::default();
 
     let chain = Arc::new(data::Chain::make(&mut clock, &mut rng, 12));
-    let (mut outbound, mut inbound) = PeerHandle::start(
-        PeerConfig {
-            signer: data::make_signer(&mut rng),
-            chain: chain.clone(),
-            peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
-            force_encoding: outbound_encoding,
-        },
-        PeerConfig {
-            signer: data::make_signer(&mut rng),
-            chain: chain.clone(),
-            peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
-            force_encoding: inbound_encoding,
-        },
-    )
-    .await?;
+    let inbound_cfg = PeerConfig {
+        signer: data::make_signer(&mut rng),
+        chain: chain.clone(),
+        peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
+        force_encoding: inbound_encoding,
+        start_handshake_with: None,
+    };
+    let outbound_cfg = PeerConfig {
+        signer: data::make_signer(&mut rng),
+        chain: chain.clone(),
+        peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
+        force_encoding: outbound_encoding,
+        start_handshake_with: Some(inbound_cfg.id()),
+    };
+
+    let (outbound_stream, inbound_stream) = PeerHandle::start_connection().await;
+    let mut inbound = PeerHandle::start_endpoint(inbound_cfg, inbound_stream).await;
+    let mut outbound = PeerHandle::start_endpoint(outbound_cfg, outbound_stream).await;
 
     assert_eq!(Response::HandshakeDone, outbound.recv().await);
     assert_eq!(Response::HandshakeDone, inbound.recv().await);
@@ -114,26 +90,28 @@ async fn test_peer_communication(
 
     // PartialEncodedChunkRequest
     let want = chain.blocks[5].chunks()[2].chunk_hash();
-    let msg = outbound.routed_message(RoutedMessageBody::PartialEncodedChunkRequest(
-        PartialEncodedChunkRequestMsg {
+    let msg = outbound.routed_message(
+        RoutedMessageBody::PartialEncodedChunkRequest(PartialEncodedChunkRequestMsg {
             chunk_hash: want.clone(),
             part_ords: vec![],
             tracking_shards: Default::default(),
-        },
-    ));
+        }),
+        inbound.cfg.id(),
+    );
     outbound.send(PeerMessage::Routed(msg)).await;
     assert_eq!(Response::ChunkRequest(want), inbound.recv().await);
 
     // PartialEncodedChunkResponse
     let want_hash = chain.blocks[3].chunks()[0].chunk_hash();
     let want_parts = data::make_chunk_parts(chain.chunks[&want_hash].clone());
-    let msg = outbound.routed_message(RoutedMessageBody::PartialEncodedChunkResponse(
-        PartialEncodedChunkResponseMsg {
+    let msg = outbound.routed_message(
+        RoutedMessageBody::PartialEncodedChunkResponse(PartialEncodedChunkResponseMsg {
             chunk_hash: want_hash,
             parts: want_parts.clone(),
             receipts: vec![],
-        },
-    ));
+        }),
+        inbound.cfg.id(),
+    );
     outbound.send(PeerMessage::Routed(msg)).await;
     assert_eq!(Response::Chunk(want_parts), inbound.recv().await);
 
@@ -184,6 +162,107 @@ async fn peer_communication() -> anyhow::Result<()> {
             test_peer_communication(outbound.clone(), inbound.clone())
                 .await
                 .with_context(|| format!("(outbound={outbound:?},inbound={inbound:?})"))?;
+        }
+    }
+    Ok(())
+}
+
+async fn test_handshake(outbound_encoding: Option<Encoding>, inbound_encoding: Option<Encoding>) {
+    let mut rng = make_rng(89028037453);
+    let mut clock = FakeClock::default();
+
+    let chain = Arc::new(data::Chain::make(&mut clock, &mut rng, 12));
+    let inbound_cfg = PeerConfig {
+        signer: data::make_signer(&mut rng),
+        chain: chain.clone(),
+        peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
+        force_encoding: inbound_encoding,
+        start_handshake_with: None,
+    };
+    let outbound_cfg = PeerConfig {
+        signer: data::make_signer(&mut rng),
+        chain: chain.clone(),
+        peers: (0..5).map(|_| data::make_peer_info(&mut rng)).collect(),
+        force_encoding: outbound_encoding,
+        start_handshake_with: None,
+    };
+    let (outbound_stream, inbound_stream) = PeerHandle::start_connection().await;
+    let inbound = PeerHandle::start_endpoint(inbound_cfg, inbound_stream).await;
+    let mut outbound = Stream::new(outbound_encoding, outbound_stream);
+
+    // Send too old PROTOCOL_VERSION, expect ProtocolVersionMismatch
+    let mut handshake = Handshake {
+        protocol_version: PEER_MIN_ALLOWED_PROTOCOL_VERSION - 1,
+        oldest_supported_version: PEER_MIN_ALLOWED_PROTOCOL_VERSION - 1,
+        sender_peer_id: outbound_cfg.id(),
+        target_peer_id: inbound.cfg.id(),
+        sender_listen_port: Some(outbound.local_addr.port()),
+        sender_chain_info: outbound_cfg.chain.get_info(),
+        partial_edge_info: outbound_cfg.partial_edge_info(&inbound.cfg.id(), 1),
+    };
+    // We will also introduce chain_id mismatch, but ProtocolVersionMismatch is expected to take priority.
+    handshake.sender_chain_info.genesis_id.chain_id = "unknown_chain".to_string();
+    outbound.write(&PeerMessage::Handshake(handshake.clone())).await;
+
+    let resp = outbound.read().await;
+    let resp_type: &'static str = (&resp).into();
+    let reason = if let PeerMessage::HandshakeFailure(_, reason) = resp {
+        reason
+    } else {
+        panic!("want HandshakeFailure, got {}", resp_type);
+    };
+    let reason_type: &'static str = (&reason).into();
+    let version = if let HandshakeFailureReason::ProtocolVersionMismatch { version, .. } = reason {
+        version
+    } else {
+        panic!("want ProtocolVersionMismatch, got {}", reason_type);
+    };
+
+    // Send mismatching chain_id, expect GenesisMismatch.
+    // We fix protocol_version, but chain_id is still mismatching.
+    handshake.protocol_version = version;
+    handshake.oldest_supported_version = version;
+    outbound.write(&PeerMessage::Handshake(handshake.clone())).await;
+    let resp = outbound.read().await;
+    let resp_type: &'static str = (&resp).into();
+    let reason = if let PeerMessage::HandshakeFailure(_, reason) = resp {
+        reason
+    } else {
+        panic!("want HandshakeFailure, got {}", resp_type);
+    };
+    let reason_type: &'static str = (&reason).into();
+    let genesis_id = if let HandshakeFailureReason::GenesisMismatch(genesis_id) = reason {
+        genesis_id
+    } else {
+        panic!("want GenesisMismatch, got {}", reason_type);
+    };
+
+    // Send a correct Handshake, expect a matching Handshake response.
+    handshake.sender_chain_info.genesis_id.chain_id = genesis_id.chain_id;
+    outbound.write(&PeerMessage::Handshake(handshake.clone())).await;
+    let resp = outbound.read().await;
+    let resp_type: &'static str = (&resp).into();
+    let _resp = if let PeerMessage::Handshake(handshake) = resp {
+        handshake
+    } else {
+        panic!("want Handshake, got {}", resp_type);
+    };
+}
+
+#[tokio::test]
+// Verifies that HandshakeFailures are served correctly.
+async fn handshake() -> anyhow::Result<()> {
+    init_test_logger();
+    let encodings = [None, Some(Encoding::Proto), Some(Encoding::Borsh)];
+    for outbound in &encodings {
+        for inbound in &encodings {
+            println!("oubound = {:?}, inbound = {:?}", outbound, inbound);
+            if let (Some(a), Some(b)) = (outbound, inbound) {
+                if *a != *b {
+                    continue;
+                }
+            }
+            test_handshake(outbound.clone(), inbound.clone()).await;
         }
     }
     Ok(())
