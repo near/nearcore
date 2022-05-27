39ab20bc8f3832e9f66a098d600ce9b2998e1cd7
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2022-05-27 15:05:06 -0400

    fix(pytests): fix gc_after_sync.py failure (#6901)
    e.g.: https://nayduck.near.org/#/test/322670
    
    After waiting for the node that's been restarted to sync, the block at
    height HEAD-10 might not be stored since state sync will start at the
    beginning of the epoch, which may be after that point. So start checking
    blocks at max(HEAD-10, epoch_start) instead

--- pytest/lib/cluster.py
+++ pytest/lib/cluster.py
@@ -269,8 +269,12 @@ class BaseNode(object):
 
         return reversed(heights)
 
-    def get_validators(self):
-        return self.json_rpc('validators', [None])
+    def get_validators(self, epoch_id=None):
+        if epoch_id is None:
+            args = [None]
+        else:
+            args = {'epoch_id': epoch_id}
+        return self.json_rpc('validators', args)
 
     def get_account(self, acc, finality='optimistic', do_assert=True):
         res = self.json_rpc('query', {
