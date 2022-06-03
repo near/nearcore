3de700947199b7f339f699f3003af70c800fe925
Min Zhang <min@near.org>
2022-06-03 17:12:46 -0400

    fix CI

--- chain/client/src/sync.rs
+++ chain/client/src/sync.rs
@@ -1628,7 +1628,9 @@ mod test {
         }
         let block_headers = blocks.iter().map(|b| b.header().clone()).collect::<Vec<_>>();
         let peer_infos = create_peer_infos(2);
-        env.clients[1].chain.sync_block_headers(block_headers, &mut |_| unreachable!()).unwrap();
+        let mut challenges = vec![];
+        env.clients[1].chain.sync_block_headers(block_headers, &mut challenges).unwrap();
+        assert!(challenges.is_empty());
 
         // fetch three blocks at a time
         for i in 0..3 {
@@ -1696,7 +1698,9 @@ mod test {
         }
         let block_headers = blocks.iter().map(|b| b.header().clone()).collect::<Vec<_>>();
         let peer_infos = create_peer_infos(2);
-        env.clients[1].chain.sync_block_headers(block_headers, &mut |_| unreachable!()).unwrap();
+        let mut challenges = vec![];
+        env.clients[1].chain.sync_block_headers(block_headers, &mut challenges).unwrap();
+        assert!(challenges.is_empty());
         let is_state_sync = block_sync.block_sync(&mut env.clients[1].chain, &peer_infos).unwrap();
         assert!(!is_state_sync);
         let requested_block_hashes = collect_hashes_from_network_adapter(network_adapter.clone());
