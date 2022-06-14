a40c1d6e80775ade0eaa1845b3e7c65da3ff72ff
Michal Nazarewicz <mina86@mina86.com>
2022-06-14 17:10:25 +0200

    simplify serde defaults

--- core/store/src/config.rs
+++ core/store/src/config.rs
@@ -3,20 +3,18 @@ use near_primitives::version::DbVersion;
 const STORE_PATH: &str = "data";
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[serde(default)]
 pub struct StoreConfig {
     /// Path to the database.  If relative, resolved relative to neard home
     /// directory.  This is useful if node runs with a separate disk holding the
     /// database.
-    #[serde(default)]
     pub path: Option<std::path::PathBuf>,
 
     /// Collect internal storage layer statistics.
     /// Minor performance impact is expected.
-    #[serde(default)]
     pub enable_statistics: bool,
 
     /// Re-export storage layer statistics as prometheus metrics.
-    #[serde(default = "StoreConfig::default_enable_statistics_export")]
     pub enable_statistics_export: bool,
 
     /// Maximum number of store files being opened simultaneously.
@@ -26,7 +24,6 @@ pub struct StoreConfig {
     /// needs.
     /// Increasing this value up to a value higher than 1024 also requires setting `ulimit -n` in
     /// Linux.
-    #[serde(default = "StoreConfig::default_max_open_files")]
     pub max_open_files: u32,
 
     /// Cache size for DBCol::State column.
@@ -34,56 +31,16 @@ pub struct StoreConfig {
     /// Increasing DBCol::State cache size helps making storage more efficient. On the other hand we
     /// don't want to increase hugely requirements for running a node so currently we use a small
     /// default value for it.
-    #[serde(default = "StoreConfig::default_col_state_cache_size")]
     pub col_state_cache_size: bytesize::ByteSize,
 
     /// Block size used internally in RocksDB.
     /// Default value: 16KiB.
     /// We're still experimented with this parameter and it seems decreasing its value can improve
     /// the performance of the storage
-    #[serde(default = "StoreConfig::default_block_size")]
     pub block_size: bytesize::ByteSize,
 }
 
 impl StoreConfig {
-    /// Returns the default value for the `max_open_files` database limite.
-    ///
-    /// We used to use value of 512 but we were hitting that limit often and
-    /// store had to constantly close and reopen the same set of files.  Running
-    /// state viewer on a dense set of 500 blocks did almost 200k file opens
-    /// (having less than 7K unique files opened, some files were opened 400+
-    /// times).  Using 10k limit for max_open_files led to performance
-    /// improvement of ~11%.
-    const fn default_max_open_files() -> u32 {
-        10_000
-    }
-
-    /// Returns the default [`DBCol::State`] cache size.
-    ///
-    /// We used to have the same cache size for all columns, 32 MiB.  When some
-    /// RocksDB inefficiencies were found [`DBCol::State`] cache size was
-    /// increased up to 512 MiB.  This was done on 13th of Nov 2021 and we
-    /// consider increasing the value.  Tests have shown that increase to 25 GiB
-    /// (we've used this big value to estimate performance improvement headroom)
-    /// having `max_open_files` at 10k improved performance of state viewer by
-    /// 60%.
-    const fn default_col_state_cache_size() -> bytesize::ByteSize {
-        bytesize::ByteSize::mib(512)
-    }
-
-    /// Returns the default value for database block size.
-    ///
-    /// This value was taken from the Openethereum default parameter and we use
-    /// it since then.
-    const fn default_block_size() -> bytesize::ByteSize {
-        bytesize::ByteSize::kib(16)
-    }
-
-    /// Returns the default for `enable_statistics_export` setting.
-    const fn default_enable_statistics_export() -> bool {
-        true
-    }
-
     /// Returns configuration meant for tests.
     ///
     /// Since tests often operate with less data than real node, the test
@@ -108,10 +65,28 @@ impl Default for StoreConfig {
         Self {
             path: None,
             enable_statistics: false,
-            enable_statistics_export: Self::default_enable_statistics_export(),
-            max_open_files: Self::default_max_open_files(),
-            col_state_cache_size: Self::default_col_state_cache_size(),
-            block_size: Self::default_block_size(),
+            enable_statistics_export: true,
+
+            // We used to use value of 512 but we were hitting that limit often
+            // and store had to constantly close and reopen the same set of
+            // files.  Running state viewer on a dense set of 500 blocks did
+            // almost 200k file opens (having less than 7K unique files opened,
+            // some files were opened 400+ times).  Using 10k limit for
+            // max_open_files led to performance improvement of ~11%.
+            max_open_files: 10_000,
+
+            // We used to have the same cache size for all columns, 32 MiB.
+            // When some RocksDB inefficiencies were found [`DBCol::State`]
+            // cache size was increased up to 512 MiB.  This was done on 13th of
+            // Nov 2021 and we consider increasing the value.  Tests have shown
+            // that increase to 25 GiB (we've used this big value to estimate
+            // performance improvement headroom) having `max_open_files` at 10k
+            // improved performance of state viewer by 60%.
+            col_state_cache_size: bytesize::ByteSize::mib(512),
+
+            // This value was taken from the Openethereum default parameter and
+            // we use it since then.
+            block_size: bytesize::ByteSize::kib(16)
         }
     }
 }
