d718ecd8f931facb5265710e26ba8307fe3a3ee0
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-06-22 11:08:11 +0100

    perf: avoid unnecessary allocations when merklizing stuff (#7087)
    * CryptoHash::hash_borsh computes the hash in the streaming fashion,
      without allocating an intermediary vec
    * EncodedShardChunkBody, we avoid cloning boxes, taking references
      instead.

--- core/primitives/src/sharding.rs
+++ core/primitives/src/sharding.rs
@@ -810,7 +810,9 @@ impl EncodedShardChunkBody {
     }
 
     pub fn get_merkle_hash_and_paths(&self) -> (MerkleHash, Vec<MerklePath>) {
-        merklize(&self.parts.iter().map(|x| x.as_ref().unwrap().clone()).collect::<Vec<_>>())
+        let parts: Vec<&[u8]> =
+            self.parts.iter().map(|x| x.as_deref().unwrap()).collect::<Vec<_>>();
+        merklize(&parts)
     }
 }
 
