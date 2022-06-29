d8bef1e2e83cbd2135e2d0a3fa2ce2f54516aeea
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-06-30 00:21:32 +0200

    fix(shardnet): Create the correct account.

--- pytest/tests/shardnet/scripts/create_account.sh
+++ pytest/tests/shardnet/scripts/create_account.sh
@@ -3,7 +3,7 @@ set -x
 
 account_id=$(grep account_id /home/ubuntu/.near/shardnet/validator_key.json | awk -F'"' '{print $4}')
 mkdir -p /home/ubuntu/.near-credentials/shardnet/
-echo "{\"account_id\":\"$account_id\",\"public_key\":\"$1\",\"private_key\":\"$2\"}" > /home/ubuntu/.near-credentials/shardnet/near.json
+echo "{\"account_id\":\"near\",\"public_key\":\"$1\",\"private_key\":\"$2\"}" > /home/ubuntu/.near-credentials/shardnet/near.json
 pk=$(grep public_key /home/ubuntu/.near/shardnet/validator_key.json | awk -F'"' '{print $4}')
 cp /home/ubuntu/.near/shardnet/validator_key.json /home/ubuntu/.near-credentials/shardnet/$account_id.json
 NEAR_ENV=shardnet near --nodeUrl=http://127.0.0.1:3030 create-account $account_id --masterAccount near --initialBalance 1000000 --publicKey $pk
