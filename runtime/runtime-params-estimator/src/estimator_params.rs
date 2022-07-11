near/nearcore

commit 0ec3f95a84dadc9e5a513eb6de05aee5f8218f96

chore: drop gratitious dependency (#7154)
sha256 crate just glues hex and sha2 together, no need to pull this in

https://github.com/baoyachi/sha256-rs/blob/master/src/lib.rs

runtime/runtime-params-estimator/src/estimator_params.rs
@@ -2,7 +2,7 @@
 //! These parameters have been estimated manually and are now hard-coded for a more deterministic estimation of runtime parameters.
 //! This module contains the hard-coded constants as well as the code to manually re-estimate them.
 
-use near_primitives::types::Gas;
+use near_primitives::{hash::CryptoHash, types::Gas};
 use num_rational::Ratio;
 
 use crate::{config::GasMetric, gas_cost::GasCost};
@@ -31,12 +31,14 @@ pub(crate) fn sha256_cost(metric: GasMetric, repeats: u64) -> GasCost {
 }
 
 fn exec_sha256(repeats: u64) -> i64 {
-    use sha256::digest;
     let mut result = 0;
     for index in 0..repeats {
         let input = "what should I do but tend upon the hours, and times of your desire";
-        let val = digest(input);
-        assert_eq!(val, "9b4d38fd42c985baec11564a84366de0cbd26d3425ec4ce1266e26b7b951ac08");
+        let val = CryptoHash::hash_bytes(input.as_bytes());
+        assert_eq!(
+            val.to_string(),
+            "9b4d38fd42c985baec11564a84366de0cbd26d3425ec4ce1266e26b7b951ac08"
+        );
         result += val.as_bytes()[(index % 64) as usize] as i64;
     }
     result

runtime/runtime-params-estimator/Cargo.toml
@@ -22,7 +22,6 @@ borsh = "0.9"
 num-rational = "0.3"
 anyhow = "1"
 chrono = "0.4"
-sha256 = "1.0.2"
 bytesize = "1.1"
 tracing = "0.1"
 tracing-span-tree = "0.1"

commit bd9db71624cbf97aa23958cce64230eeb20594ec

fix: CpuBenchmarkSha256 encoding in assertion
regression from #7154

Previously, the string was converted to hex, while the `CryptoHash`
uses bs58 encoding.

This changes the output of the function. Which is completely irrelevant.
We only care that the hash gets copmuted and no compiler can remove it.

runtime/runtime-params-estimator/src/estimator_params.rs
@@ -35,11 +35,8 @@ fn exec_sha256(repeats: u64) -> i64 {
     for index in 0..repeats {
         let input = "what should I do but tend upon the hours, and times of your desire";
         let val = CryptoHash::hash_bytes(input.as_bytes());
-        assert_eq!(
-            val.to_string(),
-            "9b4d38fd42c985baec11564a84366de0cbd26d3425ec4ce1266e26b7b951ac08"
-        );
-        result += val.as_bytes()[(index % 64) as usize] as i64;
+        assert_eq!(val.to_string(), "BTEVNkcDtaui6SJC19Efnrf7A3dCKT9v8y24NiC3S7RR");
+        result += val.as_bytes()[(index % 32) as usize] as i64;
     }
     result
 }