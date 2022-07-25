fbac41f761d54e87fcfec220ff9e29893a50761b
nikurt <86772482+nikurt@users.noreply.github.com>
2022-07-25 12:11:57 +0200

    Update pytest/tests/shardnet/scripts/create_account.sh
    Co-authored-by: Michal Nazarewicz <mina86@mina86.com>

--- pytest/tests/shardnet/scripts/create_account.sh
+++ pytest/tests/shardnet/scripts/create_account.sh
@@ -5,5 +5,5 @@ account_id=$(grep account_id /home/ubuntu/.near/shardnet/validator_key.json | aw
 mkdir -p /home/ubuntu/.near-credentials/shardnet/
 echo "{\"account_id\":\"near\",\"public_key\":\"$1\",\"private_key\":\"$2\"}" > /home/ubuntu/.near-credentials/shardnet/near.json
 pk=$(grep public_key /home/ubuntu/.near/shardnet/validator_key.json | awk -F'"' '{print $4}')
-cp /home/ubuntu/.near/shardnet/validator_key.json /home/ubuntu/.near-credentials/shardnet/${account_id}.json
+cp /home/ubuntu/.near/shardnet/validator_key.json /home/ubuntu/.near-credentials/shardnet/"$account_id".json
 NEAR_ENV=shardnet near --nodeUrl=http://127.0.0.1:3030 create-account $account_id --masterAccount near --initialBalance 1000000 --publicKey $pk
