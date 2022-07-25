2afb9113f3b0765af0736d105fbe853cc1c67fce
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-07-25 13:50:43 +0100

    builder methods

--- chain/chain/src/test_utils/validator_schedule.rs
+++ chain/chain/src/test_utils/validator_schedule.rs
@@ -1,3 +1,5 @@
+use near_primitives::types::{AccountId, NumShards};
+
 /// Validator schedule describes how block and chunk producers are selected by
 /// the KeyValue runtime.
 ///
@@ -5,5 +7,28 @@
 /// to select the validators. For for tests though, we just want to select them
 /// by fiat.
 pub struct ValidatorSchedule {
+    block_producers: Vec<Vec<AccountId>>,
+    validator_groups: u64,
+    num_shards: NumShards,
+}
 
+impl ValidatorSchedule {
+    pub fn new() -> Self {
+        Self { block_producers: Vec::new(), validator_groups: 1, num_shards: 1 }
+    }
+    pub fn block_producers_per_epoch(
+        mut self,
+        block_producers: Vec<Vec<AccountId>>,
+    ) -> ValidatorSchedule {
+        self.block_producers = block_producers;
+        self
+    }
+    pub fn validator_groups(mut self, validator_groups: u64) -> Self {
+        self.validator_groups = validator_groups;
+        self
+    }
+    pub fn num_shards(mut self, num_shards: NumShards) -> Self {
+        self.num_shards = num_shards;
+        self
+    }
 }
