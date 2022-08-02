487fc6d15693dee1c809b1a0eb44984dbc40e069
Akhi Singhania <akhi3030@gmail.com>
2022-08-02 16:05:20 +0200

    move another test

--- integration-tests/src/tests/client/features.rs
+++ integration-tests/src/tests/client/features.rs
@@ -1,2 +1,4 @@
+#[cfg(feature = "protocol_feature_account_id_in_function_call_permission")]
+mod account_id_in_function_call_permission;
 #[cfg(feature = "protocol_feature_fix_contract_loading_cost")]
 mod fix_contract_loading_cost;
