b56bc32d001412f52cc99bd059d876d1a1ae086d
Akhi Singhania <akhi3030@gmail.com>
2022-08-02 16:15:22 +0200

    minor documentation comment

--- integration-tests/src/tests/client/features.rs
+++ integration-tests/src/tests/client/features.rs
@@ -1,3 +1,5 @@
+/// Collection of feature specific tests
+
 #[cfg(feature = "protocol_feature_account_id_in_function_call_permission")]
 mod account_id_in_function_call_permission;
 #[cfg(feature = "protocol_feature_fix_contract_loading_cost")]
