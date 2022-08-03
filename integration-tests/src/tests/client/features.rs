5f6fb829a6abac5853ee0d616a52938f5225785e
Akhi Singhania <akhi3030@gmail.com>
2022-08-03 11:03:09 +0200

    factor out another test

--- integration-tests/src/tests/client/features.rs
+++ integration-tests/src/tests/client/features.rs
@@ -5,3 +5,4 @@ mod account_id_in_function_call_permission;
 #[cfg(feature = "protocol_feature_fix_contract_loading_cost")]
 mod fix_contract_loading_cost;
 mod restore_receipts_after_fix;
+mod storage_usage_fix;
