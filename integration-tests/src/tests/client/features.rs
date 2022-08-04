f424d05f0d218b0cc918a47b53a78c4b339dcfca
Akhi Singhania <akhi3030@gmail.com>
2022-08-04 14:18:17 +0200

    more pr feedback

--- integration-tests/src/tests/client/features.rs
+++ integration-tests/src/tests/client/features.rs
@@ -4,5 +4,5 @@
 mod account_id_in_function_call_permission;
 #[cfg(feature = "protocol_feature_fix_contract_loading_cost")]
 mod fix_contract_loading_cost;
+mod fix_storage_usage;
 mod restore_receipts_after_fix_apply_chunks;
-mod storage_usage_fix;
