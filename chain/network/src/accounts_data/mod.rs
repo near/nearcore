near/nearcore

commit 80f7d3fe0d58281d30e78579a465378230c13705

Implemented demultiplexing for rate limiting broadcasts. (#7182)
* Implemented demultiplexing for rate limiting broadcasts.

* snapshot

* applied comments

* added test for rate limiting

* moved config verification inside PeerManagerActor::new

* applied comments

* fixed failing integration tests

* yet another test fix

chain/network/src/accounts_data/mod.rs
@@ -111,7 +111,7 @@ struct CacheInner {
     /// key is the public key of the account in the given epoch.
     /// It will be used to verify new incoming versions of SignedAccountData
     /// for this account.
-    data: HashMap<(EpochId, AccountId), SignedAccountData>,
+    data: HashMap<(EpochId, AccountId), Arc<SignedAccountData>>,
 }
 
 impl CacheInner {
@@ -123,7 +123,7 @@ impl CacheInner {
                 _ => true,
             }
     }
-    fn try_insert(&mut self, d: SignedAccountData) -> Option<SignedAccountData> {
+    fn try_insert(&mut self, d: Arc<SignedAccountData>) -> Option<Arc<SignedAccountData>> {
         if !self.is_new(&d) {
             return None;
         }
@@ -165,7 +165,10 @@ impl Cache {
     /// Returns the verified new data and an optional error.
     /// Note that even if error has been returned the partially validated output is returned
     /// anyway.
-    fn verify(&self, data: Vec<SignedAccountData>) -> (Vec<SignedAccountData>, Option<Error>) {
+    fn verify(
+        &self,
+        data: Vec<Arc<SignedAccountData>>,
+    ) -> (Vec<Arc<SignedAccountData>>, Option<Error>) {
         // Filter out non-interesting data, so that we never check signatures for valid non-interesting data.
         // Bad peers may force us to check signatures for fake data anyway, but we will ban them after first invalid signature.
         // It locks epochs for reading for a short period.
@@ -200,7 +203,7 @@ impl Cache {
         // Verify the signatures in parallel.
         // Verification will stop at the first encountered error.
         let (data, ok) = try_map(data_and_keys.into_values().par_bridge(), |(d, key)| {
-            if d.payload().verify(&key) {
+            if d.payload().verify(&key).is_ok() {
                 return Some(d);
             }
             return None;
@@ -216,8 +219,8 @@ impl Cache {
     /// WriteLock is acquired only for the final update (after verification).
     pub async fn insert(
         self: &Arc<Self>,
-        data: Vec<SignedAccountData>,
-    ) -> (Vec<SignedAccountData>, Option<Error>) {
+        data: Vec<Arc<SignedAccountData>>,
+    ) -> (Vec<Arc<SignedAccountData>>, Option<Error>) {
         let this = self.clone();
         // Execute verification on the rayon threadpool.
         let (data, err) = rayon_spawn(move || this.verify(data)).await;
@@ -228,7 +231,7 @@ impl Cache {
     }
 
     /// Copies and returns all the AccountData in the cache.
-    pub fn dump(&self) -> Vec<SignedAccountData> {
+    pub fn dump(&self) -> Vec<Arc<SignedAccountData>> {
         self.inner.read().data.values().cloned().collect()
     }
 }

chain/network/src/peer_manager/testonly.rs
@@ -1,12 +1,12 @@
 use crate::broadcast;
 use crate::config;
 use crate::network_protocol::testonly as data;
-use crate::network_protocol::PeerAddr;
+use crate::network_protocol::{PeerAddr, PeerMessage, SyncAccountsData};
+use crate::peer::peer_actor;
 use crate::peer_manager::peer_manager_actor;
 use crate::peer_manager::peer_manager_actor::Event as PME;
 use crate::testonly::actix::ActixSystem;
 use crate::testonly::fake_client;
-use crate::testonly::Rng;
 use crate::types::{ChainInfo, GetNetworkInfo, PeerManagerMessageRequest, SetChainInfo};
 use crate::PeerManagerActor;
 use actix::Actor;
@@ -27,6 +27,15 @@ pub struct ActorHandler {
     pub cfg: config::NetworkConfig,
     pub events: broadcast::Receiver<Event>,
     pub actix: ActixSystem<PeerManagerActor>,
+}
+
+pub fn unwrap_sync_accounts_data_processed(ev: Event) -> Option<SyncAccountsData> {
+    match ev {
+        Event::PeerManager(peer_manager_actor::Event::Peer(
+            peer_actor::Event::MessageProcessed(PeerMessage::SyncAccountsData(msg)),
+        )) => Some(msg),
+        _ => None,
+    }
 }
 
 impl ActorHandler {
@@ -67,29 +76,29 @@ impl ActorHandler {
             .await;
     }
 
+    // Awaits until the accounts_data state matches `want`.
     pub async fn wait_for_accounts_data(
         &mut self,
         want: &HashMap<(EpochId, AccountId), Vec<PeerAddr>>,
     ) {
-        // WARNING: this loop might become a spin-lock if any of the calls in the loop iteration
-        // was generating an event. To fix that, wait for specific events.
         loop {
             let info = self.actix.addr.send(GetNetworkInfo).await.unwrap();
             let got: HashMap<_, _> = info
                 .tier1_accounts
                 .into_iter()
-                .map(|d| ((d.epoch_id, d.account_id), d.peers))
+                .map(|d| ((d.epoch_id.clone(), d.account_id.clone()), d.peers.clone()))
                 .collect();
             if &got == want {
                 break;
             }
-            self.events.recv().await;
+            // It is important that we wait for the next PeerMessage::SyncAccountsData to get
+            // PROCESSED, not just RECEIVED. Otherwise we would get a race condition.
+            self.events.recv_until(unwrap_sync_accounts_data_processed).await;
         }
     }
 }
 
-pub async fn start(rng: &mut Rng, chain: Arc<data::Chain>) -> ActorHandler {
-    let cfg = chain.make_config(rng);
+pub async fn start(cfg: config::NetworkConfig, chain: Arc<data::Chain>) -> ActorHandler {
     let (send, recv) = broadcast::unbounded_channel();
     let actix = ActixSystem::spawn({
         let cfg = cfg.clone();

chain/network/src/peer_manager/tests.rs
@@ -1,3 +1,4 @@
+use crate::concurrency::demux;
 use crate::config;
 use crate::network_protocol::testonly as data;
 use crate::network_protocol::{Encoding, PeerAddr, SyncAccountsData};
@@ -14,6 +15,7 @@ use near_network_primitives::time;
 use near_network_primitives::types::{Ping, RoutedMessageBody};
 use near_primitives::network::PeerId;
 use pretty_assertions::assert_eq;
+use rand::seq::SliceRandom as _;
 use rand::Rng as _;
 use std::collections::HashSet;
 use std::sync::Arc;
@@ -28,7 +30,7 @@ async fn repeated_data_in_sync_routing_table() {
     let rng = &mut rng;
     let mut clock = time::FakeClock::default();
     let chain = Arc::new(data::Chain::make(&mut clock, rng, 10));
-    let pm = peer_manager::testonly::start(rng, chain.clone()).await;
+    let pm = peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await;
     let cfg = peer::testonly::PeerConfig {
         signer: data::make_signer(rng),
         chain,
@@ -95,7 +97,7 @@ async fn ttl() {
     let rng = &mut rng;
     let mut clock = time::FakeClock::default();
     let chain = Arc::new(data::Chain::make(&mut clock, rng, 10));
-    let mut pm = peer_manager::testonly::start(rng, chain.clone()).await;
+    let mut pm = peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await;
     let cfg = peer::testonly::PeerConfig {
         signer: data::make_signer(rng),
         chain,
@@ -186,7 +188,7 @@ async fn accounts_data_broadcast() {
     let clock = clock.clock();
     let clock = &clock;
 
-    let pm = peer_manager::testonly::start(rng, chain.clone()).await;
+    let pm = peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await;
 
     let take_sync = |ev| match ev {
         peer::testonly::Event::Peer(peer_actor::Event::MessageProcessed(
@@ -201,7 +203,7 @@ async fn accounts_data_broadcast() {
     let (mut peer1, got1) = add_peer(clock, rng, chain.clone(), &pm.cfg).await;
     assert_eq!(got1.accounts_data, vec![]);
 
-    // Send some data and wait for it to be broadcasted back.
+    // Send some data. It won't be broadcasted back.
     let msg = SyncAccountsData {
         accounts_data: vec![data[0].clone(), data[1].clone()],
         incremental: true,
@@ -209,8 +211,6 @@ async fn accounts_data_broadcast() {
     };
     let want = msg.accounts_data.clone();
     peer1.send(PeerMessage::SyncAccountsData(msg)).await;
-    let got1 = peer1.events.recv_until(take_sync).await;
-    assert_eq!(got1.accounts_data.as_set(), want.as_set());
 
     // Connect another peer and perform initial full sync.
     let (mut peer2, got2) = add_peer(clock, rng, chain.clone(), &pm.cfg).await;
@@ -224,9 +224,7 @@ async fn accounts_data_broadcast() {
     };
     let want = vec![data[2].clone()];
     peer1.send(PeerMessage::SyncAccountsData(msg)).await;
-    let got1 = peer1.events.recv_until(take_sync).await;
     let got2 = peer2.events.recv_until(take_sync).await;
-    assert_eq!(got1.accounts_data.as_set(), want.as_set());
     assert_eq!(got2.accounts_data.as_set(), want.as_set());
 
     // Send a request for a full sync.
@@ -267,9 +265,9 @@ async fn accounts_data_gradual_epoch_change() {
     // each position separately. If it was a Vec, the rest
     // of the test would be way harder to express.
     let mut pms = [
-        peer_manager::testonly::start(rng, chain.clone()).await,
-        peer_manager::testonly::start(rng, chain.clone()).await,
-        peer_manager::testonly::start(rng, chain.clone()).await,
+        peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await,
+        peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await,
+        peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await,
     ];
 
     // 0 <-> 1 <-> 2
@@ -305,4 +303,90 @@ async fn accounts_data_gradual_epoch_change() {
             pm.wait_for_accounts_data(&want).await;
         }
     }
+}
+
+// Test is expected to take ~5s.
+// Test with 20 peer managers connected in layers:
+// - 1st 5 and 2nd 5 are connected in full bipartite graph.
+// - 2nd 5 and 3rd 5 ...
+// - 3rd 5 and 4th 5 ...
+// All of them are validators.
+#[tokio::test(flavor = "multi_thread")]
+async fn accounts_data_rate_limiting() {
+    init_test_logger();
+    let mut rng = make_rng(921853233);
+    let rng = &mut rng;
+    let mut clock = time::FakeClock::default();
+    let chain = Arc::new(data::Chain::make(&mut clock, rng, 10));
+
+    // TODO(gprusak) 10 connections per peer is not much, try to scale up this test 2x (some config
+    // tweaking might be required).
+    let n = 4; // layers
+    let m = 5; // peer managers per layer
+    let mut pms = vec![];
+    for _ in 0..n * m {
+        let mut cfg = chain.make_config(rng);
+        cfg.accounts_data_broadcast_rate_limit = demux::RateLimit { qps: 0.5, burst: 1 };
+        pms.push(peer_manager::testonly::start(cfg, chain.clone()).await);
+    }
+    // Construct a 4-layer bipartite graph.
+    let mut connections = 0;
+    for i in 0..n - 1 {
+        for j in 0..m {
+            for k in 0..m {
+                let pi = pms[(i + 1) * m + k].peer_info();
+                pms[i * m + j].connect_to(&pi).await;
+                connections += 1;
+            }
+        }
+    }
+
+    // Validator configs.
+    let vs: Vec<_> = pms.iter().map(|pm| pm.cfg.validator.clone().unwrap()).collect();
+
+    // Construct ChainInfo for a new epoch,
+    // with tier1_accounts containing all validators.
+    let e = data::make_epoch_id(rng);
+    let mut chain_info = chain.get_chain_info();
+    chain_info.tier1_accounts = Arc::new(
+        vs.iter()
+            .map(|v| ((e.clone(), v.signer.validator_id().clone()), v.signer.public_key()))
+            .collect(),
+    );
+
+    // Advance epoch in random order.
+    pms.shuffle(rng);
+    for pm in &mut pms {
+        pm.set_chain_info(chain_info.clone()).await;
+    }
+
+    // Capture the event streams at the start, so that we can compute
+    // the total number of SyncAccountsData messages exchanged in the process.
+    let events: Vec<_> = pms.iter().map(|pm| pm.events.clone()).collect();
+
+    // Wait for data to arrive.
+    let want =
+        vs.iter().map(|v| ((e.clone(), v.signer.validator_id().clone()), peer_addrs(&v))).collect();
+    for pm in &mut pms {
+        pm.wait_for_accounts_data(&want).await;
+    }
+
+    // Count the SyncAccountsData messages exchanged.
+    let mut msgs = 0;
+    for mut es in events {
+        while let Some(ev) = es.try_recv() {
+            if peer_manager::testonly::unwrap_sync_accounts_data_processed(ev).is_some() {
+                msgs += 1;
+            }
+        }
+    }
+
+    // We expect 3 rounds communication to cover the distance from 1st layer to 4th layer
+    // and +1 full sync at handshake.
+    // The communication is bidirectional, which gives 8 messages per connection.
+    // Then add +50% to accomodate for test execution flakiness (12 messages per connection).
+    // TODO(gprusak): if the test is still flaky, upgrade FakeClock for stricter flow control.
+    let want_max = connections * 12;
+    println!("got {msgs}, want <= {want_max}");
+    assert!(msgs <= want_max, "got {msgs} messages, want at most {want_max}");
 }

chain/network/src/accounts_data/tests.rs
@@ -89,17 +89,17 @@ async fn happy_path() {
     assert_eq!(cache.dump(), vec![]); // empty after initial set_keys.
 
     // initial insert
-    let a0 = signers[0].make_account_data(rng, now);
-    let a1 = signers[1].make_account_data(rng, now);
+    let a0 = Arc::new(signers[0].make_account_data(rng, now));
+    let a1 = Arc::new(signers[1].make_account_data(rng, now));
     let res = cache.clone().insert(vec![a0.clone(), a1.clone()]).await;
     assert_eq!([&a0, &a1].as_set(), unwrap(&res).as_set());
     assert_eq!([&a0, &a1].as_set(), cache.dump().as_set());
 
     // entries of various types
-    let a0new = signers[0].make_account_data(rng, now + time::Duration::seconds(1));
-    let a1old = signers[1].make_account_data(rng, now - time::Duration::seconds(1));
-    let a2 = signers[2].make_account_data(rng, now);
-    let a5 = signers[5].make_account_data(rng, now);
+    let a0new = Arc::new(signers[0].make_account_data(rng, now + time::Duration::seconds(1)));
+    let a1old = Arc::new(signers[1].make_account_data(rng, now - time::Duration::seconds(1)));
+    let a2 = Arc::new(signers[2].make_account_data(rng, now));
+    let a5 = Arc::new(signers[5].make_account_data(rng, now));
     let res = cache
         .clone()
         .insert(vec![
@@ -143,11 +143,12 @@ async fn data_too_large() {
 
     let cache = Arc::new(Cache::new());
     cache.set_keys(e);
-    let a0 = signers[0].make_account_data(rng, now);
-    let a1 = signers[1].make_account_data(rng, now);
-    let mut a2_too_large = signers[2].make_account_data(rng, now);
+    let a0 = Arc::new(signers[0].make_account_data(rng, now));
+    let a1 = Arc::new(signers[1].make_account_data(rng, now));
+    let mut a2_too_large: SignedAccountData = signers[2].make_account_data(rng, now);
     *a2_too_large.payload_mut() =
         (0..crate::network_protocol::MAX_ACCOUNT_DATA_SIZE_BYTES + 1).map(|_| 17).collect();
+    let a2_too_large = Arc::new(a2_too_large);
 
     // too large payload => DataTooLarge
     let res = cache
@@ -177,10 +178,12 @@ async fn invalid_signature() {
 
     let cache = Arc::new(Cache::new());
     cache.set_keys(e);
-    let a0 = signers[0].make_account_data(rng, now);
+    let a0 = Arc::new(signers[0].make_account_data(rng, now));
     let mut a1 = signers[1].make_account_data(rng, now);
     let mut a2_invalid_sig = signers[2].make_account_data(rng, now);
     *a2_invalid_sig.signature_mut() = a1.signature_mut().clone();
+    let a1 = Arc::new(a1);
+    let a2_invalid_sig = Arc::new(a2_invalid_sig);
 
     // invalid signature => InvalidSignature
     let res = cache
@@ -210,10 +213,10 @@ async fn single_account_multiple_data() {
 
     let cache = Arc::new(Cache::new());
     cache.set_keys(e);
-    let a0 = signers[0].make_account_data(rng, now);
-    let a1 = signers[1].make_account_data(rng, now);
-    let a2old = signers[2].make_account_data(rng, now);
-    let a2new = signers[2].make_account_data(rng, now + time::Duration::seconds(1));
+    let a0 = Arc::new(signers[0].make_account_data(rng, now));
+    let a1 = Arc::new(signers[1].make_account_data(rng, now));
+    let a2old = Arc::new(signers[2].make_account_data(rng, now));
+    let a2new = Arc::new(signers[2].make_account_data(rng, now + time::Duration::seconds(1)));
 
     // 2 entries for the same (epoch_id,account_id) => SingleAccountMultipleData
     let res =

chain/network/src/broadcast/mod.rs
@@ -68,4 +68,13 @@ impl<T: Clone + Send> Receiver<T> {
             }
         }
     }
+
+    pub fn try_recv(&mut self) -> Option<T> {
+        let l = self.channel.stream.read().unwrap();
+        if l.len() <= self.next {
+            return None;
+        }
+        self.next += 1;
+        Some(l[self.next - 1].clone())
+    }
 }

chain/network/src/concurrency/mod.rs
@@ -1,0 +1,4 @@
+pub mod demux;
+
+#[cfg(test)]
+mod tests;

chain/network/src/concurrency/tests.rs
@@ -1,0 +1,21 @@
+use crate::concurrency::demux;
+
+#[tokio::test]
+async fn test_demux() {
+    let demux = demux::Demux::new(demux::RateLimit { qps: 50., burst: 1 });
+    for _ in 0..5 {
+        let mut handles = vec![];
+        for i in 0..1000 {
+            let demux = demux.clone();
+            handles.push(tokio::spawn(async move {
+                let j = demux
+                    .call(i, |is: Vec<u64>| async { is.into_iter().map(|i| i + 1).collect() })
+                    .await;
+                assert_eq!(i + 1, j);
+            }));
+        }
+        for h in handles {
+            h.await.unwrap();
+        }
+    }
+}

chain/network/src/network_protocol/mod.rs
@@ -31,6 +31,7 @@ use near_primitives::validator_signer::ValidatorSigner;
 use near_primitives::version::PEER_MIN_ALLOWED_PROTOCOL_VERSION;
 use protobuf::Message as _;
 use std::fmt;
+use std::sync::Arc;
 
 #[derive(PartialEq, Eq, Clone, Debug, Hash)]
 pub struct PeerAddr {
@@ -74,7 +75,7 @@ impl std::str::FromStr for PeerAddr {
     }
 }
 
-#[derive(PartialEq, Eq, Clone, Debug, Hash)]
+#[derive(PartialEq, Eq, Debug, Hash)]
 pub struct AccountData {
     pub peers: Vec<PeerAddr>,
     pub account_id: AccountId,
@@ -119,7 +120,7 @@ impl AccountData {
     }
 }
 
-#[derive(PartialEq, Eq, Clone, Debug, Hash)]
+#[derive(PartialEq, Eq, Debug, Hash)]
 pub struct AccountKeySignedPayload {
     payload: Vec<u8>,
     signature: near_crypto::Signature,
@@ -129,14 +130,21 @@ impl AccountKeySignedPayload {
     pub fn len(&self) -> usize {
         self.payload.len()
     }
-    pub fn verify(&self, key: &PublicKey) -> bool {
-        self.signature.verify(&self.payload, key)
+    pub fn signature(&self) -> &near_crypto::Signature {
+        &self.signature
+    }
+    pub fn verify(&self, key: &PublicKey) -> Result<(), ()> {
+        match self.signature.verify(&self.payload, key) {
+            true => Ok(()),
+            false => Err(()),
+        }
     }
 }
 
-// TODO(gprusak): if we expect this to be large, perhaps we should
-// pass around an Arc, rather than pass it by value.
-#[derive(PartialEq, Eq, Clone, Debug, Hash)]
+// TODO(gprusak): this is effectively immutable, and we always pass it around
+// in an Arc, so the Arc can be moved inside (except that constructing malformed
+// SignedAccountData for tests may get a little tricky).
+#[derive(PartialEq, Eq, Debug, Hash)]
 pub struct SignedAccountData {
     account_data: AccountData,
     // Serialized and signed AccountData.
@@ -225,7 +233,7 @@ pub enum HandshakeFailureReason {
 /// See SyncAccountsData in network_protocol/network.proto.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct SyncAccountsData {
-    pub accounts_data: Vec<SignedAccountData>,
+    pub accounts_data: Vec<Arc<SignedAccountData>>,
     pub requesting_full_sync: bool,
     pub incremental: bool,
 }

commit 40aed54d815bce92ed42db3f41bae64092b30bfe

deflaked network tests (#7350)
actix::stop causes all the futures from the actix runtime to be dropped. Until we replace actix with structured concurrency we cannot enforce driving futures to completion :/.

Fixed a race condition introduced in https://github.com/near/nearcore/pull/7182

I've executed the test suite locally 50 times to confirm that deflaking took effect.

chain/network/src/accounts_data/mod.rs
@@ -53,6 +53,10 @@ impl Drop for MustCompleteGuard {
 /// that they can be put together into a tokio::select block. All the higher level logic
 /// would greatly benefit (in terms of readability and bug-resistance) from being non-abortable.
 /// Rust doesn't support linear types as of now, so best we can do is a runtime check.
+/// TODO(gprusak): we would like to make the futures non-abortable, however with the current
+/// semantics of actix, which drops all the futures when stopped this is not feasible.
+/// Reconsider how to introduce must_complete to our codebase.
+#[allow(dead_code)]
 fn must_complete<Fut: Future>(fut: Fut) -> impl Future<Output = Fut::Output> {
     let guard = MustCompleteGuard;
     async move {
@@ -64,13 +68,16 @@ fn must_complete<Fut: Future>(fut: Fut) -> impl Future<Output = Fut::Output> {
 
 /// spawns a closure on a global rayon threadpool and awaits its completion.
 /// Returns the closure result.
+/// WARNING: panicking within a rayon task seems to be causing a double panic,
+/// and hence the panic message is not visible when running "cargo test".
 async fn rayon_spawn<T: 'static + Send>(f: impl 'static + Send + FnOnce() -> T) -> T {
-    must_complete(async move {
-        let (send, recv) = tokio::sync::oneshot::channel();
-        rayon::spawn(move || log_assert!(send.send(f()).is_ok(), "rayon_spawn has been aborted"));
-        recv.await.unwrap()
-    })
-    .await
+    let (send, recv) = tokio::sync::oneshot::channel();
+    rayon::spawn(move || {
+        if send.send(f()).is_err() {
+            tracing::warn!("rayon_spawn has been aborted");
+        }
+    });
+    recv.await.unwrap()
 }
 
 /// Applies f to the iterated elements and collects the results, until the first None is returned.

chain/network/src/peer_manager/testonly.rs
@@ -1,7 +1,7 @@
 use crate::broadcast;
 use crate::config;
 use crate::network_protocol::testonly as data;
-use crate::network_protocol::{PeerAddr, PeerMessage, SyncAccountsData};
+use crate::network_protocol::{PeerAddr, PeerMessage, SignedAccountData, SyncAccountsData};
 use crate::peer::peer_actor;
 use crate::peer_manager::peer_manager_actor;
 use crate::peer_manager::peer_manager_actor::Event as PME;
@@ -14,7 +14,7 @@ use near_network_primitives::types::{OutboundTcpConnect, PeerInfo};
 use near_primitives::network::PeerId;
 use near_primitives::types::{AccountId, EpochId};
 use near_store::test_utils::create_test_store;
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::sync::Arc;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -35,6 +35,23 @@ pub fn unwrap_sync_accounts_data_processed(ev: Event) -> Option<SyncAccountsData
             peer_actor::Event::MessageProcessed(PeerMessage::SyncAccountsData(msg)),
         )) => Some(msg),
         _ => None,
+    }
+}
+
+#[derive(PartialEq, Eq, Hash)]
+pub struct NormalAccountData {
+    pub epoch_id: EpochId,
+    pub account_id: AccountId,
+    pub peers: Vec<PeerAddr>,
+}
+
+impl From<&Arc<SignedAccountData>> for NormalAccountData {
+    fn from(d: &Arc<SignedAccountData>) -> Self {
+        Self {
+            epoch_id: d.epoch_id.clone(),
+            account_id: d.account_id.clone(),
+            peers: d.peers.clone(),
+        }
     }
 }
 
@@ -77,17 +94,10 @@ impl ActorHandler {
     }
 
     // Awaits until the accounts_data state matches `want`.
-    pub async fn wait_for_accounts_data(
-        &mut self,
-        want: &HashMap<(EpochId, AccountId), Vec<PeerAddr>>,
-    ) {
+    pub async fn wait_for_accounts_data(&mut self, want: &HashSet<NormalAccountData>) {
         loop {
             let info = self.actix.addr.send(GetNetworkInfo).await.unwrap();
-            let got: HashMap<_, _> = info
-                .tier1_accounts
-                .into_iter()
-                .map(|d| ((d.epoch_id.clone(), d.account_id.clone()), d.peers.clone()))
-                .collect();
+            let got: HashSet<_> = info.tier1_accounts.iter().map(|d| d.into()).collect();
             if &got == want {
                 break;
             }

chain/network/src/peer_manager/tests.rs
@@ -6,7 +6,7 @@ use crate::peer;
 use crate::peer::peer_actor;
 use crate::peer_manager;
 use crate::peer_manager::peer_manager_actor::Event as PME;
-use crate::peer_manager::testonly::Event;
+use crate::peer_manager::testonly::{Event, NormalAccountData};
 use crate::testonly::{make_rng, AsSet as _, Rng};
 use crate::types::{PeerMessage, RoutingTableUpdate};
 use itertools::Itertools;
@@ -188,7 +188,7 @@ async fn accounts_data_broadcast() {
     let clock = clock.clock();
     let clock = &clock;
 
-    let pm = peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await;
+    let mut pm = peer_manager::testonly::start(chain.make_config(rng), chain.clone()).await;
 
     let take_sync = |ev| match ev {
         peer::testonly::Event::Peer(peer_actor::Event::MessageProcessed(
@@ -211,6 +211,7 @@ async fn accounts_data_broadcast() {
     };
     let want = msg.accounts_data.clone();
     peer1.send(PeerMessage::SyncAccountsData(msg)).await;
+    pm.wait_for_accounts_data(&want.iter().map(|d| d.into()).collect()).await;
 
     // Connect another peer and perform initial full sync.
     let (mut peer2, got2) = add_peer(clock, rng, chain.clone(), &pm.cfg).await;
@@ -297,7 +298,11 @@ async fn accounts_data_gradual_epoch_change() {
         // Wait for data to arrive.
         let want = vs
             .iter()
-            .map(|v| ((e.clone(), v.signer.validator_id().clone()), peer_addrs(&v)))
+            .map(|v| NormalAccountData {
+                epoch_id: e.clone(),
+                account_id: v.signer.validator_id().clone(),
+                peers: peer_addrs(v),
+            })
             .collect();
         for pm in &mut pms {
             pm.wait_for_accounts_data(&want).await;
@@ -365,8 +370,14 @@ async fn accounts_data_rate_limiting() {
     let events: Vec<_> = pms.iter().map(|pm| pm.events.clone()).collect();
 
     // Wait for data to arrive.
-    let want =
-        vs.iter().map(|v| ((e.clone(), v.signer.validator_id().clone()), peer_addrs(&v))).collect();
+    let want = vs
+        .iter()
+        .map(|v| NormalAccountData {
+            epoch_id: e.clone(),
+            account_id: v.signer.validator_id().clone(),
+            peers: peer_addrs(&v),
+        })
+        .collect();
     for pm in &mut pms {
         pm.wait_for_accounts_data(&want).await;
     }

chain/network/src/testonly/actix.rs
@@ -12,7 +12,12 @@ impl Thread {
 
 impl Drop for Thread {
     fn drop(&mut self) {
-        self.0.take().unwrap().join().unwrap().unwrap();
+        let res = self.0.take().unwrap().join();
+        // Panic, unless we are in test and are already panicking.
+        // A double panic prevents "cargo test" from displaying error message.
+        if !std::thread::panicking() {
+            res.unwrap().unwrap();
+        }
     }
 }
 