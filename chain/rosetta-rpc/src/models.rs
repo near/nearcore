add1390034763834648e7f3180c90f6b58f55ecf
firatNEAR <102993450+firatNEAR@users.noreply.github.com>
2022-08-05 13:26:03 +0200

    Fix/rosetta rpc predecessor (#7348)
    * Add predecessor_id to Rosetta /block endpoint

--- chain/rosetta-rpc/src/models.rs
+++ chain/rosetta-rpc/src/models.rs
@@ -770,6 +770,9 @@ pub(crate) struct OperationMetadata {
     /// Has to be specified for FUNCTION_CALL operation
     #[serde(skip_serializing_if = "Option::is_none")]
     pub attached_gas: Option<crate::utils::SignedDiff<near_primitives::types::Gas>>,
+    /// Has to be specified for TRANSFER operation
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub predecessor_id: Option<AccountIdentifier>,
 }
 
 /// Operations contain all balance-changing information within a transaction.
