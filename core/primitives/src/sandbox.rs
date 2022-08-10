f7c40fa265112e83a5c4667449fea1a32339041f
Michal Nazarewicz <mina86@mina86.com>
2022-08-10 15:15:23 +0200

    const

--- core/primitives/src/sandbox.rs
+++ core/primitives/src/sandbox.rs
@@ -3,10 +3,12 @@ pub mod state_patch {
     use crate::state_record::StateRecord;
 
     /// Changes to the state to be applied via sandbox-only state patching
-    /// functionality.
+    /// feature.
     ///
     /// As we only expose this functionality for sandbox, we make sure that the
-    /// object is only constructable if sandbox feature is enabled.
+    /// object can be non-empty only if `sandbox` feature is enabled.  On
+    /// non-sandbox build, this struct is ZST and its methods are essentially
+    /// short-circuited by treating the type as always empty.
     #[derive(Default)]
     pub struct SandboxStatePatch {
         records: Vec<StateRecord>,
@@ -52,20 +54,25 @@ pub mod state_patch {
     pub struct SandboxStatePatch;
 
     impl SandboxStatePatch {
-        pub const fn is_empty(&self) -> bool {
+        #[inline(always)]
+        pub fn is_empty(&self) -> bool {
             true
         }
-        pub const fn clear(&self) {}
+        #[inline(always)]
+        pub fn clear(&self) {}
+        #[inline(always)]
         pub fn take(&mut self) -> Self {
             Self
         }
-        pub const fn merge(&self, _other: SandboxStatePatch) {}
+        #[inline(always)]
+        pub fn merge(&self, _other: SandboxStatePatch) {}
     }
 
     impl IntoIterator for SandboxStatePatch {
         type Item = StateRecord;
         type IntoIter = std::iter::Empty<StateRecord>;
 
+        #[inline(always)]
         fn into_iter(self) -> Self::IntoIter {
             std::iter::empty()
         }
