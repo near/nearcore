8080aa39e39fa2c52fadbb766585ccec597b7a99
firatNEAR <firat@near.org>
2022-08-18 14:40:29 +0200

    Fix estimator

--- runtime/runtime-params-estimator/src/estimator_context.rs
+++ runtime/runtime-params-estimator/src/estimator_context.rs
@@ -124,7 +124,7 @@ impl<'c> Testbed<'c> {
     pub(crate) fn trie_caching_storage(&mut self) -> TrieCachingStorage {
         let store = self.inner.store();
         let caching_storage =
-            TrieCachingStorage::new(store, TrieCache::new(), ShardUId::single_shard());
+            TrieCachingStorage::new(store, TrieCache::new(), ShardUId::single_shard(), false);
         caching_storage
     }
 
