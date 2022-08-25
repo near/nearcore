028fa7c9fbf7705277d8dc2ee6b95697503cf026
Jakob Meier <mail@jakobmeier.ch>
2022-08-25 15:45:38 +0200

    fix: default genesis minimum gas price (#7475)
    Update the default value for `min_gas_price` when creating a new chain
    to the effective value we use in mainnet today. (10e9 ->10e8)
    
    This changes the gas price for new chains, localnet and sandbox.
    
    Also add some comments to `fn min_gas_price` to improve clarity.
    
    Resolves #7466

--- nearcore/src/config.rs
+++ nearcore/src/config.rs
@@ -107,8 +107,8 @@ pub const NUM_BLOCKS_PER_YEAR: u64 = 365 * 24 * 60 * 60;
 /// Initial gas limit.
 pub const INITIAL_GAS_LIMIT: Gas = 1_000_000_000_000_000;
 
-/// Initial gas price.
-pub const MIN_GAS_PRICE: Balance = 1_000_000_000;
+/// Initial and minimum gas price.
+pub const MIN_GAS_PRICE: Balance = 100_000_000;
 
 /// Protocol treasury account
 pub const PROTOCOL_TREASURY_ACCOUNT: &str = "near";
