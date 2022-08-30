490c64e36f8352590398b22dc56d9120187323df
Jakob Meier <mail@jakobmeier.ch>
2022-08-30 14:06:29 +0200

    feat(estimator): compact RocksDB in testbed setup (#7502)
    Run compaction on the DB after inserting all records from the state
    dump. This brings the layout of SST files to a more stable and optimal
    layout. See #4771 for more details.
    
    This is an estimator only change.

--- runtime/runtime-params-estimator/src/lib.rs
+++ runtime/runtime-params-estimator/src/lib.rs
@@ -38,6 +38,17 @@
 //! costs and don't want to just run everything in order (as that would be to
 //! slow), we have a very simple manual caching infrastructure in place.
 //!
+//! To run estimations on a non-empty DB with standardised content, we first
+//! dump all records to a `StateDump` written to a file. Then for each
+//! iteration of a an estimation, we first load the records from this dump into
+//! a fresh database. Afterwards, it is crucial to run compaction on RocksDB
+//! before starting measurements. Otherwise, the SST file layout can be very
+//! inefficient, as there was no time to restructure them. We assume that in
+//! production, the inflow of new data is not as bulky and therefore it should
+//! always be reasonably compacted. Also, without forcing it before
+//! measurements start, compaction may start during the measurement and makes
+//! the results unstable.
+//!
 //! Notes on code architecture:
 //!
 //! To keep estimations comprehensible, each estimation has a simple function
