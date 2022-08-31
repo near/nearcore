2fa43623c005100444fcc5d3cb5e74193b8d060b
mzhangmzz <34969888+mzhangmzz@users.noreply.github.com>
2022-08-31 11:33:13 -0400

    Improve the logic of handling old protocol versions (#7501)
    This PR introduces two changes:
    1. Complete the logic that make nodes panic if their protocol version is old. Previously, the check was only triggered at epoch boundaries. If validators restart their nodes, their nodes will keep running. 
    2. Introduce a new protocol feature that forbids blocks produced with old protocol versions. This can help kick out validators that are not upgraded.

--- chain/chain-primitives/src/error.rs
+++ chain/chain-primitives/src/error.rs
@@ -147,6 +147,9 @@ pub enum Error {
     /// `next_bps_hash` doens't correspond to the actual next block producers set
     #[error("Invalid Next BP Hash")]
     InvalidNextBPHash,
+    /// The block has a protocol version that's outdated
+    #[error("Invalid protocol version")]
+    InvalidProtocolVersion,
     /// The block doesn't have approvals from 50% of the block producers
     #[error("Not enough approvals")]
     NotEnoughApprovals,
@@ -282,6 +285,7 @@ impl Error {
             | Error::InvalidStateRequest(_)
             | Error::InvalidRandomnessBeaconOutput
             | Error::InvalidBlockMerkleRoot
+            | Error::InvalidProtocolVersion
             | Error::NotAValidator
             | Error::InvalidChallengeRoot => true,
         }
