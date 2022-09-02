ecb17b0a23271541a47a366cf1901157c04d7625
Min Zhang <min@near.org>
2022-09-02 12:33:28 -0400

    address comments

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -264,16 +264,14 @@ impl ProtocolFeature {
             ProtocolFeature::FixContractLoadingCost => 129,
             #[cfg(feature = "protocol_feature_account_id_in_function_call_permission")]
             ProtocolFeature::AccountIdInFunctionCallPermission => 130,
-            #[cfg(all(
-                feature = "protocol_feature_reject_blocks_with_outdated_protocol_version",
-                not(feature = "shardnet")
-            ))]
-            ProtocolFeature::RejectBlocksWithOutdatedProtocolVersions => 132,
-            #[cfg(all(
-                feature = "protocol_feature_reject_blocks_with_outdated_protocol_version",
-                feature = "shardnet"
-            ))]
-            ProtocolFeature::RejectBlocksWithOutdatedProtocolVersions => 102,
+            #[cfg(feature = "protocol_feature_reject_blocks_with_outdated_protocol_version")]
+            ProtocolFeature::RejectBlocksWithOutdatedProtocolVersions => {
+                if cfg!(feature = "shardnet") {
+                    102
+                } else {
+                    132
+                }
+            }
             #[cfg(feature = "shardnet")]
             ProtocolFeature::ShardnetShardLayoutUpgrade => 102,
         }
