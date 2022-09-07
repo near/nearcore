41c3377f667ba8501de699d124bdf5e0c29a19d2
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-09-07 20:59:02 +0200

    Exporter and SERVICE_NAME

--- core/o11y/src/lib.rs
+++ core/o11y/src/lib.rs
@@ -7,6 +7,7 @@ use once_cell::sync::OnceCell;
 use opentelemetry::sdk::trace::{self, IdGenerator, Sampler, Tracer};
 use opentelemetry::sdk::Resource;
 use opentelemetry::KeyValue;
+use opentelemetry_semantic_conventions::resource::SERVICE_NAME;
 use std::borrow::Cow;
 use std::path::PathBuf;
 use tracing::level_filters::LevelFilter;
@@ -196,11 +197,12 @@ where
 {
     let tracer = opentelemetry_otlp::new_pipeline()
         .tracing()
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
         .with_trace_config(
             trace::config()
                 .with_sampler(Sampler::AlwaysOn)
                 .with_id_generator(IdGenerator::default())
-                .with_resource(Resource::new(vec![KeyValue::new("service", "neard")])),
+                .with_resource(Resource::new(vec![KeyValue::new(SERVICE_NAME, "neard")])),
         )
         .install_batch(opentelemetry::runtime::Tokio)
         .unwrap();
