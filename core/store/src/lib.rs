c1db25903653e9b9dd2b0cef02abc580c112701e
Michal Nazarewicz <mina86@mina86.com>
2022-09-08 19:42:28 +0200

    fmt

--- core/store/src/lib.rs
+++ core/store/src/lib.rs
@@ -31,9 +31,9 @@ use crate::db::{
 pub use crate::trie::iterator::TrieIterator;
 pub use crate::trie::update::{TrieUpdate, TrieUpdateIterator, TrieUpdateValuePtr};
 pub use crate::trie::{
-    estimator, split_state, ApplyStatePartResult, KeyForStateChanges, PartialStorage,
-    ShardTries, Trie, TrieAccess, TrieCache, TrieCachingStorage,
-    TrieChanges, TrieConfig, TrieNodeWithSize, TrieStorage, WrappedTrieChanges,
+    estimator, split_state, ApplyStatePartResult, KeyForStateChanges, PartialStorage, ShardTries,
+    Trie, TrieAccess, TrieCache, TrieCachingStorage, TrieChanges, TrieConfig, TrieNodeWithSize,
+    TrieStorage, WrappedTrieChanges,
 };
 
 mod columns;
