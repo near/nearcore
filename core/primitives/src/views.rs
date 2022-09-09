2e42d9829e26ccf8f3d1dc5c1ca2b9b2b86c4d31
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-09-09 17:03:20 +0200

    store: add proof to ViewState response (#7593)
    Introduce visited_nodes to TrieIterator which records raw bytes of all
    the nodes the iterator visits.  Those are then returned as proof in
    response to ViewState request.  Since the exact nodes the iterator
    visited are included in the proof, it is therefore possible to verify
    all the values returned in the state.
    
    At the moment the proof is always included.
    
    Fixes: https://github.com/near/nearcore/pull/2076

--- core/primitives/src/views.rs
+++ core/primitives/src/views.rs
@@ -194,9 +194,6 @@ impl From<AccessKeyView> for AccessKey {
     }
 }
 
-/// Set of serialized TrieNodes that are encoded in base64. Represent proof of inclusion of some TrieNode in the MerkleTrie.
-pub type TrieProofPath = Vec<String>;
-
 /// Item of the state, key and value are serialized in base64 and proof for inclusion of given state item.
 #[cfg_attr(feature = "deepsize_feature", derive(deepsize::DeepSizeOf))]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
@@ -205,14 +202,18 @@ pub struct StateItem {
     pub key: Vec<u8>,
     #[serde(with = "base64_format")]
     pub value: Vec<u8>,
-    pub proof: TrieProofPath,
+    /// Deprecated, always empty, eventually will be deleted.
+    // TODO(mina86): This was deprecated in 1.30.  Get rid of the field
+    // altogether at 1.33 or something.
+    #[serde(default)]
+    pub proof: Vec<()>,
 }
 
 #[cfg_attr(feature = "deepsize_feature", derive(deepsize::DeepSizeOf))]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct ViewStateResult {
     pub values: Vec<StateItem>,
-    pub proof: TrieProofPath,
+    pub proof: Vec<Arc<[u8]>>,
 }
 
 #[cfg_attr(feature = "deepsize_feature", derive(deepsize::DeepSizeOf))]
