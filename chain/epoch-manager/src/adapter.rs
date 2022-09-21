f36550ff44e0f572ee58b982a856b6c191340676
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-09-21 13:39:11 +0100

    refactor: add EpochManagerAdapter infra  (#7652)
    Part of https://github.com/near/nearcore/issues/6910
    
    The grand plan is to separate EpochManager out of NightshadeRuntime. As
    a first step, I want to separate the *types* of runtime and epoch
    manager, while keeping them in the same object. I want to do this via
    the following series of steps:
    
    ```
    trait RuntimeAdapter {}
    ```
    
    ```
    trait EpochManagerAdapter {}
    
    trait RuntimeAdapter: EpochManagerAdapter {}
    ```
    
    ```
    trait EpochManagerAdapter {}
    
    trait RuntimeAdapter {}
    ```
    
    This PR introduces the EpochManagerAdapter infra.
    
    You can see the rough idea of the plan in https://github.com/near/nearcore/pull/7606

---
+++ chain/epoch-manager/src/adapter.rs
@@ -0,0 +1,43 @@
+use near_primitives::types::EpochId;
+
+use crate::{EpochManager, EpochManagerHandle};
+use std::sync::{RwLockReadGuard, RwLockWriteGuard};
+
+/// A trait that abstracts the interface of the EpochManager.
+///
+/// It is intended to be an intermediate state in a refactor: we want to remove
+/// epoch manager stuff from RuntimeAdapter's interface, and, as a first step,
+/// we move it to a new trait. The end goal is for the code to use the concrete
+/// epoch manager type directly. Though, we might want to still keep this trait
+/// in, to allow for easy overriding of epoch manager in tests.
+pub trait EpochManagerAdapter: Send + Sync {
+    /// Check if epoch exists.
+    fn epoch_exists(&self, epoch_id: &EpochId) -> bool;
+}
+
+/// A technical plumbing trait to conveniently implement [`EpochManagerAdapter`]
+/// for `NightshadeRuntime` without too much copy-paste.
+///
+/// Once we remove `RuntimeAdapter: EpochManagerAdapter` bound, we could get rid
+/// of this trait and instead add inherent methods directly to
+/// `EpochManagerHandle`.
+pub trait HasEpochMangerHandle {
+    fn write(&self) -> RwLockWriteGuard<EpochManager>;
+    fn read(&self) -> RwLockReadGuard<EpochManager>;
+}
+
+impl HasEpochMangerHandle for EpochManagerHandle {
+    fn write(&self) -> RwLockWriteGuard<EpochManager> {
+        self.write()
+    }
+    fn read(&self) -> RwLockReadGuard<EpochManager> {
+        self.read()
+    }
+}
+
+impl<T: HasEpochMangerHandle + Send + Sync> EpochManagerAdapter for T {
+    fn epoch_exists(&self, epoch_id: &EpochId) -> bool {
+        let epoch_manager = self.read();
+        epoch_manager.get_epoch_info(epoch_id).is_ok()
+    }
+}
