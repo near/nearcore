05d5b7bb8d6eb7e31cb6e9e55d0194c85c8f5bdc
robin-near <111538878+robin-near@users.noreply.github.com>
2022-09-28 15:16:10 -0700

    Use the underlying reproducible random impls instead of StdRng. (#7717)
    * Use the underlying reproducible random impls.
    
    * Improve testing
    
    * fix deny
    
    * fmt
    
    Co-authored-by: Michal Nazarewicz <mina86@mina86.com>

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -9,9 +9,9 @@ use itertools::Itertools;
 use near_o11y::log_assert;
 use near_primitives::sandbox::state_patch::SandboxStatePatch;
 use near_primitives::time::Clock;
-use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
 use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 use tracing::{debug, error, info, warn, Span};
 
 use near_chain_primitives::error::{BlockKnownError, Error, LogTransientStorageError};
@@ -1536,13 +1536,20 @@ impl Chain {
         }
         // sort the receipts deterministically so the order that they will be processed is deterministic
         for (_, receipt_proofs) in receipt_proofs_by_shard_id.iter_mut() {
-            let mut slice = [0u8; 32];
-            slice.copy_from_slice(block.hash().as_ref());
-            let mut rng: StdRng = SeedableRng::from_seed(slice);
-            receipt_proofs.shuffle(&mut rng);
+            Self::shuffle_receipt_proofs(receipt_proofs, block.hash());
         }
 
         Ok(receipt_proofs_by_shard_id)
+    }
+
+    fn shuffle_receipt_proofs<ReceiptProofType>(
+        receipt_proofs: &mut Vec<ReceiptProofType>,
+        block_hash: &CryptoHash,
+    ) {
+        let mut slice = [0u8; 32];
+        slice.copy_from_slice(block_hash.as_ref());
+        let mut rng: ChaCha20Rng = SeedableRng::from_seed(slice);
+        receipt_proofs.shuffle(&mut rng);
     }
 
     #[cfg(test)]
@@ -5356,5 +5363,21 @@ impl Chain {
                 hash: *block_hash,
             })
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use near_primitives::hash::CryptoHash;
+
+    #[test]
+    pub fn receipt_randomness_reproducibility() {
+        // Sanity check that the receipt shuffling implementation does not change.
+        let mut receipt_proofs = vec![0, 1, 2, 3, 4, 5, 6];
+        crate::Chain::shuffle_receipt_proofs(
+            &mut receipt_proofs,
+            &CryptoHash::hash_bytes(&[1, 2, 3, 4, 5]),
+        );
+        assert_eq!(receipt_proofs, vec![2, 3, 1, 4, 0, 5, 6],);
     }
 }
