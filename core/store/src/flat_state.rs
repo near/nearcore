64742c7f7a4cbab85d722408eec17317f3fcbd1d
Longarithm <the.aleksandr.logunov@gmail.com>
2022-09-30 22:07:33 +0400

    minor fix

--- core/store/src/flat_state.rs
+++ core/store/src/flat_state.rs
@@ -653,16 +653,16 @@ impl FlatStorageState {
         Ok(vec![])
     }
 
-    // Requests updating flat head.
-    // If new head is not a child of current flat head, does nothing and returns Ok. It can happen in case of two forks:
-    //
-    //      (flat head)        /-------> 6
-    // 1 ->      2     -> 3 -> 4
-    //                         \---> 5
-    //
-    // where for (5) we call `update_flat_head(3)` and then for (6) we call `update_flat_head(2)`.
-    // Otherwise updates the head of the flat storage, including updating the flat state in memory and on disk
-    // and updating the flat state to reflect the state at the new head.
+    /// Requests updating flat head.
+    /// If new head is not a child of current flat head, does nothing and returns Ok. It can happen when we have a fork:
+    ///
+    ///      (flat head)        /-------> 6
+    /// 1 ->      2     -> 3 -> 4
+    ///                         \---> 5
+    ///
+    /// where for (5) we can call `update_flat_head(3)` and then for (6) we can call `update_flat_head(2)`.
+    /// Otherwise updates the head of the flat storage, including updating the flat state in memory and on disk
+    /// and updating the flat state to reflect the state at the new head.
     // TODO (#7327): implement garbage collection of old deltas.
     #[cfg(feature = "protocol_feature_flat_state")]
     pub fn update_flat_head(&self, new_head: &CryptoHash) -> Result<(), FlatStorageError> {
@@ -679,7 +679,7 @@ impl FlatStorageState {
                 return Ok(());
             }
         };
-        // If new head height is not greater than current head height, return Ok.
+        // If new head height is not greater than current head height, it is not a child of flat head, so we return Ok.
         if new_head_height <= current_head_height {
             return Ok(());
         }
