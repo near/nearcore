04035c79cbafa14da66498eb0edce4757b602023
robin-near <111538878+robin-near@users.noreply.github.com>
2022-10-12 13:01:18 -0700

    store: flatten TransactionResult column to remove need for RMW (#7799)
    * [TransactionResult] Flatten TransactionResult column to remove need for RMW
    
    * Fix issues.
    
    * Address comments
    
    * Migrations minor change.
    
    * Key ChainStoreCacheUpdate::outcomes with (id, blockhash) as well.
    
    * Minor update.
    
    * Use insert_ser

--- tools/state-viewer/src/apply_chain_range.rs
+++ tools/state-viewer/src/apply_chain_range.rs
@@ -13,9 +13,7 @@ use near_chain_configs::Genesis;
 use near_primitives::borsh::maybestd::sync::Arc;
 use near_primitives::hash::CryptoHash;
 use near_primitives::receipt::DelayedReceiptIndices;
-use near_primitives::transaction::{
-    Action, ExecutionOutcomeWithId, ExecutionOutcomeWithIdAndProof,
-};
+use near_primitives::transaction::{Action, ExecutionOutcomeWithId, ExecutionOutcomeWithProof};
 use near_primitives::trie_key::TrieKey;
 use near_primitives::types::chunk_extra::ChunkExtra;
 use near_primitives::types::{BlockHeight, ShardId};
@@ -90,15 +88,17 @@ fn old_outcomes(
     new_outcomes
         .iter()
         .map(|outcome| {
-            store
-                .get_ser::<Vec<ExecutionOutcomeWithIdAndProof>>(
-                    DBCol::TransactionResult,
+            let old_outcome = store
+                .iter_prefix_ser::<ExecutionOutcomeWithProof>(
+                    DBCol::TransactionResultForBlock,
                     outcome.id.as_ref(),
                 )
+                .next()
                 .unwrap()
-                .unwrap()[0]
-                .outcome_with_id
-                .clone()
+                .unwrap()
+                .1
+                .outcome;
+            ExecutionOutcomeWithId { id: outcome.id, outcome: old_outcome }
         })
         .collect()
 }
