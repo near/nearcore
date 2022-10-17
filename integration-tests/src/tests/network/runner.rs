e2a0e03fae24e0d0709d1fcde05ea581d9823c73
Grzegorz Prusak <pompon.pompon@gmail.com>
2022-10-17 13:38:24 +0200

    all tests pass

--- integration-tests/src/tests/network/runner.rs
+++ integration-tests/src/tests/network/runner.rs
@@ -99,8 +99,10 @@ fn setup_network_node(
         time::Clock::real(),
         db.clone(),
         config,
-        client_actor.recipient(),
-        view_client_actor.recipient(),
+        near_network::client::Client {
+            client_addr: client_actor.recipient(),
+            view_client_addr: view_client_actor.recipient(),
+        },
         genesis_id,
     )
     .unwrap();
