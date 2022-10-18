7ea4ce87b369ecf5dd6b918a9b5741fe390e36d0
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-10-18 15:53:15 +0200

    macro tune

--- core/o11y/src/macros.rs
+++ core/o11y/src/macros.rs
@@ -1,23 +1,11 @@
-#[macro_export]
-macro_rules! handler_span_use_context {
-    ($span:expr, $msg:expr) => {{}};
-}
-
 /// Use this at the beginning of `handle` methods to reduce the boilerplate.
 #[macro_export]
 macro_rules! handler_span {
     ($target:expr, $msg:expr $(, $extra_fields:tt)*) => {{
         let WithSpanContext { msg, context, .. } = $msg;
 
-        fn last_component_of_name(name: &str) -> &str {
-            name.rsplit_once("::").map_or(name, |(_, name)| name)
-        }
-        fn type_name_of<T>(_: &T) -> &str {
-            last_component_of_name(std::any::type_name::<T>())
-        }
-
-        let actor = last_component_of_name(std::any::type_name::<Self>());
-        let handler = type_name_of(&msg);
+        let actor = near_o11y::macros::last_component_of_name(std::any::type_name::<Self>());
+        let handler = near_o11y::macros::type_name_of(&msg);
 
         let span = tracing::debug_span!(
             target: $target,
@@ -30,4 +18,17 @@ macro_rules! handler_span {
         span.set_parent(context);
         (span, msg)
     }};
+}
+
+/// For internal use by `handler_span!`.
+/// Given 'abc::bcd::cde' returns 'cde'.
+/// Given 'abc' returns 'abc'.
+pub fn last_component_of_name(name: &str) -> &str {
+    name.rsplit_once("::").map_or(name, |(_, name)| name)
+}
+
+/// For internal use by `handler_span!`.
+/// Returns the last component of the name of type `T`.
+pub fn type_name_of<T>(_: &T) -> &str {
+    last_component_of_name(std::any::type_name::<T>())
 }
