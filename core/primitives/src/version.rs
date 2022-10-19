3f42764fe9596be29749f867d343cfa13fcffb35
Michal Nazarewicz <mina86@mina86.com>
2022-10-19 16:51:44 +0200

    core: represent no voting schedule as timestamp in the past
    Rather than using `None` to represent lack of protocol upgrade voting
    schedule, simply use a timestamp far in the past.  Specifically, the
    default value of a DateTime which corresponds to start of Unix epoch.
    This simplifies the code by making it unnecessary to handle `None`
    case specially.

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -176,15 +176,14 @@ pub const PROTOCOL_VERSION: ProtocolVersion = if cfg!(feature = "nightly_protoco
 
 /// The points in time after which the voting for the latest protocol version
 /// should start.
-pub static PROTOCOL_UPGRADE_SCHEDULE: Lazy<Option<ProtocolUpgradeVotingSchedule>> =
-    Lazy::new(|| {
-        if cfg!(feature = "shardnet") {
-            Some(ProtocolUpgradeVotingSchedule::from_str("2022-09-05 15:00:00").unwrap())
-        } else {
-            // Update to latest protocol version on release.
-            None
-        }
-    });
+pub const PROTOCOL_UPGRADE_SCHEDULE: Lazy<ProtocolUpgradeVotingSchedule> = Lazy::new(|| {
+    if cfg!(feature = "shardnet") {
+        ProtocolUpgradeVotingSchedule::from_str("2022-09-05 15:00:00").unwrap()
+    } else {
+        // Update to latest protocol version on release.
+        ProtocolUpgradeVotingSchedule::default()
+    }
+});
 
 /// Gives new clients an option to upgrade without announcing that they support
 /// the new version.  This gives non-validator nodes time to upgrade.  See
@@ -193,7 +192,7 @@ pub fn get_protocol_version(next_epoch_protocol_version: ProtocolVersion) -> Pro
     get_protocol_version_internal(
         next_epoch_protocol_version,
         PROTOCOL_VERSION,
-        &PROTOCOL_UPGRADE_SCHEDULE,
+        *PROTOCOL_UPGRADE_SCHEDULE,
     )
 }
 
