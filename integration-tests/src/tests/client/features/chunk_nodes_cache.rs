34f847e9c35677b966cf2ac8995585e7174b6b23
mzhangmzz <34969888+mzhangmzz@users.noreply.github.com>
2022-10-20 18:34:11 -0400

    Do not use flat storage for storage_write (#7885)
    As an intermediate step, we will only enable flat storage for storate_read, but not storage_write.

--- integration-tests/src/tests/client/features/chunk_nodes_cache.rs
+++ integration-tests/src/tests/client/features/chunk_nodes_cache.rs
@@ -149,19 +149,8 @@ fn compare_node_counts() {
         })
         .collect();
 
-    if cfg!(feature = "protocol_feature_flat_state") {
-        // If flat storage is enabled, we shouldn't observe any trie node reads during transaction processing.
-        // For the first pair of write calls there are no contract values in storage, thus we see zero db reads.
-        assert_eq!(tx_node_counts[0], TrieNodesCount { db_reads: 0, mem_reads: 0 });
-        // For all other write calls we read the value reference from flat storage and the value from state.
-        // The first read doesn't count, so we should observe only two DB reads each time.
-        (1..4).for_each(|i| {
-            assert_eq!(tx_node_counts[i], TrieNodesCount { db_reads: 2, mem_reads: 0 })
-        });
-    } else {
-        assert_eq!(tx_node_counts[0], TrieNodesCount { db_reads: 4, mem_reads: 0 });
-        assert_eq!(tx_node_counts[1], TrieNodesCount { db_reads: 12, mem_reads: 0 });
-        assert_eq!(tx_node_counts[2], TrieNodesCount { db_reads: 8, mem_reads: 4 });
-        assert_eq!(tx_node_counts[3], TrieNodesCount { db_reads: 8, mem_reads: 4 });
-    }
+    assert_eq!(tx_node_counts[0], TrieNodesCount { db_reads: 4, mem_reads: 0 });
+    assert_eq!(tx_node_counts[1], TrieNodesCount { db_reads: 12, mem_reads: 0 });
+    assert_eq!(tx_node_counts[2], TrieNodesCount { db_reads: 8, mem_reads: 4 });
+    assert_eq!(tx_node_counts[3], TrieNodesCount { db_reads: 8, mem_reads: 4 });
 }
