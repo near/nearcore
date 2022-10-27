017485abb0a64bcb60a0ff1a794b3c494cbf499d
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-10-27 14:24:38 +0200

    Download records.json when initializing configs

--- neard/src/cli.rs
+++ neard/src/cli.rs
@@ -211,6 +211,9 @@ pub(super) struct InitCmd {
     /// Specify a custom download URL for the genesis file.
     #[clap(long)]
     download_genesis_url: Option<String>,
+    /// Specify a custom download URL for the records file.
+    #[clap(long)]
+    download_records_url: Option<String>,
     /// Specify a custom download URL for the config file.
     #[clap(long)]
     download_config_url: Option<String>,
@@ -289,6 +292,7 @@ impl InitCmd {
             self.genesis.as_deref(),
             self.download_genesis,
             self.download_genesis_url.as_deref(),
+            self.download_records_url.as_deref(),
             self.download_config,
             self.download_config_url.as_deref(),
             self.boot_nodes.as_deref(),
