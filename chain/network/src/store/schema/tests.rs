41ad9349b7db799796c2e1fec1b939faaccc5172
pompon0 <pompon.pompon@gmail.com>
2022-11-04 13:36:24 +0100

    moved fixing local edges to a background task. (#7977)
    Instead of triggering a fixing call when the unexpected edge arrives, the consistency between the graph and actual connection set will be checked periodically. This way it is straightforward to see that we will always eventually arrive at a consistent state.

--- chain/network/src/store/schema/tests.rs
+++ chain/network/src/store/schema/tests.rs
@@ -6,8 +6,8 @@ use crate::testonly::make_rng;
 fn borsh_wrapper_is_transparent() {
     let mut rng = make_rng(423423);
     let rng = &mut rng;
-    let s1 = data::make_signer(rng);
-    let s2 = data::make_signer(rng);
+    let s1 = data::make_secret_key(rng);
+    let s2 = data::make_secret_key(rng);
     let e = data::make_edge(&s1, &s2);
     assert_eq!(Borsh(e.clone()).try_to_vec().unwrap(), e.try_to_vec().unwrap());
 }
