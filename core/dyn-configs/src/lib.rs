0924754a9c2ec9fe4f387d39387d50b798296c68
Antonio Yang <yanganto@gmail.com>
2022-11-14 17:18:08 +0800

    Add metric for dynamic config change

--- core/dyn-configs/src/lib.rs
+++ core/dyn-configs/src/lib.rs
@@ -1,6 +1,17 @@
 #![doc = include_str!("../README.md")]
 
+use near_o11y::metrics::{try_create_int_counter, IntCounter};
+use once_cell::sync::Lazy;
 use std::sync::atomic::{AtomicU64, Ordering};
+
+/// An indicate for dynamic config changes
+pub static DYN_CONFIG_CHANGE: Lazy<IntCounter> = Lazy::new(|| {
+    try_create_int_counter(
+        "near_dynamic_config_changes",
+        "Total number of changes on dynamic configure",
+    )
+    .unwrap()
+});
 
 // NOTE: AtomicU64 is the same unit as BlockHeight, and use to store the expected blockheight to
 // shutdown
@@ -12,4 +23,5 @@ pub fn reload(expected_shutdown: Option<u64>) {
     } else {
         EXPECTED_SHUTDOWN_AT.store(0, Ordering::Relaxed);
     }
+    DYN_CONFIG_CHANGE.inc();
 }
