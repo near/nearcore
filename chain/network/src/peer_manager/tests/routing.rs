97e4dec91170f7772ab987d57e7c0b489cba3403
Grzegorz Prusak <pompon.pompon@gmail.com>
2022-11-16 21:11:06 +0100

    wtf

--- chain/network/src/peer_manager/tests/routing.rs
+++ chain/network/src/peer_manager/tests/routing.rs
@@ -158,11 +158,13 @@ async fn wait_for_edges(
     want: &HashSet<Edge>,
 ) {
     let mut got = HashSet::new();
+    tracing::info!(target: "test", "want edges: {:?}",want.iter().map(|e|e.hash()).collect::<Vec<_>>());
     while &got != want {
         match events.recv().await {
             peer::testonly::Event::Network(PME::MessageProcessed(
                 PeerMessage::SyncRoutingTable(msg),
             )) => {
+                tracing::info!(target: "test", "got edges: {:?}",msg.edges.iter().map(|e|e.hash()).collect::<Vec<_>>());
                 got.extend(msg.edges);
                 assert!(want.is_superset(&got), "want: {:#?}, got: {:#?}", want, got);
             }
