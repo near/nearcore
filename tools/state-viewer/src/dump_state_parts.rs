71fcf13a38fd258796bc29427620a4aa70f60faa
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-11-22 13:15:06 +0100

    Easier epoch selection for dumping state parts

--- tools/state-viewer/src/dump_state_parts.rs
+++ tools/state-viewer/src/dump_state_parts.rs
@@ -1,15 +1,97 @@
+use crate::epoch_info::iterate_and_filter;
+use clap::Subcommand;
 use near_chain::{ChainStore, ChainStoreAccess, RuntimeAdapter};
+use near_epoch_manager::EpochManager;
+use near_primitives::epoch_manager::epoch_info::EpochInfo;
 use near_primitives::state_part::PartId;
 use near_primitives::syncing::get_num_state_parts;
+use near_primitives::types::EpochId;
 use near_primitives_core::hash::CryptoHash;
-use near_primitives_core::types::ShardId;
+use near_primitives_core::types::{BlockHeight, EpochHeight, ShardId};
 use near_store::Store;
 use nearcore::{NearConfig, NightshadeRuntime};
 use std::path::Path;
+use std::str::FromStr;
 use std::sync::Arc;
 
+#[derive(Subcommand, Debug, Clone)]
+pub(crate) enum EpochSelection {
+    /// Last complete epoch.
+    LastComplete,
+    /// Fetch the given epoch.
+    EpochId { epoch_id: String },
+    /// Fetch epochs at the given height.
+    EpochHeight { epoch_height: EpochHeight },
+    /// Fetch an epoch containing the given block hash.
+    BlockHash { block_hash: String },
+    /// Fetch an epoch containing the given block height.
+    BlockHeight { block_height: BlockHeight },
+}
+
+fn get_epoch_id(
+    epoch_selection: EpochSelection,
+    store: Store,
+    chain_store: &mut ChainStore,
+    epoch_manager: &mut EpochManager,
+) -> EpochId {
+    match epoch_selection {
+        EpochSelection::LastComplete => {
+            let last_block_hash = &chain_store.head().unwrap().last_block_hash;
+            epoch_manager.get_prev_epoch_id(last_block_hash).unwrap()
+        }
+        EpochSelection::EpochId { epoch_id } => EpochId(CryptoHash::from_str(&epoch_id).unwrap()),
+        EpochSelection::EpochHeight { epoch_height } => {
+            // Fetch epochs at the given height.
+            // There should only be one epoch at a given height. But this is a debug tool, let's check
+            // if there are multiple epochs at a given height.
+            let epoch_ids =
+                iterate_and_filter(store, |epoch_info| epoch_info.epoch_height() == epoch_height);
+            assert_eq!(epoch_ids.len(), 1, "{:#?}", epoch_ids);
+            epoch_ids[0].clone()
+        }
+        EpochSelection::BlockHash { block_hash } => {
+            let block_hash = CryptoHash::from_str(&block_hash).unwrap();
+            epoch_manager.get_epoch_id(&block_hash).unwrap()
+        }
+        EpochSelection::BlockHeight { block_height } => {
+            // Fetch an epoch containing the given block height.
+            let block_hash = chain_store.get_block_hash_by_height(block_height).unwrap();
+            epoch_manager.get_epoch_id(&block_hash).unwrap()
+        }
+    }
+}
+
+fn get_prev_hash_of_epoch(
+    epoch_info: &EpochInfo,
+    chain_store: &ChainStore,
+    epoch_manager: &mut EpochManager,
+) -> CryptoHash {
+    let head = chain_store.head().unwrap();
+    let mut cur_block_info = epoch_manager.get_block_info(&head.last_block_hash).unwrap();
+    loop {
+        let cur_epoch_info = epoch_manager.get_epoch_info(cur_block_info.epoch_id()).unwrap();
+        let cur_epoch_height = cur_epoch_info.epoch_height();
+        assert!(
+            cur_epoch_height >= epoch_info.epoch_height(),
+            "cur_block_info: {:#?}, epoch_info.epoch_height: {}",
+            cur_block_info,
+            epoch_info.epoch_height()
+        );
+        let epoch_first_block_info =
+            epoch_manager.get_block_info(cur_block_info.epoch_first_block()).unwrap();
+        let prev_epoch_last_block_info =
+            epoch_manager.get_block_info(epoch_first_block_info.prev_hash()).unwrap();
+
+        if cur_epoch_height == epoch_info.epoch_height() {
+            return *prev_epoch_last_block_info.hash();
+        }
+
+        cur_block_info = prev_epoch_last_block_info;
+    }
+}
+
 pub(crate) fn dump_state_parts(
-    sync_prev_hash: CryptoHash,
+    epoch_selection: EpochSelection,
     shard_id: ShardId,
     part_id: Option<u64>,
     home_dir: &Path,
@@ -19,13 +101,19 @@ pub(crate) fn dump_state_parts(
 ) {
     let runtime_adapter: Arc<dyn RuntimeAdapter> =
         Arc::new(NightshadeRuntime::from_config(home_dir, store.clone(), &near_config));
+    let mut epoch_manager =
+        EpochManager::new_from_genesis_config(store.clone(), &near_config.genesis.config)
+            .expect("Failed to start Epoch Manager");
 
-    let chain_store = ChainStore::new(
+    let mut chain_store = ChainStore::new(
         store.clone(),
         near_config.genesis.config.genesis_height,
         !near_config.client_config.archive,
     );
 
+    let epoch_id = get_epoch_id(epoch_selection, store, &mut chain_store, &mut epoch_manager);
+    let epoch = runtime_adapter.get_epoch_info(&epoch_id).unwrap();
+    let sync_prev_hash = get_prev_hash_of_epoch(&epoch, &mut chain_store, &mut epoch_manager);
     let sync_prev_block = chain_store.get_block(&sync_prev_hash).unwrap();
 
     assert!(runtime_adapter.is_next_block_epoch_start(&sync_prev_hash).unwrap());
@@ -41,7 +129,7 @@ pub(crate) fn dump_state_parts(
         runtime_adapter.get_state_root_node(shard_id, &sync_prev_hash, &state_root).unwrap();
 
     let num_parts = get_num_state_parts(state_root_node.memory_usage);
-    tracing::debug!(num_parts);
+    tracing::debug!(epoch_height = epoch.epoch_height(), num_parts);
 
     std::fs::create_dir_all(output_dir).unwrap();
     for part_id in if let Some(part_id) = part_id { part_id..part_id + 1 } else { 0..num_parts } {
