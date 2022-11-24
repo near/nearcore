7276a43d3ce87806f60df5511d7281c4d2dea882
Grzegorz Prusak <pompon.pompon@gmail.com>
2022-11-24 12:34:16 +0100

    fixed wait_for_routing_table

--- chain/network/src/peer_manager/network_state/routing.rs
+++ chain/network/src/peer_manager/network_state/routing.rs
@@ -96,6 +96,7 @@ impl NetworkState {
                     }
                 }
                 // Broadcast new edges to all other peers.
+                this.config.event_sink.push(Event::EdgesAdded(edges.clone()));
                 this.broadcast_routing_table_update(RoutingTableUpdate::from_edges(edges));
                 results
             })
