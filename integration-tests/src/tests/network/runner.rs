affd713cfe2897061417eb9e611224d4d810e792
Grzegorz Prusak <pompon.pompon@gmail.com>
2022-11-30 13:18:20 +0100

    compilation fix

--- integration-tests/src/tests/network/runner.rs
+++ integration-tests/src/tests/network/runner.rs
@@ -223,7 +223,7 @@ impl StateMachine {
                     debug!(target: "network", num_prev_actions, action = ?action_clone, "runner.rs: Action");
                     let pm = info.get_node(from)?.actix.addr.clone();
                     let peer_info = info.runner.test_config[to].peer_info();
-                    match tcp::Stream::connect(&peer_info).await {
+                    match tcp::Stream::connect(&peer_info, tcp::Tier::T2).await {
                         Ok(stream) => { pm.send(PeerManagerMessageRequest::OutboundTcpConnect(stream).with_span_context()).await?; },
                         Err(err) => tracing::debug!("tcp::Stream::connect({peer_info}): {err}"),
                     }
