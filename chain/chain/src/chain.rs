db156a6d52f4ebd1f36146fdfdfdf976d7b79dfd
Longarithm <the.aleksandr.logunov@gmail.com>
2022-12-20 16:14:33 +0400

    comment assertion

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -2154,8 +2154,10 @@ impl Chain {
                     flat_storage_creator.update_status(shard_id, &self.store)?;
                 }
                 None => {
-                    #[cfg(feature = "protocol_feature_flat_state")]
-                    debug_assert!(false, "Flat storage state for shard {shard_id} does not exist and its creation was not initiated");
+                    // TODO (#8250): enable this assertion. Currently it doesn't work because runtime may be implemented
+                    // with KeyValueRuntime which doesn't support flat storage.
+                    // #[cfg(feature = "protocol_feature_flat_state")]
+                    // debug_assert!(false, "Flat storage state for shard {shard_id} does not exist and its creation was not initiated");
                 }
             }
         }
