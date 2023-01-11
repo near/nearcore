d6a7587c4fe999d3080d53b85567459aa26632f4
Simonas Kazlauskas <git@kazlauskas.me>
2023-01-11 20:59:20 +0200

    scripts: maintain clippy lints in a bash array
    This is going to be a lil' bit more readable and scannable, especially
    as the list of lints we specify grows, which it is quite guaranteed to.

--- scripts/run_clippy.sh
+++ scripts/run_clippy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # clippy adoption is in progress, see https://github.com/near/nearcore/issues/8145
-cargo clippy -- \
-    -A clippy::all \
-    -D clippy::correctness \
-    -D clippy::suspicious
 
+LINTS=(
+  -A clippy::all
+  -D clippy::correctness
+  -D clippy::suspicious
+)
+
+cargo clippy -- "${LINTS[@]}"
