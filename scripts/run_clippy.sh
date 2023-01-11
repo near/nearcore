277ed7bb55f0ab13e9c602b2149974ca27669275
Anton Puhach <anton@near.org>
2023-01-11 10:25:29 +0100

    impl

--- scripts/run_clippy.sh
+++ scripts/run_clippy.sh
@@ -2,5 +2,6 @@
 # clippy adoption is in progress, see https://github.com/near/nearcore/issues/8145
 cargo clippy -- \
     -A clippy::all \
-    -D clippy::correctness
+    -D clippy::correctness \
+    -D clippy::suspicious
 
