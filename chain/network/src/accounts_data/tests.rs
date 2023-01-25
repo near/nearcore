88d6d5b010ecebf3fb11ef00d4970efc863fe5c0
pompon0 <pompon.pompon@gmail.com>
2023-01-25 15:02:29 +0100

    fixed broadcasting AccountData after node restart. (#8407)
    AccountData is broadcasted by validator node periodically (every 15min).
    AccountData with greatest `version` field is considered the freshest.
    We do not store the latest broadcasted version in storage, so each time node is restarted, is starts with version = 1, unless it learns the latest broadcasted version (from the previous execution) from the network.
    It means that it may happen that the correct AccountData will be broadcasted only after 15min from restart.
    
    This in turn means that the validator might be not reachable (messages cannot be routed to it) for 15min which is unacceptable. We fix that by making the validator node broadcast a new version of AccountData as soon as it learns about stale AccountData (but with higher version) from the network.
    
    Alternative solutions considered are described in the accounts_data/mod.rs (see documentation of is_new() function).

--- chain/network/src/accounts_data/tests.rs
+++ chain/network/src/accounts_data/tests.rs
@@ -3,6 +3,7 @@ use crate::network_protocol::testonly as data;
 use crate::network_protocol::SignedAccountData;
 use crate::testonly::{assert_is_superset, make_rng, AsSet as _, Rng};
 use crate::time;
+use near_o11y::testonly::init_test_logger;
 use near_primitives::validator_signer::InMemoryValidatorSigner;
 use pretty_assertions::assert_eq;
 use std::sync::Arc;
@@ -34,6 +35,7 @@ fn make_signers(rng: &mut Rng, n: usize) -> Vec<InMemoryValidatorSigner> {
 
 #[tokio::test]
 async fn happy_path() {
+    init_test_logger();
     let mut rng = make_rng(2947294234);
     let rng = &mut rng;
     let clock = time::FakeClock::default();
@@ -50,7 +52,7 @@ async fn happy_path() {
     // initial insert
     let a0 = Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[0]));
     let a1 = Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[1]));
-    let res = cache.clone().insert(vec![a0.clone(), a1.clone()]).await;
+    let res = cache.clone().insert(&clock.clock(), vec![a0.clone(), a1.clone()]).await;
     assert_eq!([&a0, &a1].as_set(), unwrap(&res).as_set());
     assert_eq!([&a0, &a1].as_set(), cache.load().data.values().collect());
 
@@ -61,12 +63,15 @@ async fn happy_path() {
     let a5 = Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[5]));
     let res = cache
         .clone()
-        .insert(vec![
-            a2.clone(),    // initial value => insert
-            a0new.clone(), // with newer timestamp => insert,
-            a1old.clone(), // with older timestamp => filter out,
-            a5.clone(),    // not in e0 => filter out.
-        ])
+        .insert(
+            &clock.clock(),
+            vec![
+                a2.clone(),    // initial value => insert
+                a0new.clone(), // with newer timestamp => insert,
+                a1old.clone(), // with older timestamp => filter out,
+                a5.clone(),    // not in e0 => filter out.
+            ],
+        )
         .await;
     assert_eq!([&a2, &a0new].as_set(), unwrap(&res).as_set());
     assert_eq!([&a0new, &a1, &a2].as_set(), cache.load().data.values().collect());
@@ -81,10 +86,13 @@ async fn happy_path() {
     // insert some entries again.
     let res = cache
         .clone()
-        .insert(vec![
-            a0.clone(), // a0 is not in e1 => filter out
-            a5.clone(), // a5 is in e1 => insert,
-        ])
+        .insert(
+            &clock.clock(),
+            vec![
+                a0.clone(), // a0 is not in e1 => filter out
+                a5.clone(), // a5 is in e1 => insert,
+            ],
+        )
         .await;
     assert_eq!([&a5].as_set(), unwrap(&res).as_set());
     assert_eq!([&a2, &a5].as_set(), cache.load().data.values().collect());
@@ -92,6 +100,7 @@ async fn happy_path() {
 
 #[tokio::test]
 async fn data_too_large() {
+    init_test_logger();
     let mut rng = make_rng(2947294234);
     let rng = &mut rng;
     let clock = time::FakeClock::default();
@@ -112,11 +121,14 @@ async fn data_too_large() {
     // too large payload => DataTooLarge
     let res = cache
         .clone()
-        .insert(vec![
-            a0.clone(),
-            a1.clone(),
-            a2_too_large.clone(), // invalid entry => DataTooLarge
-        ])
+        .insert(
+            &clock.clock(),
+            vec![
+                a0.clone(),
+                a1.clone(),
+                a2_too_large.clone(), // invalid entry => DataTooLarge
+            ],
+        )
         .await;
     assert_eq!(Some(Error::DataTooLarge), res.1);
     // Partial update is allowed, in case an error is encountered.
@@ -127,6 +139,7 @@ async fn data_too_large() {
 
 #[tokio::test]
 async fn invalid_signature() {
+    init_test_logger();
     let mut rng = make_rng(2947294234);
     let rng = &mut rng;
     let clock = time::FakeClock::default();
@@ -146,11 +159,14 @@ async fn invalid_signature() {
     // invalid signature => InvalidSignature
     let res = cache
         .clone()
-        .insert(vec![
-            a0.clone(),
-            a1.clone(),
-            a2_invalid_sig.clone(), // invalid entry => DataTooLarge
-        ])
+        .insert(
+            &clock.clock(),
+            vec![
+                a0.clone(),
+                a1.clone(),
+                a2_invalid_sig.clone(), // invalid entry => DataTooLarge
+            ],
+        )
         .await;
     assert_eq!(Some(Error::InvalidSignature), res.1);
     // Partial update is allowed, in case an error is encountered.
@@ -161,6 +177,7 @@ async fn invalid_signature() {
 
 #[tokio::test]
 async fn single_account_multiple_data() {
+    init_test_logger();
     let mut rng = make_rng(2947294234);
     let rng = &mut rng;
     let clock = time::FakeClock::default();
@@ -176,12 +193,91 @@ async fn single_account_multiple_data() {
     let a2new = Arc::new(make_account_data(rng, &clock.clock(), 2, &signers[2]));
 
     // 2 entries for the same (epoch_id,account_id) => SingleAccountMultipleData
-    let res =
-        cache.clone().insert(vec![a0.clone(), a1.clone(), a2old.clone(), a2new.clone()]).await;
+    let res = cache
+        .clone()
+        .insert(&clock.clock(), vec![a0.clone(), a1.clone(), a2old.clone(), a2new.clone()])
+        .await;
     assert_eq!(Some(Error::SingleAccountMultipleData), res.1);
     // Partial update is allowed, in case an error is encountered.
     assert_is_superset(&[&a0, &a1, &a2old, &a2new].as_set(), &res.0.as_set());
     // Partial update should match the state, this also verifies that only 1 of the competing
     // entries has been applied.
     assert_eq!(res.0.as_set(), cache.load().data.values().collect());
+}
+
+/// Test checking that cache immediately overrides any inserted AccountData for local.signer
+/// with local.data.
+#[tokio::test]
+async fn set_local() {
+    init_test_logger();
+    let mut rng = make_rng(2947294234);
+    let rng = &mut rng;
+    let clock = time::FakeClock::default();
+
+    let signers: Vec<_> = make_signers(rng, 3);
+    let e0 = Arc::new(data::make_account_keys(&signers[0..2]));
+    let e1 = Arc::new(data::make_account_keys(&signers[1..3]));
+
+    let cache = Arc::new(Cache::new());
+    assert!(cache.set_keys(e0.clone()));
+
+    // Set local while local.signer is in cache.keys.
+    // A new AccountData should be signed.
+    let local = LocalData {
+        signer: Arc::new(signers[0].clone()),
+        data: Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[0]).data.clone()),
+    };
+    let got = cache.set_local(&clock.clock(), local.clone()).unwrap();
+    assert_eq!(local.data.as_ref(), &got.data);
+    assert_eq!(local.signer.public_key(), got.account_key);
+    clock.advance(time::Duration::hours(1));
+
+    // Insert new version while local data is set and local.signer is in cache.keys.
+    // Cache should immediately emit AccountData for local.signer which overrides
+    // the new version received.
+    let a0 = Arc::new(make_account_data(rng, &clock.clock(), 7, &signers[0]));
+    // Regular entry for a signer in cache.keys. The new version should be accepted.
+    let a1 = Arc::new(make_account_data(rng, &clock.clock(), 10, &signers[1]));
+    // Regular entry for a signer outside of cache.keys. The new version should be ignored.
+    let a2 = Arc::new(make_account_data(rng, &clock.clock(), 8, &signers[2]));
+
+    let res = cache.clone().insert(&clock.clock(), vec![a0.clone(), a1.clone(), a2.clone()]).await;
+    assert_eq!(res.0.as_set(), cache.load().data.values().collect());
+    let res: HashMap<_, _> = unwrap(&res).iter().map(|a| (a.account_key.clone(), a)).collect();
+    let got = res.get(&signers[0].public_key()).unwrap();
+    assert_eq!(local.data.as_ref(), &got.data);
+    assert!(a0.version < got.version);
+    assert_eq!(a1.as_ref(), res.get(&signers[1].public_key()).unwrap().as_ref());
+    assert_eq!(None, res.get(&signers[2].public_key()));
+
+    // Insert new version while local data is set but local.signer is not in cache.keys.
+    // local data should be just ignored.
+    clock.advance(time::Duration::hours(1));
+    assert!(cache.set_keys(e1.clone()));
+    let a0 = Arc::new(make_account_data(rng, &clock.clock(), got.version + 1, &signers[0]));
+    // Regular entries for a signers in cache.keys. The new version should be accepted.
+    let a1 = Arc::new(make_account_data(rng, &clock.clock(), a1.version + 1, &signers[1]));
+    let a2 = Arc::new(make_account_data(rng, &clock.clock(), a2.version + 1, &signers[2]));
+
+    let res = cache.clone().insert(&clock.clock(), vec![a0.clone(), a1.clone(), a2.clone()]).await;
+    assert_eq!(res.0.as_set(), cache.load().data.values().collect());
+    assert_eq!([&a1, &a2].as_set(), unwrap(&res).as_set());
+
+    // Update local data to a signer in cache.keys.
+    let local = LocalData {
+        signer: Arc::new(signers[2].clone()),
+        data: Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[2]).data.clone()),
+    };
+    let got = cache.set_local(&clock.clock(), local.clone()).unwrap();
+    assert_eq!(local.data.as_ref(), &got.data);
+    assert_eq!(local.signer.public_key(), got.account_key);
+    assert_eq!([&a1, &got].as_set(), cache.load().data.values().collect());
+
+    // Update local data to a signer outside of cache.keys.
+    let local = LocalData {
+        signer: Arc::new(signers[0].clone()),
+        data: Arc::new(make_account_data(rng, &clock.clock(), 1, &signers[0]).data.clone()),
+    };
+    assert_eq!(None, cache.set_local(&clock.clock(), local.clone()));
+    assert_eq!([&a1, &got].as_set(), cache.load().data.values().collect());
 }
