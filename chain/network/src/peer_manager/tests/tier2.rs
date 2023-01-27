e49c2df46f66f4d69f47c2ca0aff27e0930b05c7
Saketh Are <saketh.are@gmail.com>
2023-01-27 11:09:28 -0500

    wait properly for connection closed

--- chain/network/src/peer_manager/tests/tier2.rs
+++ chain/network/src/peer_manager/tests/tier2.rs
@@ -1,9 +1,12 @@
+use crate::broadcast;
 use crate::network_protocol::testonly as data;
 use crate::peer_manager::network_state::RECENT_OUTBOUND_CONNECTIONS_MIN_DURATION;
 use crate::peer_manager::network_state::RECONNECT_ATTEMPT_INTERVAL;
+use crate::peer_manager::peer_manager_actor::Event as PME;
 use crate::peer_manager::peer_manager_actor::UPDATE_RECENT_OUTBOUND_CONNECTIONS_INTERVAL;
 use crate::peer_manager::testonly::start as start_pm;
 use crate::peer_manager::testonly::ActorHandler;
+use crate::peer_manager::testonly::Event;
 use crate::tcp;
 use crate::testonly::make_rng;
 use crate::time;
@@ -20,6 +23,15 @@ async fn check_recent_outbound_connections(pm: &ActorHandler, wanted: Vec<PeerId
         .await;
 
     assert!(got == wanted, "Expected {:?} but got {:?}", wanted, got);
+}
+
+async fn wait_for_connection_closed(events: &mut broadcast::Receiver<Event>) {
+    events
+        .recv_until(|ev| match ev {
+            Event::PeerManager(PME::ConnectionClosed(_)) => Some(()),
+            _ => None,
+        })
+        .await
 }
 
 #[tokio::test]
@@ -77,8 +89,10 @@ async fn test_recent_outbound_connection_after_disconnect() {
     tracing::info!(target:"test", "check that pm0 stores the outbound connection to pm1");
     check_recent_outbound_connections(&pm0, vec![id1.clone()]).await;
 
+    tracing::info!(target:"test", "have pm1 disconnect from pm0");
+    let mut pm0_ev = pm0.events.from_now();
     pm1.disconnect(&id0).await;
-    pm0.wait_for_num_connected_peers(0).await;
+    wait_for_connection_closed(&mut pm0_ev).await;
 
     tracing::info!(target:"test", "check that pm0 retains the stored outbound connection to pm1 after disconnect");
     clock.advance(UPDATE_RECENT_OUTBOUND_CONNECTIONS_INTERVAL);
@@ -119,8 +133,9 @@ async fn test_outbound_connection_storage_order() {
     check_recent_outbound_connections(&pm0, vec![id3.clone(), id2.clone(), id1.clone()]).await;
 
     tracing::info!(target:"test", "disconnect pm2 and check that it remains in storage but moves to the end of the order");
+    let mut pm0_ev = pm0.events.from_now();
     pm2.disconnect(&id0).await;
-    pm0.wait_for_num_connected_peers(2).await;
+    wait_for_connection_closed(&mut pm0_ev).await;
     clock.advance(UPDATE_RECENT_OUTBOUND_CONNECTIONS_INTERVAL);
     check_recent_outbound_connections(&pm0, vec![id3.clone(), id1.clone(), id2.clone()]).await;
 }
@@ -178,8 +193,12 @@ async fn test_reconnect_after_restart_inbound_side() {
     tracing::info!(target:"test", "check that pm0 stores the outbound connection to pm1");
     check_recent_outbound_connections(&pm0, vec![id1.clone()]).await;
 
-    tracing::info!(target:"test", "drop pm1 and start it again with the same config, check that pm0 reconnects");
+    tracing::info!(target:"test", "drop pm1");
+    let mut pm0_ev = pm0.events.from_now();
     drop(pm1);
+    wait_for_connection_closed(&mut pm0_ev).await;
+
+    tracing::info!(target:"test", "start pm1 again with the same config, check that pm0 reconnects");
     let _pm1 = start_pm(clock.clock(), TestDB::new(), pm1_cfg.clone(), chain.clone()).await;
     clock.advance(RECONNECT_ATTEMPT_INTERVAL);
     pm0.wait_for_direct_connection(id1.clone()).await;
@@ -211,8 +230,9 @@ async fn test_reconnect_after_disconnect_inbound_side() {
     check_recent_outbound_connections(&pm0, vec![id1.clone()]).await;
 
     tracing::info!(target:"test", "have pm1 disconnect gracefully from pm0");
+    let mut pm0_ev = pm0.events.from_now();
     pm1.disconnect(&id0).await;
-    pm0.wait_for_num_connected_peers(0).await;
+    wait_for_connection_closed(&mut pm0_ev).await;
 
     tracing::info!(target:"test", "check that pm0 reconnects");
     clock.advance(RECONNECT_ATTEMPT_INTERVAL);
