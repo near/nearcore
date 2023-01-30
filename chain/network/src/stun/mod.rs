c076181fd6c35f5ab0e5d24cd2116e18d1dc7ee0
Grzegorz Prusak <pompon.pompon@gmail.com>
2023-01-30 18:56:05 +0100

    stun query works

--- chain/network/src/stun/mod.rs
+++ chain/network/src/stun/mod.rs
@@ -3,7 +3,7 @@ use crate::time;
 use stun::message::{Getter as _};
 
 #[cfg(test)]
-mod test;
+mod tests;
 
 pub type ServerAddr = String;
 pub type Error = stun::Error;
@@ -11,22 +11,19 @@ pub type Error = stun::Error;
 const QUERY_TIMEOUT : time::Duration = time::Duration::seconds(5);
 
 pub async fn query(clock :&time::Clock, addr: ServerAddr) -> Result<std::net::IpAddr,Error> {
-    let socket = tokio::net::UdpSocket::bind("0:0").await?;
+    let socket = tokio::net::UdpSocket::bind("[::]:0").await?;
     socket.connect(addr).await?;
     let mut client = stun::client::ClientBuilder::new().with_conn(Arc::new(socket)).build()?;
     let mut msg = stun::message::Message::new();
-    msg.build(&[Box::<stun::agent::TransactionId>::default(), Box::new(stun::message::BINDING_REQUEST)])?;
-    // msg.new_transaction_id()?;
-    // msg.set_type(stun::message::BINDING_REQUEST);
-    tracing::debug!("msg = {msg:?}");
+    msg.new_transaction_id()?;
+    msg.set_type(stun::message::BINDING_REQUEST);
+    msg.build(&[])?;
     let (send,mut recv) = tokio::sync::mpsc::unbounded_channel();
-    tracing::info!(target:"dupa", "client.send()");
     client.send(&msg, Some(Arc::new(send))).await?;
     // Note that both clock.sleep() and recv.recv() are cancellable,
     // so it is safe to use them in tokio::select!.
     let ip = tokio::select! {
         _ = clock.sleep(QUERY_TIMEOUT) => {
-            tracing::debug!(target:"dupa", "clock.sleep() terminated");
             return Err(Error::ErrTransactionTimeOut);
         }
         e = recv.recv() => match e {
@@ -38,7 +35,6 @@ pub async fn query(clock :&time::Clock, addr: ServerAddr) -> Result<std::net::Ip
                 return Err(Error::ErrTransactionStopped);
             }
             Some(e) => {
-                tracing::debug!(target:"dupa", "got sth");
                 let mut addr = stun::xoraddr::XorMappedAddress::default();
                 addr.get_from(&e.event_body?)?;
                 addr.ip
---
+++ chain/network/src/stun/tests.rs
@@ -0,0 +1,38 @@
+use crate::time;
+use near_o11y::testonly::init_test_logger;
+use std::sync::Arc;
+
+struct TestAuthHandler;
+
+impl turn::auth::AuthHandler for TestAuthHandler {
+    fn auth_handle(&self, _username: &str, _realm: &str, _src_addr: std::net::SocketAddr) -> Result<Vec<u8>,turn::Error> {
+       unimplemented!(); 
+    }
+}
+
+#[tokio::test]
+async fn test_query() {
+    init_test_logger();
+
+    let server_conn = Arc::new(tokio::net::UdpSocket::bind("[::1]:0").await.unwrap());
+    let server_addr = server_conn.local_addr().unwrap();
+
+    let server = turn::server::Server::new(turn::server::config::ServerConfig {
+        conn_configs: vec![turn::server::config::ConnConfig {
+            conn: server_conn,
+            relay_addr_generator: Box::new(turn::relay::relay_none::RelayAddressGeneratorNone {
+                address: "[::]".to_owned(),
+                net: Arc::new(webrtc_util::vnet::net::Net::new(None)),
+            }),
+        }],
+        realm: "webrtc.rs".to_owned(),
+        auth_handler: Arc::new(TestAuthHandler),
+        channel_bind_timeout: std::time::Duration::from_secs(0),
+    })
+    .await.unwrap();
+
+    let clock = time::FakeClock::default();
+    let ip = super::query(&clock.clock(),server_addr.to_string()).await.unwrap();
+    assert_eq!(std::net::Ipv6Addr::LOCALHOST,ip);
+    server.close().await.unwrap();
+}
