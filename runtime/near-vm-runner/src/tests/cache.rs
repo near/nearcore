22c609c33dd6795682eecc18b7cf75853a473db7
Simonas Kazlauskas <git@kazlauskas.me>
2023-02-03 13:10:06 +0200

    prepare: add skeleton for v2 preparation code

--- runtime/near-vm-runner/src/tests/cache.rs
+++ runtime/near-vm-runner/src/tests/cache.rs
@@ -131,6 +131,7 @@ fn test_near_vm_artifact_output_stability() {
         let contract = ContractCode::new(near_test_contracts::arbitrary_contract(seed), None);
 
         let config = VMConfig::test();
+        // TODO: adjust this test to run for all VMKinds
         let prepared_code = prepare::prepare_contract_for_near_vm(contract.code(), &config).unwrap();
         let mut hasher = StableHasher::new();
         (&contract.code(), &prepared_code).hash(&mut hasher);
--- runtime/near-vm-runner/src/wasmer_runner.rs
+++ runtime/near-vm-runner/src/wasmer_runner.rs
@@ -246,7 +246,7 @@ impl Wasmer0VM {
         code: &ContractCode,
     ) -> Result<wasmer_runtime::Module, CompilationError> {
         let _span = tracing::debug_span!(target: "vm", "Wasmer0VM::compile_uncached").entered();
-        let prepared_code = prepare::prepare_contract(code.code(), &self.config)
+        let prepared_code = prepare::prepare_contract(code.code(), &self.config, VMKind::Wasmer0)
             .map_err(CompilationError::PrepareError)?;
         wasmer_runtime::compile(&prepared_code).map_err(|err| match err {
             wasmer_runtime::error::CompileError::ValidationError { .. } => {
@@ -266,6 +266,7 @@ impl Wasmer0VM {
         cache: Option<&dyn CompiledContractCache>,
     ) -> Result<Result<wasmer_runtime::Module, CompilationError>, CacheError> {
         let module_or_error = self.compile_uncached(code);
+        // TODO: this needs to incorporate the protocol version too now
         let key = get_contract_cache_key(code, VMKind::Wasmer0, &self.config);
 
         if let Some(cache) = cache {
