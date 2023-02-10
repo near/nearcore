a5fd2336cf6dd1b12cf5d880e9963a6f245c3210
jakob <jakob@near.org>
2023-02-10 13:12:54 +0100

    test: first meta tx integration tests
    1. Extend the test utilities to make meta transaction tests easy to add.
    2. Add two tests as an example.
      a) NEAR token transfer
      b) simple function call
    
    I will cover more cases in future PRs. This is to establish the
    framework:
    - `fn meta_tx` in `trait User`
    - Checker function specific to meta transactions
    - Provide helpers to do tricky gas checks
    
    The 2 test cases show that it works as intended.

--- core/primitives/src/test_utils.rs
+++ core/primitives/src/test_utils.rs
@@ -20,6 +20,7 @@ use crate::transaction::{
 use crate::types::{AccountId, Balance, EpochId, EpochInfoProvider, Gas, Nonce};
 use crate::validator_signer::{InMemoryValidatorSigner, ValidatorSigner};
 use crate::version::PROTOCOL_VERSION;
+use crate::views::{ExecutionStatusView, FinalExecutionOutcomeView, FinalExecutionStatus};
 
 pub fn account_new(amount: Balance, code_hash: CryptoHash) -> Account {
     Account::new(amount, 0, code_hash, std::mem::size_of::<Account>() as u64)
@@ -486,4 +487,21 @@ pub fn create_test_signer(account_name: &str) -> InMemoryValidatorSigner {
         KeyType::ED25519,
         account_name,
     )
+}
+
+impl FinalExecutionOutcomeView {
+    #[track_caller]
+    /// Check transaction and all transitive receipts for success status.
+    pub fn assert_success(&self) {
+        assert_eq!(self.status, FinalExecutionStatus::SuccessValue(Vec::new()));
+        for (i, receipt) in self.receipts_outcome.iter().enumerate() {
+            assert!(
+                matches!(
+                    receipt.outcome.status,
+                    ExecutionStatusView::SuccessReceiptId(_) | ExecutionStatusView::SuccessValue(_),
+                ),
+                "receipt #{i} failed: {receipt:?}",
+            );
+        }
+    }
 }
--- integration-tests/src/user/mod.rs
+++ integration-tests/src/user/mod.rs
@@ -2,9 +2,13 @@ use std::sync::Arc;
 
 use futures::{future::LocalBoxFuture, FutureExt};
 
+#[cfg(feature = "protocol_feature_nep366_delegate_action")]
+use near_crypto::{InMemorySigner, KeyType};
 use near_crypto::{PublicKey, Signer};
 use near_jsonrpc_primitives::errors::ServerError;
 use near_primitives::account::AccessKey;
+#[cfg(feature = "protocol_feature_nep366_delegate_action")]
+use near_primitives::delegate_action::{DelegateAction, NonDelegateAction, SignedDelegateAction};
 use near_primitives::hash::CryptoHash;
 use near_primitives::receipt::Receipt;
 use near_primitives::transaction::{
@@ -238,6 +242,41 @@ pub trait User {
             signer_id.clone(),
             signer_id,
             vec![Action::Stake(StakeAction { stake, public_key })],
+        )
+    }
+
+    #[cfg(feature = "protocol_feature_nep366_delegate_action")]
+    fn meta_tx(
+        &self,
+        signer_id: AccountId,
+        receiver_id: AccountId,
+        relayer_id: AccountId,
+        actions: Vec<Action>,
+    ) -> Result<FinalExecutionOutcomeView, ServerError> {
+        let inner_signer =
+            InMemorySigner::from_seed(signer_id.clone(), KeyType::ED25519, &signer_id);
+        let user_nonce = self
+            .get_access_key(&signer_id, &inner_signer.public_key)
+            .expect("failed reading user's nonce for access key")
+            .nonce;
+        let delegate_action = DelegateAction {
+            sender_id: signer_id.clone(),
+            receiver_id,
+            actions: actions
+                .into_iter()
+                .map(|action| NonDelegateAction::try_from(action).unwrap())
+                .collect(),
+            nonce: user_nonce + 1,
+            max_block_height: 100,
+            public_key: inner_signer.public_key(),
+        };
+        let signature = inner_signer.sign(delegate_action.get_hash().as_bytes());
+        let signed_delegate_action = SignedDelegateAction { delegate_action, signature };
+
+        self.sign_and_commit_actions(
+            relayer_id,
+            signer_id,
+            vec![Action::Delegate(signed_delegate_action)],
         )
     }
 }
