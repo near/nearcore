ae4505aab7d70d4aae75e06fe56193c40634c32f
Saketh Are <saketh.are@gmail.com>
2023-02-17 12:29:47 -0500

    address review comments

--- tools/debug-ui/src/Tier1View.tsx
+++ tools/debug-ui/src/Tier1View.tsx
@@ -1,6 +1,6 @@
 import { MouseEvent, ReactElement, useCallback, useMemo, useState } from "react";
 import { useQuery } from "react-query";
-import { fetchEpochInfo, fetchFullStatus, PeerInfoView } from "./api";
+import { fetchEpochInfo, fetchFullStatus, PeerInfoView, PeerAddr } from "./api";
 import { formatDurationInMillis, formatTraffic, addDebugPortLink } from "./utils";
 import "./Tier1View.scss";
 
@@ -35,83 +35,76 @@ export const Tier1View = ({ addr }: Tier1ViewProps) => {
         </div>;
     }
 
-    let rendered = new Set();
-    const network_info = fullStatus.detailed_debug_status!.network_info;
+    const networkInfo = fullStatus.detailed_debug_status!.network_info;
 
-    let rows = new Array();
+    const rendered = new Set();
+    const rows = new Array();
 
     // tier1_connections contains TIER1 nodes we are currently connected to
-    network_info.tier1_connections!.forEach(function (peer) {
-        let account_key = "";
-        let proxies = new Array();
+    for (const peer of networkInfo.tier1_connections!) {
+        let accountKey = "";
+        let proxies:PeerAddr[] = [];
 
-        network_info.tier1_accounts_data!.forEach(account => {
+        networkInfo.tier1_accounts_data!.forEach(account => {
             if (account.peer_id == peer.peer_id) {
-                account_key = account.account_key;
-                account.proxies.forEach(proxy => {
-                    proxies.push(proxy.peer_id.substr(8, 5) + "...@" + proxy.addr);
-                });
+                accountKey = account.account_key;
+                proxies = account.proxies;
             }
         });
 
-        rendered.add(account_key);
+        rendered.add(accountKey);
 
-        let account_id = "";
-        network_info.known_producers.forEach(producer => {
+        let accountId = "";
+        networkInfo.known_producers.forEach(producer => {
             if (producer.peer_id == peer.peer_id) {
-                account_id = producer.account_id;
+                accountId = producer.account_id;
             }
         });
 
-        let last_ping = formatDurationInMillis(peer.last_time_received_message_millis)
-        let last_ping_class = ""
-        if (fullStatus.node_public_key == account_key) {
-            last_ping = "n/a"
+        let lastPing = formatDurationInMillis(peer.last_time_received_message_millis);
+        let lastPingClass = "";
+        if (fullStatus.node_public_key == accountKey) {
+            lastPing = "n/a"
         } else if (peer.last_time_received_message_millis > 60 * 1000) {
-            last_ping_class = "peer_far_behind";
+            lastPingClass = "peer_far_behind";
         }
 
-        rows.push(<tr key={account_key}>
+        rows.push(<tr key={accountKey}>
             <td>{addDebugPortLink(peer.addr)}</td>
-            <td>{account_key.substring(8, 14)}...</td>
-            <td>{account_id}</td>
-            <td><CollapsableProxyList proxies={proxies} /></td>
+            <td>{accountKey.substring(8, 14)}...</td>
+            <td>{accountId}</td>
+            <td><CollapsibleProxyList proxies={proxies} /></td>
             <td>{peer.peer_id.substring(8, 14)}...</td>
-            <td className={last_ping_class}>{last_ping}</td>
+            <td className={lastPingClass}>{lastPing}</td>
             <td>{JSON.stringify(peer.tracked_shards)}</td>
             <td>{JSON.stringify(peer.archival)}</td>
             <td>{peer.is_outbound_peer ? 'OUT' : 'IN'}</td>
             <td>{formatDurationInMillis(peer.connection_established_time_millis)}</td>
             <td>{formatTraffic(peer.received_bytes_per_sec, peer.sent_bytes_per_sec)}</td>
         </tr>);
-    });
+    }
 
     // tier1_accounts_data contains data about TIER1 nodes we would like to connect to
-    network_info.tier1_accounts_data!.forEach(account => {
-        let account_key = account.account_key;
+    for (const account of networkInfo.tier1_accounts_data!) {
+        let accountKey = account.account_key;
 
-        if (rendered.has(account_key)) {
-            return;
+        if (rendered.has(accountKey)) {
+            continue;
         }
-        rendered.add(account_key);
+        rendered.add(accountKey);
 
-        let account_id = "";
-        network_info.known_producers.forEach(producer => {
+        let accountId = "";
+        networkInfo.known_producers.forEach(producer => {
             if (producer.peer_id == account.peer_id) {
-                account_id = producer.account_id;
+                accountId = producer.account_id;
             }
         });
 
-        let proxies = new Array();
-        account.proxies.forEach(proxy => {
-            proxies.push(proxy.peer_id.substr(8, 5) + "...@" + proxy.addr);
-        });
-
-        rows.push(<tr key={account_key}>
+        rows.push(<tr key={accountKey}>
             <td></td>
-            <td>{account_key.substring(8, 14)}...</td>
-            <td>{account_id}</td>
-            <td><CollapsableProxyList proxies={proxies} /></td>
+            <td>{accountKey.substring(8, 14)}...</td>
+            <td>{accountId}</td>
+            <td><CollapsibleProxyList proxies={account.proxies} /></td>
             <td>{account.peer_id.substring(8, 14)}...</td>
             <td></td>
             <td></td>
@@ -120,18 +113,18 @@ export const Tier1View = ({ addr }: Tier1ViewProps) => {
             <td></td>
             <td></td>
         </tr>);
-    });
+    }
 
     // tier1_accounts_keys contains accounts whose data we would like to collect
-    network_info.tier1_accounts_keys!.forEach(account_key => {
-        if (rendered.has(account_key)) {
-            return;
+    for (const accountKey of networkInfo.tier1_accounts_keys!) {
+        if (rendered.has(accountKey)) {
+            continue;
         }
-        rendered.add(account_key);
+        rendered.add(accountKey);
 
-        rows.push(<tr key={account_key}>
+        rows.push(<tr key={accountKey}>
             <td></td>
-            <td>{account_key.substring(8, 14)}...</td>
+            <td>{accountKey.substring(8, 14)}...</td>
             <td></td>
             <td></td>
             <td></td>
@@ -142,7 +135,7 @@ export const Tier1View = ({ addr }: Tier1ViewProps) => {
             <td></td>
             <td></td>
         </tr>);
-    });
+    }
 
     return <div className="tier1-peers-view">
         <table className="peers">
@@ -168,21 +161,26 @@ export const Tier1View = ({ addr }: Tier1ViewProps) => {
     </div>;
 };
 
-const CollapsableProxyList = ({ proxies }: { proxies: string[] }) => {
+const CollapsibleProxyList = ({ proxies }: { proxies: PeerAddr[] }) => {
     const [showAll, setShowAll] = useState(false);
     const callback = useCallback((e: MouseEvent) => {
         e.preventDefault();
         setShowAll((show) => !show);
     }, []);
     const MAX_TO_SHOW = 2;
-    if (proxies.length <= MAX_TO_SHOW) {
-        return <>{proxies.join(', ')}</>;
+
+    const formatted = proxies.map(proxy => {
+        return proxy.peer_id.substring(8, 14) + "...@" + proxy.addr;
+    });
+
+    if (formatted.length <= MAX_TO_SHOW) {
+        return <>{formatted.join(', ')}</>;
     }
     if (showAll) {
-        return <>{proxies.join(', ')} <a href="#" onClick={callback}>Show fewer</a></>;
+        return <>{formatted.join(', ')} <a href="#" onClick={callback}>Show fewer</a></>;
     }
     return <>
-        {proxies.slice(0, MAX_TO_SHOW).join(', ')}, ...<br />
-        <a href="#" onClick={callback}>Show all {proxies.length}</a>
+        {formatted.slice(0, MAX_TO_SHOW).join(', ')}, ...<br />
+        <a href="#" onClick={callback}>Show all {formatted.length}</a>
     </>;
 }
