717532e334adbfd75aad3aa4109dcc9515fb549a
Saketh Are <saketh.are@gmail.com>
2023-02-24 10:53:50 -0500

    PeersResponse: always include connected peers (#8619)
    * ensure PeersResponse includes connected Peers
    
    * include direct_peers as separate field of PeersResponse
    
    * clean unused imports
    
    * fix tests
    
    * include some indirect_peers in tests
    
    * actually don't include it bc of borsh
    
    * name fields in network_protocol::PeersResponse

--- chain/network/src/peer/peer_actor.rs
+++ chain/network/src/peer/peer_actor.rs
@@ -3,8 +3,8 @@ use crate::concurrency::atomic_cell::AtomicCell;
 use crate::concurrency::demux;
 use crate::network_protocol::{
     Edge, EdgeState, Encoding, OwnedAccount, ParsePeerMessageError, PartialEdgeInfo,
-    PeerChainInfoV2, PeerIdOrHash, PeerInfo, RawRoutedMessage, RoutedMessageBody, RoutedMessageV2,
-    RoutingTableUpdate, StateResponseInfo, SyncAccountsData,
+    PeerChainInfoV2, PeerIdOrHash, PeerInfo, PeersResponse, RawRoutedMessage, RoutedMessageBody,
+    RoutedMessageV2, RoutingTableUpdate, StateResponseInfo, SyncAccountsData,
 };
 use crate::peer::stream;
 use crate::peer::tracker::Tracker;
@@ -1117,21 +1117,31 @@ impl PeerActor {
                     .network_state
                     .peer_store
                     .healthy_peers(self.network_state.config.max_send_peers as usize);
-                if !peers.is_empty() {
-                    tracing::debug!(target: "network", "Peers request from {}: sending {} peers.", self.peer_info, peers.len());
-                    self.send_message_or_log(&PeerMessage::PeersResponse(peers));
+                let direct_peers = self.network_state.get_direct_peers();
+                if !peers.is_empty() || !direct_peers.is_empty() {
+                    tracing::debug!(target: "network", "Peers request from {}: sending {} peers and {} direct peers.", self.peer_info, peers.len(), direct_peers.len());
+                    self.send_message_or_log(&PeerMessage::PeersResponse(PeersResponse {
+                        peers,
+                        direct_peers,
+                    }));
                 }
                 self.network_state
                     .config
                     .event_sink
                     .push(Event::MessageProcessed(conn.tier, peer_msg));
             }
-            PeerMessage::PeersResponse(peers) => {
-                tracing::debug!(target: "network", "Received peers from {}: {} peers.", self.peer_info, peers.len());
+            PeerMessage::PeersResponse(PeersResponse { peers, direct_peers }) => {
+                tracing::debug!(target: "network", "Received peers from {}: {} peers and {} direct peers.", self.peer_info, peers.len(), direct_peers.len());
                 let node_id = self.network_state.config.node_id();
                 self.network_state.peer_store.add_indirect_peers(
                     &self.clock,
                     peers.into_iter().filter(|peer_info| peer_info.id != node_id),
+                );
+                // Direct peers of the responding peer are still indirect peers for this node.
+                // However, we may treat them with more trust in the future.
+                self.network_state.peer_store.add_indirect_peers(
+                    &self.clock,
+                    direct_peers.into_iter().filter(|peer_info| peer_info.id != node_id),
                 );
                 self.network_state
                     .config
