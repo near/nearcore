f8b21b7ce9bf62a3b9416591997e2004f987fc2e
posvyatokum <posvyatokum@edu.hse.ru>
2023-02-24 10:54:33 +0000

    adding script for handling migration to split storage

---
+++ scripts/split_storage_migration.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Takes one argument -- chain_id (mainnet or testnet)
+# First, prepares config files using jq.
+# Restarts neard and waits for cold head to sync with final head
+# Downloads latest roc db in hot-data
+# Stops neard, prepares rpc db to be used as hot db, changes config to look at hot db instead of archival db
+# Restarts neard
+# After that node is running in split storage mode
+# Legacy archival db can be removed
+
+NEAR_HOME=/home/ubuntu/.near
+chain=$1
+
+if [ "$chain" != "testnet" ] && [ "$chain" != "mainnet" ]
+then
+  echo "Chain should be 'mainnet' or 'testnet', got '$chain'"
+  exit 1
+fi
+
+# Prepare all configs
+function prepare_configs {
+  sudo apt update
+  sudo apt install jq -y
+
+  # make archival config
+  jq '.archive = true | .save_trie_changes=true' < $NEAR_HOME/config.json > $NEAR_HOME/config.json.archival
+
+  # create migration config that has cold_store part
+  jq '.cold_store = .store' < $NEAR_HOME/config.json.archival > $NEAR_HOME/config.json.migration
+
+  # create split storage config that points to the right hot storage
+  jq '.store.path = "hot-data"' < $NEAR_HOME/config.json.migration > $NEAR_HOME/config.json.split
+}
+
+# Initialize cold storage and make it up to date
+function init_cold_storage {
+  # Switch to migration mode
+  cp $NEAR_HOME/config.json.migration $NEAR_HOME/config.json
+  sudo systemctl restart neard
+
+  # Wait for migration success status
+  while true
+  do
+    head=$(curl 0.0.0.0:3030/metrics | grep 'block_height_head' | tail -n1 | awk '{print $2}')
+    cold_head=$(curl 0.0.0.0:3030/metrics | grep 'cold_head_height' | tail -n1 | awk '{print $2}')
+
+    echo "$head"
+    echo "$cold_head"
+
+    if [[ -n "$head" && -n "$cold_head" ]]
+    then
+      if ((head - cold_head < 1000))
+      then
+        break
+      fi
+    fi
+
+    sleep 120
+  done
+}
+
+# Download latest rpc backup
+function download_latest_rpc_backup {
+  echo 'Starting rpc download'
+  aws s3 --no-sign-request cp "s3://near-protocol-public/backups/$chain/rpc/latest" .
+  latest=$(cat latest)
+  aws s3 --no-sign-request cp --no-sign-request --recursive "s3://near-protocol-public/backups/$chain/rpc/$latest" $NEAR_HOME/hot-data
+}
+
+# Finish migration to split storage
+function finish_split_storage_migration {
+  # Change hot store type
+  echo 'Changing rpc db kind to hot'
+  sudo systemctl stop neard
+  /home/ubuntu/neard cold-store prepare-hot --store-relative-path='hot-data'
+
+  # Switch to split storage mode
+  echo 'Starting split storage run'
+  cp $NEAR_HOME/config.json.split $NEAR_HOME/config.json
+  sudo systemctl restart neard
+}
+
+# Backup old config.json
+cp $NEAR_HOME/config.json $NEAR_HOME/config.json.init
+prepare_configs
+init_cold_storage
+download_latest_rpc_backup
+finish_split_storage_migration
