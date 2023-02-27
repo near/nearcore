d08b15d60c70ab2188046f150f6d4ee97ed73441
posvyatokum <posvyatokum@edu.hse.ru>
2023-02-27 10:53:04 +0000

    run_with_trie_changes + check DbKind after migration + small fixes

--- scripts/split_storage_migration.sh
+++ scripts/split_storage_migration.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # Takes one argument -- chain_id (mainnet or testnet)
+# Prerequisites:
+# - systemd neard service
+# - neard home is in /home/ubuntu/.near
+# - neard binary is in /home/ubuntu/neard
+# - /home/ubuntu/.near/config.json is initialised
+# - metrics are exported to 3030 port
+# - aws s3 client
 # First, prepares config files using jq.
 # Restarts neard and waits for cold head to sync with final head
 # Downloads latest rpc db in hot-data
@@ -33,9 +40,20 @@ function prepare_configs {
   jq '.store.path = "hot-data"' < $NEAR_HOME/config.json.migration > $NEAR_HOME/config.json.split
 }
 
+# Run for 10 minutes with TrieChanges
+function run_with_trie_changes {
+  echo 'Starting an archival run with TrieChanges'
+  cp $NEAR_HOME/config.json.archival $NEAR_HOME/config.json
+  sudo systemctl restart neard
+
+  echo 'Waiting 10 minutes'
+  sleep 600
+}
+
 # Initialize cold storage and make it up to date
 function init_cold_storage {
   # Switch to migration mode
+  echo 'Starting initial migration run'
   cp $NEAR_HOME/config.json.migration $NEAR_HOME/config.json
   sudo systemctl restart neard
 
@@ -65,7 +83,7 @@ function download_latest_rpc_backup {
   echo 'Starting rpc download'
   aws s3 --no-sign-request cp "s3://near-protocol-public/backups/$chain/rpc/latest" .
   latest=$(cat latest)
-  aws s3 --no-sign-request cp --no-sign-request --recursive "s3://near-protocol-public/backups/$chain/rpc/$latest" $NEAR_HOME/hot-data
+  aws s3 --no-sign-request cp --recursive "s3://near-protocol-public/backups/$chain/rpc/$latest" $NEAR_HOME/hot-data
 }
 
 # Finish migration to split storage
@@ -79,11 +97,30 @@ function finish_split_storage_migration {
   echo 'Starting split storage run'
   cp $NEAR_HOME/config.json.split $NEAR_HOME/config.json
   sudo systemctl restart neard
+
+  sleep 300
+
+  hot_db_kind=$(curl \
+    -H "Content-Type: application/json" \
+    -X POST \
+    --data '{"jsonrpc":"2.0","method":"EXPERIMENTAL_split_storage_info","params":[],"id":"dontcare"}' \
+    0.0.0.0:3030 \
+    | jq '.result.hot_db_kind')
+
+  echo "Hot DbKind is '$hot_db_kind'"
+  if [ "$hot_db_kind" != '"Hot"' ]
+  then
+    "Hot DbKind is not 'Hot'"
+    exit 1
+  else
+    "Hot DbKind is correct"
+  fi
 }
 
 # Backup old config.json
 cp $NEAR_HOME/config.json $NEAR_HOME/config.json.init
 prepare_configs
+run_with_trie_changes
 init_cold_storage
 download_latest_rpc_backup
 finish_split_storage_migration
