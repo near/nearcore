0b6b772c8ddc6036391b902ec4bda21fa4388dc4
Andrei <122784628+andrei-near@users.noreply.github.com>
2023-02-28 10:23:39 +0000

    Feat: enabling runner and diffrunner fuzz targets (#8648)
    * enabling runner fuzz target
    
    * wasm dependency
    
    * lto=thin
    
    * adding all targets
    
    * raising exception if build failed
    
    * setting lto="thin"
    
    * loading contract code into binary
    
    * disabling unused fuzz targets
    
    * removing OnceCell lazy initialization

--- scripts/build_fuzzers.py
+++ scripts/build_fuzzers.py
@@ -12,6 +12,10 @@ G_BUCKET = 'gs://fuzzer_targets/master/'
 ARCH_CONFIG_NAME = 'x86_64-unknown-linux-gnu'
 BUILDER_START_TIME = time.strftime('%Y%m%d%H%M%S', time.gmtime())
 TAR_NAME = f'nearcore-master-{BUILDER_START_TIME}.tar.gz'
+ENV = os.environ
+# Default lto="fat" will cause rustc builder to use ~35GB of memory
+# This reduces memory usage to <10GB
+ENV.update({'RUSTFLAGS': "-C lto=thin"})
 
 
 def push_to_google_bucket(archive_name: str) -> None:
@@ -30,32 +34,26 @@ def push_to_google_bucket(archive_name: str) -> None:
 def main() -> None:
     fuzz_bin_list = []
 
-    crates = [
-        i for i in toml.load('nightly/fuzz.toml')['target']
-        # temporary limit for fuzz targets building
-        if i['runner'] in ['borsh', 'serde']
-    ]
+    crates = [i for i in toml.load('nightly/fuzz.toml')['target']]
     logger.debug(f"Crates from nightly/fuzz.toml: \n{crates}")
 
     for target in crates:
         logger.info(f"working on {target}")
         runner = target['runner']
-        try:
-            subprocess.run(
-                [
-                    'cargo',
-                    '+nightly',
-                    'fuzz',
-                    'build',
-                    runner,
-                ],
-                check=True,
-                cwd=target['crate'],
-            )
-            fuzz_bin_list.append(f"target/{ARCH_CONFIG_NAME}/release/{runner}")
 
-        except subprocess.CalledProcessError as e:
-            logger.info(f"Failed to build fuzz target: {target}")
+        subprocess.run(
+            [
+                'cargo',
+                '+nightly',
+                'fuzz',
+                'build',
+                runner,
+            ],
+            check=True,
+            cwd=target['crate'],
+            env=ENV,
+        )
+        fuzz_bin_list.append(f"target/{ARCH_CONFIG_NAME}/release/{runner}")
 
     logger.info(
         f"Fuzzer binaries to upload: {[os.path.basename(f) for f in fuzz_bin_list]}"
