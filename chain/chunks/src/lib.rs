c3df9e98086beebc43b81941735d90466837b70b
robin-near <robin@near.org>
2023-03-07 11:36:54 -0800

    [TestLoop] (9.1/n) Forgot to include the tests into the source tree.

--- chain/chunks/src/lib.rs
+++ chain/chunks/src/lib.rs
@@ -2004,6 +2004,9 @@ impl PartialEncodedChunkResponseSource {
 
 #[cfg(test)]
 mod test {
+    mod basic;
+    mod multi;
+
     use assert_matches::assert_matches;
     use near_async::messaging::IntoSender;
     use near_chain::types::EpochManagerAdapter;
