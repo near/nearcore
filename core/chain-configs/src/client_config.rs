8f8be446f864143043f6682ecb39bd5730447ac1
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-03-27 17:38:08 +0200

    fix: Remove not-used config fields.
    * protocol_upgrade_num_epochs
    * reduce_wait_for_missing_block
    * announce_account_horizon
    
    epoch_sync_enabled is also useless, will follow-up, untanglish it is too involved.
    
    #7842

--- core/chain-configs/src/client_config.rs
+++ core/chain-configs/src/client_config.rs
@@ -88,8 +88,6 @@ pub struct ClientConfig {
     pub max_block_production_delay: Duration,
     /// Maximum duration before skipping given height.
     pub max_block_wait_delay: Duration,
-    /// Duration to reduce the wait for each missed block by validator.
-    pub reduce_wait_for_missing_block: Duration,
     /// Skip waiting for sync (for testing or single node testnet).
     pub skip_sync_wait: bool,
     /// How often to check that we are not out of sync.
@@ -120,8 +118,6 @@ pub struct ClientConfig {
     pub epoch_length: BlockHeightDelta,
     /// Number of block producer seats
     pub num_block_producer_seats: NumSeats,
-    /// Maximum blocks ahead of us before becoming validators to announce account.
-    pub announce_account_horizon: BlockHeightDelta,
     /// Time to persist Accounts Id in the router without removing them.
     pub ttl_account_id_router: Duration,
     /// Horizon at which instead of fetching block, fetch full state.
@@ -209,7 +205,6 @@ impl ClientConfig {
             min_block_production_delay: Duration::from_millis(min_block_prod_time),
             max_block_production_delay: Duration::from_millis(max_block_prod_time),
             max_block_wait_delay: Duration::from_millis(3 * min_block_prod_time),
-            reduce_wait_for_missing_block: Duration::from_millis(0),
             skip_sync_wait,
             sync_check_period: Duration::from_millis(100),
             sync_step_period: Duration::from_millis(10),
@@ -224,7 +219,6 @@ impl ClientConfig {
             produce_empty_blocks: true,
             epoch_length: 10,
             num_block_producer_seats,
-            announce_account_horizon: 5,
             ttl_account_id_router: Duration::from_secs(60 * 60),
             block_fetch_horizon: 50,
             state_fetch_horizon: 5,
