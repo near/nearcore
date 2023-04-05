near/nearcore

commit 31da79079b68d3e3268a26bfb2c560e8069b1768

store: refactor handling of database migration (#7584)
Move logic of driving the database migration to StoreOpener.  This is
done by introducing StoreMigrator trait which is passed when creating
StoreOpener through a new NodeStorage::opener_with_migrator method.
Object implementing that trait contains code for performing migrations
one level at a time and StoreOpener is what is now checking the
database version and looping through all the migrations calling the
migrator.

The change is motivated by future plans of adding cold database to
NodeStorage.  With cold database, versions of two databases will need
to be checked and it’s cleanest to encapsulate all that logic in
StoreOpener.  This makes rest of the code pretty much unaware of how
versions are stored and only dedicated migration code needs to deal
with performing single-version migrations.

nearcore/src/migrations.rs
@@ -4,22 +4,25 @@ use near_primitives::runtime::migration_data::MigrationData;
 use near_primitives::types::Gas;
 use near_primitives::utils::index_to_bytes;
 use near_store::migrations::BatchedStoreUpdate;
-use near_store::{DBCol, Temperature};
+use near_store::version::{DbVersion, DB_VERSION};
+use near_store::{DBCol, NodeStorage, Temperature};
 
 /// Fix an issue with block ordinal (#5761)
 // This migration takes at least 3 hours to complete on mainnet
 pub fn migrate_30_to_31(
-    store_opener: &near_store::StoreOpener,
+    storage: &NodeStorage,
     near_config: &crate::NearConfig,
 ) -> anyhow::Result<()> {
-    let store = store_opener.open()?.get_store(Temperature::Hot);
+    let store = storage.get_store(Temperature::Hot);
     if near_config.client_config.archive && &near_config.genesis.config.chain_id == "mainnet" {
         do_migrate_30_to_31(&store, &near_config.genesis.config)?;
     }
-    near_store::version::set_store_version(&store, 31)?;
     Ok(())
 }
 
+/// Migrates the database from version 30 to 31.
+///
+/// Recomputes block ordinal due to a bug fixed in #5761.
 pub fn do_migrate_30_to_31(
     store: &near_store::Store,
     genesis_config: &near_chain_configs::GenesisConfig,
@@ -74,6 +77,84 @@ pub fn load_migration_data(chain_id: &str) -> MigrationData {
         } else {
             ReceiptResult::default()
         },
+    }
+}
+
+pub(super) struct Migrator<'a> {
+    config: &'a crate::config::NearConfig,
+}
+
+impl<'a> Migrator<'a> {
+    pub fn new(config: &'a crate::config::NearConfig) -> Self {
+        Self { config }
+    }
+}
+
+/// Asserts that node’s configuration does not use deprecated snapshot config
+/// options.
+///
+/// The `use_db_migration_snapshot` and `db_migration_snapshot_path`
+/// configuration options have been deprecated in favour of
+/// `store.migration_snapshot`.
+///
+/// This function panics if the old options are set and shows instruction how to
+/// migrate to the new options.
+///
+/// This is a hack which stops `StoreOpener` before it attempts migration.
+/// Ideally we would propagate errors nicely but that would complicate the API
+/// a wee bit so instead we’re panicking.  This shouldn’t be a big deal since
+/// the deprecated options are going away ‘soon’.
+fn assert_no_deprecated_config(config: &crate::config::NearConfig) {
+    use near_store::config::MigrationSnapshot;
+
+    let example = match (
+        config.config.use_db_migration_snapshot,
+        config.config.db_migration_snapshot_path.as_ref(),
+    ) {
+        (None, None) => return,
+        (Some(false), _) => MigrationSnapshot::Enabled(false),
+        (_, None) => MigrationSnapshot::Enabled(true),
+        (_, Some(path)) => MigrationSnapshot::Path(path.join("migration-snapshot")),
+    };
+    panic!(
+        "‘use_db_migration_snapshot’ and ‘db_migration_snapshot_path’ options \
+         are deprecated.\nSet ‘store.migration_snapshot’ to instead, e.g.:\n{}",
+        example.format_example()
+    )
+}
+
+impl<'a> near_store::StoreMigrator for Migrator<'a> {
+    fn check_support(&self, version: DbVersion) -> Result<(), &'static str> {
+        assert_no_deprecated_config(self.config);
+        // TODO(mina86): Once open ranges in match are stabilised, get rid of
+        // this constant and change the match to be 27..DB_VERSION.
+        const LAST_SUPPORTED: DbVersion = DB_VERSION - 1;
+        match version {
+            0..=26 => Err("1.26"),
+            27..=LAST_SUPPORTED => Ok(()),
+            _ => unreachable!(),
+        }
+    }
+
+    fn migrate(&self, storage: &NodeStorage, version: DbVersion) -> Result<(), anyhow::Error> {
+        match version {
+            0..=26 => unreachable!(),
+            27 => {
+                // version 27 => 28: add DBCol::StateChangesForSplitStates
+                //
+                // Does not need to do anything since open db with option
+                // `create_missing_column_families`.  Nevertheless need to bump
+                // db version, because db_version 27 binary can't open
+                // db_version 28 db.  Combine it with migration from 28 to 29;
+                // don’t do anything here.
+                Ok(())
+            }
+            28 => near_store::migrations::migrate_28_to_29(storage),
+            29 => near_store::migrations::migrate_29_to_30(storage),
+            30 => migrate_30_to_31(storage, &self.config),
+            31 => near_store::migrations::migrate_31_to_32(storage),
+            DB_VERSION.. => unreachable!(),
+        }
     }
 }
 

core/store/src/config.rs
@@ -111,6 +111,26 @@ impl Mode {
     pub const fn must_create(self) -> bool {
         matches!(self, Mode::Create)
     }
+
+    /// Returns variant of the mode which prohibits creation of the database or
+    /// `None` if the mode requires creation of a new database.
+    pub const fn but_cannot_create(self) -> Option<Self> {
+        match self {
+            Self::ReadOnly | Self::ReadWriteExisting => Some(self),
+            Self::ReadWrite => Some(Self::ReadWriteExisting),
+            Self::Create => None,
+        }
+    }
+
+    /// Returns variant of the mode which requires creation of the database or
+    /// `None` if the mode prohibits creation of a new database.
+    pub const fn but_must_create(self) -> Option<Self> {
+        match self {
+            Self::ReadOnly => None,
+            Self::ReadWriteExisting => None,
+            Self::ReadWrite | Self::Create => Some(Self::Create),
+        }
+    }
 }
 
 impl StoreConfig {

core/store/src/migrations.rs
@@ -7,8 +7,7 @@ use near_primitives::hash::CryptoHash;
 use near_primitives::types::validator_stake::ValidatorStake;
 use near_primitives::types::AccountId;
 
-use crate::version::set_store_version;
-use crate::{DBCol, Store, StoreOpener, StoreUpdate};
+use crate::{DBCol, Store, StoreUpdate};
 
 pub struct BatchedStoreUpdate<'a> {
     batch_size_limit: usize,
@@ -71,18 +70,22 @@ where
     store_update.finish()
 }
 
-pub fn migrate_28_to_29(store_opener: &StoreOpener) -> anyhow::Result<()> {
-    let store = store_opener.open().unwrap().get_store(crate::Temperature::Hot);
-    let mut store_update = store.store_update();
-    store_update.delete_all(DBCol::_NextBlockWithNewChunk);
-    store_update.delete_all(DBCol::_LastBlockWithNewChunk);
-    store_update.commit()?;
-
-    set_store_version(&store, 29)?;
+/// Migrates database from version 28 to 29.
+///
+/// Deletes all data from _NextBlockWithNewChunk and _LastBlockWithNewChunk
+/// columns.
+pub fn migrate_28_to_29(storage: &crate::NodeStorage) -> anyhow::Result<()> {
+    let mut update = storage.get_store(crate::Temperature::Hot).store_update();
+    update.delete_all(DBCol::_NextBlockWithNewChunk);
+    update.delete_all(DBCol::_LastBlockWithNewChunk);
+    update.commit()?;
     Ok(())
 }
 
-pub fn migrate_29_to_30(store_opener: &StoreOpener) -> anyhow::Result<()> {
+/// Migrates database from version 29 to 30.
+///
+/// Migrates all structures that use ValidatorStake to versionized version.
+pub fn migrate_29_to_30(storage: &crate::NodeStorage) -> anyhow::Result<()> {
     use near_primitives::epoch_manager::block_info::BlockInfo;
     use near_primitives::epoch_manager::epoch_info::EpochSummary;
     use near_primitives::epoch_manager::AGGREGATOR_KEY;
@@ -94,7 +97,7 @@ pub fn migrate_29_to_30(store_opener: &StoreOpener) -> anyhow::Result<()> {
     };
     use std::collections::BTreeMap;
 
-    let store = store_opener.open()?.get_store(crate::Temperature::Hot);
+    let store = storage.get_store(crate::Temperature::Hot);
 
     #[derive(BorshDeserialize)]
     pub struct OldEpochSummary {
@@ -169,17 +172,12 @@ pub fn migrate_29_to_30(store_opener: &StoreOpener) -> anyhow::Result<()> {
     }
 
     store_update.finish()?;
-
-    set_store_version(&store, 30)?;
     Ok(())
 }
 
-pub fn migrate_31_to_32(store_opener: &StoreOpener) -> anyhow::Result<()> {
-    let store = store_opener.open().unwrap().get_store(crate::Temperature::Hot);
-    let mut store_update = store.store_update();
-    store_update.delete_all(DBCol::_ChunkPerHeightShard);
-    store_update.commit()?;
-
-    set_store_version(&store, 32)?;
+pub fn migrate_31_to_32(storage: &crate::NodeStorage) -> anyhow::Result<()> {
+    let mut update = storage.get_store(crate::Temperature::Hot).store_update();
+    update.delete_all(DBCol::_ChunkPerHeightShard);
+    update.commit()?;
     Ok(())
 }

core/store/src/db/rocksdb/snapshot.rs
@@ -16,11 +16,11 @@ use ::rocksdb::checkpoint::Checkpoint;
 /// informational messages pointing where the snapshot resides and how to
 /// recover data from it.
 #[derive(Debug)]
-pub struct Snapshot(Option<std::path::PathBuf>);
+pub(crate) struct Snapshot(Option<std::path::PathBuf>);
 
 /// Possible errors when creating a checkpoint.
 #[derive(Debug)]
-pub enum SnapshotError {
+pub(crate) enum SnapshotError {
     /// Snapshot at requested location already exists.
     ///
     /// More specifically, the specified path exists (since the code does only
@@ -55,21 +55,31 @@ impl std::convert::From<::rocksdb::Error> for SnapshotError {
 
 impl Snapshot {
     /// Creates a Snapshot object which represents lack of a snapshot.
-    pub const fn no_snapshot() -> Self {
+    pub const fn none() -> Self {
         Self(None)
     }
 
-    /// Creates a new snapshot for given database.
+    /// Possibly creates a new snapshot for given database.
     ///
-    /// If the snapshot already exists, returns [`SnapshotError::AlreadyExists`]
-    /// error.  (More specifically, if the `snapshot_path` already exists since
-    /// the method does not make any more sophisticated checks whether the path
-    /// contains a snapshot or not).
-    pub(crate) fn new(
+    /// If the snapshot is disabled via `config.migration_snapshot` option,
+    /// a ‘no snapshot’ object is returned.  It can be thought as `None` but
+    /// `remove` method can be called on it so it’s tiny bit more ergonomic.
+    ///
+    /// Otherwise, path to the snapshot is determined from `config` taking as
+    /// `db_path` as the base directory for relative paths.  If the snapshot
+    /// already exists, the function returns [`SnapshotError::AlreadyExists`]
+    /// error.  (More specifically, if the path already exists since the method
+    /// does not make any more sophisticated checks whether the path contains
+    /// a snapshot or not).
+    pub fn new(
         db_path: &std::path::Path,
         config: &crate::StoreConfig,
-        snapshot_path: std::path::PathBuf,
     ) -> Result<Self, SnapshotError> {
+        let snapshot_path = match config.migration_snapshot.get_path(db_path) {
+            Some(snapshot_path) => snapshot_path,
+            None => return Ok(Self::none()),
+        };
+
         tracing::info!(target: "db", snapshot_path=%snapshot_path.display(),
                        "Creating database snapshot");
         if snapshot_path.exists() {
@@ -85,7 +95,7 @@ impl Snapshot {
 
     /// Deletes the checkpoint from the file system.
     ///
-    /// Does nothing if the object has been created via [`Self::no_snapshot`].
+    /// Does nothing if the object has been created via [`Self::none`].
     pub fn remove(mut self) -> Result<(), SnapshotRemoveError> {
         if let Some(path) = self.0.take() {
             tracing::info!(target: "db", snapshot_path=%path.display(),
@@ -94,11 +104,6 @@ impl Snapshot {
         } else {
             Ok(())
         }
-    }
-
-    /// Returns path to the snapshot if it exists.
-    pub fn path(&self) -> Option<&std::path::Path> {
-        self.0.as_deref()
     }
 }
 
@@ -126,24 +131,29 @@ fn test_snapshot_creation() {
     use assert_matches::assert_matches;
 
     let (_tmpdir, opener) = crate::NodeStorage::test_opener();
+    let new = || Snapshot::new(&opener.path(), &opener.config());
+
+    // Creating snapshot fails if database doesn’t exist.
+    let err = format!("{:?}", new().unwrap_err());
+    assert!(err.contains("create_if_missing is false"), "{err:?}");
 
     // Create the database
     core::mem::drop(opener.open().unwrap());
 
     // Creating snapshot should work now.
-    let snapshot = opener.new_migration_snapshot().unwrap();
+    let snapshot = new().unwrap();
 
     // Snapshot already exists so cannot create a new one.
-    assert_matches!(opener.new_migration_snapshot(), Err(SnapshotError::AlreadyExists(_)));
+    assert_matches!(new().unwrap_err(), SnapshotError::AlreadyExists(_));
 
     snapshot.remove().unwrap();
 
     // This should work correctly again since the snapshot has been removed.
-    opener.new_migration_snapshot().unwrap();
+    core::mem::drop(new().unwrap());
 
     // And this again should fail.  We don’t remove the snapshot in
     // Snapshot::drop.
-    assert_matches!(opener.new_migration_snapshot(), Err(SnapshotError::AlreadyExists(_)));
+    assert_matches!(new().unwrap_err(), SnapshotError::AlreadyExists(_));
 }
 
 /// Tests that reading data from a snapshot is possible.
@@ -153,7 +163,6 @@ fn test_snapshot_recovery() {
     const COL: crate::DBCol = crate::DBCol::BlockMisc;
 
     let (tmpdir, opener) = crate::NodeStorage::test_opener();
-    let path = opener.config().migration_snapshot.get_path(opener.path()).unwrap();
 
     // Populate some data
     {
@@ -164,7 +173,8 @@ fn test_snapshot_recovery() {
     }
 
     // Create snapshot
-    let snapshot = opener.new_migration_snapshot().unwrap();
+    let snapshot = Snapshot::new(&opener.path(), &opener.config()).unwrap();
+    let path = snapshot.0.clone().unwrap();
 
     // Delete the data from the database.
     {
@@ -180,7 +190,7 @@ fn test_snapshot_recovery() {
     {
         let mut config = opener.config().clone();
         config.path = Some(path);
-        let opener = crate::StoreOpener::new(tmpdir.path(), &config);
+        let opener = crate::NodeStorage::opener(tmpdir.path(), &config);
         let store = opener.open().unwrap().get_store(crate::Temperature::Hot);
         assert_eq!(Some(&b"value"[..]), store.get(COL, KEY).unwrap().as_deref());
     }

core/store/src/lib.rs
@@ -25,7 +25,7 @@ use near_primitives::trie_key::{trie_key_parsers, TrieKey};
 use near_primitives::types::{AccountId, CompiledContractCache, StateRoot};
 
 use crate::db::{
-    refcount, DBIterator, DBOp, DBSlice, DBTransaction, Database, RocksDB, StoreStatistics,
+    refcount, DBIterator, DBOp, DBSlice, DBTransaction, Database, StoreStatistics,
     GENESIS_JSON_HASH_KEY, GENESIS_STATE_ROOTS_KEY,
 };
 pub use crate::trie::iterator::TrieIterator;
@@ -49,8 +49,7 @@ mod trie;
 pub mod version;
 
 pub use crate::config::{Mode, StoreConfig};
-pub use crate::db::rocksdb::snapshot::{Snapshot, SnapshotError};
-pub use crate::opener::StoreOpener;
+pub use crate::opener::{StoreMigrator, StoreOpener, StoreOpenerError};
 
 /// Specifies temperature of a storage.
 ///
@@ -102,7 +101,7 @@ impl NodeStorage {
     pub fn test_opener() -> (tempfile::TempDir, StoreOpener<'static>) {
         static CONFIG: Lazy<StoreConfig> = Lazy::new(StoreConfig::test_config);
         let dir = tempfile::tempdir().unwrap();
-        let opener = Self::opener(dir.path(), &CONFIG);
+        let opener = StoreOpener::new(dir.path(), &CONFIG);
         (dir, opener)
     }
 
@@ -152,9 +151,9 @@ impl NodeStorage {
     /// well.  For example, garbage collection only ever touches hot storage but
     /// it should go through [`Store`] interface since data it manipulates
     /// (e.g. blocks) are live in both databases.
-    pub fn get_inner(&self, temp: Temperature) -> Arc<dyn Database> {
+    pub fn get_inner(&self, temp: Temperature) -> &Arc<dyn Database> {
         match temp {
-            Temperature::Hot => self.storage.clone(),
+            Temperature::Hot => &self.storage,
         }
     }
 

commit 20d1dccae5957c408d38721129eabcac7c1aaad2

Remove deprecated db migration options (#8885)
`assert_no_deprecated_config()` was introduced in #7584, more than 6 months ago.
Should be safe to delete.

nearcore/src/config.rs
@@ -1,26 +1,10 @@
-use anyhow::{anyhow, bail, Context};
-use near_primitives::static_clock::StaticClock;
-use near_primitives::test_utils::create_test_signer;
-use num_rational::Rational32;
-use std::fs;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
-use std::str::FromStr;
-use std::sync::Arc;
-use std::time::Duration;
-
-use near_config_utils::{ValidationError, ValidationErrors};
-
-#[cfg(test)]
-use tempfile::tempdir;
-use tracing::{info, warn};
-
 use crate::download_file::{run_download_file, FileDownloadError};
+use anyhow::{anyhow, bail, Context};
 use near_chain_configs::{
     get_initial_supply, ClientConfig, GCConfig, Genesis, GenesisConfig, GenesisValidationMode,
     LogSummaryStyle, MutableConfigValue,
 };
+use near_config_utils::{ValidationError, ValidationErrors};
 use near_crypto::{InMemorySigner, KeyFile, KeyType, PublicKey, Signer};
 #[cfg(feature = "json_rpc")]
 use near_jsonrpc::RpcConfig;
@@ -32,6 +16,8 @@ use near_primitives::hash::CryptoHash;
 use near_primitives::shard_layout::account_id_to_shard_id;
 use near_primitives::shard_layout::ShardLayout;
 use near_primitives::state_record::StateRecord;
+use near_primitives::static_clock::StaticClock;
+use near_primitives::test_utils::create_test_signer;
 use near_primitives::types::{
     AccountId, AccountInfo, Balance, BlockHeight, BlockHeightDelta, Gas, NumBlocks, NumSeats,
     NumShards, ShardId,
@@ -42,6 +28,17 @@ use near_primitives::version::PROTOCOL_VERSION;
 #[cfg(feature = "rosetta_rpc")]
 use near_rosetta_rpc::RosettaRpcConfig;
 use near_telemetry::TelemetryConfig;
+use num_rational::Rational32;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
+#[cfg(test)]
+use tempfile::tempdir;
+use tracing::{info, warn};
 
 /// Initial balance used in tests.
 pub const TESTING_INIT_BALANCE: Balance = 1_000_000_000 * NEAR_BASE;
@@ -327,17 +324,8 @@ pub struct Config {
     /// Configuration for the
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub split_storage: Option<SplitStorageConfig>,
-    // TODO(mina86): Remove those two altogether at some point.  We need to be
-    // somewhat careful though and make sure that we don’t start silently
-    // ignoring this option without users setting corresponding store option.
-    // For the time being, we’re failing inside of create_db_checkpoint if this
-    // option is set.
-    /// Deprecated; use `store.migration_snapshot` instead.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub use_db_migration_snapshot: Option<bool>,
-    /// Deprecated; use `store.migration_snapshot` instead.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub db_migration_snapshot_path: Option<PathBuf>,
+    /// The node will stop after the head exceeds this height.
+    /// The node usually stops within several seconds after reaching the target height.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub expected_shutdown: Option<BlockHeight>,
     /// Options for dumping state of every epoch to S3.
@@ -377,8 +365,6 @@ impl Default for Config {
             view_client_throttle_period: default_view_client_throttle_period(),
             trie_viewer_state_size_limit: default_trie_viewer_state_size_limit(),
             max_gas_burnt_view: None,
-            db_migration_snapshot_path: None,
-            use_db_migration_snapshot: None,
             store: near_store::StoreConfig::default(),
             cold_store: None,
             split_storage: None,

nearcore/src/migrations.rs
@@ -86,42 +86,8 @@ impl<'a> Migrator<'a> {
     }
 }
 
-/// Asserts that node’s configuration does not use deprecated snapshot config
-/// options.
-///
-/// The `use_db_migration_snapshot` and `db_migration_snapshot_path`
-/// configuration options have been deprecated in favour of
-/// `store.migration_snapshot`.
-///
-/// This function panics if the old options are set and shows instruction how to
-/// migrate to the new options.
-///
-/// This is a hack which stops `StoreOpener` before it attempts migration.
-/// Ideally we would propagate errors nicely but that would complicate the API
-/// a wee bit so instead we’re panicking.  This shouldn’t be a big deal since
-/// the deprecated options are going away ‘soon’.
-fn assert_no_deprecated_config(config: &crate::config::NearConfig) {
-    use near_store::config::MigrationSnapshot;
-
-    let example = match (
-        config.config.use_db_migration_snapshot,
-        config.config.db_migration_snapshot_path.as_ref(),
-    ) {
-        (None, None) => return,
-        (Some(false), _) => MigrationSnapshot::Enabled(false),
-        (_, None) => MigrationSnapshot::Enabled(true),
-        (_, Some(path)) => MigrationSnapshot::Path(path.join("migration-snapshot")),
-    };
-    panic!(
-        "‘use_db_migration_snapshot’ and ‘db_migration_snapshot_path’ options \
-         are deprecated.\nSet ‘store.migration_snapshot’ to instead, e.g.:\n{}",
-        example.format_example()
-    )
-}
-
 impl<'a> near_store::StoreMigrator for Migrator<'a> {
     fn check_support(&self, version: DbVersion) -> Result<(), &'static str> {
-        assert_no_deprecated_config(self.config);
         // TODO(mina86): Once open ranges in match are stabilised, get rid of
         // this constant and change the match to be 27..DB_VERSION.
         const LAST_SUPPORTED: DbVersion = DB_VERSION - 1;