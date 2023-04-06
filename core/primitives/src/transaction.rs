bb5b4b5d1e9908b34e967f2f85e16f7fc78d9b3f
Jakob Meier <mail@jakobmeier.ch>
2023-04-06 16:28:38 +0200

    refactor: pull o11y/pretty into its own crate (#8894)
    This is a first step toward reducing dependencies in near-primitives.
    See #8888 for context.
    
    `near-o11y` has too many dependencies to be pulled into crates like
    `near-primitives` that only need it for pretty printing.
    
    The new crate `near-fmt` is light-weight, it only depends on
    `near-primitives-core` and contains only the pretty printing code.

--- core/primitives/src/transaction.rs
+++ core/primitives/src/transaction.rs
@@ -7,7 +7,7 @@ use crate::serialize::{base64_format, dec_format};
 use crate::types::{AccountId, Balance, Gas, Nonce};
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_crypto::{PublicKey, Signature};
-use near_o11y::pretty;
+use near_fmt::{AbbrBytes, Slice};
 use near_primitives_core::profile::{ProfileDataV2, ProfileDataV3};
 use near_primitives_core::types::Compute;
 use std::borrow::Borrow;
@@ -132,7 +132,7 @@ impl From<DeployContractAction> for Action {
 impl fmt::Debug for DeployContractAction {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("DeployContractAction")
-            .field("code", &format_args!("{}", pretty::AbbrBytes(&self.code)))
+            .field("code", &format_args!("{}", AbbrBytes(&self.code)))
             .finish()
     }
 }
@@ -159,7 +159,7 @@ impl fmt::Debug for FunctionCallAction {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("FunctionCallAction")
             .field("method_name", &format_args!("{}", &self.method_name))
-            .field("args", &format_args!("{}", pretty::AbbrBytes(&self.args)))
+            .field("args", &format_args!("{}", AbbrBytes(&self.args)))
             .field("gas", &format_args!("{}", &self.gas))
             .field("deposit", &format_args!("{}", &self.deposit))
             .finish()
@@ -350,7 +350,7 @@ impl fmt::Debug for ExecutionStatus {
             ExecutionStatus::Unknown => f.write_str("Unknown"),
             ExecutionStatus::Failure(e) => f.write_fmt(format_args!("Failure({})", e)),
             ExecutionStatus::SuccessValue(v) => {
-                f.write_fmt(format_args!("SuccessValue({})", pretty::AbbrBytes(v)))
+                f.write_fmt(format_args!("SuccessValue({})", AbbrBytes(v)))
             }
             ExecutionStatus::SuccessReceiptId(receipt_id) => {
                 f.write_fmt(format_args!("SuccessReceiptId({})", receipt_id))
@@ -458,8 +458,8 @@ impl Default for ExecutionMetadata {
 impl fmt::Debug for ExecutionOutcome {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ExecutionOutcome")
-            .field("logs", &pretty::Slice(&self.logs))
-            .field("receipt_ids", &pretty::Slice(&self.receipt_ids))
+            .field("logs", &Slice(&self.logs))
+            .field("receipt_ids", &Slice(&self.receipt_ids))
             .field("burnt_gas", &self.gas_burnt)
             .field("compute_usage", &self.compute_usage.unwrap_or_default())
             .field("tokens_burnt", &self.tokens_burnt)
--- runtime/near-vm-logic/src/logic.rs
+++ runtime/near-vm-logic/src/logic.rs
@@ -2409,7 +2409,7 @@ impl<'a> VMLogic<'a> {
 
         near_o11y::io_trace!(
             storage_op = "write",
-            key = %near_o11y::pretty::Bytes(&key),
+            key = %near_fmt::Bytes(&key),
             size = value_len,
             evicted_len = evicted.as_ref().map(Vec::len),
             tn_mem_reads = nodes_delta.mem_reads,
@@ -2518,7 +2518,7 @@ impl<'a> VMLogic<'a> {
 
         near_o11y::io_trace!(
             storage_op = "read",
-            key = %near_o11y::pretty::Bytes(&key),
+            key = %near_fmt::Bytes(&key),
             size = read.as_ref().map(Vec::len),
             tn_db_reads = nodes_delta.db_reads,
             tn_mem_reads = nodes_delta.mem_reads,
@@ -2590,7 +2590,7 @@ impl<'a> VMLogic<'a> {
 
         near_o11y::io_trace!(
             storage_op = "remove",
-            key = %near_o11y::pretty::Bytes(&key),
+            key = %near_fmt::Bytes(&key),
             evicted_len = removed.as_ref().map(Vec::len),
             tn_mem_reads = nodes_delta.mem_reads,
             tn_db_reads = nodes_delta.db_reads,
@@ -2655,7 +2655,7 @@ impl<'a> VMLogic<'a> {
 
         near_o11y::io_trace!(
             storage_op = "exists",
-            key = %near_o11y::pretty::Bytes(&key),
+            key = %near_fmt::Bytes(&key),
             tn_mem_reads = nodes_delta.mem_reads,
             tn_db_reads = nodes_delta.db_reads,
         );
