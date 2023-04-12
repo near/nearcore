ea69a4eb48dafb0cd0672cf9ee8bccfb9f99795d
robin-near <robin@near.org>
2023-04-12 10:29:31 -0700

    [Debug UI] Dockerize debug UI project for deployment.

--- tools/debug-ui/src/index.tsx
+++ tools/debug-ui/src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import '@patternfly/react-core/dist/styles/base.css';
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { App } from './App';
 import { LogVisualizer } from './log_visualizer/LogVisualizer';
+import { LandingPage } from './LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -16,7 +17,7 @@ const router = createBrowserRouter([
         // Root path redirects to the current host - this is useful if we serve the app from the node
         // itself.
         path: '/',
-        element: <Navigate to={'/' + window.location.host} />,
+        element: <LandingPage />,
     },
     {
         path: '/logviz',
