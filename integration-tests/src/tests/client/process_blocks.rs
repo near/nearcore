e6097302a2d85daa86fe984d25fc570e029feb2e
Ekleog-NEAR <96595974+Ekleog-NEAR@users.noreply.github.com>
2023-04-18 13:13:19 +0200

    add a protocol version running near-vm (#8912)
    This closes up the near-vm work. I believe https://github.com/near/nearcore/pull/8886#discussion_r1159879325 is the only remaining point to be checked in former PRs, in addition to all the code added in this PR.
    
    Note that there are still things in #8323 that are not in this PR, because either I noticed they actually should not be merged (eg. the wasmparser bump would be a protocol upgrade, so will wait until we get limited replayability), or because they are actually orthogonal to this landing.

--- integration-tests/src/tests/client/process_blocks.rs
+++ integration-tests/src/tests/client/process_blocks.rs
@@ -2858,11 +2858,11 @@ fn test_execution_metadata() {
         "cost": "CONTRACT_LOADING_BYTES",
         "gas_used": "18423750"
       },
-      // We spend two wasm instructions (call & drop).
+      // We spend two wasm instructions (call & drop), plus 8 ops for initializing the stack.
       {
         "cost_category": "WASM_HOST_COST",
         "cost": "WASM_INSTRUCTION",
-        "gas_used": (config.wasm_config.regular_op_cost as u64 * 2).to_string()
+        "gas_used": (config.wasm_config.regular_op_cost as u64 * 10).to_string()
       }
     ]);
     let outcome = &execution_outcome.receipts_outcome[0].outcome;
