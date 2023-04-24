5ca01a1e009359be49fb22db0ec69b81a1677159
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-04-24 13:56:59 +0200

    verbosity

--- core/o11y/src/testonly.rs
+++ core/o11y/src/testonly.rs
@@ -44,6 +44,6 @@ pub fn init_test_module_logger(module: &str) {
 }
 
 pub fn init_integration_logger() {
-    let env_filter = EnvFilter::new("actix_web=warn,info");
+    let env_filter = EnvFilter::new("actix_web=warn,info,sync=debug,state_sync_dump=debug"); // FIXME // TODO
     setup_subscriber_from_filter(env_filter);
 }
