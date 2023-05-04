03caaec9988a195e2038cac4c27493ac4e879cce
Jakob Meier <mail@jakobmeier.ch>
2023-05-04 21:24:53 +0200

    revert: near-vm and finite-wasm changes (#9009)
    The fuzzer found an issue with finite-wasm, and we are currently
    investigating unexpected gas changes due to near-vm.
    
    Revert both changes for now so they aren't included in v1.34.

--- runtime/runtime-params-estimator/src/main.rs
+++ runtime/runtime-params-estimator/src/main.rs
@@ -61,7 +61,7 @@ struct CliArgs {
     #[clap(long, default_value = "time", possible_values = &["icount", "time"])]
     metric: String,
     /// Which VM to test.
-    #[clap(long, possible_values = &["wasmer", "wasmer2", "wasmtime", "near-vm"])]
+    #[clap(long, possible_values = &["wasmer", "wasmer2", "wasmtime"])]
     vm_kind: Option<String>,
     /// Render existing `costs.txt` as `RuntimeConfig`.
     #[clap(long)]
@@ -289,7 +289,6 @@ fn run_estimation(cli_args: CliArgs) -> anyhow::Result<Option<CostTable>> {
         Some("wasmer") => VMKind::Wasmer0,
         Some("wasmer2") => VMKind::Wasmer2,
         Some("wasmtime") => VMKind::Wasmtime,
-        Some("near-vm") => VMKind::NearVm,
         None => VMKind::for_protocol_version(PROTOCOL_VERSION),
         Some(other) => unreachable!("Unknown vm_kind {}", other),
     };
