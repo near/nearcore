869aed02816e9626a1f9024d9b4d7db41d01f4d2
Simonas Kazlauskas <git@kazlauskas.me>
2023-05-15 18:03:48 +0300

    Fix a test

--- runtime/runtime-params-estimator/src/main.rs
+++ runtime/runtime-params-estimator/src/main.rs
@@ -512,7 +512,7 @@ mod tests {
             fs_keys_per_delta: 1,
             skip_build_test_contract: false,
             metric: "time".to_owned(),
-            vm_kind: None,
+            vm_kind: VMKind::for_protocol_version(PROTOCOL_VERSION),
             costs_file: None,
             compare_to: None,
             costs: Some(costs),
