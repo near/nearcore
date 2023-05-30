a635290c672b3343388ece9a22a1643f485612ab
Aleksandr Logunov <alex.logunov@near.org>
2023-05-30 16:25:06 +0400

    fix: tests for invalid state parts (#9122)
    Check that invalid state parts don't pass validation. Add missing `StorageError` kind for one of test cases. Now we check two scenarios:
    * MissingTrieValue - should be returned if some trie value is missing during validation - e.g. some intermediate node wasn't sent;
    * UnexpectedTrieValue - should be returned if some extra trie value was included to state part. Necessary to check, otherwise malicious actor can spam nodes with large amounts of non-existent nodes.

--- core/store/src/trie/mod.rs
+++ core/store/src/trie/mod.rs
@@ -1261,7 +1261,7 @@ mod tests {
 
         assert_eq!(trie3.get(b"dog"), Ok(Some(b"puppy".to_vec())));
         assert_eq!(trie3.get(b"horse"), Ok(Some(b"stallion".to_vec())));
-        assert_eq!(trie3.get(b"doge"), Err(StorageError::TrieNodeMissing));
+        assert_eq!(trie3.get(b"doge"), Err(StorageError::MissingTrieValue));
     }
 
     #[test]
--- nearcore/src/runtime/mod.rs
+++ nearcore/src/runtime/mod.rs
@@ -1198,7 +1198,7 @@ impl RuntimeAdapter for NightshadeRuntime {
     fn validate_state_part(&self, state_root: &StateRoot, part_id: PartId, data: &[u8]) -> bool {
         match BorshDeserialize::try_from_slice(data) {
             Ok(trie_nodes) => {
-                match Trie::validate_trie_nodes_for_part(state_root, part_id, trie_nodes) {
+                match Trie::validate_state_part(state_root, part_id, trie_nodes) {
                     Ok(_) => true,
                     // Storage error should not happen
                     Err(err) => {
