63dc63b6197df8bb3972385d5056c22591de5923
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-06-01 11:16:34 +0200

    clippy

--- nearcore/src/runtime/mod.rs
+++ nearcore/src/runtime/mod.rs
@@ -1963,7 +1963,7 @@ mod test {
             let epoch_manager = EpochManager::new_arc_handle(store.clone(), &genesis.config);
             let runtime = NightshadeRuntime::new(
                 dir.path(),
-                store.clone(),
+                store,
                 &genesis,
                 epoch_manager.clone(),
                 None,
