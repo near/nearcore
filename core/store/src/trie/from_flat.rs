6488519c7a12554a137d90f9b4a12a83e2fc77d8
Shreyan Gupta <shreyan@near.org>
2023-06-07 15:42:13 -0700

    Separate write location

--- core/store/src/trie/from_flat.rs
+++ core/store/src/trie/from_flat.rs
@@ -1,29 +1,18 @@
 use crate::flat::{store_helper, FlatStorageManager};
-use crate::{ShardTries, Store, Trie, TrieConfig};
+use crate::{ShardTries, Store, Trie, TrieConfig, TrieDBStorage, TrieStorage};
 use near_primitives::{shard_layout::ShardUId, state::FlatStateValue};
 use std::time::Instant;
 
-pub fn construct_trie_from_flat(store: Store, shard_uid: ShardUId) {
+pub fn construct_trie_from_flat(store: Store, write_store: Store, shard_uid: ShardUId) {
     let timer = Instant::now();
-
-    let new_shard_uid = ShardUId { version: shard_uid.version, shard_id: 10 };
-    let trie_config = TrieConfig::default();
-    let flat_storage_manager = FlatStorageManager::new(store.clone());
-    let tries = ShardTries::new(
-        store.clone(),
-        trie_config,
-        &[shard_uid, new_shard_uid],
-        flat_storage_manager,
-    );
-    let trie = tries.get_view_trie_for_shard(shard_uid, Trie::EMPTY_ROOT);
-
+    let trie_storage = TrieDBStorage::new(store.clone(), shard_uid);
     let mut num_entries = 0;
     let entries =
         store_helper::iter_flat_state_entries(shard_uid, &store, None, None).map(|entry| {
             let (key, value) = entry.unwrap();
             let value = match value {
                 FlatStateValue::Ref(ref_value) => {
-                    trie.storage.retrieve_raw_bytes(&ref_value.hash).unwrap().to_vec()
+                    trie_storage.retrieve_raw_bytes(&ref_value.hash).unwrap().to_vec()
                 }
                 FlatStateValue::Inlined(inline_value) => inline_value,
             };
@@ -36,8 +25,13 @@ pub fn construct_trie_from_flat(store: Store, shard_uid: ShardUId) {
             (key, Some(value))
         });
 
-    let mut store_update = tries.store_update();
-    let new_trie = tries.get_trie_for_shard(new_shard_uid, Trie::EMPTY_ROOT);
+    let tries = ShardTries::new(
+        write_store.clone(),
+        TrieConfig::default(),
+        &[shard_uid],
+        FlatStorageManager::new(write_store.clone()),
+    );
+    let new_trie = tries.get_trie_for_shard(shard_uid, Trie::EMPTY_ROOT);
     let trie_changes = new_trie.update(entries).unwrap();
 
     println!(
@@ -47,7 +41,8 @@ pub fn construct_trie_from_flat(store: Store, shard_uid: ShardUId) {
         timer.elapsed()
     );
 
-    tries.apply_all(&trie_changes, new_shard_uid, &mut store_update);
+    let mut store_update = tries.store_update();
+    tries.apply_all(&trie_changes, shard_uid, &mut store_update);
     store_update.commit().unwrap();
 
     println!("Committed at time {:?}", timer.elapsed());
--- tools/flat-storage/src/commands.rs
+++ tools/flat-storage/src/commands.rs
@@ -50,6 +50,7 @@ enum SubCommand {
 #[derive(Parser)]
 pub struct ConstructTriedFromFlatCmd {
     shard_id: u64,
+    write_store_path: PathBuf,
 }
 
 #[derive(Parser)]
@@ -345,10 +346,19 @@ impl FlatStorageCommand {
                     near_store::Mode::ReadWriteExisting,
                 );
 
+                let write_opener = NodeStorage::opener(
+                    &cmd.write_store_path,
+                    false,
+                    &near_config.config.store,
+                    None,
+                );
+                let write_node_storage = write_opener.open_in_mode(Mode::Create).unwrap();
+                let write_store = write_node_storage.get_hot_store();
+
                 let tip = chain_store.final_head().unwrap();
                 let shard_uid = epoch_manager.shard_id_to_uid(cmd.shard_id, &tip.epoch_id)?;
 
-                construct_trie_from_flat(store, shard_uid);
+                construct_trie_from_flat(store, write_store, shard_uid);
             }
         }
 
