faba85cf4e8c58a0d6511ae77e8b374bb65d2ff4
Saketh Are <saketh.are@gmail.com>
2023-06-09 14:12:43 -0400

    fall back to V2 if V1 doesn't have route

--- chain/network/src/peer_manager/network_state/routing.rs
+++ chain/network/src/peer_manager/network_state/routing.rs
@@ -150,7 +150,10 @@ impl NetworkState {
     ) -> Result<PeerId, FindRouteError> {
         match target {
             PeerIdOrHash::PeerId(peer_id) => {
-                self.graph.routing_table.find_next_hop_for_target(peer_id)
+                match self.graph.routing_table.find_next_hop_for_target(peer_id) {
+                    Ok(peer_id) => Ok(peer_id),
+                    Err(_) => self.graph_v2.routing_table.find_next_hop_for_target(peer_id),
+                }
             }
             PeerIdOrHash::Hash(hash) => self
                 .tier2_route_back
