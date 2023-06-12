950a42b8e5e6e18000a3169fa12f851ddc4772db
Andrei Kashin <kashin.andrej@gmail.com>
2023-06-12 14:54:08 +0100

    Add more documentation

--- nearcore/src/config.rs
+++ nearcore/src/config.rs
@@ -340,6 +340,10 @@ pub struct Config {
     pub state_sync: Option<StateSyncConfig>,
     /// Limit of the size of per-shard transaction pool measured in bytes. If not set, the size
     /// will be unbounded.
+    /// New transactions that bring the size of the pool over this limit will be rejected. This
+    /// guarantees that the node will use bounded resources to store incoming transactinos.
+    /// Setting this value too low (<1MB) on the validator might lead to production of smaller
+    /// chunks and underutilizing the capacity of the network.
     #[serde(default = "default_transaction_pool_size_limit")]
     pub transaction_pool_size_limit: Option<u64>,
 }
