ba6e3b2c1937160638dc4f079c562a12bd006d09
Shreyan Gupta <shreyan@near.org>
2023-06-12 16:29:05 -0700

    more logging, size_limit 250 mb

--- core/store/src/trie/from_flat.rs
+++ core/store/src/trie/from_flat.rs
@@ -27,30 +27,34 @@ pub fn construct_trie_from_flat(store: Store, write_store: Store, shard_uid: Sha
     );
     let mut trie_root = Trie::EMPTY_ROOT;
 
+    println!("{:.2?} : Starting processing batches at time ", timer.elapsed());
     while let Some(batch) = get_batch(&mut entries) {
+        let inner_timer = Instant::now();
+
         // Apply and commit changes
-        let batch_count = batch.len();
+        let count = batch.len();
         let new_trie = tries.get_trie_for_shard(shard_uid, trie_root);
         let trie_changes = new_trie.update(batch).unwrap();
+
+        println!("{:.2?} : {} trie update {:.2?}", timer.elapsed(), count, inner_timer.elapsed());
+
         let mut store_update = tries.store_update();
         tries.apply_all(&trie_changes, shard_uid, &mut store_update);
         store_update.commit().unwrap();
         trie_root = trie_changes.new_root;
 
-        println!("Processing batch count {} at time {:?}", batch_count, timer.elapsed());
+        println!("{:.2?} : store update {:.2?}", timer.elapsed(), inner_timer.elapsed());
     }
 
-    println!("Completed with root {} at time {:?}", trie_root, timer.elapsed());
+    println!("{:.2?} : Completed with root {}", timer.elapsed(), trie_root);
 }
 
 fn get_batch(
     entries: &mut impl Iterator<Item = (Vec<u8>, Option<Vec<u8>>)>,
 ) -> Option<Vec<(Vec<u8>, Option<Vec<u8>>)>> {
-    let size_limit = 2 * 1000_000_000; // 2 GB
-    let vec_capacity = (2.2 * 1000_000_000.0) as usize; // 2.2 GB
-
+    let size_limit = 250 * 1000_000; // 250 MB
     let mut size = 0;
-    let mut partial_entries = Vec::with_capacity(vec_capacity);
+    let mut partial_entries = Vec::new();
     while let Some(entry) = entries.next() {
         size += entry.0.len() + entry.1.as_ref().unwrap().len();
         partial_entries.push(entry);
