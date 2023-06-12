f545274a3d35f165cdf98a1274d79b04f93c30b6
Shreyan Gupta <shreyan@near.org>
2023-06-12 14:12:29 -0700

    break into batches

--- core/store/src/trie/from_flat.rs
+++ core/store/src/trie/from_flat.rs
@@ -6,8 +6,8 @@ use std::time::Instant;
 pub fn construct_trie_from_flat(store: Store, write_store: Store, shard_uid: ShardUId) {
     let timer = Instant::now();
     let trie_storage = TrieDBStorage::new(store.clone(), shard_uid);
-    let mut num_entries = 0;
-    let entries =
+
+    let mut entries =
         store_helper::iter_flat_state_entries(shard_uid, &store, None, None).map(|entry| {
             let (key, value) = entry.unwrap();
             let value = match value {
@@ -16,12 +16,6 @@ pub fn construct_trie_from_flat(store: Store, write_store: Store, shard_uid: Sha
                 }
                 FlatStateValue::Inlined(inline_value) => inline_value,
             };
-
-            num_entries += 1;
-            if num_entries % 500 == 0 {
-                println!("Processed {} entries at time {:?}", num_entries, timer.elapsed());
-            }
-
             (key, Some(value))
         });
 
@@ -31,19 +25,42 @@ pub fn construct_trie_from_flat(store: Store, write_store: Store, shard_uid: Sha
         &[shard_uid],
         FlatStorageManager::new(write_store.clone()),
     );
-    let new_trie = tries.get_trie_for_shard(shard_uid, Trie::EMPTY_ROOT);
-    let trie_changes = new_trie.update(entries).unwrap();
+    let mut trie_root = Trie::EMPTY_ROOT;
 
-    println!(
-        "Done processing {} entries. New root {} processed at time {:?}",
-        num_entries,
-        trie_changes.new_root,
-        timer.elapsed()
-    );
+    while let Some(batch) = get_batch(&mut entries) {
+        // Apply and commit changes
+        let batch_count = batch.len();
+        let new_trie = tries.get_trie_for_shard(shard_uid, trie_root);
+        let trie_changes = new_trie.update(batch).unwrap();
+        let mut store_update = tries.store_update();
+        tries.apply_all(&trie_changes, shard_uid, &mut store_update);
+        store_update.commit().unwrap();
+        trie_root = trie_changes.new_root;
 
-    let mut store_update = tries.store_update();
-    tries.apply_all(&trie_changes, shard_uid, &mut store_update);
-    store_update.commit().unwrap();
+        println!("Processing batch count {} at time {:?}", batch_count, timer.elapsed());
+    }
 
-    println!("Committed at time {:?}", timer.elapsed());
+    println!("Completed with root {} at time {:?}", trie_root, timer.elapsed());
+}
+
+fn get_batch(
+    entries: &mut impl Iterator<Item = (Vec<u8>, Option<Vec<u8>>)>,
+) -> Option<Vec<(Vec<u8>, Option<Vec<u8>>)>> {
+    let size_limit = 2 * 1000_000_000; // 2 GB
+    let vec_capacity = (2.2 * 1000_000_000.0) as usize; // 2.2 GB
+
+    let mut size = 0;
+    let mut partial_entries = Vec::with_capacity(vec_capacity);
+    while let Some(entry) = entries.next() {
+        size += entry.0.len() + entry.1.as_ref().unwrap().len();
+        partial_entries.push(entry);
+        if size > size_limit {
+            break;
+        }
+    }
+    if partial_entries.is_empty() {
+        None
+    } else {
+        Some(partial_entries)
+    }
 }
