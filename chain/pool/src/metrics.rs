0df29fc927f0fe3b3a0c8be9cbb0e42efa4c7241
Andrei Kashin <kashin.andrej@gmail.com>
2023-06-13 15:08:26 +0100

    Update metric description

--- chain/pool/src/metrics.rs
+++ chain/pool/src/metrics.rs
@@ -4,7 +4,7 @@ use once_cell::sync::Lazy;
 pub static TRANSACTION_POOL_COUNT: Lazy<IntGaugeVec> = Lazy::new(|| {
     near_o11y::metrics::try_create_int_gauge_vec(
         "near_transaction_pool_entries",
-        "Total number of transactions currently in the pools tracked by the node",
+        "Total number of transactions currently tracked by the node in a given shard pool",
         &["shard_id"],
     )
     .unwrap()
@@ -13,7 +13,7 @@ pub static TRANSACTION_POOL_COUNT: Lazy<IntGaugeVec> = Lazy::new(|| {
 pub static TRANSACTION_POOL_SIZE: Lazy<IntGaugeVec> = Lazy::new(|| {
     near_o11y::metrics::try_create_int_gauge_vec(
         "near_transaction_pool_size",
-        "Total size in bytes of transactions currently in the pools tracked by the node",
+        "Total size in bytes of transactions currently tracked by the node in a given shard pool",
         &["shard_id"],
     )
     .unwrap()
