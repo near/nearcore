924f537689ef5960a1a66dd1d62400a9c6e8f9e1
Andrei Kashin <kashin.andrej@gmail.com>
2023-06-15 21:38:03 +0200

    refactor: Store contract deps alongside the code (#9197)
    To simplify running benchmark, we pull all necessary WASM contracts and put them in res/ folder. We don't expect them to change often and this will greatly simplify running the workload.
    If we do need to update them, script `download_contracts.sh` should help with this.

--- pytest/tests/loadtest/locust/common/social.py
+++ pytest/tests/loadtest/locust/common/social.py
@@ -305,8 +305,7 @@ def on_locust_init(environment, **kwargs):
 def _(parser):
     parser.add_argument(
         "--social-db-wasm",
-        type=str,
-        required=False,
+        default="res/social_db.wasm",
         help=
         "Path to the compiled SocialDB contract, get it from https://github.com/NearSocial/social-db/tree/aa7fafaac92a7dd267993d6c210246420a561370/res"
     )
