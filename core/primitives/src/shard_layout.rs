2fdc2d9d4da4d0daa0f5234d4ef10337e1ac475a
wacban <wac.banasik@gmail.com>
2023-06-19 15:35:16 +0100

    feat: remove fixed shards from shard layout

--- core/primitives/src/shard_layout.rs
+++ core/primitives/src/shard_layout.rs
@@ -75,9 +75,7 @@ type ShardSplitMap = Vec<Vec<ShardId>>;
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct ShardLayoutV1 {
-    /// num_shards = fixed_shards.len() + boundary_accounts.len() + 1
-    /// Each account and all sub-accounts map to the shard of position in this array.
-    fixed_shards: Vec<AccountId>,
+    /// TODO(wacban) better description.
     /// The rest are divided by boundary_accounts to ranges, each range is mapped to a shard
     boundary_accounts: Vec<AccountId>,
     /// Maps shards from the last shard layout to shards that it splits to in this shard layout,
@@ -109,14 +107,13 @@ impl ShardLayout {
 
     /// Return a V1 Shardlayout
     pub fn v1(
-        fixed_shards: Vec<AccountId>,
         boundary_accounts: Vec<AccountId>,
         shards_split_map: Option<ShardSplitMap>,
         version: ShardVersion,
     ) -> Self {
         let to_parent_shard_map = if let Some(shards_split_map) = &shards_split_map {
             let mut to_parent_shard_map = HashMap::new();
-            let num_shards = (fixed_shards.len() + boundary_accounts.len() + 1) as NumShards;
+            let num_shards = (boundary_accounts.len() + 1) as NumShards;
             for (parent_shard_id, shard_ids) in shards_split_map.iter().enumerate() {
                 for &shard_id in shard_ids {
                     let prev = to_parent_shard_map.insert(shard_id, parent_shard_id as ShardId);
@@ -129,7 +126,6 @@ impl ShardLayout {
             None
         };
         Self::V1(ShardLayoutV1 {
-            fixed_shards,
             boundary_accounts,
             shards_split_map,
             to_parent_shard_map,
@@ -140,8 +136,7 @@ impl ShardLayout {
     /// Returns a V1 ShardLayout. It is only used in tests
     pub fn v1_test() -> Self {
         ShardLayout::v1(
-            vec!["test0"].into_iter().map(|s| s.parse().unwrap()).collect(),
-            vec!["abc", "foo"].into_iter().map(|s| s.parse().unwrap()).collect(),
+            vec!["abc", "foo", "test0"].into_iter().map(|s| s.parse().unwrap()).collect(),
             Some(vec![vec![0, 1, 2, 3]]),
             1,
         )
@@ -150,7 +145,6 @@ impl ShardLayout {
     /// Returns the simple nightshade layout that we use in production
     pub fn get_simple_nightshade_layout() -> ShardLayout {
         ShardLayout::v1(
-            vec![],
             vec!["aurora", "aurora-0", "kkuuue2akv_1630967379.near"]
                 .into_iter()
                 .map(|s| s.parse().unwrap())
@@ -212,7 +206,7 @@ impl ShardLayout {
     pub fn num_shards(&self) -> NumShards {
         match self {
             Self::V0(v0) => v0.num_shards,
-            Self::V1(v1) => (v1.fixed_shards.len() + v1.boundary_accounts.len() + 1) as NumShards,
+            Self::V1(v1) => (v1.boundary_accounts.len() + 1) as NumShards,
         }
     }
 
@@ -225,11 +219,6 @@ impl ShardLayout {
 /// Maps an account to the shard that it belongs to given a shard_layout
 /// For V0, maps according to hash of account id
 /// For V1, accounts are divided to ranges, each range of account is mapped to a shard.
-/// There are also some fixed shards, each of which is mapped to an account and all sub-accounts.
-///     For example, for ShardLayoutV1{ fixed_shards: ["aurora"], boundary_accounts: ["near"]}
-///     Account "aurora" and all its sub-accounts will be mapped to shard_id 0.
-///     For the rest of accounts, accounts <= "near" will be mapped to shard_id 1 and
-///     accounts > "near" will be mapped shard_id 2.
 pub fn account_id_to_shard_id(account_id: &AccountId, shard_layout: &ShardLayout) -> ShardId {
     match shard_layout {
         ShardLayout::V0(ShardLayoutV0 { num_shards, .. }) => {
@@ -237,13 +226,10 @@ pub fn account_id_to_shard_id(account_id: &AccountId, shard_layout: &ShardLayout
             let (bytes, _) = stdx::split_array::<32, 8, 24>(hash.as_bytes());
             u64::from_le_bytes(*bytes) % num_shards
         }
-        ShardLayout::V1(ShardLayoutV1 { fixed_shards, boundary_accounts, .. }) => {
-            for (shard_id, fixed_account) in fixed_shards.iter().enumerate() {
-                if is_top_level_account(fixed_account, account_id) {
-                    return shard_id as ShardId;
-                }
-            }
-            let mut shard_id = fixed_shards.len() as ShardId;
+        ShardLayout::V1(ShardLayoutV1 { boundary_accounts, .. }) => {
+            // TODO As we scale up the number of shards we can consider changing
+            // that to a binary search.
+            let mut shard_id = 0 as ShardId;
             for boundary_account in boundary_accounts {
                 if boundary_account.cmp(account_id) == Greater {
                     break;
@@ -261,13 +247,6 @@ pub fn account_id_to_shard_uid(account_id: &AccountId, shard_layout: &ShardLayou
         account_id_to_shard_id(account_id, shard_layout),
         shard_layout,
     )
-}
-
-fn is_top_level_account(top_account: &AccountId, account: &AccountId) -> bool {
-    match account.as_ref().strip_suffix(top_account.as_ref()) {
-        None => false,
-        Some(rest) => rest.is_empty() || rest.ends_with('.'),
-    }
 }
 
 /// ShardUId is an unique representation for shards from different shard layout
