e0d74ada8b18f180191c2d13c274fa6c0e7c909c
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-06-19 18:32:01 +0200

    fix test-mock-node

--- tools/mock-node/src/setup.rs
+++ tools/mock-node/src/setup.rs
@@ -228,7 +228,7 @@ pub fn setup_mock_node(
                     tracing::info!(
                         target: "mock_node",
                         "Done {}/{} parts for shard {}",
-                        finished_parts_count.load(Ordering::SeqCst) + 1,
+                        finished_parts_count.load(Ordering::SeqCst),
                         num_parts,
                         shard_id,
                     );
@@ -293,6 +293,8 @@ mod tests {
     use std::ops::ControlFlow;
     use std::sync::{Arc, RwLock};
     use std::time::Duration;
+    use near_chain::{ChainStore, ChainStoreAccess};
+    use near_epoch_manager::{EpochManager, EpochManagerAdapter};
 
     // Just a test to test that the basic mocknet setup works
     // This test first starts a localnet with one validator node that generates 20 blocks
@@ -370,6 +372,7 @@ mod tests {
                             }
 
                             if block.header.height >= epoch_length * 2 + 2 {
+                                tracing::info!(block_height = ?block.header.height, expected = epoch_length * 2 + 2, "Time to stop");
                                 System::current().stop()
                             }
                         }
@@ -388,16 +391,33 @@ mod tests {
         let dir1 = tempfile::Builder::new().prefix("test1").tempdir().unwrap();
         let mut near_config1 = load_test_config("", tcp::ListenerAddr::reserve_for_test(), genesis);
         near_config1.client_config.min_num_peers = 1;
-        near_config1.client_config.tracked_shards =
-            (0..near_config1.genesis.config.shard_layout.num_shards()).collect();
+        near_config1.client_config.tracked_shards = vec![0]; // Track all shards.
         let network_config = MockNetworkConfig::with_delay(Duration::from_millis(10));
+
+        let client_start_height = {
+            let store = near_store::NodeStorage::opener(dir.path(), near_config1.config.archive, &near_config1.config.store, None)
+                .open()
+                .unwrap()
+                .get_hot_store();
+            let epoch_manager = EpochManager::new_arc_handle(store.clone(), &near_config1.genesis.config);
+            let chain_store = ChainStore::new(
+                store.clone(),
+                near_config1.genesis.config.genesis_height,
+                near_config1.client_config.save_trie_changes,
+            );
+            let network_head_hash = chain_store.head().unwrap().last_block_hash;
+            let last_epoch_start_height  = epoch_manager.get_epoch_start_height(&network_head_hash).unwrap();
+            // Needs to be the last block of an epoch.
+            last_epoch_start_height - 1
+        };
+
         run_actix(async {
             let MockNode { rpc_client, .. } = setup_mock_node(
                 dir1.path(),
                 dir.path(),
                 near_config1,
                 &network_config,
-                epoch_length * 2,
+                client_start_height,
                 None,
                 None,
                 false,
@@ -405,7 +425,7 @@ mod tests {
             );
             wait_or_timeout(100, 60000, || async {
                 if let Ok(status) = rpc_client.status().await {
-                    if status.sync_info.latest_block_height >= epoch_length * 2 {
+                    if status.sync_info.latest_block_height >= client_start_height {
                         System::current().stop();
                         return ControlFlow::Break(());
                     }
