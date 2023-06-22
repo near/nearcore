2262d5a202d003937e55e2510a88a48d0770c6eb
mercepluka <luka@calimero.network>
2023-06-22 16:58:21 +0200

    debug

--- core/chain-configs/src/config_store.rs
+++ core/chain-configs/src/config_store.rs
@@ -10,7 +10,6 @@ use std::path::Path;
 use serde_json;
 use std::env;
 
-//pub const RESOURCES_DIR: &str = "./core/chain-configs/res/";
 pub const RESOURCES_DIR: &str = "core/chain-configs/res/";
 pub const CONFIG_CHANGE_FILENAME: &str = "config_change_list.json";
 
@@ -29,17 +28,6 @@ pub struct ChainConfigStore {
 }
 
 impl ChainConfigStore {
-    /*
-    let mut file = File::open(path).expect("Failed to open genesis config file.");
-        let mut json_str = String::new();
-        file.read_to_string(&mut json_str)
-            .expect("Failed to read the genesis config file to string. ");
-        let json_str_without_comments: String =
-            near_config_utils::strip_comments_from_json_str(&json_str)
-                .expect("Failed to strip comments from Genesis config file.");
-        serde_json::from_str(&json_str_without_comments)
-            .expect("Failed to deserialize the genesis records.")
-     */
     pub fn new(genesis_config_loader: GenesisConfigLoader) -> Self {
         let initial_chain_config = Arc::new(ChainConfig::new(genesis_config_loader));
         let mut store = BTreeMap::new();
@@ -50,13 +38,11 @@ impl ChainConfigStore {
 
     fn populate_config_store(store: &BTreeMap<EpochHeight, Arc<ChainConfigLoader>>) {
         let current_dir = env::current_dir().expect("Failed to get the current directory");
-        let path1 = current_dir.join(RESOURCES_DIR);
-        let path2 = path1.join(CONFIG_CHANGE_FILENAME);
+        let path = current_dir.join(RESOURCES_DIR).join(CONFIG_CHANGE_FILENAME);
         println!("Mirko: {:?}", current_dir);
-        println!("Mirko: {:?}", path1);
-        println!("Mirko: {:?}", path2);
+        println!("Mirko: {:?}", path);
         println!("Mirko: {:?}", env::current_dir().unwrap());
-        let mut file = File::open(&path2).expect("Failed to open config change list file.");
+        let mut file = File::open(&path).expect("Failed to open config change list file.");
         let mut json_str = String::new();
         file.read_to_string(&mut json_str)
             .expect("Failed to read the config change list file to string. ");
