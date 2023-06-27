46b0ee5b628484898e8cdd535f2c72550a50bbbc
mercepluka <luka@calimero.network>
2023-06-27 10:18:35 +0200

    fixed naming

--- core/chain-configs/src/config_store.rs
+++ core/chain-configs/src/config_store.rs
@@ -10,13 +10,13 @@ use serde_json;
 use std::env;
 
 pub const RESOURCES_DIR: &str = "core/chain-configs/res/";
-pub const CONFIG_CHANGE_FILENAME: &str = "config_change_list.json";
+pub const CONFIG_PATCH_LIST_FILENAME: &str = "config_patch_list.json";
 
-/// Stores epochs when new config changes should be applied.
-/// This list is used to load files with config changes.
+/// Stores epochs when new config patches should be applied.
+/// This list is used to load files with config patches.
 #[derive(Debug, serde::Deserialize)]
-pub struct ConfigChangeList {
-    epoch_changes: Vec<EpochHeight>,
+pub struct ConfigPatchList {
+    epoch_patches: Vec<EpochHeight>,
 }
 
 /// Stores chain config for each epoch where it was updated.
@@ -37,9 +37,9 @@ impl ChainConfigStore {
     }
 
     fn populate_config_store(store: &mut BTreeMap<EpochHeight, Arc<ChainConfigPatch>>) {
-        let config_change_list = Self::get_config_change_list();
+        let config_patch_list = Self::get_config_patch_list();
 
-        for epoch_height in config_change_list.epoch_changes {
+        for epoch_height in config_patch_list.epoch_patches {
             let chain_config_patch = Self::load_chain_config(epoch_height);
             store.insert(epoch_height, Arc::new(chain_config_patch));
         }
@@ -48,29 +48,29 @@ impl ChainConfigStore {
     fn load_chain_config(epoch_height: EpochHeight) -> ChainConfigPatch {
         let current_dir = env::current_dir().expect("Failed to get the current directory");
         let path = current_dir.join(RESOURCES_DIR).join(epoch_height.to_string() + ".json");
-        let mut file = File::open(&path).expect("Failed to open config change file.");
+        let mut file = File::open(&path).expect("Failed to open chain config patch file.");
         let mut json_str = String::new();
         file.read_to_string(&mut json_str)
-            .expect("Failed to read the config change file to string. ");
+            .expect("Failed to read the chain config patch file to string. ");
         let json_str_without_comments: String =
             near_config_utils::strip_comments_from_json_str(&json_str)
-                .expect("Failed to strip comments from config change file.");
+                .expect("Failed to strip comments from chain config patch file.");
         serde_json::from_str(&json_str_without_comments)
-            .expect("Failed to deserialize the config change file.")
+            .expect("Failed to deserialize the chain config patch file.")
     }
 
-    fn get_config_change_list() -> ConfigChangeList {
+    fn get_config_patch_list() -> ConfigPatchList {
         let current_dir = env::current_dir().expect("Failed to get the current directory");
-        let path = current_dir.join(RESOURCES_DIR).join(CONFIG_CHANGE_FILENAME);
-        let mut file = File::open(&path).expect("Failed to open config change list file.");
+        let path = current_dir.join(RESOURCES_DIR).join(CONFIG_PATCH_LIST_FILENAME);
+        let mut file = File::open(&path).expect("Failed to open config patch list file.");
         let mut json_str = String::new();
         file.read_to_string(&mut json_str)
-            .expect("Failed to read the config change list file to string. ");
+            .expect("Failed to read the config patch list file to string. ");
         let json_str_without_comments: String =
             near_config_utils::strip_comments_from_json_str(&json_str)
-                .expect("Failed to strip comments from config change list file.");
+                .expect("Failed to strip comments from config patch list file.");
         serde_json::from_str(&json_str_without_comments)
-            .expect("Failed to deserialize the config change list file.")
+            .expect("Failed to deserialize the config patch list file.")
     }
 
     pub fn from_chain_config(chain_config: ChainConfig) -> Self {
@@ -90,8 +90,8 @@ impl ChainConfigStore {
     /// Returns initial config with applied all relevat config overrides for provided epoch.
     pub fn get_config(&self, epoch_height: EpochHeight) -> Arc<ChainConfig> {
         let mut config = self.initial_chain_config.as_ref().clone();
-        for (_, config_change_patch) in self.store.range(..=epoch_height) {
-            config = config.apply_patch(config_change_patch.as_ref());
+        for (_, config_patch) in self.store.range(..=epoch_height) {
+            config = config.apply_patch(config_patch.as_ref());
         }
         Arc::new(config)
     }
