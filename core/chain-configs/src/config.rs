ef3d9fe612f4fe4cb537d2ff72a5e6c3fc524b7e
mercepluka <luka@calimero.network>
2023-06-27 09:58:14 +0200

    fixed parsing errors

--- core/chain-configs/src/config.rs
+++ core/chain-configs/src/config.rs
@@ -55,22 +55,22 @@ impl ChainConfigPatch {
     pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, ValidationError> {
         let mut file = File::open(&path).map_err(|_| ValidationError::GenesisFileError {
             error_message: format!(
-                "Could not open chain config file at path {}.",
+                "Could not open chain config patch file at path {}.",
                 &path.as_ref().display()
             ),
         })?;
         let mut json_str = String::new();
         file.read_to_string(&mut json_str).map_err(|_| ValidationError::GenesisFileError {
-            error_message: "Failed to read chain config file to string. ".to_string(),
+            error_message: "Failed to read chain config patch file to string. ".to_string(),
         })?;
         let json_str_without_comments = near_config_utils::strip_comments_from_json_str(&json_str)
             .map_err(|_| ValidationError::GenesisFileError {
-                error_message: "Failed to strip comments from chain config file".to_string(),
+                error_message: "Failed to strip comments from chain config patch file".to_string(),
             })?;
         let chain_config =
             serde_json::from_str::<ChainConfigPatch>(&json_str_without_comments).map_err(|_| {
                 ValidationError::GenesisFileError {
-                    error_message: "Failed to deserialize the chain config records.".to_string(),
+                    error_message: "Failed to deserialize the chain config patch records.".to_string(),
                 }
             })?;
         Ok(chain_config)
