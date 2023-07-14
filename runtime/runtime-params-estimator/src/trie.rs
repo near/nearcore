d5471533441d54b47735bf13086b50cdf49594af
Jakob Meier <mail@jakobmeier.ch>
2023-07-14 12:01:08 +0200

    chore(estimator): remove TTN read estimation
    Since we have flat storage for reads, we no longer charge for touched
    trie nodes (TTN) on reads.
    Remove the gas estimation for it.
    
    More specifically, we used to estimate TTN cost as
    `max(read_ttn, write_ttn)` and therefore had 3 numbers reported.
    (read, write, combined).
    Now we only need a single number reported.

--- runtime/runtime-params-estimator/src/trie.rs
+++ runtime/runtime-params-estimator/src/trie.rs
@@ -69,59 +69,6 @@ pub(crate) fn write_node(
     cost
 }
 
-pub(crate) fn read_node_from_db(
-    ctx: &mut EstimatorContext,
-    warmup_iters: usize,
-    measured_iters: usize,
-    final_key_len: usize,
-) -> GasCost {
-    let block_latency = 0;
-    let overhead = overhead_per_measured_block(ctx, block_latency);
-    let mut testbed = ctx.testbed();
-    let tb = testbed.transaction_builder();
-    // Prepare a long chain in the trie
-    let signer = tb.random_account();
-    let key = "j".repeat(final_key_len);
-    let mut setup_block = Vec::new();
-    for key_len in 0..final_key_len {
-        let key = &key.as_bytes()[..key_len];
-        let value = b"0";
-        setup_block.push(tb.account_insert_key(signer.clone(), key, value));
-    }
-    let mut blocks = Vec::with_capacity(1 + 2 * warmup_iters + 2 * measured_iters);
-    blocks.push(setup_block);
-    blocks.extend(
-        iter::repeat_with(|| vec![tb.account_has_key(signer.clone(), &key[0..1])])
-            .take(measured_iters + warmup_iters),
-    );
-    blocks.extend(
-        iter::repeat_with(|| vec![tb.account_has_key(signer.clone(), &key)])
-            .take(measured_iters + warmup_iters),
-    );
-    let results = &testbed.measure_blocks(blocks, block_latency)[1..];
-    let (short_key_results, long_key_results) = results.split_at(measured_iters + warmup_iters);
-    let (cost_short_key, ext_cost_short_key) = aggregate_per_block_measurements(
-        1,
-        short_key_results[warmup_iters..].to_vec(),
-        Some(overhead.clone()),
-    );
-    let (cost_long_key, ext_cost_long_key) = aggregate_per_block_measurements(
-        1,
-        long_key_results[warmup_iters..].to_vec(),
-        Some(overhead),
-    );
-    let nodes_touched_delta = ext_cost_long_key[&ExtCosts::touching_trie_node]
-        - ext_cost_short_key[&ExtCosts::touching_trie_node];
-    // The exact number of touched nodes is a implementation that we don't want
-    // to test here but it should be close to 2*final_key_len
-    assert!(nodes_touched_delta as usize <= 2 * final_key_len + 10);
-    assert!(nodes_touched_delta as usize >= 2 * final_key_len - 10);
-    let cost_delta =
-        cost_long_key.saturating_sub(&cost_short_key, &NonNegativeTolerance::PER_MILLE);
-    let cost = cost_delta / nodes_touched_delta;
-    cost
-}
-
 pub(crate) fn read_node_from_chunk_cache(testbed: &mut Testbed) -> GasCost {
     let debug = testbed.config.debug;
     let iters = 200;
