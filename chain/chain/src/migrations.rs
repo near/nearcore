f1145fe8a68087491eea76356ac074476cd18668
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-07-18 14:37:16 +0200

    fix(state-sync): Apply state parts via state-viewer

--- chain/chain/src/migrations.rs
+++ chain/chain/src/migrations.rs
@@ -24,20 +24,26 @@ pub fn check_if_block_is_first_with_chunk_of_version(
     prev_block_hash: &CryptoHash,
     shard_id: ShardId,
 ) -> Result<bool, Error> {
+    tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #1");
     // Check that block belongs to the first epoch with current protocol version
     // to avoid get_epoch_id_of_last_block_with_chunk call in the opposite case
     if is_first_epoch_with_protocol_version(epoch_manager, prev_block_hash)? {
+        tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #2");
         // Compare only epochs because we already know that current epoch is the first one with current protocol version
         // convert shard id to shard id of previous epoch because number of shards may change
         let shard_id = epoch_manager.get_prev_shard_ids(prev_block_hash, vec![shard_id])?[0];
+        tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #3");
         let prev_epoch_id = chain_store.get_epoch_id_of_last_block_with_chunk(
             epoch_manager,
             prev_block_hash,
             shard_id,
         )?;
+        tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #4");
         let epoch_id = epoch_manager.get_epoch_id_from_prev_block(prev_block_hash)?;
+        tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #5");
         Ok(prev_epoch_id != epoch_id)
     } else {
+        tracing::error!(?prev_block_hash, ?shard_id, "check_if_block_is_first_with_chunk_of_version #6");
         Ok(false)
     }
 }
