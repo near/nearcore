dae314b941e062931953446f358c81884f87e8a0
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-07-24 18:59:43 +0200

    fmt

--- tools/database/src/run_migrations.rs
+++ tools/database/src/run_migrations.rs
@@ -10,7 +10,7 @@ impl RunMigrationsCommand {
             near_chain_configs::GenesisValidationMode::UnsafeFast,
         )
         .unwrap_or_else(|e| panic!("Error loading config: {:#}", e));
-        nearcore::open_storage(home_dir, near_config)?;
+        nearcore::open_storage(home_dir, &mut near_config)?;
         Ok(())
     }
 }
