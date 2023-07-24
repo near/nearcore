bef509cdf8ec79cf7ddb69ce36dfec204329e263
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-07-24 18:59:28 +0200

    fmt

--- tools/database/src/run_migrations.rs
+++ tools/database/src/run_migrations.rs
@@ -4,11 +4,12 @@ use std::path::Path;
 pub(crate) struct RunMigrationsCommand {}
 
 impl RunMigrationsCommand {
-    pub(crate) fn run(
-        &self,
-        home_dir: &Path,
-        near_config: &mut nearcore::NearConfig,
-    ) -> anyhow::Result<()> {
+    pub(crate) fn run(&self, home_dir: &Path) -> anyhow::Result<()> {
+        let mut near_config = nearcore::config::load_config(
+            &home,
+            near_chain_configs::GenesisValidationMode::UnsafeFast,
+        )
+        .unwrap_or_else(|e| panic!("Error loading config: {:#}", e));
         nearcore::open_storage(home_dir, near_config)?;
         Ok(())
     }
