855097247ef9cf78efd7068b82a149febc007c28
nikurt <86772482+nikurt@users.noreply.github.com>
2023-07-25 11:05:23 +0200

    fix(state-viewer): Scan db columns for incoming and outgoing receipts (#9340)
    * fix(state-viewer): Scan incoming/outgoing receipts columns
    
    * fix(state-viewer): Scan incoming/outgoing receipts columns
    
    * fix(state-viewer): Scan incoming/outgoing receipts columns
    
    ---------
    
    Co-authored-by: near-bulldozer[bot] <73298989+near-bulldozer[bot]@users.noreply.github.com>

--- tools/state-viewer/src/scan_db.rs
+++ tools/state-viewer/src/scan_db.rs
@@ -7,7 +7,7 @@ use near_primitives::epoch_manager::epoch_info::EpochInfo;
 use near_primitives::epoch_manager::AGGREGATOR_KEY;
 use near_primitives::receipt::Receipt;
 use near_primitives::shard_layout::{get_block_shard_uid_rev, ShardUId};
-use near_primitives::sharding::{ChunkHash, ShardChunk, StateSyncInfo};
+use near_primitives::sharding::{ChunkHash, ReceiptProof, ShardChunk, StateSyncInfo};
 use near_primitives::state::FlatStateValue;
 use near_primitives::syncing::{
     ShardStateSyncResponseHeader, StateHeaderKey, StatePartKey, StateSyncDumpProgress,
@@ -151,6 +151,14 @@ fn format_key_and_value<'a>(
         DBCol::HeaderHashesByHeight => (
             Box::new(BlockHeight::try_from_slice(key).unwrap()),
             Box::new(HashSet::<CryptoHash>::try_from_slice(value).unwrap()),
+        ),
+        DBCol::IncomingReceipts => (
+            Box::new(get_block_shard_id_rev(key).unwrap()),
+            Box::new(Vec::<ReceiptProof>::try_from_slice(value).unwrap()),
+        ),
+        DBCol::OutgoingReceipts => (
+            Box::new(get_block_shard_id_rev(key).unwrap()),
+            Box::new(Vec::<Receipt>::try_from_slice(value).unwrap()),
         ),
         DBCol::OutcomeIds => (
             Box::new(get_block_shard_id_rev(key).unwrap()),
