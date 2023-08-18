8a2b91eec91afc2999ebe86c0ffe8f7b0b1a806d
Longarithm <the.aleksandr.logunov@gmail.com>
2023-08-18 22:30:22 +0400

    fix

--- core/store/src/trie/mod.rs
+++ core/store/src/trie/mod.rs
@@ -52,8 +52,6 @@ use self::accounting_cache::TrieAccountingCache;
 use self::trie_recording::TrieRecorder;
 use self::trie_storage::TrieMemoryPartialStorage;
 pub use from_flat::construct_trie_from_flat;
-use near_cache::SyncLruCache;
-use near_vm_runner::logic::CompiledContract;
 
 const POISONED_LOCK_ERR: &str = "The lock was poisoned.";
 
