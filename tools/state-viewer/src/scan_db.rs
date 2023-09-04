93a6ba434d3eebf2b8a1c5fce2ab9339c8d5383d
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-09-04 17:21:01 +0200

    Reverted a change to nibbles

--- tools/state-viewer/src/scan_db.rs
+++ tools/state-viewer/src/scan_db.rs
@@ -20,7 +20,7 @@ use near_primitives_core::hash::CryptoHash;
 use near_primitives_core::types::BlockHeight;
 use near_store::flat::delta::KeyForFlatStateDelta;
 use near_store::flat::{FlatStateChanges, FlatStateDeltaMetadata};
-use near_store::{DBCol, NibbleSlice, RawTrieNodeWithSize, Store, TrieChanges};
+use near_store::{DBCol, RawTrieNodeWithSize, Store, TrieChanges};
 use std::collections::HashSet;
 use std::fmt::Debug;
 use strum::IntoEnumIterator;
