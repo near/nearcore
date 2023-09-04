c7fff666a7b396d469e9664d0a34d553b57cc9d3
Jakob Meier <mail@jakobmeier.ch>
2023-09-04 11:27:10 +0200

    feat(estimator): --check-only option for estimator (#9468)
    * feat(estimator): --check-only option for estimator
    
    There are several expensive things we do in the estimator that are only
    necessary to make results more accurate. If we want to have CI checks,
    we want to avoid them. `--check-only` allows to opt out.
    
    Specifically, `--check-only` does:
    - Disable CPU cache spoiling.
    - Use a single tx per block instead of multiple.
    - Reduce RocksDB benchmarks to a single operation.
    
    * fix compilation error in existing test
    
    * flip config from `--check-only` to `--accurate`

--- runtime/runtime-params-estimator/src/utils.rs
+++ runtime/runtime-params-estimator/src/utils.rs
@@ -122,7 +122,12 @@ pub(crate) fn fn_cost_count(
     ext_cost: ExtCosts,
     block_latency: usize,
 ) -> (GasCost, u64) {
-    let block_size = 20;
+    // Block size: 20 is a good number if you want to reduce the effect of
+    // constant-per-block overhead and the tx takes less than 50 Tgas to
+    // execute. It's hard-coded because the range of values supported depends on
+    // each estimation. For a check-only run, a single tx per block is faster
+    // and good enough.
+    let block_size = if ctx.config.accurate { 20 } else { 1 };
     let mut make_transaction = |tb: &mut TransactionBuilder| -> SignedTransaction {
         let sender = tb.random_unused_account();
         tb.transaction_from_function_call(sender, method, Vec::new())
