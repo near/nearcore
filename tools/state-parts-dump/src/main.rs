ea0552b9f3df86f4853e18880f5fa1ed5454a0e4
Xiangyi Zheng <xiangyi@near.org>
2023-09-05 19:10:05 -0700

    in progress

--- tools/state-parts-dump/src/main.rs
+++ tools/state-parts-dump/src/main.rs
@@ -1,44 +1,40 @@
 mod rpc_requests;
 mod cli;
+mod metrics;
+mod helpers;
 use near_primitives::types::{EpochId, StateRoot};
 use near_primitives::hash::CryptoHash;
 use std::str::FromStr;
+use crate::cli::StatePartsDumpCheckStatus;
+use crate::helpers::{get_processing_epoch_information, run_dump_check};
 
+// the script can query near rpc endpoint for latest epoch_height
+// if the latest epoch_height > the last time we polled for epoch_height, and epoch_dump_check_progress = Done, then it means
+// the check for last epoch was done, and we should check for last epoch_height + 1.
 fn main() {
-    println!( "start");
+
     let rpc_client = crate::rpc_requests::RpcClient::new("mainnet");
-    let latest_block_hash = rpc_client.get_latest_block_hash().unwrap();
-    let latest_epoch_id = rpc_client.get_epoch_id(&latest_block_hash).unwrap();
-
-    let epoch_id = EpochId(CryptoHash::from_str(&latest_epoch_id).unwrap());
     
-    let prev_epoch_id_str = rpc_client.get_prev_epoch_id(&latest_epoch_id).unwrap();
-    let prev_epoch_height = rpc_client.get_epoch_height(&latest_epoch_id).unwrap() - 1;
-    let state_root_str = rpc_client.get_prev_epoch_state_root(&prev_epoch_id_str).unwrap();
-    // //TODO: convert state_root from a String to StateRoot (CryptoHash)
-    let state_root: StateRoot = CryptoHash::from_str(&state_root_str).unwrap();
-    let prev_epoch_id = EpochId(CryptoHash::from_str(&prev_epoch_id_str).unwrap());
+    let mut last_check_status = Ok(StatePartsDumpCheckStatus::WaitingForParts{epoch_height: 0});
 
-    println!("latest_epoch_id: {}, latest_block_hash: {}, prev_epoch_id_str: {}, prev_epoch_height: {}, state_root_str: {}", latest_epoch_id, latest_block_hash, prev_epoch_id_str, prev_epoch_height, state_root_str);
-    
-    let dump_check = crate::cli::StatePartsDumpCheck::new(
-        "mainnet".to_string(),
-        prev_epoch_id,
-        prev_epoch_height,
-        3,
-        state_root,
-        None,
-        None,
-        None,
-        Some("state-parts".to_string())
-    );
+    let mut last_check_iter_info;
+    while true {
+        let dump_check_iter_info_res = get_processing_epoch_information(&rpc_client);
+        if let Err(_) = dump_check_iter_info_res {
+            break;
+        }
+        let dump_check_iter_info = dump_check_iter_info_res.unwrap();
+        
+        if let Ok(StatePartsDumpCheckStatus::Done{epoch_height: epoch_height}) = last_check_status && epoch_height >= dump_check_iter_info.prev_epoch_height {
+            // TODO: sleep 5 mins
+            break;
+        }
 
-    
-    let sys = actix::System::new();
-    sys.block_on(async move {
-        dump_check.run().await;
-    });
-    sys.run().unwrap();
+        last_check_iter_info = dump_check_iter_info;
+        let check_result = run_dump_check(&rpc_client, &dump_check_iter_info).await;
+        last_check_status = check_result;
+    }
+
 
 
 }
