5de285e3c10c026a4ce0c63d5b16f3c0c816c065
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-09-08 15:12:25 +0200

    Fix gc_after_sync, gc_sync_after_sync and skip_epoch.

--- pytest/tests/sanity/skip_epoch.py
+++ pytest/tests/sanity/skip_epoch.py
@@ -5,96 +5,46 @@
 #  - BPs went offline after some epoch
 # Warn: this test may not clean up ~/.near if fails early
 
-import sys, time, base58
+import base58
 import pathlib
+import sys
+import time
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / 'lib'))
 
 from cluster import init_cluster, spin_up_node, load_config
 from configured_logger import logger
 from transaction import sign_staking_tx
+import state_sync_lib
 import utils
 
 TIMEOUT = 600
+
+EPOCH_LENGTH = 30
 # the height we spin up the second node
-TARGET_HEIGHT = 35
-EPOCH_LENGTH = 12
+TARGET_HEIGHT = int(EPOCH_LENGTH * 2.8)
 
 config = load_config()
+node_config = state_sync_lib.get_state_sync_config_combined()
 # give more stake to the bootnode so that it can produce the blocks alone
 near_root, node_dirs = init_cluster(
     4, 1, 4, config,
     [["min_gas_price", 0], ["max_inflation_rate", [0, 1]],
      ["epoch_length", EPOCH_LENGTH], ["block_producer_kickout_threshold", 20],
-     ["chunk_producer_kickout_threshold", 20]], {
-         0: {
-             "view_client_throttle_period": {
-                 "secs": 0,
-                 "nanos": 0
-             },
-             "consensus": {
-                 "state_sync_timeout": {
-                     "secs": 2,
-                     "nanos": 0
-                 }
-             }
-         },
-         1: {
-             "view_client_throttle_period": {
-                 "secs": 0,
-                 "nanos": 0
-             },
-             "consensus": {
-                 "state_sync_timeout": {
-                     "secs": 2,
-                     "nanos": 0
-                 }
-             }
-         },
-         2: {
-             "view_client_throttle_period": {
-                 "secs": 0,
-                 "nanos": 0
-             },
-             "consensus": {
-                 "state_sync_timeout": {
-                     "secs": 2,
-                     "nanos": 0
-                 }
-             }
-         },
-         3: {
-             "view_client_throttle_period": {
-                 "secs": 0,
-                 "nanos": 0
-             },
-             "consensus": {
-                 "state_sync_timeout": {
-                     "secs": 2,
-                     "nanos": 0
-                 }
-             }
-         },
-         4: {
-             "tracked_shards": [0, 1, 2, 3],
-             "view_client_throttle_period": {
-                 "secs": 0,
-                 "nanos": 0
-             }
-         }
-     })
+     ["chunk_producer_kickout_threshold", 20]],
+    {x: node_config for x in range(5)})
 
 started = time.time()
 
 boot_node = spin_up_node(config, near_root, node_dirs[0], 0)
 boot_node.stop_checking_store()
-node3 = spin_up_node(config, near_root, node_dirs[2], 2, boot_node=boot_node)
-node4 = spin_up_node(config, near_root, node_dirs[3], 3, boot_node=boot_node)
+node2 = spin_up_node(config, near_root, node_dirs[2], 2, boot_node=boot_node)
+node3 = spin_up_node(config, near_root, node_dirs[3], 3, boot_node=boot_node)
 observer = spin_up_node(config, near_root, node_dirs[4], 4, boot_node=boot_node)
 observer.stop_checking_store()
 
 ctx = utils.TxContext([4, 4, 4, 4, 4],
-                      [boot_node, None, node3, node4, observer])
+                      [boot_node, None, node2, node3, observer])
 initial_balances = ctx.get_balances()
 total_supply = sum(initial_balances)
 
@@ -119,19 +69,13 @@ for height, hash_ in utils.poll_blocks(observer,
 
 logger.info("stage 1 done")
 
-# 2. Spin up the second node and make sure it gets to 35 as well, and doesn't diverge
-node2 = spin_up_node(config, near_root, node_dirs[1], 1, boot_node=boot_node)
-node2.stop_checking_store()
+# 2. Spin up the second node and make sure it gets to TARGET_HEIGHT
+node1 = spin_up_node(config, near_root, node_dirs[1], 1, boot_node=boot_node)
+node1.stop_checking_store()
 
-while True:
-    assert time.time() - started < TIMEOUT
-    sync_info = node2.get_status()['sync_info']
-    if (sync_info['latest_block_height'] > TARGET_HEIGHT and
-            not sync_info['syncing']):
-        break
-    time.sleep(0.1)
+node1_height, _ = utils.wait_for_blocks(node1, target=TARGET_HEIGHT)
 
-logger.info("stage 2 done")
+logger.info(f"stage 2 done, node1_height: {node1_height}")
 
 # 3. During (1) we sent some txs. Make sure the state changed. We can't compare to the
 #    expected balances directly, since the tx sent to the shard that node1 is responsible
@@ -144,8 +88,8 @@ balances = ctx.get_balances()
 logger.info("New balances: %s\nNew total supply: %s" %
             (balances, sum(balances)))
 
-assert (balances != initial_balances)
-assert (sum(balances) == total_supply)
+assert balances != initial_balances
+assert sum(balances) == total_supply
 
 initial_balances = balances
 
@@ -161,9 +105,9 @@ def get_validators():
 
 logger.info(get_validators())
 
-# The stake for node2 must be higher than that of boot_node, so that it can produce blocks
+# The stake for node1 must be higher than that of boot_node, so that it can produce blocks
 # after the boot_node is brought down
-tx = sign_staking_tx(node2.signer_key, node2.validator_key,
+tx = sign_staking_tx(node1.signer_key, node1.validator_key,
                      50000000000000000000000000000000, 20,
                      base58.b58decode(hash_.encode('utf8')))
 boot_node.send_tx(tx)
@@ -187,7 +131,7 @@ ctx.next_nonce = 100
 # 5. Bring down the first node, then wait until epoch T+3
 last_height = observer.get_latest_block().height
 
-ctx.nodes = [boot_node, node2, node3, node4, observer]
+ctx.nodes = [boot_node, node1, node2, node3, observer]
 ctx.act_to_val = [4, 4, 4, 4, 4]
 
 boot_node.kill()
@@ -195,6 +139,7 @@ boot_node.kill()
 for height, hash_ in utils.poll_blocks(observer,
                                        timeout=TIMEOUT,
                                        poll_interval=0.1):
+    logger.info(f'height: {height}')
     if height > last_height + 1:
         ctx.send_moar_txs(hash_, 10, False)
         logger.info(f'Sending txs at height {height}')
@@ -204,6 +149,7 @@ start_epoch = -1
 for epoch_height in utils.poll_epochs(observer,
                                       epoch_length=EPOCH_LENGTH,
                                       timeout=TIMEOUT):
+    logger.info(f'epoch_height: {epoch_height}')
     if start_epoch == -1:
         start_epoch = epoch_height
     if epoch_height >= start_epoch + 3:
@@ -213,7 +159,7 @@ balances = ctx.get_balances()
 logger.info("New balances: %s\nNew total supply: %s" %
             (balances, sum(balances)))
 
-ctx.nodes = [observer, node2]
+ctx.nodes = [observer, node1]
 ctx.act_to_val = [0, 0, 0, 0, 0]
 logger.info("Observer sees: %s" % ctx.get_balances())
 
