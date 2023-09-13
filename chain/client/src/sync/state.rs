e2421b815ecafca2f418656fe60496b1cf66ba45
wacban <wac.banasik@gmail.com>
2023-09-13 13:56:46 +0000

    test: improvements to the state_sync_fail.py nayduck test

--- chain/client/src/sync/state.rs
+++ chain/client/src/sync/state.rs
@@ -283,6 +283,8 @@ impl StateSync {
         if epoch_manager.get_shard_layout(&prev_epoch_id)?
             != epoch_manager.get_shard_layout(&epoch_id)?
         {
+            // This error message is used in tests to ensure node exists for the
+            // correct reason. When changing it please also update the tests.
             panic!("cannot sync to the first epoch after sharding upgrade. Please wait for the next epoch or find peers that are more up to date");
         }
         let split_states = epoch_manager.will_shard_layout_change(&prev_hash)?;
