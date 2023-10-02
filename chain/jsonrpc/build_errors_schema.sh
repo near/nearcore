517d055d004c5071d6aa4046f0b951b95b48aebc
Simonas Kazlauskas <git@kazlauskas.me>
2023-10-02 19:59:07 +0300

    ci: run Rust tests on GHA

--- chain/jsonrpc/build_errors_schema.sh
+++ chain/jsonrpc/build_errors_schema.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "${0%/*}/../.." # ensure we're in the workspace directory
-rm target/rpc_errors_schema.json
+rm -f target/rpc_errors_schema.json
 cargo build -p near-jsonrpc --features dump_errors_schema
 cp target/rpc_errors_schema.json chain/jsonrpc/res/rpc_errors_schema.json
