9ed686683938a4867362ec84ce506e2a93b63e7c
Shreyan Gupta <shreyan@near.org>
2023-10-02 10:03:38 -0700

    [resharding] Remove unit tests in favor of integration tests (#9597)
    After trying out a couple of things to try to get the unit tests working
    with snapshotting for our resharding module, I feel like the complexity
    of resharding functions has reached a place where it might be fine to
    just remove the unit test module in favor of the integration tests.
    
    We have more flexibility with the integration tests and for now, the
    unit test and the integration tests are basically doing the same thing,
    The effort to make the unit test work with snapshot is probably not
    worth it.

--- integration-tests/src/tests/client/mod.rs
+++ integration-tests/src/tests/client/mod.rs
@@ -7,10 +7,10 @@ mod epoch_sync;
 mod features;
 mod flat_storage;
 mod process_blocks;
+mod resharding;
 mod runtimes;
 #[cfg(feature = "sandbox")]
 mod sandbox;
-mod sharding_upgrade;
 mod state_dump;
 mod state_snapshot;
 mod sync_state_nodes;
