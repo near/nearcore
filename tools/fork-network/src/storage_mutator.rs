9bc10b7d5397005fe885bc2197e2ca1227038493
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-10-06 14:31:41 +0200

    fork command: Split into smaller functions, add comments. Rebase

--- tools/fork-network/src/storage_mutator.rs
+++ tools/fork-network/src/storage_mutator.rs
@@ -1,76 +1,93 @@
-use crate::single_shard_storage_mutator::SingleShardStorageMutator;
+use std::sync::Arc;
+
+use near_chain::types::RuntimeAdapter;
 use near_crypto::PublicKey;
 use near_epoch_manager::EpochManagerAdapter;
-use near_primitives::account::{AccessKey, Account};
-use near_primitives::hash::CryptoHash;
-use near_primitives::types::{AccountId, EpochId, StateRoot};
+use near_primitives::{
+    account::{AccessKey, Account},
+    borsh::{self},
+    hash::CryptoHash,
+    trie_key::TrieKey,
+    types::{AccountId, EpochId, StateRoot},
+};
+use near_store::{flat::FlatStateChanges, ShardTries, TrieUpdate};
 use nearcore::NightshadeRuntime;
-use std::sync::Arc;
 
 /// Object that updates the existing state. Combines all changes, commits them
 /// and returns new state roots.
 pub(crate) struct StorageMutator {
     epoch_manager: Arc<dyn EpochManagerAdapter>,
     epoch_id: EpochId,
-    mutators: Vec<SingleShardStorageMutator>,
+    tries: Vec<TrieUpdate>,
+    shard_tries: ShardTries,
 }
 
 impl StorageMutator {
     pub(crate) fn new(
         epoch_manager: Arc<dyn EpochManagerAdapter>,
         runtime: &NightshadeRuntime,
-        epoch_id: EpochId,
+        epoch_id: &EpochId,
         prev_block_hash: CryptoHash,
-        state_roots: Vec<StateRoot>,
+        state_roots: &[StateRoot],
     ) -> anyhow::Result<Self> {
-        let shard_layout = epoch_manager.get_shard_layout(&epoch_id)?;
+        let shard_layout = epoch_manager.get_shard_layout(epoch_id)?;
         let num_shards = shard_layout.num_shards();
-
-        let mut mutators = vec![];
+        let mut trie_updates = Vec::with_capacity(num_shards as usize);
         for shard_id in 0..num_shards {
-            mutators.push(SingleShardStorageMutator::new(
+            let trie = runtime.get_trie_for_shard(
                 shard_id,
-                runtime,
-                prev_block_hash,
+                &prev_block_hash,
                 state_roots[shard_id as usize],
-            )?);
+                false,
+            )?;
+            let trie_update = TrieUpdate::new(trie);
+            trie_updates.push(trie_update);
         }
-        Ok(Self { epoch_manager, epoch_id, mutators })
-    }
-
-    fn mutator(
-        &mut self,
-        account_id: &AccountId,
-    ) -> anyhow::Result<&mut SingleShardStorageMutator> {
-        let shard_id = self.epoch_manager.account_id_to_shard_id(&account_id, &self.epoch_id)?;
-        Ok(&mut self.mutators[shard_id as usize])
+        Ok(Self {
+            epoch_manager,
+            epoch_id: epoch_id.clone(),
+            tries: trie_updates,
+            shard_tries: runtime.get_tries(),
+        })
     }
 
     pub(crate) fn set_account(
         &mut self,
-        account_id: &AccountId,
+        account_id: AccountId,
         value: Account,
     ) -> anyhow::Result<()> {
-        self.mutator(account_id)?.set_account(account_id.clone(), value)
+        let shard_id = self.epoch_manager.account_id_to_shard_id(&account_id, &self.epoch_id)?;
+        self.tries[shard_id as usize].set(TrieKey::Account { account_id }, borsh::to_vec(&value)?);
+        Ok(())
     }
 
     pub(crate) fn set_access_key(
         &mut self,
-        account_id: &AccountId,
+        account_id: AccountId,
         public_key: PublicKey,
         access_key: AccessKey,
     ) -> anyhow::Result<()> {
-        self.mutator(account_id)?.set_access_key(account_id.clone(), public_key, access_key)
+        let shard_id = self.epoch_manager.account_id_to_shard_id(&account_id, &self.epoch_id)?;
+        self.tries[shard_id as usize]
+            .set(TrieKey::AccessKey { account_id, public_key }, borsh::to_vec(&access_key)?);
+        Ok(())
     }
 
     pub(crate) fn commit(self) -> anyhow::Result<Vec<StateRoot>> {
         let shard_layout = self.epoch_manager.get_shard_layout(&self.epoch_id)?;
         let all_shard_uids = shard_layout.get_shard_uids();
-        let mut state_roots = vec![];
-        for (mutator, shard_uid) in self.mutators.into_iter().zip(all_shard_uids.into_iter()) {
-            let state_root = mutator.commit(&shard_uid)?;
+        let mut state_roots = Vec::new();
+
+        let mut update = self.shard_tries.store_update();
+        for (mut trie_update, shard_uid) in self.tries.into_iter().zip(all_shard_uids.into_iter()) {
+            trie_update.commit(near_primitives::types::StateChangeCause::Migration);
+            let (_, trie_updates, raw_changes) = trie_update.finalize()?;
+            let state_root = self.shard_tries.apply_all(&trie_updates, shard_uid, &mut update);
             state_roots.push(state_root);
+            let flat_state_changes = FlatStateChanges::from_state_changes(&raw_changes);
+            flat_state_changes.apply_to_flat_state(&mut update, shard_uid);
         }
+        update.commit()?;
         Ok(state_roots)
     }
 }
