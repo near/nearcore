e4e319792d4be06428efaf998065af0e46bd1bbf
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2023-10-09 14:41:29 +0100

    jsonrpc: add tx serialisation tests (#9653)
    I mixed up adding tests and introducing new functionality here
    https://github.com/near/nearcore/pull/9648
    Please have a look just at tests separately

--- chain/jsonrpc/src/api/transactions.rs
+++ chain/jsonrpc/src/api/transactions.rs
@@ -61,3 +61,51 @@ fn decode_signed_transaction(value: Value) -> Result<SignedTransaction, RpcParse
     SignedTransaction::try_from_slice(&bytes)
         .map_err(|err| RpcParseError(format!("Failed to decode transaction: {}", err)))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api::RpcRequest;
+    use near_jsonrpc_primitives::types::transactions::{
+        RpcBroadcastTransactionRequest, RpcTransactionStatusCommonRequest,
+    };
+    use near_primitives::borsh::BorshSerialize;
+    use near_primitives::hash::CryptoHash;
+    use near_primitives::serialize::to_base64;
+    use near_primitives::transaction::SignedTransaction;
+
+    #[test]
+    fn test_serialize_tx_status_params_as_vec() {
+        let tx_hash = CryptoHash::new().to_string();
+        let account_id = "sender.testnet";
+        let params = serde_json::json!([tx_hash, account_id]);
+        assert!(RpcTransactionStatusCommonRequest::parse(params).is_ok());
+    }
+
+    #[test]
+    fn test_serialize_tx_status_params_as_binary_signed_tx() {
+        let tx_hash = CryptoHash::new();
+        let tx = SignedTransaction::empty(tx_hash);
+        let bytes_tx = tx.try_to_vec().unwrap();
+        let str_tx = to_base64(&bytes_tx);
+        let params = serde_json::json!([str_tx]);
+        assert!(RpcTransactionStatusCommonRequest::parse(params).is_ok());
+    }
+
+    // The params are invalid because sender_account_id is missing
+    #[test]
+    fn test_serialize_invalid_tx_status_params() {
+        let tx_hash = CryptoHash::new().to_string();
+        let params = serde_json::json!([tx_hash]);
+        assert!(RpcTransactionStatusCommonRequest::parse(params).is_err());
+    }
+
+    #[test]
+    fn test_serialize_send_tx_params_as_binary_signed_tx() {
+        let tx_hash = CryptoHash::new();
+        let tx = SignedTransaction::empty(tx_hash);
+        let bytes_tx = tx.try_to_vec().unwrap();
+        let str_tx = to_base64(&bytes_tx);
+        let params = serde_json::json!([str_tx]);
+        assert!(RpcBroadcastTransactionRequest::parse(params).is_ok());
+    }
+}
