b982518f5da18ef9eff4a4b7c38e92e1725d9c76
Longarithm <the.aleksandr.logunov@gmail.com>
2023-10-11 15:06:31 +0400

    debug

--- chain/chain/src/store.rs
+++ chain/chain/src/store.rs
@@ -1227,6 +1227,7 @@ impl ChainStoreAccess for ChainStore {
         block_hash: &CryptoHash,
         shard_uid: &ShardUId,
     ) -> Result<Arc<ChunkExtra>, Error> {
+        println!("get_chunk_extra {:?} {:?}", block_hash, shard_uid);
         option_to_not_found(
             self.read_with_cache(
                 DBCol::ChunkExtra,
