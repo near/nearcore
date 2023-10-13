18dc5e200f885b19eb337ac006bca9f6fdf1bc92
Longarithm <the.aleksandr.logunov@gmail.com>
2023-10-13 19:25:04 +0400

    debug

--- chain/chain/src/store.rs
+++ chain/chain/src/store.rs
@@ -2046,7 +2046,7 @@ impl<'a> ChainStoreUpdate<'a> {
         outgoing_receipts: Vec<Receipt>,
     ) {
         {
-            let key = &get_block_shard_id(hash, *shard_id);
+            let key = &get_block_shard_id(hash, shard_id);
             println!("KEY = {:?}\nOR = {:?}", key, outgoing_receipts);
         }
         self.chain_store_cache_update
