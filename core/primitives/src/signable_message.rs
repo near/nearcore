8c8811319e0634dd3eacf2948f54bf3db99e4b5f
Vlad Frolov <frolvlad@gmail.com>
2023-10-29 22:40:14 +0100

    fixed core/primitives/src/signable_message.rs

--- core/primitives/src/signable_message.rs
+++ core/primitives/src/signable_message.rs
@@ -221,15 +221,15 @@ impl From<SignableMessageType> for MessageDiscriminant {
 #[cfg(test)]
 mod tests {
     use near_crypto::{InMemorySigner, KeyType, PublicKey};
+    use near_primitives_core::account::id::AccountIdRef;
 
     use super::*;
     use crate::action::delegate::{DelegateAction, SignedDelegateAction};
 
     // Note: this is currently a simplified copy of near-primitives::test_utils::create_user_test_signer
     // TODO: consider whether it’s worth re-unifying them? it’s test-only code anyway.
-    fn create_user_test_signer(account_name: &str) -> InMemorySigner {
-        let account_id = account_name.parse().unwrap();
-        InMemorySigner::from_seed(account_id, KeyType::ED25519, account_name)
+    fn create_user_test_signer(account_id: &AccountIdRef) -> InMemorySigner {
+        InMemorySigner::from_seed(account_id.to_owned(), KeyType::ED25519, account_id.as_str())
     }
 
     // happy path for NEP-366 signature
@@ -237,7 +237,7 @@ mod tests {
     fn nep_366_ok() {
         let sender_id: AccountId = "alice.near".parse().unwrap();
         let receiver_id: AccountId = "bob.near".parse().unwrap();
-        let signer = create_user_test_signer(sender_id.as_ref());
+        let signer = create_user_test_signer(&sender_id);
 
         let delegate_action = delegate_action(sender_id, receiver_id, signer.public_key());
         let signable = SignableMessage::new(&delegate_action, SignableMessageType::DelegateAction);
@@ -251,7 +251,7 @@ mod tests {
     fn nep_366_wrong_nep() {
         let sender_id: AccountId = "alice.near".parse().unwrap();
         let receiver_id: AccountId = "bob.near".parse().unwrap();
-        let signer = create_user_test_signer(sender_id.as_ref());
+        let signer = create_user_test_signer(&sender_id);
 
         let delegate_action = delegate_action(sender_id, receiver_id, signer.public_key());
         let wrong_nep = 777;
@@ -269,7 +269,7 @@ mod tests {
     fn nep_366_wrong_msg_type() {
         let sender_id: AccountId = "alice.near".parse().unwrap();
         let receiver_id: AccountId = "bob.near".parse().unwrap();
-        let signer = create_user_test_signer(sender_id.as_ref());
+        let signer = create_user_test_signer(&sender_id);
 
         let delegate_action = delegate_action(sender_id, receiver_id, signer.public_key());
         let correct_nep = 366;
