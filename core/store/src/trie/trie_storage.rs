2053c5b2c94959a04f520cd7dbd27fbdec773922
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-11-13 15:40:15 +0100

    debug

--- core/store/src/trie/trie_storage.rs
+++ core/store/src/trie/trie_storage.rs
@@ -533,6 +533,7 @@ fn read_node_from_db(
     hash: &CryptoHash,
 ) -> Result<Arc<[u8]>, StorageError> {
     tracing::info!(?shard_uid, ?hash, "read_node_from_db");
+    assert_eq!(shard_uid.version,1);
     let key = TrieCachingStorage::get_key_from_shard_uid_and_hash(shard_uid, hash);
     let val = store
         .get(DBCol::State, key.as_ref())
