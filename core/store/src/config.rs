e2f50c8d29d20d4c4f49cfc8dc6d5b65f7b9dbee
Longarithm <the.aleksandr.logunov@gmail.com>
2023-11-15 19:35:24 +0400

    disclaimer

--- core/store/src/config.rs
+++ core/store/src/config.rs
@@ -269,6 +269,10 @@ impl Default for StoreConfig {
                 "sweat_the_oracle.testnet".to_owned(),
             ],
 
+            // TODO(#9511): Consider adding here shard id 3 or all shards after
+            // this feature will be tested. Until that, use at your own risk.
+            // It will speed up processing of shards where it is enabled, but
+            // requires more RAM and takes several minutes on startup.
             load_mem_tries_for_shards: Default::default(),
             load_mem_tries_for_all_shards: false,
 
