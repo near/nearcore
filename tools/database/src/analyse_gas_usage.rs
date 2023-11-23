67967f50749d3433c917246e8ad533609470ac4a
Jan Ciolek <jan@near.org>
2023-11-23 15:22:33 +0100

    Implement analysing gas usage
    For each block we want to gather information about how much gas was
    used in every shard and account.
    
    Gas is used when transactions and receipts are executed, and every
    such execution produces an ExecutionOutcome, which is saved in the
    database.
    We can read all ExecutionOutcomes originating from a given shard
    from the database, and get all of the needed information from there.
    Every ExecutionOutcome contains the AccountId of the executor
    and the amount of gas that was burned during the execution.
    It's everything that is needed for analysing gas usage.
    
    Usage from all blocks is merged into a single instace of GasUsageStats
    and the interesting pieces of information are displayed for the user.

--- tools/database/src/analyse_gas_usage.rs
+++ tools/database/src/analyse_gas_usage.rs
@@ -1,13 +1,29 @@
+use std::collections::BTreeMap;
 use std::path::PathBuf;
 use std::rc::Rc;
+use std::sync::Arc;
 
 use clap::Parser;
 use near_chain::{Block, ChainStore};
 use near_epoch_manager::EpochManager;
-use near_store::{NodeStorage, Store};
+
+use near_primitives::epoch_manager::block_info::BlockInfo;
+use near_primitives::hash::CryptoHash;
+use near_primitives::shard_layout::{account_id_to_shard_id, ShardLayout};
+use near_primitives::transaction::ExecutionOutcome;
+use near_primitives::types::{AccountId, BlockHeight, EpochId, ShardId};
+
+use near_store::{NodeStorage, ShardUId, Store};
 use nearcore::open_storage;
 
 use crate::block_iterators::LastNBlocksIterator;
+
+/// `Gas` is an u64, but it stil might overflow when analysing a large amount of blocks.
+/// 1ms of compute is about 1TGas = 10^12 gas. One epoch takes 43200 seconds (43200000ms).
+/// This means that the amount of gas consumed during a single epoch can reach 43200000 * 10^12 = 4.32 * 10^19
+/// 10^19 doesn't fit in u64, so we need to use u128
+/// To avoid overflows, let's use `BigGas` for storing gas amounts in the code.
+type BigGas = u128;
 
 #[derive(Parser)]
 pub(crate) struct AnalyseGasUsageCommand {
@@ -44,13 +60,169 @@ impl AnalyseGasUsageCommand {
     }
 }
 
+#[derive(Clone, Debug, Default)]
+struct GasUsageInShard {
+    pub used_gas_per_account: BTreeMap<AccountId, BigGas>,
+    pub used_gas_total: BigGas,
+}
+
+impl GasUsageInShard {
+    pub fn new() -> GasUsageInShard {
+        GasUsageInShard { used_gas_per_account: BTreeMap::new(), used_gas_total: 0 }
+    }
+
+    pub fn add_used_gas(&mut self, account: AccountId, used_gas: BigGas) {
+        let old_used_gas: &BigGas = self.used_gas_per_account.get(&account).unwrap_or(&0);
+        let new_used_gas: BigGas = old_used_gas.checked_add(used_gas).unwrap();
+        self.used_gas_per_account.insert(account, new_used_gas);
+
+        self.used_gas_total = self.used_gas_total.checked_add(used_gas).unwrap();
+    }
+
+    pub fn merge(&mut self, other: &GasUsageInShard) {
+        for (account_id, used_gas) in &other.used_gas_per_account {
+            self.add_used_gas(account_id.clone(), *used_gas);
+        }
+        self.used_gas_total = self.used_gas_total.checked_add(other.used_gas_total).unwrap();
+    }
+}
+
+#[derive(Clone, Debug)]
+struct GasUsageStats {
+    pub shards: BTreeMap<ShardUId, GasUsageInShard>,
+}
+
+impl GasUsageStats {
+    pub fn new() -> GasUsageStats {
+        GasUsageStats { shards: BTreeMap::new() }
+    }
+
+    pub fn add_gas_usage_in_shard(&mut self, shard_uid: ShardUId, shard_usage: GasUsageInShard) {
+        match self.shards.get_mut(&shard_uid) {
+            Some(existing_shard_usage) => existing_shard_usage.merge(&shard_usage),
+            None => {
+                let _ = self.shards.insert(shard_uid, shard_usage);
+            }
+        }
+    }
+
+    pub fn used_gas_total(&self) -> BigGas {
+        let mut result: BigGas = 0;
+        for shard_usage in self.shards.values() {
+            result = result.checked_add(shard_usage.used_gas_total).unwrap();
+        }
+        result
+    }
+
+    pub fn merge(&mut self, other: GasUsageStats) {
+        for (shard_uid, shard_usage) in other.shards {
+            self.add_gas_usage_in_shard(shard_uid, shard_usage);
+        }
+    }
+}
+
+fn get_gas_usage_in_block(
+    block: &Block,
+    chain_store: &ChainStore,
+    epoch_manager: &EpochManager,
+) -> GasUsageStats {
+    let block_info: Arc<BlockInfo> = epoch_manager.get_block_info(block.hash()).unwrap();
+    let epoch_id: &EpochId = block_info.epoch_id();
+    let shard_layout: ShardLayout = epoch_manager.get_shard_layout(epoch_id).unwrap();
+
+    let mut result = GasUsageStats::new();
+
+    // Go over every chunk in this block and gather data
+    for chunk_header in block.chunks().iter() {
+        let shard_id: ShardId = chunk_header.shard_id();
+        let shard_uid: ShardUId = ShardUId::from_shard_id_and_layout(shard_id, &shard_layout);
+
+        let mut gas_usage_in_shard = GasUsageInShard::new();
+
+        // The outcome of each transaction and receipt executed in this chunk is saved in the database as an ExecutionOutcome.
+        // Go through all ExecutionOutcomes from this chunk and record the gas usage.
+        let outcome_ids: Vec<CryptoHash> =
+            chain_store.get_outcomes_by_block_hash_and_shard_id(block.hash(), shard_id).unwrap();
+        for outcome_id in outcome_ids {
+            let outcome: ExecutionOutcome = chain_store
+                .get_outcome_by_id_and_block_hash(&outcome_id, block.hash())
+                .unwrap()
+                .unwrap()
+                .outcome;
+
+            // Sanity check - make sure that the executor of this outcome belongs to this shard
+            let account_shard: ShardId =
+                account_id_to_shard_id(&outcome.executor_id, &shard_layout);
+            assert_eq!(account_shard, shard_id);
+
+            gas_usage_in_shard.add_used_gas(outcome.executor_id, outcome.gas_burnt.into());
+        }
+
+        result.add_gas_usage_in_shard(shard_uid, gas_usage_in_shard);
+    }
+
+    result
+}
+
 fn analyse_gas_usage(
     blocks_iter: impl Iterator<Item = Block>,
-    _chain_store: &ChainStore,
-    _epoch_manager: &EpochManager,
+    chain_store: &ChainStore,
+    epoch_manager: &EpochManager,
 ) {
+    // Gather statistics about gas usage in all of the blocks
+    let mut blocks_count: usize = 0;
+    let mut first_analysed_block: Option<(BlockHeight, CryptoHash)> = None;
+    let mut last_analysed_block: Option<(BlockHeight, CryptoHash)> = None;
+
+    let mut gas_usage_stats = GasUsageStats::new();
+
     for block in blocks_iter {
-        println!("Analysing block with height: {}", block.header().height());
-        // TODO: Analyse
+        blocks_count += 1;
+        if first_analysed_block.is_none() {
+            first_analysed_block = Some((block.header().height(), *block.hash()));
+        }
+        last_analysed_block = Some((block.header().height(), *block.hash()));
+
+        let gas_usage_in_block: GasUsageStats =
+            get_gas_usage_in_block(&block, chain_store, epoch_manager);
+        gas_usage_stats.merge(gas_usage_in_block);
+    }
+
+    // Calculates how much percent of `big` is `small` and returns it as a string.
+    // Example: as_percentage_of(10, 100) == "10.0%"
+    let as_percentage_of = |small: BigGas, big: BigGas| {
+        if big > 0 {
+            format!("{:.1}%", small as f64 / big as f64 * 100.0)
+        } else {
+            format!("-")
+        }
+    };
+
+    // Print out the analysis
+    if blocks_count == 0 {
+        println!("No blocks to analyse!");
+        return;
+    }
+    println!("");
+    println!("Analysed {} blocks between:", blocks_count);
+    if let Some((block_height, block_hash)) = first_analysed_block {
+        println!("Block: height = {block_height}, hash = {block_hash}");
+    }
+    if let Some((block_height, block_hash)) = last_analysed_block {
+        println!("Block: height = {block_height}, hash = {block_hash}");
+    }
+    let total_gas: BigGas = gas_usage_stats.used_gas_total();
+    println!("");
+    println!("Total gas used: {}", total_gas);
+    println!("");
+    for (shard_uid, shard_usage) in &gas_usage_stats.shards {
+        println!("Shard: {}", shard_uid);
+        println!(
+            "  Gas usage: {} ({} of total)",
+            shard_usage.used_gas_total,
+            as_percentage_of(shard_usage.used_gas_total, total_gas)
+        );
+        println!("  Number of accounts: {}", shard_usage.used_gas_per_account.len());
+        println!("");
     }
 }
