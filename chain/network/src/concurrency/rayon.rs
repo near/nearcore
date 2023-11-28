a355f0e675fdd34c59616eb5d253daa350001a46
Jan Ciolek <jan@near.org>
2023-11-28 04:35:56 +0100

    feat: add rayon::try_map_result
    Add a function which does the same thing as `concurrency::rayon::try_map`, but operates
    on `Result` instead of `Option`.
    
    It calls the provided function on every item from the iterator and collects
    the results into a Vec. On the first error it stops the execution.
    When an error occurs it returns the items that were successfully processed along with the error.
    
    It's needed because the return type of `SnapshotHostInfo::verify()` will soon be changed
    from `Option` to `Result` and `try_map` can't be used to collect Results.
    
    The implementation is a bit different from `try_map`. I managed to implement it using
    only rayon iterator combinators, without the use of atomics. I think it's a bit cleaner
    this way.

--- chain/network/src/concurrency/rayon.rs
+++ chain/network/src/concurrency/rayon.rs
@@ -1,4 +1,5 @@
-use rayon::iter::ParallelIterator;
+use rayon::iter::{Either, ParallelIterator};
+use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 /// spawns a closure on a global rayon threadpool and awaits its completion.
@@ -45,4 +46,42 @@ pub fn try_map<I: ParallelIterator, T: Send>(
         })
         .collect();
     (res, ok.load(Ordering::Acquire))
+}
+
+/// Applies `func` to the iterated elements and collects the outputs. On the first `Error` the execution is stopped.
+/// Returns the outputs collected so far and a [`Result`] ([`Result::Err`] iff any `Error` was returned).
+/// Same as [`try_map`], but it operates on [`Result`] instead of [`Option`].
+pub fn try_map_result<I: ParallelIterator, T: Send, E: Error + Send>(
+    iter: I,
+    func: impl Sync + Send + Fn(I::Item) -> Result<T, E>,
+) -> (Vec<T>, Result<(), E>) {
+    // Call the function on every input value and emit some items for every result.
+    // On a successful call this iterator emits one item: `Some(Ok(output_value))`
+    // When an error occurs, the iterator emits two items: `Some(Err(the_error))` and `None``
+    // The `None` will later be used to tell rayon to stop the execution.
+    let optional_result_iter /* impl Iterator<Item = Option<Result<T, E>>> */ = iter
+        .map(|v| match func(v) {
+            Ok(val) => Either::Left(std::iter::once(Some(Ok(val)))),
+            Err(err) => Either::Right([Some(Err(err)), None].into_iter()),
+        })
+        .flatten_iter();
+
+    // `while_some()` monitors a stream of `Option` values and stops the execution when it spots a `None` value.
+    // It's used to implement the short-circuit logic - on the first error rayon will stop processing subsequent items.
+    let results_iter = optional_result_iter.while_some();
+
+    // Split the results into two groups - the left group contains the outputs resulting from a successful execution,
+    // while the right group contains errors. Collect them into twp separate Vecs.
+    let (outputs, errors): (Vec<T>, Vec<E>) = results_iter
+        .map(|res| match res {
+            Ok(value) => Either::Left(value),
+            Err(error) => Either::Right(error),
+        })
+        .collect();
+
+    // Return the output and the first error (if there was any)
+    match errors.into_iter().next() {
+        Some(first_error) => (outputs, Err(first_error)),
+        None => (outputs, Ok(())),
+    }
 }
