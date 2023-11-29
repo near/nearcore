176b92ff640c00da0f85c7e8f53e0773b3141ba3
Ekleog-NEAR <96595974+Ekleog-NEAR@users.noreply.github.com>
2023-11-29 19:47:35 +0100

    add a Justfile for reproducing CI locally, use it for CI (#10235)

--- chain/jsonrpc/build_errors_schema.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd "${0%/*}/../.." # ensure we're in the workspace directory
-rm -f target/rpc_errors_schema.json
-cargo check -p near-jsonrpc --features dump_errors_schema
-cp target/rpc_errors_schema.json chain/jsonrpc/res/rpc_errors_schema.json
