3d6c03b935107255b6e49f4a1dd34f5a03c42901
Anton Puhach <anton@near.org>
2023-12-01 14:27:48 +0100

    impl

--- tools/state-viewer/src/commands.rs
+++ tools/state-viewer/src/commands.rs
@@ -12,6 +12,8 @@ use itertools::Itertools;
 use near_chain::chain::collect_receipts_from_response;
 use near_chain::migrations::check_if_block_is_first_with_chunk_of_version;
 use near_chain::types::ApplyTransactionResult;
+use near_chain::types::ApplyTransactionsBlockContext;
+use near_chain::types::ApplyTransactionsChunkContext;
 use near_chain::types::RuntimeAdapter;
 use near_chain::types::RuntimeStorageConfig;
 use near_chain::{ChainStore, ChainStoreAccess, ChainStoreUpdate, Error};
@@ -88,21 +90,20 @@ pub(crate) fn apply_block(
 
         runtime
             .apply_transactions(
-                shard_id,
                 RuntimeStorageConfig::new(*chunk_inner.prev_state_root(), use_flat_storage),
-                height,
-                block.header().raw_timestamp(),
-                block.header().prev_hash(),
-                block.hash(),
+                ApplyTransactionsChunkContext {
+                    shard_id,
+                    last_validator_proposals: chunk_inner.prev_validator_proposals(),
+                    gas_limit: chunk_inner.gas_limit(),
+                    is_new_chunk: true,
+                    is_first_block_with_chunk_of_version,
+                },
+                ApplyTransactionsBlockContext::from_header(
+                    block.header(),
+                    prev_block.header().next_gas_price(),
+                ),
                 &receipts,
                 chunk.transactions(),
-                chunk_inner.prev_validator_proposals(),
-                prev_block.header().next_gas_price(),
-                chunk_inner.gas_limit(),
-                block.header().challenges_result(),
-                *block.header().random_value(),
-                true,
-                is_first_block_with_chunk_of_version,
             )
             .unwrap()
     } else {
@@ -111,21 +112,20 @@ pub(crate) fn apply_block(
 
         runtime
             .apply_transactions(
-                shard_id,
                 RuntimeStorageConfig::new(*chunk_extra.state_root(), use_flat_storage),
-                block.header().height(),
-                block.header().raw_timestamp(),
-                block.header().prev_hash(),
-                block.hash(),
+                ApplyTransactionsChunkContext {
+                    shard_id,
+                    last_validator_proposals: chunk_extra.validator_proposals(),
+                    gas_limit: chunk_extra.gas_limit(),
+                    is_new_chunk: false,
+                    is_first_block_with_chunk_of_version: false,
+                },
+                ApplyTransactionsBlockContext::from_header(
+                    block.header(),
+                    block.header().next_gas_price(),
+                ),
                 &[],
                 &[],
-                chunk_extra.validator_proposals(),
-                block.header().next_gas_price(),
-                chunk_extra.gas_limit(),
-                block.header().challenges_result(),
-                *block.header().random_value(),
-                false,
-                false,
             )
             .unwrap()
     };
