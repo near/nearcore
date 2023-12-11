d85e6378df494750dcca1fdae3dafa8225e37396
wacban <wac.banasik@gmail.com>
2023-12-11 13:53:43 +0000

    cleanup

--- pytest/lib/resharding_lib.py
+++ pytest/lib/resharding_lib.py
@@ -1,5 +1,7 @@
 # A library with the common constants and functions for testing resharding.
 
+# TODO(resharding) The resharding V2 is now stabilized so the V1 code is no
+# longer exercised and can be removed.
 V1_PROTOCOL_VERSION = 48
 V2_PROTOCOL_VERSION = 64
 
