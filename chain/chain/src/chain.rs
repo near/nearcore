2dc1c9fa8520b7b6a0c8ab6888f08898b3f20d21
Akhi Singhania <akhi3030@gmail.com>
2023-12-15 12:53:16 +0100

    wip

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -3460,7 +3460,7 @@ impl Chain {
     ) -> Result<(), Error> {
         let mut chain_store_update = self.mut_store().store_update();
         chain_store_update.gc_col_state_parts(sync_hash, shard_id, num_parts)?;
-        Ok(chain_store_update.commit()?)
+        chain_store_update.commit()
     }
 
     pub fn catchup_blocks_step(
@@ -3573,7 +3573,7 @@ impl Chain {
             }
         };
 
-        return Ok(());
+        Ok(())
     }
 
     /// For given pair of block headers and shard id, return information about
