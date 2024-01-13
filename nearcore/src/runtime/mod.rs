478672399d33f1a7d0784721d9af1121a3cd9e29
Longarithm <the.aleksandr.logunov@gmail.com>
2024-01-13 03:54:52 -0300

    not panic

--- nearcore/src/runtime/mod.rs
+++ nearcore/src/runtime/mod.rs
@@ -888,7 +888,8 @@ impl RuntimeAdapter for NightshadeRuntime {
             Ok(result) => Ok(result),
             Err(e) => match e {
                 Error::StorageError(err) => match &err {
-                    StorageError::FlatStorageBlockNotSupported(_) => Err(err.into()),
+                    StorageError::FlatStorageBlockNotSupported(_)
+                    | StorageError::MissingTrieValue(..) => Err(err.into()),
                     _ => panic!("{err}"),
                 },
                 _ => Err(e),
