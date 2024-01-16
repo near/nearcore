6c31ad6c0051192e94888775458ea7bf3727f97a
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2024-01-16 10:15:08 -0500

    feat(state-sync): implement peer selection for state part requests
    This implements the ideas laid out here: https://github.com/near/nearcore/issues/10213,
    and implements a select_host() function for the SnapshotHostsCache. This function
    selects a peer to request the part from based on the up-to-date SnapshotHostInfos
    we've seen, and sorts them by a priority score calculated from the Peer Id and Part Id,
    remembering who we've requested from so we can try different peers on subsequent calls
    
    A goal of the implementation is to have as little impact as possible on nodes not
    running state sync, which will be most nodes. So here we lazily only store data associated
    with peer selection for a given part after select_host() has been called for that part, and
    the added part_received() function is meant to serve as a signal that we can free this data

--- chain/network/src/snapshot_hosts/mod.rs
+++ chain/network/src/snapshot_hosts/mod.rs
@@ -8,10 +8,14 @@ use crate::concurrency;
 use crate::network_protocol::SnapshotHostInfo;
 use crate::network_protocol::SnapshotHostInfoVerificationError;
 use lru::LruCache;
+use near_primitives::hash::CryptoHash;
 use near_primitives::network::PeerId;
+use near_primitives::state_part::PartId;
+use near_primitives::types::ShardId;
 use parking_lot::Mutex;
 use rayon::iter::ParallelBridge;
-use std::collections::HashMap;
+use sha2::{Digest, Sha256};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::sync::Arc;
 
 #[cfg(test)]
@@ -32,11 +36,143 @@ pub struct Config {
     /// least-recently-used cache. In the future, we may wish to
     /// implement something more sophisticated.
     pub snapshot_hosts_cache_size: u32,
+    /// The number of hosts we'll add to structures related to state part peer
+    /// selection each time we need to request parts from a new peer
+    pub part_selection_cache_batch_size: u32,
+}
+
+pub(crate) fn priority_score(peer_id: &PeerId, part_id: &PartId) -> [u8; 32] {
+    let mut h = Sha256::new();
+    h.update(peer_id.public_key().key_data());
+    h.update(part_id.idx.to_le_bytes());
+    h.finalize().into()
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct PartPriority {
+    score: [u8; 32],
+    // TODO: consider storing this on disk, so we can remember who hasn't
+    // been able to provide us with the parts across restarts
+    times_returned: usize,
+}
+
+impl PartPriority {
+    fn inc(&mut self) {
+        self.times_returned += 1;
+    }
+}
+
+impl PartialOrd for PartPriority {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for PartPriority {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.times_returned.cmp(&other.times_returned).then_with(|| self.score.cmp(&other.score))
+    }
+}
+
+#[derive(Default)]
+struct PartPeerSelector {
+    peers: BTreeMap<PartPriority, PeerId>,
+}
+
+impl PartPeerSelector {
+    fn next(&mut self) -> Option<PeerId> {
+        match self.peers.pop_first() {
+            Some((mut priority, peer_id)) => {
+                priority.inc();
+                self.peers.insert(priority, peer_id.clone());
+                Some(peer_id)
+            }
+            None => None,
+        }
+    }
+
+    fn insert_peer(&mut self, peer_id: PeerId, score: [u8; 32]) {
+        self.peers.insert(PartPriority { score, times_returned: 0 }, peer_id);
+    }
+
+    fn len(&self) -> usize {
+        self.peers.len()
+    }
+
+    fn tried_everybody(&self) -> bool {
+        self.peers.iter().all(|(priority, _peer_id)| priority.times_returned > 0)
+    }
+}
+
+#[derive(Default)]
+struct PeerSelector {
+    selectors: HashMap<u64, PartPeerSelector>,
+}
+
+fn do_insert(
+    peers: &mut BTreeMap<[u8; 32], PeerId>,
+    peer_id: PeerId,
+    part_id: &PartId,
+    score: [u8; 32],
+) {
+    if let Some(other) = peers.insert(score, peer_id.clone()) {
+        // could handle it but it's so unlikely it's not really worth it. So this log statement serves
+        // as code documentation explaining what's happening more than anything else
+        tracing::warn!(
+            "Currently ignoring peer {} in place of {} for state part requests for part ID {:?} due to duplicate rendezvous hash score",
+            &other, peer_id, part_id,
+        )
+    }
+}
+
+impl PeerSelector {
+    fn next(&mut self, part_id: &PartId) -> Option<PeerId> {
+        self.selectors.entry(part_id.idx).or_default().next()
+    }
+
+    fn len(&self, part_id: &PartId) -> usize {
+        match self.selectors.get(&part_id.idx) {
+            Some(s) => s.len(),
+            None => 0,
+        }
+    }
+
+    fn insert_peer(&mut self, part_id: &PartId, peer_id: PeerId, score: [u8; 32]) {
+        let s = self.selectors.entry(part_id.idx).or_default();
+        s.insert_peer(peer_id, score);
+    }
+
+    fn seen_peers(&self, part_id: &PartId) -> HashSet<PeerId> {
+        match self.selectors.get(&part_id.idx) {
+            Some(s) => {
+                let mut ret = HashSet::new();
+                for (_s, peer_id) in s.peers.iter() {
+                    ret.insert(peer_id.clone());
+                }
+                ret
+            }
+            None => HashSet::new(),
+        }
+    }
+
+    // have we already returned every peer we know about?
+    fn tried_everybody(&self, part_id: &PartId) -> bool {
+        match self.selectors.get(&part_id.idx) {
+            Some(s) => s.tried_everybody(),
+            None => true,
+        }
+    }
+
+    fn clear(&mut self, part_id: &PartId) {
+        self.selectors.remove(&part_id.idx);
+    }
 }
 
 struct Inner {
     /// The latest known SnapshotHostInfo for each node in the network
     hosts: LruCache<PeerId, Arc<SnapshotHostInfo>>,
+    state_part_selectors: HashMap<ShardId, PeerSelector>,
+    part_selection_cache_batch_size: usize,
 }
 
 impl Inner {
@@ -57,14 +193,57 @@ impl Inner {
         self.hosts.push(d.peer_id.clone(), d.clone());
         Some(d)
     }
+
+    // Try to insert up to max_entries_added more peers into the state part selector for this part ID
+    fn insert_peers(
+        &mut self,
+        sync_hash: &CryptoHash,
+        shard_id: ShardId,
+        part_id: &PartId,
+        max_entries_added: usize,
+    ) {
+        let selector = self.state_part_selectors.get(&shard_id).unwrap();
+        let seen_peers = selector.seen_peers(part_id);
+
+        let mut new_peers = BTreeMap::new();
+        for (peer_id, info) in self.hosts.iter() {
+            if seen_peers.contains(peer_id)
+                || info.sync_hash != *sync_hash
+                || !info.shards.contains(&shard_id)
+            {
+                continue;
+            }
+            let score = priority_score(peer_id, part_id);
+            if new_peers.len() < max_entries_added {
+                do_insert(&mut new_peers, peer_id.clone(), part_id, score);
+            } else {
+                let worst_score = new_peers.last_key_value().unwrap().0.clone();
+                // we say <= instead of < to emphasize the warn! log in do_insert()
+                if score <= worst_score {
+                    do_insert(&mut new_peers, peer_id.clone(), part_id, score);
+                    assert!(new_peers.remove(&worst_score).is_some());
+                }
+            }
+        }
+        let selector = self.state_part_selectors.get_mut(&shard_id).unwrap();
+        for (score, peer_id) in new_peers {
+            selector.insert_peer(part_id, peer_id, score);
+        }
+    }
 }
 
 pub(crate) struct SnapshotHostsCache(Mutex<Inner>);
 
 impl SnapshotHostsCache {
     pub fn new(config: Config) -> Self {
+        debug_assert!(config.part_selection_cache_batch_size > 0);
         let hosts = LruCache::new(config.snapshot_hosts_cache_size as usize);
-        Self(Mutex::new(Inner { hosts }))
+        let state_part_selectors = HashMap::new();
+        Self(Mutex::new(Inner {
+            hosts,
+            state_part_selectors,
+            part_selection_cache_batch_size: config.part_selection_cache_batch_size as usize,
+        }))
     }
 
     /// Selects new data and verifies the signatures.
@@ -132,5 +311,38 @@ impl SnapshotHostsCache {
 
     pub fn get_hosts(&self) -> Vec<Arc<SnapshotHostInfo>> {
         self.0.lock().hosts.iter().map(|(_, v)| v.clone()).collect()
+    }
+
+    // Selecs a peer to send the request for this part ID to. Chooses based on a priority score
+    // calculated as a hash of the Peer ID plus the part ID, and will return different hosts
+    // on subsequent calls, eventually iterating over all valid SnapshotHostInfos we know about
+    // TODO: get rid of the dead_code and hook this up to the decentralized state sync
+    #[allow(dead_code)]
+    pub fn select_host(
+        &self,
+        sync_hash: &CryptoHash,
+        shard_id: ShardId,
+        part_id: &PartId,
+    ) -> Option<PeerId> {
+        let mut inner = self.0.lock();
+        let num_hosts = inner.hosts.len();
+        let selector = inner.state_part_selectors.entry(shard_id).or_default();
+
+        if selector.tried_everybody(part_id) && selector.len(part_id) < num_hosts {
+            let max_entries_added = inner.part_selection_cache_batch_size;
+            inner.insert_peers(sync_hash, shard_id, part_id, max_entries_added);
+        }
+        let selector = inner.state_part_selectors.get_mut(&shard_id).unwrap();
+        selector.next(part_id)
+    }
+
+    // Lets us know that we have already successfully retrieved this part, and we can free any data
+    // associated with it that we were going to use to respond to future calls to select_host()
+    // TODO: get rid of the dead_code and hook this up to the decentralized state sync
+    #[allow(dead_code)]
+    pub fn part_received(&self, _sync_hash: &CryptoHash, shard_id: ShardId, part_id: &PartId) {
+        let mut inner = self.0.lock();
+        let selector = inner.state_part_selectors.entry(shard_id).or_default();
+        selector.clear(part_id);
     }
 }
