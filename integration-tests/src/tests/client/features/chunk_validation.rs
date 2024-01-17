26c42e6ae4476fb5553d873c3bdd33bb993b68ad
Longarithm <the.aleksandr.logunov@gmail.com>
2024-01-17 01:04:47 -0300

    fix

--- integration-tests/src/tests/client/features/chunk_validation.rs
+++ integration-tests/src/tests/client/features/chunk_validation.rs
@@ -167,8 +167,8 @@ fn test_chunk_validation_basic() {
             );
             let blocks_processed = if rng.gen_bool(0.1) {
                 if round < blocks_to_produce - 1 {
-                    for shard_id in chunk_producers.get(validator_id).unwrap_or_default() {
-                        if block.chunks().get(shard_id as usize).unwrap().prev_block_hash()
+                    for shard_id in (*chunk_producers.get(validator_id)).unwrap_or_default() {
+                        if block.chunks().get(*shard_id as usize).unwrap().prev_block_hash()
                             != &CryptoHash::default()
                         {
                             chunks_count += 1;
