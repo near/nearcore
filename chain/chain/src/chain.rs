429761c69185a4fd59e72e36bc6790526c4e2850
Jan Ciolek <jan@near.org>
2024-02-15 17:09:34 +0000

    fix: get_prev_chunk_header - don't panic when a chunk doesn't exist

--- chain/chain/src/chain.rs
+++ chain/chain/src/chain.rs
@@ -4183,7 +4183,11 @@ impl Chain {
         shard_id: ShardId,
     ) -> Result<ShardChunkHeader, Error> {
         let prev_shard_id = epoch_manager.get_prev_shard_ids(prev_block.hash(), vec![shard_id])?[0];
-        Ok(prev_block.chunks().get(prev_shard_id as usize).unwrap().clone())
+        Ok(prev_block
+            .chunks()
+            .get(prev_shard_id as usize)
+            .ok_or(Error::InvalidShardId(shard_id))?
+            .clone())
     }
 
     pub fn group_receipts_by_shard(
