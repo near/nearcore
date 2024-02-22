10a99ffc28deabba8b398c11de18fc7e76a9e308
Anton Puhach <pugachag@gmail.com>
2024-02-22 14:57:19 +0100

    apply review comments

--- integration-tests/src/tests/client/features/adversarial_behaviors.rs
+++ integration-tests/src/tests/client/features/adversarial_behaviors.rs
@@ -258,20 +258,16 @@ fn test_banning_chunk_producer_when_seeing_invalid_chunk_base(
 
                         // This is the first block with invalid chunks in the current epoch.
                         // In pre-stateless validation protocol the first block with invalid chunks
-                        // was skipped, but stateless validation is able to deal with invalid chunks
-                        // without skipping blocks. The expected behavior depends on whether we are
-                        // using stateless validation or not.
-                        if uses_stateless_validation {
-                            // With stateless validation the block usually isn't skipped. Chunk validators
-                            // won't send chunk endorsements for this chunk, which means that it won't be
-                            // included in the block at all.
-                            this_block_should_be_skipped = false;
-                        } else {
-                            // In the old protocol, chunks are first included in the block and then the block
-                            // is validated. This means that this block, which includes an invalid chunk,
-                            // will be invalid and it should be skipped. Once this happens, the malicious
-                            // chunk producer is banned for the whole epoch and no blocks are skipped until
-                            // we reach the next epoch.
+                        // was skipped.
+                        // In the old protocol, chunks are first included in the block and then the block
+                        // is validated. This means that this block, which includes an invalid chunk,
+                        // will be invalid and it should be skipped. Once this happens, the malicious
+                        // chunk producer is banned for the whole epoch and no blocks are skipped until
+                        // we reach the next epoch.
+                        // With stateless validation the block usually isn't skipped. Chunk validators
+                        // won't send chunk endorsements for this chunk, which means that it won't be
+                        // included in the block at all.
+                        if !uses_stateless_validation {
                             this_block_should_be_skipped = true;
                         }
                     }
