474e90697415f56c04796ec77655e5bcadf8f99b
jaswinder6991 <jassmultani2@gmail.com>
2024-02-27 22:47:16 +0530

    log assert macro inline in primitives

--- core/primitives/src/epoch_manager.rs
+++ core/primitives/src/epoch_manager.rs
@@ -1333,10 +1333,11 @@ pub mod epoch_sync {
             let epoch_first_header = self.get_epoch_first_header()?;
             let header = self.get_header(*hash, EpochSyncHashType::Other)?;
 
-            log_assert!(
-                epoch_first_header.epoch_id() == header.epoch_id(),
-                "We can only correctly reconstruct headers from this epoch"
-            );
+            if epoch_first_header.epoch_id() != header.epoch_id() {
+                let msg = "We can only correctly reconstruct headers from this epoch";
+                debug_assert!(false, "{}", msg);
+                tracing::error!(message = msg);
+            }
 
             let last_finalized_height = if *header.last_final_block() == CryptoHash::default() {
                 0
