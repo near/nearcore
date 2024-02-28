902f15512f6b06e07738d9848ab49ac8a5a5d061
Dimitris Apostolou <dimitris.apostolou@icloud.com>
2024-02-28 20:05:32 +0200

    fix: fix feature = "cargo-clippy" deprecation (#10679)
    https://blog.rust-lang.org/2024/02/28/Clippy-deprecating-feature-cargo-clippy.html

--- runtime/near-vm/engine/src/lib.rs
+++ runtime/near-vm/engine/src/lib.rs
@@ -2,12 +2,9 @@
 
 #![deny(missing_docs, trivial_numeric_casts, unused_extern_crates)]
 #![warn(unused_import_braces)]
+#![cfg_attr(clippy, allow(clippy::new_without_default, clippy::new_without_default))]
 #![cfg_attr(
-    feature = "cargo-clippy",
-    allow(clippy::new_without_default, clippy::new_without_default)
-)]
-#![cfg_attr(
-    feature = "cargo-clippy",
+    clippy,
     warn(
         clippy::float_arithmetic,
         clippy::mut_mut,
