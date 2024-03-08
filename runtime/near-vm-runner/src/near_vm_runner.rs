ee31eb9a05a30600962407bffa195693c8e6b9a4
Simonas Kazlauskas <git@kazlauskas.me>
2024-03-08 11:17:49 +0200

    runtime: hotfix to allow up-to 256 concurrent runtime invocations
    This is not a proper fix as certain configuration settings can still
    make this number to be exceeded, but the changes there are a little more
    involved than what I'd be comfortable with making in a rush, especially
    for a backport to the 1.37 release.
    
    In order to keep the initial memory use reasonably bounded, I have
    chosen to reduce the initial size of each map to a smaller number.
    Since the runtime reuses most recently returned memories first, in
    use-cases where there are few concurrent accesses, only that many
    memories will get resized to fit all the required contracts over time,
    thus keeping the memory use reasonably limited. This will result in
    slightly less predictable function call performance for the first
    invocation of the very few first contract invocations that
    require more memory, but this seems like a required tradeoff to avoid
    preallocating dozens of GB of mappings that will later go mostly unused.

--- runtime/near-vm-runner/src/near_vm_runner.rs
+++ runtime/near-vm-runner/src/near_vm_runner.rs
@@ -258,7 +258,7 @@ impl NearVM {
                 // that particular function call will be slower. Not to mention there isn't a
                 // strong guarantee on the upper bound of the memory that the contract runtime may
                 // require.
-                LimitedMemoryPool::new(8, 64 * 1024 * 1024).unwrap_or_else(|e| {
+                LimitedMemoryPool::new(256, 1 * 1024 * 1024).unwrap_or_else(|e| {
                     panic!("could not pre-allocate resources for the runtime: {e}");
                 })
             })
