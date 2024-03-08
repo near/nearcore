fe15cfaa1e2094db05c1913452138e1c0e0553d9
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2024-03-08 18:43:43 +0200

    fix command for gas estimation

--- runtime/runtime-params-estimator/src/main.rs
+++ runtime/runtime-params-estimator/src/main.rs
@@ -340,7 +340,7 @@ fn main_docker(
         let mut buf = String::new();
         buf.push_str("set -ex;\n");
         buf.push_str("cd /host/nearcore;\n");
-        buf.push_str("cargo build --manifest-path /host/nearcore/Cargo.toml");
+        buf.push_str("CFLAGS='-D__BLST_PORTABLE__' cargo build --manifest-path /host/nearcore/Cargo.toml");
         buf.push_str(" --package runtime-params-estimator --bin runtime-params-estimator");
 
         // Feature "required" is always necessary for accurate measurements.
