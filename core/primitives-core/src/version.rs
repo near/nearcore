0efb74bdedcb0d11255557871db57a452c5b8593
Shreyan Gupta <shreyan@near.org>
2024-03-11 20:47:21 +0530

    [resharding] Nightshade V3 shard layout for protocol version 65 (#10725)
    This PR is to be cherry-picked into release 1.38.0 for splitting shard 2
    into two parts.
    
    Hot and kai-ching both fall on shard 2 which has been causing a lot of
    congestion.
    
    Zulip thread:
    https://near.zulipchat.com/#narrow/stream/308695-nearone.2Fprivate/topic/constant.20congestion.20on.20shard.202/near/425367222
    
    ---------
    
    Co-authored-by: wacban <wac.banasik@gmail.com>

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -113,6 +113,8 @@ pub enum ProtocolFeature {
     /// Resharding V2. A new implementation for resharding and a new shard
     /// layout for the production networks.
     SimpleNightshadeV2,
+    /// Built on top of Resharding V2. Changes shard layout to V3 to split shard 2 into two parts.
+    SimpleNightshadeV3,
     /// In case not all validator seats are occupied our algorithm provide incorrect minimal seat
     /// price - it reports as alpha * sum_stake instead of alpha * sum_stake / (1 - alpha), where
     /// alpha is min stake ratio
@@ -186,6 +188,7 @@ impl ProtocolFeature {
             ProtocolFeature::RestrictTla
             | ProtocolFeature::TestnetFewerBlockProducers
             | ProtocolFeature::SimpleNightshadeV2 => 64,
+            ProtocolFeature::SimpleNightshadeV3 => 65,
 
             // StatelessNet features
             ProtocolFeature::StatelessValidationV0 => 80,
