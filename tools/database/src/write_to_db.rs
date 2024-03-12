afc8f939681d051b56f1a68ffff8e3d2d5f6065e
posvyatokum <posvyatokum@edu.hse.ru>
2024-03-12 18:55:53 +0000

    make command more elaborate to maybe expand it in the future

--- tools/database/src/write_to_db.rs
+++ tools/database/src/write_to_db.rs
@@ -1,10 +1,25 @@
 use near_store::{DBCol, NodeStorage};
 use std::path::Path;
 
+#[derive(clap::Subcommand)]
+enum BlockMiscKeySelector {
+    StateSnapshot,
+}
+
+#[derive(clap::Subcommand)]
+enum ColumnSelector {
+    BlockMisc {
+        #[clap(subcommand)]
+        key: BlockMiscKeySelector,
+    },
+}
+
 #[derive(clap::Args)]
 pub(crate) struct WriteCryptoHashCommand {
     #[clap(long)]
     hash: near_primitives::hash::CryptoHash,
+    #[clap(subcommand)]
+    column: ColumnSelector,
 }
 
 impl WriteCryptoHashCommand {
@@ -23,7 +38,19 @@ impl WriteCryptoHashCommand {
         let storage = opener.open()?;
         let store = storage.get_hot_store();
         let mut store_update = store.store_update();
-        store_update.set_ser(DBCol::BlockMisc, near_store::STATE_SNAPSHOT_KEY, &self.hash)?;
+
+        match &self.column {
+            ColumnSelector::BlockMisc { key } => match key {
+                BlockMiscKeySelector::StateSnapshot => {
+                    store_update.set_ser(
+                        DBCol::BlockMisc,
+                        near_store::STATE_SNAPSHOT_KEY,
+                        &self.hash,
+                    )?;
+                }
+            },
+        }
+
         Ok(store_update.commit()?)
     }
 }
