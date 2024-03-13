7020ba963f33ee3edc7540a458c348421d3f4d1e
wacban <wac.banasik@gmail.com>
2024-03-13 10:04:34 +0100

    feat(congestion): fancy global transaction stop strategy

--- tools/congestion-model/src/strategy/mod.rs
+++ tools/congestion-model/src/strategy/mod.rs
@@ -1,11 +1,13 @@
 use crate::model::ChunkExecutionContext;
 use crate::{QueueId, ShardId};
 
+pub use fancy_global_transaction_stop::FancyGlobalTransactionStop;
 pub use global_tx_stop::GlobalTxStopShard;
 pub use new_tx_last::NewTxLast;
 pub use no_queues::NoQueueShard;
 pub use simple_backpressure::SimpleBackpressure;
 
+mod fancy_global_transaction_stop;
 mod global_tx_stop;
 mod new_tx_last;
 mod no_queues;
