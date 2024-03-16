ba29063908bd25c7bb9c3646ef33dd55d7e8b5b3
Jakob Meier <mail@jakobmeier.ch>
2024-03-16 08:08:29 +0100

    congestion: Max size, gas and bytes in summary (#10793)
    Instead of showing the snapshot of the model in the end, it is better to
    display the max values throughout the model execution in the final
    summary table.
    
    Also add gas and byte values of queues, rather than just the number of
    receipts.
    
    This makes the summary table much more useful to quickly compare the
    performance of many different strategies.

--- tools/congestion-model/src/evaluation/summary_table.rs
+++ tools/congestion-model/src/evaluation/summary_table.rs
@@ -1,27 +1,32 @@
-use crate::{Model, PGAS};
+use super::{GasThroughput, Progress, ShardQueueLengths};
+use crate::PGAS;
 
 pub fn print_summary_header() {
     println!(
-        "{:<25}{:<25}{:>25}{:>25}{:>25}",
-        "WORKLOAD", "STRATEGY", "BURNT GAS", "TRANSACTIONS FINISHED", "MAX QUEUE LEN",
+        "{:<25}{:<25}{:>25}{:>25}{:>16}{:>16}{:>16}",
+        "WORKLOAD",
+        "STRATEGY",
+        "BURNT GAS",
+        "TRANSACTIONS FINISHED",
+        "MAX QUEUE LEN",
+        "MAX QUEUE SIZE",
+        "MAX QUEUE PGAS",
     );
 }
 
-pub fn print_summary_row(model: &Model, workload: &str, strategy: &str) {
-    let queues = model.queue_lengths();
-    let throughput = model.gas_throughput();
-    let progress = model.progress();
-
-    let mut max_queue_len = 0;
-    for q in queues.values() {
-        let len = q.incoming_receipts + q.queued_receipts;
-        max_queue_len = len.max(max_queue_len);
-    }
-
+pub fn print_summary_row(
+    workload: &str,
+    strategy: &str,
+    progress: &Progress,
+    throughput: &GasThroughput,
+    max_queues: &ShardQueueLengths,
+) {
     println!(
-        "{workload:<25}{strategy:<25}{:>20} PGas{:>25}{:>25}",
+        "{workload:<25}{strategy:<25}{:>20} PGas{:>25}{:>16}{:>16}{:>16}",
         throughput.total / PGAS,
         progress.finished_transactions,
-        max_queue_len
+        max_queues.queued_receipts.num,
+        bytesize::ByteSize::b(max_queues.queued_receipts.size),
+        max_queues.queued_receipts.gas / PGAS,
     );
 }
