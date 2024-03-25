ec05a81834df83ac586785075c0da2f9bacda53f
Adam Chudaś <adam@chudas.net>
2024-03-25 17:07:50 +0100

    Unload mem-trie elsewhere

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -2370,8 +2370,6 @@ impl Client {
                 for shard_id in &tracking_shards {
                     let shard_uid =
                         self.epoch_manager.shard_id_to_uid(*shard_id, &epoch_id).unwrap();
-                    // Explicitly unload mem-trie for shard that we start state sync, even if the shard was tracked in the same epoch.
-                    self.runtime_adapter.unload_mem_trie(&shard_uid);
                     match self.state_sync_adapter.clone().read() {
                         Ok(sync_adapter) => sync_adapter.send(
                             shard_uid,
