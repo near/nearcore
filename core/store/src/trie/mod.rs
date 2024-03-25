73b882795298c54bd6ce0df63b8fedadefc81dee
Aleksandr Logunov <alex.logunov@near.org>
2024-03-25 14:21:36 +0400

    fix: record nodes for writes in memtrie (#10841)
    Fixes #10769.
    
    ## UPD: New motivation
    
    While all said below is true, the main motivation is to account for
    nodes impacted at `squash_nodes` which can be touched without much pain
    only at `Trie::update`.
    
    ## Old motivation
    
    The root cause of errors "node not found" on reads from recorded storage
    is that nodes touched only at `Trie::update` - KV updates in the end of
    chunk applictaion - were never recorded on memtrie.
    It happens automatically on disk tries. For memtries we retroactively
    account for trie node accesses, but this wasn't done in similar way for
    memtrie updates.
    While infrastructure indeed needs to be improved so these issues won't
    surprise us anymore, the easiest way is to account for all accesses in
    the same way as `move_node_to_mutable` does - on memtrie paths like
    `ensure_updated` which move node out of storage to memory, which means
    that node has to be recorded, so trie can be validated. Unfortunately
    refcount changes are not enough to cover this, so I record trie accesses
    separately. Also, memtrie doesn't have access to values, so I have to
    retrieve values and record them in the `Trie::update`.
    
    ### Testing
    
    * New logic is covered by trie_recording tests. I also discovered an
    issue there - `destructively_delete_in_memory_state_from_disk` was
    removing all keys because key decoding was incorrect. Now these tests
    fully cover trie access scenario: first we have get&get_ref accesses,
    and in the very end we have update call for some KV pairs. I confirmed
    that for incorrect impl some tests fail because of the same "node not
    found" error, and also that new sanity check on `Trie::update` works.
    * test_memtrie_discrepancy is renamed and works now
    
    ---------
    
    Co-authored-by: Longarithm <the.aleksandr.logunov@gmail.com>

--- core/store/src/trie/mod.rs
+++ core/store/src/trie/mod.rs
@@ -1498,7 +1498,44 @@ impl Trie {
                         None => trie_update.delete(&key),
                     }
                 }
-                Ok(trie_update.to_trie_changes())
+                let (trie_changes, trie_accesses) = trie_update.to_trie_changes();
+
+                // Sanity check for tests: all modified trie items must be
+                // present in ever accessed trie items.
+                #[cfg(test)]
+                {
+                    for t in trie_changes.deletions.iter() {
+                        let hash = t.trie_node_or_value_hash;
+                        assert!(
+                            trie_accesses.values.contains_key(&hash)
+                                || trie_accesses.nodes.contains_key(&hash),
+                            "Hash {} is not present in trie accesses",
+                            hash
+                        );
+                    }
+                }
+
+                // Retroactively record all accessed trie items which are
+                // required to process trie update but were not recorded at
+                // processing lookups.
+                // The main case is a branch with two children, one of which
+                // got removed, so we need to read another one and squash it
+                // together with parent.
+                if let Some(recorder) = &self.recorder {
+                    for (node_hash, serialized_node) in trie_accesses.nodes {
+                        recorder.borrow_mut().record(&node_hash, serialized_node);
+                    }
+                    for (value_hash, value) in trie_accesses.values {
+                        let value = match value {
+                            FlatStateValue::Ref(_) => {
+                                self.storage.retrieve_raw_bytes(&value_hash)?
+                            }
+                            FlatStateValue::Inlined(value) => value.into(),
+                        };
+                        recorder.borrow_mut().record(&value_hash, value);
+                    }
+                }
+                Ok(trie_changes)
             }
             None => {
                 let mut memory = NodesStorage::new();
