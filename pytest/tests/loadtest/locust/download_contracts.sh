99653fce452277fc5680876f8e2880959914338a
tayfunelmas <tayfunelmas@gmail.com>
2024-04-01 18:04:12 -0700

    Update wasm files for locust tests and the download script.

--- pytest/tests/loadtest/locust/download_contracts.sh
+++ pytest/tests/loadtest/locust/download_contracts.sh
@@ -2,8 +2,7 @@
 #
 # Downloads the WASM contracts necessary for all workloads and stores them in "res" folder.
 
-cd res
-wget https://raw.githubusercontent.com/NearSocial/social-db/master/res/social_db_release.wasm -O social_db.wasm
-wget https://raw.githubusercontent.com/sweatco/sweat-near/main/res/sweat.wasm -O sweat.wasm
-ln -s ../../../../../runtime/near-test-contracts/res/fungible_token.wasm fungible_token.wasm
-ln -s ../../../../../runtime/near-test-contracts/res/backwards_compatible_rs_contract.wasm congestion.wasm
+wget https://raw.githubusercontent.com/NearSocial/social-db/master/res/social_db_release.wasm -O res/social_db.wasm
+wget https://raw.githubusercontent.com/sweatco/sweat-near/main/res/sweat.wasm -O res/sweat.wasm
+cp ../../../../runtime/near-test-contracts/res/fungible_token.wasm res/fungible_token.wasm
+cp ../../../../runtime/near-test-contracts/res/backwards_compatible_rs_contract.wasm res/congestion.wasm
