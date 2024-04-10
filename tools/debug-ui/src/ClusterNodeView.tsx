1141947b78c8513eb7d2df87d0b849c6cdf6b72c
Jan Ciolek <149345204+jancionear@users.noreply.github.com>
2024-04-10 19:52:58 +0200

    fix: debug-ui: use tanstack/react-query instead of react-query (#10917)
    Fixes: https://github.com/near/nearcore/issues/10914
    
    I'm not sure if that's the proper solution, would appreciate a review
    from someone familiar with the javascript ecosystem.
    
    I found this solution on stackoverflow:
    
    https://stackoverflow.com/questions/73207151/could-not-find-a-declaration-file-for-module-react-query

--- tools/debug-ui/src/ClusterNodeView.tsx
+++ tools/debug-ui/src/ClusterNodeView.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import {
     SyncStatusResponse,
     TrackedShardsResponse,
--- tools/debug-ui/src/EpochValidatorsView.tsx
+++ tools/debug-ui/src/EpochValidatorsView.tsx
@@ -1,5 +1,5 @@
 import { useId } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { Tooltip } from 'react-tooltip';
 import { ValidatorKickoutReason, fetchEpochInfo } from './api';
 import './EpochValidatorsView.scss';
