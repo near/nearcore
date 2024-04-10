4c10f945705890358d354c5b5c5d7ee9f0d5b330
Jakob Meier <mail@jakobmeier.ch>
2024-04-10 10:03:30 +0200

    congestion: show 90th tx delay percentile in summary (#11008)
    For quickly comparing many different parameters in the fine-tuning
    phase, knowing the median latency is not good enoungh.

--- tools/congestion-model/src/evaluation/summary_table.rs
+++ tools/congestion-model/src/evaluation/summary_table.rs
@@ -3,12 +3,13 @@ use crate::PGAS;
 
 pub fn print_summary_header() {
     println!(
-        "{:<25}{:<25}{:>25}{:>25}{:>16}{:>16}{:>16}{:>16}",
+        "{:<25}{:<25}{:>25}{:>25}{:>16}{:>16}{:>16}{:>16}{:>16}",
         "WORKLOAD",
         "STRATEGY",
         "BURNT GAS",
         "TRANSACTIONS FINISHED",
         "MEDIAN TX DELAY",
+        "90p TX DELAY",
         "MAX QUEUE LEN",
         "MAX QUEUE SIZE",
         "MAX QUEUE PGAS",
@@ -24,10 +25,11 @@ pub fn print_summary_row(
     user_experience: &UserExperience,
 ) {
     println!(
-        "{workload:<25}{strategy:<25}{:>20} PGas{:>25}{:>16}{:>16}{:>16}{:>16}",
+        "{workload:<25}{strategy:<25}{:>20} PGas{:>25}{:>16}{:>16}{:>16}{:>16}{:>16}",
         throughput.total / PGAS,
         progress.finished_transactions,
         user_experience.successful_tx_delay_median,
+        user_experience.successful_tx_delay_90th_percentile,
         max_queues.queued_receipts.num,
         bytesize::ByteSize::b(max_queues.queued_receipts.size),
         max_queues.queued_receipts.gas / PGAS,
