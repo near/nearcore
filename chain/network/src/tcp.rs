24ad8bcb8ca95ae0da95749aab723fb452b5b1f1
Saketh Are <saketh.are@gmail.com>
2024-04-12 12:44:42 -0700

    set recv and send buffer sizes for TIER2 connections

--- chain/network/src/tcp.rs
+++ chain/network/src/tcp.rs
@@ -8,6 +8,10 @@ use std::sync::Mutex;
 
 const LISTENER_BACKLOG: u32 = 128;
 
+// Socket options configuring buffer sizes for TIER2 connections
+const TIER2_SO_RCVBUF: u32 = 1000000;
+const TIER2_SO_SNDBUF: u32 = 1000000;
+
 /// TEST-ONLY: guards ensuring that OS considers the given TCP listener port to be in use until
 /// this OS process is terminated.
 pub(crate) static RESERVED_LISTENER_ADDRS: Lazy<
@@ -87,6 +91,23 @@ impl Stream {
         let addr = peer_info
             .addr
             .ok_or_else(|| anyhow!("Trying to connect to peer with no public address"))?;
+
+        let socket = match addr {
+            std::net::SocketAddr::V4(_) => tokio::net::TcpSocket::new_v4()?,
+            std::net::SocketAddr::V6(_) => tokio::net::TcpSocket::new_v6()?,
+        };
+
+        match tier {
+            Tier::T2 => {
+                socket.set_recv_buffer_size(TIER2_SO_RCVBUF)?;
+                socket.set_send_buffer_size(TIER2_SO_SNDBUF)?;
+
+                tracing::debug!(target: "network", "SO_RCVBUF wanted {} got {:?}", TIER2_SO_RCVBUF, socket.recv_buffer_size());
+                tracing::debug!(target: "network", "SO_SNDBUF wanted {} got {:?}", TIER2_SO_SNDBUF, socket.send_buffer_size());
+            }
+            _ => {}
+        };
+
         // The `connect` may take several minutes. This happens when the
         // `SYN` packet for establishing a TCP connection gets silently
         // dropped, in which case the default TCP timeout is applied. That's
@@ -95,12 +116,9 @@ impl Stream {
         // Why exactly a second? It was hard-coded in a library we used
         // before, so we keep it to preserve behavior. Removing the timeout
         // completely was observed to break stuff for real on the testnet.
-        let stream = tokio::time::timeout(
-            std::time::Duration::from_secs(1),
-            tokio::net::TcpStream::connect(addr),
-        )
-        .await?
-        .context("TcpStream::connect()")?;
+        let stream = tokio::time::timeout(std::time::Duration::from_secs(1), socket.connect(addr))
+            .await?
+            .context("TcpStream::connect()")?;
         Ok(Stream::new(stream, StreamType::Outbound { peer_id: peer_info.id.clone(), tier })?)
     }
 
