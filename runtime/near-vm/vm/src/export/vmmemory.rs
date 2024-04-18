7682eb458dc1833b661178e5550cda96894f68b5
Léo Gaspard <leo@near.org>
2024-04-18 14:26:24 +0000

    reduce access to VMMemory::from to read-only

--- runtime/near-vm/vm/src/export/vmmemory.rs
+++ runtime/near-vm/vm/src/export/vmmemory.rs
@@ -7,7 +7,7 @@ use std::sync::Arc;
 #[derive(Debug, Clone)]
 pub struct VMMemory {
     /// Pointer to the containing `Memory`.
-    pub from: Arc<dyn Memory>,
+    from: Arc<dyn Memory>,
 
     /// A “reference” to the instance through the
     /// `InstanceRef`. `None` if it is a host memory.
@@ -27,6 +27,16 @@ unsafe impl Send for VMMemory {}
 unsafe impl Sync for VMMemory {}
 
 impl VMMemory {
+    /// Create a new `VMMemory`
+    pub fn new(from: Arc<dyn Memory>, instance_ref: Option<WeakOrStrongInstanceRef>) -> Self {
+        Self { from, instance_ref }
+    }
+
+    /// Retrieve the memory this export is having
+    pub fn from(&self) -> &Arc<dyn Memory> {
+        &self.from
+    }
+
     /// Get the type for this exported memory
     pub fn ty(&self) -> MemoryType {
         self.from.ty()
