ce6595ba71a6b151041fbd016f6a816675398b8e
Michael Birch <michael.birch@aurora.dev>
2024-04-19 17:19:14 +0200

    Perform DB migration on EpochSummary

--- core/primitives/src/types/chunk_validator_stats.rs
+++ core/primitives/src/types/chunk_validator_stats.rs
@@ -1,17 +1,13 @@
 use {
     super::{NumBlocks, ValidatorStats},
     borsh::{self, BorshDeserialize, BorshSerialize},
-    std::{
-        borrow::Cow,
-        io::{self, Read, Write},
-    },
 };
 
 /// An extension to `ValidatorStats` which also tracks endorsements
 /// coming from stateless validators. This struct is backwards compatible
-/// with `ValidatorStats` in the sense that the V1 serialization is identical
-/// to the original `ValidatorStats`.
-#[derive(Clone, Debug, Eq)]
+/// with `ValidatorStats` in the sense that the V1 variant contains the
+/// original type.
+#[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq)]
 pub enum ChunkValidatorStats {
     V1(ValidatorStats),
     V2(ChunkValidatorStatsV2),
@@ -42,28 +38,24 @@ impl PartialEq for ChunkValidatorStats {
 
 impl ChunkValidatorStats {
     const DEFAULT_STATS: ValidatorStats = ValidatorStats { produced: 0, expected: 0 };
-    /// We assume the that number of chunks produced by a single validator never exceeds
-    /// this number, otherwise we will not be able to accurately distinguish
-    /// V1 vs V2 serialized stats.
-    const V1_CUTOFF: u64 = 0xffffffff;
 
     pub const fn new() -> Self {
         Self::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(0), expected: NU64(0) },
+            production: ValidatorStats { produced: 0, expected: 0 },
             endorsement: ValidatorStats { produced: 0, expected: 0 },
         })
     }
 
     pub const fn new_with_production(produced: u64, expected: u64) -> Self {
         Self::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(produced), expected: NU64(expected) },
+            production: ValidatorStats { produced, expected },
             endorsement: ValidatorStats { produced: 0, expected: 0 },
         })
     }
 
     pub const fn new_with_endorsement(produced: u64, expected: u64) -> Self {
         Self::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(0), expected: NU64(0) },
+            production: ValidatorStats { produced: 0, expected: 0 },
             endorsement: ValidatorStats { produced, expected },
         })
     }
@@ -71,28 +63,28 @@ impl ChunkValidatorStats {
     pub fn produced(&self) -> NumBlocks {
         match self {
             Self::V1(v1) => v1.produced,
-            Self::V2(v2) => v2.production.produced.0,
+            Self::V2(v2) => v2.production.produced,
         }
     }
 
     pub fn expected(&self) -> NumBlocks {
         match self {
             Self::V1(v1) => v1.expected,
-            Self::V2(v2) => v2.production.expected.0,
+            Self::V2(v2) => v2.production.expected,
         }
     }
 
     pub fn produced_mut(&mut self) -> &mut NumBlocks {
         match self {
             Self::V1(v1) => &mut v1.produced,
-            Self::V2(v2) => &mut v2.production.produced.0,
+            Self::V2(v2) => &mut v2.production.produced,
         }
     }
 
     pub fn expected_mut(&mut self) -> &mut NumBlocks {
         match self {
             Self::V1(v1) => &mut v1.expected,
-            Self::V2(v2) => &mut v2.production.expected.0,
+            Self::V2(v2) => &mut v2.production.expected,
         }
     }
 
@@ -107,10 +99,7 @@ impl ChunkValidatorStats {
         match self {
             Self::V1(v1) => {
                 *self = Self::V2(ChunkValidatorStatsV2 {
-                    production: NValidatorStats {
-                        produced: NU64(v1.produced),
-                        expected: NU64(v1.expected),
-                    },
+                    production: ValidatorStats { produced: v1.produced, expected: v1.expected },
                     endorsement: ValidatorStats::default(),
                 });
                 match self {
@@ -125,37 +114,8 @@ impl ChunkValidatorStats {
 
 #[derive(Default, BorshSerialize, BorshDeserialize, Clone, Debug, PartialEq, Eq)]
 pub struct ChunkValidatorStatsV2 {
-    pub production: NValidatorStats,
+    pub production: ValidatorStats,
     pub endorsement: ValidatorStats,
-}
-
-/// Identical to `ValidatorStats`, but with different serialization
-/// to allow distinguishing later versions of `ChunkValidatorStats`
-/// from the original `ValidatorStats`.
-#[derive(Default, BorshSerialize, BorshDeserialize, Clone, Debug, PartialEq, Eq)]
-pub struct NValidatorStats {
-    pub produced: NU64,
-    pub expected: NU64,
-}
-
-/// Wrapper around `u64` with a different Borsh serialization.
-/// This allows distinguishing later versions of `ChunkValidatorStats`
-/// from the original `ValidatorStats`.
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct NU64(pub u64);
-
-impl BorshSerialize for NU64 {
-    fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        let x = !self.0;
-        x.serialize(writer)
-    }
-}
-
-impl BorshDeserialize for NU64 {
-    fn deserialize_reader<R: Read>(reader: &mut R) -> io::Result<Self> {
-        let x: u64 = BorshDeserialize::deserialize_reader(reader)?;
-        Ok(Self(!x))
-    }
 }
 
 // If the endorsement part of V2 is all zeros then it can possibly
@@ -163,71 +123,8 @@ impl BorshDeserialize for NU64 {
 fn v1_v2_eq(v1: &ValidatorStats, v2: &ChunkValidatorStatsV2) -> bool {
     v2.endorsement.produced == 0
         && v2.endorsement.expected == 0
-        && v1.produced == v2.production.produced.0
-        && v1.expected == v2.production.expected.0
-}
-
-/// This type is structurally is identical to `ChunkValidatorStats`.
-/// The difference is that this type derives the borsh traits to avoid
-/// boiler-plate, while `ChunkValidatorStats` has a custom serialization
-/// implementation for backwards compatibility with `ValidatorStats`.
-#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq)]
-enum AutoBorsh<'a> {
-    V1(Cow<'a, ValidatorStats>),
-    V2(Cow<'a, ChunkValidatorStatsV2>),
-}
-
-impl<'a> From<&'a ChunkValidatorStats> for AutoBorsh<'a> {
-    fn from(value: &'a ChunkValidatorStats) -> Self {
-        match value {
-            ChunkValidatorStats::V1(v1) => Self::V1(Cow::Borrowed(v1)),
-            ChunkValidatorStats::V2(v2) => Self::V2(Cow::Borrowed(v2)),
-        }
-    }
-}
-
-impl<'a> From<AutoBorsh<'a>> for ChunkValidatorStats {
-    fn from(value: AutoBorsh<'a>) -> Self {
-        match value {
-            AutoBorsh::V1(v1) => Self::V1(v1.into_owned()),
-            AutoBorsh::V2(v2) => Self::V2(v2.into_owned()),
-        }
-    }
-}
-
-impl BorshSerialize for ChunkValidatorStats {
-    fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        match self {
-            // V1 is backwards compatible with `ValidatorStats`
-            Self::V1(v1) => v1.serialize(writer),
-            // Later versions follow the usual borsh serialization convention
-            later_version => AutoBorsh::from(later_version).serialize(writer),
-        }
-    }
-}
-
-impl BorshDeserialize for ChunkValidatorStats {
-    fn deserialize_reader<R: Read>(reader: &mut R) -> io::Result<Self> {
-        let x: u64 = BorshDeserialize::deserialize_reader(reader)?;
-
-        // If the first 8 bytes deserialize as a small u64 number
-        // then it must be a V1 type because the V2 type uses `NU64`,
-        // which would look like big u64 numbers under normal borsh rules.
-        if x <= Self::V1_CUTOFF {
-            let expected: u64 = BorshDeserialize::deserialize_reader(reader)?;
-            let v1 = ValidatorStats { produced: x, expected };
-            return Ok(Self::V1(v1));
-        }
-
-        // Serialized size is 1 + 8 + 8 + 8 + 8
-        // (enum variant byte, production stats, endorsement stats)
-        let mut serialized_bytes = [0u8; 33];
-        serialized_bytes[0..8].copy_from_slice(&x.to_le_bytes());
-        reader.read_exact(&mut serialized_bytes[8..33])?;
-
-        let auto_borsh: AutoBorsh = borsh::from_slice(&serialized_bytes)?;
-        Ok(auto_borsh.into())
-    }
+        && v1.produced == v2.production.produced
+        && v1.expected == v2.production.expected
 }
 
 #[test]
@@ -248,7 +145,7 @@ fn test_mutability() {
     assert_eq!(
         stats,
         ChunkValidatorStats::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(1), expected: NU64(1) },
+            production: ValidatorStats { produced: 1, expected: 1 },
             endorsement: ValidatorStats { produced: 10, expected: 10 }
         })
     );
@@ -257,7 +154,7 @@ fn test_mutability() {
     assert_eq!(
         stats,
         ChunkValidatorStats::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(1), expected: NU64(2) },
+            production: ValidatorStats { produced: 1, expected: 2 },
             endorsement: ValidatorStats { produced: 10, expected: 10 }
         })
     );
@@ -266,7 +163,7 @@ fn test_mutability() {
     assert_eq!(
         stats,
         ChunkValidatorStats::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(2), expected: NU64(2) },
+            production: ValidatorStats { produced: 2, expected: 2 },
             endorsement: ValidatorStats { produced: 10, expected: 10 }
         })
     );
@@ -278,7 +175,7 @@ fn test_mutability() {
     assert_eq!(
         stats,
         ChunkValidatorStats::V2(ChunkValidatorStatsV2 {
-            production: NValidatorStats { produced: NU64(2), expected: NU64(2) },
+            production: ValidatorStats { produced: 2, expected: 2 },
             endorsement: ValidatorStats { produced: 20, expected: 20 }
         })
     );
@@ -293,30 +190,4 @@ fn test_eq() {
 
     // V1 and V2 can be equal if the endorsements parts of V2 are zero.
     assert_eq!(v1, v2);
-}
-
-#[test]
-fn test_borsh_serialization() {
-    let stats = ValidatorStats {
-        produced: ChunkValidatorStats::V1_CUTOFF - 1,
-        expected: ChunkValidatorStats::V1_CUTOFF,
-    };
-    let serialized = borsh::to_vec(&stats).unwrap();
-
-    // `ChunkValidatorStats` is backwards compatible with `ValidatorStats` so
-    // we can deserialize into `ChunkValidatorStats`.
-    let result: ChunkValidatorStats = borsh::from_slice(&serialized).unwrap();
-    assert_eq!(result, ChunkValidatorStats::V1(stats));
-
-    let v2_stats = ChunkValidatorStats::V2(ChunkValidatorStatsV2 {
-        production: NValidatorStats {
-            produced: NU64(ChunkValidatorStats::V1_CUTOFF - 1),
-            expected: NU64(ChunkValidatorStats::V1_CUTOFF),
-        },
-        endorsement: ValidatorStats { produced: 0, expected: 0 },
-    });
-    let serialized = borsh::to_vec(&v2_stats).unwrap();
-
-    let result: ChunkValidatorStats = borsh::from_slice(&serialized).unwrap();
-    assert_eq!(result, v2_stats);
 }
