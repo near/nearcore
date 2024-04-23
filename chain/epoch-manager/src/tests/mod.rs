f95087bc508eda09ceecd7b8cb4a113b109cef7f
Michael Birch <michael.birch@aurora.dev>
2024-04-23 06:06:49 -0400

    Feat(stateless-validation): Rewards for stateless validators (#11121)
    The goal of this PR is to give rewards to stateless validators
    (validators which do not produce blocks or chunks but do validate chunks
    by producing endorsements).
    
    This means updating `EpochInfoAggregator` to track the expected number
    of endorsements for each validator as well as how many were actually
    produced. For the MVP we are not actually tracking endorsements
    directly, instead we assume that if a chunk was included in a block then
    all stateless validators for that shard should be rewarded (because
    there must have been a sufficient number of endorsements for the block
    producer to include the chunk). However, this logic will need to be
    iterated on in future versions of the stateless validation protocol.
    Additionally, the reward calculator required updating to use the new
    endorsement stats as part of the uptime calculation which forms the
    basis of the rewards.
    
    Both of those changes are relatively minor. The technical snag in all of
    this is that the `EpochInfoAggregator` and other structures related to
    validator stats are persisted in the node state via borsh serialization.
    ~To avoid a large database migration, I have made this update backwards
    compatible with the old format without the endorsement stats. The logic
    for this is in `core/primitives/src/types/chunk_validator_stats.rs`. The
    legacy variant is distinguished from the new one in the serialization by
    serializing the one's compliment of the `u64` values for the new
    variant. During deserialization we check if the first `u64` is larger
    than 2^32 - 1 or not. If it is larger then we assume the value is
    actually a one's compliment and proceed with the new variant
    deserialization, and otherwise we do the legacy deserialization.~
    
    ~This means that the serialized values can be interpreted improperly if
    2^32 (4.29 billion) or more chunks are produced by a single validator
    during one epoch. I do not expect this to be an issue, but it is a
    limitation worth documenting.~
    
    @Longarithm told me that the DB migration is actually fine here because
    it's not that much data. So the DB migration of the `EpochValidatorInfo`
    column is also implemented.

--- chain/epoch-manager/src/tests/mod.rs
+++ chain/epoch-manager/src/tests/mod.rs
@@ -729,7 +729,7 @@ fn test_validator_reward_one_validator() {
         "test2".parse().unwrap(),
         BlockChunkValidatorStats {
             block_stats: ValidatorStats { produced: 1, expected: 1 },
-            chunk_stats: ValidatorStats { produced: 1, expected: 1 },
+            chunk_stats: ChunkValidatorStats::new_with_production(1, 1),
         },
     );
     let mut validator_stakes = HashMap::new();
@@ -819,14 +819,14 @@ fn test_validator_reward_weight_by_stake() {
         "test1".parse().unwrap(),
         BlockChunkValidatorStats {
             block_stats: ValidatorStats { produced: 1, expected: 1 },
-            chunk_stats: ValidatorStats { produced: 1, expected: 1 },
+            chunk_stats: ChunkValidatorStats::new_with_production(1, 1),
         },
     );
     validator_online_ratio.insert(
         "test2".parse().unwrap(),
         BlockChunkValidatorStats {
             block_stats: ValidatorStats { produced: 1, expected: 1 },
-            chunk_stats: ValidatorStats { produced: 1, expected: 1 },
+            chunk_stats: ChunkValidatorStats::new_with_production(1, 1),
         },
     );
     let mut validators_stakes = HashMap::new();
@@ -942,7 +942,7 @@ fn test_reward_multiple_shards() {
         "test2".parse().unwrap(),
         BlockChunkValidatorStats {
             block_stats: ValidatorStats { produced: 1, expected: 1 },
-            chunk_stats: ValidatorStats { produced: 1, expected: 1 },
+            chunk_stats: ChunkValidatorStats::new_with_production(1, 1),
         },
     );
     let mut validators_stakes = HashMap::new();
@@ -2591,18 +2591,18 @@ fn test_validator_kickout_sanity() {
             (
                 0,
                 HashMap::from([
-                    (0, ValidatorStats { produced: 100, expected: 100 }),
-                    (1, ValidatorStats { produced: 80, expected: 100 }),
-                    (2, ValidatorStats { produced: 70, expected: 100 }),
+                    (0, ChunkValidatorStats::new_with_production(100, 100)),
+                    (1, ChunkValidatorStats::new_with_production(80, 100)),
+                    (2, ChunkValidatorStats::new_with_production(70, 100)),
                 ]),
             ),
             (
                 1,
                 HashMap::from([
-                    (0, ValidatorStats { produced: 70, expected: 100 }),
-                    (1, ValidatorStats { produced: 79, expected: 100 }),
-                    (3, ValidatorStats { produced: 100, expected: 100 }),
-                    (4, ValidatorStats { produced: 100, expected: 100 }),
+                    (0, ChunkValidatorStats::new_with_production(70, 100)),
+                    (1, ChunkValidatorStats::new_with_production(79, 100)),
+                    (3, ChunkValidatorStats::new_with_production(100, 100)),
+                    (4, ChunkValidatorStats::new_with_production(100, 100)),
                 ]),
             ),
         ]),
@@ -2624,35 +2624,35 @@ fn test_validator_kickout_sanity() {
                 "test0".parse().unwrap(),
                 BlockChunkValidatorStats {
                     block_stats: ValidatorStats { produced: 100, expected: 100 },
-                    chunk_stats: ValidatorStats { produced: 170, expected: 200 }
+                    chunk_stats: ChunkValidatorStats::new_with_production(170, 200),
                 }
             ),
             (
                 "test1".parse().unwrap(),
                 BlockChunkValidatorStats {
                     block_stats: ValidatorStats { produced: 90, expected: 100 },
-                    chunk_stats: ValidatorStats { produced: 159, expected: 200 }
+                    chunk_stats: ChunkValidatorStats::new_with_production(159, 200),
                 }
             ),
             (
                 "test2".parse().unwrap(),
                 BlockChunkValidatorStats {
                     block_stats: ValidatorStats { produced: 100, expected: 100 },
-                    chunk_stats: ValidatorStats { produced: 70, expected: 100 }
+                    chunk_stats: ChunkValidatorStats::new_with_production(70, 100),
                 }
             ),
             (
                 "test3".parse().unwrap(),
                 BlockChunkValidatorStats {
                     block_stats: ValidatorStats { produced: 89, expected: 100 },
-                    chunk_stats: ValidatorStats { produced: 100, expected: 100 }
+                    chunk_stats: ChunkValidatorStats::new_with_production(100, 100),
                 }
             ),
             (
                 "test4".parse().unwrap(),
                 BlockChunkValidatorStats {
                     block_stats: ValidatorStats { produced: 0, expected: 0 },
-                    chunk_stats: ValidatorStats { produced: 100, expected: 100 }
+                    chunk_stats: ChunkValidatorStats::new_with_production(100, 100),
                 }
             ),
         ])
@@ -2696,15 +2696,15 @@ fn test_max_kickout_stake_ratio() {
         (
             0,
             HashMap::from([
-                (0, ValidatorStats { produced: 0, expected: 100 }),
-                (1, ValidatorStats { produced: 0, expected: 100 }),
+                (0, ChunkValidatorStats::new_with_production(0, 100)),
+                (1, ChunkValidatorStats::new_with_production(0, 100)),
             ]),
         ),
         (
             1,
             HashMap::from([
-                (2, ValidatorStats { produced: 100, expected: 100 }),
-                (4, ValidatorStats { produced: 50, expected: 100 }),
+                (2, ChunkValidatorStats::new_with_production(100, 100)),
+                (4, ChunkValidatorStats::new_with_production(50, 100)),
             ]),
         ),
     ]);
@@ -2735,35 +2735,35 @@ fn test_max_kickout_stake_ratio() {
             "test0".parse().unwrap(),
             BlockChunkValidatorStats {
                 block_stats: ValidatorStats { produced: 50, expected: 100 },
-                chunk_stats: ValidatorStats { produced: 0, expected: 100 },
+                chunk_stats: ChunkValidatorStats::new_with_production(0, 100),
             },
         ),
         (
             "test1".parse().unwrap(),
             BlockChunkValidatorStats {
                 block_stats: ValidatorStats { produced: 70, expected: 100 },
-                chunk_stats: ValidatorStats { produced: 0, expected: 100 },
+                chunk_stats: ChunkValidatorStats::new_with_production(0, 100),
             },
         ),
         (
             "test2".parse().unwrap(),
             BlockChunkValidatorStats {
                 block_stats: ValidatorStats { produced: 70, expected: 100 },
-                chunk_stats: ValidatorStats { produced: 100, expected: 100 },
+                chunk_stats: ChunkValidatorStats::new_with_production(100, 100),
             },
         ),
         (
             "test3".parse().unwrap(),
             BlockChunkValidatorStats {
                 block_stats: ValidatorStats { produced: 0, expected: 0 },
-                chunk_stats: ValidatorStats { produced: 0, expected: 0 },
+                chunk_stats: ChunkValidatorStats::default(),
             },
         ),
         (
             "test4".parse().unwrap(),
             BlockChunkValidatorStats {
                 block_stats: ValidatorStats { produced: 0, expected: 0 },
-                chunk_stats: ValidatorStats { produced: 50, expected: 100 },
+                chunk_stats: ChunkValidatorStats::new_with_production(50, 100),
             },
         ),
     ]);
