984aefba13adbaa2b1a2d0914f9cbc0d48f69c20
Simonas Kazlauskas <git@kazlauskas.me>
2024-04-30 14:18:22 +0300

    replace counter with a histogram

--- runtime/near-vm-runner/src/metrics.rs
+++ runtime/near-vm-runner/src/metrics.rs
@@ -1,24 +1,29 @@
-use near_o11y::metrics::{try_create_int_counter_vec, IntCounterVec};
+use near_o11y::metrics::{try_create_histogram_vec, HistogramVec};
 use near_parameters::vm::VMKind;
 use once_cell::sync::Lazy;
-use std::cell::RefCell;
+use std::{cell::RefCell, time::Duration};
 
 thread_local! {
-    static METRICS: RefCell<Metrics> = const { RefCell::new(Metrics { near_vm_compilations: 0 }) };
+    static METRICS: RefCell<Metrics> = const { RefCell::new(Metrics {
+        near_vm_compilation_time: Duration::new(0, 0),
+        wasmtime_compilation_time: Duration::new(0, 0),
+    }) };
 }
 
-pub static COMPILATIONS: Lazy<IntCounterVec> = Lazy::new(|| {
-    try_create_int_counter_vec(
-        "near_vm_runner_compilations_total",
-        "How many compilations have occurred so far?",
+pub static COMPILATION_TIME: Lazy<HistogramVec> = Lazy::new(|| {
+    try_create_histogram_vec(
+        "near_vm_runner_compilation_seconds",
+        "Histogram of how long it takes to compile things",
         &["vm_kind", "shard_id"],
+        None,
     )
     .unwrap()
 });
 
 #[derive(Default, Copy, Clone)]
 pub struct Metrics {
-    near_vm_compilations: u64,
+    near_vm_compilation_time: Duration,
+    wasmtime_compilation_time: Duration,
 }
 
 impl Metrics {
@@ -34,16 +39,26 @@ impl Metrics {
     }
 
     pub fn report(&mut self, shard_id: &str) {
-        COMPILATIONS.with_label_values(&["near_vm", shard_id]).inc_by(self.near_vm_compilations);
-        self.near_vm_compilations = 0;
+        if !self.near_vm_compilation_time.is_zero() {
+            COMPILATION_TIME
+                .with_label_values(&["near_vm", shard_id])
+                .observe(self.near_vm_compilation_time.as_secs_f64());
+            self.near_vm_compilation_time = Duration::default();
+        }
+        if !self.wasmtime_compilation_time.is_zero() {
+            COMPILATION_TIME
+                .with_label_values(&["wasmtime", shard_id])
+                .observe(self.wasmtime_compilation_time.as_secs_f64());
+            self.wasmtime_compilation_time = Duration::default();
+        }
     }
 }
 
-pub(crate) fn compilation_event(kind: VMKind) {
+pub(crate) fn compilation_duration(kind: VMKind, duration: Duration) {
     METRICS.with_borrow_mut(|m| match kind {
         VMKind::Wasmer0 => {}
-        VMKind::Wasmtime => {}
+        VMKind::Wasmtime => m.wasmtime_compilation_time += duration,
         VMKind::Wasmer2 => {}
-        VMKind::NearVm => m.near_vm_compilations += 1,
+        VMKind::NearVm => m.near_vm_compilation_time += duration,
     });
 }
