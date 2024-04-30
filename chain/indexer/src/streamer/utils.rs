fe13eea08ebe3618c83703684cde517d05b7f200
Eugene The Dream <ek@remote>
2024-04-30 14:51:34 -0700

    Revert Cargo.lock to master. Remove unnecessary utils change

--- chain/indexer/src/streamer/utils.rs
+++ chain/indexer/src/streamer/utils.rs
@@ -45,7 +45,7 @@ pub(crate) async fn convert_transactions_sir_into_local_receipts(
                     },
                     prev_block_gas_price,
                     true,
-                    block.header.latest_protocol_version,
+                    protocol_version,
                 );
                 views::ReceiptView {
                     predecessor_id: tx.transaction.signer_id.clone(),
