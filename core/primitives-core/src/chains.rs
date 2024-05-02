f9f0b3e279ffa8a957205e985bf5cc4ee759b162
tayfunelmas <tayfunelmas@gmail.com>
2024-05-02 14:24:40 -0700

    Fix typo

--- core/primitives-core/src/chains.rs
+++ core/primitives-core/src/chains.rs
@@ -9,5 +9,5 @@ pub const TESTNET: &str = "testnet";
 /// Temporary StatelessNet active from 2024-02-01.
 pub const STATELESSNET: &str = "statelessnet";
 
-/// Pre-reloease testing environment.
+/// Pre-release testing environment.
 pub const MOCKNET: &str = "mocknet";
