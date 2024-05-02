08939e002c9386178299bb6f3b79e9f7b590fec5
Shreyan Gupta <shreyan@near.org>
2024-05-02 13:14:45 -0700

    handle

--- chain/client/src/test_utils/test_loop/partial_witness_actor.rs
+++ chain/client/src/test_utils/test_loop/partial_witness_actor.rs
@@ -1,4 +1,5 @@
 use crate::{PartialWitnessActor, PartialWitnessSenderForClientMessage};
+use near_async::messaging::Handler;
 use near_async::test_loop::event_handler::LoopEventHandler;
 use near_network::state_witness::PartialWitnessSenderForNetworkMessage;
 
@@ -6,22 +7,22 @@ pub fn forward_messages_from_network_to_partial_witness_actor(
 ) -> LoopEventHandler<PartialWitnessActor, PartialWitnessSenderForNetworkMessage> {
     LoopEventHandler::new_simple(|msg, partial_witness_actor: &mut PartialWitnessActor| match msg {
         PartialWitnessSenderForNetworkMessage::_chunk_state_witness_ack(msg) => {
-            partial_witness_actor.handle_chunk_state_witness_ack(msg.0);
+            partial_witness_actor.handle(msg);
         }
         PartialWitnessSenderForNetworkMessage::_partial_encoded_state_witness(msg) => {
-            partial_witness_actor.handle_partial_encoded_state_witness(msg.0).unwrap();
+            partial_witness_actor.handle(msg);
         }
         PartialWitnessSenderForNetworkMessage::_partial_encoded_state_witness_forward(msg) => {
-            partial_witness_actor.handle_partial_encoded_state_witness_forward(msg.0).unwrap();
+            partial_witness_actor.handle(msg);
         }
     })
 }
 
 pub fn forward_messages_from_client_to_partial_witness_actor(
 ) -> LoopEventHandler<PartialWitnessActor, PartialWitnessSenderForClientMessage> {
-    LoopEventHandler::new_simple(|msg, state_partial_actor: &mut PartialWitnessActor| match msg {
+    LoopEventHandler::new_simple(|msg, partial_witness_actor: &mut PartialWitnessActor| match msg {
         PartialWitnessSenderForClientMessage::_distribute_chunk_state_witness(msg) => {
-            state_partial_actor.handle_distribute_state_witness_request(msg).unwrap();
+            partial_witness_actor.handle(msg);
         }
     })
 }
