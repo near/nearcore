942652c17a90494103f172be36af06969fd93672
wacban <wac.banasik@gmail.com>
2024-05-03 08:15:32 +0000

    more todos

--- chain/client/src/stateless_validation/chunk_validator/mod.rs
+++ chain/client/src/stateless_validation/chunk_validator/mod.rs
@@ -278,14 +278,10 @@ pub(crate) fn pre_validate_chunk_state_witness(
     }
 
     let main_transition_params = if last_chunk_block.header().is_genesis() {
-        // TODO(congestion_control): check if this epoch_id / protocol version is right for genesis
-        let epoch_id = epoch_manager
-            .get_next_epoch_id_from_prev_block(last_chunk_block.header().prev_hash())?;
+        let epoch_id = last_chunk_block.header().epoch_id();
+        let genesis_protocol_version = epoch_manager.get_epoch_protocol_version(&epoch_id)?;
         MainTransition::Genesis {
-            chunk_extra: chain.genesis_chunk_extra(
-                shard_id,
-                epoch_manager.get_epoch_protocol_version(&epoch_id)?,
-            )?,
+            chunk_extra: chain.genesis_chunk_extra(shard_id, genesis_protocol_version)?,
             block_hash: *last_chunk_block.hash(),
             shard_id,
         }
