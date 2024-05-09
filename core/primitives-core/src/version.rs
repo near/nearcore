77d40ee94736b318a410c9573bbf17c10246710f
Simonas Kazlauskas <git@kazlauskas.me>
2024-05-09 23:49:29 +0300

    yield_resume: stabilize (#11277)
    I believe I missed nothing, but let me know if I did.
    
    cc @posvyatokum

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -207,9 +207,7 @@ impl ProtocolFeature {
             | ProtocolFeature::SimpleNightshadeV2 => 64,
             ProtocolFeature::SimpleNightshadeV3 => 65,
             ProtocolFeature::DecreaseFunctionCallBaseCost => 66,
-
-            // Nightly features which should be tested for compatibility with resharding
-            ProtocolFeature::YieldExecution => 78,
+            ProtocolFeature::YieldExecution => 67,
 
             // This protocol version is reserved for use in resharding tests. An extra resharding
             // is simulated on top of the latest shard layout in production. Note that later
