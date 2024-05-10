5b5d9ba78a927c48d17b883edd5c9978f7e2ab18
Saketh Are <saketh.are@gmail.com>
2024-05-10 12:09:55 -0400

    refactor

--- integration-tests/src/tests/client/features/yield_timeouts.rs
+++ integration-tests/src/tests/client/features/yield_timeouts.rs
@@ -11,20 +11,13 @@ use near_primitives::transaction::{
 use near_primitives::views::FinalExecutionStatus;
 use nearcore::test_utils::TestEnvNightshadeSetupExt;
 
-/// Creates a promise yield, advances sufficiently many blocks,
-/// then verifies that the callback was executed by timeout.
-#[test]
-fn simple_yield_timeout() {
+/// Create environment with an unresolved promise yield callback.
+fn prepare_env_with_yield() -> (TestEnv, CryptoHash) {
     init_test_logger();
-    let mut genesis = Genesis::test(vec!["test0".parse().unwrap(), "test1".parse().unwrap()], 1);
+    let genesis = Genesis::test(vec!["test0".parse().unwrap(), "test1".parse().unwrap()], 1);
     let mut env = TestEnv::builder(&genesis.config).nightshade_runtimes(&genesis).build();
     let genesis_block = env.clients[0].chain.get_block_by_height(0).unwrap();
     let signer = InMemorySigner::from_seed("test0".parse().unwrap(), KeyType::ED25519, "test0");
-
-    let yield_timeout_length =
-        RuntimeConfig::test().wasm_config.limit_config.yield_timeout_length_in_blocks;
-
-    let mut next_block_height = 1;
 
     // Submit transaction deploying contract to test0
     let tx = SignedTransaction::from_actions(
@@ -41,9 +34,8 @@ fn simple_yield_timeout() {
     assert_eq!(env.clients[0].process_tx(tx, false, false), ProcessTxResponse::ValidTx);
 
     // Allow two blocks for the contract to be deployed
-    for _ in 0..2 {
-        env.produce_block(0, next_block_height);
-        next_block_height += 1;
+    for i in 1..3 {
+        env.produce_block(0, i);
     }
     assert!(matches!(
         env.clients[0].chain.get_final_transaction_result(&tx_hash).unwrap().status,
@@ -70,15 +62,25 @@ fn simple_yield_timeout() {
         ProcessTxResponse::ValidTx
     );
 
-    // Allow two blocks for the function call to occur
-    for _ in 0..2 {
-        env.produce_block(0, next_block_height);
-        next_block_height += 1;
+    // Allow two blocks for the function call to be executed
+    for i in 3..5 {
+        env.produce_block(0, i);
     }
+
+    (env, yield_tx_hash)
+}
+
+/// Advances sufficiently many blocks, then verifies that the callback was executed by timeout.
+#[test]
+fn simple_yield_timeout() {
+    let (mut env, yield_tx_hash) = prepare_env_with_yield();
+
+    let yield_timeout_length =
+        RuntimeConfig::test().wasm_config.limit_config.yield_timeout_length_in_blocks;
+
     // Advance yield_timeout_length blocks, during which the yield will await resumption
-    for _ in 0..yield_timeout_length {
-        env.produce_block(0, next_block_height);
-        next_block_height += 1;
+    for i in 5..(5 + yield_timeout_length) {
+        env.produce_block(0, i);
 
         // The transaction will not have a final result until the timeout is reached
         assert!(matches!(
@@ -88,8 +90,7 @@ fn simple_yield_timeout() {
     }
 
     // Advance one more block, triggering the timeout
-    env.produce_block(0, next_block_height);
-    next_block_height += 1;
+    env.produce_block(0, 5 + yield_timeout_length);
     assert_eq!(
         env.clients[0].chain.get_final_transaction_result(&yield_tx_hash).unwrap().status,
         FinalExecutionStatus::SuccessValue(vec![23u8]),
