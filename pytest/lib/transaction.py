28db0fcfca5470176964e0d9b8212ddd50d7fdcd
Viktar Makouski <viktar@neaar.org>
2024-05-20 23:26:26 +0300

    trying emulate signature

--- pytest/lib/transaction.py
+++ pytest/lib/transaction.py
@@ -40,7 +40,7 @@ def sign_and_serialize_transaction(receiverId, nonce, actions, blockHash,
     tx.blockHash = blockHash
     signature = Signature()
     signature.keyType = 0
-    signature.data = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+    signature.data = bytes("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 'utf-8')
     signedTx = SignedTransaction()
     signedTx.transaction = tx
     signedTx.signature = signature
