3a1865369272e98b0b0f909ab98fb13d6f44a934
tayfunelmas <tayfunelmas@gmail.com>
2024-05-29 16:39:29 -0700

    Panic for statelessnet as well.

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -362,8 +362,7 @@ impl Client {
             Arc::new(ChunkEndorsementTracker::new(epoch_manager.clone()));
         // Chunk validator should panic if there is a validator error in non-production chains (eg. mocket and localnet).
         let panic_on_validation_error = config.chain_id != near_primitives::chains::MAINNET
-            && config.chain_id != near_primitives::chains::TESTNET
-            && config.chain_id != near_primitives::chains::STATELESSNET;
+            && config.chain_id != near_primitives::chains::TESTNET;
         let chunk_validator = ChunkValidator::new(
             validator_signer.clone(),
             epoch_manager.clone(),
