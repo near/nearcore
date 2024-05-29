a306347a2aefedfd73c23d67651cbfb31d6b28d9
tayfunelmas <tayfunelmas@gmail.com>
2024-05-28 17:12:07 -0700

    Cargo format

--- chain/client/src/stateless_validation/chunk_validator/mod.rs
+++ chain/client/src/stateless_validation/chunk_validator/mod.rs
@@ -24,6 +24,7 @@ use near_chain::{Block, Chain, ChainStoreAccess};
 use near_chain_primitives::Error;
 use near_epoch_manager::EpochManagerAdapter;
 use near_network::types::{NetworkRequests, PeerManagerMessageRequest};
+use near_o11y::log_assert_fail;
 use near_pool::TransactionGroupIteratorWrapper;
 use near_primitives::apply::ApplyChunkReason;
 use near_primitives::hash::{hash, CryptoHash};
@@ -38,7 +39,6 @@ use near_primitives::transaction::SignedTransaction;
 use near_primitives::types::chunk_extra::ChunkExtra;
 use near_primitives::types::ShardId;
 use near_primitives::validator_signer::ValidatorSigner;
-use near_o11y::log_assert_fail;
 use near_store::{PartialStorage, ShardUId};
 use near_vm_runner::logic::ProtocolVersion;
 use orphan_witness_pool::OrphanStateWitnessPool;
@@ -155,7 +155,7 @@ impl ChunkValidator {
         )?;
         let shard_uid = epoch_manager.shard_id_to_uid(last_header.shard_id(), &epoch_id)?;
         let panic_on_validation_error = self.panic_on_validation_error;
-        
+
         if let Ok(prev_chunk_extra) = chain.get_chunk_extra(prev_block_hash, &shard_uid) {
             match validate_chunk_with_chunk_extra(
                 chain.chain_store(),
@@ -179,7 +179,10 @@ impl ChunkValidator {
                     if panic_on_validation_error {
                         panic!("Failed to validate chunk using existing chunk extra: {:?}", err);
                     } else {
-                        log_assert_fail!("Failed to validate chunk using existing chunk extra: {:?}", err);
+                        log_assert_fail!(
+                            "Failed to validate chunk using existing chunk extra: {:?}",
+                            err
+                        );
                     }
                     return Err(err);
                 }
