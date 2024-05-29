9464a469702baf043e78aa909ca7125bc5b03d3f
Andrei Kashin <kashin.andrej@gmail.com>
2024-05-29 13:35:14 +0100

    Introduce async FT workload

--- pytest/tests/loadtest/locust/locustfiles/ft.py
+++ pytest/tests/loadtest/locust/locustfiles/ft.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[4] / 'lib'))
 
 from configured_logger import new_logger
-from locust import between, task
+from locust import constant_throughput, task
 from common.base import NearUser
 from common.ft import TransferFT
 
@@ -22,13 +22,13 @@ class FTTransferUser(NearUser):
     Registers itself on an FT contract in the setup phase, then just sends FTs to
     random users.
     """
-    wait_time = between(1, 3)  # random pause between transactions
+    wait_time = constant_throughput(1.0)
 
     @task
     def ft_transfer(self):
         receiver = self.ft.random_receiver(self.account_id)
         tx = TransferFT(self.ft.account, self.account, receiver, how_much=1)
-        self.send_tx(tx, locust_name="FT transfer")
+        self.send_tx_async(tx, locust_name="FT transfer")
 
     def on_start(self):
         super().on_start()
