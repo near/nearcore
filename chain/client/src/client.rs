7574a58c4a107d0facd3d6c25589e1f4ca93fd3f
Jan Ciolek <jan.ciolek@nearone.org>
2024-05-29 13:15:55 +0000

    Don't calculate transaction size on older protocol versions

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -76,11 +76,11 @@ use near_primitives::sharding::{
 use near_primitives::transaction::SignedTransaction;
 use near_primitives::types::chunk_extra::ChunkExtra;
 use near_primitives::types::{AccountId, ApprovalStake, BlockHeight, EpochId, NumBlocks, ShardId};
-use near_primitives::unwrap_or_return;
 use near_primitives::utils::MaybeValidated;
 use near_primitives::validator_signer::ValidatorSigner;
 use near_primitives::version::PROTOCOL_VERSION;
 use near_primitives::views::{CatchupStatusView, DroppedReason};
+use near_primitives::{checked_feature, unwrap_or_return};
 use near_store::ShardUId;
 use reed_solomon_erasure::galois_8::ReedSolomon;
 use std::cmp::max;
@@ -1053,13 +1053,19 @@ impl Client {
                 source: StorageDataSource::Db,
                 state_patch: Default::default(),
             };
+            let epoch_id = self.epoch_manager.get_epoch_id_from_prev_block(&prev_block.hash())?;
+            let protocol_version = self.epoch_manager.get_epoch_protocol_version(&epoch_id)?;
             let last_chunk_transactions_size = match last_chunk_opt {
-                Some(prev_chunk) => borsh::to_vec(prev_chunk.transactions())
-                    .map_err(|e| {
-                        Error::ChunkProducer(format!("Failed to serialize transactions: {e}"))
-                    })?
-                    .len(),
-                None => 0,
+                Some(prev_chunk)
+                    if checked_feature!("stable", WitnessTransactionLimits, protocol_version) =>
+                {
+                    borsh::to_vec(prev_chunk.transactions())
+                        .map_err(|e| {
+                            Error::ChunkProducer(format!("Failed to serialize transactions: {e}"))
+                        })?
+                        .len()
+                }
+                _ => 0,
             };
             runtime.prepare_transactions(
                 storage_config,
