7b94e00a686827e56321c5ae8bcec76a1a2553cf
Jan Ciolek <jan.ciolek@nearone.org>
2024-06-05 13:30:12 +0000

    Fix meta_tx_create_near_implicit_account_fails test
    This test tried to send a large (~170kB) receipt and
    expected the receipt to be processed immediately.
    The receipt wasn't processed immediately because it's
    aboce the usual outgoing size limit.
    The easiest way to fix this is to increase the limit
    for this particular test.

--- integration-tests/src/tests/client/features/delegate_action.rs
+++ integration-tests/src/tests/client/features/delegate_action.rs
@@ -826,7 +826,12 @@ fn meta_tx_create_eth_implicit_account_fails() {
 fn meta_tx_create_and_use_implicit_account(new_account: AccountId) {
     let relayer = bob_account();
     let sender = alice_account();
-    let node = RuntimeNode::new(&relayer);
+
+    let node = RuntimeNode::new_with_modified_config(&relayer, |runtime_config| {
+        // Increase the outgoing receipts limit to allow the large receipt to be processed immediately.
+        // Without this change the receipt would be processed somewhere in the next few blocks.
+        runtime_config.witness_config.outgoing_receipts_usual_size_limit = 200_000;
+    });
 
     // Check the account doesn't exist, yet. We will attempt creating it.
     node.view_account(&new_account).expect_err("account already exists");
