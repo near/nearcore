942b907918c6be281a56f181e68a0bd12b2fe312
Shreyan Gupta <shreyan@near.org>
2024-06-08 03:10:56 -0700

    [test_loop] Introduce TestLoopPeerManagerActor for handling network messages across clients

--- chain/network/src/test_loop.rs
+++ chain/network/src/test_loop.rs
@@ -1,4 +1,307 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use near_async::messaging::{Actor, AsyncSender, CanSend, Handler, SendAsync, Sender};
+use near_async::time::Clock;
+use near_primitives::hash::CryptoHash;
+use near_primitives::network::PeerId;
 use near_primitives::types::AccountId;
+use once_cell::sync::Lazy;
+
+use crate::client::{
+    BlockApproval, BlockResponse, ChunkEndorsementMessage, ProcessTxRequest, ProcessTxResponse,
+};
+use crate::shards_manager::ShardsManagerRequestFromNetwork;
+use crate::state_witness::{
+    ChunkStateWitnessAckMessage, PartialEncodedStateWitnessForwardMessage,
+    PartialEncodedStateWitnessMessage, PartialWitnessSenderForNetwork,
+};
+use crate::types::{
+    NetworkRequests, NetworkResponses, PeerManagerMessageRequest, PeerManagerMessageResponse,
+    SetChainInfo,
+};
+
+/// Subset of ClientSenderForNetwork required for the TestLoop network.
+/// We skip over the message handlers from view client.
+#[derive(
+    Clone, near_async::MultiSend, near_async::MultiSenderFrom, near_async::MultiSendMessage,
+)]
+#[multi_send_message_derive(Debug)]
+#[multi_send_input_derive(Debug, Clone, PartialEq, Eq)]
+pub struct ClientSenderForTestLoopNetwork {
+    pub block: AsyncSender<BlockResponse, ()>,
+    pub block_approval: AsyncSender<BlockApproval, ()>,
+    pub transaction: AsyncSender<ProcessTxRequest, ProcessTxResponse>,
+    pub chunk_endorsement: AsyncSender<ChunkEndorsementMessage, ()>,
+}
+
+type NetworkRequestHandler = Arc<dyn Fn(NetworkRequests) -> Option<NetworkRequests>>;
+
+/// A custom actor for the TestLoop framework that can be used to send network messages across clients
+/// in a multi-node test.
+///
+/// This actor has a set of handlers to handle PeerManagerMessageRequest messages. We have a set of
+/// default handlers that handle messages sent to client, partial_witness actor, and shards_manager.
+/// It is possible to override these handlers by registering a new handler using the
+/// `register_override_handler()` method.
+///
+/// The signature of the handler is `dyn Fn(NetworkRequests) -> Option<NetworkRequests>`.
+/// If the handler returns None, it means that the message was handled and no further processing is
+/// required. If the handler returns Some(request), it means that the message was not handled and
+/// the request should be passed to the next handler in the chain.
+///
+/// It's possible for a handler to modify the data in request and return it. This can be useful for
+/// simulating things like malicious actors where we can modify the data in the request.
+///
+/// In case no handler is able to handle the request, the actor will panic.
+///
+/// NOTE: To make the override functionality work with the default handlers, the handlers are tried in
+/// reverse order.
+///
+/// Examples of custom handlers
+/// - Override handler to skip sending messages to or from a specific client.
+/// - Override handler to simulate more network delays.
+/// - Override handler to modify data and simulate malicious behavior.
+#[derive(Default)]
+pub struct TestLoopPeerManagerActor {
+    handlers: Vec<NetworkRequestHandler>,
+}
+
+impl Actor for TestLoopPeerManagerActor {}
+
+impl TestLoopPeerManagerActor {
+    /// Create a new TestLoopPeerManagerActor with default handlers for client, partial_witness, and shards_manager.
+    /// Note that we should be able to access the senders for these actors from the data type.
+    pub fn new<'a, T>(clock: Clock, account_id: &AccountId, datas: &'a Vec<T>) -> Self
+    where
+        AccountId: From<&'a T>,
+        ClientSenderForTestLoopNetwork: From<&'a T>,
+        PartialWitnessSenderForNetwork: From<&'a T>,
+        Sender<ShardsManagerRequestFromNetwork>: From<&'a T>,
+    {
+        let mut actor = Self { handlers: Vec::new() };
+        vec![
+            network_message_to_client_handler(&account_id, make_sender_map(datas)),
+            network_message_to_partial_witness_handler(&account_id, make_sender_map(datas)),
+            network_message_to_shards_manager_handler(clock, &account_id, make_sender_map(datas)),
+            network_message_to_state_snapshot_handler(),
+        ]
+        .into_iter()
+        .for_each(|handler| actor.register_override_handler(handler));
+        actor
+    }
+
+    /// Register a new handler to override the default handlers.
+    pub fn register_override_handler(&mut self, handler: NetworkRequestHandler) {
+        self.handlers.push(handler);
+    }
+}
+
+// Helper function to create a map of senders from a list of data.
+// Converts Vec<Data> to HashMap<AccountId, Sender>
+fn make_sender_map<'a, T, U>(datas: &'a Vec<T>) -> HashMap<AccountId, U>
+where
+    AccountId: From<&'a T>,
+    U: From<&'a T>,
+{
+    let mut senders = HashMap::new();
+    for data in datas.iter() {
+        senders.insert(data.into(), data.into());
+    }
+    senders
+}
+
+impl Handler<SetChainInfo> for TestLoopPeerManagerActor {
+    fn handle(&mut self, _msg: SetChainInfo) {}
+}
+
+impl Handler<PeerManagerMessageRequest> for TestLoopPeerManagerActor {
+    fn handle(&mut self, msg: PeerManagerMessageRequest) -> PeerManagerMessageResponse {
+        match msg {
+            PeerManagerMessageRequest::NetworkRequests(request) => {
+                // Iterate over the handlers in reverse order to allow for overriding the default handlers.
+                let mut request = Some(request);
+                for handler in self.handlers.iter().rev() {
+                    if let Some(new_request) = handler(request.take().unwrap()) {
+                        request = Some(new_request);
+                    } else {
+                        return PeerManagerMessageResponse::NetworkResponses(
+                            NetworkResponses::NoResponse,
+                        );
+                    }
+                }
+                // If no handler was able to handle the request, panic.
+                if let Some(request) = request {
+                    panic!("Unhandled request: {:?}", request);
+                }
+            }
+            msg => panic!("Unexpected message: {:?}", msg),
+        };
+        PeerManagerMessageResponse::NetworkResponses(NetworkResponses::NoResponse)
+    }
+}
+
+fn network_message_to_client_handler(
+    my_account_id: &AccountId,
+    client_senders: HashMap<AccountId, ClientSenderForTestLoopNetwork>,
+) -> NetworkRequestHandler {
+    let my_account_id = my_account_id.clone();
+    Arc::new(move |request| match request {
+        NetworkRequests::Block { block } => {
+            for (account_id, sender) in client_senders.iter() {
+                if account_id != &my_account_id {
+                    let _ = sender.send_async(BlockResponse {
+                        block: block.clone(),
+                        peer_id: PeerId::random(),
+                        was_requested: false,
+                    });
+                }
+            }
+            None
+        }
+        NetworkRequests::Approval { approval_message } => {
+            assert!(
+                approval_message.target != my_account_id,
+                "Sending message to self not supported."
+            );
+            let sender = client_senders.get(&approval_message.target).unwrap();
+            let _ = sender.send_async(BlockApproval(approval_message.approval, PeerId::random()));
+            None
+        }
+        NetworkRequests::ForwardTx(account, transaction) => {
+            assert!(account != my_account_id, "Sending message to self not supported.");
+            let sender = client_senders.get(&account).unwrap();
+            let _ = sender.send_async(ProcessTxRequest {
+                transaction,
+                is_forwarded: true,
+                check_only: false,
+            });
+            None
+        }
+        NetworkRequests::ChunkEndorsement(target, endorsement) => {
+            assert!(target != my_account_id, "Sending message to self not supported.");
+            let sender = client_senders.get(&target).unwrap();
+            let _ = sender.send_async(ChunkEndorsementMessage(endorsement));
+            None
+        }
+        _ => Some(request),
+    })
+}
+
+fn network_message_to_partial_witness_handler(
+    my_account_id: &AccountId,
+    partial_witness_senders: HashMap<AccountId, PartialWitnessSenderForNetwork>,
+) -> NetworkRequestHandler {
+    let my_account_id = my_account_id.clone();
+    Arc::new(move |request| match request {
+        NetworkRequests::ChunkStateWitnessAck(target, witness_ack) => {
+            // TODO(testloop): Convert account check to a panic.
+            if target != my_account_id {
+                let sender = partial_witness_senders.get(&target).unwrap();
+                sender.send(ChunkStateWitnessAckMessage(witness_ack));
+            }
+            None
+        }
+
+        NetworkRequests::PartialEncodedStateWitness(validator_witness_tuple) => {
+            for (target, partial_witness) in validator_witness_tuple.into_iter() {
+                // TODO(testloop): Convert account check to a panic.
+                if target != my_account_id {
+                    let sender = partial_witness_senders.get(&target).unwrap();
+                    sender.send(PartialEncodedStateWitnessMessage(partial_witness));
+                }
+            }
+            None
+        }
+        NetworkRequests::PartialEncodedStateWitnessForward(chunk_validators, partial_witness) => {
+            for target in chunk_validators {
+                // TODO(testloop): Convert account check to a panic.
+                if target != my_account_id {
+                    let sender = partial_witness_senders.get(&target).unwrap();
+                    sender.send(PartialEncodedStateWitnessForwardMessage(partial_witness.clone()));
+                }
+            }
+            None
+        }
+        _ => Some(request),
+    })
+}
+
+fn network_message_to_state_snapshot_handler() -> NetworkRequestHandler {
+    Arc::new(move |request| match request {
+        NetworkRequests::SnapshotHostInfo { .. } => None,
+        _ => Some(request),
+    })
+}
+
+#[derive(Default, Clone)]
+struct RouteLookup(Arc<Mutex<HashMap<CryptoHash, AccountId>>>);
+
+impl RouteLookup {
+    fn new() -> Self {
+        Self(Arc::new(Mutex::new(HashMap::new())))
+    }
+
+    fn add_route(&self, return_account_id: &AccountId) -> CryptoHash {
+        let mut guard = self.0.lock().unwrap();
+        let route_id = CryptoHash::hash_borsh(guard.len());
+        guard.insert(route_id, return_account_id.clone());
+        route_id
+    }
+
+    fn get_destination(&self, route_id: CryptoHash) -> AccountId {
+        let guard = self.0.lock().unwrap();
+        guard.get(&route_id).unwrap().clone()
+    }
+}
+
+fn network_message_to_shards_manager_handler(
+    clock: Clock,
+    my_account_id: &AccountId,
+    shards_manager_senders: HashMap<AccountId, Sender<ShardsManagerRequestFromNetwork>>,
+) -> Arc<dyn Fn(NetworkRequests) -> Option<NetworkRequests>> {
+    static ROUTE_LOOKUP: Lazy<RouteLookup> = Lazy::new(RouteLookup::new);
+    let my_account_id = my_account_id.clone();
+    Arc::new(move |request| match request {
+        NetworkRequests::PartialEncodedChunkRequest { target, request, .. } => {
+            let route_back = ROUTE_LOOKUP.add_route(&my_account_id);
+            let target = target.account_id.unwrap();
+            assert!(target != my_account_id, "Sending message to self not supported.");
+            let sender = shards_manager_senders.get(&target).unwrap();
+            sender.send(ShardsManagerRequestFromNetwork::ProcessPartialEncodedChunkRequest {
+                partial_encoded_chunk_request: request,
+                route_back,
+            });
+            None
+        }
+        NetworkRequests::PartialEncodedChunkResponse { route_back, response } => {
+            let target = ROUTE_LOOKUP.get_destination(route_back);
+            assert!(target != my_account_id, "Sending message to self not supported.");
+            let sender = shards_manager_senders.get(&target).unwrap();
+            sender.send(ShardsManagerRequestFromNetwork::ProcessPartialEncodedChunkResponse {
+                partial_encoded_chunk_response: response,
+                received_time: clock.now(),
+            });
+            None
+        }
+        NetworkRequests::PartialEncodedChunkMessage { account_id, partial_encoded_chunk } => {
+            assert!(account_id != my_account_id, "Sending message to self not supported.");
+            let sender = shards_manager_senders.get(&account_id).unwrap();
+            sender.send(ShardsManagerRequestFromNetwork::ProcessPartialEncodedChunk(
+                partial_encoded_chunk.into(),
+            ));
+            None
+        }
+        NetworkRequests::PartialEncodedChunkForward { account_id, forward } => {
+            assert!(account_id != my_account_id, "Sending message to self not supported.");
+            let sender = shards_manager_senders.get(&account_id).unwrap();
+            sender
+                .send(ShardsManagerRequestFromNetwork::ProcessPartialEncodedChunkForward(forward));
+            None
+        }
+        _ => Some(request),
+    })
+}
 
 /// A multi-instance test using the TestLoop framework can support routing
 /// lookup for network messages, as long as the Data type contains AccountId.
