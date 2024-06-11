aa67b0a935879f6c7313084731895724a29919fd
Adam Chudaś <adam@chudas.net>
2024-06-11 18:16:31 +0200

    Update pytest

--- pytest/tests/sanity/validator_switch_key_quick.py
+++ pytest/tests/sanity/validator_switch_key_quick.py
@@ -5,76 +5,85 @@
 # and make sure that the network doesn't stall even after
 # the non-validating node becomes a validator.
 
+import unittest
 import sys, time
 import pathlib
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / 'lib'))
 
+from configured_logger import logger
 from cluster import start_cluster
 
 EPOCH_LENGTH = 20
 TIMEOUT = 100
-NUM_VALIDATORS = 2
 
-client_config = {
-    "tracked_shards": [0],  # Track all shards
-    "state_sync_enabled": True,
-    "store.state_snapshot_enabled": True
-}
 # It is important for the non-validating node to track all shards as well.
 # It needs to have all the data when it switches to validator role.
-config_map = {i: client_config for i in range(NUM_VALIDATORS + 1)}
-nodes = start_cluster(
-    NUM_VALIDATORS, 1, 1, None,
-    [["epoch_length", EPOCH_LENGTH], ["block_producer_kickout_threshold", 10],
-     ["chunk_producer_kickout_threshold", 10]], config_map)
-time.sleep(2)
+config_map = {
+    2: {
+        "tracked_shards": [0],  # Track all shards
+    }
+}
 
-nodes[NUM_VALIDATORS].reset_validator_key(nodes[0].validator_key)
-nodes[0].kill()
-nodes[NUM_VALIDATORS].reload_updateable_config()
-nodes[NUM_VALIDATORS].stop_checking_store()
-time.sleep(2)
 
-block = nodes[1].get_latest_block()
-max_height = block.height + 4 * EPOCH_LENGTH
-start_time = time.time()
+class ValidatorSwitchKeyQuickTest(unittest.TestCase):
 
-while True:
-    assert time.time() - start_time < TIMEOUT, 'Validators got stuck'
-    old_validator_height = nodes[1].get_latest_block().height
-    new_validator_height = nodes[NUM_VALIDATORS].get_latest_block().height
-    if old_validator_height > max_height and new_validator_height > max_height:
-        break
-    info = nodes[1].json_rpc('validators', 'latest')
-    next_validators = info['result']['next_validators']
-    account_ids = [v['account_id'] for v in next_validators]
-    print(account_ids)
-    assert len(
-        account_ids) == NUM_VALIDATORS, 'Number of validators do not match'
+    def test_validator_switch_key_quick(self):
+        [other_validator, old_validator, new_validator
+        ] = start_cluster(2, 1, 1, None,
+                          [["epoch_length", EPOCH_LENGTH],
+                           ["block_producer_kickout_threshold", 10],
+                           ["chunk_producer_kickout_threshold", 10]],
+                          config_map)
+        time.sleep(2)
 
-    # We copied over 'test0' validator key, along with validator account ID.
-    # Therefore, despite nodes[0] being stopped, 'test0' still figures as active validator.
-    assert sorted(account_ids)[0] == 'test0'
-    statuses = sorted([(node_idx, nodes[node_idx].get_latest_block())
-                       for node_idx in range(1, NUM_VALIDATORS + 1)],
-                      key=lambda element: element[1].height)
-    print(statuses)
-    last = statuses.pop()
-    cur_height = last[1].height
-    node = nodes[last[0]]
-    succeed = True
-    for _, block in statuses:
-        try:
-            node.get_block(block.hash)
-        except Exception:
-            succeed = False
-            break
-    # Both validators should be synchronized
-    print('Succeed', succeed)
-    if statuses[0][1].height > max_height - EPOCH_LENGTH // 2 and succeed:
-        # If nodes are synchronized and the current height is close to `max_height` we can finish.
-        sys.exit(0)
-    time.sleep(1)
+        new_validator.reset_validator_key(other_validator.validator_key)
+        other_validator.kill()
+        new_validator.reload_updateable_config()
+        new_validator.stop_checking_store()
+        time.sleep(2)
 
-assert False, 'Nodes are not synced'
+        block = old_validator.get_latest_block()
+        max_height = block.height + 4 * EPOCH_LENGTH
+        start_time = time.time()
+
+        while True:
+            assert time.time() - start_time < TIMEOUT, 'Validators got stuck'
+
+            info = old_validator.json_rpc('validators', 'latest')
+            next_validators = info['result']['next_validators']
+            account_ids = [v['account_id'] for v in next_validators]
+            # We copied over 'test0' validator key, along with validator account ID.
+            # Therefore, despite nodes[0] being stopped, 'test0' still figures as active validator.
+            assert sorted(account_ids) == ['test0', 'test1']
+
+            last_block_per_node = [
+                new_validator.get_latest_block(),
+                old_validator.get_latest_block()
+            ]
+            height_per_node = list(map(lambda block: block.height, last_block_per_node))
+            logger.info(height_per_node)
+            if max(height_per_node) > max_height:
+                break
+
+            synchronized = True
+            for i, node in enumerate([new_validator, old_validator]):
+                try:
+                    node.get_block(last_block_per_node[1 - i].hash)
+                except Exception:
+                    synchronized = False
+                    break
+            
+            # Both validators should be synchronized
+            logger.info(f'Synchronized {synchronized}')
+            if height_per_node[0] > max_height - EPOCH_LENGTH // 2 and synchronized:
+                # If nodes are synchronized and the current height is close to `max_height` we can finish.
+                return
+
+            time.sleep(1)
+
+        assert False, 'Nodes are not synced'
+
+
+if __name__ == '__main__':
+    unittest.main()
