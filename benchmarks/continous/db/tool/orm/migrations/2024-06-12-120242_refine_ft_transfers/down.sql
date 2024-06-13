b4b1823a3b28fa378eed4f24c71a946165bc292f
Moritz <moritz.zielke@gmail.com>
2024-06-13 16:57:27 +0200

    Fix function name

--- benchmarks/continous/db/tool/orm/migrations/2024-06-12-120242_refine_ft_transfers/down.sql
+++ benchmarks/continous/db/tool/orm/migrations/2024-06-12-120242_refine_ft_transfers/down.sql
@@ -1,6 +1,6 @@
 -- Assumes regularly values handled here cannot be negative, hence -1 flags out
 -- of range values.
-create function convert_int_to_bigint(x bigint)
+create function convert_bigint_to_int(x bigint)
 returns integer as $$
 begin
     if x between -2147483648 and 2147483647 then
@@ -14,10 +14,10 @@ $$ language plpgsql;
 alter table ft_transfers drop column time_end;
 -- Ensure the bigint -> integer conversion will succeed.
 update ft_transfers set
-    size_state_bytes = convert_int_to_bigint(size_state_bytes),
-    total_transactions = convert_int_to_bigint(total_transactions);
+    size_state_bytes = convert_bigint_to_int(size_state_bytes),
+    total_transactions = convert_bigint_to_int(total_transactions);
 alter table ft_transfers
 alter column size_state_bytes type integer,
 alter column total_transactions type integer;
 
-drop function convert_int_to_bigint (integer);
+drop function convert_bigint_to_int (integer);
