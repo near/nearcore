0b564184454af06594d91ed3c86c6dc9dc738bd5
Shreyan Gupta <shreyan@near.org>
2024-06-14 00:58:22 -0700

    [stateless_validation] Partial witness RATIO_DATA_PARTS under protocol config (#11574)
    Follow up from PR https://github.com/near/nearcore/pull/11571 where we
    need to have the change in ratio under a protocol upgrade.

--- chain/client/src/stateless_validation/partial_witness/encoding.rs
+++ chain/client/src/stateless_validation/partial_witness/encoding.rs
@@ -1,15 +1,18 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
+use near_primitives::checked_feature;
 use near_primitives::reed_solomon::{
     reed_solomon_decode, reed_solomon_encode, reed_solomon_part_length,
 };
 use near_primitives::stateless_validation::EncodedChunkStateWitness;
+use near_vm_runner::logic::ProtocolVersion;
 use reed_solomon_erasure::galois_8::ReedSolomon;
 
 /// Ratio of the number of data parts to total parts in the Reed Solomon encoding.
 /// The tradeoff here is having a higher ratio is better for handling missing parts and network errors
 /// but increases the size of the encoded state witness and the total network bandwidth requirements.
+const RATIO_DATA_PARTS_OLD: f32 = 0.8;
 const RATIO_DATA_PARTS: f32 = 0.6;
 
 /// Type alias around what ReedSolomon represents data part as.
@@ -25,9 +28,9 @@ pub struct WitnessEncoder {
 }
 
 impl WitnessEncoder {
-    pub fn new(total_parts: usize) -> WitnessEncoder {
+    pub fn new(total_parts: usize, protocol_version: ProtocolVersion) -> WitnessEncoder {
         let rs = if total_parts > 1 {
-            let data_parts = num_witness_data_parts(total_parts);
+            let data_parts = num_witness_data_parts(total_parts, protocol_version);
             Some(ReedSolomon::new(data_parts, total_parts - data_parts).unwrap())
         } else {
             None
@@ -74,7 +77,7 @@ impl WitnessEncoder {
 
 /// We keep one encoder for each length of chunk_validators to avoid re-creating the encoder.
 pub struct WitnessEncoderCache {
-    instances: HashMap<usize, Arc<WitnessEncoder>>,
+    instances: HashMap<(usize, ProtocolVersion), Arc<WitnessEncoder>>,
 }
 
 impl WitnessEncoderCache {
@@ -82,18 +85,35 @@ impl WitnessEncoderCache {
         Self { instances: HashMap::new() }
     }
 
-    pub fn entry(&mut self, total_parts: usize) -> Arc<WitnessEncoder> {
+    pub fn entry(
+        &mut self,
+        total_parts: usize,
+        protocol_version: ProtocolVersion,
+    ) -> Arc<WitnessEncoder> {
         self.instances
-            .entry(total_parts)
-            .or_insert_with(|| Arc::new(WitnessEncoder::new(total_parts)))
+            .entry((total_parts, protocol_version))
+            .or_insert_with(|| Arc::new(WitnessEncoder::new(total_parts, protocol_version)))
             .clone()
     }
 }
 
-pub fn witness_part_length(encoded_witness_size: usize, total_parts: usize) -> usize {
-    reed_solomon_part_length(encoded_witness_size, num_witness_data_parts(total_parts))
+pub fn witness_part_length(
+    encoded_witness_size: usize,
+    total_parts: usize,
+    protocol_version: ProtocolVersion,
+) -> usize {
+    reed_solomon_part_length(
+        encoded_witness_size,
+        num_witness_data_parts(total_parts, protocol_version),
+    )
 }
 
-fn num_witness_data_parts(total_parts: usize) -> usize {
-    std::cmp::max((total_parts as f32 * RATIO_DATA_PARTS) as usize, 1)
+fn num_witness_data_parts(total_parts: usize, protocol_version: ProtocolVersion) -> usize {
+    let ratio_data_parts =
+        if checked_feature!("stable", ChangePartialWitnessDataPartsRequired, protocol_version) {
+            RATIO_DATA_PARTS
+        } else {
+            RATIO_DATA_PARTS_OLD
+        };
+    std::cmp::max((total_parts as f32 * ratio_data_parts) as usize, 1)
 }
