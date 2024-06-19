7f35ac27ac1106f6ec3c87e04e6646cb617c1d79
Shreyan Gupta <shreyan@near.org>
2024-06-18 23:55:25 -0700

    fix

--- integration-tests/src/tests/mod.rs
+++ integration-tests/src/tests/mod.rs
@@ -1,13 +1,13 @@
 mod client;
+mod genesis_helpers;
 mod nearcore;
+mod nearcore_utils;
 mod network;
 mod runtime;
 mod standard_cases;
 mod test_catchup;
 mod test_errors;
+mod test_helpers;
 mod test_overflows;
 mod test_simple;
 mod test_tps_regression;
-mod test_helpers;
-mod genesis_helpers;
-mod nearcore_utils;
