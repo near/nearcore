6d64363e035e0e99fce151a3acd15a0609a95359
Aleksandr Logunov <alex.logunov@near.org>
2024-06-25 17:46:58 +0400

    refactor: TestLoop state sync dumper event + warmup (#11664)
    Make
    * queueing state sync dumper start & stop
    * warming up the chain until genesis_height + 3
    
    parts of generic setup because they are always needed. Additional
    motivation for warmup - for nayduck tests it happens uncontrollably
    anyway, because there is always delay between starting nodes and sending
    first transaction / triggering first event.
    
    After this, existing tests should become compact enough to understand,
    and new tests can be written, based on them.

--- integration-tests/src/test_loop/env.rs
+++ integration-tests/src/test_loop/env.rs
@@ -1,5 +1,5 @@
 use near_async::messaging::{IntoMultiSender, IntoSender, Sender};
-use near_async::test_loop::data::TestLoopDataHandle;
+use near_async::test_loop::data::{TestLoopData, TestLoopDataHandle};
 use near_async::test_loop::sender::TestLoopSender;
 use near_async::test_loop::TestLoopV2;
 use near_async::time::Duration;
@@ -17,6 +17,54 @@ const NETWORK_DELAY: Duration = Duration::milliseconds(10);
 pub struct TestLoopEnv {
     pub test_loop: TestLoopV2,
     pub datas: Vec<TestData>,
+}
+
+impl TestLoopEnv {
+    /// Reach block with height `genesis_height + 3`. Check that it can be done
+    /// within 5 seconds. Ensure that all clients have block
+    /// `genesis_height + 2` and it has all chunks.
+    /// Needed because for smaller heights blocks may not get all chunks and/or
+    /// approvals.
+    pub fn warmup(self) -> Self {
+        let Self { mut test_loop, datas } = self;
+
+        let client_handle = datas[0].client_sender.actor_handle();
+        let genesis_height = test_loop.data.get(&client_handle).client.chain.genesis().height();
+        test_loop.run_until(
+            |test_loop_data| {
+                let client_actor = test_loop_data.get(&client_handle);
+                client_actor.client.chain.head().unwrap().height == genesis_height + 3
+            },
+            Duration::seconds(5),
+        );
+        for idx in 0..datas.len() {
+            let client_handle = datas[idx].client_sender.actor_handle();
+            let event = move |test_loop_data: &mut TestLoopData| {
+                let client_actor = test_loop_data.get(&client_handle);
+                let block =
+                    client_actor.client.chain.get_block_by_height(genesis_height + 2).unwrap();
+                let num_shards = block.header().chunk_mask().len();
+                assert_eq!(block.header().chunk_mask(), vec![true; num_shards]);
+            };
+            test_loop.send_adhoc_event("assertions".to_owned(), Box::new(event));
+        }
+        test_loop.run_instant();
+
+        Self { test_loop, datas }
+    }
+
+    /// Used to finish off remaining events that are still in the loop. This can be necessary if the
+    /// destructor of some components wait for certain condition to become true. Otherwise, the
+    /// destructors may end up waiting forever. This also helps avoid a panic when destructing
+    /// TestLoop itself, as it asserts that all events have been handled.
+    pub fn shutdown_and_drain_remaining_events(mut self, timeout: Duration) {
+        // State sync dumper is not an Actor, handle stopping separately.
+        for node_data in self.datas {
+            self.test_loop.data.get_mut(&node_data.state_sync_dumper_handle).stop();
+        }
+
+        self.test_loop.shutdown_and_drain_remaining_events(timeout);
+    }
 }
 
 pub struct TestData {
--- integration-tests/src/test_loop/tests/multinode_test_loop_example.rs
+++ integration-tests/src/test_loop/tests/multinode_test_loop_example.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use near_async::test_loop::data::TestLoopData;
 use near_async::time::Duration;
 use near_chain_configs::test_genesis::TestGenesisBuilder;
 use near_client::test_utils::test_loop::ClientQueries;
@@ -42,35 +41,6 @@ fn test_client_with_multi_test_loop() {
     let TestLoopEnv { mut test_loop, datas: node_datas } =
         builder.genesis(genesis).clients(clients).disable_gc().build();
 
-    // Bootstrap the test by starting the components.
-    for idx in 0..NUM_CLIENTS {
-        let state_sync_dumper_handle = node_datas[idx].state_sync_dumper_handle.clone();
-        test_loop.send_adhoc_event("start_state_sync_dumper".to_owned(), move |test_loop_data| {
-            test_loop_data.get_mut(&state_sync_dumper_handle).start().unwrap();
-        });
-    }
-
-    // Give it some condition to stop running at. Here we run the test until the first client
-    // reaches height 10003, with a timeout of 5sec (failing if it doesn't reach 10003 in time).
-    let client_handle = node_datas[0].client_sender.actor_handle();
-    test_loop.run_until(
-        |test_loop_data| {
-            let client_actor = test_loop_data.get(&client_handle);
-            client_actor.client.chain.head().unwrap().height == 10003
-        },
-        Duration::seconds(5),
-    );
-    for idx in 0..NUM_CLIENTS {
-        let client_handle = node_datas[idx].client_sender.actor_handle();
-        let event = move |test_loop_data: &mut TestLoopData| {
-            let client_actor = test_loop_data.get(&client_handle);
-            let block = client_actor.client.chain.get_block_by_height(10002).unwrap();
-            assert_eq!(block.header().chunk_mask(), &(0..NUM_CLIENTS).map(|_| true).collect_vec());
-        };
-        test_loop.send_adhoc_event("assertions".to_owned(), Box::new(event));
-    }
-    test_loop.run_instant();
-
     let first_epoch_tracked_shards = {
         let clients = node_datas
             .iter()
@@ -99,11 +69,8 @@ fn test_client_with_multi_test_loop() {
     tracing::info!("Later epoch tracked shards: {:?}", later_epoch_tracked_shards);
     assert_ne!(first_epoch_tracked_shards, later_epoch_tracked_shards);
 
-    for idx in 0..NUM_CLIENTS {
-        test_loop.data.get_mut(&node_datas[idx].state_sync_dumper_handle).stop();
-    }
-
     // Give the test a chance to finish off remaining events in the event loop, which can
     // be important for properly shutting down the nodes.
-    test_loop.shutdown_and_drain_remaining_events(Duration::seconds(20));
+    TestLoopEnv { test_loop, datas: node_datas }
+        .shutdown_and_drain_remaining_events(Duration::seconds(20));
 }
