663268dec87a7336d7d18cfb600fe0fab948d191
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2024-07-09 19:25:21 -0400

    bring stable protocol version

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -271,7 +271,7 @@ impl ProtocolFeature {
 /// Current protocol version used on the mainnet.
 /// Some features (e. g. FixStorageUsage) require that there is at least one epoch with exactly
 /// the corresponding version
-const STABLE_PROTOCOL_VERSION: ProtocolVersion = 81;
+const STABLE_PROTOCOL_VERSION: ProtocolVersion = 67;
 
 /// Largest protocol version supported by the current binary.
 pub const PROTOCOL_VERSION: ProtocolVersion = if cfg!(feature = "statelessnet_protocol") {
