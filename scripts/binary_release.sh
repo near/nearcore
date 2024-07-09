823f7fbd524e21d6cf7858f8120517127a903f33
Anton Puhach <pugachag@gmail.com>
2024-07-09 13:54:33 +0200

    impl

--- scripts/binary_release.sh
+++ scripts/binary_release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -xeo pipefail
 
-release="${1:-release}"
+release="${1:-neard-release}"
 
 case "$release" in
-  release|nightly-release|perf-release|assertions-release)
+  release|nightly-release|perf-release|assertions-release|test-features-release)
     ;;
   *)  
     echo "Unsupported release type '$release'. Please provide no argument for normal release or provide nightly-release for nightly."
