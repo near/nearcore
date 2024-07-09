a1e1b78f93ce065e1cc4e3b24129bfceb6533b3b
Anton Puhach <pugachag@gmail.com>
2024-07-09 13:57:14 +0200

    Add test-features-release

--- scripts/binary_release.sh
+++ scripts/binary_release.sh
@@ -4,7 +4,7 @@ set -xeo pipefail
 release="${1:-neard-release}"
 
 case "$release" in
-  neard-release|nightly-release|perf-release|assertions-release|statelessnet-release)
+  neard-release|nightly-release|perf-release|assertions-release|statelessnet-release|test-features-release)
     ;;
   *)  
     echo "Unsupported release type '$release'. Please provide no argument for normal release or provide nightly-release for nightly."
