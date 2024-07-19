a66595f1242332e94e0abfb70bf6dd372ad45ddf
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2024-07-19 17:57:14 +0300

    fix stable protocol version

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -239,7 +239,7 @@ impl ProtocolFeature {
 /// Current protocol version used on the mainnet.
 /// Some features (e. g. FixStorageUsage) require that there is at least one epoch with exactly
 /// the corresponding version
-const STABLE_PROTOCOL_VERSION: ProtocolVersion = 71;
+const STABLE_PROTOCOL_VERSION: ProtocolVersion = 70;
 
 /// Largest protocol version supported by the current binary.
 pub const PROTOCOL_VERSION: ProtocolVersion = if cfg!(feature = "statelessnet_protocol") {
