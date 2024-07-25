d5c6f8868a8400d8a0e3eebbc8c69c2e79f137ec
Andrei Kashin <kashin.andrej@gmail.com>
2024-07-25 10:29:55 +0100

    [bench] Disable tx pool state size limit (#11835)
    As this is limiting the throughput of FT benchmark.

--- core/parameters/src/config_store.rs
+++ core/parameters/src/config_store.rs
@@ -145,6 +145,8 @@ impl RuntimeConfigStore {
                 config.congestion_control_config.max_tx_gas = 10u64.pow(16);
                 config.congestion_control_config.min_tx_gas = 10u64.pow(16);
                 config.witness_config.main_storage_proof_size_soft_limit = 999_999_999_999_999;
+                config.witness_config.new_transactions_validation_state_size_soft_limit =
+                    999_999_999_999_999;
                 let mut wasm_config = vm::Config::clone(&config.wasm_config);
                 wasm_config.limit_config.per_receipt_storage_proof_size_limit = 999_999_999_999_999;
                 config.wasm_config = Arc::new(wasm_config);
