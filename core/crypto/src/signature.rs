94d7cabe9a68fe69ed90fdc55bbac1970d0ecc7d
Aleksandr Logunov <alex.logunov@near.org>
2024-07-29 01:25:08 +0200

    feat: protocol schema check - generics and tests (#11781)
    Implementing ProtocolStruct dependency for arbitrary structs with
    generics.
    
    The most challenging struct to solve is 
    
    ```
    pub struct EpochInfoV4 {
        ...
        pub validator_kickout: HashMap<AccountId, ValidatorKickoutReason>,
    }
    ```
    
    If `ValidatorKickoutReason` changes, I want to list all structs which
    were potentially impacted. Fortunately, `syn` allows to extract even
    these types as `syn::GenericArgument::Type`. However, in const
    environment we cannot allocate vectors. So I bravely assume that we
    never use 4 types in any protocol-related generic and take first 4 of
    them. For HashMap, 2 are enough.
    
    The fix allows to get from `EpochInfoV4` to `ValidatorKickoutReason` in
    `compute_hash`. I checked that if I break enum order, **both**
    `ValidatorKickoutReason` and `EpochInfoV4` **deterministically** change.
    
    Also implementing simple tests showing hash differences for different
    structures. Checked locally that without generics support
    `test_different_containers` doesn't work.
    
    Next steps are to add `ProtocolStruct` for all protocol structs and
    implement a CI check.

--- core/crypto/src/signature.rs
+++ core/crypto/src/signature.rs
@@ -1,3 +1,5 @@
+use near_structs_checker_lib::ProtocolStruct;
+
 use borsh::{BorshDeserialize, BorshSerialize};
 use ed25519_dalek::ed25519::signature::{Signer, Verifier};
 use primitive_types::U256;
@@ -64,7 +66,9 @@ fn split_key_type_data(value: &str) -> Result<(KeyType, &str), crate::errors::Pa
     }
 }
 
-#[derive(Clone, Eq, Ord, PartialEq, PartialOrd, derive_more::AsRef, derive_more::From)]
+#[derive(
+    Clone, Eq, Ord, PartialEq, PartialOrd, derive_more::AsRef, derive_more::From, ProtocolStruct,
+)]
 #[cfg_attr(test, derive(bolero::TypeGenerator))]
 #[as_ref(forward)]
 pub struct Secp256K1PublicKey([u8; 64]);
@@ -86,7 +90,9 @@ impl std::fmt::Debug for Secp256K1PublicKey {
     }
 }
 
-#[derive(Clone, Eq, Ord, PartialEq, PartialOrd, derive_more::AsRef, derive_more::From)]
+#[derive(
+    Clone, Eq, Ord, PartialEq, PartialOrd, derive_more::AsRef, derive_more::From, ProtocolStruct,
+)]
 #[cfg_attr(test, derive(bolero::TypeGenerator))]
 #[as_ref(forward)]
 pub struct ED25519PublicKey(pub [u8; ed25519_dalek::PUBLIC_KEY_LENGTH]);
@@ -109,7 +115,7 @@ impl std::fmt::Debug for ED25519PublicKey {
 }
 
 /// Public key container supporting different curves.
-#[derive(Clone, PartialEq, PartialOrd, Ord, Eq)]
+#[derive(Clone, PartialEq, PartialOrd, Ord, Eq, ProtocolStruct)]
 #[cfg_attr(test, derive(bolero::TypeGenerator))]
 pub enum PublicKey {
     /// 256 bit elliptic curve based public-key.
