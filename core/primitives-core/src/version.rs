d8997200a988113b564bfe58ee9ac1d69a285418
Shreyan Gupta <shreyan.gupta96@gmail.com>
2024-07-29 16:16:02 -0700

    nightly bump

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -252,7 +252,7 @@ pub const PROTOCOL_VERSION: ProtocolVersion = if cfg!(feature = "statelessnet_pr
     82
 } else if cfg!(feature = "nightly_protocol") {
     // On nightly, pick big enough version to support all features.
-    143
+    144
 } else {
     // Enable all stable features.
     STABLE_PROTOCOL_VERSION
