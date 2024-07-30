696a8464b76c6cee7d2a074a4ab838f474e70143
Jan Ciolek <jan.ciolek@nearone.org>
2024-07-30 12:22:30 +0000

    Set testnet voting to Tue, Aug 8th 10AM UTC

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -81,7 +81,12 @@ pub const PROTOCOL_UPGRADE_SCHEDULE: Lazy<ProtocolUpgradeVotingSchedule> = Lazy:
     // let schedule = vec![(v1_datetime, v1_protocol_version), (v2_datetime, v2_protocol_version)];
     // ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, schedule).unwrap()
 
-    ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, vec![]).unwrap()
+    // 2.1.0
+    // Tuesday
+    let protocol_version = 70;
+    let datetime = ProtocolUpgradeVotingSchedule::parse_datetime("2024-08-06 10:00:00").unwrap();
+    let schedule = vec![(datetime, protocol_version)];
+    ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, schedule).unwrap()
 });
 
 /// Gives new clients an option to upgrade without announcing that they support
