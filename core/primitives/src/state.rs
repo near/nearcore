3ce5ed5286f7344d864c4915de7335167c84e496
Aleksandr Logunov <alex.logunov@near.org>
2024-08-01 14:11:23 +0400

    test: protocol schema CI check; first batch of structs (#11855)
    Adding "Protocol Schema" check to CI - you can see it in the list of
    checks. This test will ensure that structs involved in protocol or saved
    to DB doesn't accidentally change, see #11755. If that's not the case,
    it will output list of impacted structs and suggest to change
    `protocol_schema.toml` file.
    
    Example of output in such case:
    ```
    Loaded 123 structs
    New struct: FlatStateValue with hash 83834662
    New struct: ValueRef with hash 2322946441
    New TOML file written to: ./target/protocol_schema.toml
    Please review the changes and copy the file to protocol_schema.toml if they are correct.
    ```
    
    Enabling it for first group of structs, mainly from core/primitives, to
    check that it works. I will add remaining structs later.
    
    The change is mainly renaming ProtocolStruct with ProtocolSchema as I
    see it a better fit now, and add ProtocolSchema to the list of derives.
    Note that if `protocol_schema` and `enable_const_type_id` are not
    enabled, this derive will be no-op, so it won't impact regular code
    generation.
    
    cc @nagisa
    
    ---------
    
    Co-authored-by: Simonas Kazlauskas <github@kazlauskas.me>

--- core/primitives/src/state.rs
+++ core/primitives/src/state.rs
@@ -1,9 +1,10 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
 use near_primitives_core::hash::{hash, CryptoHash};
+use near_schema_checker_lib::ProtocolSchema;
 
 /// State value reference. Used to charge fees for value length before retrieving the value itself.
-#[derive(BorshSerialize, BorshDeserialize, Clone, PartialEq, Eq, Hash)]
+#[derive(BorshSerialize, BorshDeserialize, Clone, PartialEq, Eq, Hash, ProtocolSchema)]
 pub struct ValueRef {
     /// Value length in bytes.
     pub length: u32,
@@ -62,7 +63,7 @@ mod tests {
     }
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq, Eq)]
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone, PartialEq, Eq, ProtocolSchema)]
 pub enum FlatStateValue {
     Ref(ValueRef),
     Inlined(Vec<u8>),
