f8e4359b248702b4a04e888d69dac1c901c35c74
robin-near <robin@nearone.org>
2024-08-05 11:34:44 -0700

    [TestLoop] Add a test for syncing, and necessary TestLoop enhancements.

--- integration-tests/src/test_loop/tests/mod.rs
+++ integration-tests/src/test_loop/tests/mod.rs
@@ -5,4 +5,5 @@ pub mod in_memory_tries;
 pub mod multinode_stateless_validators;
 pub mod multinode_test_loop_example;
 pub mod simple_test_loop_example;
+pub mod syncing;
 pub mod view_requests_to_archival_node;
