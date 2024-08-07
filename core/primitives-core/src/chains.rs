7768bcf2f407e2846b5cc7e3565654894bb2eb24
Tayfun Elmas <tayfunelmas@gmail.com>
2024-08-07 15:56:13 +0300

    chore: Remove code that behaves differently for chain_id == "statelessnet" (#11882)
    The chain with id "statelessnet" is about to be turned down, thus we can
    now remove the code that behaves differently for the statelessnet chain.
    
    NOTE: This will be merged after the statelessnet is really turned down.

--- core/primitives-core/src/chains.rs
+++ core/primitives-core/src/chains.rs
@@ -6,9 +6,6 @@ pub const MAINNET: &str = "mainnet";
 /// Primary testing environment.
 pub const TESTNET: &str = "testnet";
 
-/// Temporary StatelessNet active from 2024-02-01.
-pub const STATELESSNET: &str = "statelessnet";
-
 /// Pre-release testing environment.
 pub const MOCKNET: &str = "mocknet";
 
