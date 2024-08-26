412e55451089554f4044ffa0c40da35c49e4feb8
Jan Ciolek <jan.ciolek@nearone.org>
2024-08-26 10:19:18 +0000

    Set tesnet voting date to Monday 2024-09-02 10:00 UTC

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -81,7 +81,12 @@ pub const PROTOCOL_UPGRADE_SCHEDULE: Lazy<ProtocolUpgradeVotingSchedule> = Lazy:
     // let schedule = vec![(v1_datetime, v1_protocol_version), (v2_datetime, v2_protocol_version)];
     // ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, schedule).unwrap()
 
-    ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, vec![]).unwrap()
+    // 2.2.0-rc.1
+    // Monday
+    let protocol_version = 71;
+    let datetime = ProtocolUpgradeVotingSchedule::parse_datetime("2024-09-02 10:00:00").unwrap();
+    let schedule = vec![(datetime, protocol_version)];
+    ProtocolUpgradeVotingSchedule::new_from_env_or_schedule(PROTOCOL_VERSION, schedule).unwrap()
 });
 
 /// Gives new clients an option to upgrade without announcing that they support
