cd077dbf8a47979247a6fb27598844dd2707e992
Trisfald <trisfald@gmail.com>
2024-08-29 17:58:46 +0200

    add brief logging statement before each command

--- scripts/recover_missing_archival_data.sh
+++ scripts/recover_missing_archival_data.sh
@@ -27,6 +27,7 @@ systemctl is-active --quiet neard && echo "neard must be stopped" && exit 1
 ##########
 export RUST_LOG=info
 
+echo "Regenerating trie nodes between heights 109913254 - 110050000 in shard 2"
 # DB writes = 377624
 /home/ubuntu/neard view-state -t cold --readwrite apply-range --start-index 109913254 --end-index 110050000 --shard-id 2 --storage trie-free --save-state cold sequential
 
@@ -36,15 +37,19 @@ export RUST_LOG=info
 ##########
 export RUST_LOG=debug
 
+echo "Resharding database at height 114580307 and shard 0"
 # DB writes >= 133646770, batch_count = 16569
 /home/ubuntu/neard database resharding --height 114580307 --shard-id 0 --restore
 
+echo "Resharding database at height 114580307 and shard 1"
 # DB writes = 79328587, batch_count = 13437
 /home/ubuntu/neard database resharding --height 114580307 --shard-id 1 --restore
 
+echo "Resharding database at height 114580307 and shard 2"
 # DB writes >= 92619516, batch_count = 16467 
 /home/ubuntu/neard database resharding --height 114580307 --shard-id 2 --restore
 
+echo "Resharding database at height 114580307 and shard 3"
 # DB writes = 258676854, batch_count = 28279
 /home/ubuntu/neard database resharding --height 114580307 --shard-id 3 --restore
 
@@ -54,17 +59,22 @@ export RUST_LOG=debug
 ##########
 export RUST_LOG=debug
 
+echo "Resharding database at height 115185107 and shard 0"
 # DB writes = 135273932, batch_count = 16856
 /home/ubuntu/neard database resharding --height 115185107 --shard-id 0 --restore
 
+echo "Resharding database at height 115185107 and shard 1"
 # DB writes = 79959560, batch_count = 13549
 /home/ubuntu/neard database resharding --height 115185107 --shard-id 1 --restore
 
+echo "Resharding database at height 115185107 and shard 2"
 # DB writes = 188940809, batch_count = 19209
 /home/ubuntu/neard database resharding --height 115185107 --shard-id 2 --restore
 
+echo "Resharding database at height 115185107 and shard 3"
 # DB writes = 66932493, batch_count = 9886
 /home/ubuntu/neard database resharding --height 115185107 --shard-id 3 --restore
 
+echo "Resharding database at height 115185107 and shard 4"
 # DB writes = 194985099, batch_count = 18833
 /home/ubuntu/neard database resharding --height 115185107 --shard-id 4 --restore
