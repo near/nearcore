1555d7ba518771867fd8b3baa69efbd2aaff99fa
Tayfun Elmas <tayfunelmas@gmail.com>
2024-09-06 18:47:42 +0300

    chore: Update replay-headers to report differences in rewards and kickouts (#12048)
    We have been using the `neard replay-headers` command to simulate
    validator rewards and kickouts computations for past epochs to check the
    difference between original and replayed runs.
    
    Submitting the new version of the command we have been using for
    simulating the reward/kickout computations for chunk validators in the
    context of #11900.
    
    This version no longer assumes that the original and replayed values are
    the same and prints the diffs in kickouts and rewards (stakes).

--- tools/state-viewer/src/replay_headers.rs
+++ tools/state-viewer/src/replay_headers.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::path::Path;
 
 use itertools::Itertools;
@@ -10,8 +12,11 @@ use near_epoch_manager::EpochManagerAdapter;
 use near_epoch_manager::EpochManagerHandle;
 use near_primitives::epoch_block_info::BlockInfo;
 use near_primitives::stateless_validation::chunk_endorsements_bitmap::ChunkEndorsementsBitmap;
+use near_primitives::types::AccountId;
+use near_primitives::types::ValidatorKickoutReason;
 use near_primitives::types::{BlockHeight, ValidatorInfoIdentifier};
 use near_primitives::version::ProtocolFeature;
+use near_primitives::views::EpochValidatorInfo;
 use near_store::db::{MixedDB, ReadOrder, TestDB};
 use near_store::{Mode, NodeStorage, Store, Temperature};
 use nearcore::NearConfig;
@@ -38,7 +43,6 @@ pub(crate) fn replay_headers(
     let end_height: BlockHeight = end_height.unwrap_or_else(|| chain_store.head().unwrap().height);
 
     let epoch_manager = EpochManager::new_arc_handle(store, &near_config.genesis.config);
-
     let replay_store = create_replay_store(home_dir, &near_config);
     let epoch_manager_replay =
         EpochManager::new_arc_handle(replay_store, &near_config.genesis.config);
@@ -67,21 +71,144 @@ pub(crate) fn replay_headers(
                 .expect("Could not determine if block is last block in epoch")
             {
                 let identifier = ValidatorInfoIdentifier::BlockHash(block_hash);
-                let validator_info = epoch_manager.get_validator_info(identifier.clone()).unwrap();
-                let validator_info_replay =
+                let original_validators =
+                    epoch_manager.get_validator_info(identifier.clone()).unwrap();
+                let replayed_validators =
                     epoch_manager_replay.get_validator_info(identifier).unwrap();
 
-                assert_eq!(validator_info.epoch_height, validator_info_replay.epoch_height);
-                tracing::info!(
+                assert_eq!(original_validators.epoch_height, replayed_validators.epoch_height);
+                println!(
                     "Comparing validator infos for epoch height {} block height {}",
-                    validator_info.epoch_height,
-                    height
+                    original_validators.epoch_height, height
                 );
-
-                assert_eq!(validator_info, validator_info_replay);
+                // Compare production and kickout statistics between original and replay runs.
+                compare_validator_production_stats(&original_validators, &replayed_validators);
+                compare_validator_kickout_stats(&original_validators, &replayed_validators);
+            } else if header.height() % 1000 == 0 {
+                tracing::debug!("@ height {}", header.height());
             }
         }
     }
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+struct ValidatorSummary {
+    stake: i128,
+    /// Block production percentage (ie. for 60%, this field will be 60).
+    block_production_percent: i64,
+    /// Chunk production percentage (ie. for 60%, this field will be 60).
+    chunk_production_percent: i64,
+    /// Endorsement percentage (ie. for 60%, this field will be 60).
+    chunk_endorsement_percent: i64,
+}
+
+fn compare_validator_production_stats(left: &EpochValidatorInfo, right: &EpochValidatorInfo) {
+    let left_summaries = get_validator_summary(left);
+    let right_summaries = get_validator_summary(right);
+    let left_accounts: HashSet<AccountId> = left_summaries.keys().cloned().collect();
+    let right_accounts: HashSet<AccountId> = right_summaries.keys().cloned().collect();
+    for account_id in left_accounts.union(&right_accounts) {
+        let left_summary = left_summaries.get(account_id);
+        let right_summary = right_summaries.get(account_id);
+        if left_summary.is_some() ^ right_summary.is_some() {
+            println!("{}: {:?} --> {:?}", account_id.as_str(), left_summary, right_summary);
+            continue;
+        }
+        // Note: Both values cannot be None since we are looping over union of the hashmap keys.
+        let left_summary = left_summary.unwrap();
+        let right_summary = right_summary.unwrap();
+        if left_summary.stake != right_summary.stake {
+            println!(
+                "{}: Stake [diff={}] {} --> {}",
+                account_id.as_str(),
+                right_summary.stake - left_summary.stake,
+                left_summary.stake,
+                right_summary.stake
+            );
+        }
+        if left_summary.block_production_percent != right_summary.block_production_percent {
+            println!(
+                "{}: Blocks [diff=%{}] %{} --> %{}",
+                account_id.as_str(),
+                right_summary.block_production_percent - left_summary.block_production_percent,
+                left_summary.block_production_percent,
+                right_summary.block_production_percent
+            );
+        }
+        if left_summary.chunk_production_percent != right_summary.chunk_production_percent {
+            println!(
+                "{}: Chunks [diff=%{}] %{} --> %{}",
+                account_id.as_str(),
+                right_summary.chunk_production_percent - left_summary.chunk_production_percent,
+                left_summary.chunk_production_percent,
+                right_summary.chunk_production_percent
+            );
+        }
+        if left_summary.chunk_endorsement_percent != right_summary.chunk_endorsement_percent {
+            println!(
+                "{}: Endorsements [diff=%{}] %{} --> %{}",
+                account_id.as_str(),
+                right_summary.chunk_endorsement_percent - left_summary.chunk_endorsement_percent,
+                left_summary.chunk_endorsement_percent,
+                right_summary.chunk_endorsement_percent
+            );
+        }
+    }
+}
+
+fn compare_validator_kickout_stats(left: &EpochValidatorInfo, right: &EpochValidatorInfo) {
+    let left_kickouts = get_validator_kickouts(left);
+    let right_kickouts = get_validator_kickouts(right);
+    let left_accounts: HashSet<AccountId> = left_kickouts.keys().cloned().collect();
+    let right_accounts: HashSet<AccountId> = right_kickouts.keys().cloned().collect();
+    for account_id in left_accounts.union(&right_accounts) {
+        let left_kickout = left_kickouts.get(account_id);
+        let right_kickout = right_kickouts.get(account_id);
+        if left_kickout.is_some() ^ right_kickout.is_some() {
+            println!("{}: {:?} --> {:?}", account_id.as_str(), left_kickout, right_kickout);
+            continue;
+        }
+        // Note: Both values cannot be None since we are looping over union of the hashmap keys.
+        let left_kickout = left_kickout.unwrap();
+        let right_kickout = right_kickout.unwrap();
+        if left_kickout != right_kickout {
+            println!("{}: {:?} --> {:?}", account_id.as_str(), left_kickout, right_kickout);
+        }
+    }
+}
+
+/// Returns a mapping from validator account id to the summary of production statistics.
+fn get_validator_summary(
+    validator_info: &EpochValidatorInfo,
+) -> HashMap<AccountId, ValidatorSummary> {
+    let mut validator_to_summary = HashMap::new();
+    for validator in validator_info.current_validators.iter() {
+        let summary = ValidatorSummary {
+            stake: validator.stake.try_into().unwrap(),
+            block_production_percent: (100.0 * (validator.num_produced_blocks as f64)
+                / (validator.num_expected_blocks as f64))
+                .floor() as i64,
+            chunk_production_percent: (100.0 * (validator.num_produced_chunks as f64)
+                / (validator.num_expected_chunks as f64))
+                .floor() as i64,
+            chunk_endorsement_percent: (100.0 * (validator.num_produced_endorsements as f64)
+                / (validator.num_expected_endorsements as f64))
+                .floor() as i64,
+        };
+        validator_to_summary.insert(validator.account_id.clone(), summary);
+    }
+    validator_to_summary
+}
+
+/// Returns a mapping from validator account id to the kickout reasong (from previous epoch).
+fn get_validator_kickouts(
+    validator_info: &EpochValidatorInfo,
+) -> HashMap<AccountId, ValidatorKickoutReason> {
+    let mut kickouts = HashMap::new();
+    for kickout in validator_info.prev_epoch_kickout.iter() {
+        kickouts.insert(kickout.account_id.clone(), kickout.reason.clone());
+    }
+    kickouts
 }
 
 /// Returns the [`BlockInfo`] corresponding to the header.
