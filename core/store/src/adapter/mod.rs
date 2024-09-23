88a4bb7b8335c549d22e052cf065dafce5e676f7
Shreyan Gupta <shreyan.gupta96@gmail.com>
2024-09-23 12:06:04 -0700

    some comments

--- core/store/src/adapter/mod.rs
+++ core/store/src/adapter/mod.rs
@@ -4,11 +4,50 @@ use std::ops::{Deref, DerefMut};
 
 use crate::{Store, StoreUpdate};
 
+/// Internal enum that can store either an owned StoreUpdate to a reference to StoreUpdate.
+/// 
+/// While dealing with store update, the typical pattern is to do something like:
+/// ```rust, ignore
+/// let store_update: StoreUpdate = store.store_update();
+/// 
+/// store_update.set_foo("bar");
+/// some_large_update_function(&mut store_update);
+/// 
+/// store_update.commit()?;
+/// ```
+/// Now with StoreAdapters, store could be of any of the type of the adapters, example `FlatStoreAdapter`.
+/// In that case, we expect the above pattern to look similar, however we would expect calls to
+/// `flat_store.store_update()` to return type `FlatStoreUpdateAdapter` instead of `StoreUpdate`.
+/// 
+/// At the same time we would like to allow conversion of `StoreUpdate` to `FlatStoreUpdateAdapter`.
+/// 
+/// ```rust, ignore
+/// fn update_flat_store(flat_store_update: &mut FlatStoreUpdateAdapter) {
+///     ...
+/// }
+/// 
+/// // Pattern 1: reference to store_update
+/// let store_update: StoreUpdate = store.store_update();
+/// update_flat_store(&mut store_update.flat_store_update());
+/// store_update.commit()?;
+/// 
+/// // Pattern 2: owned store_update
+/// let flat_store: FlatStoreAdapter = store.flat_store();
+/// let flat_store_update: FlatStoreUpdateAdapter<'static> = flat_store.store_update();
+/// update_flat_store(&mut flat_store_update);
+/// flat_store_update.commit()?;
+/// ```
+/// 
+/// To make both these patterns possible, where in pattern 1, flat_store_update holds a reference to store_update
+/// and in pattern 2, flat_store_update owns the instance of store_update, we use this enum.
+/// 
+/// Note that owned versions of flat_store_update have a static lifetime as compared to borrowed versions.
 enum StoreUpdateHolder<'a> {
     Reference(&'a mut StoreUpdate),
     Owned(StoreUpdate),
 }
 
+// Seamless conversion from &store_update_holder to &store_update.
 impl Deref for StoreUpdateHolder<'_> {
     type Target = StoreUpdate;
 
@@ -20,6 +59,7 @@ impl Deref for StoreUpdateHolder<'_> {
     }
 }
 
+// Seamless conversion from &mut store_update_holder to &mut store_update.
 impl DerefMut for StoreUpdateHolder<'_> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         match self {
@@ -29,6 +69,8 @@ impl DerefMut for StoreUpdateHolder<'_> {
     }
 }
 
+// Static instances of StoreUpdateHolder always hold an owned StoreUpdate instance.
+// In such case it should be possible to convert it to StoreUpdate.
 impl Into<StoreUpdate> for StoreUpdateHolder<'static> {
     fn into(self) -> StoreUpdate {
         match self {
@@ -38,6 +80,8 @@ impl Into<StoreUpdate> for StoreUpdateHolder<'static> {
     }
 }
 
+/// Simple adapter wrapper on top of Store to provide a more ergonomic interface for different store types.
+/// We provide simple inter-convertibility between different store types like FlatStoreAdapter and TrieStoreAdapter.
 pub trait StoreAdapter {
     fn store(&self) -> Store;
 
@@ -46,6 +90,11 @@ pub trait StoreAdapter {
     }
 }
 
+/// Simple adapter wrapper on top of StoreUpdate to provide a more ergonomic interface for
+/// different store update types.
+/// We provide simple inter-convertibility between different store update types like FlatStoreUpdateAdapter
+/// and TrieStoreUpdateAdapter, however these are conversions by reference only.
+/// The underlying StoreUpdate instance remains the same.
 pub trait StoreUpdateAdapter: Sized {
     fn store_update(&mut self) -> &mut StoreUpdate;
 
