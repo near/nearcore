0ebf4df25271886d09f2582e41af01e76c581492
Shreyan Gupta <shreyan.gupta96@gmail.com>
2024-09-25 12:07:42 -0700

    fmt

--- core/store/src/adapter/mod.rs
+++ core/store/src/adapter/mod.rs
@@ -5,42 +5,42 @@ use std::ops::{Deref, DerefMut};
 use crate::{Store, StoreUpdate};
 
 /// Internal enum that can store either an owned StoreUpdate to a reference to StoreUpdate.
-/// 
+///
 /// While dealing with store update, the typical pattern is to do something like:
 /// ```rust, ignore
 /// let store_update: StoreUpdate = store.store_update();
-/// 
+///
 /// store_update.set_foo("bar");
 /// some_large_update_function(&mut store_update);
-/// 
+///
 /// store_update.commit()?;
 /// ```
 /// Now with StoreAdapters, store could be of any of the type of the adapters, example `FlatStoreAdapter`.
 /// In that case, we expect the above pattern to look similar, however we would expect calls to
 /// `flat_store.store_update()` to return type `FlatStoreUpdateAdapter` instead of `StoreUpdate`.
-/// 
+///
 /// At the same time we would like to allow conversion of `StoreUpdate` to `FlatStoreUpdateAdapter`.
-/// 
+///
 /// ```rust, ignore
 /// fn update_flat_store(flat_store_update: &mut FlatStoreUpdateAdapter) {
 ///     ...
 /// }
-/// 
+///
 /// // Pattern 1: reference to store_update
 /// let store_update: StoreUpdate = store.store_update();
 /// update_flat_store(&mut store_update.flat_store_update());
 /// store_update.commit()?;
-/// 
+///
 /// // Pattern 2: owned store_update
 /// let flat_store: FlatStoreAdapter = store.flat_store();
 /// let flat_store_update: FlatStoreUpdateAdapter<'static> = flat_store.store_update();
 /// update_flat_store(&mut flat_store_update);
 /// flat_store_update.commit()?;
 /// ```
-/// 
+///
 /// To make both these patterns possible, where in pattern 1, flat_store_update holds a reference to store_update
 /// and in pattern 2, flat_store_update owns the instance of store_update, we use this enum.
-/// 
+///
 /// Note that owned versions of flat_store_update have a static lifetime as compared to borrowed versions.
 enum StoreUpdateHolder<'a> {
     Reference(&'a mut StoreUpdate),
