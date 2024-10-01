923647de276b479b4af4fa7546a05f437221b5e4
wacban <wac.banasik@gmail.com>
2024-10-01 16:30:56 +0100

    trie_key

--- core/primitives/src/test_utils.rs
+++ core/primitives/src/test_utils.rs
@@ -1011,7 +1011,7 @@ impl EpochInfoProvider for MockEpochInfoProvider {
         _account_id: &AccountId,
         _epoch_id: &EpochId,
     ) -> Result<ShardId, EpochError> {
-        Ok(0)
+        Ok(ShardId::new(0))
     }
 }
 
