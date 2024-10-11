77ddba62a4e2156f6c7382394caa133849fde210
wacban <wac.banasik@gmail.com>
2024-10-11 14:54:33 +0100

    types.rs

--- core/primitives-core/src/types.rs
+++ core/primitives-core/src/types.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::hash::CryptoHash;
 
 /// Account identifier. Provides access to user's state.
@@ -44,9 +46,9 @@ pub type PromiseId = Vec<ReceiptIndex>;
 
 pub type ProtocolVersion = u32;
 
-/// The shard identifier. The ShardId is currently being migrated to a newtype -
-/// please see the new ShardId definition below.
-pub type ShardId = u64;
+// /// The shard identifier. The ShardId is currently being migrated to a newtype -
+// /// please see the new ShardId definition below.
+// pub type ShardId = u64;
 
 /// The ShardIndex is the index of the shard in an array of shard data.
 /// Historically the ShardId was always in the range 0..NUM_SHARDS and was used
@@ -58,7 +60,16 @@ pub type ShardIndex = usize;
 // ShardId as a newtype. It should be replaced / removed / inlined once the
 // transition is complete.
 pub const fn new_shard_id_tmp(id: u64) -> ShardId {
-    id
+    // id
+    ShardId::new(id)
+}
+
+// TODO(wacban) This is a temporary solution to aid the transition to having
+// ShardId as a newtype. It should be replaced / removed / inlined once the
+// transition is complete.
+pub const fn shard_id_as_usize(id: ShardId) -> usize {
+    id.get() as usize
+    // id
 }
 
 // TODO(wacban) This is a temporary solution to aid the transition to having
@@ -71,131 +82,148 @@ pub fn new_shard_id_vec_tmp(vec: &[u64]) -> Vec<ShardId> {
 // TODO(wacban) This is a temporary solution to aid the transition to having
 // ShardId as a newtype. It should be replaced / removed / inlined once the
 // transition is complete.
+pub const fn shard_id_as_u16(id: ShardId) -> u16 {
+    id.get() as u16
+    // id as u16
+}
+
+// TODO(wacban) This is a temporary solution to aid the transition to having
+// ShardId as a newtype. It should be replaced / removed / inlined once the
+// transition is complete.
 pub const fn shard_id_as_u32(id: ShardId) -> u32 {
-    id as u32
+    id.get() as u32
+    // id as u32
+}
+
+// TODO(wacban) This is a temporary solution to aid the transition to having
+// ShardId as a newtype. It should be replaced / removed / inlined once the
+// transition is complete.
+pub const fn shard_id_as_u64(id: ShardId) -> u64 {
+    id.get()
+    // id
 }
 
 // TODO(wacban) Complete the transition to ShardId as a newtype.
-// /// The shard identifier. It may be a arbitrary number - it does not need to be
-// /// a number in the range 0..NUM_SHARDS. The shard ids do not need to be
-// /// sequential or contiguous.
-// ///
-// /// The shard id is wrapped in a newtype to prevent the old pattern of using
-// /// indices in range 0..NUM_SHARDS and casting to ShardId. Once the transition
-// /// if fully complete it potentially may be simplified to a regular type alias.
-// #[derive(
-//     arbitrary::Arbitrary,
-//     borsh::BorshSerialize,
-//     borsh::BorshDeserialize,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     Hash,
-//     Clone,
-//     Copy,
-//     Debug,
-//     PartialEq,
-//     Eq,
-//     PartialOrd,
-//     Ord,
-// )]
-// pub struct ShardId(u64);
+/// The shard identifier. It may be a arbitrary number - it does not need to be
+/// a number in the range 0..NUM_SHARDS. The shard ids do not need to be
+/// sequential or contiguous.
+///
+/// The shard id is wrapped in a newtype to prevent the old pattern of using
+/// indices in range 0..NUM_SHARDS and casting to ShardId. Once the transition
+/// if fully complete it potentially may be simplified to a regular type alias.
+#[derive(
+    arbitrary::Arbitrary,
+    borsh::BorshSerialize,
+    borsh::BorshDeserialize,
+    serde::Serialize,
+    serde::Deserialize,
+    Hash,
+    Clone,
+    Copy,
+    Debug,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+)]
+pub struct ShardId(u64);
 
-// impl ShardId {
-//     /// Create a new shard id. Please note that this function should not be used
-//     /// to convert a shard index (a number in 0..num_shards range) to ShardId.
-//     /// Instead the ShardId should be obtained from the shard_layout.
-//     ///
-//     /// ```
-//     /// // BAD USAGE:
-//     /// for shard_index in 1..num_shards {
-//     ///     let shard_id = ShardId::new(shard_index); // Incorrect!!!
-//     /// }
-//     /// ```
-//     /// ```
-//     /// // GOOD USAGE 1:
-//     /// for shard_index in 1..num_shards {
-//     ///     let shard_id = shard_layout.get_shard_id(shard_index);
-//     /// }
-//     /// // GOOD USAGE 2:
-//     /// for shard_id in shard_layout.shard_ids() {
-//     ///     let shard_id = shard_layout.get_shard_id(shard_index);
-//     /// }
-//     /// ```
-//     pub const fn new(id: u64) -> Self {
-//         Self(id)
-//     }
+impl ShardId {
+    /// Create a new shard id. Please note that this function should not be used
+    /// to convert a shard index (a number in 0..num_shards range) to ShardId.
+    /// Instead the ShardId should be obtained from the shard_layout.
+    ///
+    /// ```
+    /// // BAD USAGE:
+    /// for shard_index in 1..num_shards {
+    ///     let shard_id = ShardId::new(shard_index); // Incorrect!!!
+    /// }
+    /// ```
+    /// ```
+    /// // GOOD USAGE 1:
+    /// for shard_index in 1..num_shards {
+    ///     let shard_id = shard_layout.get_shard_id(shard_index);
+    /// }
+    /// // GOOD USAGE 2:
+    /// for shard_id in shard_layout.shard_ids() {
+    ///     let shard_id = shard_layout.get_shard_id(shard_index);
+    /// }
+    /// ```
+    pub const fn new(id: u64) -> Self {
+        Self(id)
+    }
 
-//     /// Get the numerical value of the shard id. This should not be used as an
-//     /// index into an array, as the shard id may be any arbitrary number.
-//     pub fn get(self) -> u64 {
-//         self.0
-//     }
+    /// Get the numerical value of the shard id. This should not be used as an
+    /// index into an array, as the shard id may be any arbitrary number.
+    pub const fn get(self) -> u64 {
+        self.0
+    }
 
-//     pub fn to_le_bytes(self) -> [u8; 8] {
-//         self.0.to_le_bytes()
-//     }
+    pub fn to_le_bytes(self) -> [u8; 8] {
+        self.0.to_le_bytes()
+    }
 
-//     pub fn from_le_bytes(bytes: [u8; 8]) -> Self {
-//         Self(u64::from_le_bytes(bytes))
-//     }
+    pub fn from_le_bytes(bytes: [u8; 8]) -> Self {
+        Self(u64::from_le_bytes(bytes))
+    }
 
-//     // TODO This is not great, in ShardUId shard_id is u32.
-//     // Currently used for some metrics so kinda ok.
-//     pub fn max() -> Self {
-//         Self(u64::MAX)
-//     }
-// }
+    // TODO This is not great, in ShardUId shard_id is u32.
+    // Currently used for some metrics so kinda ok.
+    pub fn max() -> Self {
+        Self(u64::MAX)
+    }
+}
 
-// impl Display for ShardId {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{}", self.0)
-//     }
-// }
+impl Display for ShardId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
-// impl From<u64> for ShardId {
-//     fn from(id: u64) -> Self {
-//         Self(id)
-//     }
-// }
+impl From<u64> for ShardId {
+    fn from(id: u64) -> Self {
+        Self(id)
+    }
+}
 
-// impl Into<u64> for ShardId {
-//     fn into(self) -> u64 {
-//         self.0
-//     }
-// }
+impl Into<u64> for ShardId {
+    fn into(self) -> u64 {
+        self.0
+    }
+}
 
-// impl From<u32> for ShardId {
-//     fn from(id: u32) -> Self {
-//         Self(id as u64)
-//     }
-// }
+impl From<u32> for ShardId {
+    fn from(id: u32) -> Self {
+        Self(id as u64)
+    }
+}
 
-// impl Into<u32> for ShardId {
-//     fn into(self) -> u32 {
-//         self.0 as u32
-//     }
-// }
+impl Into<u32> for ShardId {
+    fn into(self) -> u32 {
+        self.0 as u32
+    }
+}
 
-// impl From<i32> for ShardId {
-//     fn from(id: i32) -> Self {
-//         Self(id as u64)
-//     }
-// }
+impl From<i32> for ShardId {
+    fn from(id: i32) -> Self {
+        Self(id as u64)
+    }
+}
 
-// impl From<usize> for ShardId {
-//     fn from(id: usize) -> Self {
-//         Self(id as u64)
-//     }
-// }
+impl From<usize> for ShardId {
+    fn from(id: usize) -> Self {
+        Self(id as u64)
+    }
+}
 
-// impl From<u16> for ShardId {
-//     fn from(id: u16) -> Self {
-//         Self(id as u64)
-//     }
-// }
+impl From<u16> for ShardId {
+    fn from(id: u16) -> Self {
+        Self(id as u64)
+    }
+}
 
-// impl Into<u16> for ShardId {
-//     fn into(self) -> u16 {
-//         self.0 as u16
-//     }
-// }
+impl Into<u16> for ShardId {
+    fn into(self) -> u16 {
+        self.0 as u16
+    }
+}
