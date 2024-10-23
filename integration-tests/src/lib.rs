15336ba86249fa4fffc8e16a7160979e2fb7b3c0
Jan Ciolek <jan.ciolek@nearone.org>
2024-10-23 17:49:15 +0000

    check that boxing a struct doesn't change serialization format

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -1,3 +1,5 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
 pub mod node;
 pub mod runtime_utils;
 pub mod user;
@@ -6,3 +8,24 @@ pub mod user;
 mod test_loop;
 #[cfg(test)]
 mod tests;
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Struct1 {
+    numbers: Vec<i32>,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Struct2 {
+    numbers: Box<Vec<i32>>,
+}
+
+#[test]
+fn box_doesnt_change_serialization() {
+    let s1 = Struct1 { numbers: vec![1, 2, 3, 4] };
+
+    let ser = borsh::to_vec(&s1).unwrap();
+
+    let deser: Struct2 = borsh::from_slice(ser.as_slice()).unwrap();
+
+    assert_eq!(deser.numbers.as_slice(), &[1, 2, 3, 4]);
+}
