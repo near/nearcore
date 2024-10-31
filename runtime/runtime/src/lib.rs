cc672a2d842da8cfe7ebefc5be1ff33c93461406
Jan Malinowski <jan.ciolek@nearone.org>
2024-10-31 21:08:52 +0000

    Add StateStoredReceiptV1

--- runtime/runtime/src/lib.rs
+++ runtime/runtime/src/lib.rs
@@ -1744,7 +1744,7 @@ impl Runtime {
                 processing_state.delayed_receipts.push(
                     &mut processing_state.state_update,
                     &receipt,
-                    &processing_state.apply_state.config,
+                    &processing_state.apply_state,
                 )?;
             } else {
                 if let Some(nsi) = &mut next_schedule_after {
@@ -1927,7 +1927,7 @@ impl Runtime {
                 processing_state.delayed_receipts.push(
                     &mut processing_state.state_update,
                     receipt,
-                    &processing_state.apply_state.config,
+                    &processing_state.apply_state,
                 )?;
             } else {
                 if let Some(nsi) = &mut next_schedule_after {
@@ -2675,6 +2675,7 @@ pub mod estimator {
             outgoing_limit,
             outgoing_buffers: ShardsOutgoingReceiptBuffer::load(&state_update.trie)?,
             outgoing_receipts,
+            protocol_version: apply_state.current_protocol_version,
         });
         let empty_pipeline = ReceiptPreparationPipeline::new(
             std::sync::Arc::clone(&apply_state.config),
