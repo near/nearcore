near/nearcore

commit 4c23860efd015b395f8e9a217d024999a859465f

refactor: make witness reed solomon parts reusable  (#12341)
This PR makes code related to witness parts tracking generic so it can
be reused for deployed contracts distribution.
We need to merge it ASAP, tests will be added in a separate PR.

core/primitives/src/stateless_validation/state_witness.rs
@@ -5,6 +5,7 @@ use super::{ChunkProductionKey, SignatureDifferentiator};
 use crate::bandwidth_scheduler::BandwidthRequests;
 use crate::challenge::PartialState;
 use crate::congestion_info::CongestionInfo;
+use crate::reed_solomon::{ReedSolomonEncoderDeserialize, ReedSolomonEncoderSerialize};
 use crate::sharding::{ChunkHash, ReceiptProof, ShardChunkHeader, ShardChunkHeaderV3};
 use crate::transaction::SignedTransaction;
 use crate::types::EpochId;
@@ -45,6 +46,18 @@ impl
         STATE_WITNESS_COMPRESSION_LEVEL,
     > for EncodedChunkStateWitness
 {
+}
+
+impl ReedSolomonEncoderSerialize for EncodedChunkStateWitness {
+    fn serialize_single_part(&self) -> std::io::Result<Vec<u8>> {
+        Ok(self.as_slice().to_vec())
+    }
+}
+
+impl ReedSolomonEncoderDeserialize for EncodedChunkStateWitness {
+    fn deserialize_single_part(data: &[u8]) -> std::io::Result<Self> {
+        Ok(EncodedChunkStateWitness::from_boxed_slice(data.to_vec().into_boxed_slice()))
+    }
 }
 
 pub type ChunkStateWitnessSize = usize;

chain/client/src/stateless_validation/partial_witness/encoding.rs
@@ -1,100 +1,13 @@
-use std::collections::HashMap;
-use std::sync::Arc;
-
-use near_primitives::reed_solomon::{
-    reed_solomon_decode, reed_solomon_encode, reed_solomon_part_length,
-};
-use near_primitives::stateless_validation::state_witness::EncodedChunkStateWitness;
-use near_primitives::utils::compression::CompressedData;
-use reed_solomon_erasure::galois_8::ReedSolomon;
+use near_primitives::reed_solomon::{reed_solomon_num_data_parts, reed_solomon_part_length};
 
 /// Ratio of the number of data parts to total parts in the Reed Solomon encoding.
 /// The tradeoff here is having a higher ratio is better for handling missing parts and network errors
 /// but increases the size of the encoded state witness and the total network bandwidth requirements.
-const RATIO_DATA_PARTS: f32 = 0.6;
-
-/// Type alias around what ReedSolomon represents data part as.
-/// This should help with making the code a bit more understandable.
-pub type WitnessPart = Option<Box<[u8]>>;
-
-/// Reed Solomon encoder wrapper for encoding and decoding state witness parts.
-pub struct WitnessEncoder {
-    /// None corresponds to the case when we are the only validator for the chunk
-    /// since ReedSolomon does not support having exactly 1 total part count and
-    /// no parity parts.
-    rs: Option<ReedSolomon>,
-}
-
-impl WitnessEncoder {
-    fn new(total_parts: usize) -> WitnessEncoder {
-        let rs = if total_parts > 1 {
-            let data_parts = num_witness_data_parts(total_parts);
-            Some(ReedSolomon::new(data_parts, total_parts - data_parts).unwrap())
-        } else {
-            None
-        };
-        Self { rs }
-    }
-
-    pub fn total_parts(&self) -> usize {
-        match self.rs {
-            Some(ref rs) => rs.total_shard_count(),
-            None => 1,
-        }
-    }
-
-    pub fn data_parts(&self) -> usize {
-        match self.rs {
-            Some(ref rs) => rs.data_shard_count(),
-            None => 1,
-        }
-    }
-
-    pub fn encode(&self, witness: &EncodedChunkStateWitness) -> (Vec<WitnessPart>, usize) {
-        match self.rs {
-            Some(ref rs) => reed_solomon_encode(rs, witness),
-            None => {
-                (vec![Some(witness.as_slice().to_vec().into_boxed_slice())], witness.size_bytes())
-            }
-        }
-    }
-
-    pub fn decode(
-        &self,
-        parts: &mut [WitnessPart],
-        encoded_length: usize,
-    ) -> Result<EncodedChunkStateWitness, std::io::Error> {
-        match self.rs {
-            Some(ref rs) => reed_solomon_decode(rs, parts, encoded_length),
-            None => {
-                Ok(EncodedChunkStateWitness::from_boxed_slice(parts[0].as_ref().unwrap().clone()))
-            }
-        }
-    }
-}
-
-/// We keep one encoder for each length of chunk_validators to avoid re-creating the encoder.
-pub struct WitnessEncoderCache {
-    instances: HashMap<usize, Arc<WitnessEncoder>>,
-}
-
-impl WitnessEncoderCache {
-    pub fn new() -> Self {
-        Self { instances: HashMap::new() }
-    }
-
-    pub fn entry(&mut self, total_parts: usize) -> Arc<WitnessEncoder> {
-        self.instances
-            .entry(total_parts)
-            .or_insert_with(|| Arc::new(WitnessEncoder::new(total_parts)))
-            .clone()
-    }
-}
+pub const WITNESS_RATIO_DATA_PARTS: f64 = 0.6;
 
 pub fn witness_part_length(encoded_witness_size: usize, total_parts: usize) -> usize {
-    reed_solomon_part_length(encoded_witness_size, num_witness_data_parts(total_parts))
-}
-
-fn num_witness_data_parts(total_parts: usize) -> usize {
-    std::cmp::max((total_parts as f32 * RATIO_DATA_PARTS) as usize, 1)
+    reed_solomon_part_length(
+        encoded_witness_size,
+        reed_solomon_num_data_parts(total_parts, WITNESS_RATIO_DATA_PARTS),
+    )
 }

chain/client/src/stateless_validation/partial_witness/partial_witness_actor.rs
@@ -17,6 +17,7 @@ use near_network::state_witness::{
 use near_network::types::{NetworkRequests, PeerManagerAdapter, PeerManagerMessageRequest};
 use near_parameters::RuntimeConfig;
 use near_performance_metrics_macros::perf;
+use near_primitives::reed_solomon::ReedSolomonEncoderCache;
 use near_primitives::sharding::ShardChunkHeader;
 use near_primitives::stateless_validation::contract_distribution::{
     ChunkContractAccesses, ChunkContractDeployments, CodeBytes, CodeHash, ContractCodeRequest,
@@ -40,7 +41,7 @@ use crate::stateless_validation::validate::{
     validate_chunk_contract_accesses, validate_partial_encoded_state_witness,
 };
 
-use super::encoding::WitnessEncoderCache;
+use super::encoding::WITNESS_RATIO_DATA_PARTS;
 use super::partial_witness_tracker::PartialEncodedStateWitnessTracker;
 use near_primitives::utils::compression::CompressedData;
 
@@ -59,7 +60,7 @@ pub struct PartialWitnessActor {
     state_witness_tracker: ChunkStateWitnessTracker,
     /// Reed Solomon encoder for encoding state witness parts.
     /// We keep one wrapper for each length of chunk_validators to avoid re-creating the encoder.
-    encoders: WitnessEncoderCache,
+    witness_encoders: ReedSolomonEncoderCache,
 }
 
 impl Actor for PartialWitnessActor {}
@@ -157,8 +158,8 @@ impl PartialWitnessActor {
             epoch_manager,
             partial_witness_tracker,
             state_witness_tracker: ChunkStateWitnessTracker::new(clock),
-            encoders: WitnessEncoderCache::new(),
             runtime,
+            witness_encoders: ReedSolomonEncoderCache::new(WITNESS_RATIO_DATA_PARTS),
         }
     }
 
@@ -207,7 +208,7 @@ impl PartialWitnessActor {
         );
 
         // Break the state witness into parts using Reed Solomon encoding.
-        let encoder = self.encoders.entry(chunk_validators.len());
+        let encoder = self.witness_encoders.entry(chunk_validators.len());
         let (parts, encoded_length) = encoder.encode(&witness_bytes);
 
         Ok(chunk_validators

chain/client/src/stateless_validation/partial_witness/partial_witness_tracker.rs
@@ -10,6 +10,9 @@ use near_chain::Error;
 use near_epoch_manager::EpochManagerAdapter;
 use near_primitives::challenge::PartialState;
 use near_primitives::hash::CryptoHash;
+use near_primitives::reed_solomon::{
+    InsertPartResult, ReedSolomonEncoder, ReedSolomonEncoderCache, ReedSolomonPartsTracker,
+};
 use near_primitives::stateless_validation::contract_distribution::{CodeBytes, CodeHash};
 use near_primitives::stateless_validation::partial_witness::PartialEncodedStateWitness;
 use near_primitives::stateless_validation::state_witness::{
@@ -22,8 +25,9 @@ use time::ext::InstantExt as _;
 use crate::client_actor::ClientSenderForPartialWitness;
 use crate::metrics;
 
-use super::encoding::{WitnessEncoder, WitnessEncoderCache, WitnessPart};
 use near_primitives::utils::compression::CompressedData;
+
+use super::encoding::WITNESS_RATIO_DATA_PARTS;
 
 /// Max number of chunks to keep in the witness tracker cache. We reach here only after validation
 /// of the partial_witness so the LRU cache size need not be too large.
@@ -58,82 +62,13 @@ impl AccessedContractsState {
     }
 }
 
-struct WaitingPartsState {
-    parts: Vec<WitnessPart>,
-    encoded_length: usize,
-    data_parts_present: usize,
-    encoder: Arc<WitnessEncoder>,
-    total_parts_size: usize,
-}
-
 enum WitnessPartsState {
     /// Haven't received any parts yet.
     Empty,
     /// Received at least one part, but not enough to decode the witness.
-    WaitingParts(WaitingPartsState),
+    WaitingParts(ReedSolomonPartsTracker<EncodedChunkStateWitness>),
     /// Received enough parts and tried decoding the witness.
     Decoded { decode_result: DecodePartialWitnessResult, decoded_at: Instant },
-}
-
-impl WaitingPartsState {
-    fn new(encoder: Arc<WitnessEncoder>, encoded_length: usize) -> Self {
-        Self {
-            data_parts_present: 0,
-            parts: vec![None; encoder.total_parts()],
-            total_parts_size: 0,
-            encoded_length,
-            encoder,
-        }
-    }
-
-    fn data_parts_required(&self) -> usize {
-        self.encoder.data_parts()
-    }
-
-    fn has_enough_parts(&self) -> bool {
-        self.data_parts_present >= self.data_parts_required()
-    }
-
-    fn insert_part(
-        &mut self,
-        partial_witness: PartialEncodedStateWitness,
-    ) -> Option<DecodePartialWitnessResult> {
-        let ChunkProductionKey { shard_id, height_created, .. } =
-            partial_witness.chunk_production_key();
-        let part_ord = partial_witness.part_ord();
-        let encoded_length = partial_witness.encoded_length();
-        let part = partial_witness.into_part();
-
-        // Check if the part is already present.
-        if self.parts[part_ord].is_some() {
-            tracing::warn!(
-                target: "client",
-                ?shard_id,
-                height_created,
-                part_ord,
-                "Received duplicate or redundant state witness part."
-            );
-            return None;
-        }
-
-        if self.encoded_length != encoded_length {
-            tracing::warn!(
-                target: "client",
-                expected = self.encoded_length,
-                actual = encoded_length,
-                "Partial encoded witness length field doesn't match",
-            );
-            return None;
-        }
-
-        // Increment the count of data parts present even if the part has been decoded before.
-        // We use this in metrics to track the number of parts received. Insert the part into the cache entry.
-        self.data_parts_present += 1;
-        self.total_parts_size += part.len();
-        self.parts[part_ord] = Some(part);
-
-        self.has_enough_parts().then(|| self.encoder.decode(&mut self.parts, self.encoded_length))
-    }
 }
 
 struct CacheEntry {
@@ -144,7 +79,7 @@ struct CacheEntry {
 }
 
 enum CacheUpdate {
-    WitnessPart(PartialEncodedStateWitness, Arc<WitnessEncoder>),
+    WitnessPart(PartialEncodedStateWitness, Arc<ReedSolomonEncoder>),
     AccessedContractHashes(HashSet<CodeHash>),
     AccessedContractCodes(Vec<CodeBytes>),
 }
@@ -168,7 +103,7 @@ impl CacheEntry {
 
     pub fn data_parts_present(&self) -> Option<usize> {
         match &self.witness_parts {
-            WitnessPartsState::WaitingParts(data) => Some(data.data_parts_present),
+            WitnessPartsState::WaitingParts(parts) => Some(parts.data_parts_present()),
             WitnessPartsState::Empty | WitnessPartsState::Decoded { .. } => None,
         }
     }
@@ -176,7 +111,7 @@ impl CacheEntry {
     pub fn total_size(&self) -> usize {
         let parts_size = match &self.witness_parts {
             WitnessPartsState::Empty => 0,
-            WitnessPartsState::WaitingParts(parts) => parts.total_parts_size,
+            WitnessPartsState::WaitingParts(parts) => parts.total_parts_size(),
             WitnessPartsState::Decoded { decode_result, .. } => {
                 decode_result.as_ref().map_or(0, |witness| witness.size_bytes())
             }
@@ -211,10 +146,10 @@ impl CacheEntry {
     fn process_witness_part(
         &mut self,
         partial_witness: PartialEncodedStateWitness,
-        encoder: Arc<WitnessEncoder>,
+        encoder: Arc<ReedSolomonEncoder>,
     ) {
         if matches!(self.witness_parts, WitnessPartsState::Empty) {
-            let parts = WaitingPartsState::new(encoder, partial_witness.encoded_length());
+            let parts = ReedSolomonPartsTracker::new(encoder, partial_witness.encoded_length());
             self.witness_parts = WitnessPartsState::WaitingParts(parts);
         }
         let parts = match &mut self.witness_parts {
@@ -222,15 +157,47 @@ impl CacheEntry {
             WitnessPartsState::WaitingParts(parts) => parts,
             WitnessPartsState::Decoded { .. } => return,
         };
-        if let Some(decode_result) = parts.insert_part(partial_witness) {
-            self.witness_parts =
-                WitnessPartsState::Decoded { decode_result, decoded_at: Instant::now() };
-            metrics::DECODE_PARTIAL_WITNESS_ACCESSED_CONTRACTS_STATE_COUNT
-                .with_label_values(&[
-                    &self.shard_id.to_string(),
-                    self.accessed_contracts.metrics_label(),
-                ])
-                .inc();
+        let key = partial_witness.chunk_production_key();
+        if parts.encoded_length() != partial_witness.encoded_length() {
+            tracing::warn!(
+                target: "client",
+                ?key,
+                expected = parts.encoded_length(),
+                actual = partial_witness.encoded_length(),
+                "Partial encoded witness encoded length field does not match",
+            );
+            return;
+        }
+        let part_ord = partial_witness.part_ord();
+        let part = partial_witness.into_part();
+        match parts.insert_part(part_ord, part) {
+            InsertPartResult::Accepted => {}
+            InsertPartResult::PartAlreadyAvailable => {
+                tracing::warn!(
+                    target: "client",
+                    ?key,
+                    part_ord,
+                    "Received duplicate or redundant state witness part"
+                );
+            }
+            InsertPartResult::InvalidPartOrd => {
+                tracing::warn!(
+                    target: "client",
+                    ?key,
+                    part_ord,
+                    "Received invalid partial witness part ord"
+                );
+            }
+            InsertPartResult::Decoded(decode_result) => {
+                self.witness_parts =
+                    WitnessPartsState::Decoded { decode_result, decoded_at: Instant::now() };
+                metrics::DECODE_PARTIAL_WITNESS_ACCESSED_CONTRACTS_STATE_COUNT
+                    .with_label_values(&[
+                        &self.shard_id.to_string(),
+                        self.accessed_contracts.metrics_label(),
+                    ])
+                    .inc();
+            }
         }
     }
 
@@ -347,7 +314,7 @@ pub struct PartialEncodedStateWitnessTracker {
     /// times.
     processed_witnesses: LruCache<ChunkProductionKey, ()>,
     /// Reed Solomon encoder for decoding state witness parts.
-    encoders: WitnessEncoderCache,
+    encoders: ReedSolomonEncoderCache,
 }
 
 impl PartialEncodedStateWitnessTracker {
@@ -362,7 +329,7 @@ impl PartialEncodedStateWitnessTracker {
             processed_witnesses: LruCache::new(
                 NonZeroUsize::new(PROCESSED_WITNESSES_CACHE_SIZE).unwrap(),
             ),
-            encoders: WitnessEncoderCache::new(),
+            encoders: ReedSolomonEncoderCache::new(WITNESS_RATIO_DATA_PARTS),
         }
     }
 
@@ -466,7 +433,7 @@ impl PartialEncodedStateWitnessTracker {
         Ok(())
     }
 
-    fn get_encoder(&mut self, key: &ChunkProductionKey) -> Result<Arc<WitnessEncoder>, Error> {
+    fn get_encoder(&mut self, key: &ChunkProductionKey) -> Result<Arc<ReedSolomonEncoder>, Error> {
         // The expected number of parts for the Reed Solomon encoding is the number of chunk validators.
         let num_parts = self
             .epoch_manager

core/primitives/src/reed_solomon.rs
@@ -1,13 +1,19 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use itertools::Itertools;
 use reed_solomon_erasure::galois_8::ReedSolomon;
+use std::collections::HashMap;
 use std::io::Error;
+use std::sync::Arc;
+
+/// Type alias around what ReedSolomon represents data part as.
+/// This should help with making the code a bit more understandable.
+pub type ReedSolomonPart = Option<Box<[u8]>>;
 
 // Encode function takes a serializable object and returns a tuple of parts and length of encoded data
 pub fn reed_solomon_encode<T: BorshSerialize>(
     rs: &ReedSolomon,
     data: T,
-) -> (Vec<Option<Box<[u8]>>>, usize) {
+) -> (Vec<ReedSolomonPart>, usize) {
     let mut bytes = borsh::to_vec(&data).unwrap();
     let encoded_length = bytes.len();
 
@@ -36,7 +42,7 @@ pub fn reed_solomon_encode<T: BorshSerialize>(
 // Return an error if the reed solomon decoding fails or borsh deserialization fails.
 pub fn reed_solomon_decode<T: BorshDeserialize>(
     rs: &ReedSolomon,
-    parts: &mut [Option<Box<[u8]>>],
+    parts: &mut [ReedSolomonPart],
     encoded_length: usize,
 ) -> Result<T, Error> {
     if let Err(err) = rs.reconstruct(parts) {
@@ -55,4 +61,183 @@ pub fn reed_solomon_decode<T: BorshDeserialize>(
 
 pub fn reed_solomon_part_length(encoded_length: usize, data_parts: usize) -> usize {
     (encoded_length + data_parts - 1) / data_parts
+}
+
+pub fn reed_solomon_num_data_parts(total_parts: usize, ratio_data_parts: f64) -> usize {
+    std::cmp::max((total_parts as f64 * ratio_data_parts) as usize, 1)
+}
+
+pub struct ReedSolomonEncoder {
+    /// ReedSolomon does not support having exactly 1 total part count and
+    /// no parity parts, so we use None for that
+    rs: Option<ReedSolomon>,
+}
+
+/// We need separate ReedSolomonEncoder[Serialize|Deserialize] traits instead
+/// of just using Borsh directly because of already existing code for handling
+/// chunk state witness where we do not use Borsh for single part encoding.
+/// This was accidental, but unfortunately we cannot change it because now
+/// it is a part of the networking protocol.
+pub trait ReedSolomonEncoderSerialize: BorshSerialize {
+    fn serialize_single_part(&self) -> std::io::Result<Vec<u8>> {
+        borsh::to_vec(self)
+    }
+}
+
+pub trait ReedSolomonEncoderDeserialize: BorshDeserialize {
+    fn deserialize_single_part(data: &[u8]) -> std::io::Result<Self> {
+        Self::try_from_slice(data)
+    }
+}
+
+impl ReedSolomonEncoder {
+    pub fn new(total_parts: usize, ratio_data_parts: f64) -> ReedSolomonEncoder {
+        let rs = if total_parts > 1 {
+            let data_parts = reed_solomon_num_data_parts(total_parts, ratio_data_parts);
+            Some(ReedSolomon::new(data_parts, total_parts - data_parts).unwrap())
+        } else {
+            None
+        };
+        Self { rs }
+    }
+
+    pub fn total_parts(&self) -> usize {
+        match self.rs {
+            Some(ref rs) => rs.total_shard_count(),
+            None => 1,
+        }
+    }
+
+    pub fn data_parts(&self) -> usize {
+        match self.rs {
+            Some(ref rs) => rs.data_shard_count(),
+            None => 1,
+        }
+    }
+
+    pub fn encode<T: ReedSolomonEncoderSerialize>(
+        &self,
+        data: &T,
+    ) -> (Vec<ReedSolomonPart>, usize) {
+        match self.rs {
+            Some(ref rs) => reed_solomon_encode(rs, data),
+            None => {
+                let bytes = T::serialize_single_part(&data).unwrap();
+                let size = bytes.len();
+                (vec![Some(bytes.into_boxed_slice())], size)
+            }
+        }
+    }
+
+    pub fn decode<T: ReedSolomonEncoderDeserialize>(
+        &self,
+        parts: &mut [ReedSolomonPart],
+        encoded_length: usize,
+    ) -> Result<T, std::io::Error> {
+        match self.rs {
+            Some(ref rs) => reed_solomon_decode(rs, parts, encoded_length),
+            None => {
+                if parts.len() != 1 {
+                    return Err(std::io::Error::other(format!(
+                        "Expected single part, received {}",
+                        parts.len()
+                    )));
+                }
+                let Some(part) = &parts[0] else {
+                    return Err(std::io::Error::other("Received part is not expected to be None"));
+                };
+                T::deserialize_single_part(part.as_ref())
+            }
+        }
+    }
+}
+
+pub struct ReedSolomonEncoderCache {
+    ratio_data_parts: f64,
+    instances: HashMap<usize, Arc<ReedSolomonEncoder>>,
+}
+
+impl ReedSolomonEncoderCache {
+    pub fn new(ratio_data_parts: f64) -> Self {
+        Self { ratio_data_parts, instances: HashMap::new() }
+    }
+
+    /// Gets an encoder (or adds a new one to the cache if not present) for a
+    /// given number of the total parts.
+    pub fn entry(&mut self, total_parts: usize) -> Arc<ReedSolomonEncoder> {
+        self.instances
+            .entry(total_parts)
+            .or_insert_with(|| {
+                Arc::new(ReedSolomonEncoder::new(total_parts, self.ratio_data_parts))
+            })
+            .clone()
+    }
+}
+
+pub struct ReedSolomonPartsTracker<T> {
+    parts: Vec<ReedSolomonPart>,
+    encoded_length: usize,
+    data_parts_present: usize,
+    encoder: Arc<ReedSolomonEncoder>,
+    total_parts_size: usize,
+    phantom: std::marker::PhantomData<T>,
+}
+
+pub enum InsertPartResult<T> {
+    Accepted,
+    PartAlreadyAvailable,
+    InvalidPartOrd,
+    Decoded(std::io::Result<T>),
+}
+
+impl<T: ReedSolomonEncoderDeserialize> ReedSolomonPartsTracker<T> {
+    pub fn new(encoder: Arc<ReedSolomonEncoder>, encoded_length: usize) -> Self {
+        Self {
+            data_parts_present: 0,
+            parts: vec![None; encoder.total_parts()],
+            total_parts_size: 0,
+            encoded_length,
+            encoder,
+            phantom: std::marker::PhantomData,
+        }
+    }
+
+    pub fn data_parts_present(&self) -> usize {
+        self.data_parts_present
+    }
+
+    pub fn total_parts_size(&self) -> usize {
+        self.total_parts_size
+    }
+
+    pub fn data_parts_required(&self) -> usize {
+        self.encoder.data_parts()
+    }
+
+    pub fn has_enough_parts(&self) -> bool {
+        self.data_parts_present >= self.data_parts_required()
+    }
+
+    pub fn encoded_length(&self) -> usize {
+        self.encoded_length
+    }
+
+    pub fn insert_part(&mut self, part_ord: usize, part: Box<[u8]>) -> InsertPartResult<T> {
+        if part_ord >= self.parts.len() {
+            return InsertPartResult::InvalidPartOrd;
+        }
+        if self.parts[part_ord].is_some() {
+            return InsertPartResult::PartAlreadyAvailable;
+        }
+
+        self.data_parts_present += 1;
+        self.total_parts_size += part.len();
+        self.parts[part_ord] = Some(part);
+
+        if self.has_enough_parts() {
+            InsertPartResult::Decoded(self.encoder.decode(&mut self.parts, self.encoded_length))
+        } else {
+            InsertPartResult::Accepted
+        }
+    }
 }

commit 500d7f903c472284db0424e761d10b80ee749b25

fix(CI) - fix the check_build_public_libraries workflow (#12357)
It looks like the "Windows check for building public libraries" CI check
was broken by https://github.com/near/nearcore/pull/12341

Running `just check_build_public_libraries` produced errors and
warnings:
```
cargo check -p near-primitives -p near-crypto -p near-jsonrpc-primitives -p near-chain-configs -p near-primitives-core
    Checking near-primitives v0.0.0 (/home/jan/code/nearcore/core/primitives)
error[E0432]: unresolved import `crate::reed_solomon`
  --> core/primitives/src/stateless_validation/contract_distribution.rs:10:12
   |
10 | use crate::reed_solomon::{ReedSolomonEncoderDeserialize, ReedSolomonEncoderSerialize};
   |            ^^^^^^^^^^^^ could not find `reed_solomon` in the crate root
   |
note: found an item that was configured out
  --> core/primitives/src/lib.rs:30:9
   |
30 | pub mod reed_solomon;
   |         ^^^^^^^^^^^^
note: the item is gated behind the `solomon` feature
  --> core/primitives/src/lib.rs:29:7
   |
29 | #[cfg(feature = "solomon")]
   |       ^^^^^^^^^^^^^^^^^^^

error[E0432]: unresolved import `crate::reed_solomon`
  --> core/primitives/src/stateless_validation/state_witness.rs:8:12
   |
8  | use crate::reed_solomon::{ReedSolomonEncoderDeserialize, ReedSolomonEncoderSerialize};
   |            ^^^^^^^^^^^^ could not find `reed_solomon` in the crate root
   |
note: found an item that was configured out
  --> core/primitives/src/lib.rs:30:9
   |
30 | pub mod reed_solomon;
   |         ^^^^^^^^^^^^
note: the item is gated behind the `solomon` feature
  --> core/primitives/src/lib.rs:29:7
   |
29 | #[cfg(feature = "solomon")]
   |       ^^^^^^^^^^^^^^^^^^^

warning: unused import: `ShardId`
  --> core/primitives/src/block.rs:18:35
   |
18 | use near_primitives_core::types::{ShardId, ShardIndex};
   |                                   ^^^^^^^
   |
   = note: `#[warn(unused_imports)]` on by default
```

core/primitives/src/block.rs
@@ -15,7 +15,7 @@ use crate::sharding::{ChunkHashHeight, ShardChunkHeader, ShardChunkHeaderV1};
 use crate::types::{Balance, BlockHeight, EpochId, Gas};
 use crate::version::{ProtocolVersion, SHARD_CHUNK_HEADER_UPGRADE_VERSION};
 use borsh::{BorshDeserialize, BorshSerialize};
-use near_primitives_core::types::{ShardId, ShardIndex};
+use near_primitives_core::types::ShardIndex;
 use near_schema_checker_lib::ProtocolSchema;
 use near_time::Utc;
 use primitive_types::U256;
@@ -142,7 +142,7 @@ fn genesis_chunk(
     genesis_protocol_version: u32,
     genesis_height: u64,
     initial_gas_limit: u64,
-    shard_id: ShardId,
+    shard_id: crate::types::ShardId,
     state_root: CryptoHash,
     congestion_info: Option<crate::congestion_info::CongestionInfo>,
 ) -> crate::sharding::EncodedShardChunk {

core/primitives/src/block_header.rs
@@ -536,6 +536,9 @@ impl BlockHeaderV5 {
 /// Used in the BlockHeader::new_impl to specify the source of the block header signature.
 enum SignatureSource<'a> {
     /// Use the given signer to sign a new block header.
+    /// This variant is used only when some features are enabled. There is a warning
+    /// because it's unused in the default configuration, where the features are disabled.
+    #[allow(dead_code)]
     Signer(&'a ValidatorSigner),
     /// Use a previously-computed signature (for reconstructing an already-produced block header).
     Signature(Signature),

core/primitives/src/stateless_validation/contract_distribution.rs
@@ -7,6 +7,7 @@ use near_primitives_core::hash::CryptoHash;
 use near_primitives_core::types::AccountId;
 use near_schema_checker_lib::ProtocolSchema;
 
+#[cfg(feature = "solomon")]
 use crate::reed_solomon::{ReedSolomonEncoderDeserialize, ReedSolomonEncoderSerialize};
 use crate::{utils::compression::CompressedData, validator_signer::ValidatorSigner};
 
@@ -338,7 +339,9 @@ impl ChunkContractDeploys {
     }
 }
 
+#[cfg(feature = "solomon")]
 impl ReedSolomonEncoderSerialize for ChunkContractDeploys {}
+#[cfg(feature = "solomon")]
 impl ReedSolomonEncoderDeserialize for ChunkContractDeploys {}
 
 #[derive(Clone, Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, ProtocolSchema)]

core/primitives/src/stateless_validation/state_witness.rs
@@ -5,6 +5,7 @@ use super::{ChunkProductionKey, SignatureDifferentiator};
 use crate::bandwidth_scheduler::BandwidthRequests;
 use crate::challenge::PartialState;
 use crate::congestion_info::CongestionInfo;
+#[cfg(feature = "solomon")]
 use crate::reed_solomon::{ReedSolomonEncoderDeserialize, ReedSolomonEncoderSerialize};
 use crate::sharding::{ChunkHash, ReceiptProof, ShardChunkHeader, ShardChunkHeaderV3};
 use crate::transaction::SignedTransaction;
@@ -48,12 +49,14 @@ impl
 {
 }
 
+#[cfg(feature = "solomon")]
 impl ReedSolomonEncoderSerialize for EncodedChunkStateWitness {
     fn serialize_single_part(&self) -> std::io::Result<Vec<u8>> {
         Ok(self.as_slice().to_vec())
     }
 }
 
+#[cfg(feature = "solomon")]
 impl ReedSolomonEncoderDeserialize for EncodedChunkStateWitness {
     fn deserialize_single_part(data: &[u8]) -> std::io::Result<Self> {
         Ok(EncodedChunkStateWitness::from_boxed_slice(data.to_vec().into_boxed_slice()))