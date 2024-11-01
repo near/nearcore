35458f96ef208b4e571b83ca716c7e0608a300df
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2024-11-01 17:56:15 -0400

    stop adding after 2

--- chain/chain/src/state_sync.rs
+++ chain/chain/src/state_sync.rs
@@ -58,7 +58,8 @@ fn save_epoch_new_chunks<T: ChainStoreAccess>(
     let mut done = true;
     for (shard_index, num_new_chunks) in num_new_chunks.iter_mut().enumerate() {
         let new_chunk = header.chunk_mask()[shard_index];
-        if new_chunk {
+        // Only need to reach 2, so don't bother adding more than that
+        if new_chunk && *num_new_chunks < 2 {
             *num_new_chunks += 1;
         }
         if *num_new_chunks < 2 {
