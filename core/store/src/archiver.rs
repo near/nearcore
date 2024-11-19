de632c2ac98f6ffed0ad9adb0ef1a1c67639d685
Tayfun Elmas <tayfunelmas@gmail.com>
2024-11-19 08:56:05 -0800

    Change filename encoding

--- core/store/src/archiver.rs
+++ core/store/src/archiver.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::{io, sync::Arc};
 
 use near_primitives::block::Tip;
-use near_primitives::serialize::to_base64;
 use std::io::Read;
 use std::io::Write;
 use strum::IntoEnumIterator;
@@ -59,7 +58,7 @@ impl Archiver {
     }
 
     pub fn get_head(&self) -> io::Result<Option<Tip>> {
-        let cold_head = self.cold_store.get_ser::<Tip>(DBCol::BlockMisc, HEAD_KEY)?;
+        let cold_head = self.cold_store.get_ser::<Tip>(DBCol::BlockMisc, HEAD_KEY).unwrap();
         Ok(cold_head)
     }
 
@@ -68,14 +67,14 @@ impl Archiver {
         {
             let mut transaction = DBTransaction::new();
             transaction.set(DBCol::BlockMisc, HEAD_KEY.to_vec(), borsh::to_vec(&tip)?);
-            self.cold_db.write(transaction)?;
+            self.cold_db.write(transaction).unwrap();
         }
 
         // Write COLD_HEAD_KEY to the cold db.
         {
             let mut transaction = DBTransaction::new();
             transaction.set(DBCol::BlockMisc, COLD_HEAD_KEY.to_vec(), borsh::to_vec(&tip)?);
-            self.cold_db.write(transaction)?;
+            self.cold_db.write(transaction).unwrap();
         }
         Ok(())
     }
@@ -116,8 +115,9 @@ struct FilesystemArchiver {
 
 impl FilesystemArchiver {
     fn open(path: &std::path::Path) -> io::Result<Self> {
-        let col_to_dir = Self::setup_dirs(path)?;
-        let dir = rustix::fs::open(path, rustix::fs::OFlags::DIRECTORY, rustix::fs::Mode::empty())?;
+        let col_to_dir = Self::setup_dirs(path).unwrap();
+        let dir = rustix::fs::open(path, rustix::fs::OFlags::DIRECTORY, rustix::fs::Mode::empty())
+            .unwrap();
         tracing::debug!(
             target: "archiver",
             path = %path.display(),
@@ -127,13 +127,13 @@ impl FilesystemArchiver {
     }
 
     fn setup_dirs(base_path: &std::path::Path) -> io::Result<HashMap<DBCol, std::path::PathBuf>> {
-        std::fs::create_dir_all(base_path)?;
+        std::fs::create_dir_all(base_path).unwrap();
         let mut col_to_dir = HashMap::new();
         for col in DBCol::iter() {
             if col.is_cold() {
                 let path: std::path::PathBuf =
                     [base_path, Self::to_dirname(col)].into_iter().collect();
-                std::fs::create_dir(&path)?;
+                std::fs::create_dir(&path).unwrap();
                 col_to_dir.insert(col, path);
             }
         }
@@ -143,8 +143,8 @@ impl FilesystemArchiver {
     #[inline]
     fn get_path(&self, col: DBCol, key: &[u8]) -> std::path::PathBuf {
         let dirname = self.col_to_dir.get(&col);
-        let filename: std::path::PathBuf = to_base64(key).into();
-        [dirname.as_ref().unwrap(), &filename].into_iter().collect()
+        let filename = bs58::encode(key).with_alphabet(bs58::Alphabet::BITCOIN).into_string();
+        [dirname.as_ref().unwrap(), std::path::Path::new(&filename)].into_iter().collect()
     }
 
     fn to_dirname(col: DBCol) -> &'static std::path::Path {
@@ -153,44 +153,44 @@ impl FilesystemArchiver {
 
     fn write_file(&self, col: DBCol, key: &[u8], value: &[u8]) -> io::Result<()> {
         use rustix::fs::{Mode, OFlags};
-        let mut temp_file = tempfile::Builder::new().make_in("", |filename| {
-            let mode = Mode::RUSR | Mode::WUSR | Mode::RGRP | Mode::WGRP;
-            let flags = OFlags::CREATE | OFlags::TRUNC | OFlags::WRONLY;
-            Ok(std::fs::File::from(rustix::fs::openat(&self.base_dir, filename, flags, mode)?))
-        })?;
+        let mut temp_file = tempfile::Builder::new()
+            .make_in("", |filename| {
+                let mode = Mode::RUSR | Mode::WUSR | Mode::RGRP | Mode::WGRP;
+                let flags = OFlags::CREATE | OFlags::TRUNC | OFlags::WRONLY;
+                Ok(std::fs::File::from(rustix::fs::openat(&self.base_dir, filename, flags, mode)?))
+            })
+            .unwrap();
 
-        temp_file.write_all(value)?;
+        temp_file.write_all(value).unwrap();
 
-        let temp_filename = temp_file.into_temp_path();
-        let final_filename = self.get_path(col, key);
-        // This is atomic, so there wouldn't be instances where getters see an intermediate state.
-        rustix::fs::renameat(&self.base_dir, &*temp_filename, &self.base_dir, final_filename)?;
-        // Don't attempt deleting the temporary file now that it has been moved.
-        std::mem::forget(temp_filename);
+        let temp_path = temp_file.into_temp_path();
+        let file_path = self.get_path(col, key);
+        rustix::fs::renameat(&self.base_dir, &*temp_path, &self.base_dir, file_path).unwrap();
+        std::mem::forget(temp_path);
         Ok(())
     }
 
     fn read_file(&self, col: DBCol, key: &[u8]) -> io::Result<Option<Vec<u8>>> {
         use rustix::fs::{Mode, OFlags};
-        let filename = self.get_path(col, key);
         let mode = Mode::empty();
         let flags = OFlags::RDONLY;
-        let file = rustix::fs::openat(&self.base_dir, &filename, flags, mode);
+        let file_path = self.get_path(col, key);
+        let file = rustix::fs::openat(&self.base_dir, &file_path, flags, mode);
         let file = match file {
             Err(rustix::io::Errno::NOENT) => return Ok(None),
             Err(e) => return Err(e.into()),
             Ok(file) => file,
         };
-        let stat = rustix::fs::fstat(&file)?;
+        let stat = rustix::fs::fstat(&file).unwrap();
         let mut buffer: Vec<u8> = Vec::with_capacity(stat.st_size.try_into().unwrap());
         let mut file = std::fs::File::from(file);
-        file.read_to_end(&mut buffer)?;
+        file.read_to_end(&mut buffer).unwrap();
         Ok(Some(buffer))
     }
 
     fn delete_file(&self, col: DBCol, key: &[u8]) -> io::Result<()> {
-        let filename = self.get_path(col, key);
-        Ok(rustix::fs::unlinkat(&self.base_dir, &filename, rustix::fs::AtFlags::empty())?)
+        let file_path = self.get_path(col, key);
+        Ok(rustix::fs::unlinkat(&self.base_dir, &file_path, rustix::fs::AtFlags::empty())?)
     }
 }
 
@@ -208,7 +208,7 @@ impl ArchivalStorage for FilesystemArchiver {
                     self.write_file(col, &key, &value)
                 }
                 DBOp::UpdateRefcount { col, key, value } => {
-                    let existing = self.read_file(col, &key)?;
+                    let existing = self.read_file(col, &key).unwrap();
                     let operands = [value.as_slice()];
                     let merged =
                         refcount::refcount_merge(existing.as_ref().map(Vec::as_slice), operands);
@@ -225,7 +225,8 @@ impl ArchivalStorage for FilesystemArchiver {
                 DBOp::Delete { .. } | DBOp::DeleteAll { .. } | DBOp::DeleteRange { .. } => {
                     unreachable!("Delete operations unsupported")
                 }
-            }?;
+            }
+            .unwrap();
         }
         Ok(())
     }
