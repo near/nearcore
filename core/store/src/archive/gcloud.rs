8f9000cdce3d0bf803b35538c277f8f8d6cb716a
Tayfun Elmas <tayfunelmas@gmail.com>
2024-11-20 03:44:41 -0800

    Add async runner

--- core/store/src/archive/gcloud.rs
+++ core/store/src/archive/gcloud.rs
@@ -7,22 +7,28 @@ use crate::DBCol;
 use super::ArchivalStorage;
 
 pub(crate) struct GoogleCloudArchiver {
-    _gcs_client: Arc<cloud_storage::Client>,
-    _bucket: String,
+    gcs_client: Arc<cloud_storage::Client>,
+    bucket: String,
 }
 
 impl GoogleCloudArchiver {
     pub(crate) fn open(bucket: &str) -> Self {
-        Self {
-            _gcs_client: Arc::new(cloud_storage::Client::default()),
-            _bucket: bucket.to_string(),
-        }
+        Self { gcs_client: Arc::new(cloud_storage::Client::default()), bucket: bucket.to_string() }
     }
 }
 
 impl ArchivalStorage for GoogleCloudArchiver {
-    fn put(&self, _col: DBCol, _key: &[u8], _value: &[u8]) -> io::Result<()> {
-        unimplemented!()
+    fn put(&self, col: DBCol, key: &[u8], value: &[u8]) -> io::Result<()> {
+        let async_runtime = tokio::runtime::Builder::new_current_thread().build().unwrap();
+        let _ = async_runtime.block_on(async {
+            let location = "fake";
+            tracing::debug!(target: "archiver", key_len=key.len(), data_len = value.len(), ?location, "Writing to GCS");
+            self.gcs_client
+                    .object()
+                    .create(&self.bucket, value.to_vec(), location, "application/octet-stream")
+                    .await
+        }).map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+        Ok(())
     }
 
     fn get(&self, _col: DBCol, _key: &[u8]) -> io::Result<Option<Vec<u8>>> {
