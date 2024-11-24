600c2d4bc501731c44ae776b7c4421593a807ad0
Saketh Are <saketh.are@gmail.com>
2024-11-24 08:21:34 -0500

    adjust comment

--- chain/client/src/sync/state/shard.rs
+++ chain/client/src/sync/state/shard.rs
@@ -86,11 +86,12 @@ pub(super) async fn run_state_sync_for_shard(
     *status.lock().unwrap() = ShardSyncStatus::StateDownloadParts;
     let mut parts_to_download: Vec<u64> = (0..num_parts).collect();
     {
-        // Peer selection is designed such that requests for the same state parts are made to the
-        // same hosts, allowing the system to benefit from caching of parts on the host side. At
-        // the start of an epoch a number of nodes begin state sync at the same time. If we don't
-        // shuffle the order in which parts are requested, they will all request the same parts
-        // around the same time, producing spikes of traffic to particular hosts.
+        // Peer selection is designed such that different nodes downloading the same part will tend
+        // to send the requests to the same host. It allows the host to benefit from caching the part.
+        //
+        // At the start of an epoch, a number of nodes begin state sync at the same time. If we
+        // don't randomize the order in which the parts are requested, the nodes will request the
+        // parts in roughly the same order, producing spikes of traffic to particular hosts.
         let mut rng = thread_rng();
         parts_to_download.shuffle(&mut rng);
     }
