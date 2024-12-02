248e5c3ce0b1629eb2974ef1aefb242b7647f418
Tayfun Elmas <tayfunelmas@gmail.com>
2024-12-02 04:14:00 -0800

    utils

--- core/store/src/archive/mod.rs
+++ core/store/src/archive/mod.rs
@@ -1,13 +1,12 @@
 use std::collections::HashMap;
 use std::{io, sync::Arc};
 
-use crate::cold_storage::ColdMigrationStore;
 use borsh::BorshDeserialize;
 use filesystem::FilesystemStorage;
 use gcloud::GoogleCloudStorage;
-use near_primitives::block::{BlockHeader, Tip};
+use near_primitives::block::Tip;
 use near_primitives::types::BlockHeight;
-use strum::IntoEnumIterator;
+use utils::{get_cold_head, get_tip_at_height, map_cold_column_to_path, set_head_tx};
 
 use crate::db::refcount;
 use crate::db::DBOp;
@@ -21,6 +20,7 @@ use crate::{Store, COLD_HEAD_KEY};
 
 mod filesystem;
 mod gcloud;
+mod utils;
 
 /// Opener for the arhival storage, which results in an `ArchivalStore` instance.
 pub struct ArchivalStoreOpener {
@@ -111,12 +111,7 @@ impl ArchivalStore {
     pub fn update_head(&self, hot_store: &Store, height: &BlockHeight) -> io::Result<()> {
         tracing::debug!(target: "cold_store", "update HEAD of archival data to {}", height);
 
-        let height_key = height.to_le_bytes();
-        let block_hash_key =
-            hot_store.get_or_err_for_cold(DBCol::BlockHeight, &height_key)?.as_slice().to_vec();
-        let tip_header = &hot_store
-            .get_ser_or_err_for_cold::<BlockHeader>(DBCol::BlockHeader, &block_hash_key)?;
-        let tip = Tip::from_header(tip_header);
+        let tip = get_tip_at_height(hot_store, height)?;
 
         // Write head to the archival storage.
         self.set_head(&tip)?;
@@ -286,94 +281,4 @@ impl ArchivalStore {
 pub(crate) trait ExternalStorage: Sync + Send {
     fn put(&self, _path: &std::path::Path, _value: &[u8]) -> io::Result<()>;
     fn get(&self, _path: &std::path::Path) -> io::Result<Option<Vec<u8>>>;
-}
-
-/// Creates a transaction to write head to the archival storage.
-fn set_head_tx(tip: &Tip) -> io::Result<DBTransaction> {
-    let mut tx = DBTransaction::new();
-    // TODO: Write these to the same file for external storage.
-    tx.set(DBCol::BlockMisc, HEAD_KEY.to_vec(), borsh::to_vec(&tip)?);
-    tx.set(DBCol::BlockMisc, COLD_HEAD_KEY.to_vec(), borsh::to_vec(&tip)?);
-    Ok(tx)
-}
-
-/// Reads the head from the Cold DB.
-fn get_cold_head(cold_db: &Arc<ColdDB>) -> io::Result<Option<Tip>> {
-    cold_db
-        .get_raw_bytes(DBCol::BlockMisc, HEAD_KEY)?
-        .as_deref()
-        .map(Tip::try_from_slice)
-        .transpose()
-}
-
-/// Builds a map from column to relative path to store (key, value) pairs for the respective column.
-///
-/// If `container` is present, prepends it to the path for each column.
-fn map_cold_column_to_path(
-    container: Option<&std::path::Path>,
-) -> HashMap<DBCol, std::path::PathBuf> {
-    DBCol::iter()
-        .filter_map(|col| {
-            // BlockMisc is managed in the cold/archival storage but not marked as cold column.
-            if col.is_cold() || col == DBCol::BlockMisc {
-                let dirname = cold_column_dirname(col)
-                    .unwrap_or_else(|| panic!("Cold column {} has no entry for dirname", col));
-                let path = container
-                    .map_or_else(|| dirname.into(), |c| c.join(std::path::Path::new(dirname)));
-                Some((col, path))
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-/// Maps each column to a string to be used as a directory name in the storage.
-/// This mapping is partial and only supports cold DB columns and `DBCol::BlockMisc`.
-fn cold_column_dirname(col: DBCol) -> Option<&'static str> {
-    let dirname = match col {
-        DBCol::BlockMisc => "BlockMisc",
-        DBCol::Block => "Block",
-        DBCol::BlockExtra => "BlockExtra",
-        DBCol::BlockInfo => "BlockInfo",
-        DBCol::BlockPerHeight => "BlockPerHeight",
-        DBCol::ChunkExtra => "ChunkExtra",
-        DBCol::ChunkHashesByHeight => "ChunkHashesByHeight",
-        DBCol::Chunks => "Chunks",
-        DBCol::IncomingReceipts => "IncomingReceipts",
-        DBCol::NextBlockHashes => "NextBlockHashes",
-        DBCol::OutcomeIds => "OutcomeIds",
-        DBCol::OutgoingReceipts => "OutgoingReceipts",
-        DBCol::Receipts => "Receipts",
-        DBCol::State => "State",
-        DBCol::StateChanges => "StateChanges",
-        DBCol::StateChangesForSplitStates => "StateChangesForSplitStates",
-        DBCol::StateHeaders => "StateHeaders",
-        DBCol::TransactionResultForBlock => "TransactionResultForBlock",
-        DBCol::Transactions => "Transactions",
-        DBCol::StateShardUIdMapping => "StateShardUIdMapping",
-        _ => "",
-    };
-    dirname.is_empty().then_some(dirname)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::cold_column_dirname;
-    use crate::DBCol;
-    use strum::IntoEnumIterator;
-
-    /// Tets that all cold-DB columns and BlockMisc have mappings in `cold_column_dirname` function.
-    #[test]
-    fn test_cold_column_dirname() {
-        for col in DBCol::iter() {
-            if col.is_cold() || col == DBCol::BlockMisc {
-                assert!(
-                    cold_column_dirname(col).is_some(),
-                    "Cold column {} has no entry for dirname",
-                    col
-                );
-            }
-        }
-    }
 }
