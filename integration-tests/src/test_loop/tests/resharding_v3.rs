9e23c1cb9878e22715aa92adc7ab6619a1c54928
Trisfald <trisfald@gmail.com>
2024-12-03 15:41:13 +0100

    reduce block delay

--- integration-tests/src/test_loop/tests/resharding_v3.rs
+++ integration-tests/src/test_loop/tests/resharding_v3.rs
@@ -1005,5 +1005,5 @@ fn test_resharding_v3_load_mem_trie() {
 #[test]
 #[cfg_attr(not(feature = "test_features"), ignore)]
 fn test_resharding_v3_slower_post_processing_tasks() {
-    test_resharding_v3_base(TestReshardingParameters::new().delay_flat_state_resharding(5));
+    test_resharding_v3_base(TestReshardingParameters::new().delay_flat_state_resharding(3));
 }
