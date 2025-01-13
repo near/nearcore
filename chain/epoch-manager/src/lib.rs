6110d42e1ee898c969f7f6c526047c49f0fa1ca0
slava <s.v.savenko@gmail.com>
2025-01-13 16:12:05 +0100

    12488. Replace ShardUIdAndIndex by  ShardInfo

--- chain/epoch-manager/src/lib.rs
+++ chain/epoch-manager/src/lib.rs
@@ -41,7 +41,7 @@ use validator_stats::{
 };
 
 pub use crate::adapter::EpochManagerAdapter;
-pub use crate::adapter::ShardUIdAndIndex;
+pub use near_primitives::shard_layout::ShardInfo;
 pub use crate::proposals::proposals_to_epoch_info;
 pub use crate::reward_calculator::RewardCalculator;
 pub use crate::reward_calculator::NUM_SECONDS_IN_A_YEAR;
