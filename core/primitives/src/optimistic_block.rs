4a65ac439ac716b73095505a2c91e9419fa8ec6f
Razvan Barbascu <razvan@nearone.org>
2025-01-17 14:10:18 +0000

    Introduce Inner field, change hashing logic

--- core/primitives/src/optimistic_block.rs
+++ core/primitives/src/optimistic_block.rs
@@ -3,35 +3,44 @@ use near_crypto::Signature;
 
 use crate::block::BlockHeader;
 use crate::hash::{hash, CryptoHash};
+use crate::merkle::combine_hash;
 use crate::types::BlockHeight;
 
 #[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq, PartialEq)]
-pub struct OptimisticBlock {
+pub struct OptimisticBlockInner {
     pub block_height: BlockHeight,
-    pub prev_block_hash: CryptoHash,
     pub block_timestamp: u64,
-
     // Data to confirm the correctness of randomness beacon output
     pub random_value: CryptoHash,
     pub vrf_value: near_crypto::vrf::Value,
     pub vrf_proof: near_crypto::vrf::Proof,
+}
 
+/// An optimistic block is independent of specific chunks and can be generated
+/// and distributed immediately after the previous block is processed.
+#[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq, PartialEq)]
+#[borsh(init=init)]
+pub struct OptimisticBlock {
+    pub prev_block_hash: CryptoHash,
+    pub inner: OptimisticBlockInner,
     /// Signature of the block producer.
     pub signature: Signature,
+    #[borsh(skip)]
     pub hash: CryptoHash,
 }
 
 impl OptimisticBlock {
     #[cfg(feature = "clock")]
-    pub fn new(
-        prev: &BlockHeader,
+    pub fn produce(
+        prev_block_header: &BlockHeader,
         height: BlockHeight,
         signer: &crate::validator_signer::ValidatorSigner,
         clock: near_time::Clock,
         sandbox_delta_time: Option<near_time::Duration>,
     ) -> Self {
-        let prev_block_hash = *prev.hash();
-        let (vrf_value, vrf_proof) = signer.compute_vrf_with_proof(prev.random_value().as_ref());
+        let prev_block_hash = *prev_block_header.hash();
+        let (vrf_value, vrf_proof) =
+            signer.compute_vrf_with_proof(prev_block_header.random_value().as_ref());
         let random_value = hash(vrf_value.0.as_ref());
 
         let now = clock.now_utc().unix_timestamp_nanos() as u64;
@@ -39,38 +48,30 @@ impl OptimisticBlock {
         let now = now + sandbox_delta_time.unwrap().whole_nanoseconds() as u64;
         #[cfg(not(feature = "sandbox"))]
         debug_assert!(sandbox_delta_time.is_none());
-        let time = if now <= prev.raw_timestamp() { prev.raw_timestamp() + 1 } else { now };
+        let time = if now <= prev_block_header.raw_timestamp() {
+            prev_block_header.raw_timestamp() + 1
+        } else {
+            now
+        };
 
-        Self {
+        let inner = OptimisticBlockInner {
             block_height: height,
-            prev_block_hash,
             block_timestamp: time,
             random_value,
             vrf_value,
             vrf_proof,
-            signature: Default::default(),
-            hash: Default::default(),
-        }
-    }
+        };
 
-    pub fn produce_optimistic(
-        prev: &BlockHeader,
-        height: BlockHeight,
-        signer: &crate::validator_signer::ValidatorSigner,
-        clock: near_time::Clock,
-        sandbox_delta_time: Option<near_time::Duration>,
-    ) -> Self {
-        let mut optimistic_block = Self::new(prev, height, signer, clock, sandbox_delta_time);
-        optimistic_block.hash_and_sign(signer);
-        optimistic_block
-    }
-
-    pub fn hash_and_sign(&mut self, signer: &crate::validator_signer::ValidatorSigner) {
-        self.signature = Default::default();
-        self.hash = Default::default();
-        let hash = hash(&borsh::to_vec(self).expect("Failed to serialize"));
+        let inner_hash = hash(&borsh::to_vec(&inner).expect("Failed to serialize"));
+        let hash = combine_hash(&inner_hash, &prev_block_hash);
         let signature = signer.sign_bytes(hash.as_ref());
-        self.signature = signature;
-        self.hash = hash;
+
+        Self { prev_block_hash, inner, signature, hash }
+    }
+
+    /// Recompute the hash after deserialization.
+    pub fn init(&mut self) {
+        let inner_hash = hash(&borsh::to_vec(&self.inner).expect("Failed to serialize"));
+        self.hash = combine_hash(&inner_hash, &self.prev_block_hash);
     }
 }
