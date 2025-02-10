5929da58663edb160c678c68920f7d4befec766e
Anton Puhach <anton@near.org>
2025-02-10 18:21:44 +0100

    feat: add costs for deploying global contracts  (#12882)
    This PR adds gas and storage costs associated with deploying global
    contract:
    * `action_deploy_global_contract` is exactly the same as
    `action_deploy_contract`
    * `action_deploy_global_contract_per_byte`:
      * send costs are the same as `action_deploy_contract_per_byte`
    * execution costs should cover distribution of the contract to all
    shards:
    * this is pretty expensive for the network, so want want to change
    significant amount of gas for that
    * we still want to be able to fit max allowed contracts size into single
    chunk space: `max_gas_burnt = 300_000_000_000_000`, `max_contract_size =
    4_194_304`, so it should be at most `max_gas_burnt / max_contract_size =
    71_525_573`
    * we need to allow for some margin for other costs, so we can round it
    down to `70_000_000`
    * `global_contract_storage_amount_per_byte` corresponds to the amount of
    yN burned per byte of deployed contract code
    * value corresponds to 10x storage staking cost
    `storage_amount_per_byte`.
    
    `ProtocolFeature::GlobalContracts` is moved to nightly.

--- core/parameters/src/cost.rs
+++ core/parameters/src/cost.rs
@@ -307,6 +307,8 @@ pub enum ActionCosts {
     new_data_receipt_base = 13,
     new_data_receipt_byte = 14,
     delegate = 15,
+    deploy_global_contract_base = 16,
+    deploy_global_contract_byte = 17,
 }
 
 impl ExtCosts {
@@ -438,6 +440,8 @@ pub struct StorageUsageConfig {
     pub num_bytes_account: u64,
     /// Additional number of bytes for a k/v record
     pub num_extra_bytes_record: u64,
+    /// Amount of yN burned per byte of deployed Global Contract code.
+    pub global_contract_storage_amount_per_byte: Balance,
 }
 
 impl RuntimeFeesConfig {
@@ -532,6 +536,16 @@ impl RuntimeFeesConfig {
                     send_not_sir: 200_000_000_000,
                     execution: 200_000_000_000,
                 },
+                ActionCosts::deploy_global_contract_base => Fee {
+                    send_sir: 184_765_750_000,
+                    send_not_sir: 184_765_750_000,
+                    execution: 184_765_750_000,
+                },
+                ActionCosts::deploy_global_contract_byte => Fee {
+                    send_sir: 6_812_999,
+                    send_not_sir: 6_812_999,
+                    execution: 70_000_000,
+                },
             },
         }
     }
@@ -563,11 +577,17 @@ impl StorageUsageConfig {
             num_bytes_account: 100,
             num_extra_bytes_record: 40,
             storage_amount_per_byte: 909 * 100_000_000_000_000_000,
+            global_contract_storage_amount_per_byte: 100_000_000_000_000_000_000,
         }
     }
 
     pub(crate) fn free() -> StorageUsageConfig {
-        Self { num_bytes_account: 0, num_extra_bytes_record: 0, storage_amount_per_byte: 0 }
+        Self {
+            num_bytes_account: 0,
+            num_extra_bytes_record: 0,
+            storage_amount_per_byte: 0,
+            global_contract_storage_amount_per_byte: 0,
+        }
     }
 }
 
