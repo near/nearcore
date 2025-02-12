3c20bb668cf1d22fe47fad18232e05789d4893dd
Razvan Barbascu <razvan@nearone.org>
2025-02-12 18:44:40 +0000

    fix test

--- integration-tests/src/test_loop/tests/optimistic_block.rs
+++ integration-tests/src/test_loop/tests/optimistic_block.rs
@@ -58,14 +58,13 @@ fn test_optimistic_block() {
         assert!(chain.optimistic_block_chunks.num_chunks() <= 3 * num_shards);
         // There should be at least one optimistic block result in the cache.
         assert!(chain.apply_chunk_results_cache.len() > 0);
-        // Optimistic block result should be used at least once.
-        assert!(
-            chain.apply_chunk_results_cache.hits() == chain.head().map_or(0, |t| t.height as usize)
-        );
+        // Optimistic block result should be used at every height.
+        // We do not process the genesis block nor the last block.
+        let expected_hits = chain.head().map_or(0, |t| t.height - 2);
+        assert!(chain.apply_chunk_results_cache.hits() >= (expected_hits as usize));
 
-        // Because there is no optimistic block distribution yet, there should
-        // be at least one miss for each shard.
-        assert!(chain.apply_chunk_results_cache.misses() == 0);
+        // Current logic queries cache multiple times. It is ok to see misses.
+        assert!(chain.apply_chunk_results_cache.misses() > 0);
     }
 
     env.shutdown_and_drain_remaining_events(Duration::seconds(20));
