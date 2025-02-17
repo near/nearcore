92bf9b5399261c7300367cd5341b0b64f3a36b80
Stefan Neamtu <stefan.neamtu@gmail.com>
2025-02-17 17:49:55 +0100

    add len fn

--- runtime/runtime/src/config.rs
+++ runtime/runtime/src/config.rs
@@ -1,7 +1,7 @@
 //! Settings of the parameters of the runtime.
 
 use near_primitives::account::AccessKeyPermission;
-use near_primitives::action::{DeployGlobalContractAction, GlobalContractIdentifier};
+use near_primitives::action::DeployGlobalContractAction;
 use near_primitives::errors::IntegerOverflowError;
 use near_primitives::version::FIXED_MINIMUM_NEW_RECEIPT_GAS_VERSION;
 use near_primitives_core::types::ProtocolVersion;
@@ -147,10 +147,7 @@ pub fn total_send_fees(
                         * num_bytes
             }
             UseGlobalContract(action) => {
-                let num_bytes = match &action.contract_identifier {
-                    GlobalContractIdentifier::CodeHash(_) => 32,
-                    GlobalContractIdentifier::AccountId(id) => id.len(),
-                } as u64;
+                let num_bytes = action.contract_identifier.len() as u64;
                 fees.fee(ActionCosts::action_use_global_contract_base).send_fee(sender_is_receiver)
                     + fees
                         .fee(ActionCosts::action_use_global_contract_byte)
@@ -244,10 +241,7 @@ pub fn exec_fee(config: &RuntimeConfig, action: &Action, receiver_id: &AccountId
                 + fees.fee(ActionCosts::deploy_global_contract_byte).exec_fee() * num_bytes
         }
         UseGlobalContract(action) => {
-            let num_bytes = match &action.contract_identifier {
-                GlobalContractIdentifier::CodeHash(_) => 32,
-                GlobalContractIdentifier::AccountId(id) => id.len(),
-            } as u64;
+            let num_bytes = action.contract_identifier.len() as u64;
             fees.fee(ActionCosts::action_use_global_contract_base).exec_fee()
                 + fees.fee(ActionCosts::action_use_global_contract_byte).exec_fee() * num_bytes
         }
