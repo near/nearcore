0e023da3156d0f4f03459f3d519d7df9933614ef
Stefan Neamtu <stefan.neamtu@gmail.com>
2025-02-26 14:50:03 +0100

    .

--- pytest/tests/contracts/deploy_call_global_smart_contract.py
+++ pytest/tests/contracts/deploy_call_global_smart_contract.py
@@ -38,7 +38,7 @@ def test_deploy_global_contract():
     time.sleep(3)
 
     last_block_hash = nodes[0].get_latest_block().hash_bytes
-    tx = sign_function_call_tx(nodes[0].signer_key,
+    tx = sign_function_call_tx(nodes[1].signer_key,
                                nodes[1].signer_key.account_id, 'log_something',
                                [], 150 * GGAS, 1, 30, last_block_hash)
     res = nodes[1].send_tx_and_wait(tx, 20)
