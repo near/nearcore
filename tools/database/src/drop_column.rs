7794b3cada48daeff089c1d88f4ff68ffd64da17
Adam Chudaś <adam@chudas.net>
2025-03-06 10:33:06 +0000

    fmt + spellcheck

--- tools/database/src/drop_column.rs
+++ tools/database/src/drop_column.rs
@@ -9,17 +9,22 @@ pub(crate) struct DropColumnCommand {
     column: String,
 }
 
-
 impl DropColumnCommand {
     pub(crate) fn run(&self, home: &PathBuf) -> anyhow::Result<()> {
         let mut db = open_rocksdb(home, near_store::Mode::ReadWrite)?;
         let column = match resolve_column(&self.column) {
             Ok(col) => {
-                println!("Column {} is recognized by the binary. Note that it will be automatically recreated each time neard is run.", self.column);
+                println!(
+                    "Column {} is recognized by the binary. Note that it will be automatically recreated each time neard is run.",
+                    self.column
+                );
                 Some(col)
             }
             Err(_) => {
-                println!("Column {} is not recognized by the binary. Will attempt to remove it.", self.column);
+                println!(
+                    "Column {} is not recognized by the binary. Will attempt to remove it.",
+                    self.column
+                );
                 None
             }
         };
