d52e2cc3bc9685fdd27b764cd51192fbc42c4b17
Adam Chudaś <adam@chudas.net>
2025-03-12 08:32:10 +0000

    confirmation

--- tools/database/src/drop_column.rs
+++ tools/database/src/drop_column.rs
@@ -1,5 +1,6 @@
 use crate::utils::{open_rocksdb, resolve_column};
 use clap::Parser;
+use dialoguer::Confirm;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -11,6 +12,21 @@ pub(crate) struct DropColumnCommand {
 
 impl DropColumnCommand {
     pub(crate) fn run(&self, home: &PathBuf) -> anyhow::Result<()> {
+        if !Confirm::new()
+            .with_prompt(format!(
+                "WARNING: You are about to drop the column '{}'.\n\
+                That would break the database, unless you know what you are doing.\n\
+                Also, the column may be automatically restored (empty) the next time you run neard.\n\
+                Are you sure?",
+                self.column
+            ))
+            .default(false)
+            .interact()?
+        {
+            println!("Operation canceled.");
+            return Ok(());
+        }
+
         let mut db = open_rocksdb(home, near_store::Mode::ReadWrite)?;
         let column = resolve_column(&self.column)?;
         db.drop_column(column)?;
