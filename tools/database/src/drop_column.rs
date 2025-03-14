db14e7f422837c18a625296dfca67302a0256fc9
Adam Chudaś <adam@chudas.net>
2025-03-14 20:15:09 +0000

    use confirmation

--- tools/database/src/drop_column.rs
+++ tools/database/src/drop_column.rs
@@ -1,4 +1,4 @@
-use crate::utils::resolve_column;
+use crate::utils::{get_user_confirmation, resolve_column};
 use clap::Parser;
 use near_chain_configs::GenesisValidationMode;
 use near_store::DBCol;
@@ -14,10 +14,23 @@ pub(crate) struct DropColumnCommand {
 }
 
 impl DropColumnCommand {
-    pub(crate) fn run(&self, home_dir: &PathBuf, genesis_validation: GenesisValidationMode) -> anyhow::Result<()> {
-        let unwanted_columns: Vec<DBCol> = self.column.iter()
-            .map(|col| resolve_column(col))
-            .collect::<Result<Vec<_>, _>>()?;
+    pub(crate) fn run(
+        &self,
+        home_dir: &PathBuf,
+        genesis_validation: GenesisValidationMode,
+    ) -> anyhow::Result<()> {
+        let unwanted_columns: Vec<DBCol> =
+            self.column.iter().map(|col| resolve_column(col)).collect::<Result<Vec<_>, _>>()?;
+
+        if !get_user_confirmation(&format!(
+            "WARNING: You are about to drop columns '{:?}'.\n\
+            That would break the database, unless you know what you are doing.\n\
+            Also, columns may be automatically restored (empty) the next time you run neard.",
+            self.column
+        )) {
+            println!("Operation canceled.");
+            return Ok(());
+        }
 
         println!("Dropping columns: {unwanted_columns:?}");
         let near_config = load_config(home_dir, genesis_validation)
@@ -30,6 +43,7 @@ impl DropColumnCommand {
             &unwanted_columns,
             false,
         )?;
+        println!("Dropped columns");
         Ok(())
     }
 }
