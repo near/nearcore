08ff40350c16422b54092cc06b22b91112250e00
Anton Puhach <anton@near.org>
2025-03-17 09:52:31 +0100

    fix: reimplement global contract distribution (#13120)
    Current implementation of global contracts distribution doesn't work
    with bandwidth scheduler, see [this
    thread](https://near.zulipchat.com/#narrow/channel/308695-nearone.2Fprivate/topic/Global.20contracts.20and.20bandwidth.20scheduler)
    for more details.
    
    This PR contains alternative implementation: instead of generating one
    `GlobalContractDistribution` for all shards we create receipt targeting
    single shard (`target_shard`) as well as containing list of all shards
    where this global contract was already distributed
    (`already_delivered_shards`).
    When processing such receipt we take the next shard in the current shard
    layout for which this contract was not yet distributed and create next
    global contract distribution receipt for that shard.
    
    Such implementation has the following properties:
    - There is exactly one receipt in flight for single global contract.
    This reduces cross shard traffic spike as a result of global contract
    deployment. Bandwidth scheduler would have taken care of that anyway, so
    probably not a huge win.
    - It works well with resharding: in case of a shard layout change, the
    receipt will be sent to the first child shard, which will then forward
    it to the other shard. Test for that will be added in a separate PR.
    
    Notes:
    - `Receipt::receiver_shard` should be used istead of
    `shard_layout.account_id_to_shard_id(receipt.receiver_id())` to
    determine target shard
    - all global contracts related logic in runtime is moved into a
    dedicated module, `action_use_global_contract` is not changed
    - `GlobalContractData` is renamed to `GlobalContractDistributionReceipt`
    and made into versioned enum to make it easier to make updates in the
    future
    
    Original implementation PR: #12793
    Part of #12715

--- chain/chunks/src/logic.rs
+++ chain/chunks/src/logic.rs
@@ -92,12 +92,12 @@ pub fn make_outgoing_receipts_proofs(
     let shard_layout =
         epoch_manager.get_shard_layout_from_prev_block(chunk_header.prev_block_hash())?;
 
-    let hashes = Chain::build_receipts_hashes(&outgoing_receipts, &shard_layout);
+    let hashes = Chain::build_receipts_hashes(&outgoing_receipts, &shard_layout)?;
     let (root, proofs) = merklize(&hashes);
     assert_eq!(chunk_header.prev_outgoing_receipts_root(), root);
 
     let mut receipts_by_shard =
-        Chain::group_receipts_by_shard(outgoing_receipts.to_vec(), &shard_layout);
+        Chain::group_receipts_by_shard(outgoing_receipts.to_vec(), &shard_layout)?;
     let mut result = vec![];
     for (proof_shard_index, proof) in proofs.into_iter().enumerate() {
         let proof_shard_id = shard_layout.get_shard_id(proof_shard_index)?;
