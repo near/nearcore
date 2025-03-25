b7e78c947b27c0a2947d5d7a1d43de7b01aab7bd
Waclaw Banasik <wacban@users.noreply.github.com>
2025-03-25 17:02:18 +0000

    cleanup(congestion) - removed local congestion control (#13202)
    Local congestion control has been replaced by global congestion control.
    The global cc is deployed so we can clean up the old local congestion
    control.
    
    Now the fun question is whether this is a protocol upgrade. This PR
    removes a state read so it should be considered a protocol upgrade. In
    practice it should be fine because prepare transactions is not called by
    the chunk validators since RelaxedChunkValidation was introduced. I
    would appreciate reviewers double checking that assumption.
    
    ---------
    
    Co-authored-by: Vladimir Miloserdov <vladimir.miloserdov@nearone.org>
    Co-authored-by: Jan Malinowski <149345204+jancionear@users.noreply.github.com>
    Co-authored-by: Akhilesh Singhania <akhi3030@gmail.com>

--- chain/chain/src/runtime/tests.rs
+++ chain/chain/src/runtime/tests.rs
@@ -1664,43 +1664,42 @@ fn prepare_transactions(
 /// Check that transactions validation fails if provided empty storage proof.
 #[test]
 fn test_prepare_transactions_empty_storage_proof() {
+    // First prepare transactions using proper db and check all transactions are
+    // included in the result.
+    let db_storage_source = StorageDataSource::Db;
+    let (transaction_count, prepared_transactions) =
+        test_prepare_transactions_helper(db_storage_source);
+    assert_ne!(transaction_count, 0);
+    assert_eq!(prepared_transactions.transactions.len(), transaction_count);
+
+    // Second prepare transactions using empty storage proof and check that no
+    // transactions are included in the result.
+    let empty_storage_source =
+        StorageDataSource::Recorded(PartialStorage { nodes: PartialState::default() });
+    let (transaction_count, prepared_transactions) =
+        test_prepare_transactions_helper(empty_storage_source);
+    assert_ne!(transaction_count, 0);
+    assert_eq!(prepared_transactions.transactions.len(), 0);
+}
+
+// Helper function to test prepare_transactions with different storage sources.
+fn test_prepare_transactions_helper(
+    storage_source: StorageDataSource,
+) -> (usize, PreparedTransactions) {
     let (env, chain, mut transaction_pool) = get_test_env_with_chain_and_pool();
     let transactions_count = transaction_pool.len();
 
     let storage_config = RuntimeStorageConfig {
         state_root: env.state_roots[0],
         use_flat_storage: true,
-        source: StorageDataSource::Db,
+        source: storage_source,
         state_patch: Default::default(),
     };
 
-    let proposed_transactions = prepare_transactions(
-        &env,
-        &chain,
-        &mut PoolIteratorWrapper::new(&mut transaction_pool),
-        storage_config,
-    )
-    .unwrap();
-
-    assert_eq!(proposed_transactions.transactions.len(), transactions_count);
-
-    let validator_storage_config = RuntimeStorageConfig {
-        state_root: env.state_roots[0],
-        use_flat_storage: true,
-        source: StorageDataSource::Recorded(PartialStorage {
-            nodes: PartialState::default(), // We use empty storage proof here.
-        }),
-        state_patch: Default::default(),
-    };
-
-    let validation_result = prepare_transactions(
-        &env,
-        &chain,
-        &mut PoolIteratorWrapper::new(&mut transaction_pool),
-        validator_storage_config,
-    );
-
-    assert!(validation_result.is_err());
+    let mut transaction_groups = PoolIteratorWrapper::new(&mut transaction_pool);
+    let prepared_transactions =
+        prepare_transactions(&env, &chain, &mut transaction_groups, storage_config).unwrap();
+    (transactions_count, prepared_transactions)
 }
 
 #[test]
