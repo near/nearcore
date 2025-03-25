bec509ff5d1411d7a44f678090dba156c254b1e4
Jan Malinowski <149345204+jancionear@users.noreply.github.com>
2025-03-25 14:39:44 +0000

    fix(indexer): Don't create ValidatedTransaction in indexer (#13201)
    This code caused the indexer to crash on forknet:
    ```rust
    let signer = InMemorySigner::test_signer(&indexer_tx.transaction.signer_id);
    let signed_tx = tx.sign(&signer);
    let validated_tx = ValidatedTransaction::new(runtime_config, signed_tx).unwrap();
    ```
    
    The transaction is signed using a dummy signer, but then in
    `ValidatedTransaction::new` the signature is verified against the real
    public key from the transaction, which fails and the `unwrap()` panics,
    which crashes the indexer.
    
    To fix the problem I made `tx_cost` take a normal `Transaction` and pass
    the `tx` there without having to create a `ValidatedTransaction`.

--- runtime/runtime/src/config.rs
+++ runtime/runtime/src/config.rs
@@ -11,7 +11,7 @@ use num_traits::pow::Pow;
 // Just re-exporting RuntimeConfig for backwards compatibility.
 use near_parameters::{ActionCosts, RuntimeConfig, transfer_exec_fee, transfer_send_fee};
 pub use near_primitives::num_rational::Rational32;
-use near_primitives::transaction::{Action, DeployContractAction, ValidatedTransaction};
+use near_primitives::transaction::{Action, DeployContractAction, Transaction};
 use near_primitives::types::{AccountId, Balance, Compute, Gas};
 
 /// Describes the cost of converting this transaction into a receipt.
@@ -249,11 +249,10 @@ pub fn exec_fee(config: &RuntimeConfig, action: &Action, receiver_id: &AccountId
 /// Returns transaction costs for a given transaction.
 pub fn tx_cost(
     config: &RuntimeConfig,
-    validated_tx: &ValidatedTransaction,
+    tx: &Transaction,
     gas_price: Balance,
     protocol_version: ProtocolVersion,
 ) -> Result<TransactionCost, IntegerOverflowError> {
-    let tx = validated_tx.to_tx();
     let sender_is_receiver = tx.receiver_id() == tx.signer_id();
     let fees = &config.fees;
     let mut gas_burnt: Gas = fees.fee(ActionCosts::new_action_receipt).send_fee(sender_is_receiver);
