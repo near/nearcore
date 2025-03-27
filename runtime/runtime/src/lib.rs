6179376b3cf3292a387d9a66ca54547a00839155
Simonas Kazlauskas <git@kazlauskas.me>
2025-03-27 17:46:22 +0200

    refactor: rename commit_charging_for_tx to better reflect its behaviour (#13217)
    The function commits nothing.
    
    Stacked on top of #13216
    
    ---------
    
    Co-authored-by: Vladimir Miloserdov <milosvova@gmail.com>

--- runtime/runtime/src/lib.rs
+++ runtime/runtime/src/lib.rs
@@ -10,7 +10,7 @@ use crate::prefetch::TriePrefetcher;
 pub use crate::types::SignedValidPeriodTransactions;
 use crate::verifier::{StorageStakingError, check_storage_stake, validate_receipt};
 pub use crate::verifier::{
-    ZERO_BALANCE_ACCOUNT_STORAGE_LIMIT, commit_charging_for_tx, validate_transaction,
+    ZERO_BALANCE_ACCOUNT_STORAGE_LIMIT, set_tx_state_changes, validate_transaction,
     verify_and_charge_tx_ephemeral,
 };
 use bandwidth_scheduler::{BandwidthSchedulerOutput, run_bandwidth_scheduler};
@@ -380,7 +380,7 @@ impl Runtime {
         };
 
         metrics::TRANSACTION_PROCESSED_SUCCESSFULLY_TOTAL.inc();
-        commit_charging_for_tx(
+        set_tx_state_changes(
             state_update,
             validated_tx,
             &verification_result.signer,
@@ -407,9 +407,11 @@ impl Runtime {
                 actions: validated_tx.actions().to_vec(),
             }),
         });
-        stats.balance.tx_burnt_amount =
-            safe_add_balance(stats.balance.tx_burnt_amount, verification_result.burnt_amount)
-                .map_err(|_| InvalidTxError::CostOverflow)?;
+
+        match safe_add_balance(stats.balance.tx_burnt_amount, verification_result.burnt_amount) {
+            Ok(new) => stats.balance.tx_burnt_amount = new,
+            Err(_) => return Err(InvalidTxError::CostOverflow),
+        }
         let gas_burnt = verification_result.gas_burnt;
         let compute_usage = verification_result.gas_burnt;
         let outcome = ExecutionOutcomeWithId {
