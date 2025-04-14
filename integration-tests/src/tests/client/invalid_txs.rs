e4de120c35876e8596dd2daccc409ba743a84358
akhi <akhi3030@gmail.com>
2025-04-14 18:35:24 +0200

    rename field in producechunkresult

--- integration-tests/src/tests/client/invalid_txs.rs
+++ integration-tests/src/tests/client/invalid_txs.rs
@@ -90,12 +90,12 @@ fn test_invalid_transactions_no_panic() {
                 assert!(matches!(res, ProcessTxResponse::ValidTx))
             }
 
-            let (ProduceChunkResult { chunk, encoded_chunk_parts_paths, receipts }, _) =
+            let (ProduceChunkResult { encoded_chunk, encoded_chunk_parts_paths, receipts }, _) =
                 create_chunk(client, transactions);
 
             let shard_chunk = client
                 .persist_and_distribute_encoded_chunk(
-                    chunk,
+                    encoded_chunk,
                     encoded_chunk_parts_paths,
                     receipts,
                     client.validator_signer.get().unwrap().validator_id().clone(),
@@ -214,12 +214,12 @@ fn test_invalid_transactions_dont_invalidate_chunk() {
     let chunk_producer = env.get_chunk_producer_at_offset(&tip, 1, ShardId::new(0));
     let block_producer = env.get_block_producer_at_offset(&tip, 1);
     let client = env.client(&chunk_producer);
-    let (ProduceChunkResult { chunk, encoded_chunk_parts_paths, receipts }, _) =
+    let (ProduceChunkResult { encoded_chunk, encoded_chunk_parts_paths, receipts }, _) =
         create_chunk(client, chunk_transactions);
 
     let shard_chunk = client
         .persist_and_distribute_encoded_chunk(
-            chunk,
+            encoded_chunk,
             encoded_chunk_parts_paths,
             receipts,
             client.validator_signer.get().unwrap().validator_id().clone(),
