b93245a9d52f8b768b374c8b8d4e4bb240695718
Jan Malinowski <jan.ciolek@nearone.org>
2025-04-14 11:46:40 +0000

    Add span in rayon async computation spawner

--- chain/chain/src/rayon_spawner.rs
+++ chain/chain/src/rayon_spawner.rs
@@ -3,8 +3,17 @@ use near_async::futures::AsyncComputationSpawner;
 pub struct RayonAsyncComputationSpawner;
 
 impl AsyncComputationSpawner for RayonAsyncComputationSpawner {
-    fn spawn_boxed(&self, _name: &str, f: Box<dyn FnOnce() + Send>) {
+    fn spawn_boxed(&self, task_name: &str, f: Box<dyn FnOnce() + Send>) {
         let dispatcher = tracing::dispatcher::get_default(|it| it.clone());
-        rayon::spawn(move || tracing::dispatcher::with_default(&dispatcher, f))
+        let tname = task_name.to_string();
+        rayon::spawn(move || {
+            let _span = tracing::debug_span!(
+                target: "client",
+                "rayon_async_computation",
+                task_name = tname,
+            )
+            .entered();
+            tracing::dispatcher::with_default(&dispatcher, f)
+        })
     }
 }
