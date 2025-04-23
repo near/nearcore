94d41ff3950ec78580cf9b8ec54fb554f134ff82
Darioush Jalali <darioush@Darioush-N1-mbp.local>
2025-04-23 13:16:23 -0700

    testonly/stream.rs: remove loop in read

--- chain/network/src/testonly/stream.rs
+++ chain/network/src/testonly/stream.rs
@@ -16,17 +16,16 @@ impl Stream {
     }
 
     pub async fn read(&mut self) -> Result<PeerMessage, std::io::Error> {
-        loop {
-            // TODO: simplify / remove this loop
-            let n = self.stream.stream.read_u32_le().await? as usize;
-            let mut buf = BytesMut::new();
-            buf.resize(n, 0);
-            self.stream.stream.read_exact(&mut buf[..]).await?;
-            if let Ok(msg) = PeerMessage::deserialize(&buf[..]) {
-                return Ok(msg);
-            }
-            panic!("unknown encoding");
-        }
+        let n = self.stream.stream.read_u32_le().await? as usize;
+        let mut buf = BytesMut::new();
+        buf.resize(n, 0);
+        self.stream.stream.read_exact(&mut buf[..]).await?;
+        PeerMessage::deserialize(&buf[..]).map_err(|e| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Failed to deserialize: {}", e),
+            )
+        })
     }
 
     pub async fn write(&mut self, msg: &PeerMessage) {
