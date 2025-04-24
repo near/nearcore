732f46e4706cb2670056b6b8fa5f55a7328416ea
Aleksandr Logunov <alex.logunov@near.org>
2025-04-24 14:43:35 +0400

    chore: run sharded benchmark using mirror (#13384)
    ### Goals
    
    * Use mirror/forknet tooling to run sharded benchmark - for example,
    control runs using start-nodes and stop-nodes.
    * Put creation of 1M accounts to genesis - their creation in synth-bm
    after cluster start is a bottleneck.
    
    ### Plan
    
    `set_validators` from empty state:
    
    1. First, create empty genesis from the list of validators.
    2. Then, setup the "chain" view on the genesis state, allowing writes to
    RocksDB.
    3. Write 1M accounts to RocksDB and put resulting state roots to
    genesis.json. Also, write these accounts to single file per shard to be
    used by tx-generator.
    
    There are also multiple refactorings done to make it possible.
    I also suggest a slight change in tx-generator - allow storing accounts
    in single json file. Otherwise, **removal** of 1M files on reset is so
    slow that it negates performance gains. @ssavenko-near
    
    Unfortunately this is not very reviewable because there are a lot of
    events needed to setup forknet. So I made an overview for @Trisfald in a
    call. I hope it becomes better in the future, especially if we put some
    scenarios to testloop (e.g. run chain for a while, then fork it and
    check that the fork is runnable)
     
    ### Testing
    
    * I've run sharded benchmark, getting 21K TPS (not fully optimised)
    * I've run forknet based on mainnet dump to see that it still works.

--- integration-tests/src/tests/client/flat_storage.rs
+++ integration-tests/src/tests/client/flat_storage.rs
@@ -32,15 +32,13 @@ fn setup_env(genesis: &Genesis, store: Store) -> TestEnv {
 #[test]
 fn test_flat_storage_iter() {
     init_test_logger();
-    let num_shards = 3;
     let boundary_accounts = vec!["test0".parse().unwrap(), "test1".parse().unwrap()];
     let shard_layout = ShardLayout::multi_shard_custom(boundary_accounts, 0);
 
-    let genesis = Genesis::test_with_seeds(
+    let genesis = Genesis::from_accounts(
         Clock::real(),
         vec!["test0".parse().unwrap()],
         1,
-        vec![1; num_shards],
         shard_layout.clone(),
     );
 
