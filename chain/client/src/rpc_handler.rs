52f638ed8784326ef046ed340c1cd5615078f5c4
wacban <wac.banasik@gmail.com>
2025-04-29 12:13:53 +0200

    add head height

--- chain/client/src/rpc_handler.rs
+++ chain/client/src/rpc_handler.rs
@@ -49,9 +49,11 @@ impl Handler<ChunkEndorsementMessage> for RpcHandler {
         let chunk_hash = msg.0.chunk_hash();
         let chunk = self.chain_store.get_chunk(&chunk_hash);
         if let Ok(chunk) = chunk {
+            let head = self.chain_store.head().unwrap();
+            let head_height = head.height;
             let height_created = chunk.height_created();
             let height_included = chunk.height_included();
-            tracing::debug!(target: "client", ?chunk_hash, ?height_created, ?height_included, "Already received chunk endorsement.");
+            tracing::debug!(target: "client", ?head_height, ?chunk_hash, ?height_created, ?height_included, "Already received chunk endorsement.");
         }
         let mut tracker = self.chunk_endorsement_tracker.lock().unwrap();
         if let Err(err) = tracker.process_chunk_endorsement(msg.0) {
