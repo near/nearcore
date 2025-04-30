6ac886885c094b3a115c3eed11546ed882e75b7c
Saketh Are <saketh.are@gmail.com>
2025-04-30 11:20:59 -0400

    include next epoch BPs when forwarding chunks

--- chain/chunks/src/shards_manager_actor.rs
+++ chain/chunks/src/shards_manager_actor.rs
@@ -1908,10 +1908,18 @@ impl ShardsManagerActor {
         let forward =
             PartialEncodedChunkForwardMsg::from_header_and_parts(chunk_header, owned_parts);
 
-        let block_producers = self.epoch_manager.get_epoch_block_producers_ordered(&epoch_id)?;
+        let next_epoch_id = self.epoch_manager.get_next_epoch_id(latest_block_hash)?;
+        let this_epoch_block_producers =
+            self.epoch_manager.get_epoch_block_producers_ordered(&epoch_id)?;
+        let next_epoch_block_producers =
+            self.epoch_manager.get_epoch_block_producers_ordered(&next_epoch_id)?;
+        let block_producers: Vec<_> =
+            this_epoch_block_producers.into_iter().chain(next_epoch_block_producers).collect();
+
         let current_chunk_height = chunk_header.height_created();
 
-        // We only forward the parts to the block producers
+        // Parts are forwarded to all block producers (of this epoch or next)
+        // which care about the shard.
         let shard_id = chunk_header.shard_id();
         let mut accounts_forwarded_to = HashSet::new();
         accounts_forwarded_to.insert(me.clone());
