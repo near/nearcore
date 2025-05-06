6aff3c5b41772ac17eaec1247b4441763e24ec7c
Simonas Kazlauskas <git@kazlauskas.me>
2025-05-06 14:48:31 +0300

    separate thread for async computation spawner

--- chain/chain/src/rayon_spawner.rs
+++ chain/chain/src/rayon_spawner.rs
@@ -1,17 +1,13 @@
-use std::sync::OnceLock;
-
 use near_async::futures::AsyncComputationSpawner;
-
-static CHAIN_POOL: OnceLock<rayon::ThreadPool> = OnceLock::new();
 
 pub struct RayonAsyncComputationSpawner;
 
 impl AsyncComputationSpawner for RayonAsyncComputationSpawner {
     fn spawn_boxed(&self, _name: &str, f: Box<dyn FnOnce() + Send>) {
-        let pool = CHAIN_POOL.get_or_init(|| {
-            rayon::ThreadPoolBuilder::new().build().expect("build async computation spawner pool")
-        });
+        // let pool = CHAIN_POOL.get_or_init(|| {
+        //     rayon::ThreadPoolBuilder::new().build().expect("build async computation spawner pool")
+        // });
         let dispatcher = tracing::dispatcher::get_default(|it| it.clone());
-        pool.spawn(move || tracing::dispatcher::with_default(&dispatcher, f))
+        std::thread::spawn(move || tracing::dispatcher::with_default(&dispatcher, f));
     }
 }
