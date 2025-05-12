a26c2599f029b241b960f2f0d0d9eecfffa80c68
robin-near <robin@nearone.org>
2025-05-12 12:12:02 -0700

    [Debug UI] Fix routing due to react-router v7 upgrade, and fix the lint errors.

--- tools/debug-ui/src/App.tsx
+++ tools/debug-ui/src/App.tsx
@@ -21,40 +21,45 @@ export const App = () => {
         <div className="App">
             <HeaderBar addr={addr} />
             <div className="navbar">
-                <NavLink to="last_blocks" className={navLinkClassName}>
+                <NavLink to="../last_blocks" className={navLinkClassName}>
                     Latest Blocks
                 </NavLink>
-                <NavLink to="network_info" className={navLinkClassName}>
+                <NavLink to="../network_info" className={navLinkClassName}>
                     Network Info
                 </NavLink>
-                <NavLink to="epoch_info" className={navLinkClassName}>
+                <NavLink to="../epoch_info" className={navLinkClassName}>
                     Epoch Info
                 </NavLink>
-                <NavLink to="chain_and_chunk_info" className={navLinkClassName}>
+                <NavLink to="../chain_and_chunk_info" className={navLinkClassName}>
                     Chain & Chunk Info
                 </NavLink>
-                <NavLink to="sync_info" className={navLinkClassName}>
+                <NavLink to="../sync_info" className={navLinkClassName}>
                     Sync Info
                 </NavLink>
-                <NavLink to="validator_info" className={navLinkClassName}>
+                <NavLink to="../validator_info" className={navLinkClassName}>
                     Validator Info
                 </NavLink>
-                <NavLink to="cluster" className={navLinkClassName}>
+                <NavLink to="../cluster" className={navLinkClassName}>
                     Cluster View
                 </NavLink>
-                <NavLink to="entity_debug" className={navLinkClassName}>
+                <NavLink to="../entity_debug" className={navLinkClassName}>
                     Entity Debug
                 </NavLink>
             </div>
             <Routes>
-                <Route path="" element={<Navigate to="cluster" />} />
                 <Route path="last_blocks" element={<LatestBlocksView addr={addr} />} />
-                <Route path="network_info/*" element={<NetworkInfoView addr={addr} />} />
-                <Route path="epoch_info/*" element={<EpochInfoView addr={addr} />} />
-                <Route
-                    path="chain_and_chunk_info/*"
-                    element={<ChainAndChunkInfoView addr={addr} />}
-                />
+                <Route path="network_info">
+                    <Route index element={<Navigate to="current" />} />
+                    <Route path="*" element={<NetworkInfoView addr={addr} />} />
+                </Route>
+                <Route path="epoch_info">
+                    <Route index element={<Navigate to="recent" />} />
+                    <Route path="*" element={<EpochInfoView addr={addr} />} />
+                </Route>
+                <Route path="chain_and_chunk_info">
+                    <Route index element={<Navigate to="chain_info_summary" />} />
+                    <Route path="*" element={<ChainAndChunkInfoView addr={addr} />} />
+                </Route>
                 <Route path="sync_info" element={<SyncInfoView addr={addr}></SyncInfoView>} />
                 <Route path="validator_info" element={<div>TODO</div>} />
                 <Route path="cluster" element={<ClusterView initialAddr={addr} />} />
--- tools/debug-ui/src/ChainAndChunkInfoView.tsx
+++ tools/debug-ui/src/ChainAndChunkInfoView.tsx
@@ -1,5 +1,5 @@
 import './ChainAndChunkInfoView.scss';
-import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import { BlocksView } from './BlocksView';
 import { ChainInfoSummaryView } from './ChainInfoSummaryView';
 import { FloatingChunksView } from './FloatingChunksView';
@@ -12,18 +12,17 @@ export const ChainAndChunkInfoView = ({ addr }: ChainAndChunkInfoViewProps) => {
     return (
         <div className="chain-and-chunk-info-view">
             <div className="navbar">
-                <NavLink to="chain_info_summary" className={navLinkClassName}>
+                <NavLink to="../chain_info_summary" className={navLinkClassName}>
                     Chain Info Summary
                 </NavLink>
-                <NavLink to="floating_chunks" className={navLinkClassName}>
+                <NavLink to="../floating_chunks" className={navLinkClassName}>
                     Floating Chunks
                 </NavLink>
-                <NavLink to="blocks" className={navLinkClassName}>
+                <NavLink to="../blocks" className={navLinkClassName}>
                     Blocks
                 </NavLink>
             </div>
             <Routes>
-                <Route path="" element={<Navigate to="chain_info_summary" />} />
                 <Route path="chain_info_summary" element={<ChainInfoSummaryView addr={addr} />} />
                 <Route path="floating_chunks" element={<FloatingChunksView addr={addr} />} />
                 <Route path="blocks" element={<BlocksView addr={addr} />} />
