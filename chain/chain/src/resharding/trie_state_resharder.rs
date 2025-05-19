ff53ff4b25afa2ee99332971c5e92ab14c942ce1
Darioush Jalali <darioush.jalali@nearone.org>
2025-05-19 13:22:02 -0700

    wip: update trie store using batched iterations

--- chain/chain/src/resharding/trie_state_resharder.rs
+++ chain/chain/src/resharding/trie_state_resharder.rs
@@ -4,8 +4,13 @@
 use std::fmt::Debug;
 use std::sync::Arc;
 
-use itertools::Itertools;
-use near_store::adapter::StoreAdapter;
+use borsh::{BorshDeserialize, BorshSerialize};
+use near_primitives::hash::CryptoHash;
+use near_store::adapter::trie_store::TrieStoreUpdateAdapter;
+use near_store::adapter::{StoreAdapter, StoreUpdateAdapter};
+use near_store::db::TRIE_STATE_RESHARDING_STATUS_KEY;
+use near_store::metrics::trie_state_metrics;
+use near_store::{DBCol, ShardTries, StorageError};
 use tracing::{error, info};
 
 use crate::resharding::event_type::ReshardingSplitShardParams;
@@ -15,10 +20,48 @@ use near_chain_primitives::Error;
 use near_epoch_manager::EpochManagerAdapter;
 use near_o11y::metrics::IntGauge;
 use near_primitives::shard_layout::ShardUId;
-use near_primitives::types::{BlockHeight, StateRoot};
-use near_store::flat::{
-    FlatStorageReadyStatus, FlatStorageReshardingStatus, FlatStorageStatus, ParentSplitParameters,
-};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct TrieStateReshardingChildStatus {
+    shard_uid: ShardUId,
+    next_key: Vec<u8>,
+
+    #[borsh(skip)]
+    metrics: Option<TrieStateResharderMetrics>,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct TrieStateReshardingStatus {
+    state_root: CryptoHash,
+    children: Vec<TrieStateReshardingChildStatus>,
+}
+
+impl TrieStateReshardingStatus {
+    fn new(state_root: CryptoHash, left: ShardUId, right: ShardUId) -> Self {
+        Self {
+            state_root,
+            children: vec![
+                TrieStateReshardingChildStatus { shard_uid: left, next_key: vec![], metrics: None },
+                TrieStateReshardingChildStatus {
+                    shard_uid: right,
+                    next_key: vec![],
+                    metrics: None,
+                },
+            ],
+        }
+    }
+
+    fn with_metrics(mut self) -> Self {
+        for child in &mut self.children {
+            child.metrics = Some(TrieStateResharderMetrics::new(&child.shard_uid));
+        }
+        self
+    }
+
+    fn done(&self) -> bool {
+        self.children.is_empty()
+    }
+}
 
 /// TrieStateResharder is responsible for handling state resharding operations.
 pub struct TrieStateResharder {
@@ -40,249 +83,105 @@ impl TrieStateResharder {
         Self { epoch_manager, runtime, handle, resharding_config }
     }
 
+    //
+    fn process_batch_and_update_status(
+        &self,
+        state_root: CryptoHash,
+        status: &mut TrieStateReshardingStatus,
+    ) -> Result<(), Error> {
+        let tries = self.runtime.get_tries();
+        let batch_size = self.resharding_config.get().batch_size.as_u64() as usize;
+        while let Some(child) = status.children.first_mut() {
+            let shard_uid = child.shard_uid;
+            let mut store_update = self.runtime.store().store_update();
+            let next_key = next_batch(
+                self.runtime.get_tries(),
+                shard_uid,
+                state_root,
+                child.next_key.clone(),
+                batch_size,
+                &mut store_update.trie_store_update(),
+            )?;
+
+            if let Some(metrics) = &child.metrics {
+                metrics.inc_processed_batches();
+            }
+
+            if let Some(next_key) = next_key {
+                child.next_key = next_key;
+            } else {
+                // No more keys to process for this child shard.
+                status.children.remove(0);
+            };
+
+            // Commit the changes to the store, along with the status.
+            if status.done() {
+                store_update.delete(DBCol::Misc, TRIE_STATE_RESHARDING_STATUS_KEY);
+            } else {
+                store_update.set(
+                    DBCol::Misc,
+                    TRIE_STATE_RESHARDING_STATUS_KEY,
+                    &borsh::to_vec(status)?,
+                );
+            }
+            store_update.commit()?;
+        }
+
+        Ok(())
+    }
+
+    fn load_status(&self) -> Result<Option<TrieStateReshardingStatus>, Error> {
+        Ok(self
+            .runtime
+            .store()
+            .get_ser::<TrieStateReshardingStatus>(DBCol::Misc, TRIE_STATE_RESHARDING_STATUS_KEY)?)
+    }
+
     /// Start a resharding operation by iterating the memtries of each child shard,
     /// writing the result to the `State` column of the respective shard.
     pub fn start_resharding_blocking(
         &self,
         event: &ReshardingSplitShardParams,
     ) -> Result<(), Error> {
-        let status = self.runtime.store().flat_store().get_flat_storage_status(event.parent_shard);
-        let Ok(FlatStorageStatus::Ready(FlatStorageReadyStatus { flat_head })) = status else {
-            error!(target: "resharding", ?status, ?event, "flat storage shard split task: parent shard is not ready");
-            panic!("impossible to recover from a flat storage split shard failure!");
-        };
-
         // Get the new shard layout right after resharding.
         let next_epoch_id = self.epoch_manager.get_next_epoch_id(&event.resharding_block.hash)?;
         let shard_layout = self.epoch_manager.get_shard_layout(&next_epoch_id)?;
+        let resharding_block =
+            self.runtime.store().chain_store().get_block_header(&event.resharding_block.hash)?;
+        let state_root = resharding_block.prev_state_root();
 
-        let split_params = ParentSplitParameters {
-            left_child_shard: event.left_child_shard,
-            right_child_shard: event.right_child_shard,
-            shard_layout,
-            resharding_blocks: vec![event.resharding_block],
-            flat_head,
-        };
-        self.start_resharding_blocking_impl(event.parent_shard, split_params);
+        let status = self.load_status()?;
+        // TODO: existing status means resharding was in progress, error out so the node operator
+        // manually resumes it.
+        // Add some code like this:
+        //  tracing::error!(target: "resharding", "impossible to recover from a state shard split failure!");
+        //  panic!("impossible to recover from a state split shard failure!")
+
+        let mut status = TrieStateReshardingStatus::new(
+            *state_root,
+            event.left_child_shard,
+            event.right_child_shard,
+        )
+        .with_metrics();
+        while !status.done() {
+            // Process the batch and update the status.
+            self.process_batch_and_update_status(status.state_root, &mut status)?;
+        }
 
         Ok(())
-    }
-
-    fn start_resharding_blocking_impl(
-        &self,
-        parent_shard: ShardUId,
-        split_params: ParentSplitParameters,
-    ) {
-        let left_child_shard = split_params.left_child_shard;
-        let right_child_shard = split_params.right_child_shard;
-        match self.split_shard_task_blocking(parent_shard, split_params) {
-            // All good.
-            FlatStorageReshardingTaskResult::Successful { .. } => {}
-            // The task has been cancelled. Nothing else to do.
-            FlatStorageReshardingTaskResult::Cancelled => {
-                return;
-            }
-            FlatStorageReshardingTaskResult::Failed => {
-                tracing::error!(target: "resharding", "impossible to recover from a state shard split failure!");
-                panic!("impossible to recover from a state split shard failure!")
-            }
-        }
-
-        for child_shard in [left_child_shard, right_child_shard] {
-            match self.shard_catchup_task_blocking(child_shard) {
-                // All good.
-                FlatStorageReshardingTaskResult::Successful { .. } => {}
-                // The task has been cancelled. Nothing else to do.
-                FlatStorageReshardingTaskResult::Cancelled => {}
-                FlatStorageReshardingTaskResult::Failed => {
-                    panic!("impossible to recover from a state shard catchup failure!")
-                }
-            }
-        }
     }
 
     /// Resume an interrupted resharding operation.
     pub fn resume(&self, shard_uid: ShardUId) -> Result<(), Error> {
-        let status = self.runtime.get_flat_storage_manager().get_flat_storage_status(shard_uid);
-        let resharding_status = match status {
-            FlatStorageStatus::Disabled
-            | FlatStorageStatus::Empty
-            | FlatStorageStatus::Creation(_)
-            | FlatStorageStatus::Ready(_) => {
-                tracing::info!(target: "resharding", ?shard_uid, ?status, "did not resume resharding");
-                return Ok(());
-            }
-            // We only need to resume resharding if the status is `Resharding`.
-            FlatStorageStatus::Resharding(status) => status,
-        };
-
-        match resharding_status {
-            FlatStorageReshardingStatus::CreatingChild => {
-                // Nothing to do here because the parent will take care of resuming work.
-            }
-            FlatStorageReshardingStatus::SplittingParent(status) => {
-                let parent_shard_uid = shard_uid;
-                info!(target: "resharding", ?parent_shard_uid, ?status, "resuming state shard split");
-                // On resume, flat storage status is already set correctly and read from DB.
-                // Thus, we don't need to care about cancelling other existing resharding events.
-                // However, we don't know the current state of children shards,
-                // so it's better to clean them.
-                self.clean_children_shards(&status)?;
-                self.start_resharding_blocking_impl(parent_shard_uid, status);
-            }
-            FlatStorageReshardingStatus::CatchingUp(_) => {
-                info!(target: "resharding", ?shard_uid, ?resharding_status, "resuming state shard catchup");
-                match self.shard_catchup_task_blocking(shard_uid) {
-                    // All good.
-                    FlatStorageReshardingTaskResult::Successful { .. } => {}
-                    FlatStorageReshardingTaskResult::Failed => {
-                        panic!("impossible to recover from a state shard catchup failure!")
-                    }
-                    // The task has been cancelled. Nothing else to do.
-                    FlatStorageReshardingTaskResult::Cancelled => {}
-                }
-            }
+        let status = self.load_status()?;
+        let status = status.unwrap(); // TODO: should always have a status if resuming
+        let mut status = status.with_metrics();
+        while !status.done() {
+            // Process the batch and update the status.
+            self.process_batch_and_update_status(status.state_root, &mut status)?;
         }
+
         Ok(())
-    }
-
-    /// Clean up any data in the children shards.
-    fn clean_children_shards(&self, status: &ParentSplitParameters) -> Result<(), Error> {
-        let ParentSplitParameters { left_child_shard, right_child_shard, .. } = status;
-        info!(target: "resharding", ?left_child_shard, ?right_child_shard, "cleaning up children shards state's content");
-        let mut store_update = self.runtime.store().flat_store().store_update();
-        for child in [left_child_shard, right_child_shard] {
-            store_update.remove_all_values(*child);
-        }
-        store_update.commit()?;
-        Ok(())
-    }
-
-    /// Execute the shard split task.
-    fn split_shard_task_blocking(
-        &self,
-        parent_shard: ShardUId,
-        split_params: ParentSplitParameters,
-    ) -> FlatStorageReshardingTaskResult {
-        info!(target: "resharding", "state shard split task execution");
-
-        let metrics = FlatStorageReshardingShardSplitMetrics::new(
-            parent_shard,
-            split_params.left_child_shard,
-            split_params.right_child_shard,
-        );
-
-        self.split_shard_task_preprocessing(parent_shard, &split_params, &metrics);
-
-        let task_status =
-            self.split_shard_task_blocking_impl(parent_shard, &split_params, &metrics);
-        self.split_shard_task_postprocessing(parent_shard, split_params, &metrics, task_status);
-        info!(target: "resharding", ?task_status, "state shard split task finished");
-        task_status
-    }
-
-    /// Prepare for the shard split operation.
-    fn split_shard_task_preprocessing(
-        &self,
-        _parent_shard: ShardUId,
-        _split_params: &ParentSplitParameters,
-        _metrics: &FlatStorageReshardingShardSplitMetrics,
-    ) {
-        // Implementation would go here
-    }
-
-    /// Execute the core shard split processing.
-    fn split_shard_task_blocking_impl(
-        &self,
-        parent_shard: ShardUId,
-        split_params: &ParentSplitParameters,
-        metrics: &FlatStorageReshardingShardSplitMetrics,
-    ) -> FlatStorageReshardingTaskResult {
-        // Exit early if the task has already been cancelled.
-        if self.handle.is_cancelled() {
-            return FlatStorageReshardingTaskResult::Cancelled;
-        }
-
-        // Determines after how many bytes worth of key-values the process stops to commit changes
-        // and to check cancellation.
-        let batch_size = self.resharding_config.get().batch_size.as_u64() as usize;
-        metrics.set_split_shard_batch_size(batch_size);
-        // Delay between every batch.
-
-        // Implementation would continue here
-
-        FlatStorageReshardingTaskResult::Successful { num_batches_done: 0 }
-    }
-
-    /// Finalize the shard split operation.
-    fn split_shard_task_postprocessing(
-        &self,
-        parent_shard: ShardUId,
-        split_params: ParentSplitParameters,
-        metrics: &FlatStorageReshardingShardSplitMetrics,
-        task_status: FlatStorageReshardingTaskResult,
-    ) {
-        info!(target: "resharding", ?parent_shard, ?task_status, ?split_params, "state shard split task: post-processing");
-
-        let ParentSplitParameters {
-            left_child_shard,
-            right_child_shard,
-            flat_head,
-            resharding_blocks,
-            ..
-        } = split_params;
-        let resharding_block = resharding_blocks.into_iter().exactly_one().unwrap();
-
-        // Implementation would continue here
-    }
-
-    /// Execute the shard catchup task.
-    fn shard_catchup_task_blocking(&self, shard_uid: ShardUId) -> FlatStorageReshardingTaskResult {
-        // Exit early if the task has already been cancelled.
-        if self.handle.is_cancelled() {
-            return FlatStorageReshardingTaskResult::Cancelled;
-        }
-
-        info!(target: "resharding", ?shard_uid, "state shard catchup task started");
-        let metrics = FlatStorageReshardingShardCatchUpMetrics::new(&shard_uid);
-
-        // Apply deltas and then create the storage.
-        let (num_batches_done, flat_head) = match self
-            .shard_catchup_apply_deltas_blocking(shard_uid, &metrics)
-        {
-            Ok(ShardCatchupApplyDeltasOutcome::Succeeded(num_batches_done, tip)) => {
-                (num_batches_done, tip)
-            }
-            Ok(ShardCatchupApplyDeltasOutcome::Cancelled) => {
-                return FlatStorageReshardingTaskResult::Cancelled;
-            }
-            Err(err) => {
-                error!(target: "resharding", ?shard_uid, ?err, "state shard catchup delta application failed!");
-                return FlatStorageReshardingTaskResult::Failed;
-            }
-        };
-
-        // Implementation would continue here
-
-        FlatStorageReshardingTaskResult::Successful { num_batches_done }
-    }
-
-    /// Apply deltas during shard catchup.
-    fn shard_catchup_apply_deltas_blocking(
-        &self,
-        shard_uid: ShardUId,
-        metrics: &FlatStorageReshardingShardCatchUpMetrics,
-    ) -> Result<ShardCatchupApplyDeltasOutcome, Error> {
-        // How many block heights of deltas are applied in a single commit.
-        let catch_up_blocks = self.resharding_config.get().catch_up_blocks;
-        // Delay between every batch.
-        let batch_delay = self.resharding_config.get().batch_delay.unsigned_abs();
-
-        info!(target: "resharding", ?shard_uid, ?batch_delay, ?catch_up_blocks, "state shard catchup: starting delta apply");
-
-        // Implementation would continue here
-
-        Ok(ShardCatchupApplyDeltasOutcome::Succeeded(0, StateRoot::default()))
     }
 }
 
@@ -292,62 +191,48 @@ impl Debug for TrieStateResharder {
     }
 }
 
-/// Result of a flat storage resharding task.
-#[derive(Clone, Debug, Copy, Eq, PartialEq)]
-enum FlatStorageReshardingTaskResult {
-    Successful { num_batches_done: usize },
-    Failed,
-    Cancelled,
+/// Metrics for tracking store column update during resharding.
+struct TrieStateResharderMetrics {
+    processed_batches: IntGauge,
 }
 
-/// Outcome of applying deltas during shard catchup.
-enum ShardCatchupApplyDeltasOutcome {
-    Succeeded(usize, StateRoot),
-    Cancelled,
-}
-
-/// Metrics for tracking shard split operations.
-struct FlatStorageReshardingShardSplitMetrics {
-    status: IntGauge,
-    // Other metrics fields would be here
-}
-
-impl FlatStorageReshardingShardSplitMetrics {
-    fn new(
-        parent_shard: ShardUId,
-        left_child_shard: ShardUId,
-        right_child_shard: ShardUId,
-    ) -> Self {
-        Self {
-            status: IntGauge::new("status", "Status of resharding").unwrap(),
-            // Initialize other metrics
-        }
-    }
-
-    fn set_split_shard_batch_size(&self, batch_size: usize) {
-        // Implementation
-    }
-}
-
-/// Metrics for tracking shard catchup operations.
-struct FlatStorageReshardingShardCatchUpMetrics {
-    status: IntGauge,
-    head_height: IntGauge,
-}
-
-impl FlatStorageReshardingShardCatchUpMetrics {
+impl TrieStateResharderMetrics {
     pub fn new(shard_uid: &ShardUId) -> Self {
-        Self {
-            status: IntGauge::new("status", "Status of resharding").unwrap(),
-            head_height: IntGauge::new("head_height", "Height of the flat storage head").unwrap(),
+        let processed_batches = trie_state_metrics::STATE_COL_RESHARDING_PROCESSED_BATCHES
+            .with_label_values(&[&shard_uid.to_string()]);
+        Self { processed_batches }
+    }
+
+    pub fn inc_processed_batches(&self) {
+        self.processed_batches.inc();
+    }
+}
+
+fn next_batch(
+    tries: ShardTries,
+    shard_uid: ShardUId,
+    state_root: CryptoHash,
+    seek_key: Vec<u8>,
+    batch_size: usize,
+    store_update: &mut TrieStoreUpdateAdapter,
+) -> Result<Option<Vec<u8>>, StorageError> {
+    let trie = tries.get_trie_for_shard(shard_uid, state_root).recording_reads_new_recorder();
+    let locked = trie.lock_for_iter();
+    let mut iter = locked.iter()?;
+    iter.seek(seek_key, true)?;
+
+    let mut next_key: Option<Vec<u8>> = None;
+    for item in iter {
+        let (key, _val) = item?; // Handle StorageError
+        let stats = trie.recorder_stats().expect("trie recorder stats should be available");
+        if stats.total_size >= batch_size {
+            next_key = Some(key);
+            break;
         }
     }
 
-    pub fn set_status(&self, status: &FlatStorageStatus) {
-        // Implementation
-    }
-
-    pub fn set_head_height(&self, height: BlockHeight) {
-        // Implementation
-    }
+    let trie_changes =
+        trie.recorded_trie_changes(state_root).expect("trie changes should be available");
+    tries.apply_all(&trie_changes, shard_uid, store_update);
+    Ok(next_key)
 }
