bc3205339148eee53ebb357ef2d72b5a06799f22
Longarithm <the.alex.logunov@gmail.com>
2025-05-27 12:20:21 +0400

    add

---
+++ chain/client/src/stateless_validation/state_witness_producer.rs
@@ -0,0 +1,69 @@
+use super::partial_witness::partial_witness_actor::DistributeStateWitnessRequest;
+use crate::Client;
+use crate::stateless_validation::chunk_validator::send_chunk_endorsement_to_block_producers;
+use near_async::messaging::{CanSend, IntoSender};
+use near_chain::BlockHeader;
+use near_chain::stateless_validation::state_witness::CreateWitnessResult;
+use near_chain_primitives::Error;
+use near_primitives::sharding::{ShardChunk, ShardChunkHeader};
+use near_primitives::types::EpochId;
+use near_primitives::validator_signer::ValidatorSigner;
+use std::sync::Arc;
+
+impl Client {
+    /// Distributes the chunk state witness to chunk validators that are
+    /// selected to validate this chunk.
+    pub fn send_chunk_state_witness_to_chunk_validators(
+        &mut self,
+        epoch_id: &EpochId,
+        prev_block_header: &BlockHeader,
+        prev_chunk_header: &ShardChunkHeader,
+        chunk: &ShardChunk,
+        validator_signer: &Option<Arc<ValidatorSigner>>,
+    ) -> Result<(), Error> {
+        let chunk_header = chunk.cloned_header();
+        let shard_id = chunk_header.shard_id();
+        let _span = tracing::debug_span!(target: "client", "send_chunk_state_witness", chunk_hash=?chunk_header.chunk_hash(), ?shard_id).entered();
+
+        let my_signer = validator_signer
+            .as_ref()
+            .ok_or_else(|| Error::NotAValidator(format!("send state witness")))?;
+        let CreateWitnessResult { state_witness, main_transition_shard_id, contract_updates } =
+            self.chain.chain_store().create_state_witness(
+                self.epoch_manager.as_ref(),
+                my_signer.validator_id().clone(),
+                prev_block_header,
+                prev_chunk_header,
+                chunk,
+            )?;
+
+        if self.config.save_latest_witnesses {
+            self.chain.chain_store.save_latest_chunk_state_witness(&state_witness)?;
+        }
+
+        let height = chunk_header.height_created();
+        if self
+            .epoch_manager
+            .get_chunk_validator_assignments(epoch_id, shard_id, height)?
+            .contains(my_signer.validator_id())
+        {
+            // Bypass state witness validation if we created state witness. Endorse the chunk immediately.
+            tracing::debug!(target: "client", chunk_hash=?chunk_header.chunk_hash(), ?shard_id, "send_chunk_endorsement_from_chunk_producer");
+            if let Some(endorsement) = send_chunk_endorsement_to_block_producers(
+                &chunk_header,
+                self.epoch_manager.as_ref(),
+                my_signer.as_ref(),
+                &self.network_adapter.clone().into_sender(),
+            ) {
+                self.chunk_endorsement_tracker.process_chunk_endorsement(endorsement)?;
+            }
+        }
+
+        self.partial_witness_adapter.send(DistributeStateWitnessRequest {
+            state_witness,
+            contract_updates,
+            main_transition_shard_id,
+        });
+        Ok(())
+    }
+}
