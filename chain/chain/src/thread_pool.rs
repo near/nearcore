0e1ccf569830371158a22ce27e084577ca9109e9
Adam Wierzbicki <adam.wierzbicki@nearone.org>
2025-06-02 14:52:59 +0200

    review suggestions

--- chain/chain/src/thread_pool.rs
+++ chain/chain/src/thread_pool.rs
@@ -5,12 +5,13 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Duration;
 use thiserror::Error;
 use thread_priority::{RealtimeThreadSchedulePolicy, ThreadBuilder, ThreadSchedulePolicy};
+use tracing::debug;
 
 pub(crate) trait ThreadLimit {
     fn max_threads(&self) -> usize;
 }
 
-/// TODO: Implement `ThreadLimit` which returns the current number of tracked shards
+/// TODO: Implement `ThreadLimit` which returns the current number of tracked/validated shards
 pub(crate) type ConstLimit = usize;
 
 impl ThreadLimit for ConstLimit {
@@ -20,14 +21,20 @@ impl ThreadLimit for ConstLimit {
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
-type IdleQueue = Arc<Mutex<VecDeque<oneshot::Sender<Option<Job>>>>>;
+type IdleThreadQueue = Arc<Mutex<VecDeque<oneshot::Sender<Option<Job>>>>>;
 
-pub(crate) struct ThreadPool<C: ThreadLimit> {
+pub(crate) struct ThreadPool<Limit: ThreadLimit> {
+    /// Name of the pool. Used for logging/debugging purposes.
     name: &'static str,
-    limit: C,
+    /// Limit of running threads.
+    limit: Limit,
+    /// Timeout after which an idle thread terminates.
     idle_timeout: Duration,
-    num_threads: AtomicUsize,
-    idle_queue: IdleQueue,
+    /// Counter of currently running worker threads (active or idle).
+    worker_counter: Arc<AtomicUsize>,
+    /// Queue of oneshot senders which allow sending jobs to idle threads.
+    /// Once a worker thread is done processing its job, it pushes a sender into this queue.
+    idle_thread_queue: IdleThreadQueue,
 }
 
 #[derive(Error, Debug)]
@@ -38,14 +45,14 @@ pub(crate) enum Error {
     Spawn(#[from] std::io::Error),
 }
 
-impl<C: ThreadLimit> ThreadPool<C> {
-    pub(crate) fn new(name: &'static str, idle_timeout: Duration, limit: C) -> Self {
+impl<Limit: ThreadLimit> ThreadPool<Limit> {
+    pub(crate) fn new(name: &'static str, idle_timeout: Duration, limit: Limit) -> Self {
         Self {
             name,
             limit,
             idle_timeout,
-            num_threads: Default::default(),
-            idle_queue: Default::default(),
+            worker_counter: Default::default(),
+            idle_thread_queue: Default::default(),
         }
     }
 
@@ -56,42 +63,63 @@ impl<C: ThreadLimit> ThreadPool<C> {
     pub(crate) fn spawn_boxed(&self, job: Job) -> Result<(), Error> {
         // Try to use one of the existing idle threads
         let mut job = Some(job);
-        let mut idle_guard = self.idle_queue.lock();
-        while let Some(sender) = idle_guard.pop_front() {
+        let mut queue_guard = self.idle_thread_queue.lock();
+        while let Some(sender) = queue_guard.pop_front() {
             job = match sender.send(job) {
                 Ok(()) => return Ok(()),
-                Err(err) => {
-                    // Idle thread has been terminated
-                    self.num_threads.fetch_sub(1, Ordering::Relaxed);
-                    err.into_inner()
-                }
+                Err(err) => err.into_inner(),
             }
         }
-        drop(idle_guard);
+        drop(queue_guard);
         self.try_spawn_thread(job.unwrap())
     }
 
     fn try_spawn_thread(&self, job: Job) -> Result<(), Error> {
         let name = self.name;
         let limit = self.limit.max_threads();
-        if self.num_threads.load(Ordering::Relaxed) >= limit {
-            return Err(Error::ThreadLimitReached { name, limit });
-        }
+        self.worker_counter
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |v| (v < limit).then_some(v + 1))
+            .map_err(|_| Error::ThreadLimitReached { name, limit })?;
 
         let idle_timeout = self.idle_timeout;
-        let idle_queue = self.idle_queue.clone();
+        let idle_queue = self.idle_thread_queue.clone();
+        let worker_counter = self.worker_counter.clone();
         ThreadBuilder::default()
             .name(name)
             .policy(ThreadSchedulePolicy::Realtime(RealtimeThreadSchedulePolicy::RoundRobin))
             // TODO: Set thread priority
-            .spawn_careless(move || run_worker(job, idle_timeout, idle_queue))?;
-        self.num_threads.fetch_add(1, Ordering::Relaxed);
+            .spawn(move |res| {
+                if let Err(err) = res {
+                    debug!("Setting scheduler policy failed: {err}")
+                };
+                run_worker(job, idle_timeout, idle_queue, worker_counter)
+            })
+            .map_err(|err| {
+                self.worker_counter.fetch_sub(1, Ordering::Relaxed);
+                err
+            })?;
 
         Ok(())
     }
 }
 
-fn run_worker(mut job: Job, idle_timeout: Duration, idle_queue: IdleQueue) {
+/// This struct ensures that the thread counter decrements when a thread dies,
+/// even in case of a panic.
+struct WorkerCounterGuard(Arc<AtomicUsize>);
+
+impl Drop for WorkerCounterGuard {
+    fn drop(&mut self) {
+        self.0.fetch_sub(1, Ordering::Relaxed);
+    }
+}
+
+fn run_worker(
+    mut job: Job,
+    idle_timeout: Duration,
+    idle_queue: IdleThreadQueue,
+    worker_counter: Arc<AtomicUsize>,
+) {
+    let _counter_guard = WorkerCounterGuard(worker_counter);
     loop {
         job();
         // Notify the pool that this thread is idle by pushing the sender into the idle queue
@@ -102,6 +130,6 @@ fn run_worker(mut job: Job, idle_timeout: Duration, idle_queue: IdleQueue) {
             Ok(Some(job)) => job,
             _ => break,
         }
-        // No need to notify the pool that this thread has terminated – dropping the receiver is enough
     }
+    // No need to notify the pool that this thread has terminated – dropping the receiver is enough
 }
