f94a6bc095d2f4086b4f50485c1943390ef93c47
Shreyan Gupta <shreyan.gupta96@gmail.com>
2025-06-05 13:33:09 -0700

    restore test_simple.rs

--- integration-tests/src/tests/other_tests/test_simple.rs
+++ integration-tests/src/tests/other_tests/test_simple.rs
@@ -66,16 +66,16 @@ fn run_multiple_nodes(num_nodes: usize, num_trials: usize, test_prefix: &str) {
 }
 
 #[test]
-fn slow_test_2_10_multiple_nodes() {
+fn ultra_slow_test_2_10_multiple_nodes() {
     heavy_test(|| run_multiple_nodes(2, 10, "2_10"));
 }
 
 #[test]
-fn slow_test_4_10_multiple_nodes() {
+fn ultra_slow_test_4_10_multiple_nodes() {
     heavy_test(|| run_multiple_nodes(4, 10, "4_10"));
 }
 
 #[test]
-fn test_7_10_multiple_nodes() {
+fn ultra_slow_test_7_10_multiple_nodes() {
     heavy_test(|| run_multiple_nodes(7, 10, "7_10"));
 }
