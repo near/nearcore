a04804f2c6b4d9e05428466b146cc90775371b52
Shreyan Gupta <shreyan.gupta96@gmail.com>
2025-06-06 12:28:37 -0700

    update comment

--- chain/chain/src/garbage_collection.rs
+++ chain/chain/src/garbage_collection.rs
@@ -1134,12 +1134,10 @@ fn gc_parent_shard_after_resharding(
     epoch_manager: &dyn EpochManagerAdapter,
     block_hash: &CryptoHash,
 ) -> Result<(), Error> {
-    // Clear out state for the parent shard. Note that this function is called for every block in the epoch,
-    // but the actual cleanup only happens the first time we call
-    // `trie_store_update.delete_shard_uid_prefixed_state` while the rest of the calls are no-ops.
-    //
-    // We are unable to call `epoch_manager.will_shard_layout_change(block_hash)` here as `block_hash`
-    // has already been garbage collected.
+    // Clear out state for the parent shard. Note that this function is called at every epoch boundary,
+    // even if there is no resharding.
+    // It's fine to do that as after the first call to `trie_store_update.delete_shard_uid_prefixed_state`
+    // the rest of the calls in future epochs are no-ops.
     if !epoch_manager.is_last_block_in_finished_epoch(block_hash)? {
         return Ok(());
     }
