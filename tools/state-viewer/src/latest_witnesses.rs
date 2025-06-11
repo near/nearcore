1f3a14c155384a267c424db738a14cf01b2189c8
Shreyan Gupta <shreyan.gupta96@gmail.com>
2025-06-11 00:24:26 -0700

    fix

--- tools/state-viewer/src/latest_witnesses.rs
+++ tools/state-viewer/src/latest_witnesses.rs
@@ -61,7 +61,7 @@ fn setup_chain(home_dir: &Path, near_config: NearConfig, store: Store) -> Chain 
         &chain_genesis,
         DoomslugThresholdMode::TwoThirds,
         false,
-        near_config.validator_signer.clone(),
+        near_config.validator_signer,
     )
     .unwrap()
 }
