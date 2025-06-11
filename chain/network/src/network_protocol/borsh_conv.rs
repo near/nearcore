71cdb29355bff3834d687e6afd82b2628589c1ee
Stefan Neamtu <stefan.neamtu@gmail.com>
2025-06-11 17:18:18 +0200

    protobufs

--- chain/network/src/network_protocol/borsh_conv.rs
+++ chain/network/src/network_protocol/borsh_conv.rs
@@ -1,7 +1,7 @@
-use crate::network_protocol::borsh_ as net;
 /// Contains borsh <-> network_protocol conversions.
 use crate::network_protocol::{self as mem, RoutedMessageV3};
 use crate::network_protocol::{PeersRequest, PeersResponse};
+use crate::network_protocol::{RoutedMessageV1, borsh_ as net};
 
 impl From<&net::Handshake> for mem::Handshake {
     fn from(x: &net::Handshake) -> Self {
@@ -169,10 +169,9 @@ impl TryFrom<&net::PeerMessage> for mem::PeerMessage {
             net::PeerMessage::Block(b) => mem::PeerMessage::Block(b),
             net::PeerMessage::OptimisticBlock(ob) => mem::PeerMessage::OptimisticBlock(ob),
             net::PeerMessage::Transaction(t) => mem::PeerMessage::Transaction(t),
-            net::PeerMessage::DeprecatedRouted(r) => mem::PeerMessage::Routed(Box::new(
+            net::PeerMessage::Routed(r) => mem::PeerMessage::Routed(Box::new(
                 mem::RoutedMessage::V3(RoutedMessageV3::from(*r)),
             )),
-            net::PeerMessage::Routed(r) => mem::PeerMessage::Routed(r),
             net::PeerMessage::Disconnect => mem::PeerMessage::Disconnect(mem::Disconnect {
                 // This flag is used by the disconnecting peer to advise the other peer that there
                 // is a reason to remove the connection from storage (for example, a peer ban).
@@ -248,7 +247,17 @@ impl From<&mem::PeerMessage> for net::PeerMessage {
             mem::PeerMessage::Block(b) => net::PeerMessage::Block(b),
             mem::PeerMessage::OptimisticBlock(ob) => net::PeerMessage::OptimisticBlock(ob),
             mem::PeerMessage::Transaction(t) => net::PeerMessage::Transaction(t),
-            mem::PeerMessage::Routed(r) => net::PeerMessage::Routed(r),
+            mem::PeerMessage::Routed(r) => {
+                let msg = r.msg();
+                let msg_v1 = RoutedMessageV1 {
+                    target: msg.target.clone(),
+                    author: msg.author.clone(),
+                    ttl: msg.ttl,
+                    body: msg.body.clone().into(),
+                    signature: msg.signature.clone().unwrap_or_default(),
+                };
+                net::PeerMessage::Routed(Box::new(msg_v1))
+            }
             mem::PeerMessage::Disconnect(_) => net::PeerMessage::Disconnect,
             mem::PeerMessage::Challenge(c) => net::PeerMessage::Challenge(c),
             mem::PeerMessage::StateRequestHeader(shard_id, sync_hash) => {
