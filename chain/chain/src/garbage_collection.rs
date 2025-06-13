78a18d214d4da9be0b33dee9ad3beec72a2fcd2d
Saketh Are <saketh.are@gmail.com>
2025-06-12 20:01:34 -0400

    store invalid state witnesses for debugging purposes (#13601)
    This PR makes nodes store received invalid state witnesses in a separate
    DB column for debugging purposes.

--- chain/chain/src/garbage_collection.rs
+++ chain/chain/src/garbage_collection.rs
@@ -1075,6 +1075,12 @@ impl<'a> ChainStoreUpdate<'a> {
             DBCol::LatestWitnessesByIndex => {
                 store_update.delete(col, key);
             }
+            DBCol::InvalidChunkStateWitnesses => {
+                store_update.delete(col, key);
+            }
+            DBCol::InvalidWitnessesByIndex => {
+                store_update.delete(col, key);
+            }
             DBCol::StateSyncNewChunks => {
                 store_update.delete(col, key);
             }
