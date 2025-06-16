8fad657a138725625f3abc593f8fd4861abfdb4d
Adam Chudaś <18039094+staffik@users.noreply.github.com>
2025-06-16 15:25:01 +0200

    fix(resharding): Do not delete parent shard state if it is referenced (#13708)
    Exactly what @Trisfald proposed, lgtm: [#nearone/private > 2.7 release
    testing @
    💬](https://near.zulipchat.com/#narrow/channel/308695-nearone.2Fprivate/topic/2.2E7.20release.20testing/near/523911042)
    
    I think we were protected from that before, but recent changes removed
    that protection:
    - Only run it after new resharding:
    https://github.com/near/nearcore/pull/13680/files#diff-770f5411fa2d9f774f59424194e326fb2d9722e2fd0c011933614325b6ac91eaL1138
    - Assert that the old mapping does not exist:
    https://github.com/near/nearcore/pull/13693/files#diff-770f5411fa2d9f774f59424194e326fb2d9722e2fd0c011933614325b6ac91eaL1167

--- chain/chain/src/garbage_collection.rs
+++ chain/chain/src/garbage_collection.rs
@@ -18,6 +18,7 @@ use near_primitives::types::{
 use near_primitives::utils::{
     get_block_shard_id, get_block_shard_id_rev, get_outcome_id_block_hash, index_to_bytes,
 };
+use near_store::adapter::trie_store::get_shard_uid_mapping;
 use near_store::adapter::{StoreAdapter, StoreUpdateAdapter};
 use near_store::{DBCol, KeyForStateChanges, ShardTries, ShardUId};
 
@@ -1161,10 +1162,22 @@ fn gc_parent_shard_after_resharding(
     let shard_layout = epoch_manager.get_shard_layout(&next_epoch_id)?;
     let mut trie_store_update = store.trie_store().store_update();
     for parent_shard_uid in shard_layout.get_split_parent_shard_uids() {
-        // Delete the state of the parent shard
-        tracing::debug!(target: "garbage_collection", ?parent_shard_uid, "resharding state cleanup");
-        trie_store_update.delete_shard_uid_prefixed_state(parent_shard_uid);
+        // Check if any child shard still map to this parent shard
+        let children_shards =
+            shard_layout.get_children_shards_uids(parent_shard_uid.shard_id()).unwrap();
+        let has_active_mapping = children_shards.into_iter().any(|child_shard_uid| {
+            let mapped_shard_uid = get_shard_uid_mapping(&store, child_shard_uid);
+            mapped_shard_uid == parent_shard_uid && mapped_shard_uid != child_shard_uid
+        });
+        if !has_active_mapping {
+            // Delete the state of the parent shard
+            tracing::debug!(target: "garbage_collection", ?parent_shard_uid, "resharding state cleanup");
+            trie_store_update.delete_shard_uid_prefixed_state(parent_shard_uid);
+        } else {
+            tracing::debug!(target: "garbage_collection", ?parent_shard_uid, "skipping parent shard cleanup - active mappings exist");
+        }
     }
+
     chain_store_update.merge(trie_store_update.into());
     Ok(())
 }
