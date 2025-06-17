8206e7e0650aba769f5310f308925fa51b3f200b
Trisfald <trisfald@gmail.com>
2025-06-17 15:28:05 +0200

    add start_validating_chunk span

--- chain/client/src/stateless_validation/chunk_validator/mod.rs
+++ chain/client/src/stateless_validation/chunk_validator/mod.rs
@@ -85,6 +85,16 @@ impl ChunkValidator {
         signer: &Arc<ValidatorSigner>,
         save_witness_if_invalid: bool,
     ) -> Result<(), Error> {
+        let _span = tracing::debug_span!(
+            target: "client",
+            "start_validating_chunk",
+            height = %state_witness.chunk_production_key().height_created,
+            shard_id = %state_witness.chunk_production_key().shard_id,
+            validator = %signer.validator_id(),
+            tag_block_production = true,
+        )
+        .entered();
+
         let prev_block_hash = state_witness.chunk_header().prev_block_hash();
         let ChunkProductionKey { epoch_id, .. } = state_witness.chunk_production_key();
         let shard_id = state_witness.chunk_header().shard_id();
