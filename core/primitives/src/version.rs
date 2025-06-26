d2690ae75908da18c62ec5a546e153db27096e3f
Adam Chudaś <adam@chudas.net>
2025-06-26 16:16:37 +0000

    Prepare 2.7.0-rc.1

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -60,7 +60,11 @@ pub fn get_protocol_upgrade_schedule(chain_id: &str) -> ProtocolUpgradeVotingSch
             schedule
         }
         TESTNET => {
-            let schedule = vec![];
+            // Voting starts Wednesday 03:00 UTC.
+            let v1_protocol_version = 78;
+            let v1_datetime =
+                ProtocolUpgradeVotingSchedule::parse_datetime("2025-07-02 03:00:00").unwrap();
+            let schedule = vec![(v1_datetime, v1_protocol_version)];
             schedule
         }
         _ => {
