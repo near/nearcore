0fb5c632f441d7d200c392974b08e1a8d718e63d
Anton Puhach <anton@nearone.org>
2025-07-01 20:03:48 +0200

    impl

--- core/primitives/src/types.rs
+++ core/primitives/src/types.rs
@@ -216,6 +216,9 @@ pub enum StateChangeCause {
     /// State change that is happens due to migration that happens in first block of an epoch
     /// after protocol upgrade
     Migration,
+    /// Used to be ReshardingV2, deprecated in #13155
+    /// We need to keep it to preserve enum variant tags for borsh serialization.
+    _Deprecated,
     /// Update persistent state kept by Bandwidth Scheduler after running the scheduling algorithm.
     BandwidthSchedulerStateUpdate,
 }
