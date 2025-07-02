300b64039154c6302dc23706970f8d5bc77a4b84
Darioush Jalali <darioush.jalali@nearone.org>
2025-07-02 14:00:00 -0700

    use more histogram buckets

--- core/o11y/src/span_duration_logger.rs
+++ core/o11y/src/span_duration_logger.rs
@@ -16,9 +16,13 @@ pub(crate) static SPAN_BUSY_DURATIONS: LazyLock<HistogramVec> = LazyLock::new(||
         "near_span_busy_duration",
         "Busy duration of spans",
         &["name", "level", "target", "measure"],
-        // Cover the range from 0.01s to 10s.
+        // Cover the range from 0.01s to 1s.
         // Keep the number of buckets small to limit the memory usage.
-        Some(vec![0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0]),
+        Some(vec![
+            0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22, 0.24, 0.26, 0.28,
+            0.30, 0.32, 0.34, 0.36, 0.38, 0.40, 0.42, 0.44, 0.46, 0.48, 0.50, 0.52, 0.54, 0.56,
+            0.58, 0.60, 0.75, 1.00,
+        ]),
     )
     .unwrap()
 });
