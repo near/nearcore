459f8ae16e4adf008483554a93d162322b232c11
Slava Savenko <svyatoslav.savenko@nearone.org>
2025-07-08 12:16:43 +0200

    add option to download extended perf data in mocknet pytest

--- pytest/tests/mocknet/helpers/get-profile.sh
+++ pytest/tests/mocknet/helpers/get-profile.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 # 0. Get record duration (default: 10s)
 record_secs="${1:-10}"
+mode="${2:-simple}"
 
 # 1. Remove old files
 rm -f "$HOME/perf."*
@@ -19,16 +20,47 @@ if [ -z "$pid" ]; then
   exit 1
 fi
 
-# 4. Record perf
-echo "▶️ Recording perf data for PID $pid for ${record_secs}s..."
-perf record -e cpu-clock -F 1000 -g --call-graph dwarf,65528 \
-  -p "$pid" -o "$HOME/perf.data" -- sleep "$record_secs" 2>/dev/null
+# normal perf data
+gather_simple_perf() {
+  # 4. Record perf
+  echo "▶️ Recording perf data for PID $pid for ${record_secs}s..."
+  perf record -e cpu-clock -F 1000 -g --call-graph dwarf,65528 \
+      -p "$pid" -o "perf.data" -- sleep "${record_secs}"
 
-# 5. Convert to script
-output_file="$HOME/perf-$(hostname).script"
-echo "📜 Converting perf.data to ${output_file}"
-perf script -F +pid -i "$HOME/perf.data" > "${output_file}" 2>/dev/null
+  # 5. Convert to script
+  output_file="${HOME}/perf-$(hostname).script"
+  echo "📜 Converting perf.data to ${output_file}"
+  perf script -F +pid -i "perf.data" > "${output_file}" 2>/dev/null
 
-# 6. Compress
-gzip -f "${output_file}"
-echo "✅ Done: ${output_file}.gz"
+  # 6. Compress
+  gzip -f "${output_file}"
+  echo "✅ Done: ${output_file}.gz"
+}
+
+# extended perf data
+gather_extended_perf() {
+  sudo sysctl kernel.sched_schedstats=1
+  
+  output_dir="./perf-data"
+  mkdir -p ${output_dir}
+  
+  perf stat \
+    -p ${pid} \
+    --per-thread \
+    -- sleep "${record_secs}" &> "${output_dir}/perf-stat.out"
+
+  perf record -e cpu-clock -F1000 -g --call-graph fp,65528 -p ${pid} -o perf-on-cpu.data -- sleep "${record_secs}"
+  perf script -F +pid -i perf-on-cpu.data > "${output_dir}/perf-on-cpu.script"
+
+  sudo perf record -g --call-graph fp,65528 -p ${pid} --off-cpu -o perf-off-cpu.data -- sleep 6
+  sudo perf script --header -i perf-off-cpu.data > "${output_dir}/perf-off-cpu.script"
+  sudo chown ${USER} "${output_dir}/perf-off-cpu.script"
+
+  tar czf perf-data.tar.gz "${output_dir}"
+}
+
+if [ "$mode" = "extended" ]; then
+  gather_extended_perf
+else
+  gather_simple_perf
+fi
