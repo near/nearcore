93263dd3c6da203e356142d588e76baddc1a57e0
Attila Toth <attila.toth@nearone.org>
2025-07-10 15:08:29 +0200

    cargo fmt

--- chain/client/src/chunk_executor_actor.rs
+++ chain/client/src/chunk_executor_actor.rs
@@ -260,9 +260,7 @@ impl ChunkExecutorActor {
         let store = self.chain_store.store();
         let prev_block = self.chain_store.get_block(prev_block_hash)?;
         let prev_block_is_genesis = *prev_block_hash == self.genesis_hash;
-        if !prev_block_is_genesis
-            && !self.core_processor.all_execution_results_exist(&prev_block)
-        {
+        if !prev_block_is_genesis && !self.core_processor.all_execution_results_exist(&prev_block) {
             tracing::debug!(target: "chunk_executor", %block_hash, %prev_block_hash, "missing execution results to allow validating receipts");
             return Ok(TryApplyChunksOutcome::NotReady);
         }
