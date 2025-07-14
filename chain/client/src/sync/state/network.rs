33c8271ded3d2a8520686457199b62394646d9b2
Saketh Are <saketh.are@gmail.com>
2025-07-14 13:09:32 -0400

    state_sync: fix attempt logic for parts (#13891)
    Similar to #13464, which fixed this logic for headers.
    
    The old code carries the assumption that the fallback source will work
    eventually. Rather we should continue making attempts to both sources
    until the needed part is obtained.
    
    The fix should be included to the 2.7 release.
    
    ---
    
    As a follow up, it should be possible to test this by adding a fake
    bucket in the state_sync_decentralized.py pytest. We can also introduce
    some random failures of the p2p requests to make sure the bucket gets
    hit (unsuccessfully).

--- chain/client/src/sync/state/network.rs
+++ chain/client/src/sync/state/network.rs
@@ -72,7 +72,7 @@ impl StateSyncDownloadSourcePeerSharedState {
         };
 
         let Some(request) = self.pending_requests.get(&key) else {
-            tracing::debug!(target: "sync", "Received {:?} expecting {:?}", key, self.pending_requests.keys());
+            tracing::debug!(target: "sync", "Received {:?} from {}", key, peer_id);
             return Err(near_chain::Error::Other("Unexpected state response".to_owned()));
         };
 
@@ -188,7 +188,10 @@ impl StateSyncDownloadSourcePeer {
             }
         };
 
-        let state_value = PendingPeerRequestValue { peer_id: Some(request_sent_to_peer), sender };
+        tracing::debug!(target: "sync", ?key, ?request_sent_to_peer, "p2p request sent");
+
+        let state_value =
+            PendingPeerRequestValue { peer_id: Some(request_sent_to_peer.clone()), sender };
 
         // Ensures that the key is removed from the map of pending requests when this scope exits,
         // whether on success or timeout.
@@ -202,6 +205,7 @@ impl StateSyncDownloadSourcePeer {
         select! {
             _ = clock.sleep_until(deadline) => {
                 increment_download_count(key.shard_id, typ, "network", "timeout");
+                tracing::debug!(target: "sync", ?key, ?request_sent_to_peer, "p2p request timed out");
                 Err(near_chain::Error::Other("Timeout".to_owned()))
             }
             _ = cancel.cancelled() => {
@@ -211,6 +215,7 @@ impl StateSyncDownloadSourcePeer {
             result = receiver => {
                 match result {
                     Ok(result) => {
+                        tracing::debug!(target: "sync", ?key, ?request_sent_to_peer, "p2p request succeeded");
                         increment_download_count(key.shard_id, typ, "network", "success");
                         Ok(result)
                     }
