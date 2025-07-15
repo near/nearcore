c09161b6add92422beab0af670102fc79b0a7586
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-15 17:11:58 +0800

    upd for last commit

--- chain/jsonrpc-primitives/src/types/status.rs
+++ chain/jsonrpc-primitives/src/types/status.rs
@@ -50,6 +50,7 @@ pub struct RpcDebugStatusResponse {
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
+#[serde(deny_unknown_fields)]
 pub struct RpcHealthResponse {}
 
 #[derive(thiserror::Error, Debug, Clone, serde::Serialize, serde::Deserialize)]
