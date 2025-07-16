346e6bcc5558ffb86faf6b8d96e6128cc1ed6ecf
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-16 20:36:09 +0800

    Revert "upd for last commit"
    This reverts commit c09161b6add92422beab0af670102fc79b0a7586.

--- chain/jsonrpc-primitives/src/types/status.rs
+++ chain/jsonrpc-primitives/src/types/status.rs
@@ -50,7 +50,6 @@ pub struct RpcDebugStatusResponse {
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
-#[serde(deny_unknown_fields)]
 pub struct RpcHealthResponse {}
 
 #[derive(thiserror::Error, Debug, Clone, serde::Serialize, serde::Deserialize)]
