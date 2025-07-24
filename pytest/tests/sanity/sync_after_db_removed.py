c0395b071490acaded3bd53afeccb088bc7ccff8
Slava Savenko <svyatoslav.savenko@nearone.org>
2025-07-24 15:24:13 +0200

    wip

--- pytest/tests/sanity/sync_after_db_removed.py
+++ pytest/tests/sanity/sync_after_db_removed.py
@@ -5,6 +5,7 @@
 
 import pathlib
 import sys
+import unittest
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / 'lib'))
 
@@ -17,73 +18,57 @@ EPOCH_LENGTH = 15
 TARGET_HEIGHT = int(EPOCH_LENGTH * 6)
 AFTER_SYNC_HEIGHT = EPOCH_LENGTH * 10
 
+class EpochSyncTest(unittest.TestCase):
 
-node_config = state_sync_lib.get_state_sync_config_p2p_no_external()
-# We're generating many blocks here - lower the min production delay to speed
-# up the test running time a little.
-node_config["consensus.min_block_production_delay"] = {
-    "secs": 0,
-    "nanos": 100000000
-}
-node_config["consensus.max_block_production_delay"] = {
-    "secs": 0,
-    "nanos": 300000000
-}
-node_config["consensus.max_block_wait_delay"] = {"secs": 0, "nanos": 600000000}
-node_config["consensus.block_fetch_horizon"] = 1
-node_config["gc_step_period"] = {"secs": 0, "nanos": 100000000}
-node_config["gc_blocks_limit"] = 3
+    def setUp(self):
+        pass
 
-nodes = start_cluster(
-    num_nodes=4,
-    num_observers=1,
-    num_shards=4,
-    config=None,
-    genesis_config_changes=[["epoch_length", EPOCH_LENGTH],
-           ["num_block_producer_seats_per_shard", [5]],
-           ["validators", 0, "amount", "60000000000000000000000000000000"],
-           ["block_producer_kickout_threshold", 50],
-           ["chunk_producer_kickout_threshold", 50],
-           [
-               "records", 0, "Account", "account", "locked",
-               "60000000000000000000000000000000"
-           ], ["total_supply", "5010000000000000000000000000000000"]],
-    client_config_changes={x: node_config for x in range(4)})
+    def test(self):
+        self.node_config = state_sync_lib.get_state_sync_config_p2p(None)
+        # We're generating many blocks here - lower the min production delay to speed
+        # up the test running time a little.
+        self.node_config["consensus.min_block_production_delay"] = {
+            "secs": 0,
+            "nanos": 100000000
+        }
+        self.node_config["consensus.max_block_production_delay"] = {
+            "secs": 0,
+            "nanos": 300000000
+        }
+        self.node_config["consensus.max_block_wait_delay"] = {"secs": 0, "nanos": 600000000}
+        self.node_config["consensus.block_fetch_horizon"] = 1
+        self.node_config["gc_step_period"] = {"secs": 0, "nanos": 100000000}
+        self.node_config["gc_blocks_limit"] = 3
 
-node0_height, _ = utils.wait_for_blocks(nodes[0], target=TARGET_HEIGHT)
+        self.nodes = start_cluster(
+            num_nodes=4,
+            num_observers=0,
+            num_shards=4,
+            config=None,
+            genesis_config_changes=[["epoch_length", EPOCH_LENGTH],
+                   ["num_block_producer_seats_per_shard", [1, 1, 1, 1]],
+                   ["validators", 0, "amount", "60000000000000000000000000000000"],
+                   ["block_producer_kickout_threshold", 50],
+                   ["chunk_producer_kickout_threshold", 50],
+                   [
+                       "records", 0, "Account", "account", "locked",
+                       "60000000000000000000000000000000"
+                   ], ["total_supply", "5010000000000000000000000000000000"]],
+            client_config_changes={x: self.node_config for x in range(4)})
+        
+        node0_height, _ = utils.wait_for_blocks(self.nodes[0], target=TARGET_HEIGHT)
 
-logger.info('Kill node 1')
-nodes[1].kill()
+        logger.info('Kill node 1')
+        self.nodes[1].kill()
+        
+  
+        # remove the state from node 1 here
 
-node0_height, _ = utils.wait_for_blocks(nodes[0], target=AFTER_SYNC_HEIGHT)
+        logger.info('Restart node 1')
+        self.nodes[1].start(boot_node=self.nodes[0])
+        
+        node1_height, _ = utils.wait_for_blocks(self.nodes[1], target=TARGET_HEIGHT + EPOCH_LENGTH + 1)
 
-logger.info('Restart node 1')
-nodes[1].start(boot_node=nodes[1])
+if __name__ == '__main__':
+    unittest.main()
 
-node1_height, _ = utils.wait_for_blocks(nodes[1], target=node0_height)
-
-epoch_id = nodes[1].json_rpc('block', [node1_height],
-                             timeout=15)['result']['header']['epoch_id']
-epoch_start_height = nodes[1].get_validators(
-    epoch_id=epoch_id)['result']['epoch_start_height']
-
-# all fresh data should be synced
-blocks_count = 0
-for height in range(max(node1_height - 10, epoch_start_height),
-                    node1_height + 1):
-    block0 = nodes[0].json_rpc('block', [height], timeout=15)
-    block1 = nodes[1].json_rpc('block', [height], timeout=15)
-    assert block0 == block1, (block0, block1)
-    if 'result' in block0:
-        blocks_count += 1
-assert blocks_count > 0
-
-# all old data should be GCed
-blocks_count = 0
-for height in range(1, EPOCH_LENGTH * 4):
-    block0 = nodes[0].json_rpc('block', [height], timeout=15)
-    block1 = nodes[1].json_rpc('block', [height], timeout=15)
-    assert block0 == block1, (block0, block1)
-    if 'result' in block0:
-        blocks_count += 1
-assert blocks_count == 0
