8e9906c2770190c2c34b93688fc5e86a615f5e33
Slava Savenko <svyatoslav.savenko@nearone.org>
2025-08-25 20:28:39 +0200

    wip. create outcome for each transaction in the processed chunk

--- runtime/runtime/src/tests/apply.rs
+++ runtime/runtime/src/tests/apply.rs
@@ -3044,7 +3044,7 @@ fn test_transaction_ordering_with_apply() {
 
     let txs = vec![
         bob_tx1.clone(),
-        alice_invalid_tx,
+        alice_invalid_tx.clone(),
         alice_tx1.clone(),
         bob_tx2.clone(),
         alice_tx2.clone(),
@@ -3072,22 +3072,35 @@ fn test_transaction_ordering_with_apply() {
         )
         .expect("apply should succeed");
 
-    let expected_order = vec![
-        bob_tx1.get_hash(),
-        alice_tx1.get_hash(),
-        bob_tx2.get_hash(),
-        alice_tx2.get_hash(),
-        bob_tx3.get_hash(),
-    ];
+    let expected_order = if ProtocolFeature::InvalidTxGenerateOutcomes.enabled(PROTOCOL_VERSION) {
+        vec![
+            bob_tx1.get_hash(),
+            alice_invalid_tx.get_hash(),
+            alice_tx1.get_hash(),
+            bob_tx2.get_hash(),
+            alice_tx2.get_hash(),
+            bob_tx3.get_hash(),
+        ]
+    } else {
+        vec![
+            bob_tx1.get_hash(),
+            alice_tx1.get_hash(),
+            bob_tx2.get_hash(),
+            alice_tx2.get_hash(),
+            bob_tx3.get_hash(),
+        ]
+    };
 
     // Note: The 3 local receipts are generated for valid transactions
     // where signer_id == receiver_id - tx2, tx4, tx6 (not for tx1 as it is dropped).
+    let num_outcomes = expected_order.len();
     assert_eq!(
         apply_result.outcomes.len(),
-        8,
-        "should have processed 5 transactions and 3 local receipts"
+        num_outcomes + 3,
+        "should have processed {num_outcomes} transactions and 3 local receipts"
     );
-    let tx_outcomes = apply_result.outcomes.iter().take(5).map(|o| o.id).collect::<Vec<_>>();
+    let tx_outcomes =
+        apply_result.outcomes.iter().take(num_outcomes).map(|o| o.id).collect::<Vec<_>>();
     assert_eq!(tx_outcomes, expected_order, "outcomes are not in expected sorted order");
 }
 
@@ -3195,9 +3208,19 @@ fn test_expired_transaction() {
             Default::default(),
         )
         .expect("apply should succeed");
-    assert_eq!(
-        apply_result.outcomes.len(),
-        0,
-        "should have not produced any outcomes for the expired tx"
-    );
+
+    if ProtocolFeature::InvalidTxGenerateOutcomes.enabled(PROTOCOL_VERSION) {
+        assert_eq!(
+            apply_result.outcomes.len(),
+            1,
+            "expired tx should produce an outcome with status `Failure`"
+        );
+        assert_matches!(apply_result.outcomes[0].outcome.status, ExecutionStatus::Failure(_));
+    } else {
+        assert_eq!(
+            apply_result.outcomes.len(),
+            0,
+            "should have not produced any outcomes for the expired tx"
+        );
+    }
 }
