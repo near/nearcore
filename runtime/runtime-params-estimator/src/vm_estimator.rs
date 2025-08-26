ec517ed27ad9631508859f51e5e9703b044aa114
Vadim Volodin <vad.e.volodin@gmail.com>
2025-08-26 15:52:58 +0800

    Update runtime/runtime-params-estimator/src/vm_estimator.rs
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>

--- runtime/runtime-params-estimator/src/vm_estimator.rs
+++ runtime/runtime-params-estimator/src/vm_estimator.rs
@@ -31,7 +31,7 @@ pub(crate) fn create_context(input: Vec<u8>) -> VMContext {
         account_locked_balance: 1u128,
         storage_usage: 12,
         attached_deposit: 2u128,
-        prepaid_gas: Gas::from_gas(10_u64.pow(18)),
+        prepaid_gas: Gas::from_tgas(1_000_000),
         random_seed: vec![0, 1, 2],
         view_config: None,
         output_data_receivers: vec![],
