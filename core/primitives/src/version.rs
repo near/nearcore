69d4df90d0b5bf24a44e673ee593ba366a0d6338
Adam Chudaś <adam@chudas.net>
2025-08-28 13:55:29 +0000

    voting

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -74,7 +74,7 @@ pub fn get_protocol_upgrade_schedule(chain_id: &str) -> ProtocolUpgradeVotingSch
         _ => {
             let v1_protocol_version = 80;
             let v1_datetime =
-                ProtocolUpgradeVotingSchedule::parse_datetime("2025-08-28 10:00:00").unwrap();
+                ProtocolUpgradeVotingSchedule::parse_datetime("2025-08-28 15:00:00").unwrap();
             let schedule = vec![(v1_datetime, v1_protocol_version)];
             schedule
         }
