b97399384fe5a156174004d8ffa93601bde34986
Razvan Barbascu <razvan@nearone.org>
2025-08-29 14:03:06 +0100

    refactor stake distribution

--- pytest/tests/mocknet/utils.py
+++ pytest/tests/mocknet/utils.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from retrying import retry
 from typing import Literal
 import requests
 
@@ -27,32 +28,46 @@ class StakeDistribution:
     def __init__(self, num_chunk_producers):
         self.num_chunk_producers = num_chunk_producers
         self.producer_idx = 0
+        self.validator_idx = self.num_chunk_producers
 
-    def get_next_validator_stake(self):
+    def _get_next_validator_stake(self):
         pass
 
-    def get_next_producer_stake(self):
+    def _get_next_producer_stake(self):
         pass
 
     def get_stake(self, node):
+        """
+        Get the next stake for the node.
+        If the node is a validator, the stake is the next validator stake.
+        If the node is a producer, the stake is the next producer stake.
+        If the node can be a producer but we already have enough producers,
+        the stake of a validator will be used instead.
+        """
         if node.role(
         ) == 'validator' or self.producer_idx >= self.num_chunk_producers:
-            amount = self.get_next_validator_stake()
+            amount = self._get_next_validator_stake()
+            self.validator_idx += 1
         else:
-            amount = self.get_next_producer_stake()
+            amount = self._get_next_producer_stake()
+            self.producer_idx += 1
         return amount
 
 
 class MainnetStakeDistribution(StakeDistribution):
     """
     Uses the mainnet validator stakes as the stake distribution.
-    To avoid going below the seat price, the stake of the bottom `double_after` validators is doubled.
+    To avoid going below the seat price, the stake of the bottom `DOUBLE_THRESHOLD` validators is doubled.
     If more validators are requested than available, the stake of the last validator is used for the remaining validators.
-    To avoid mainnet stake leaking to the mocknet, the stake is amplified by `multiplier`.
+    To avoid mainnet validators leaking to the mocknet, the stake is amplified by `MULTIPLIER`.
     """
 
+    DOUBLE_THRESHOLD = 100
+    MULTIPLIER = 100000
+
     @staticmethod
-    def get_mainnet_stakes():
+    @retry(wait_fixed=500, stop_max_attempt_number=6)
+    def _get_mainnet_stakes():
         """
         Makes an RPC call to mainnet to get current validator stakes
         """
@@ -73,37 +88,39 @@ class MainnetStakeDistribution(StakeDistribution):
             raise Exception(
                 f"Failed to get mainnet stakes: {response.status_code}")
 
+    def _set_mainnet_stakes(self):
+        self._mainnet_stakes = MainnetStakeDistribution._get_mainnet_stakes()
+        # Double the bottom stakes to avoid going below the seat price.
+        self._mainnet_stakes[-MainnetStakeDistribution.DOUBLE_THRESHOLD:] = [
+            s * 2 for s in
+            self._mainnet_stakes[-MainnetStakeDistribution.DOUBLE_THRESHOLD:]
+        ]
+        # Amplify the stakes to avoid mainnet validators leaking to the mocknet.
+        self._mainnet_stakes = [
+            s * MainnetStakeDistribution.MULTIPLIER
+            for s in self._mainnet_stakes
+        ]
+        self._mainnet_stakes.sort(reverse=True)
+
     def __init__(self, num_chunk_producers):
         super().__init__(num_chunk_producers)
-        self.mainnet_stakes = MainnetStakeDistribution.get_mainnet_stakes()
-        self.num_validators = len(self.mainnet_stakes)
-        self.validator_idx = self.num_chunk_producers
-        self.double_after = 100
-        self.multiplier = 100000
+        self._set_mainnet_stakes()
 
-    def get_next_validator_stake(self):
-        if self.validator_idx >= self.num_validators:
-            stake = self.mainnet_stakes[-1]
-        else:
-            stake = self.mainnet_stakes[self.validator_idx]
-            self.validator_idx += 1
-        if self.validator_idx >= self.num_validators - self.double_after:
-            stake = stake * 2
-        return stake * self.multiplier
+    def _get_next_validator_stake(self):
+        idx = min(self.validator_idx, len(self._mainnet_stakes) - 1)
+        return self._mainnet_stakes[idx]
 
-    def get_next_producer_stake(self):
-        stake = self.mainnet_stakes[self.producer_idx]
-        self.producer_idx += 1
-        return stake * self.multiplier
+    def _get_next_producer_stake(self):
+        idx = min(self.producer_idx, len(self._mainnet_stakes) - 1)
+        return self._mainnet_stakes[idx]
 
 
 class StaticStakeDistribution(StakeDistribution):
 
-    def get_next_validator_stake(self):
+    def _get_next_validator_stake(self):
         return 10**32
 
-    def get_next_producer_stake(self):
-        self.producer_idx += 1
+    def _get_next_producer_stake(self):
         return 10**33
 
 
