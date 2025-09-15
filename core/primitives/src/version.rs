882e1d016cb9cd7426b8136b119e688323d379d1
Stefan Neamtu <stefan.neamtu@gmail.com>
2025-09-15 12:57:38 +0900

    voting date

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -60,10 +60,10 @@ pub fn get_protocol_upgrade_schedule(chain_id: &str) -> ProtocolUpgradeVotingSch
             schedule
         }
         TESTNET => {
-            // Voting starts Tuesday 14:00 UTC.
+            // Voting starts Monday 21:00 UTC.
             let v1_protocol_version = 80;
             let v1_datetime =
-                ProtocolUpgradeVotingSchedule::parse_datetime("2025-09-09 14:00:00").unwrap();
+                ProtocolUpgradeVotingSchedule::parse_datetime("2025-09-22 21:00:00").unwrap();
             let schedule = vec![(v1_datetime, v1_protocol_version)];
             schedule
         }
