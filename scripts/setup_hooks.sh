ce211c8296f8cc12ca443acbd9b9dbc995d291f9
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-09-17 10:09:26 +0300

    fix: hook setup quirks

--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
-ln -s -f ${DIR}/pre-commit ${HOOK_DIR}
+ln -s -f "${DIR}/pre-commit" "${HOOK_DIR}/pre-commit"
