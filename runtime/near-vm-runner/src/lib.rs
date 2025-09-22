d47e88273def3239ab55e3a417b4293a3ed58473
Roman Volosatovs <roman@cosmonic.com>
2025-09-22 14:44:58 +0200

    runtime: re-export old wasmparser
    Signed-off-by: Roman Volosatovs <roman@cosmonic.com>

--- runtime/near-vm-runner/src/lib.rs
+++ runtime/near-vm-runner/src/lib.rs
@@ -55,7 +55,7 @@ pub(crate) const EXPORT_PREFIX: &str = "\0";
 pub mod internal {
     pub use crate::runner::VMKindExt;
     #[cfg(feature = "prepare")]
-    pub use wasmparser_latest as wasmparser;
+    pub use wasmparser;
 }
 
 /// Drop something somewhat lazily.
