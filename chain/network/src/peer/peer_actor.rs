2808fd98e578f39370a1e195fcb55c92d6a30f02
robin-near <robin@nearone.org>
2025-09-29 18:38:31 +0000

    Fix CI.

--- chain/network/src/peer/peer_actor.rs
+++ chain/network/src/peer/peer_actor.rs
@@ -38,7 +38,7 @@ use lru::LruCache;
 use near_async::futures::{DelayedActionRunner, DelayedActionRunnerExt, FutureSpawnerExt};
 use near_async::messaging::{self, CanSend, IntoSender, SendAsync};
 use near_async::tokio::TokioRuntimeHandle;
-use near_async::{time, ActorSystem, Message};
+use near_async::{ActorSystem, Message, time};
 use near_crypto::Signature;
 use near_o11y::log_assert;
 use near_o11y::span_wrapped_msg::{SpanWrapped, SpanWrappedMessageExt};
