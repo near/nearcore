cab120df688da9ebc0eb2fc4797b436d926d3a29
Adam Chudaś <adam@chudas.net>
2025-09-30 08:11:24 +0000

    wip

--- core/store/src/archive/cloud_storage/mod.rs
+++ core/store/src/archive/cloud_storage/mod.rs
@@ -1,5 +1,8 @@
+pub(crate) mod block_data;
 pub mod opener;
 pub mod update;
 
 /// Represents the external storage for archival data.
-pub struct CloudStorage;
+pub struct CloudStorage {
+    //connection: ExternalConnection,
+}
