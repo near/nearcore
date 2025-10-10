044dc9bdc532dcb9a5b6ffa901753b657f353cfd
Shreyan Gupta <shreyan.gupta96@gmail.com>
2025-10-09 23:44:42 -0400

    [limited_replayability] Deprecate ReducedGasRefunds protocol feature (#14403)
    Additionally adding asserts in the codebase to check
    `refund_gas_price_changes` is always false.

--- chain/indexer/src/streamer/utils.rs
+++ chain/indexer/src/streamer/utils.rs
@@ -1,6 +1,5 @@
 use near_indexer_primitives::IndexerTransactionWithOutcome;
 use near_parameters::RuntimeConfig;
-use near_primitives::types::ProtocolVersion;
 use near_primitives::views;
 use node_runtime::config::tx_cost;
 
@@ -13,7 +12,6 @@ pub(crate) async fn convert_transactions_sir_into_local_receipts(
     runtime_config: &RuntimeConfig,
     txs: Vec<&IndexerTransactionWithOutcome>,
     block: &views::BlockView,
-    protocol_version: ProtocolVersion,
 ) -> Result<Vec<views::ReceiptView>, FailedToFetchData> {
     if txs.is_empty() {
         return Ok(vec![]);
@@ -44,8 +42,7 @@ pub(crate) async fn convert_transactions_sir_into_local_receipts(
                 },
             );
             // Can't use ValidatedTransaction here because transactions in a chunk can be invalid (RelaxedChunkValidation feature)
-            let cost =
-                tx_cost(&runtime_config, &tx, prev_block_gas_price, protocol_version).unwrap();
+            let cost = tx_cost(&runtime_config, &tx, prev_block_gas_price).unwrap();
             views::ReceiptView {
                 predecessor_id: indexer_tx.transaction.signer_id.clone(),
                 receiver_id: indexer_tx.transaction.receiver_id.clone(),
