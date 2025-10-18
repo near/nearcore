14ea3bec0f0197de40335a1f64fa36abf2c0bf77
Adam Chudaś <adam@chudas.net>
2025-10-18 12:53:35 +0000

    prefetch db

--- core/store/src/archive/cloud_storage/mod.rs
+++ core/store/src/archive/cloud_storage/mod.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+
 use near_external_storage::ExternalConnection;
+
+use crate::db::Database;
 
 pub mod config;
 pub mod download;
@@ -11,4 +15,5 @@ pub(super) mod file_id;
 /// Handles operations related to cloud storage used for archival data.
 pub struct CloudStorage {
     external: ExternalConnection,
+    pub(crate) prefetch_db: Option<Arc<dyn Database>>,
 }
