f4369082a8e0c59deb168a904b37f1caeb965b84
Attila Toth <attila.toth@nearone.org>
2025-10-20 17:40:10 +0200

    Comment out more indexer code.

--- chain/indexer/src/streamer/mod.rs
+++ chain/indexer/src/streamer/mod.rs
@@ -184,25 +184,25 @@ pub async fn build_streamer_message(
         });
     }
 
-    // Ideally we expect `shards_outcomes` to be empty by this time, but if something went wrong with
-    // chunks and we end up with non-empty `shards_outcomes` we want to be sure we put them into IndexerShard
-    // That might happen before the fix https://github.com/near/nearcore/pull/4228
-    for (shard_id, outcomes) in shards_outcomes {
-        // The chunk may be missing and if that happens in the first block after
-        // resharding the shard id would no longer be valid in the new shard
-        // layout. In this case we can skip the chunk.
-        let shard_index = protocol_config_view.shard_layout.get_shard_index(shard_id);
-        let Ok(shard_index) = shard_index else {
-            continue;
-        };
+    // // Ideally we expect `shards_outcomes` to be empty by this time, but if something went wrong with
+    // // chunks and we end up with non-empty `shards_outcomes` we want to be sure we put them into IndexerShard
+    // // That might happen before the fix https://github.com/near/nearcore/pull/4228
+    // for (shard_id, outcomes) in shards_outcomes {
+    //     // The chunk may be missing and if that happens in the first block after
+    //     // resharding the shard id would no longer be valid in the new shard
+    //     // layout. In this case we can skip the chunk.
+    //     let shard_index = protocol_config_view.shard_layout.get_shard_index(shard_id);
+    //     let Ok(shard_index) = shard_index else {
+    //         continue;
+    //     };
 
-        indexer_shards[shard_index].receipt_execution_outcomes.extend(outcomes.into_iter().map(
-            |outcome| IndexerExecutionOutcomeWithReceipt {
-                execution_outcome: outcome.execution_outcome,
-                receipt: outcome.receipt.expect("`receipt` must be present at this moment"),
-            },
-        ))
-    }
+    //     indexer_shards[shard_index].receipt_execution_outcomes.extend(outcomes.into_iter().map(
+    //         |outcome| IndexerExecutionOutcomeWithReceipt {
+    //             execution_outcome: outcome.execution_outcome,
+    //             receipt: outcome.receipt.expect("`receipt` must be present at this moment"),
+    //         },
+    //     ))
+    // }
 
     Ok(StreamerMessage { block, shards: indexer_shards })
 }
