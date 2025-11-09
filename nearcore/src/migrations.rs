ecc7107cc75d7a6b1cf58f16a95c54ae5019cf2b
Shreyan Gupta <shreyan.gupta96@gmail.com>
2025-11-09 22:32:57 +0530

    build error

--- nearcore/src/migrations.rs
+++ nearcore/src/migrations.rs
@@ -2,6 +2,7 @@ use near_store::Store;
 use near_store::db::metadata::{DB_VERSION, DbVersion};
 
 pub(super) struct Migrator<'a> {
+    #[allow(dead_code)]
     config: &'a crate::config::NearConfig,
 }
 
@@ -15,12 +16,11 @@ impl<'a> near_store::StoreMigrator for Migrator<'a> {
     fn check_support(&self, version: DbVersion) -> Result<(), &'static str> {
         match version {
             0..=45 => Err("1.45"),
-            46..DB_VERSION => Ok(()),
             _ => unreachable!(),
         }
     }
 
-    fn migrate(&self, store: &Store, version: DbVersion) -> anyhow::Result<()> {
+    fn migrate(&self, _store: &Store, version: DbVersion) -> anyhow::Result<()> {
         match version {
             0..=45 => unreachable!(),
             DB_VERSION.. => unreachable!(),
