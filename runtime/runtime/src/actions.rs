f423f673d8acd7801c4910dd6d8e5ce7171337c4
Darioush Jalali <darioush.jalali@nearone.org>
2025-11-12 12:28:23 -0800

    nit: reference Nonce in storage cost calculation

--- runtime/runtime/src/actions.rs
+++ runtime/runtime/src/actions.rs
@@ -23,7 +23,7 @@ use near_primitives::transaction::{
 };
 use near_primitives::types::validator_stake::ValidatorStake;
 use near_primitives::types::{
-    AccountId, Balance, BlockHeight, EpochInfoProvider, Gas, NonceIndex, StorageUsage,
+    AccountId, Balance, BlockHeight, EpochInfoProvider, Gas, Nonce, NonceIndex, StorageUsage,
 };
 use near_primitives::utils::account_is_implicit;
 use near_primitives::version::ProtocolVersion;
@@ -907,7 +907,7 @@ fn gas_key_storage_cost(
     let storage_config = &fee_config.storage_usage_config;
     let nonce_storage_usage = gas_key.num_nonces as u64
         * (borsh::object_length(&(0 as NonceIndex)).unwrap() as u64 + // NonceIndex is part of the key
-            borsh::object_length(&0u64).unwrap() as u64 + // Value of nonce
+            borsh::object_length(&(0 as Nonce)).unwrap() as u64 + // Value of nonce
             storage_config.num_extra_bytes_record);
 
     borsh::object_length(public_key).unwrap() as u64
