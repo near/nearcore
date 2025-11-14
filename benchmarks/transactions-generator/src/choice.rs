7c1f5ebec7d3fd72e0b0b6000ba78828e0aa3d37
Slava Savenko <svyatoslav.savenko@nearone.org>
2025-11-14 16:17:09 +0100

    add recevier_ids into tx-generator

--- benchmarks/transactions-generator/src/choice.rs
+++ benchmarks/transactions-generator/src/choice.rs
@@ -1,21 +1,55 @@
-pub(crate) struct Choice {
+pub(crate) struct TxAccountsSelector {
     sender: ZipfMandelbrot,
     receiver: ZipfMandelbrot,
+    receivers_from_senders_ratio: f64,
 }
 
-impl Choice {
-    pub(crate) fn new(num_accounts: usize, sender_skew: f64, receiver_skew: f64) -> Self {
+pub(crate) enum FromSendersOrReceivers {
+    FromSenders(usize),
+    FromReceivers(usize),
+}
+
+pub(crate) struct Choice {
+    pub sender_idx: usize,
+    pub receiver_idx: FromSendersOrReceivers,
+}
+
+impl TxAccountsSelector {
+    pub(crate) fn new(
+        num_accounts: usize,
+        num_receiver_accounts: usize,
+        sender_skew: f64,
+        receiver_skew: f64,
+        receivers_from_senders_ratio: f64,
+    ) -> Self {
         let q = 2.7;
+        tracing::info!(receivers_from_senders_ratio, "Created TxAccountsSelector");
         Self {
             sender: ZipfMandelbrot::new(num_accounts - 1, sender_skew, q),
-            receiver: ZipfMandelbrot::new(num_accounts - 1, receiver_skew, q),
+            receiver: ZipfMandelbrot::new(num_receiver_accounts - 1, receiver_skew, q),
+            receivers_from_senders_ratio,
         }
     }
 
-    pub(crate) fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> (usize, usize) {
+    pub(crate) fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> Choice {
         let sender_idx = self.sender.sample(rng) as usize;
+        if rng.gen_range(0. ..1.0) < self.receivers_from_senders_ratio {
+            // choose receiver from senders
+            let mut receiver_idx = self.sender.sample(rng) as usize;
+            while receiver_idx == sender_idx {
+                receiver_idx = self.sender.sample(rng) as usize;
+            }
+            return Choice {
+                sender_idx,
+                receiver_idx: FromSendersOrReceivers::FromSenders(receiver_idx),
+            };
+        }
         let receiver_idx = self.receiver.sample(rng) as usize;
-        (sender_idx, receiver_idx)
+
+        return Choice {
+            sender_idx,
+            receiver_idx: FromSendersOrReceivers::FromReceivers(receiver_idx),
+        };
     }
 }
 
