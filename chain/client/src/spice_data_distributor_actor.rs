2d27942749ad88b1c46410b460fc69f1c34d50ab
Attila Toth <attila.toth@nearone.org>
2025-11-17 16:09:28 +0100

    Fix variable name.

--- chain/client/src/spice_data_distributor_actor.rs
+++ chain/client/src/spice_data_distributor_actor.rs
@@ -830,7 +830,7 @@ impl SpiceDataDistributorActor {
             }
         }
 
-        let shards_we_apply_in_prev_block: HashSet<ShardId> = shard_layout
+        let shards_we_apply_in_next_block: HashSet<ShardId> = shard_layout
             .shard_ids()
             .filter(|shard_id| {
                 let prev_hash = block.hash();
@@ -844,11 +844,10 @@ impl SpiceDataDistributorActor {
 
         for from_shard_id in shard_layout.shard_ids() {
             // We need a receipts from a block only if we would want to apply a block after.
-            if shards_we_apply_in_prev_block.contains(&from_shard_id) {
+            if shards_we_apply_in_next_block.contains(&from_shard_id) {
                 continue;
             }
-            // TODO(spice-resharding): Handle resharding
-            for to_shard_id in shards_we_apply.iter().copied() {
+            for to_shard_id in shards_we_apply_in_next_block.iter().copied() {
                 new_ids.push(SpiceDataIdentifier::ReceiptProof {
                     block_hash: *block_hash,
                     from_shard_id,
