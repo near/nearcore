bb0b69d2794103d549fb2aec20998dbda521a114
Darioush Jalali <darioush.jalali@nearone.org>
2025-11-18 13:47:16 -0800

    feat(gas-keys): view queries (#14595)
    Adds view queries for GasKeys, mostly boilerplate.
    These are a prerequisite for test-loop tests for GasKeys.
    
    original draft: https://github.com/near/nearcore/pull/13792
    
    ---------
    
    Co-authored-by: robin-near <robin@nearone.org>

--- chain/chain/src/runtime/mod.rs
+++ chain/chain/src/runtime/mod.rs
@@ -31,8 +31,8 @@ use near_primitives::types::{
 };
 use near_primitives::version::ProtocolVersion;
 use near_primitives::views::{
-    AccessKeyInfoView, CallResult, ContractCodeView, QueryRequest, QueryResponse,
-    QueryResponseKind, ViewStateResult,
+    AccessKeyInfoView, CallResult, ContractCodeView, GasKeyInfoView, GasKeyList, GasKeyView,
+    QueryRequest, QueryResponse, QueryResponseKind, ViewStateResult,
 };
 use near_store::adapter::{StoreAdapter, StoreUpdateAdapter};
 use near_store::db::CLOUD_HEAD_KEY;
@@ -1176,6 +1176,37 @@ impl RuntimeAdapter for NightshadeRuntime {
                     block_hash: *block_hash,
                 })
             }
+            QueryRequest::ViewGasKeyList { account_id } => {
+                let gas_key_list =
+                    self.view_gas_keys(&shard_uid, *state_root, account_id).map_err(|err| {
+                        crate::near_chain_primitives::error::QueryError::from_view_gas_key_error(
+                            err,
+                            block_height,
+                            *block_hash,
+                        )
+                    })?;
+                Ok(QueryResponse {
+                    kind: QueryResponseKind::GasKeyList(GasKeyList { keys: gas_key_list }),
+                    block_height,
+                    block_hash: *block_hash,
+                })
+            }
+            QueryRequest::ViewGasKey { account_id, public_key } => {
+                let gas_key = self
+                    .view_gas_key(&shard_uid, *state_root, account_id, public_key)
+                    .map_err(|err| {
+                        crate::near_chain_primitives::error::QueryError::from_view_gas_key_error(
+                            err,
+                            block_height,
+                            *block_hash,
+                        )
+                    })?;
+                Ok(QueryResponse {
+                    kind: QueryResponseKind::GasKey(gas_key),
+                    block_height,
+                    block_hash: *block_hash,
+                })
+            }
             QueryRequest::ViewGlobalContractCode { code_hash } => self
                 .query_view_global_contract_code(
                     GlobalContractIdentifier::CodeHash(*code_hash),
@@ -1522,6 +1553,27 @@ impl node_runtime::adapter::ViewRuntimeAdapter for NightshadeRuntime {
     {
         let state_update = self.tries.new_trie_update_view(*shard_uid, state_root);
         self.trie_viewer.view_access_keys(&state_update, account_id)
+    }
+
+    fn view_gas_key(
+        &self,
+        shard_uid: &ShardUId,
+        state_root: MerkleHash,
+        account_id: &AccountId,
+        public_key: &PublicKey,
+    ) -> Result<GasKeyView, node_runtime::state_viewer::errors::ViewGasKeyError> {
+        let state_update = self.tries.new_trie_update_view(*shard_uid, state_root);
+        self.trie_viewer.view_gas_key(&state_update, account_id, public_key)
+    }
+
+    fn view_gas_keys(
+        &self,
+        shard_uid: &ShardUId,
+        state_root: MerkleHash,
+        account_id: &AccountId,
+    ) -> Result<Vec<GasKeyInfoView>, node_runtime::state_viewer::errors::ViewGasKeyError> {
+        let state_update = self.tries.new_trie_update_view(*shard_uid, state_root);
+        self.trie_viewer.view_gas_keys(&state_update, account_id)
     }
 
     fn view_state(
