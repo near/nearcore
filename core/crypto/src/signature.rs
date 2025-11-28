0a83dde650b832c0f466d39ee33212ff0fff731a
Snezhkko <snezhkodaria38@gmail.com>
2025-11-28 18:05:27 +0000

    chore: Use serde::Serializer::collect_str to avoid String allocations

--- core/crypto/src/signature.rs
+++ core/crypto/src/signature.rs
@@ -681,7 +681,7 @@ impl serde::Serialize for Signature {
     where
         S: serde::Serializer,
     {
-        serializer.serialize_str(&self.to_string())
+        serializer.collect_str(self)
     }
 }
 
