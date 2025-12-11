37c0ac7c836c1f8299a51b6d0f852cf6b935855a
Adam Chudaś <18039094+staffik@users.noreply.github.com>
2025-12-11 13:59:27 +0100

    feat(cloud_archive): upload state snapshots (#14774)
    **Changes**
    - Enable the state dumper when the cloud archive writer is configured.
    Disallow configuring both state sync/dump and cloud archive
    simultaneously.
    - Unify file naming between state sync and cloud archive, use `chain_id`
    as a prefix.
    - Add a method to retrieve the state header from cloud storage. This
    will be used in the next PR, which will test the changes introduced
    here.
    
    **Follow-ups**
    - Establish a single source of truth for determining the location of
    state parts/headers used by both state sync and cloud archive.
    - Add a configuration option for controlling the state snapshot
    frequency, since we do not want to create snapshots for every epoch.

--- nearcore/src/config_validate.rs
+++ nearcore/src/config_validate.rs
@@ -233,6 +233,13 @@ impl<'a> ConfigValidator<'a> {
             }
             return;
         };
+        if self.config.state_sync.is_some() {
+            let error_message =
+                "State sync/dump cannot be configured when cloud archive is enabled; \
+                dump settings are derived from the cloud archival config."
+                    .to_string();
+            self.validation_errors.push_config_semantics_error(error_message);
+        }
         if !CloudStorageOpener::is_storage_location_supported(&cloud_archival_config.location) {
             let error_message = format!(
                 "{} is not supported cloud storage location.",
