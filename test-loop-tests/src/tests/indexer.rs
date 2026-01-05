near/nearcore

commit f9059b706dfb4b8cda1219159391e60d48cff84f

fix(indexer): properly handle failed local transactions (#14426)
Currently indexer tries to deduce local receipts using chunk
transactions and execution outcomes. Particularly it uses the following
code to get `receipt_id`:
```
                receipt_id: *indexer_tx
                    .outcome
                    .execution_outcome
                    .outcome
                    .receipt_ids
                    .first()
                    .expect("The transaction ExecutionOutcome should have one receipt id in vec"),
```
With #14142 we now create execution outcomes for invalid transactions.
It means that code above will fail in that case because `receipt_ids`
will be empty.
This PR fixes the issue by looking at `ExecutionOutcome` `status`
instead. It also includes a bunch of refactoring to avoid unnecessary
`Transaction` creation and introduces `Receipt::from_tx` to make sure
future changes to receipt creation from transaction are reflected in the
indexer code.

Part of #14109.

chain/indexer/src/streamer/utils.rs
@@ -1,8 +1,11 @@
 use near_indexer_primitives::IndexerTransactionWithOutcome;
 use near_parameters::RuntimeConfig;
-use near_primitives::views;
-use node_runtime::config::tx_cost;
+use near_primitives::action::Action;
+use near_primitives::receipt::Receipt;
+use near_primitives::views::{self, ExecutionStatusView};
+use node_runtime::config::calculate_tx_cost;
 
+use crate::INDEXER;
 use crate::streamer::IndexerViewClientFetcher;
 
 use super::errors::FailedToFetchData;
@@ -19,53 +22,42 @@ pub(crate) async fn convert_transactions_sir_into_local_receipts(
     let prev_block = client.fetch_block(block.header.prev_hash).await?;
     let prev_block_gas_price = prev_block.header.gas_price;
 
-    let local_receipts: Vec<views::ReceiptView> = txs
-        .into_iter()
-        .map(|indexer_tx| {
-            assert_eq!(indexer_tx.transaction.signer_id, indexer_tx.transaction.receiver_id);
-            let tx = near_primitives::transaction::Transaction::V0(
-                near_primitives::transaction::TransactionV0 {
-                    signer_id: indexer_tx.transaction.signer_id.clone(),
-                    public_key: indexer_tx.transaction.public_key.clone(),
-                    nonce: indexer_tx.transaction.nonce,
-                    receiver_id: indexer_tx.transaction.receiver_id.clone(),
-                    block_hash: block.header.hash,
-                    actions: indexer_tx
-                        .transaction
-                        .actions
-                        .clone()
-                        .into_iter()
-                        .map(|action| {
-                            near_primitives::transaction::Action::try_from(action).unwrap()
-                        })
-                        .collect(),
-                },
+    let mut local_receipts = Vec::new();
+    for indexer_tx in txs {
+        let tx = &indexer_tx.transaction;
+        assert_eq!(tx.signer_id, tx.receiver_id);
+        let outcome = &indexer_tx.outcome.execution_outcome.outcome;
+        let ExecutionStatusView::SuccessReceiptId(receipt_id) = outcome.status else {
+            tracing::debug!(
+                target: INDEXER,
+                block_hash = %block.header.hash,
+                tx_hash = %tx.hash,
+                status = ?outcome.status,
+                "skip failed local tx",
             );
-            // Can't use ValidatedTransaction here because transactions in a chunk can be invalid (RelaxedChunkValidation feature)
-            let cost = tx_cost(&runtime_config, &tx, prev_block_gas_price).unwrap();
-            views::ReceiptView {
-                predecessor_id: indexer_tx.transaction.signer_id.clone(),
-                receiver_id: indexer_tx.transaction.receiver_id.clone(),
-                receipt_id: *indexer_tx
-                    .outcome
-                    .execution_outcome
-                    .outcome
-                    .receipt_ids
-                    .first()
-                    .expect("The transaction ExecutionOutcome should have one receipt id in vec"),
-                receipt: views::ReceiptEnumView::Action {
-                    signer_id: indexer_tx.transaction.signer_id.clone(),
-                    signer_public_key: indexer_tx.transaction.public_key.clone(),
-                    gas_price: cost.receipt_gas_price,
-                    output_data_receivers: vec![],
-                    input_data_ids: vec![],
-                    actions: indexer_tx.transaction.actions.clone(),
-                    is_promise_yield: false,
-                },
-                priority: 0,
-            }
-        })
-        .collect();
+            continue;
+        };
+        let actions: Vec<_> =
+            tx.actions.iter().cloned().map(Action::try_from).map(Result::unwrap).collect();
+        let cost = calculate_tx_cost(
+            &tx.receiver_id,
+            &tx.signer_id,
+            &actions,
+            &runtime_config,
+            prev_block_gas_price,
+        )
+        .unwrap();
+        let receipt = Receipt::from_tx(
+            receipt_id,
+            tx.signer_id.clone(),
+            tx.receiver_id.clone(),
+            tx.public_key.clone(),
+            cost.receipt_gas_price,
+            actions,
+        );
+        let receipt_view: views::ReceiptView = receipt.into();
+        local_receipts.push(receipt_view);
+    }
 
     Ok(local_receipts)
 }

test-loop-tests/src/tests/indexer.rs
@@ -1,39 +1,148 @@
+use assert_matches::assert_matches;
 use near_async::futures::FutureSpawnerExt;
+use near_async::messaging::CanSend;
 use near_async::time::Duration;
-use near_indexer::{AwaitForNodeSyncedEnum, IndexerConfig, SyncModeEnum, start};
+use near_client::client_actor::AdvProduceChunksMode;
+use near_client::{NetworkAdversarialMessage, ProcessTxRequest};
+use near_indexer::{AwaitForNodeSyncedEnum, IndexerConfig, StreamerMessage, SyncModeEnum, start};
 use near_o11y::testonly::init_test_logger;
+use near_primitives::gas::Gas;
 use near_primitives::shard_layout::ShardLayout;
-use near_primitives::types::Finality;
+use near_primitives::test_utils::create_user_test_signer;
+use near_primitives::transaction::SignedTransaction;
+use near_primitives::types::{AccountId, Balance, Finality, NumBlocks};
+use near_primitives::version::{PROTOCOL_VERSION, ProtocolFeature};
+use near_primitives::views::ExecutionStatusView;
 use near_store::StoreConfig;
+use tokio::sync::mpsc;
 
 use crate::setup::builder::TestLoopBuilder;
+use crate::setup::env::TestLoopEnv;
 use crate::setup::state::NodeExecutionData;
-use crate::utils::account::{create_validators_spec, validators_spec_clients};
+use crate::utils::account::{
+    create_account_id, create_validators_spec, validators_spec_clients_with_rpc,
+};
 use crate::utils::node::TestLoopNode;
 
 #[test]
 fn test_indexer_basic() {
     init_test_logger();
 
+    let mut env = setup();
+    let start_block_height = 1;
+    let last_block_height = 5;
+    let mut indexer_receiver = start_indexer(&env, SyncModeEnum::BlockHeight(start_block_height));
+    for expected_height in start_block_height..=last_block_height {
+        let msg = receive_indexer_message(&mut env, &mut indexer_receiver);
+        assert_eq!(msg.block.header.height, expected_height);
+    }
+
+    shutdown(env);
+}
+
+#[test]
+fn test_indexer_local_receipt() {
+    init_test_logger();
+
+    let mut env = setup();
+    let tx = create_local_tx(&env);
+    let rpc_node = TestLoopNode::rpc(&env.node_datas);
+    let submit_tx_height = rpc_node.head(env.test_loop_data()).height;
+    let outcome = rpc_node.execute_tx(&mut env.test_loop, tx, Duration::seconds(5)).unwrap();
+    let tx_outcome_status = outcome.transaction_outcome.outcome.status;
+    let ExecutionStatusView::SuccessReceiptId(receipt_id) = tx_outcome_status else {
+        panic!("failed to convert transaction to receipt {tx_outcome_status:?}");
+    };
+    assert_eq!(outcome.receipts_outcome.len(), 1);
+    let receipt_outcome = &outcome.receipts_outcome[0];
+
+    let tx_included_height = submit_tx_height + 3;
+    let mut indexer_receiver = start_indexer(&env, SyncModeEnum::BlockHeight(tx_included_height));
+    let msg = receive_indexer_message(&mut env, &mut indexer_receiver);
+    let indexer_shard = &msg.shards[0];
+    let chunk_view = indexer_shard.chunk.as_ref().unwrap();
+    assert_eq!(chunk_view.transactions.len(), 1);
+    assert_eq!(indexer_shard.receipt_execution_outcomes.len(), 1);
+    let receipt_execution_outcome = &indexer_shard.receipt_execution_outcomes[0];
+    assert_eq!(receipt_execution_outcome.receipt.receipt_id, receipt_id);
+    assert_eq!(&receipt_execution_outcome.execution_outcome, receipt_outcome);
+
+    shutdown(env);
+}
+
+#[test]
+fn test_indexer_failed_local_tx() {
+    init_test_logger();
+    if !ProtocolFeature::InvalidTxGenerateOutcomes.enabled(PROTOCOL_VERSION) {
+        return;
+    }
+
+    let mut env = setup();
+    let validator_node = TestLoopNode::validator(&env.node_datas, 0);
+    validator_node.send_adversarial_message(
+        &env.test_loop,
+        NetworkAdversarialMessage::AdvProduceChunks(AdvProduceChunksMode::ProduceWithoutTx),
+    );
+    let tx = create_local_tx(&env);
+    let process_tx_request =
+        ProcessTxRequest { transaction: tx, is_forwarded: false, check_only: false };
+    validator_node.data().rpc_handler_sender.send(process_tx_request);
+    // Wait for the transaction to expire. This will happen because the chunk producer
+    // does not include any transactions in the chunks (enabled by the adversarial message above).
+    validator_node.run_for_number_of_blocks(&mut env.test_loop, (TX_VALIDITY_PERIOD + 1) as usize);
+    // Make chunk producer include the transaction without checking the validity period.
+    // This effectively results in invalid transaction included in the chunk.
+    validator_node.send_adversarial_message(
+        &env.test_loop,
+        NetworkAdversarialMessage::AdvProduceChunks(
+            AdvProduceChunksMode::ProduceWithoutTxValidityCheck,
+        ),
+    );
+
+    let tx_included_height = validator_node.head(env.test_loop_data()).height + 2;
+    let mut indexer_receiver = start_indexer(&env, SyncModeEnum::BlockHeight(tx_included_height));
+    let msg = receive_indexer_message(&mut env, &mut indexer_receiver);
+    let indexer_shard = &msg.shards[0];
+    let chunk_view = indexer_shard.chunk.as_ref().unwrap();
+    assert_eq!(chunk_view.transactions.len(), 1);
+    let outcome = &chunk_view.transactions[0].outcome.execution_outcome.outcome;
+    assert_matches!(outcome.status, ExecutionStatusView::Failure(_));
+    assert!(outcome.receipt_ids.is_empty());
+    assert!(indexer_shard.receipt_execution_outcomes.is_empty());
+
+    shutdown(env);
+}
+
+fn user_account() -> AccountId {
+    create_account_id("user")
+}
+
+const TX_VALIDITY_PERIOD: NumBlocks = 5;
+
+fn setup() -> TestLoopEnv {
     let validators_spec = create_validators_spec(1, 0);
-    let clients = validators_spec_clients(&validators_spec);
+    let clients = validators_spec_clients_with_rpc(&validators_spec);
     let genesis = TestLoopBuilder::new_genesis_builder()
         .shard_layout(ShardLayout::single_shard())
         .validators_spec(validators_spec)
+        .add_user_account_simple(user_account(), Balance::from_near(1))
+        .transaction_validity_period(TX_VALIDITY_PERIOD)
         .build();
-    let mut env = TestLoopBuilder::new()
+    let env = TestLoopBuilder::new()
         .genesis(genesis)
         .epoch_config_store_from_genesis()
         .clients(clients)
         .build()
         .warmup();
+    env
+}
 
+fn start_indexer(env: &TestLoopEnv, sync_mode: SyncModeEnum) -> mpsc::Receiver<StreamerMessage> {
     let node_data = &env.node_datas[0];
     let node = TestLoopNode::for_account(&env.node_datas, &node_data.account_id);
-    let start_block_height = 1;
     let indexer_config = IndexerConfig {
         home_dir: NodeExecutionData::homedir(&env.shared_state.tempdir, &node_data.identifier),
-        sync_mode: SyncModeEnum::BlockHeight(start_block_height),
+        sync_mode,
         await_for_node_synced: AwaitForNodeSyncedEnum::StreamWhileSyncing,
         finality: Finality::None,
         validate_genesis: false,
@@ -42,7 +151,7 @@ fn test_indexer_basic() {
     let shard_tracker = node.client(env.test_loop_data()).shard_tracker.clone();
     let store_config =
         StoreConfig { path: Some(indexer_config.home_dir.clone()), ..Default::default() };
-    let (sender, mut receiver) = tokio::sync::mpsc::channel(100);
+    let (sender, receiver) = tokio::sync::mpsc::channel(100);
     let future = start(
         node_data.view_client_sender.clone().into(),
         node_data.client_sender.clone().into(),
@@ -53,25 +162,46 @@ fn test_indexer_basic() {
         env.test_loop.clock(),
     );
     env.test_loop.future_spawner("Indexer").spawn("main indexer loop", future);
-    let last_block_height = 10;
-    let mut block_heights_received = vec![];
+    receiver
+}
+
+fn receive_indexer_message(
+    env: &mut TestLoopEnv,
+    indexer_receiver: &mut mpsc::Receiver<StreamerMessage>,
+) -> StreamerMessage {
+    let mut ret: Option<StreamerMessage> = None;
     env.test_loop.run_until(
         |_| {
-            if let Ok(msg) = receiver.try_recv() {
-                let height = msg.block.header.height;
-                block_heights_received.push(height);
-                height == last_block_height
+            if let Ok(msg) = indexer_receiver.try_recv() {
+                ret = Some(msg);
+                true
             } else {
                 false
             }
         },
         Duration::seconds(20),
     );
+    ret.unwrap()
+}
 
-    assert_eq!(
-        block_heights_received,
-        (start_block_height..=last_block_height).collect::<Vec<_>>()
-    );
-
+fn shutdown(env: TestLoopEnv) {
     env.shutdown_and_drain_remaining_events(Duration::seconds(20));
+}
+
+fn create_local_tx(env: &TestLoopEnv) -> SignedTransaction {
+    let rpc_node = TestLoopNode::rpc(&env.node_datas);
+    let block_hash = rpc_node.head(env.test_loop_data()).last_block_hash;
+    let nonce = 1;
+    let tx = SignedTransaction::call(
+        nonce,
+        user_account(),
+        user_account(),
+        &create_user_test_signer(&user_account()),
+        Balance::ZERO,
+        "does_not_matter".to_owned(),
+        vec![],
+        Gas::from_teragas(100),
+        block_hash,
+    );
+    tx
 }

chain/indexer/src/streamer/mod.rs
@@ -2,6 +2,7 @@ use std::collections::{BTreeMap, HashMap};
 use std::sync::Arc;
 
 use near_async::time::{Clock, Duration};
+use near_primitives::version::ProtocolFeature;
 use parking_lot::RwLock;
 use rocksdb::DB;
 use tokio::sync::mpsc;
@@ -47,6 +48,7 @@ pub async fn build_streamer_message(
     let chunks = client.fetch_block_new_chunks(&block, shard_tracker).await?;
 
     let protocol_config_view = client.fetch_protocol_config(block.header.hash).await?;
+    let protocol_version = protocol_config_view.protocol_version;
     let shard_ids = protocol_config_view.shard_layout.shard_ids();
 
     let runtime_config_store = near_parameters::RuntimeConfigStore::new(None);
@@ -84,9 +86,19 @@ pub async fn build_streamer_message(
         let indexer_transactions = transactions
             .into_iter()
             .filter_map(|transaction| {
-                let outcome = outcomes.remove(&transaction.hash)?;
-                debug_assert!(!outcome.execution_outcome.outcome.receipt_ids.is_empty());
-                Some(IndexerTransactionWithOutcome { outcome, transaction })
+                let outcome = outcomes.remove(&transaction.hash);
+                if outcome.is_none()
+                    && ProtocolFeature::InvalidTxGenerateOutcomes.enabled(protocol_version)
+                {
+                    error!(
+                        target: INDEXER,
+                        tx_hash = %transaction.hash,
+                        shard_id = %header.shard_id,
+                        block_hash = %block.header.hash,
+                        "unexpected missing transaction outcome"
+                    );
+                }
+                outcome.map(|outcome| IndexerTransactionWithOutcome { outcome, transaction })
             })
             .collect::<Vec<IndexerTransactionWithOutcome>>();
         // All transaction outcomes have been removed.

core/primitives/src/receipt.rs
@@ -401,6 +401,29 @@ impl ReceiptPriority {
 }
 
 impl Receipt {
+    pub fn from_tx(
+        receipt_id: CryptoHash,
+        tx_signer_id: AccountId,
+        tx_receiver_id: AccountId,
+        signer_public_key: PublicKey,
+        gas_price: Balance,
+        actions: Vec<Action>,
+    ) -> Self {
+        Receipt::V0(ReceiptV0 {
+            predecessor_id: tx_signer_id.clone(),
+            receiver_id: tx_receiver_id,
+            receipt_id,
+            receipt: ReceiptEnum::Action(ActionReceipt {
+                signer_id: tx_signer_id,
+                signer_public_key,
+                gas_price,
+                output_data_receivers: vec![],
+                input_data_ids: vec![],
+                actions,
+            }),
+        })
+    }
+
     pub fn receiver_id(&self) -> &AccountId {
         match self {
             Receipt::V0(receipt) => &receipt.receiver_id,

runtime/runtime/src/config.rs
@@ -286,29 +286,35 @@ pub fn tx_cost(
     tx: &Transaction,
     receipt_gas_price: Balance,
 ) -> Result<TransactionCost, IntegerOverflowError> {
-    let sender_is_receiver = tx.receiver_id() == tx.signer_id();
+    calculate_tx_cost(tx.receiver_id(), tx.signer_id(), tx.actions(), config, receipt_gas_price)
+}
+
+pub fn calculate_tx_cost(
+    receiver_id: &AccountId,
+    signer_id: &AccountId,
+    actions: &[Action],
+    config: &RuntimeConfig,
+    receipt_gas_price: Balance,
+) -> Result<TransactionCost, IntegerOverflowError> {
+    let sender_is_receiver = receiver_id == signer_id;
     let fees = &config.fees;
     let mut gas_burnt: Gas = fees.fee(ActionCosts::new_action_receipt).send_fee(sender_is_receiver);
     gas_burnt = gas_burnt.checked_add_result(total_send_fees(
         config,
         sender_is_receiver,
-        tx.actions(),
-        tx.receiver_id(),
+        actions,
+        receiver_id,
     )?)?;
-    let prepaid_gas = total_prepaid_gas(&tx.actions())?
-        .checked_add_result(total_prepaid_send_fees(config, &tx.actions())?)?;
-
+    let prepaid_gas = total_prepaid_gas(&actions)?
+        .checked_add_result(total_prepaid_send_fees(config, &actions)?)?;
     let mut gas_remaining =
         prepaid_gas.checked_add_result(fees.fee(ActionCosts::new_action_receipt).exec_fee())?;
-    gas_remaining = gas_remaining.checked_add_result(total_prepaid_exec_fees(
-        config,
-        tx.actions(),
-        tx.receiver_id(),
-    )?)?;
+    gas_remaining =
+        gas_remaining.checked_add_result(total_prepaid_exec_fees(config, actions, receiver_id)?)?;
     let burnt_amount = safe_gas_to_balance(receipt_gas_price, gas_burnt)?;
     let remaining_gas_amount = safe_gas_to_balance(receipt_gas_price, gas_remaining)?;
     let mut total_cost = safe_add_balance(burnt_amount, remaining_gas_amount)?;
-    total_cost = safe_add_balance(total_cost, total_deposit(&tx.actions())?)?;
+    total_cost = safe_add_balance(total_cost, total_deposit(actions)?)?;
     Ok(TransactionCost { gas_burnt, gas_remaining, receipt_gas_price, total_cost, burnt_amount })
 }
 

runtime/runtime/src/lib.rs
@@ -43,7 +43,7 @@ use near_primitives::errors::{
 };
 use near_primitives::hash::CryptoHash;
 use near_primitives::receipt::{
-    ActionReceipt, DataReceipt, PromiseYieldIndices, PromiseYieldTimeout, Receipt, ReceiptEnum,
+    DataReceipt, PromiseYieldIndices, PromiseYieldTimeout, Receipt, ReceiptEnum,
     ReceiptOrStateStoredReceipt, ReceiptV0, ReceivedData, VersionedActionReceipt,
     VersionedReceiptEnum,
 };
@@ -1759,19 +1759,14 @@ impl Runtime {
                     tx_hash,
                     processing_state.apply_state.block_height,
                 );
-                let receipt = Receipt::V0(ReceiptV0 {
-                    predecessor_id: signer_id.clone(),
-                    receiver_id: tx.transaction.receiver_id().clone(),
+                let receipt = Receipt::from_tx(
                     receipt_id,
-                    receipt: ReceiptEnum::Action(ActionReceipt {
-                        signer_id: signer_id.clone(),
-                        signer_public_key: pubkey.clone(),
-                        gas_price: verification_result.receipt_gas_price,
-                        output_data_receivers: vec![],
-                        input_data_ids: vec![],
-                        actions: tx.transaction.actions().to_vec(),
-                    }),
-                });
+                    signer_id.clone(),
+                    tx.transaction.receiver_id().clone(),
+                    pubkey.clone(),
+                    verification_result.receipt_gas_price,
+                    tx.transaction.actions().to_vec(),
+                );
                 let gas_burnt = verification_result.gas_burnt;
                 let compute_usage = gas_burnt.as_gas();
                 let outcome = ExecutionOutcomeWithId {

test-loop-tests/src/tests/mod.rs
@@ -21,6 +21,7 @@ mod global_contracts;
 mod global_contracts_distribution;
 mod in_memory_tries;
 mod increase_max_congestion_missing_chunks;
+#[cfg(feature = "test_features")]
 mod indexer;
 mod malicious_chunk_producer;
 mod max_receipt_size;

test-loop-tests/src/utils/node.rs
@@ -18,6 +18,7 @@ use near_primitives::views::{
 };
 
 use crate::setup::state::NodeExecutionData;
+use crate::utils::account::{create_validator_id, rpc_account_id};
 use crate::utils::transactions::TransactionRunner;
 
 /// Represents single node in multinode test loop setup. It simplifies
@@ -37,6 +38,14 @@ impl<'a> TestLoopNode<'a> {
             .rfind(|data| &data.account_id == account_id)
             .unwrap_or_else(|| panic!("client with account id {account_id} not found"));
         Self { data }
+    }
+
+    pub fn rpc(node_datas: &'a [NodeExecutionData]) -> Self {
+        Self::for_account(node_datas, &rpc_account_id())
+    }
+
+    pub fn validator(node_datas: &'a [NodeExecutionData], validator_index: usize) -> Self {
+        Self::for_account(node_datas, &create_validator_id(validator_index))
     }
 
     #[allow(unused)]

commit 54f9fa408d4b998c7cf5cfd641883c726b2986fb

fix(indexer): properly set actions for local receipts (#14840)
Currently we convert action views from transaction view to `Action` and
then back to `ActionView` when creating `ReceiptView`:
```
        let actions: Vec<_> =
            tx.actions.iter().cloned().map(Action::try_from).map(Result::unwrap).collect();
        ...
        let receipt = Receipt::from_tx(
            ...
            actions,
        );
        let receipt_view: ReceiptView = receipt.into();
```

This results in `code` as part of `ActionView::DeployContract` being set
to `hash(hash(contract_code))` instead of intended `hash(code)`. The
same applies to `DeployGlobalContract` and
`DeployGlobalContractByAccountId` actions.

The regression was introduced in #14426. Also see
[zulip](https://near.zulipchat.com/#narrow/channel/295558-core/topic/nearblocks.20Deploy.20Global.20Contract.20code_hash.20mismatch/with/565061377)
for additional context.

chain/indexer/src/streamer/utils.rs
@@ -3,7 +3,7 @@ use near_parameters::RuntimeConfig;
 use near_primitives::action::Action;
 use near_primitives::receipt::Receipt;
 use near_primitives::types::Balance;
-use near_primitives::views::{ExecutionStatusView, ReceiptView};
+use near_primitives::views::{ExecutionStatusView, ReceiptEnumView, ReceiptView};
 use node_runtime::config::calculate_tx_cost;
 
 use crate::INDEXER;
@@ -33,15 +33,23 @@ pub(crate) fn convert_transactions_sir_into_local_receipts<'a>(
         let cost =
             calculate_tx_cost(&tx.receiver_id, &tx.signer_id, &actions, &runtime_config, gas_price)
                 .unwrap();
+        // Use empty actions here and clone actions from transactions later.
+        // Note that we cannot just pass `actions` here since conversion
+        // ActionView -> Action -> ActionView does not always preserve the
+        // content of the action.
         let receipt = Receipt::from_tx(
             receipt_id,
             tx.signer_id.clone(),
             tx.receiver_id.clone(),
             tx.public_key.clone(),
             cost.receipt_gas_price,
-            actions,
+            vec![],
         );
-        let receipt_view: ReceiptView = receipt.into();
+        let mut receipt_view: ReceiptView = receipt.into();
+        let ReceiptEnumView::Action { actions, .. } = &mut receipt_view.receipt else {
+            unreachable!("transaction is expected to be converted to an action receipt");
+        };
+        actions.clone_from(&indexer_tx.transaction.actions);
         local_receipts.push(receipt_view);
     }
     local_receipts

test-loop-tests/src/tests/indexer.rs
@@ -8,7 +8,10 @@ use near_async::time::Duration;
 use near_client::NetworkAdversarialMessage;
 use near_client::client_actor::AdvProduceChunksMode;
 use near_crypto::Signer;
-use near_indexer::{AwaitForNodeSyncedEnum, IndexerConfig, StreamerMessage, SyncModeEnum, start};
+use near_indexer::{
+    AwaitForNodeSyncedEnum, IndexerConfig, IndexerExecutionOutcomeWithReceipt, StreamerMessage,
+    SyncModeEnum, start,
+};
 use near_o11y::testonly::init_test_logger;
 use near_primitives::gas::Gas;
 use near_primitives::hash::CryptoHash;
@@ -17,7 +20,7 @@ use near_primitives::test_utils::create_user_test_signer;
 use near_primitives::transaction::{ExecutionStatus, SignedTransaction};
 use near_primitives::types::{AccountId, Balance, Finality, Nonce, NumBlocks};
 use near_primitives::version::{PROTOCOL_VERSION, ProtocolFeature};
-use near_primitives::views::ExecutionStatusView;
+use near_primitives::views::{ActionView, ExecutionStatusView, ReceiptEnumView, ReceiptView};
 use near_store::StoreConfig;
 use tokio::sync::mpsc;
 
@@ -170,6 +173,38 @@ fn test_indexer_failed_local_tx() {
     assert_matches!(outcome.status, ExecutionStatusView::Failure(_));
     assert!(outcome.receipt_ids.is_empty());
     assert!(indexer_shard.receipt_execution_outcomes.is_empty());
+
+    shutdown(env);
+}
+
+#[test]
+// TODO(spice): Assess if this test is relevant for spice and if yes fix it.
+#[cfg_attr(feature = "protocol_feature_spice", ignore)]
+fn test_indexer_deploy_contract_local_tx() {
+    init_test_logger();
+    let mut env = setup();
+    deploy_test_contract(&mut env);
+    let validator_node = TestLoopNode::from(&env.node_datas[0]);
+    let deploy_contract_height = validator_node.head(env.test_loop_data()).height;
+
+    let mut indexer_receiver =
+        start_indexer(&env, SyncModeEnum::BlockHeight(deploy_contract_height));
+    let msg = receive_indexer_message(&mut env, &mut indexer_receiver);
+    let indexer_shard = &msg.shards[0];
+    assert_eq!(indexer_shard.chunk.as_ref().unwrap().transactions.len(), 1);
+    let [
+        IndexerExecutionOutcomeWithReceipt {
+            receipt: ReceiptView { receipt: ReceiptEnumView::Action { actions, .. }, .. },
+            ..
+        },
+    ] = indexer_shard.receipt_execution_outcomes.as_slice()
+    else {
+        panic!("expected single action receipt")
+    };
+    let [ActionView::DeployContract { code }] = actions.as_slice() else {
+        panic!("expected single deploy contract action")
+    };
+    assert_eq!(code, CryptoHash::hash_bytes(near_test_contracts::rs_contract()).as_bytes());
 
     shutdown(env);
 }