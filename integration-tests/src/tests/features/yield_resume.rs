6f0752801511e18fd8ec0b43e48bdad4b7497316
Jan Malinowski <jan.ciolek@nearone.org>
2026-01-20 13:33:06 +0000

    basic tests

--- integration-tests/src/tests/features/yield_resume.rs
+++ integration-tests/src/tests/features/yield_resume.rs
@@ -1,3 +1,4 @@
+use near_chain::Error;
 use near_chain_configs::Genesis;
 use near_client::ProcessTxResponse;
 use near_crypto::InMemorySigner;
@@ -5,7 +6,6 @@ use near_o11y::testonly::init_test_logger;
 use near_primitives::hash::CryptoHash;
 use near_primitives::receipt::Receipt;
 use near_primitives::receipt::ReceiptEnum::PromiseResume;
-use near_primitives::receipt::VersionedReceiptEnum::PromiseYield;
 use near_primitives::transaction::{
     Action, DeployContractAction, FunctionCallAction, SignedTransaction,
 };
@@ -31,16 +31,6 @@ fn get_outgoing_receipts_from_latest_block(env: &TestEnv) -> Vec<Receipt> {
         .chain
         .get_outgoing_receipts_for_shard(last_block_hash, shard_id, last_block_height)
         .unwrap()
-}
-
-fn get_promise_yield_data_ids_from_latest_block(env: &TestEnv) -> Vec<CryptoHash> {
-    let mut result = vec![];
-    for receipt in get_outgoing_receipts_from_latest_block(&env) {
-        if let PromiseYield(action_receipt) = receipt.versioned_receipt() {
-            result.push(action_receipt.input_data_ids()[0]);
-        }
-    }
-    result
 }
 
 fn get_promise_resume_data_ids_from_latest_block(env: &TestEnv) -> Vec<CryptoHash> {
@@ -94,9 +84,12 @@ fn prepare_env(test_env_gas_limit: Option<u64>) -> TestEnv {
     env
 }
 
-/// In this test, yield and resume are invoked in separate transactions as quickly as possible.
+/// Submit one transaction which yields and saves data_id to state
+/// Wait 2 blocks
+/// Submit another transaction which reads data_id from state and resumes
+/// Yield-resume should work.
 #[test]
-fn yield_then_resume() {
+fn yield_then_resume_basic() {
     let mut env = prepare_env(None);
     let signer = InMemorySigner::test_signer(&"test0".parse().unwrap());
     let genesis_block = env.clients[0].chain.get_block_by_height(0).unwrap();
@@ -133,7 +126,6 @@ fn yield_then_resume() {
         env.clients[0].chain.get_partial_transaction_result(&yield_tx_hash).unwrap().status,
         FinalExecutionStatus::Started
     );
-    assert_eq!(get_promise_yield_data_ids_from_latest_block(&env).len(), 1);
 
     // Add another transaction invoking `yield_resume`.
     let resume_transaction = SignedTransaction::from_actions(
@@ -168,4 +160,144 @@ fn yield_then_resume() {
         env.clients[0].chain.get_partial_transaction_result(&yield_tx_hash).unwrap().status,
         FinalExecutionStatus::SuccessValue(vec![16u8]),
     );
+}
+
+/// Submit one transaction which yields and saves data_id to state
+/// Immediately submit another transaction which reads data_id from state and resumes
+/// Yield-resume should work.
+#[test]
+fn yield_then_resume_quick() {
+    let mut env = prepare_env(None);
+    let signer = InMemorySigner::test_signer(&"test0".parse().unwrap());
+    let genesis_block = env.clients[0].chain.get_block_by_height(0).unwrap();
+    let mut next_block_height = NEXT_BLOCK_HEIGHT_AFTER_SETUP;
+    let yield_payload = vec![6u8; 16];
+
+    // Add a transaction invoking `yield_create`.
+    let yield_transaction = SignedTransaction::from_actions(
+        200,
+        "test0".parse().unwrap(),
+        "test0".parse().unwrap(),
+        &signer,
+        vec![Action::FunctionCall(Box::new(FunctionCallAction {
+            method_name: "call_yield_create_return_promise".to_string(),
+            args: yield_payload.clone(),
+            gas: Gas::from_teragas(300),
+            deposit: Balance::ZERO,
+        }))],
+        *genesis_block.hash(),
+        0,
+    );
+    let yield_tx_hash = yield_transaction.get_hash();
+    assert_eq!(
+        env.rpc_handlers[0].process_tx(yield_transaction, false, false),
+        ProcessTxResponse::ValidTx
+    );
+
+    // Add another transaction invoking `yield_resume`.
+    let resume_transaction = SignedTransaction::from_actions(
+        201,
+        "test0".parse().unwrap(),
+        "test0".parse().unwrap(),
+        &signer,
+        vec![Action::FunctionCall(Box::new(FunctionCallAction {
+            method_name: "call_yield_resume_read_data_id_from_storage".to_string(),
+            args: yield_payload,
+            gas: Gas::from_teragas(300),
+            deposit: Balance::ZERO,
+        }))],
+        *genesis_block.hash(),
+        0,
+    );
+    assert_eq!(
+        env.rpc_handlers[0].process_tx(resume_transaction, false, false),
+        ProcessTxResponse::ValidTx
+    );
+
+    // Allow the yield create and resume to be included and processed.
+    for _ in 0..5 {
+        env.produce_block(0, next_block_height);
+        next_block_height += 1;
+    }
+
+    // In the next block the callback is executed and the promise resolves to its final result.
+    env.produce_block(0, next_block_height);
+    assert_eq!(
+        env.clients[0].chain.get_partial_transaction_result(&yield_tx_hash).unwrap().status,
+        FinalExecutionStatus::SuccessValue(vec![16u8]),
+    );
+}
+
+/// Submit one transaction with two actions. The first action yields and saves data_id to the state.
+/// The second actions reads data_id from state and resumes.
+/// Yield-resume should work.
+#[test]
+fn yield_then_resume_two_actions() {
+    let mut env = prepare_env(None);
+    let signer = InMemorySigner::test_signer(&"test0".parse().unwrap());
+    let genesis_block = env.clients[0].chain.get_block_by_height(0).unwrap();
+    let mut next_block_height = NEXT_BLOCK_HEIGHT_AFTER_SETUP;
+    let yield_payload = vec![6u8; 16];
+
+    // Add a transaction invoking `yield_create`.
+    let yield_transaction = SignedTransaction::from_actions(
+        200,
+        "test0".parse().unwrap(),
+        "test0".parse().unwrap(),
+        &signer,
+        vec![
+            Action::FunctionCall(Box::new(FunctionCallAction {
+                method_name: "call_yield_create_return_promise".to_string(),
+                args: yield_payload.clone(),
+                gas: Gas::from_teragas(150),
+                deposit: Balance::ZERO,
+            })),
+            Action::FunctionCall(Box::new(FunctionCallAction {
+                method_name: "call_yield_resume_read_data_id_from_storage".to_string(),
+                args: yield_payload,
+                gas: Gas::from_teragas(150),
+                deposit: Balance::ZERO,
+            })),
+        ],
+        *genesis_block.hash(),
+        0,
+    );
+    let yield_tx_hash = yield_transaction.get_hash();
+    assert_eq!(
+        env.rpc_handlers[0].process_tx(yield_transaction, false, false),
+        ProcessTxResponse::ValidTx
+    );
+
+    // Start transaction
+    for _ in 0..2 {
+        env.produce_block(0, next_block_height);
+        next_block_height += 1;
+    }
+
+    // The Resume receipt is sent
+    assert_eq!(get_promise_resume_data_ids_from_latest_block(&env).len(), 1);
+
+    // Transaction not finished yet
+    assert!(matches!(
+        env.clients[0].chain.get_final_transaction_result(&yield_tx_hash),
+        Err(Error::DBNotFoundErr(_))
+    ));
+
+    // Allow the resume to be included and processed.
+    for _ in 0..2 {
+        env.produce_block(0, next_block_height);
+        next_block_height += 1;
+        assert_eq!(get_promise_resume_data_ids_from_latest_block(&env).len(), 0);
+    }
+
+    // yield-resume finises, all receipts done
+    assert_eq!(
+        env.clients[0].chain.get_final_transaction_result(&yield_tx_hash).unwrap().status,
+        FinalExecutionStatus::SuccessValue(vec![1u8]),
+    );
+
+    // TODO - better check that resume succeeded?
+
+    // In the next block the callback is executed and the promise resolves to its final result.
+    env.produce_block(0, next_block_height);
 }
