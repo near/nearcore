216a7ac4a8e4e26ac90025d95a407d2611358fec
Jan Malinowski <jan.ciolek@nearone.org>
2026-02-06 17:34:49 +0000

    protocol upgrade test

--- test-loop-tests/src/tests/yield_resume.rs
+++ test-loop-tests/src/tests/yield_resume.rs
@@ -1,11 +1,16 @@
+use std::collections::BTreeMap;
+use std::ops::Deref;
 use std::str::FromStr;
+use std::sync::Arc;
 
 use assert_matches::assert_matches;
 use near_async::time::Duration;
-use near_chain_configs::test_genesis::ValidatorsSpec;
+use near_chain_configs::test_genesis::{TestEpochConfigBuilder, ValidatorsSpec};
+use near_crypto::Signer;
 use near_o11y::testonly::init_test_logger;
 use near_primitives::action::{Action, FunctionCallAction};
 use near_primitives::block::ChunkType;
+use near_primitives::epoch_manager::{EpochConfig, EpochConfigStore};
 use near_primitives::gas::Gas;
 use near_primitives::hash::CryptoHash;
 use near_primitives::receipt::{Receipt, ReceiptEnum, VersionedReceiptEnum};
@@ -13,6 +18,7 @@ use near_primitives::shard_layout::ShardLayout;
 use near_primitives::test_utils::create_user_test_signer;
 use near_primitives::transaction::SignedTransaction;
 use near_primitives::types::{AccountId, Balance};
+use near_primitives::upgrade_schedule::ProtocolUpgradeVotingSchedule;
 use near_primitives::version::{PROTOCOL_VERSION, ProtocolFeature};
 use near_primitives::views::FinalExecutionStatus;
 
@@ -454,5 +460,203 @@ fn test_yield_then_resume_two_actions_failure() {
     // PromiseYieldStatus change was not committed to the trie.
     assert_no_promise_yield_status_in_state(&env);
 
+    env.shutdown_and_drain_remaining_events(Duration::seconds(20));
+}
+
+// 1 node
+// Upgrade from protocol before InstantYieldResume and YieldResumeImprovements to after.
+// Make sure that a receipt yielded before the protocol upgrade can be resumed after the upgrade.
+// Protocol upgrade setup was taken from `fn test_protocol_upgrade`.
+#[test]
+// TODO(spice-test): Assess if this test is relevant for spice and if yes fix it.
+#[cfg_attr(feature = "protocol_feature_spice", ignore)]
+fn test_yield_resume_across_protocol_upgrade() {
+    init_test_logger();
+
+    let old_protocol = ProtocolFeature::InstantPromiseYield.protocol_version() - 1;
+    let new_protocol = ProtocolFeature::YieldResumeImprovements.protocol_version();
+    let epoch_length = 5;
+
+    // Currently this test runs only on nightly.
+    // If PROTOCOL_VERSION is lower than `new_protocol`, the client will fail with a message like:
+    // "The client protocol version is older than the protocol version of the network: 131 > 84"
+    if PROTOCOL_VERSION < new_protocol {
+        return;
+    }
+
+    // The features are not enabled in the old protocol
+    assert!(!ProtocolFeature::InstantPromiseYield.enabled(old_protocol));
+    assert!(!ProtocolFeature::YieldResumeImprovements.enabled(old_protocol));
+
+    // They are enabled in the new protocol
+    assert!(ProtocolFeature::InstantPromiseYield.enabled(new_protocol));
+    assert!(ProtocolFeature::YieldResumeImprovements.enabled(new_protocol));
+
+    let test_account: AccountId = "test0".parse().unwrap();
+    let test_account_signer: Signer = create_user_test_signer(&test_account).into();
+
+    let shard_layout = ShardLayout::single_shard();
+    let user_accounts = vec![test_account.clone()];
+    let initial_balance = Balance::from_near(1_000_000);
+    let validators_spec = ValidatorsSpec::DesiredRoles {
+        block_and_chunk_producers: vec!["validator0".parse().unwrap()],
+        chunk_validators_only: Vec::new(),
+    };
+    let clients = validators_spec_clients(&validators_spec);
+    let genesis = TestLoopBuilder::new_genesis_builder()
+        .protocol_version(old_protocol)
+        .shard_layout(shard_layout.clone())
+        .epoch_length(epoch_length)
+        .validators_spec(validators_spec.clone())
+        .add_user_accounts_simple(&user_accounts, initial_balance)
+        .build();
+
+    let genesis_epoch_info = TestEpochConfigBuilder::new()
+        .epoch_length(epoch_length)
+        .shard_layout(shard_layout.clone())
+        .validators_spec(validators_spec)
+        .build();
+
+    let mainnet_epoch_config_store = EpochConfigStore::for_chain_id("mainnet", None).unwrap();
+    let old_epoch_config: EpochConfig =
+        mainnet_epoch_config_store.get_config(old_protocol).deref().clone();
+    let new_epoch_config: EpochConfig =
+        mainnet_epoch_config_store.get_config(new_protocol).deref().clone();
+
+    // Adjust the epoch configs for the test
+    let adjust_epoch_config = |mut config: EpochConfig| -> EpochConfig {
+        config.epoch_length = epoch_length;
+        config.num_block_producer_seats = genesis_epoch_info.num_block_producer_seats;
+        config.num_chunk_producer_seats = genesis_epoch_info.num_chunk_producer_seats;
+        config.num_chunk_validator_seats = genesis_epoch_info.num_chunk_validator_seats;
+        config.with_shard_layout(shard_layout.clone())
+    };
+    let old_epoch_config = adjust_epoch_config(old_epoch_config);
+    let new_epoch_config = adjust_epoch_config(new_epoch_config);
+
+    let epoch_config_store = EpochConfigStore::test(BTreeMap::from_iter(vec![
+        (old_protocol, Arc::new(old_epoch_config)),
+        (new_protocol, Arc::new(new_epoch_config)),
+        // Need to pass a config for PROTOCOL_VERSION because some setup code in client asks for the
+        // shard layout in PROTOCOL_VERSION to estimate size of a thread pool. Not used in the test
+        // itself.
+        (PROTOCOL_VERSION, mainnet_epoch_config_store.get_config(PROTOCOL_VERSION).clone()),
+    ]));
+
+    // Immediately start voting for the new protocol version
+    let protocol_upgrade_schedule = ProtocolUpgradeVotingSchedule::new_immediate(new_protocol);
+
+    let mut env = TestLoopBuilder::new()
+        .genesis(genesis)
+        .epoch_config_store(epoch_config_store)
+        .protocol_upgrade_schedule(protocol_upgrade_schedule)
+        .clients(clients)
+        .build()
+        .warmup();
+
+    let node = TestLoopNode::for_account(&env.node_datas, &"validator0".parse().unwrap());
+
+    // We're in the epoch corresponding to the old_protocol_version
+    let start_head = node.head(env.test_loop_data());
+    assert_eq!(
+        node.client(env.test_loop_data())
+            .epoch_manager
+            .get_epoch_protocol_version(&start_head.epoch_id)
+            .unwrap(),
+        old_protocol
+    );
+
+    // Deploy the contract
+    let deploy_contract_tx = SignedTransaction::deploy_contract(
+        1,
+        &test_account,
+        near_test_contracts::rs_contract().into(),
+        &test_account_signer,
+        start_head.last_block_hash,
+    );
+    node.run_tx(&mut env.test_loop, deploy_contract_tx, Duration::seconds(5));
+
+    // Perform promise_yield_create
+    let yield_payload = vec![6u8; 16];
+    let yield_transaction = SignedTransaction::from_actions(
+        200,
+        "test0".parse().unwrap(),
+        "test0".parse().unwrap(),
+        &test_account_signer,
+        vec![Action::FunctionCall(Box::new(FunctionCallAction {
+            method_name: "call_yield_create_return_promise".to_string(),
+            args: yield_payload.clone(),
+            gas: Gas::from_teragas(300),
+            deposit: Balance::ZERO,
+        }))],
+        start_head.last_block_hash,
+    );
+    let yield_tx_hash = yield_transaction.get_hash();
+    node.submit_tx(yield_transaction);
+
+    // Allow the yield create to be included and processed.
+    node.run_for_number_of_blocks(&mut env.test_loop, 2);
+    assert_eq!(
+        node.client(env.test_loop_data())
+            .chain
+            .get_partial_transaction_result(&yield_tx_hash)
+            .unwrap()
+            .status,
+        FinalExecutionStatus::Started
+    );
+
+    // Make sure we're still on the old protocol version
+    assert_eq!(
+        node.client(env.test_loop_data())
+            .epoch_manager
+            .get_epoch_protocol_version(&node.head(env.test_loop_data()).epoch_id)
+            .unwrap(),
+        old_protocol
+    );
+
+    // Upgrade to the new protocol version
+    env.test_loop.run_until(
+        |tl_data| {
+            let cur_epoch_protocol_version = node
+                .client(tl_data)
+                .epoch_manager
+                .get_epoch_protocol_version(&node.head(tl_data).epoch_id)
+                .unwrap();
+            cur_epoch_protocol_version == new_protocol
+        },
+        Duration::seconds(30),
+    );
+
+    // Run resume
+    let resume_transaction = SignedTransaction::from_actions(
+        201,
+        "test0".parse().unwrap(),
+        "test0".parse().unwrap(),
+        &test_account_signer,
+        vec![Action::FunctionCall(Box::new(FunctionCallAction {
+            method_name: "call_yield_resume_read_data_id_from_storage".to_string(),
+            args: yield_payload,
+            gas: Gas::from_teragas(300),
+            deposit: Balance::ZERO,
+        }))],
+        start_head.last_block_hash,
+    );
+    let resume_res = node.run_tx(&mut env.test_loop, resume_transaction, Duration::seconds(5));
+    assert_eq!(resume_res, vec![1u8]);
+
+    // Allow the resume to execute
+    node.run_for_number_of_blocks(&mut env.test_loop, 2);
+
+    // Resumed callback executed, transaction finished successfully
+    assert_eq!(
+        node.client(env.test_loop_data())
+            .chain
+            .get_final_transaction_result(&yield_tx_hash)
+            .unwrap()
+            .status,
+        FinalExecutionStatus::SuccessValue(vec![16u8]),
+    );
+
+    assert_no_promise_yield_status_in_state(&env);
     env.shutdown_and_drain_remaining_events(Duration::seconds(20));
 }
