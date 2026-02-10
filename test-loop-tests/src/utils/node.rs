711ff6443eae4e7987a9ab460c49133a9ee7db4b
Darioush Jalali <darioush.jalali@nearone.org>
2026-02-10 05:36:59 -0800

    use genesis execution instead of fallback

--- test-loop-tests/src/utils/node.rs
+++ test-loop-tests/src/utils/node.rs
@@ -100,14 +100,7 @@ impl<'a> TestLoopNode<'a> {
 
     pub fn last_executed(&self, test_loop_data: &TestLoopData) -> Arc<Tip> {
         if ProtocolFeature::Spice.enabled(PROTOCOL_VERSION) {
-            let store = self.client(test_loop_data).chain.chain_store();
-            store.spice_execution_head().unwrap_or_else(|_| {
-                // spice_execution_head is not set until the first block is executed;
-                // return genesis tip as the baseline.
-                let genesis_height = store.get_genesis_height();
-                let header = store.get_block_header_by_height(genesis_height).unwrap();
-                Tip::from_header(&header).into()
-            })
+            self.client(test_loop_data).chain.chain_store().spice_execution_head().unwrap()
         } else {
             self.client(test_loop_data).chain.head().unwrap()
         }
