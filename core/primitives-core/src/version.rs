7c39a994d7bdf6cb3c7f37e03ec48e30d46b463a
Anton Puhach <anton@nearone.org>
2026-02-19 21:12:39 +0100

    chore: set stable protocol version 83

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -511,7 +511,7 @@ pub const PROD_GENESIS_PROTOCOL_VERSION: ProtocolVersion = 29;
 pub const MIN_SUPPORTED_PROTOCOL_VERSION: ProtocolVersion = 80;
 
 /// Current protocol version used on the mainnet with all stable features.
-const STABLE_PROTOCOL_VERSION: ProtocolVersion = 84;
+const STABLE_PROTOCOL_VERSION: ProtocolVersion = 83;
 
 // On nightly, pick big enough version to support all features.
 const NIGHTLY_PROTOCOL_VERSION: ProtocolVersion = 150;
