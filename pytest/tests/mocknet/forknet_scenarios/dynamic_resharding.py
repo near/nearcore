e8757f7c92efffdbb9f6ce502f77e1e17e6e93b1
Adam Wierzbicki <adam.wierzbicki@nearone.org>
2026-03-19 17:35:12 +0100

    fix epoch length and binary URL

--- pytest/tests/mocknet/forknet_scenarios/dynamic_resharding.py
+++ pytest/tests/mocknet/forknet_scenarios/dynamic_resharding.py
@@ -13,9 +13,9 @@ class DynamicResharding(TestSetup):
         self.args.start_height = self.start_height
         self.node_hardware_config = NodeHardware.SameConfig(
             num_chunk_producer_seats=20, num_chunk_validator_seats=20)
-        self.epoch_len = 6000  # ~3h
+        self.epoch_len = 20000  # ~3h
         self.has_state_dumper = False
         self.genesis_protocol_version = 84
         self.has_archival = False
         self.regions = "us-east1,europe-west4,asia-east1,us-west1"
-        self.neard_binary_url = "https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/Linux-x86_64/wiezzel/dynamic-resharding-test/08991eab412b19ce1f3ec699e8ec8c1acd95f9ba/release/neard"
+        self.neard_binary_url = "https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore/Linux-x86_64/wiezzel/dynamic-resharding-test/1923c7fc2094036dcb576e02e2af1f300e8006dd/release/neard"
