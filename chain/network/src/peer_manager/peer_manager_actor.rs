0b937b0bcc4f8eeee4950518fe355422bbd5a2ab
Andrei <122784628+andrei-near@users.noreply.github.com>
2026-03-24 15:40:51 +0000

    same for tier2 connections

--- chain/network/src/peer_manager/peer_manager_actor.rs
+++ chain/network/src/peer_manager/peer_manager_actor.rs
@@ -661,7 +661,7 @@ impl PeerManagerActor {
                         }.await;
 
                         if let Err(ref err) = result {
-                            tracing::info!(target: "network", ?err, %peer_info, "tier2 failed to connect");
+                            tracing::info!(target: "network", %err, %peer_info, "tier2 failed to connect");
                         }
                         state.peer_store.peer_connection_attempt(&clock, &peer_info.id, result);
                     }.instrument(tracing::trace_span!(target: "network", "monitor_peers_trigger_connect"))
