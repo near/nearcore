736416899d348753f8c014b08d5bbd60ac1c5fa4
shreyan-gupta <shreyan.gupta96@gmail.com>
2026-04-07 22:34:51 -0400

    feat(test-loop): convert block_chunk_signature to transport filter
    Replace override handler with transport filter that zeroes the first
    chunk signature in each PeerMessage::Block. Nodes reject tampered
    blocks, causing chain to stall. Remove #[ignore].

--- test-loop-tests/src/tests/block_chunk_signature.rs
+++ test-loop-tests/src/tests/block_chunk_signature.rs
@@ -1,30 +1,34 @@
 use crate::setup::builder::TestLoopBuilder;
 use near_async::time::Duration;
 use near_crypto::Signature;
+use near_network::types::PeerMessage;
 use near_o11y::testonly::init_test_logger;
 use near_primitives::block::Block;
 use near_primitives::block_body::BlockBody;
 use near_primitives::sharding::ShardChunkHeader;
 use std::sync::Arc;
-use std::sync::atomic::AtomicUsize;
 
 const TIMEOUT_SECONDS: i64 = 5;
 
 #[test]
-#[ignore] // TODO: convert override handler to transport filter (iteration 24-26)
 fn block_chunk_signature_rejection() {
     init_test_logger();
 
     let mut env = TestLoopBuilder::new().validators(2, 0).skip_warmup().build();
 
-    let _mutated_blocks = Arc::new(AtomicUsize::new(0));
-
-    // TODO(iteration 24-26): convert to transport message filter.
-    // Override handlers are not supported with the real PeerManagerActor.
-    // for node_data in &env.node_datas {
-    //     let peer_actor = env.test_loop.data.get_mut(&node_data.peer_manager_sender.actor_handle());
-    //     peer_actor.register_override_handler(...);
-    // }
+    // Transport filter that zeroes the first chunk's signature in every block.
+    // Nodes should reject these blocks, causing the chain to stall.
+    env.register_message_filter(move |_from, _to, msg| {
+        if let PeerMessage::Block(block) = msg {
+            let mut block = (**block).clone();
+            if let Some(chunk) = first_chunk_mut(&mut block) {
+                zero_chunk_signature(chunk);
+            }
+            Some(PeerMessage::Block(Arc::new(block)))
+        } else {
+            Some(msg.clone())
+        }
+    });
 
     env.test_loop.run_for(Duration::seconds(TIMEOUT_SECONDS));
 
@@ -35,7 +39,6 @@ fn block_chunk_signature_rejection() {
     assert!(height1 <= 3, "expected node1 to stall after signature tampering");
 }
 
-#[allow(dead_code)] // TODO(iteration 24-26): will be used after transport filter conversion
 fn first_chunk_mut(block: &mut Block) -> Option<&mut ShardChunkHeader> {
     match block {
         Block::BlockV4(block) => match &mut block.body {
@@ -47,7 +50,6 @@ fn first_chunk_mut(block: &mut Block) -> Option<&mut ShardChunkHeader> {
     }
 }
 
-#[allow(dead_code)] // TODO(iteration 24-26): will be used after transport filter conversion
 fn zero_chunk_signature(chunk: &mut ShardChunkHeader) -> bool {
     match chunk {
         ShardChunkHeader::V1(header) => replace_with_zero(&mut header.signature),
@@ -56,7 +58,6 @@ fn zero_chunk_signature(chunk: &mut ShardChunkHeader) -> bool {
     }
 }
 
-#[allow(dead_code)] // TODO(iteration 24-26): will be used after transport filter conversion
 fn replace_with_zero(signature: &mut Signature) -> bool {
     let zero_signature = Signature::empty(signature.key_type());
     if *signature == zero_signature {
