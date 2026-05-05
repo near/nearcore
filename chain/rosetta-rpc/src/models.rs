025002517b80fb9055a96fb166cf991711c2f716
Jakob Meier <inbox@jakobmeier.ch>
2026-05-05 12:52:30 +0200

    remove dead code

--- chain/rosetta-rpc/src/models.rs
+++ chain/rosetta-rpc/src/models.rs
@@ -1083,12 +1083,6 @@ pub(crate) struct SubNetworkIdentifier {
      * pub metadata: Option<serde_json::Value>, */
 }
 
-/// The timestamp of the block in milliseconds since the Unix Epoch. The
-/// timestamp is stored in milliseconds because some blockchains produce blocks
-/// more often than once a second.
-#[derive(Debug, Clone, PartialEq, PartialOrd, ToSchema, serde::Serialize, serde::Deserialize)]
-pub(crate) struct Timestamp(i64);
-
 /// Transactions contain an array of Operations that are attributable to the
 /// same TransactionIdentifier.
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
@@ -1381,13 +1375,6 @@ pub(crate) enum Nep141EventKind {
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub(crate) struct FtMintData {
-    pub owner_id: String,
-    pub amount: String,
-    pub memo: Option<String>,
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub(crate) struct FtTransferData {
     pub old_owner_id: String,
     pub new_owner_id: String,
@@ -1395,12 +1382,6 @@ pub(crate) struct FtTransferData {
     pub memo: Option<String>,
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub(crate) struct FtBurnData {
-    pub owner_id: String,
-    pub amount: String,
-    pub memo: Option<String>,
-}
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 pub(crate) struct FungibleTokenEvent {
     pub standard: String,
@@ -1436,21 +1417,6 @@ pub(crate) struct FtEvent {
     pub decimals: u32,
     pub cause: String,
     pub memo: Option<String>,
-}
-#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
-pub(crate) struct FTMetadataResponse {
-    pub spec: String,
-    pub name: String,
-    pub symbol: String,
-    pub icon: Option<String>,
-    pub reference: Option<String>,
-    pub reference_hash: Option<String>,
-    pub decimals: u32,
-}
-
-#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize, ToSchema)]
-pub(crate) struct FTAccountBalanceResponse {
-    pub amount: u128,
 }
 
 #[cfg(test)]
