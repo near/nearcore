7c64fb37eb432b122b061c068b11886143116827
Darioush Jalali <darioush.jalali@nearone.org>
2026-06-22 10:16:23 -0700

    fix(stateless-validation): fail validation on missing contract code

--- runtime/runtime/src/state_viewer/mod.rs
+++ runtime/runtime/src/state_viewer/mod.rs
@@ -410,6 +410,7 @@ impl TrieViewer {
             &epoch_info_provider.chain_id(),
             AccessOptions::DEFAULT,
         )?;
+        let contract_code_hash = contract_id_resolved.hash();
         let contract =
             pipeline.get_contract(&receipt, contract_id_resolved, 0, view_config.clone());
 
@@ -429,6 +430,7 @@ impl TrieViewer {
         );
         let outcome = execute_function_call(
             contract,
+            contract_code_hash,
             &apply_state,
             &mut runtime_ext,
             originator_id,
