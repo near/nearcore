b33184702e5c9f11e4c377f5e3ea35b7957f3c0d
Darioush Jalali <darioush.jalali@nearone.org>
2026-08-04 20:09:41 -0700

    test(spice): simplify gc test setup

--- test-loop-tests/src/tests/spice/garbage_collection.rs
+++ test-loop-tests/src/tests/spice/garbage_collection.rs
@@ -1,24 +1,19 @@
 use crate::setup::builder::TestLoopBuilder;
-use crate::utils::account::{create_validators_spec, validators_spec_clients_with_rpc};
 use near_async::time::Duration;
+use near_o11y::testonly::init_test_logger;
 use near_primitives::utils::get_block_shard_id_rev;
 use near_store::DBCol;
 
 #[test]
 #[cfg_attr(not(feature = "protocol_feature_spice"), ignore)]
 fn test_spice_chunk_certifying_block_index_is_collected_with_its_block() {
-    let validators_spec = create_validators_spec(2, 0);
-    let clients = validators_spec_clients_with_rpc(&validators_spec);
+    init_test_logger();
+
     let epoch_length = 5;
-    let genesis = TestLoopBuilder::new_genesis_builder()
-        .validators_spec(validators_spec)
-        .epoch_length(epoch_length)
-        .build();
     let mut env = TestLoopBuilder::new()
-        .genesis(genesis)
+        .validators(2, 0)
+        .epoch_length(epoch_length)
         .gc_num_epochs_to_keep(1)
-        .epoch_config_store_from_genesis()
-        .clients(clients)
         .build();
 
     env.node_runner(0).run_until(|node| node.tail() >= 2 * epoch_length, Duration::seconds(30));
