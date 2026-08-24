b4e30fa1cd7c7fa666cd1b3c2f71ab9c094ceb44
Adam Chudaś <18039094+staffik@users.noreply.github.com>
2026-08-24 19:50:00 +0000

    split the reader module into recent, historical and utils

--- chain/client/src/archive/mod.rs
+++ chain/client/src/archive/mod.rs
@@ -1,3 +1,5 @@
-pub mod cloud_archival_reader;
+pub mod cloud_archival_utils;
 pub mod cloud_archival_writer;
+pub mod cloud_historical_reader;
+pub mod cloud_recent_reader;
 pub mod cold_store_actor;
