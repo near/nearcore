233252e3835d0e317843f4d2a96f21edbeae897d
Adam Wierzbicki <adam.wierzbicki@nearone.org>
2026-09-10 17:34:18 +0200

    chore: prepare rc1 testnet release

--- core/primitives/src/version.rs
+++ core/primitives/src/version.rs
@@ -61,7 +61,11 @@ pub fn get_protocol_upgrade_schedule(chain_id: &str) -> ProtocolUpgradeVotingSch
             schedule
         }
         TESTNET => {
-            let schedule = vec![];
+            let v1_protocol_version = 87;
+            // Monday September 21st 00:00 UTC
+            let v1_datetime =
+                ProtocolUpgradeVotingSchedule::parse_datetime("2026-09-21 00:00:00").unwrap();
+            let schedule = vec![(v1_datetime, v1_protocol_version)];
             schedule
         }
         _ => {
