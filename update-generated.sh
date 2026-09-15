457537605f21b6db9bd4d38fe2841be2d1c36a2f
Anton Puhach <anton@nearone.org>
2026-09-15 17:26:06 +0200

    chore(tools): add script to update generated artifacts

---
+++ update-generated.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Updates all generated artifacts checked into the repo.
+
+set -eux
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+cargo run -p near-jsonrpc-openapi-spec > chain/jsonrpc/openapi/openapi.json
+
+set +e
+cargo run -p protocol-schema-check
+schema_status=$?
+set -e
+
+case "$schema_status" in
+    0) ;;
+    1) cp "${CARGO_TARGET_DIR:-target}/protocol_schema.toml" tools/protocol-schema-check/res/protocol_schema.toml ;;
+    *) exit "$schema_status" ;;
+esac
