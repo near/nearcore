0a0d8075de6e849e9d210492708cc75c5f7c20c3
Darioush Jalali <darioush.jalali@nearone.org>
2026-09-16 13:02:28 -0700

    revert: restore legacy VMLogic host functions (#16322)

--- runtime/near-vm-runner/src/logic/mocks/mod.rs
+++ runtime/near-vm-runner/src/logic/mocks/mod.rs
@@ -1,1 +1,2 @@
 pub mod mock_external;
+pub mod mock_memory;
--- runtime/runtime/src/receipt_manager.rs
+++ runtime/runtime/src/receipt_manager.rs
@@ -376,8 +376,7 @@ impl ReceiptManager {
     /// specified, the action should be allocated gas in
     /// [`distribute_unused_gas`](Self::distribute_unused_gas).
     ///
-    /// For more information, see the `promise_batch_action_function_call_weight` host function
-    /// in `near-vm-runner`.
+    /// For more information, see [super::VMLogic::promise_batch_action_function_call_weight].
     ///
     /// # Arguments
     ///
