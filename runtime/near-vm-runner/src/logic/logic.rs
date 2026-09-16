a2d4f583bf69a02ebfd6e16e3b9762ab916ecc17
Darioush Jalali <darioush.jalali@nearone.org>
2026-09-16 13:03:20 -0700

    fix(near-vm-runner): drop is_universal_account_id from VMLogic

--- runtime/near-vm-runner/src/logic/logic.rs
+++ runtime/near-vm-runner/src/logic/logic.rs
@@ -28,9 +28,7 @@ use near_primitives_core::hash::{CryptoHash, YieldId};
 use near_primitives_core::types::{
     AccountId, Balance, Compute, EpochHeight, Gas, GasWeight, StorageUsage,
 };
-use near_primitives_core::universal_account_id::{
-    encode_universal_account_id, is_universal_account_id,
-};
+use near_primitives_core::universal_account_id::encode_universal_account_id;
 use near_primitives_core::universal_state_init::RawStateInit;
 use std::mem::size_of;
 use std::rc::Rc;
@@ -3294,21 +3292,17 @@ bls12381_p2_decompress_base + bls12381_p2_decompress_element * num_elements`
 
         let (receipt_idx, sir) = self.promise_idx_to_receipt_idx_with_sir(promise_idx)?;
         let receiver_id = self.ext.get_receipt_receiver(receipt_idx);
-        // TODO(universal-accounts): replace with an `AccountType::Universal` check
-        // once `near-account-id` supports 0u accounts.
-        let receiver_is_universal =
-            self.config.universal_accounts && is_universal_account_id(receiver_id.as_str());
         let send_fee = transfer_send_fee(
             &self.fees_config,
             sir,
             self.config.eth_implicit_accounts,
-            receiver_is_universal,
+            self.config.universal_accounts,
             receiver_id.get_account_type(),
         );
         let exec_fee = transfer_exec_fee(
             &self.fees_config,
             self.config.eth_implicit_accounts,
-            receiver_is_universal,
+            self.config.universal_accounts,
             receiver_id.get_account_type(),
         );
         let burn_gas = send_fee.gas;
