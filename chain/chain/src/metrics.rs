0358858fe165215dfafb173f7e65db57e8dd0862
Vivienne Siffert <vivienne.siffert@nearone.org>
2026-09-17 14:15:02 +0200

    move gauge update

--- chain/chain/src/metrics.rs
+++ chain/chain/src/metrics.rs
@@ -94,8 +94,8 @@ pub static BLOCK_SPICE_UNCERTIFIED_CHUNKS: LazyLock<IntGauge> = LazyLock::new(||
 pub static SPICE_CERTIFICATION_LAG: LazyLock<IntGauge> = LazyLock::new(|| {
     try_create_int_gauge(
         "near_spice_certification_lag",
-        "Height distance from the block being processed to the oldest block with uncertified \
-         chunks, 0 when nothing older awaits certification",
+        "Height distance from the chain head to the oldest block with uncertified chunks, 0 when \
+         nothing older awaits certification",
     )
     .unwrap()
 });
