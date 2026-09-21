6212a064bc83cf95f534950b8dbf4b852c685b7f
Michael Birch <birchmd8@gmail.com>
2026-09-21 14:11:53 +0000

    feat(runtime): use latest wallet contract (#16401)
    This PR introduces a protocol feature to resolve the previous wallet
    contract global hash into the newly deployed one. This will
    automatically make all eth-implicit accounts start using the new wallet
    contract once the protocol feature is enabled.

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -493,6 +493,10 @@ pub enum ProtocolFeature {
     /// action, which creates an account whose ID is derived from its canonical state
     /// init (contract code, storage, and access keys).
     UniversalAccounts,
+    /// Use the new version of the eth-wallet contract.
+    /// If an account references the global contract hash of the old eth-wallet
+    /// contract then it will automatically resolve to the new version instead.
+    UpdatedEthWalletContract,
 }
 
 impl ProtocolFeature {
@@ -632,6 +636,7 @@ impl ProtocolFeature {
             ProtocolFeature::GlobalContractSameChunkCallFix => 87,
             ProtocolFeature::UniversalAccounts => 87,
             ProtocolFeature::FailCallToMissingGlobalContract => 88,
+            ProtocolFeature::UpdatedEthWalletContract => 88,
 
             // Nightly features:
             ProtocolFeature::FixContractLoadingCost => 129,
